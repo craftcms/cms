b3988c4691f0df86c3e91a7aa637f217bab6128d
Brad Bell <takobell@gmail.com>
2011-11-08 15:36:25 -0800

    Add default site to mysql data.

--- src/modules/install/business/MySqlData.sql
+++ src/modules/install/business/MySqlData.sql
@@ -2,4 +2,10 @@
 	Data for the table `groups`
 */
 
-INSERT INTO `blx_groups` (`Name`,`Description`) VALUES ('Administrator', 'This group has the real ultimate power.');
+INSERT INTO `blx_groups` (`name`,`description`) VALUES ('Administrator', 'This group has the real ultimate power.');
+
+/*
+	Data for the table `sites`
+*/
+
+INSERT INTO `blx_sites` (`handle`,`label`,`url`) VALUES ('default', 'Default Site', 'blxbox.dev');
