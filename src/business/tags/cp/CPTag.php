b6ec2bd7676d59f4c5fa664a420f9d6077c58fcd
Brandon Kelly <me@brandon-kelly.com>
2011-11-14 22:14:28 -0800

    Do away with the CPSectionsTag class, and just make CPTag::sections() return an ArrayTag instead

--- src/business/tags/cp/CPTag.php
+++ src/business/tags/cp/CPTag.php
@@ -2,9 +2,24 @@
 
 class CPTag extends Tag
 {
+	private static $defaultSections = array(
+		'dashboard' => 'Dashboard',
+		'content' => 'Content',
+		'assets' => 'Assets',
+		'settings' => 'Settings',
+		'guide' => 'User Guide',
+	);
+
 	public function sections()
 	{
-		return new CPSectionsTag();
+		$sectionTags = array();
+
+		foreach (self::$defaultSections as $handle => $name)
+		{
+			$sectionTags[] = new CPSectionTag($handle, $name);
+		}
+
+		return new ArrayTag($sectionTags);
 	}
 
 	public function baseUrl()
