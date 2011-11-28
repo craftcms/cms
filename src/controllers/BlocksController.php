8f5f26ce6e0156016e1f7ab9c46667536adde714
Brad Bell <takobell@gmail.com>
2011-11-28 15:47:56 -0800

    If we successfully find a controller, we test for a given action method.  If it exists we use that.

--- src/controllers/BlocksController.php
+++ src/controllers/BlocksController.php
@@ -8,58 +8,51 @@ class BlocksController extends BaseController
 	{
 		$this->_templateMatch = Blocks::app()->urlManager->getTemplateMatch();
 
-		if ($this->_templateMatch !== null || Blocks::app()->request->getParam('c', null) !== null)
+		if ($this->_templateMatch !== null)
 		{
-			if ($this->_templateMatch !== null)
-			{
-				$tempController = $this->_templateMatch->getRelativePath();
-				$tempAction = $this->_templateMatch->getFileName();
-			}
-			else
-			{
-				$tempController = Blocks::app()->request->getParam('c');
-				$pathSegs = Blocks::app()->request->getPathSegments();
-				$tempAction = $pathSegs[0];
-			}
-
-			// we found a matching controller for this request.
-			if (($ca = Blocks::app()->createController($tempController)) !== null)
-			{
-				$this->setRequestController($ca[0]);
-				// save the current controller and swap out the new one.
-				$oldController = Blocks::app()->getController();
-				$newController = $this->getRequestController();
-				Blocks::app()->setController($newController);
-
-				if (($action = $newController->createAction($tempAction)) !== null)
-				{
-					if (($parent = $newController->getModule()) === null)
-						$parent = Blocks::app();
-
-					if ($parent->beforeControllerAction($newController, $action))
-					{
-						$newController->runActionWithFilters($action, $newController->filters());
-						$parent->afterControllerAction($newController, $action);
-					}
-				}
-				else
-				{
-					// controller request, but no action specified, so load the template.
-					$this->loadTemplate($tempAction);
-				}
-
-				Blocks::app()->setController($oldController);
-			}
-			// no matching controller, so load the template.
-			else
-			{
-				parent::run($actionId);
-				$this->loadTemplate($tempController.'/'.$tempAction);
-			}
+			$tempController = $this->_templateMatch->getRelativePath();
+			$tempAction = $this->_templateMatch->getFileName();
 		}
 		else
 		{
-			throw new BlocksHttpException('404', 'Page not found.');
+			$tempController = Blocks::app()->request->getParam('c');
+			$pathSegs = Blocks::app()->request->getPathSegments();
+			$tempAction = $pathSegs[0];
+		}
+
+		// we found a matching controller for this request.
+		if (($ca = Blocks::app()->createController($tempController)) !== null)
+		{
+			$this->setRequestController($ca[0]);
+			// save the current controller and swap out the new one.
+			$oldController = Blocks::app()->getController();
+			$newController = $this->getRequestController();
+			Blocks::app()->setController($newController);
+
+			if (($action = $newController->createAction($tempAction)) !== null)
+			{
+				if (($parent = $newController->getModule()) === null)
+					$parent = Blocks::app();
+
+				if ($parent->beforeControllerAction($newController, $action))
+				{
+					$newController->runActionWithFilters($action, $newController->filters());
+					$parent->afterControllerAction($newController, $action);
+				}
+			}
+			else
+			{
+				// controller request, but no action specified, so load the template.
+				$this->loadTemplate($tempAction);
+			}
+
+			Blocks::app()->setController($oldController);
+		}
+		// no matching controller, so load the template.
+		else
+		{
+			parent::run($actionId);
+			$this->loadTemplate($tempController.'/'.$tempAction);
 		}
 	}
 
