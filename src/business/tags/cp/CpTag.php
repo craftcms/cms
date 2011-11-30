b749b6202c1e40948f51999ee4c288a1ce649d2e
Brandon Kelly <me@brandon-kelly.com>
2011-11-30 13:23:39 -0800

    Ajaxify the update notifications

--- src/business/tags/cp/CpTag.php
+++ src/business/tags/cp/CpTag.php
@@ -53,4 +53,66 @@ class CpTag extends Tag
 	{
 		return new BoolTag(true);
 	}
+
+	public function updates()
+	{
+		return new ArrayTag(array(
+			new ArrayTag(array(
+				'name' => 'Blocks Standard',
+				'version' => '1.1',
+				'notes' => '<h5>Blocks 1.1</h5>
+					<ul>
+						<li>Added a Plugin Store tab to the Control Panel.</li>
+						<li>Added a new global search input to the nav bar.</li>
+						<li>Plugins can now add new sections to the Control Panel.</li>
+					</ul>
+					<h5>Blocks 1.0 build 218</h5>
+					<ul>
+						<li>Fixed a caching issue with the Feed widget.</li>
+					</ul>'
+			)),
+			new ArrayTag(array(
+				'name' => 'Wygwam',
+				'version' => '1.1',
+				'notes' => '<h5>Wygwam 1.1</h5>
+					<ul>
+						<li>Added file browsing and uploading support.</li>
+						<li>Made it possible to toggle individual toolbar buttons.</li>
+						<li>Added a field height setting.</li>
+					</ul>'
+			)),
+			new ArrayTag(array(
+				'name' => 'Brilliant Retail',
+				'version' => '1.0.1.0',
+				'notes' => '<h5>Brilliant Retail 1.0.1.0</h5>
+					<ul>
+						<li>Added New Payment Gateway: PayPal Website Payment Standard</li>
+						<li>Added New Payment Gateway: eWay Australia</li>
+						<li>Added New Payment Gateway: PsiGate </li>
+						<li>Added New Shipping Method: \'Pickup In store\' Shipping Option (i.e. No Shipping)</li>
+						<li>Added New Report: Customer Search History Report</li>
+						<li>Added New Report: List of Orders by Customer over time</li>
+						<li>Added New Report: Product Best Sellers Report</li>
+						<li>Added purchase_version number tag to customer_download tag</li>
+						<li>Added cancel status to order</li>
+						<li>Added additional developer hooks</li>
+						<li>Updated category_menu tag to include sort parameter (accepts sort|title)</li>
+						<li>Updated image tag to include alt parameter</li>
+						<li>Updated all currency instances to pass through currency_round function</li>
+						<li>Updated exp_br_state table so state_id start at 1 and are now standard across all installations</li>
+						<li>Updated USPS with additional configuration options</li>
+						<li>Fixed shipping method pre calculations for product weight</li>
+						<li>Fixed path_separator and directory_separator on Zend library include in core file</li>
+						<li>Fixed Brilliant_retail fieldtype to allow multiple instances per page</li>
+						<li>Fixed quantity issue on cart_update and cart_add. Forced integers. </li>
+						<li>Fixed download_version tag displaying improper number in customer_download tag</li>
+						<li>Fixed issue with general sales report total</li>
+						<li>Fixed issue with javascript file names in the WYSIWYG editor</li>
+						<li>Fixed the cart_items tag to account for quantity of items </li>
+						<li>Removed countries that are not recognized by PayPal</li>
+						<li>Misc updates to Blank Theme</li>
+					</ul>'
+			))
+		));
+	}
 }
--- src/resources/scripts/updates.js
+++ src/resources/scripts/updates.js
@@ -1,61 +1,66 @@
 (function($) {
 
 
-function hideSpinner()
-{
-	$('#checking').fadeOut();
-	setTimeout(showUpdates, 200);
-}
+window.Update = Base.extend({
 
-function showUpdates()
-{
+	dom: null,
+	expanded: false,
+
+	constructor: function(div, i)
+	{
+		this.dom = {};
+		this.dom.$update = $(div);
+		this.dom.$toggle = $('.notes-toggle', this.dom.$update);
+		this.dom.$notesContainer = $('.notes-container', this.dom.$update);
+		this.dom.$notes = $('.notes', this.dom.$notesContainer);
+
+		this.dom.$toggle.on('click.update', $.proxy(this, 'toggle'));
+
+		setTimeout($.proxy(this, 'fadeIn'), i * 100);
+	},
+
+	fadeIn: function()
+	{
+		this.dom.$update.fadeIn();
+	},
+
+	toggle: function()
+	{
+		if (!this.expanded)
+			this.expand();
+		else
+			this.collapse();
+	},
+
+	expand: function()
+	{
+		var height = this.dom.$notes.outerHeight();
+		this.dom.$notesContainer.stop().animate({height: height}, $.proxy(function() {
+			//this.dom.$notesContainer.height('auto');
+		}, this));
+
+		this.dom.$toggle.html('Hide release notes');
+		this.expanded = true;
+	},
+
+	collapse: function()
+	{
+		this.dom.$notesContainer.stop().animate({height: 0});
+		this.dom.$toggle.html('Show release notes');
+		this.expanded = false;
+	}
+});
+
+
+var updatesUrl = blx.baseUrl+'/settings/updates/updates';
+$('#updates').load(updatesUrl, function() {
+	$('#checking').fadeOut();
 	$('#updates').fadeIn(function() {
-		var $updates = $('.update', this);
-		$updates.each(function(i) {
-			var $update = $(this);
-			setTimeout(function() {
-				$update.fadeIn(initUpdate);
-			}, i * 100);
+		$('.update', this).each(function(i) {
+			var update = new Update(this, i);
 		});
 	});
-}
-
-function initUpdate()
-{
-	var $update = $(this),
-		$toggle = $('.notes-toggle', $update),
-		$notes = $('.notes', $update),
-		expanded = false;
-
-	$toggle.click(function() {
-		if (!expanded)
-		{
-			var collapsedHeight = $update.height();
-			$notes.show();
-			var expandedHeight = $update.height();
-			$update.height(collapsedHeight);
-			$update.stop().animate({height: expandedHeight}, function() {
-				$update.height('auto');
-			});
-			$toggle.html('Hide release notes');
-		}
-		else
-		{
-			$notes.hide();
-			var collapsedHeight = $update.height();
-			$notes.show();
-			$update.stop().animate({height: collapsedHeight}, function() {
-				$update.height('auto');
-				$notes.hide();
-			});
-			$toggle.html('Show release notes');
-		}
-
-		expanded = !expanded;
-	});
-}
-
-setTimeout(hideSpinner, 1000);
+});
 
 
 })(jQuery);
