7e5b9ff640edd2287fb45ab21d3f9b7c50545d18
Brad Bell <takobell@gmail.com>
2011-12-07 12:01:51 -0800

    Clean-up blocksController.

--- src/controllers/BlocksController.php
+++ src/controllers/BlocksController.php
@@ -6,54 +6,61 @@ class BlocksController extends BaseController
 
 	public function run($actionId)
 	{
-
 		// if there is a C && A in the URL params, attempt to create that controller and action
 		// if there is a template match, then just use that.
 		$requestController = Blocks::app()->request->getParam('c', null);
 		$requestAction = Blocks::app()->request->getParam('a', null);
+//		$requestModule = Blocks::app()->request->getParam('m', null);
+
+//		$parent = Blocks::app();
+//		if (Blocks::app()->urlManager->currentModule !== null)
+//		{
+//			$parent = Blocks::app()->urlManager->currentModule;
+//			$requestController
+//		}
 
 		if ($requestController !== null && $requestAction !== null)
-		{
-			// we found a matching controller for this request.
-			if (($ca = Blocks::app()->createController($requestController)) !== null)
 			{
-				$newController = $ca[0];
-
-				if (($action = $newController->createAction($requestAction)) !== null)
+				// we found a matching controller for this request.
+				if (($ca = Blocks::app()->createController($requestController)) !== null)
 				{
-					$this->setRequestController($newController);
+					$newController = $ca[0];
 
-					if (($parent = $newController->getModule()) === null)
-						$parent = Blocks::app();
-
-					if ($parent->beforeControllerAction($newController, $action))
+					if (($action = $newController->createAction($requestAction)) !== null)
 					{
-						$newController->runActionWithFilters($action, $newController->filters());
-						$parent->afterControllerAction($newController, $action);
+						$this->setRequestController($newController);
+
+						if (($parent = $newController->getModule()) === null)
+							$parent = Blocks::app();
+
+						if ($parent->beforeControllerAction($newController, $action))
+						{
+							$newController->runActionWithFilters($action, $newController->filters());
+							$parent->afterControllerAction($newController, $action);
+						}
 					}
 				}
 			}
-		}
-		else
-		{
-			$this->_templateMatch = Blocks::app()->urlManager->getTemplateMatch();
-
-			if ($this->_templateMatch !== null)
-			{
-				parent::run($actionId);
-				$this->loadTemplate($this->_templateMatch->getRelativePath().'/'.$this->_templateMatch->getFileName());
-
-
-				//$tempController = $this->_templateMatch->getRelativePath();
-				//$tempAction = $this->_templateMatch->getFileName();
-			}
 			else
 			{
-				//$tempController = Blocks::app()->request->getParam('c');
-				//$pathSegs = Blocks::app()->request->getPathSegments();
-				//$tempAction = $pathSegs[0];
+				$this->_templateMatch = Blocks::app()->urlManager->getTemplateMatch();
+
+				if ($this->_templateMatch !== null)
+				{
+					parent::run($actionId);
+					$this->loadTemplate($this->_templateMatch->getRelativePath().'/'.$this->_templateMatch->getFileName());
+
+					//$tempController = $this->_templateMatch->getRelativePath();
+					//$tempAction = $this->_templateMatch->getFileName();
+				}
+				else
+				{
+					//$tempController = Blocks::app()->request->getParam('c');
+					//$pathSegs = Blocks::app()->request->getPathSegments();
+					//$tempAction = $pathSegs[0];
+				}
 			}
-		}
+
 
 		// save the current controller and swap out the new one.
 		// $oldController = Blocks::app()->getController();
