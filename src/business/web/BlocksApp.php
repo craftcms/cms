abe93198ab149c88feee44aea22ef9eadfcd1fc3
Brad Bell <takobell@gmail.com>
2011-12-23 15:58:58 -0800

    Add a null guard check to before resourceProcessor is initialized.

--- src/business/web/BlocksApp.php
+++ src/business/web/BlocksApp.php
@@ -16,7 +16,7 @@ class BlocksApp extends CWebApplication
 		{
 			$segs = $this->request->pathSegments;
 
-			if (array_shift($segs) == $this->config('resourceTriggerWord'))
+			if (($this->config('resourceTriggerWord')) !== null && (array_shift($segs) == $this->config('resourceTriggerWord')))
 			{
 				$pluginHandle = array_shift($segs);
 
