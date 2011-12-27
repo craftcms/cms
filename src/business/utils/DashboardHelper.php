472209edd0327bfa43747574cb54b512060a067b
Brandon Kelly <me@brandon-kelly.com>
2011-12-27 13:32:37 -0800

    Only fetch update info from DashboardHelper::getAlerts() if $force is true

--- src/business/utils/DashboardHelper.php
+++ src/business/utils/DashboardHelper.php
@@ -3,18 +3,21 @@
 class DashboardHelper
 {
 	// TODO: Eventually stuff like this should go into a global messaging service.
-	public static function getAlerts()
+	public static function getAlerts($fetch = false)
 	{
-		$blocksUpdateInfo = Blocks::app()->update->updateInfo;
-
 		$alerts = array();
 
-		if (Blocks::app()->site->getLicenseKeyStatus() == LicenseKeyStatus::InvalidKey)
-			$alerts[] = 'The license key you’re using isn’t authorized to run Blocks '.Blocks::getEdition().' on '.Blocks::app()->request->getServerName().'. <a href="">Manage my licenses</a>';
+		if (Blocks::app()->update->isUpdateInfoCached() || $fetch)
+		{
+			$blocksUpdateInfo = Blocks::app()->update->updateInfo;
 
-		if ($blocksUpdateInfo->newerReleases !== null && count($blocksUpdateInfo->newerReleases) > 0)
-			if (Blocks::app()->update->criticalBlocksUpdateAvailable($blocksUpdateInfo->newerReleases))
-				$alerts[] = 'There is a critical update for Blocks available. '.BlocksHtml::link('Update now', 'settings/updates');
+			if (Blocks::app()->site->getLicenseKeyStatus() == LicenseKeyStatus::InvalidKey)
+				$alerts[] = 'The license key you’re using isn’t authorized to run Blocks '.Blocks::getEdition().' on '.Blocks::app()->request->getServerName().'. <a href="">Manage my licenses</a>';
+
+			if ($blocksUpdateInfo->newerReleases !== null && count($blocksUpdateInfo->newerReleases) > 0)
+				if (Blocks::app()->update->criticalBlocksUpdateAvailable($blocksUpdateInfo->newerReleases))
+					$alerts[] = 'There is a critical update for Blocks available. '.BlocksHtml::link('Update now', 'settings/updates');
+		}
 
 		return $alerts;
 	}
