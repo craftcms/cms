ec68dc6d093cab794866ead7ba7855c58d7e1724
Brandon Kelly <me@brandon-kelly.com>
2012-01-07 08:55:08 -0800

    Added getHasMany() to ContentBlocks datatype

--- src/datatypes/ContentBlocks.php
+++ src/datatypes/ContentBlocks.php
@@ -2,6 +2,15 @@
 
 class ContentBlocks extends BlocksDataType
 {
+	public function getHasMany()
+	{
+		$hasMany = array();
+
+		// add all datatypes that have blocks
+
+		return $hasMany;
+	}
+
 	private static $attributes = array(
 		'handle'       => array('type' => AttributeType::String, 'maxSize' => 150, 'required' => true),
 		'label'        => array('type' => AttributeType::String, 'maxSize' => 500, 'required' => true),
