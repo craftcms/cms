6d6b200b88bc4f7a6b71ec3248f5ed62ef9522a4
Brad Bell <takobell@gmail.com>
2012-01-09 14:32:03 -0800

    Added a urlFormat config option that accepts Auto, PathInfo or QueryString
    Added a pathVar config option.

--- src/config/defaults.php
+++ src/config/defaults.php
@@ -23,5 +23,17 @@ $blocksConfig['cacheTimeSeconds'] = 86400;
 /*
  *
  */
-$blocksConfig['devCacheTimeSeconds'] = 5;
+$blocksConfig['devCacheTimeSeconds'] = 120;
 
+/*
+ *  If set to UrlFormat::Auto, Blocks will attempt to check if the web server's PATH_INFO is enabled.
+ *  If set to UrlFormat::PathInfo, URLs will be treated in path format (index.php/path/to/file).
+ *  If set to UrlFormat::QueryString, URLs will be treated as such (index.php?{routeVar}=path/to/file).
+ */
+$blocksConfig['urlFormat'] = UrlFormat::Auto
+;
+
+/*
+ * The variable to use when specifying paths in non PATH_INFO format.
+ */
+$blocksConfig['pathVar'] = 'p';
