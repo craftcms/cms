9824035d6472c3c5588d056fe06dffb2d8b76b11
Brandon Kelly <me@brandon-kelly.com>
2012-01-18 13:24:12 -0800

    Removed DriverMap.php

--- src/config/test.php
+++ src/config/test.php
@@ -22,7 +22,7 @@ return CMap::mergeArray(
 				'password'          => $dbConfig['password'],
 				'charset'           => $dbConfig['charset'],
 				'tablePrefix'       => rtrim($dbConfig['tablePrefix'], '_').'_',
-				'driverMap'         => getDbDriverMap(),
+				'driverMap'         => array('mysql' => 'BlocksMysqlSchema'),
 			),
 		),
 
