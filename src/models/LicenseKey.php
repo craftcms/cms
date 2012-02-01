e03396cc33742ac3cafb154e71613a48940d8dd4
Brandon Kelly <me@brandon-kelly.com>
2012-02-01 15:36:05 -0800

    Added pattern matching to the license key

--- src/models/LicenseKey.php
+++ src/models/LicenseKey.php
@@ -9,6 +9,22 @@ class LicenseKey extends BaseModel
 	protected $tableName = 'licensekeys';
 
 	protected $attributes = array(
-		'key' => array('type' => AttributeType::Char, 'length' => 36, 'required' => true, 'unique' => true)
+		'key' => array('type' => AttributeType::Char, 'length' => 36, 'matchPattern' => '/[\w0-9]{8}-[\w0-9]{4}-[\w0-9]{4}-[\w0-9]{4}-[\w0-9]{12}/', 'required' => true, 'unique' => true)
 	);
+
+	public function rules()
+	{
+		// Remove the length=36 validation rule, since that's implied by matchPattern
+		$rules = parent::rules();
+		foreach ($rules as $i => $rule)
+		{
+			if ($rule[0] == 'key' && $rule[1] == 'length')
+			{
+				array_splice($rules, $i, 1);
+				break;
+			}
+		}
+
+		return $rules;
+	}
 }
