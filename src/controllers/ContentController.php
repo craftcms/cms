87119b13ecaaae75e41829387ad0879fe8d9e2db
Brandon Kelly <me@brandon-kelly.com>
2012-02-06 11:32:15 -0800

    Basic section management

--- src/controllers/ContentController.php
+++ src/controllers/ContentController.php
@@ -22,8 +22,21 @@ class ContentController extends BaseController
 		if (empty($section))
 			$section = new Section;
 
+		$section->site_id = Blocks::app()->sites->currentSite->id;
+
 		$section->name = Blocks::app()->request->getPost('name');
 		$section->handle = Blocks::app()->request->getPost('handle');
+
+		$maxEntries = Blocks::app()->request->getPost('max_entries');
+		$section->max_entries = ($maxEntries ? $maxEntries : null);
+
+		$section->sortable = (Blocks::app()->request->getPost('sortable') === 'y');
+
+		$urlFormat = Blocks::app()->request->getPost('url_format');
+		$section->url_format = ($urlFormat ? $urlFormat : null);
+
+		$template = Blocks::app()->request->getPost('template');
+		$section->template = ($template ? $template : null);
 
 		if ($section->save())
 		{
