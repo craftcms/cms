1ae72929fa541c4d133d7bc812dc7edb6fbb27c7
Brad Bell <takobell@gmail.com>
2012-02-15 19:12:43 -0800

    Update DatabaseHelper with logic for adding custom trigger logic to AuthCode model.

--- src/business/utils/DatabaseHelper.php
+++ src/business/utils/DatabaseHelper.php
@@ -154,17 +154,27 @@ class DatabaseHelper
 	{
 		$dbName = Blocks::app()->config->getDbItem('database');
 
-		Blocks::app()->db->createCommand(
-							'CREATE
-							 TRIGGER `'.$dbName.'`.`'.Blocks::app()->config->getDbItem('tablePrefix').'_auditinfoinsert_'.$tableName.'`
-							 BEFORE INSERT ON `'.$dbName.'`.`{{'.$tableName.'}}`
-							 FOR EACH ROW
-							 SET NEW.date_created = UNIX_TIMESTAMP(),
-								 NEW.date_updated = UNIX_TIMESTAMP(),
-								 NEW.uid = UUID();
-								 END;
-								 SQL;'
-					)->execute();
+		$sql = 'CREATE
+				TRIGGER `'.$dbName.'`.`'.Blocks::app()->config->getDbItem('tablePrefix').'_auditinfoinsert_'.$tableName.'`
+				BEFORE INSERT ON `'.$dbName.'`.`{{'.$tableName.'}}`
+				FOR EACH ROW
+				SET NEW.date_created = UNIX_TIMESTAMP(),
+					NEW.date_updated = UNIX_TIMESTAMP(),';
+
+		if ($tableName == 'AuthCode')
+		{
+			$sql .= 'NEW.uid = UUID(),
+					 NEW.code = UUID();';
+		}
+		else
+		{
+			$sql .= 'NEW.uid = UUID();';
+		}
+
+		$sql .= 'END;
+				 SQL;';
+
+		Blocks::app()->db->createCommand($sql)->execute();
 	}
 
 	/**
