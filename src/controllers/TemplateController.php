290d0ed571c5c848d86081f57a78e1eccf7a9695
Brad Bell <takobell@gmail.com>
2012-02-22 11:24:26 -0800

    Allow account/password with an auto code through.

--- src/controllers/TemplateController.php
+++ src/controllers/TemplateController.php
@@ -11,10 +11,16 @@ class TemplateController extends BaseController
 	 */
 	public function actionIndex()
 	{
-		// Require user to be logged in on every page but /login in the control panel
+		// Require user to be logged in on every page but /login in the control panel and account/password with an auth code
 		if (Blocks::app()->request->mode == RequestMode::CP)
-			if (Blocks::app()->request->path !== 'login')
+		{
+			$path = Blocks::app()->request->path;
+			if ($path !== 'login')
+			{
+				if ($path !== 'account/password' || ($path == 'account/password' && Blocks::app()->request->getParam('code', null) == null))
 					$this->requireLogin();
+			}
+		}
 
 		$this->loadRequestedTemplate();
 	}
