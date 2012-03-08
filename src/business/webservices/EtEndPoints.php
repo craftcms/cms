b84e11614f59838f72398d1ee24778bfef2e36e9
takobell <brad@pixelandtonic.com>
2012-03-07 20:57:06 -0800

    Fix ET end point paths.

--- src/business/webservices/EtEndPoints.php
+++ src/business/webservices/EtEndPoints.php
@@ -6,8 +6,8 @@ namespace Blocks;
  */
 class EtEndPoints
 {
-	const Check                         = 'http://et.blockscms.com/admin.php/action/app/core/check';
-	const DownloadPackage               = 'http://et.blockscms.com/admin.php/action/app/core/downloadpackage';
-	const GetCoreReleaseFileMD5         = 'http://et.blockscms.com/admin.php/action/app/core/getcorereleasefilemd5';
-	const Ping                          = 'http://et.blockscms.com/admin.php/action/app/core/ping';
+	const Check                         = 'http://et.blockscms.com/admin.php/action/core/check';
+	const DownloadPackage               = 'http://et.blockscms.com/admin.php/action/core/downloadpackage';
+	const GetCoreReleaseFileMD5         = 'http://et.blockscms.com/admin.php/action/core/getcorereleasefilemd5';
+	const Ping                          = 'http://et.blockscms.com/admin.php/action//core/ping';
 }
