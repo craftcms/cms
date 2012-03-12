a5168c3f314e0abb23e7c2c5a4d1e14a581bf22d
takobell <brad@pixelandtonic.com>
2012-03-12 14:21:05 -0700

    Reset version number to 0.01.

--- src/business/Blocks.php
+++ src/business/Blocks.php
@@ -7,7 +7,7 @@ namespace Blocks;
 class Blocks extends \Yii
 {
 	private static $_edition = '@@@edition@@@';
-	private static $_version = '0.14';
+	private static $_version = '0.01';
 	private static $_build = '@@@build@@@';
 
 	/**
