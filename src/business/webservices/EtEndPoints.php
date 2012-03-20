0ba15c075f1d1f6211cb70b1494971d28dd47ca2
Brad Bell <takobell@gmail.com>
2012-03-20 09:48:20 -0700

    Fix typo in ET Ping path.

--- src/business/webservices/EtEndPoints.php
+++ src/business/webservices/EtEndPoints.php
@@ -9,5 +9,5 @@ class EtEndPoints
 	const Check                         = 'http://et.blockscms.com/admin.php/action/core/check';
 	const DownloadPackage               = 'http://et.blockscms.com/admin.php/action/core/downloadpackage';
 	const GetCoreReleaseFileMD5         = 'http://et.blockscms.com/admin.php/action/core/getcorereleasefilemd5';
-	const Ping                          = 'http://et.blockscms.com/admin.php/action//core/ping';
+	const Ping                          = 'http://et.blockscms.com/admin.php/action/core/ping';
 }
