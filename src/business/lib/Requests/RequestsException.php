4835e4041724d2bd312ea325ed90dbaafad8a59c
Brad Bell <takobell@gmail.com>
2012-03-20 09:46:12 -0700

    Add Requests.

---
+++ src/business/lib/Requests/RequestsException.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Exception for HTTP requests
+ *
+ * @package Requests
+ */
+
+/**
+ * Exception for HTTP requests
+ *
+ * @package Requests
+ */
+class RequestsException extends Exception {
+	/**
+	 * Type of exception
+	 *
+	 * @var string
+	 */
+	protected $type;
+
+	/**
+	 * Data associated with the exception
+	 *
+	 * @var mixed
+	 */
+	protected $data;
+
+	/**
+	 * Create a new exception
+	 *
+	 * @param string $message Exception message
+	 * @param string $type Exception type
+	 * @param mixed $data Associated data
+	 */
+	public function __construct($message, $type, $data = null) {
+		parent::__construct($message, 0);
+
+		$this->type = $type;
+		$this->data = $data;
+	}
+
+	/**
+	 * Like {@see getCode()}, but a string code.
+	 *
+	 * @codeCoverageIgnore
+	 * @return string
+	 */
+	public function getType() {
+		return $this->type;
+	}
+
+	/**
+	 * Gives any relevant data
+	 *
+	 * @codeCoverageIgnore
+	 * @return mixed
+	 */
+	public function getData() {
+		return $this->data;
+	}
+}
---
+++ src/business/lib/Requests/RequestsResponse.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * HTTP response class
+ *
+ * Contains a response from Requests::request()
+ * @package Requests
+ */
+
+/**
+ * HTTP response class
+ *
+ * Contains a response from Requests::request()
+ * @package Requests
+ */
+class RequestsResponse {
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		$this->headers = new RequestsResponseHeaders();
+	}
+
+	/**
+	 * Response body
+	 * @var string
+	 */
+	public $body = '';
+
+	/**
+	 * Raw HTTP data from the transport
+	 * @var string
+	 */
+	public $raw = '';
+
+	/**
+	 * Headers, as an associative array
+	 * @var array
+	 */
+	public $headers = array();
+
+	/**
+	 * Status code, false if non-blocking
+	 * @var integer|boolean
+	 */
+	public $status_code = false;
+
+	/**
+	 * Whether the request succeeded or not
+	 * @var boolean
+	 */
+	public $success = false;
+
+	/**
+	 * Number of redirects the request used
+	 * @var integer
+	 */
+	public $redirects = 0;
+
+	/**
+	 * URL requested
+	 * @var string
+	 */
+	public $url = '';
+
+	/**
+	 * Previous requests (from redirects)
+	 * @var array Array of RequestsResponse objects
+	 */
+	public $history = array();
+}
