5cbd9c2d46ced6f1c6736437b73f13e2c3cfec1b
Brad Bell <takobell@gmail.com>
2012-03-21 18:10:48 -0700

    Undoing logging popup stuff.

--- src/templates/logging/profile-summary-firebug.php
+++ src/templates/logging/profile-summary-firebug.php
@@ -1,21 +1,25 @@
 <script type="text/javascript">
 /*<![CDATA[*/
-if(typeof(console)=='object')
+if (typeof(console) == 'object')
 {
-	console.group("Profiling Summary Report");
-	console.log(" count1   total   average    min      max   ");
-<?php
-foreach($data as $index=>$entry)
-{
-	$proc=CJavaScript::quote($entry[0]);
-	$count=sprintf('%5d',$entry[1]);
-	$min=sprintf('%0.5f',$entry[2]);
-	$max=sprintf('%0.5f',$entry[3]);
-	$total=sprintf('%0.5f',$entry[4]);
-	$average=sprintf('%0.5f',$entry[4]/$entry[1]);
-	echo "\tconsole.log(\" $count  $total  $average  $min  $max    {$proc}\");\n";
-}
-?>
+	console.groupCollapsed("Profiling Summary Report");
+	console.log("Time:   <?php echo sprintf('%0.5f', Blocks\Blocks::getLogger()->executionTime); ?>s\n");
+	console.log("Memory: <?php echo number_format(Blocks\Blocks::getLogger()->memoryUsage/1024); ?>Kb\n");
+	console.log(" count   total   average    min      max   ");
+
+	<?php
+	foreach ($data as $index => $entry)
+	{
+		$proc    = CJavaScript::quote($entry[0]);
+		$count   = sprintf('%5d',$entry[1]);
+		$min     = sprintf('%0.5f',$entry[2]);
+		$max     = sprintf('%0.5f',$entry[3]);
+		$total   = sprintf('%0.5f',$entry[4]);
+		$average = sprintf('%0.5f',$entry[4]/$entry[1]);
+		echo "\tconsole.log(\" $count  $total  $average  $min  $max    {$proc}\");\n";
+	}
+	?>
+
 	console.groupEnd();
 }
 /*]]>*/
