3da477abba737d41f0e6b09f5907b7d0d801b039
Brad Bell <takobell@gmail.com>
2012-03-29 18:43:47 -0700

    Get yiic to work with PHP's shitty namespaces.

---
+++ src/config/console.php
@@ -0,0 +1,39 @@
+<?php
+require_once(dirname(__FILE__).'/defaults/db.php');
+require_once(dirname(__FILE__).'/../../config/db.php');
+
+return array(
+	'basePath' => dirname(__FILE__).'/..',
+
+	// autoloading model and component classes
+	'import' => array(
+		'application.business.*',
+		'application.business.services.*',
+		'application.migrations.*',
+		'application.framework.cli.commands.*',
+		'application.framework.logging.CLogger',
+	),
+
+	// application components
+	'components' => array(
+
+		'db' => array(
+			'connectionString'  => strtolower('mysql:host='.$dbConfig['server'].';dbname='.$dbConfig['database'].';port='.$dbConfig['port'].';'),
+			'emulatePrepare'    => true,
+			'username'          => $dbConfig['user'],
+			'password'          => $dbConfig['password'],
+			'charset'           => $dbConfig['charset'],
+			'tablePrefix'       => $dbConfig['tablePrefix'],
+		),
+
+		'config' => array(
+			'class' => 'Blocks\ConfigService',
+		),
+	),
+
+	'commandPath' => dirname(__FILE__).'/../business/console/commands',
+
+	'params' => array(
+		'dbConfig' => $dbConfig,
+	),
+);
