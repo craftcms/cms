8fdea8b3d8c441eb62546abb3967cd2e79b245a2
Brad Bell <takobell@gmail.com>
2012-04-01 21:41:17 -0700

    Remove test col from info table... auto-update.

---
+++ src/migrations/m120402_044028_remove_test_col_from_info.php
@@ -0,0 +1,16 @@
+<?php
+namespace Blocks;
+
+/**
+ * The class name is the UTC timestamp in the format of mYYMMDD_HHMMSS_migrationName
+ */
+class m120402_044028_remove_test_col_from_info extends \CDbMigration
+{
+	/**
+	 * Any migration code in here is wrapped inside of a transaction.
+	 */
+	public function safeUp()
+	{
+		$this->dropColumn('info', 'test');
+	}
+}
