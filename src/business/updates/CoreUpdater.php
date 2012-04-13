a77ef208e7e849552e4c0a7cf01cd96338d2796a
Brad Bell <takobell@gmail.com>
2012-04-13 13:16:38 -0700

    During an automatic update, after the patch is unzipped, but before any update logic is done, check for the writeability of each file in the manifest.  Error if one of them is not writeable.

--- src/business/updates/CoreUpdater.php
+++ src/business/updates/CoreUpdater.php
@@ -12,6 +12,7 @@ class CoreUpdater implements IUpdater
 	private $_downloadFilePath;
 	private $_tempPackageDir;
 	private $_manifestData;
+	private $_writableErrors = null;
 
 	/**
 	 *
@@ -23,7 +24,7 @@ class CoreUpdater implements IUpdater
 	}
 
 	/**
-	 *
+	 * Performs environmental requirement checks before running an update.
 	 */
 	public function checkRequirements()
 	{
@@ -42,9 +43,12 @@ class CoreUpdater implements IUpdater
 			else
 				if (!$databaseCompat)
 					throw new Exception('The update cannot be installed because Blocks requires MySQL version '.$requiredMysqlVersion.' or higher and you have MySQL version '.PHP_VERSION.' installed.');
+
+
 	}
 
 	/**
+	 * Starts the process of running a Blocks app update.
 	 * @return bool
 	 * @throws Exception
 	 */
@@ -57,61 +61,81 @@ class CoreUpdater implements IUpdater
 
 		Blocks::log('Starting the CoreUpdater.', \CLogger::LEVEL_INFO);
 
-		// get the most up-to-date build.
+		// Get the most up-to-date build.
 		$latestBuild = $this->_buildsToUpdate[0];
 		$this->_downloadFilePath = b()->path->runtimePath.UpdateHelper::constructCoreReleasePatchFileName($latestBuild->version, $latestBuild->build, Blocks::getEdition());
 		$this->_tempPackageDir = UpdateHelper::getTempDirForPackage($this->_downloadFilePath);
 
-		// download the package
+		// Download the package from ET.
 		Blocks::log('Downloading patch file to '.$this->_downloadFilePath, \CLogger::LEVEL_INFO);
 		if (!b()->et->downloadPackage($latestBuild->version, $latestBuild->build, $this->_downloadFilePath))
 			throw new Exception('There was a problem downloading the package.');
 
-		// validate
+		// Validate the downloaded package against ET.
+		Blocks::log('Validating downloaded package.', \CLogger::LEVEL_INFO);
 		if (!$this->validatePackage($latestBuild->version, $latestBuild->build))
 			throw new Exception('There was a problem validating the downloaded package.');
 
-		// unpack
+		// Unpack the downloded package.
+		Blocks::log('Unpacking the downloaded package.', \CLogger::LEVEL_INFO);
 		if (!$this->unpackPackage())
 			throw new Exception('There was a problem unpacking the downloaded package.');
 
-		// check to see if there any migrations to run.
+		// Validate that the paths in the update manifest file are all writable by Blocks
+		Blocks::log('Validating update manifest file paths are writable.', \CLogger::LEVEL_INFO);
+		if (!$this->validateManifestPathsWritable())
+			throw new Exception('Blocks needs to be able to write to the follow files, but can\'t: '.implode(',', $this->_writableErrors));
+
+		// Check to see if there any migrations to run.
+		Blocks::log('Checking to see if there are any migrations to run in the update.', \CLogger::LEVEL_INFO);
 		$this->gatherMigrations();
 
 		// Take the site offline.
+		Blocks::log('Taking the site offline for update.', \CLogger::LEVEL_INFO);
 		b()->updates->turnSystemOffBeforeUpdate();
 
-		// if there are migrations, run them.
+		// If there are migrations to run, run them.
 		if ($this->_migrationsToRun === true)
 		{
+			Blocks::log('Starting to run update migrations.', \CLogger::LEVEL_INFO);
 			if (!$this->doDatabaseUpdate())
 				throw new Exception('There was a problem updating your database.');
 		}
 
-		// backup files.
+		// Backup any files about to be updated.
+		Blocks::log('Backing up files that are about to be updated.', \CLogger::LEVEL_INFO);
 		if (!$this->backupFiles())
 			throw new Exception('There was a problem backing up your files for the update.');
 
-		// update files.
+		// Update the files.
+		Blocks::log('Performing file udpate.', \CLogger::LEVEL_INFO);
 		if (!UpdateHelper::doFileUpdate($this->_getManifestData(), $this->_tempPackageDir))
 			throw new Exception('There was a problem updating your files.');
 
-		// Bring the system online.
+		// Bring the system back online.
+		Blocks::log('Turning system back on after update.', \CLogger::LEVEL_INFO);
 		b()->updates->turnSystemOnAfterUpdate();
 
-		// clean-up leftover files.
+		// Clean-up any leftover files.
+		Blocks::log('Cleaning up temp files after update.', \CLogger::LEVEL_INFO);
 		$this->cleanTempFiles();
 
+		// Clear the updates cache.
+		Blocks::log('Clearing the update cache.', \CLogger::LEVEL_INFO);
 		if (!b()->updates->flushUpdateInfoFromCache())
 			throw new Exception('The update was performed successfully, but there was a problem invalidating the update cache.');
 
+		// Update the db with the new Blocks info.
+		Blocks::log('Setting new Blocks info in the database after update.', \CLogger::LEVEL_INFO);
 		if (!b()->updates->setNewBlocksInfo($latestBuild->version, $latestBuild->build, $latestBuild->date))
 			throw new Exception('The update was performed successfully, but there was a problem setting the new version and build number in the database.');
 
+		Blocks::log('Finished CoreUpdater.', \CLogger::LEVEL_INFO);
 		return true;
 	}
 
 	/**
+	 * Returns the relevant lines from the update manifest file starting with the current local version/build.
 	 * @return array
 	 */
 	private function _getManifestData()
@@ -120,7 +144,7 @@ class CoreUpdater implements IUpdater
 		{
 			$manifestData = UpdateHelper::getManifestData($this->_tempPackageDir->realPath);
 
-			// only use the manifest data starting from the local version
+			// Only use the manifest data starting from the local version
 			for ($counter = 0; $counter < count($manifestData); $counter++)
 			{
 				if (strpos($manifestData[$counter], '##'.$this->_updateInfo->blocks->localVersion.'.'.$this->_updateInfo->blocks->localBuild) !== false)
@@ -135,6 +159,8 @@ class CoreUpdater implements IUpdater
 	}
 
 	/**
+	 * Scans through the update manifest file for any migrations.  If it finds one, copies it to the app's miggrations
+	 * folder so they can be ran before any file updates occur.
 	 * @return mixed
 	 */
 	public function gatherMigrations()
@@ -145,7 +171,7 @@ class CoreUpdater implements IUpdater
 		{
 			$row = explode(';', $manifestData[$i]);
 
-			// we found a migration
+			// We found a migration
 			if (UpdateHelper::isManifestMigrationLine($row[0]) && $row[1] == PatchManifestFileAction::Add)
 			{
 				Blocks::log('Found migration file: '.$row[0], \CLogger::LEVEL_INFO);
@@ -156,6 +182,7 @@ class CoreUpdater implements IUpdater
 	}
 
 	/**
+	 * Run the database migrations to top.
 	 * @return bool
 	 */
 	public function doDatabaseUpdate()
@@ -167,7 +194,7 @@ class CoreUpdater implements IUpdater
 	}
 
 	/**
-	 *
+	 * Remove any temp files and/or directories that might have been created.
 	 */
 	public function cleanTempFiles()
 	{
@@ -180,7 +207,7 @@ class CoreUpdater implements IUpdater
 
 			$rowData = explode(';', $row);
 
-			// delete any files we backed up.
+			// Delete any files we backed up.
 			$backupFile = b()->file->set(b()->path->appPath.'../../'.$rowData[0].'.bak');
 			if ($backupFile->exists)
 			{
@@ -189,16 +216,17 @@ class CoreUpdater implements IUpdater
 			}
 		}
 
-		// delete the temp patch dir
+		// Delete the temp patch dir
 		$tempPatchDir = $this->_tempPackageDir;
 		$tempPatchDir->delete();
 
-		// delete the downloaded patch file.
+		// Delete the downloaded patch file.
 		$downloadPatchFile = b()->file->set($this->_downloadFilePath);
 		$downloadPatchFile->delete();
 	}
 
 	/**
+	 * Calculate the MD5 of the downloaded file and verify it with ET against the stored MD5 of the patch file.
 	 * @param $version
 	 * @param $build
 	 * @return bool
@@ -222,6 +250,7 @@ class CoreUpdater implements IUpdater
 	}
 
 	/**
+	 * Unzip the downloaded update file into the temp package directory.
 	 * @return bool
 	 */
 	public function unpackPackage()
@@ -240,6 +269,33 @@ class CoreUpdater implements IUpdater
 	}
 
 	/**
+	 * Checks to see if the files that we are about to update are writable by Blocks.
+	 * @return bool
+	 */
+	public function validateManifestPathsWritable()
+	{
+		$manifestData = $this->_getManifestData();
+
+		foreach ($manifestData as $row)
+		{
+			if (UpdateHelper::isManifestVersionInfoLine($row))
+				continue;
+
+			$rowData = explode(';', $row);
+			$file = b()->file->set(b()->path->appPath.'../../'.$rowData[0]);
+
+			// Check to see if the file we need to update is writable.
+			if (!$file->writable)
+				$this->_writableErrors[] = $file->realPath;
+
+		}
+
+		return $this->_writableErrors === null;
+	}
+
+	/**
+	 * Attempt to backup each of the update manifest files by copying them to a file with the same name with a .bak extension.
+	 * If there is an exception thrown, we attempt to roll back all of the changes.
 	 * @return bool
 	 */
 	public function backupFiles()
@@ -260,7 +316,7 @@ class CoreUpdater implements IUpdater
 				$rowData = explode(';', $row);
 				$file = b()->file->set(b()->path->appPath.'../../'.$rowData[0]);
 
-				// if the file doesn't exist, it's a new file
+				// If the file doesn't exist, it's a new file.
 				if ($file->exists)
 				{
 					Blocks::log('Backing up file '.$file->realPath);
