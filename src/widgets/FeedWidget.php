f0d84d759ed9b2fc86200226e7ee29e8c5a0338a
Brandon Kelly <brandon@pixelandtonic.com>
2012-05-01 16:39:07 -0700

    Initial work on getting the Feed widget working

--- src/widgets/FeedWidget.php
+++ src/widgets/FeedWidget.php
@@ -32,33 +32,23 @@ class FeedWidget extends Widget
 	 */
 	public function displayBody()
 	{
-		$this->items = array(
-			array(
-				'url' => 'http://feedproxy.google.com/~r/blogandtonic/~3/KrG7zZ_5zF0/assets-moty',
-				'title' => 'Assets Voted Module of the Year',
-				'date' => 'Dec 22, 2011'
-			),
-			array(
-				'url' => 'http://feedproxy.google.com/~r/blogandtonic/~3/WXqTFhOZP0o/php-developer',
-				'title' => 'We’re Looking for a PHP Developer',
-				'date' => 'Dec 2, 2011'
-			),
-			array(
-				'url' => 'http://feedproxy.google.com/~r/blogandtonic/~3/hwoW_CDbEF8/introducing-assets',
-				'title' => 'Introducing Assets',
-				'date' => 'Jun 28, 2011'
-			),
-			array(
-				'url' => 'http://feedproxy.google.com/~r/blogandtonic/~3/D2fEpY3KITQ/wygwam22',
-				'title' => 'Wygwam 2.2 Released!',
-				'date' => 'Feb 9, 2011'
-			),
-			array(
-				'url' => 'http://feedproxy.google.com/~r/blogandtonic/~3/To6-CuOmKlA/playa4',
-				'title' => 'Playa 4 has arrived!',
-				'date' => 'Feb 2, 2011'
-			)
-		);
+		$url = $this->settings['url'];
+		$cachePath = b()->path->getRuntimePath().'cache/';
+		$feed = new \SimplePie($url, $cachePath);
+		$feed->init();
+		$feed->handle_content_type();
+
+		foreach ($feed->get_items() as $item)
+		{
+			$date = new DateTime;
+			$date->setTimestamp($item->get_date('U'));
+
+			$this->items[] = array(
+				'url' => $item->get_permalink(),
+				'title' => $item->get_title(),
+				'date' => $date
+			);
+		}
 
 		return parent::displayBody();
 	}
