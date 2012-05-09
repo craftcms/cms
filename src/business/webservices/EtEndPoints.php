786c3f8f766e7a4ae5222c94299d060ca66659e5
Brad Bell <takobell@gmail.com>
2012-05-08 17:42:39 -0700

    et.blockscms.com => elliot.blockscms.com

--- src/business/webservices/EtEndPoints.php
+++ src/business/webservices/EtEndPoints.php
@@ -6,8 +6,8 @@ namespace Blocks;
  */
 class EtEndPoints
 {
-	const Check                         = 'https://et.blockscms.com/admin.php/action/core/check';
-	const DownloadPackage               = 'https://et.blockscms.com/admin.php/action/core/downloadpackage';
-	const GetCoreReleaseFileMD5         = 'https://et.blockscms.com/admin.php/action/core/getcorereleasefilemd5';
-	const Ping                          = 'https://et.blockscms.com/admin.php/action/core/ping';
+	const Check                         = 'https://elliot.blockscms.com/admin.php/action/core/check';
+	const DownloadPackage               = 'https://elliot.blockscms.com/admin.php/action/core/downloadpackage';
+	const GetCoreReleaseFileMD5         = 'https://elliot.blockscms.com/admin.php/action/core/getcorereleasefilemd5';
+	const Ping                          = 'https://elliot.blockscms.com/admin.php/action/core/ping';
 }
