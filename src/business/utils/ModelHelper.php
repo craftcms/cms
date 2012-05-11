707300a2530b2b67ed5f5976f520cc2942c31f03
Brandon Kelly <brandon@pixelandtonic.com>
2012-05-11 16:58:29 -0700

    Added FormModel, allowing form models to use the same $attributes definition as AR models
    rules() logic moved to new ModelHelper class

---
+++ src/business/utils/ModelHelper.php
@@ -0,0 +1,157 @@
+<?php
+namespace Blocks;
+
+/**
+ *
+ */
+class ModelHelper
+{
+	/**
+	 * Returns the rules array used by CActiveRecord
+	 * @param array $attributes
+	 * @param array $indexes
+	 * @return array
+	 */
+	public function createRules($attributes, $indexes = array())
+	{
+		$rules = array();
+
+		$uniques = array();
+		$required = array();
+		$emails = array();
+		$urls = array();
+		$strictLengths = array();
+		$minLengths = array();
+		$maxLengths = array();
+
+		$numberTypes = array(AttributeType::TinyInt, AttributeType::SmallInt, AttributeType::MediumInt, AttributeType::Int, AttributeType::BigInt, AttributeType::Float, AttributeType::Decimal);
+		$integerTypes = array(AttributeType::TinyInt, AttributeType::SmallInt, AttributeType::MediumInt, AttributeType::Int, AttributeType::BigInt);
+
+		foreach ($attributes as $name => $settings)
+		{
+			$type = is_string($settings) ? $settings : (isset($settings['type']) ? $settings['type'] : null);
+
+			// Catch handles, email addresses and URLs before running normalizeAttributeSettings, since 'type' will get changed to VARCHAR
+			if ($type == AttributeType::Handle)
+			{
+				$reservedWords = isset($settings['reservedWords']) ? ArrayHelper::stringToArray($settings['reservedWords']) : array();
+				$rules[] = array($name, 'Blocks\HandleValidator', 'reservedWords' => $reservedWords);
+			}
+
+			if ($type == AttributeType::Email)
+				$emails[] = $name;
+
+			if ($type == AttributeType::Url)
+				$urls[] = $name;
+
+			// Remember if it's a license key
+			$isLicenseKey = ($type == AttributeType::LicenseKey);
+
+			$settings = DatabaseHelper::normalizeAttributeSettings($settings);
+
+			// Uniques
+			if (isset($settings['unique']) && $settings['unique'] === true)
+				$uniques[] = $name;
+
+			// Only enforce 'required' validation if there's no default value
+			if (isset($settings['required']) && $settings['required'] === true && !isset($settings['default']))
+				$required[] = $name;
+
+			// Numbers
+			if (in_array($settings['type'], $numberTypes))
+			{
+				$rule = array($name, 'numerical');
+
+				if (isset($settings['min']) && is_numeric($settings['min']))
+					$rule['min'] = $settings['min'];
+
+				if (isset($settings['max']) && is_numeric($settings['max']))
+					$rule['max'] = $settings['max'];
+
+				if (in_array($settings['type'], $integerTypes))
+					$rule['integerOnly'] = true;
+
+				$rules[] = $rule;
+			}
+
+			// Enum attribute values
+			if ($settings['type'] == AttributeType::Enum)
+			{
+				$values = ArrayHelper::stringToArray($settings['values']);
+				$rules[] = array($name, 'in', 'range' => $values);
+			}
+
+			// License keys' length=36 is redundant in the context of validation, since matchPattern already enforces 36 chars
+			if ($isLicenseKey)
+				unset($settings['length']);
+
+			// Strict, min, and max lengths
+			if (isset($settings['length']) && is_numeric($settings['length']))
+				$strictLengths[(string)$settings['length']][] = $name;
+			else
+			{
+				// Only worry about min- and max-lengths if a strict length isn't set
+				if (isset($settings['minLength']) && is_numeric($settings['minLength']))
+					$minLengths[(string)$settings['minLength']][] = $name;
+
+				if (isset($settings['maxLength']) && is_numeric($settings['maxLength']))
+					$maxLengths[(string)$settings['maxLength']][] = $name;
+			}
+
+			// Regex pattern matching
+			if (!empty($settings['matchPattern']))
+				$rules[] = array($name, 'match', 'pattern' => $settings['matchPattern']);
+		}
+
+		// Catch any unique indexes
+		foreach ($indexes as $index)
+		{
+			if (isset($index['unique']) && $index['unique'] === true)
+			{
+				$columns = ArrayHelper::stringToArray($index['columns']);
+				$initialColumn = array_shift($columns);
+				$rules[] = array($initialColumn, 'Blocks\CompositeUniqueValidator', 'with' => implode(',', $columns));
+			}
+		}
+
+		if ($uniques)
+			$rules[] = array(implode(',', $uniques), 'unique');
+
+		if ($required)
+			$rules[] = array(implode(',', $required), 'required');
+
+		if ($emails)
+			$rules[] = array(implode(',', $emails), 'email');
+
+		if ($urls)
+			$rules[] = array(implode(',', $urls), 'url', 'defaultScheme' => 'http');
+
+		if ($strictLengths)
+		{
+			foreach ($strictLengths as $strictLength => $attributeNames)
+			{
+				$rules[] = array(implode(',', $attributeNames), 'length', 'is' => (int)$strictLength);
+			}
+		}
+
+		if ($minLengths)
+		{
+			foreach ($minLengths as $minLength => $attributeNames)
+			{
+				$rules[] = array(implode(',', $attributeNames), 'length', 'min' => (int)$minLength);
+			}
+		}
+
+		if ($maxLengths)
+		{
+			foreach ($maxLengths as $maxLength => $attributeNames)
+			{
+				$rules[] = array(implode(',', $attributeNames), 'length', 'max' => (int)$maxLength);
+			}
+		}
+
+		$rules[] = array(implode(',', array_keys($attributes)), 'safe', 'on' => 'search');
+
+		return $rules;
+	}
+}
---
+++ src/models/forms/FormModel.php
@@ -0,0 +1,51 @@
+<?php
+namespace Blocks;
+
+/**
+ *
+ */
+abstract class FormModel extends \CFormModel
+{
+	protected $attributes = array();
+	private $_attributes = array();
+
+	/**
+	 * Attribute Setter
+	 * @param string $name
+	 * @param mixed $value
+	 */
+	function __set($name, $value)
+	{
+		if (array_key_exists($name, $this->attributes))
+			$this->_attributes[$name] = $value;
+		else
+			throw new Exception(get_class($this).' doesn’t have an attribute “'.$name.'”.');
+	}
+
+	/**
+	 * Attribute Setter
+	 * @param string $name
+	 * @param mixed $value
+	 */
+	function __get($name)
+	{
+		if (array_key_exists($name, $this->attributes))
+		{
+			if (isset($this->_attributes[$name]))
+				return $this->_attributes[$name];
+			else
+				return null;
+		}
+		else
+			throw new Exception(get_class($this).' doesn’t have an attribute “'.$name.'”.');
+	}
+
+	/**
+	 * Used by CActiveRecord
+	 * @return array Validation rules for model's attributes
+	 */
+	public function rules()
+	{
+		return ModelHelper::createRules($this->attributes);
+	}
+}
