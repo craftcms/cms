162e4f43ce95ef7fc54e85899d2d970b8980dd0b
Brad Bell <takobell@gmail.com>
2012-05-24 22:03:29 -0700

    Site model now has an "enabled" attribute.

--- src/models/Site.php
+++ src/models/Site.php
@@ -14,7 +14,8 @@ class Site extends BaseModel
 		'handle'      => AttributeType::Handle,
 		'url'         => array('type' => AttributeType::Url, 'required' => true),
 		'license_key' => AttributeType::LicenseKey,
-		'primary'     => array('type' => AttributeType::Boolean, 'required' => false, 'default' => null, 'unique' => true)
+		'primary'     => array('type' => AttributeType::Boolean, 'required' => false, 'default' => null, 'unique' => true),
+		'enabled'     => array('type' => AttributeType::Boolean, 'required' => false, 'default' => true)
 	);
 
 	protected $hasMany = array(
