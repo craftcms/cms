d516c1f52b36865f88f976d4d67daacb70247ebf
Brad Bell <takobell@gmail.com>
2012-06-04 11:04:52 -0700

    FileLogRoute adds a separator at the end of all of it's log processing signifying the end of a request.

--- src/business/logging/FileLogRoute.php
+++ src/business/logging/FileLogRoute.php
@@ -15,4 +15,27 @@ class FileLogRoute extends \CFileLogRoute
 
 		parent::init();
 	}
+
+	/**
+	 * Saves log messages in files.
+	 * @param array $logs list of log messages
+	 */
+	protected function processLogs($logs)
+	{
+		$logFile = $this->getLogPath().DIRECTORY_SEPARATOR.$this->getLogFile();
+
+		if (@filesize($logFile) > $this->getMaxFileSize() * 1024)
+			$this->rotateFiles();
+
+		$fp = @fopen($logFile, 'a');
+		@flock($fp, LOCK_EX);
+
+		foreach ($logs as $log)
+			@fwrite($fp, $this->formatLogMessage($log[0], $log[1], $log[2], $log[3]));
+
+		@fwrite($fp, PHP_EOL.'******************************************************************************************************'.PHP_EOL);
+
+		@flock($fp, LOCK_UN);
+		@fclose($fp);
+	}
 }
