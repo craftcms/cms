0d4949e8136650a14ffa05d0c08dbb5dc945dad8
Brad Bell <takobell@gmail.com>
2012-06-22 18:04:15 -0700

    Add global t() to Blocks as a wrapper for Blocks::t()

--- src/business/Blocks.php
+++ src/business/Blocks.php
@@ -4,13 +4,13 @@ namespace Blocks;
 require_once dirname(__FILE__).'/enums/Product.php';
 require_once dirname(__FILE__).'/../blocks_info.php';
 
-
 /**
  *
 */
 class Blocks extends \Yii
 {
 	private static $_storedBlocksInfo;
+	private static $_translatedLanguages;
 
 	/**
 	 * @static
@@ -287,4 +287,19 @@ class Blocks extends \Yii
 function blx()
 {
 	return Blocks::app();
+}
+
+/**
+ * Shortcut wrapper for Blocks::t();
+ *
+ * @param       $category
+ * @param       $message
+ * @param array $params
+ * @param null  $source
+ * @param null  $language
+ * @return string
+ */
+function t($category, $message, $params = array(), $source = null, $language = null)
+{
+	return Blocks::t($category, $message, $params, $source, $language);
 }
