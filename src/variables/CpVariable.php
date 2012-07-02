6d547406f05deecc69fde2ccc7143f5a145024d8
Brandon Kelly <brandon@pixelandtonic.com>
2012-07-02 15:06:31 -0700

    New CP look

--- src/variables/CpVariable.php
+++ src/variables/CpVariable.php
@@ -10,21 +10,22 @@ class CpVariable
 	 * Get the sections of the CP.
 	 * @return array
 	 */
-	public function sections()
+	public function nav()
 	{
-		$sections[] = array('handle' => 'dashboard', 'name' => 'Dashboard', 'icon' => 'D');
-		$sections[] = array('handle' => 'content', 'name' => 'Content', 'icon' => 'C');
-		//$sections[] = array('handle' => 'assets', 'name' => 'Assets', 'icon' => 'A');
-		$sections[] = array('handle' => 'users', 'name' => 'Users', 'icon' => 'U');
-
-		if (blx()->users->getCurrentUser() && blx()->users->getCurrentUser()->admin)
-		{
-			$sections[] = array('handle' => 'settings', 'name' => 'Settings', 'icon' => 'S');
-			$sections[] = array('handle' => 'updates', 'name' => 'Updates', 'icon' => 'V');
-		}
-
-		//$sections[] = array('handle' => 'guide', 'name' => 'Guide', 'icon' => 'G');
-
-		return $sections;
+		return array(
+			array(
+				array('handle' => 'dashboard', 'name' => 'Dashboard', 'icon' => 'D'),
+			),
+			array(
+				array('handle' => 'content', 'name' => 'Content', 'icon' => 'C'),
+				array('handle' => 'assets', 'name' => 'Assets', 'icon' => 'A'),
+			),
+			array(
+				array('handle' => 'users', 'name' => 'Users', 'icon' => 'U'),
+				array('handle' => 'plugins', 'name' => 'Plugins', 'icon' => 'P'),
+				array('handle' => 'updates', 'name' => 'Updates', 'icon' => 'V', 'badge' => '1'),
+				array('handle' => 'settings', 'name' => 'Settings', 'icon' => 'S'),
+			)
+		);
 	}
 }
