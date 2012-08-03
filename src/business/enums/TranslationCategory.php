a847cfe22cdac736368d1e9e2d3e6a3b60de0c3a
Brad Bell <brad@pixelandtonic.com>
2012-08-02 18:01:30 -0700

    Add TranslationCategory enum.

---
+++ src/business/enums/TranslationCategory.php
@@ -0,0 +1,16 @@
+<?php
+namespace Blocks;
+
+/**
+ *
+ */
+class TranslationCategory
+{
+	const Email                 = 'Email';
+	const General               = 'General';
+	const App                   = 'App';
+	const Session               = 'Session';
+	const TemplateProcessing    = 'TemplateProcessing';
+	const Routing               = 'Routing';
+	const Updating              = 'Updating';
+}
