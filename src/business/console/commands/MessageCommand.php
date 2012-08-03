ab64fcb1959ba42490ebb66d3559fe7d56e1104f
Brad Bell <brad@pixelandtonic.com>
2012-08-03 14:50:10 -0700

    MessageCommand works with the TranslationCategory enum.

--- src/business/console/commands/MessageCommand.php
+++ src/business/console/commands/MessageCommand.php
@@ -24,14 +24,33 @@ class MessageCommand extends \MessageCommand
 		foreach ($translators as $translator)
 		{
 			$translator = str_replace('.', '\.', $translator);
+			$enumMatch = false;
+
+			// first check with the category name in quote
 			$n = preg_match_all('/\b'.$translator.'\s*\(\s*(\'.*?(?<!\\\\)\'|".*?(?<!\\\\)")\s*,\s*(\'.*?(?<!\\\\)\'|".*?(?<!\\\\)")\s*[,\)]/s', $subject, $matches, PREG_SET_ORDER);
+
+			if ($n == 0)
+			{
+				// now check for the category as an enum
+				$n = preg_match_all('/\b'.$translator.'\s*\(\s*(.*?(?<!\\\\).*?(?<!\\\\))\s*,\s*(\'.*?(?<!\\\\)\'|".*?(?<!\\\\)")\s*[,\)]/s', $subject, $matches, PREG_SET_ORDER);
+				if ($n > 0)
+					$enumMatch = true;
+			}
 
 			for ($i = 0; $i < $n; ++$i)
 			{
-				if (($pos = strpos($matches[$i][1], '.')) !== false)
-					$category = substr($matches[$i][1], $pos + 1, -1);
+				if ($enumMatch)
+				{
+					$segs = explode('::', $matches[$i][1]);
+					$category = $segs[1];
+				}
 				else
-					$category = substr($matches[$i][1], 1, -1);
+				{
+					if (($pos = strpos($matches[$i][1], '.')) !== false)
+						$category = substr($matches[$i][1], $pos + 1, -1);
+					else
+						$category = substr($matches[$i][1], 1, -1);
+				}
 
 				$message = $matches[$i][2];
 				$messages[$category][] = eval("return $message;");  // use eval to eliminate quote escape
