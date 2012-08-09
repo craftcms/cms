26861db0ec454984b395d5b1dbf6404fe6fa3db5
Brandon Kelly <brandon@pixelandtonic.com>
2012-08-09 13:27:18 -0700

    Et was still trying to grab the URL and license key from general settings rather than Blocks::getSiteUrl() and Blocks::getLicenseKey()

--- src/business/webservices/Et.php
+++ src/business/webservices/Et.php
@@ -75,8 +75,8 @@ class Et extends \CApplicationComponent
 		$generalSettings = blx()->settings->getSystemSettings('general');
 
 		$this->_package = new EtPackage();
-		$this->_package->url = $generalSettings['url'];
-		$this->_package->licenseKey = $generalSettings['licenseKey'];
+		$this->_package->url = Blocks::getSiteUrl();
+		$this->_package->licenseKey = Blocks::getLicenseKey();
 		$this->_package->product = '@@@product@@@';
 		$this->_package->requestDomain = blx()->request->getServerName();
 		$this->_package->requestIp = blx()->request->getUserHostAddress();
