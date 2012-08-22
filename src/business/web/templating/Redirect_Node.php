fcc53aa7277e54907b3cc0edef62e4230e850a22
Brad Bell <brad@pixelandtonic.com>
2012-08-21 17:41:27 -0700

    Twig Redirect node uses UrlHelper::generateUrl

--- src/business/web/templating/Redirect_Node.php
+++ src/business/web/templating/Redirect_Node.php
@@ -13,8 +13,8 @@ class Redirect_Node extends \Twig_Node
 	{
 		$compiler
 		    ->addDebugInfo($this)
-		    ->write('header(\'Location: \'.')
+		    ->write('header(\'Location: \'.\Blocks\UrlHelper::generateUrl(')
 		    ->subcompile($this->getNode('path'))
-		    ->raw(");\n");
+		    ->raw(").'');\n");
 	}
 }
