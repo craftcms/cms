ba0698623e51a481cec04bbbbe934a145cb266dc
Brandon Kelly <brandon@pixelandtonic.com>
2012-08-26 04:39:05 -0700

    More sidebar tweaks - sidebar is now one big list

--- src/variables/CpVariable.php
+++ src/variables/CpVariable.php
@@ -13,18 +13,12 @@ class CpVariable
 	public function nav()
 	{
 		return array(
-			array(
-				array('handle' => 'dashboard', 'name' => Blocks::t('Dashboard'), 'icon' => 'D'),
-			),
-			array(
-				array('handle' => 'content', 'name' => Blocks::t('Content'), 'icon' => 'C'),
-				array('handle' => 'assets', 'name' => Blocks::t('Assets'), 'icon' => 'A'),
-			),
-			array(
-				array('handle' => 'plugins', 'name' => Blocks::t('Plugins'), 'icon' => 'P'),
-				array('handle' => 'updates', 'name' => Blocks::t('Updates'), 'icon' => 'V', 'badge' => '1'),
-				array('handle' => 'settings', 'name' => Blocks::t('Settings'), 'icon' => 'S'),
-			)
+			array('handle' => 'dashboard', 'name' => Blocks::t('Dashboard'), 'icon' => 'D'),
+			array('handle' => 'content', 'name' => Blocks::t('Content'), 'icon' => 'C'),
+			array('handle' => 'assets', 'name' => Blocks::t('Assets'), 'icon' => 'A'),
+			array('handle' => 'plugins', 'name' => Blocks::t('Plugins'), 'icon' => 'P'),
+			array('handle' => 'updates', 'name' => Blocks::t('Updates'), 'icon' => 'V', 'badge' => '1'),
+			array('handle' => 'settings', 'name' => Blocks::t('Settings'), 'icon' => 'S'),
 		);
 	}
 }
