3a61f83c7e9e503d7dccc1c1fa83f050827048eb
Brad Bell <brad@pixelandtonic.com>
2012-09-04 16:42:27 -0700

    Add LocalizationHelper.

---
+++ src/business/utils/LocalizationHelper.php
@@ -0,0 +1,39 @@
+<?php
+namespace Blocks;
+
+/**
+ *
+ */
+class LocalizationHelper
+{
+	/**
+	 * Will take a decimal value, will remove the locale specific grouping separator and change the locale specific
+	 * decimal so a dot.
+	 *
+	 * @static
+	 * @param $number
+	 * @return mixed
+	 */
+	public static function normalizeNumber($number)
+	{
+		$language = blx()->language;
+		$languageData = blx()->localization->getLanguageData($language);
+		$decimalSymbol = $languageData->getNumberSymbol('decimal');
+		$groupSymbol = $languageData->getNumberSymbol('group');
+
+		$number = str_replace($groupSymbol, '', $number);
+		$number = str_replace($decimalSymbol, '.', $number);
+
+		return $number;
+	}
+
+	/**
+	 * @static
+	 * @param $dateTime
+	 * @return int
+	 */
+	public static function normalizeDateTime(DateTime $dateTime)
+	{
+		return $dateTime->getTimestamp();
+	}
+}
