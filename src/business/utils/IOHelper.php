e48e4c4fa8ead503637ff7fbc8c81fac3a9e60f8
Brad Bell <brad@pixelandtonic.com>
2012-09-10 13:59:13 -0700

    Fixed IOHelper doc bloc.

--- src/business/utils/IOHelper.php
+++ src/business/utils/IOHelper.php
@@ -12,8 +12,8 @@ class IOHelper
 	 * Tests whether the given file path exists on the file system.
 	 *
 	 * @static
-	 * @param string $path The path to test.
-	 * @param bool   $caseInsensitive Whether to perform a case insensitive check or not.
+	 * @param  string  $path            The path to test.
+	 * @param  bool    $caseInsensitive Whether to perform a case insensitive check or not.
 	 * @return boolean 'true' if the file exists, otherwise 'false'.
 	 */
 	public static function fileExists($path, $caseInsensitive = false)
@@ -47,8 +47,8 @@ class IOHelper
 	 * Tests whether the given folder path exists on the file system.
 	 *
 	 * @static
-	 * @param string $path The path to test.
-	 * @param bool   $caseInsensitive Whether to perform a case insensitive check or not.
+	 * @param  string  $path The path to test.
+	 * @param  bool    $caseInsensitive Whether to perform a case insensitive check or not.
 	 * @return boolean 'true' if the folder exists, otherwise 'false'.
 	 */
 	public static function folderExists($path, $caseInsensitive = false)
@@ -68,7 +68,7 @@ class IOHelper
 	 * Returns the real filesystem path of the given path.
 	 *
 	 * @static
-	 * @param string $path The path to test.
+	 * @param  string $path The path to test.
 	 * @return string The real file or folder path.
 	 */
 	public static function getRealPath($path)
@@ -81,7 +81,7 @@ class IOHelper
 	 * Tests whether the give filesystem path is readable.
 	 *
 	 * @static
-	 * @param $path The path to test.
+	 * @param  string  $path The path to test.
 	 * @return boolean 'true' if filesystem path is readable, otherwise 'false'.
 	 */
 	public static function isReadable($path)
@@ -96,8 +96,8 @@ class IOHelper
 	 * See: https://bugs.php.net/bug.php?id=27609 and https://bugs.php.net/bug.php?id=30931.
 	 *
 	 * @static
-	 * @param $path The path to test.
-	 * @return bool = 'true' if filesystem object is writable, otherwise 'false'.
+	 * @param  string $path The path to test.
+	 * @return bool   'true' if filesystem object is writable, otherwise 'false'.
 	 */
 	public static function isWritable($path)
 	{
@@ -127,8 +127,8 @@ class IOHelper
 	 * Will return the file name of the given path with or without the extension.
 	 *
 	 * @static
-	 * @param string $path The path to test.
-	 * @param bool   $includeExtension Whether to include the extension in the file name.
+	 * @param  string $path The path to test.
+	 * @param  bool   $includeExtension Whether to include the extension in the file name.
 	 * @return string The file name with or without the extension.
 	 */
 	public static function getFileName($path, $includeExtension = true)
@@ -145,8 +145,8 @@ class IOHelper
 	 * Will return the folder name of the given path either as the full path or only the single top level folder.
 	 *
 	 * @static
-	 * @param string $path     The path to test.
-	 * @param bool   $fullPath Whether to include the full path in the return results or the top level folder only.
+	 * @param  string $path     The path to test.
+	 * @param  bool   $fullPath Whether to include the full path in the return results or the top level folder only.
 	 * @return string The folder name.
 	 */
 	public static function getFolderName($path, $fullPath = true)
@@ -167,9 +167,9 @@ class IOHelper
 	 * Returns the file extension for the given path.  If there is not one, then $default is returned instead.
 	 *
 	 * @static
-	 * @param string      $path    The path to test.
-	 * @param null|string $default If the file has no extension, this one will be returned by default.
-	 * @return string The file extension.
+	 * @param  string      $path    The path to test.
+	 * @param  null|string $default If the file has no extension, this one will be returned by default.
+	 * @return string      The file extension.
 	 */
 	public static function getExtension($path, $default = null)
 	{
@@ -186,7 +186,7 @@ class IOHelper
 	 * A wrapper for {@link \CFileHelper::getMimeType}
 	 *
 	 * @static
-	 * @param $path The path to test.
+	 * @param  string $path The path to test.
 	 * @return string The mime type.
 	 */
 	public static function getMimeType($path)
@@ -199,7 +199,7 @@ class IOHelper
 	 * A wrapper for {@link \CFileHelper::getMimeTypeByExtension}
 	 *
 	 * @static
-	 * @param $path The path to test.
+	 * @param  string $path The path to test.
 	 * @return string The mime type.
 	 */
 	public static function getMimeTypeByExtension($path)
@@ -212,7 +212,7 @@ class IOHelper
 	 * Returns the last modified time for the given path in timestamp format or false if the file or folder does not exist.
 	 *
 	 * @static
-	 * @param $path The path to test.
+	 * @param  string   $path The path to test.
 	 * @return int|bool The last modified timestamp or false if the file or folder does not exist.
 	 */
 	public static function getLastTimeModified($path)
@@ -229,7 +229,7 @@ class IOHelper
 	 * Returns the file size in bytes for the given path or false if the file does not exist.
 	 *
 	 * @static
-	 * @param $path The path to test.
+	 * @param  string      $path The path to test.
 	 * @return bool|string The file size in bytes or false if the file does not exist.
 	 */
 	public static function getFileSize($path)
@@ -246,7 +246,7 @@ class IOHelper
 	 * Returns the folder size in bytes for the given path or false if the folder does not exist.
 	 *
 	 * @static
-	 * @param $path The path to test.
+	 * @param  string      $path The path to test.
 	 * @return bool|string The folder size in bytes or false if the folder does not exist.
 	 */
 	public static function getFolderSize($path)
@@ -265,7 +265,7 @@ class IOHelper
 	 * Will take a given path and normalize it to use single forward slashes for path separators.  If it is a folder, it will append a trailing forward slash to the end of the path.
 	 *
 	 * @static
-	 * @param $path The path to normalize.
+	 * @param  string $path The path to normalize.
 	 * @return string The normalized path.
 	 */
 	public static function normalizePathSeparators($path)
@@ -283,8 +283,8 @@ class IOHelper
 	 * Will take a path, make sure the file exists and if the size of the file is 0 bytes, return true.  Otherwise false.
 	 *
 	 * @static
-	 * @param $path The path to test.
-	 * @return bool Whether the file is empty or not.
+	 * @param  string $path The path to test.
+	 * @return bool  Whether the file is empty or not.
 	 */
 	public static function isFileEmpty($path)
 	{
@@ -299,8 +299,8 @@ class IOHelper
 	 * Will take a path, make sure the folder exists and if the size of the folder is 0 bytes, return true.  Otherwise false.
 	 *
 	 * @static
-	 * @param $path The path to test.
-	 * @return bool Whether the folder is empty or not.
+	 * @param  string $path The path to test.
+	 * @return bool   Whether the folder is empty or not.
 	 */
 	public static function isFolderEmpty($path)
 	{
@@ -315,9 +315,9 @@ class IOHelper
 	 * Returns owner of current filesystem object (UNIX systems). Returned value depends upon $getName parameter value.
 	 *
 	 * @static
-	 * @param         $path The path to check.
-	 * @param boolean $getName Defaults to 'true', meaning that owner name instead of ID should be returned.
-	 * @return mixed Owner name, or ID if $getName set to 'false' or false if the file or folder does not exist.
+	 * @param          $path The path to check.
+	 * @param  boolean $getName Defaults to 'true', meaning that owner name instead of ID should be returned.
+	 * @return mixed   Owner name, or ID if $getName set to 'false' or false if the file or folder does not exist.
 	 */
 	public static function getOwner($path, $getName = true)
 	{
@@ -341,9 +341,9 @@ class IOHelper
 	 * Returns group of current filesystem object (UNIX systems). Returned value depends upon $getName parameter value.
 	 *
 	 * @static
-	 * @param         $path The path to check.
-	 * @param boolean $getName Defaults to 'true', meaning that group name instead of ID should be returned.
-	 * @return mixed Group name, or ID if $getName set to 'false' or false if the file or folder does not exist.
+	 * @param          $path The path to check.
+	 * @param  boolean $getName Defaults to 'true', meaning that group name instead of ID should be returned.
+	 * @return mixed   Group name, or ID if $getName set to 'false' or false if the file or folder does not exist.
 	 */
 	public static function getGroup($path, $getName = true)
 	{
@@ -367,7 +367,7 @@ class IOHelper
 	 * Returns permissions of current filesystem object (UNIX systems).
 	 *
 	 * @static
-	 * @param $path The path to check
+	 * @param  string $path The path to check
 	 * @return string Filesystem object permissions in octal format (i.e. '0755'), false if the file or folder doesn't exist
 	 */
 	public static function getPermissions($path)
@@ -384,9 +384,9 @@ class IOHelper
 	 * Returns the contents of a folder as an array of {@link File} and/or {@link Folder} objects or false if the folder does not exist or is not readable.
 	 *
 	 * @static
-	 * @param string $path      The path to test.
-	 * @param bool   $recursive Whether to do a recursive folder search.
-	 * @param bool   $filter    The filter to use when performing the search.
+	 * @param  string     $path      The path to test.
+	 * @param  bool       $recursive Whether to do a recursive folder search.
+	 * @param  bool       $filter    The filter to use when performing the search.
 	 * @return array|bool An array of {@link File} and/or {@link Folder} objects or false if the folder does not exist or is not readable.
 	 */
 	public static function getFolderContents($path, $recursive = true, $filter = null)
@@ -410,8 +410,8 @@ class IOHelper
 	 * Will return the contents of the file as a string or an array if it exists and is readable, otherwise false.
 	 *
 	 * @static
-	 * @param string $path  The path of the file.
-	 * @param bool   $array Whether to return the contents of the file as an array or not.
+	 * @param  string            $path  The path of the file.
+	 * @param  bool              $array Whether to return the contents of the file as an array or not.
 	 * @return bool|string|array The contents of the file as a string, an array, or false if the file does not exist or is not readable.
 	 */
 	public static function getFileContents($path, $array = false)
@@ -443,7 +443,7 @@ class IOHelper
 	 * Will create a file on the file system at the given path and return a {@link File} object or false if we don't have write permissions.
 	 *
 	 * @static
-	 * @param $path The path of the file to create.
+	 * @param  string    $path The path of the file to create.
 	 * @return File|bool The newly created file as a {@link File} object or false if we don't have write permissions.
 	 */
 	public static function createFile($path)
@@ -470,8 +470,8 @@ class IOHelper
 	 * Will create a folder on the file system at the given path and return a {@link Folder} object or false if we don't have write permissions.
 	 *
 	 * @static
-	 * @param     $path The path of the file to create.
-	 * @param int $permissions The permissions to set the folder to.
+	 * @param  string      $path The path of the file to create.
+	 * @param  int         $permissions The permissions to set the folder to.
 	 * @return Folder|bool The newly created folder as a {@link Folder} object or false if we don't have write permissions.
 	 */
 	public static function createFolder($path, $permissions = static::defaultFolderPermissions)
@@ -501,11 +501,11 @@ class IOHelper
 	 * Will write $contents to a file.
 	 *
 	 * @static
-	 * @param string $path       The path of the file to write to.
-	 * @param string $contents   The contents to be written to the file.
-	 * @param bool   $autoCreate Whether or not to autocreate the file if it does not exist.
-	 * @param int    $flags      Any flags that need to be set when writing to the file.
-	 * @return bool
+	 * @param  string $path       The path of the file to write to.
+	 * @param  string $contents   The contents to be written to the file.
+	 * @param  bool   $autoCreate Whether or not to autocreate the file if it does not exist.
+	 * @param  int    $flags      Any flags that need to be set when writing to the file.
+	 * @return bool   'true' upon successful writing to the file, otherwise false.
 	 */
 	public static function writeToFile($path, $contents, $autoCreate = true, $flags = 0)
 	{
@@ -541,9 +541,9 @@ class IOHelper
 	 * Will attempt to change the owner of the given file system path (*nix only)
 	 *
 	 * @static
-	 * @param $path  The path to change the owner of.
-	 * @param $owner The new owner's name.
-	 * @return bool 'true' if successful, 'false' if not or the given path does not exist.
+	 * @param  string $path  The path to change the owner of.
+	 * @param  $owner The new owner's name.
+	 * @return bool   'true' if successful, 'false' if not or the given path does not exist.
 	 */
 	public static function changeOwner($path, $owner)
 	{
@@ -566,9 +566,9 @@ class IOHelper
 	 * Will attempt to change the group of the given file system path (*nix only)
 	 *
 	 * @static
-	 * @param $path  The path to change the group of.
-	 * @param $group The new group name.
-	 * @return bool 'true' if successful, 'false' if not, or the given path does not exist.
+	 * @param  string $path  The path to change the group of.
+	 * @param  $group The new group name.
+	 * @return bool   'true' if successful, 'false' if not, or the given path does not exist.
 	 */
 	public static function changeGroup($path, $group)
 	{
@@ -591,9 +591,9 @@ class IOHelper
 	 * Will attempt to change the permission of the given file system path (*nix only)
 	 *
 	 * @static
-	 * @param $path        The path to change the permissions of.
-	 * @param $permissions The new permissions.
-	 * @return bool 'true' if successful, 'false' if not or the path does not exist.
+	 * @param  string $path The path to change the permissions of.
+	 * @param  $permissions The new permissions.
+	 * @return bool         'true' if successful, 'false' if not or the path does not exist.
 	 */
 	public static function changePermissions($path, $permissions)
 	{
@@ -619,9 +619,9 @@ class IOHelper
 	 * Will copy a file from one path to another and create folders if necessary.
 	 *
 	 * @static
-	 * @param $path        The source path of the file.
-	 * @param $destination The destination path to copy the file to.
-	 * @return bool 'true' if the copy was successful, 'false' if it was not, the source file is not readable or does not exist.
+	 * @param  string $path        The source path of the file.
+	 * @param  string $destination The destination path to copy the file to.
+	 * @return bool   'true' if the copy was successful, 'false' if it was not, the source file is not readable or does not exist.
 	 */
 	public static function copyFile($path, $destination)
 	{
@@ -659,10 +659,10 @@ class IOHelper
 	 * Will copy the contents of one folder to another.
 	 *
 	 * @static
-	 * @param      $path        The source path to copy.
-	 * @param      $destination The destination path to copy to.
-	 * @param bool $validate    Whether to compare the size of the folders after the copy is complete.
-	 * @return bool 'true' if the copy was successful, 'false' if it was not, or $validate is true and the size of the folders do not match after the copy.
+	 * @param  string $path        The source path to copy.
+	 * @param  string $destination The destination path to copy to.
+	 * @param  bool   $validate    Whether to compare the size of the folders after the copy is complete.
+	 * @return bool   'true' if the copy was successful, 'false' if it was not, or $validate is true and the size of the folders do not match after the copy.
 	 */
 	public static function copyFolder($path, $destination, $validate = false)
 	{
@@ -704,9 +704,9 @@ class IOHelper
 	 * Renames a given file or folder to a new name.
 	 *
 	 * @static
-	 * @param $path    The original path of the file or folder.
-	 * @param $newName The new name of the file or folder.
-	 * @return bool 'true' if successful, 'false' if not or the source file or folder does not exist.
+	 * @param  string $path    The original path of the file or folder.
+	 * @param  string $newName The new name of the file or folder.
+	 * @return bool   'true' if successful, 'false' if not or the source file or folder does not exist.
 	 */
 	public static function rename($path, $newName)
 	{
@@ -739,8 +739,8 @@ class IOHelper
 	 * Purges the contents of a file.
 	 *
 	 * @static
-	 * @param $path The path of the file to clear.
-	 * @return bool 'true' if the file was successfully cleared, 'false' if it wasn't, if the file is not writable or the file does not exist.
+	 * @param  string $path The path of the file to clear.
+	 * @return bool   'true' if the file was successfully cleared, 'false' if it wasn't, if the file is not writable or the file does not exist.
 	 */
 	public static function clearFile($path)
 	{
@@ -766,8 +766,8 @@ class IOHelper
 	 * Purges the contents of a folder while leaving the folder itself.
 	 *
 	 * @static
-	 * @param $path The path of the folder to clear.
-	 * @return bool 'true' if is successfully purges the folder, 'false' if the folder does not exist.
+	 * @param  string $path The path of the folder to clear.
+	 * @return bool  'true' if is successfully purges the folder, 'false' if the folder does not exist.
 	 */
 	public static function clearFolder($path)
 	{
@@ -799,8 +799,8 @@ class IOHelper
 	 * Deletes a file from the file system.
 	 *
 	 * @static
-	 * @param $path The path of the file to delete.
-	 * @return bool 'true' if successful, 'false' if it cannot be deleted, it does not exist or it is not writable.
+	 * @param  string $path The path of the file to delete.
+	 * @return bool   'true' if successful, 'false' if it cannot be deleted, it does not exist or it is not writable.
 	 */
 	public static function deleteFile($path)
 	{
@@ -828,8 +828,8 @@ class IOHelper
 	 * Deletes a folder from the file system.
 	 *
 	 * @static
-	 * @param $path The path of the folder to delete.
-	 * @return bool 'true' if successful, 'false' if it cannot be deleted, it does not exist or it is not writable.
+	 * @param  string $path The path of the folder to delete.
+	 * @return bool   'true' if successful, 'false' if it cannot be deleted, it does not exist or it is not writable.
 	 */
 	public static function deleteFolder($path)
 	{
@@ -857,7 +857,7 @@ class IOHelper
 	 * Calculates the MD5 hash for a given file path or false if one could not be calculated or the file does not exist.
 	 *
 	 * @static
-	 * @param $path The path of the file to calculate.
+	 * @param  string      $path The path of the file to calculate.
 	 * @return bool|string The MD5 hash or false if it does not exist, isn't readable or could not be calculated.
 	 */
 	public static function getFileMD5($path)
@@ -878,8 +878,9 @@ class IOHelper
 	 * Used by {@link getFolderSize} to calculate the size of a folder.
 	 *
 	 * @static
-	 * @param $path The path of the folder.
-	 * @return int The size of the folder in bytes.
+	 * @access private
+	 * @param  string $path The path of the folder.
+	 * @return int    The size of the folder in bytes.
 	 */
 	private static function _folderSize($path)
 	{
@@ -899,6 +900,7 @@ class IOHelper
 	/**
 	 *
 	 * @static
+	 * @access private
 	 * @param      $path
 	 * @param bool $recursive
 	 * @param null $filter
