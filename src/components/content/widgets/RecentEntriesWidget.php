4611c4919830887fb26392e0ee5f4ca1911963d9
Brandon Kelly <brandon@pixelandtonic.com>
2012-09-30 13:30:18 -0700

    Recent Entries widget

---
+++ src/components/content/widgets/RecentEntriesWidget.php
@@ -0,0 +1,85 @@
+<?php
+namespace Blocks;
+
+/**
+ *
+ */
+class RecentEntriesWidget extends BaseWidget
+{
+	public $multipleInstances = true;
+
+	/**
+	 * Returns the type of widget this is.
+	 *
+	 * @return string
+	 */
+	public function getName()
+	{
+		return Blocks::t('Recent Entries');
+	}
+
+	/**
+	 * Defines the settings.
+	 *
+	 * @access protected
+	 * @return array
+	 */
+	protected function defineSettings()
+	{
+		if (Blocks::hasPackage(BlocksPackage::PublishPro))
+		{
+			$settings['section'] = array(AttributeType::Mixed, 'default' => '*');
+		}
+
+		$settings['limit'] = array(AttributeType::Number, 'default' => 10);
+
+		return $settings;
+	}
+
+	/**
+	 * Returns the widget's body HTML.
+	 *
+	 * @return string
+	 */
+	public function getSettingsHtml()
+	{
+		return blx()->templates->render('_components/widgets/RecentEntries/settings', array(
+			'settings' => $this->getSettings()
+		));
+	}
+
+	/**
+	 * Gets the widget's title.
+	 *
+	 * @return string
+	 */
+	public function getTitle()
+	{
+		if (Blocks::hasPackage(BlocksPackage::PublishPro))
+		{
+			$sectionId = $this->getSettings()->section;
+			if (is_numeric($sectionId))
+			{
+				$section = blx()->sections->getSectionById($sectionId);
+				if ($section)
+				{
+					return Blocks::t('Recently in {section}', array('section' => $section->name));
+				}
+			}
+		}
+
+		return Blocks::t('Recent Entries');
+	}
+
+	/**
+	 * Gets the widget's body HTML.
+	 *
+	 * @return string
+	 */
+	public function getBodyHtml()
+	{
+		return blx()->templates->render('_components/widgets/RecentEntries/body', array(
+			'settings' => $this->getSettings()
+		));
+	}
+}
