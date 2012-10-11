02667f789e2a9d012846414f26545756038bd0b0
Brandon Kelly <brandon@pixelandtonic.com>
2012-10-10 20:09:47 -0700

    Fixed backwards behavior with $flattenValues in getAttributes()

--- src/components/models/BaseModel.php
+++ src/components/models/BaseModel.php
@@ -150,11 +150,11 @@ abstract class BaseModel extends \CModel
 			{
 				if ($flattenValues)
 				{
-					$values[$name] = $this->$name;
+					$values[$name] = ModelHelper::packageAttributeValue($config, $this->$name);
 				}
 				else
 				{
-					$values[$name] = ModelHelper::packageAttributeValue($config, $this->$name);
+					$values[$name] = $this->$name;
 				}
 			}
 		}
