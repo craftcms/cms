435bc0f913515380e6c6e22d4c8510f18f646378
Brad Bell <brad@pixelandtonic.com>
2012-10-24 16:06:22 -0700

    Elliot => Elliott

--- src/components/et/ElliottEndPoints.php
+++ src/components/et/ElliottEndPoints.php
@@ -6,8 +6,8 @@ namespace Blocks;
  */
 class ElliottEndPoints
 {
-	const Check                         = 'https://elliot.blockscms.com/admin.php/actions/elliot/app/check';
-	const DownloadPackage               = 'https://elliot.blockscms.com/admin.php/actions/elliot/app/downloadpackage';
-	const GetAppReleaseFileMD5          = 'https://elliot.blockscms.com/admin.php/actions/elliot/app/getappreleasefilemd5';
-	const Ping                          = 'https://elliot.blockscms.com/admin.php/actions/elliot/app/ping';
+	const Check                         = 'https://elliott.blockscms.com/admin.php/actions/elliott/app/check';
+	const DownloadPackage               = 'https://elliott.blockscms.com/admin.php/actions/elliott/app/downloadpackage';
+	const GetAppReleaseFileMD5          = 'https://elliott.blockscms.com/admin.php/actions/elliott/app/getappreleasefilemd5';
+	const Ping                          = 'https://elliott.blockscms.com/admin.php/actions/elliott/app/ping';
 }
