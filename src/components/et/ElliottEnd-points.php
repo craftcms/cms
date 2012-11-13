70f6c41419c11ec4e9e681be3479bb4d46a4d09d
Brandon Kelly <brandon@pixelandtonic.com>
2012-11-12 16:34:10 -0800

    "Endpoints" is One Word, Part 1

---
+++ src/components/et/ElliottEnd-points.php
@@ -0,0 +1,13 @@
+<?php
+namespace Blocks;
+
+/**
+ *
+ */
+class ElliottEndpoints
+{
+	const CheckForUpdates      = 'https://elliott.blockscms.com/index.php/actions/elliott/blocks/checkForUpdates';
+	const DownloadPackage      = 'https://elliott.blockscms.com/index.php/actions/elliott/blocks/downloadUpdate';
+	const GetAppReleaseFileMD5 = 'https://elliott.blockscms.com/index.php/actions/elliott/blocks/getappreleasefilemd5';
+	const Ping                 = 'https://elliott.blockscms.com/index.php/actions/elliott/blocks/ping';
+}
