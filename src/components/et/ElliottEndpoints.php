31168abcd83b7767664df62f7d452b7ec2927d97
Brad Bell <brad@pixelandtonic.com>
2012-11-16 15:48:43 -0800

    Change default db collation to utf8_unicode_ci on mysql.

--- src/components/et/ElliottEndpoints.php
+++ src/components/et/ElliottEndpoints.php
@@ -7,7 +7,7 @@ namespace Blocks;
 class ElliottEndpoints
 {
 	const CheckForUpdates      = 'https://elliott.blockscms.com/actions/elliott/blocks/checkForUpdates';
-	const DownloadPackage      = 'https://elliott.blockscms.com/actions/elliott/blocks/downloadUpdate';
+	const DownloadUpdate       = 'https://elliott.blockscms.com/actions/elliott/blocks/downloadUpdate';
 	const GetAppReleaseFileMD5 = 'https://elliott.blockscms.com/actions/elliott/blocks/getappreleasefilemd5';
 	const Ping                 = 'https://elliott.blockscms.com/actions/elliott/blocks/ping';
 }
