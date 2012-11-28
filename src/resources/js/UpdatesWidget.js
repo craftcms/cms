9ac03229455d071e2a8db993d97e3cf5fc87eaed
Brandon Kelly <brandon@pixelandtonic.com>
2012-11-28 14:54:05 -0800

    Updated the updates widget

--- src/resources/js/UpdatesWidget.js
+++ src/resources/js/UpdatesWidget.js
@@ -10,35 +10,11 @@ Blocks.UpdatesWidget = Blocks.Base.extend({
 		this.$widget = $('#widget'+widgetId);
 		this.$widget.addClass('loading');
 
-		var data = {
-			handle: 'all'
-		};
-
-		Blocks.postActionRequest('update/getUpdates', data, $.proxy(function(response) {
-			var $tds = this.$widget.find('td');
-
-			for (var i = 0; i < response.updateInfo.length; i++)
-			{
-				var item = response.updateInfo[i],
-					$td = $($tds[i]);
-
-				var html = '<div><strong>'+item.name+'</strong> '+item.version+'</div>';
-
-				if (item.critical)
-				{
-					html += '<div class="badge warning">'+Blocks.t('Critical')+'</div>';
-				}
-
-				var date = new Date(item.releaseDate * 1000);
-				var year = date.getFullYear();
-				var month = date.getMonth() + 1;
-				var day = date.getDate();
-				html += '<span class="light nowrap timestamp">'+year+'-'+month+'-'+day+'</span>';
-
-				$td.html(html);
-			}
+		Blocks.postActionRequest('dashboard/checkForUpdates', $.proxy(function(response) {
 
 			this.$widget.removeClass('loading');
+			this.$widget.find('.body').html(response);
+
 		}, this));
 	}
 });
