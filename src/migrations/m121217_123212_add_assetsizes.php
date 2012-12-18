84240f083f12e9917fd82a78d2c0357da726195b
Brandon Kelly <brandon@pixelandtonic.com>
2012-12-17 23:26:19 -0800

    Changed the add_assetsizes timestamp to 12/12/17

--- src/migrations/m121217_123212_add_assetsizes.php
+++ src/migrations/m121217_123212_add_assetsizes.php
@@ -4,7 +4,7 @@ namespace Blocks;
 /**
  * The class name is the UTC timestamp in the format of mYYMMDD_HHMMSS_migrationName
  */
-class m121204_123212_add_assetsizes extends \CDbMigration
+class m121217_123212_add_assetsizes extends \CDbMigration
 {
 	/**
 	 * Any migration code in here is wrapped inside of a transaction.
