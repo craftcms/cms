e4653b1023f6d1ccd43bca721b97ed318db34ba4
Brandon Kelly <brandon@pixelandtonic.com>
2012-12-27 17:03:52 -0800

    New update.js

--- src/resources/js/update.js
+++ src/resources/js/update.js
@@ -1,107 +1,87 @@
 (function($) {
-$(document).ready(function() {
-
-var $status = $('#status'),
-	updateInfo,
-	totalUpdates,
-	updating = -1;
-
-function showError(msg)
-{
-	$status.addClass('error');
-	$status.html(msg);
-}
-
-function showSuccess(msg)
-{
-	$status.addClass('success');
-	$status.html(msg);
-}
 
 
-// make sure an update handle was provided
-if (!updateHandle)
-{
-	showError(Blocks.t('Unable to determine what to update.'));
-	return;
-}
+Blocks.Updater = Blocks.Base.extend({
 
-function getUpdateInfo()
-{
-	// get the name and latest version
-	var data = {
-		handle: updateHandle
-	};
+	$status: null,
+	data: null,
 
-	Blocks.postActionRequest('update/getUpdates', data, function(response) {
-		if (response.success)
+	init: function(handle)
+	{
+		this.$status = $('#status');
+
+		if (!handle)
 		{
-			if (response.updateInfo)
-			{
-				updateInfo = response.updateInfo;
-				totalUpdates = updateInfo.length;
-
-				updateNext();
-			}
-			else
-			{
-				showSuccess(Blocks.t('You’re already up-to-date.'));
-			}
-		}
-		else
-		{
-			var error = (data.error || Blocks.t('An unknown error occurred.'));
-			showError(error)
-		}
-	});
-}
-
-function updateNext()
-{
-	updating++;
-
-	$status.html('Updating '+updateInfo[updating].name+' to version '+updateInfo[updating].version+' ('+updating+' of '+totalUpdates+')');
-
-	var data = {
-		handle: updateInfo[updating].handle
-	};
-
-	Blocks.postActionRequest('update/runAutoUpdate', data, function(data, textStatus) {
-		if (!data || textStatus != 'success')
-		{
-			showError(Blocks.t('An unknown error occurred while updating {name}.', {'name': updateInfo[updating].name}));
-			return;
+			this.showError(Blocks.t('Unable to determine what to update.'));
 		}
 
-		if (data.error)
-		{
-			showError(data.error);
-			return;
-		}
+		this.data = {
+			handle: handle
+		};
 
-		if (updating == totalUpdates-1)
-		{
-			showSuccess(Blocks.t('All done!'));
-
-			// Redirect to the Dashboard in half a second
-			setTimeout(function() {
-				window.location = Blocks.getUrl('dashboard');
-			}, 500);
-
-			return;
-		}
-
-		updateNext();
+		this.postActionRequest('update/prepare');
 	},
 
-	function(jqXHR, textStatus, errorThrown)
+	updateStatus: function(msg)
 	{
-		showError(Blocks.t('An unknown error occurred while updating {name}.', {'name': updateInfo[updating].name}));
-		return;
-	});
-}
+		this.$status.html(msg);
+	},
 
-getUpdateInfo();
+	showError: function(msg)
+	{
+		this.updateStatus(msg);
+		this.$status.addClass('error');
+	},
 
+	postActionRequest: function(action)
+	{
+		Blocks.postActionRequest(action, this.data, $.proxy(this, 'onSuccessResponse'), $.proxy(this, 'onErrorResponse'));
+	},
+
+	onSuccessResponse: function(response)
+	{
+		if (response.data)
+		{
+			this.data = response.data;
+		}
+
+		if (response.nextStatus)
+		{
+			this.updateStatus(response.nextStatus);
+		}
+
+		if (response.nextAction)
+		{
+			this.postActionRequest(response.nextAction);
+		}
+
+		if (response.error)
+		{
+			this.$status.addClass('error');
+		}
+		else if (response.finished)
+		{
+			this.onFinish();
+		}
+	},
+
+	onErrorResponse: function()
+	{
+		this.showError(Blocks.t('An unknown error occurred. Rolling back…'));
+		this.postActionRequest('update/rollback');
+	},
+
+	onFinish: function()
+	{
+		this.updateStatus(Blocks.t('All done!'));
+		this.$status.addClass('success');
+
+		// Redirect to the Dashboard in half a second
+		setTimeout(function() {
+			window.location = Blocks.getUrl('dashboard');
+		}, 500);
+	}
 });
+
+
 })(jQuery);
