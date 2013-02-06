3e5faf2a5a8b6ed05e02c37b40978b8ab78e41ed
Brandon Kelly <brandon@pixelandtonic.com>
2013-02-06 15:19:10 -0800

    quoteDatabaseName()

--- src/components/db/helpers/DbHelper.php
+++ src/components/db/helpers/DbHelper.php
@@ -506,13 +506,13 @@ class DbHelper
 		if (StringHelper::isNullOrEmpty($prefix))
 		{
 			return blx()->db->createCommand()
-				->setText("SHOW TABLES FROM ".blx()->db->quoteValue($databaseName).";")
+				->setText("SHOW TABLES FROM ".blx()->db->quoteDatabaseName($databaseName).";")
 				->queryColumn();
 		}
 		else
 		{
 			return blx()->db->createCommand()
-				->setText("SHOW TABLES FROM ".blx()->db->quoteValue($databaseName)." LIKE '{$prefix}_%';")
+				->setText("SHOW TABLES FROM ".blx()->db->quoteDatabaseName($databaseName)." LIKE '{$prefix}_%';")
 				->queryColumn();
 		}
 
