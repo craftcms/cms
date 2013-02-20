aea6f48d2ecada36b08c19edbb9775ec4e78b048
andris-sevcenko <asevcenko@gmail.com>
2013-02-20 16:13:43 +0200

    Fix folder lookup where only folders with no parents would ever be returned.

--- src/components/assets/models/FolderCriteriaModel.php
+++ src/components/assets/models/FolderCriteriaModel.php
@@ -7,6 +7,20 @@ namespace Blocks;
 class FolderCriteriaModel extends BaseModel
 {
 	/**
+	 * If no parentId is being set, set it to false instead of null, since null means something in this context.
+	 * 
+	 * @param mixed|null $attributes
+	 */
+	public function __construct($attributes)
+	{
+		parent::__construct($attributes);
+		if (!isset($attributes['parentId']))
+		{
+			$this->parentId = false;
+		}
+	}
+
+	/**
 	 * @return array
 	 */
 	public function defineAttributes()
