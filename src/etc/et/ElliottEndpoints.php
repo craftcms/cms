90ceeb9c1ff389287723752ebee5498bc667ea3c
Brad Bell <brad@pixelandtonic.com>
2013-02-27 12:51:33 -0800

    Revert "Old."
    This reverts commit 8d2374f1e9282dc51156089ea266c360710f5ed7.

---
+++ src/etc/et/ElliottEndpoints.php
@@ -0,0 +1,12 @@
+<?php
+namespace Craft;
+
+/**
+ *
+ */
+class ElliottEndpoints
+{
+	const CheckForUpdates      = '@@@elliottEndpointUrl@@@actions/elliott/craft/checkForUpdates';
+	const DownloadUpdate       = '@@@elliottEndpointUrl@@@actions/elliott/craft/downloadUpdate';
+	const Ping                 = '@@@elliottEndpointUrl@@@actions/elliott/craft/ping';
+}
