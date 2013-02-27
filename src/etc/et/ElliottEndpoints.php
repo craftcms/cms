49eedb89c7c794c81ab94921d2fb87181a34f4ff
Brad Bell <brad@pixelandtonic.com>
2013-02-27 12:59:37 -0800

    craft => app

--- src/etc/et/ElliottEndpoints.php
+++ src/etc/et/ElliottEndpoints.php
@@ -6,7 +6,7 @@ namespace Craft;
  */
 class ElliottEndpoints
 {
-	const CheckForUpdates      = '@@@elliottEndpointUrl@@@actions/elliott/craft/checkForUpdates';
-	const DownloadUpdate       = '@@@elliottEndpointUrl@@@actions/elliott/craft/downloadUpdate';
-	const Ping                 = '@@@elliottEndpointUrl@@@actions/elliott/craft/ping';
+	const CheckForUpdates      = '@@@elliottEndpointUrl@@@actions/elliott/app/checkForUpdates';
+	const DownloadUpdate       = '@@@elliottEndpointUrl@@@actions/elliott/app/downloadUpdate';
+	const Ping                 = '@@@elliottEndpointUrl@@@actions/elliott/app/ping';
 }
