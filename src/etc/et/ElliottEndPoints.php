4dfb3b66fa77a52746079c75547bd4512c86da86
Brad Bell <brad@pixelandtonic.com>
2013-02-27 12:43:45 -0800

    Spring Cleaning.

--- src/etc/et/ElliottEndPoints.php
+++ src/etc/et/ElliottEndPoints.php
@@ -6,7 +6,7 @@ namespace Craft;
  */
 class ElliottEndPoints
 {
-	const CheckForUpdates      = 'https://elliott.buildwithcraft.com/actions/elliott/craft/checkForUpdates';
-	const DownloadUpdate       = 'https://elliott.buildwithcraft.com/actions/elliott/craft/downloadUpdate';
-	const Ping                 = 'https://elliott.buildwithcraft.com/actions/elliott/craft/ping';
+	const CheckForUpdates      = 'https://elliott.buildwithcraft.com/actions/elliott/app/checkForUpdates';
+	const DownloadUpdate       = 'https://elliott.buildwithcraft.com/actions/elliott/app/downloadUpdate';
+	const Ping                 = 'https://elliott.buildwithcraft.com/actions/elliott/app/ping';
 }
