8d88b8bd380db4ad7688d56b0c93fa5cde698b8f
Brandon Kelly <brandon@pixelandtonic.com>
2013-03-30 10:17:58 -0700

    No, the plugin does not have an icon.

--- src/variables/CpVariable.php
+++ src/variables/CpVariable.php
@@ -46,24 +46,6 @@ class CpVariable
 				{
 					$lcHandle = strtolower($plugin->getClassHandle());
 					$nav[$lcHandle] = array('name' => $plugin->getName());
-
-					// Does the plugin have an icon?
-					$resourcesPath = craft()->path->getPluginsPath().$lcHandle.'/resources/';
-
-					if (IOHelper::fileExists($resourcesPath.'icon-16x16.png'))
-					{
-						$nav[$lcHandle]['hasIcon'] = true;
-
-						$url = UrlHelper::getResourceUrl($lcHandle.'/icon-16x16.png');
-						craft()->templates->includeCss("#sidebar #nav-{$lcHandle} { background-image: url('{$url}'); }");
-
-						// Does it even have a hi-res version?
-						if (IOHelper::fileExists($resourcesPath.'icon-32x32.png'))
-						{
-							$url = UrlHelper::getResourceUrl($lcHandle.'/icon-32x32.png');
-							craft()->templates->includeHiResCss("#sidebar #nav-{$lcHandle} { background-image: url('{$url}'); }");
-						}
-					}
 				}
 			}
 		}
