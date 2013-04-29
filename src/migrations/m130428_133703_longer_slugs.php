4d43829070fc23c2f8cb82c6d7f46f8606e0ea91
Brad Bell <brad@pixelandtonic.com>
2013-04-28 17:31:30 -0700

    Defensive migration.

--- src/migrations/m130428_133703_longer_slugs.php
+++ src/migrations/m130428_133703_longer_slugs.php
@@ -13,7 +13,25 @@ class m130428_133703_longer_slugs extends BaseMigration
 	 */
 	public function safeUp()
 	{
-		$this->alterColumn('entries_i18n', 'slug', array('column' => ColumnType::Varchar, 'length' => 255, 'null' => false));
+		$entriesI18nTable = $this->dbConnection->schema->getTable('{{entries_i18n}}');
+
+		if ($entriesI18nTable)
+		{
+			if (($slugColumn = $entriesI18nTable->getColumn('slug')) !== null)
+			{
+				Craft::log('Altering the `slug` column from `entries_i18n`', LogLevel::Info, true);
+				$this->alterColumn('entries_i18n', 'slug', array('column' => ColumnType::Varchar, 'length' => 255, 'null' => false));
+				Craft::log('Altered the `slug` column from `entries_i18n`', LogLevel::Info, true);
+			}
+			else
+			{
+				Craft::log('Tried to alter the `slug` column from `entries_i18n`, but the column is missing.', LogLevel::Warning);
+			}
+		}
+		else
+		{
+			Craft::log('Tried to alter the `slug` column from `entries_i18n`, but the table does not exist!', LogLevel::Error);
+		}
 
 		return true;
 	}
