765b6c7df5320e5ec6c96ad370f376ed8ba003d5
Brandon Kelly <brandon@pixelandtonic.com>
2013-05-11 13:39:56 -0700

    Color fieldtype

---
+++ src/fieldtypes/ColorFieldType.php
@@ -0,0 +1,43 @@
+<?php
+namespace Craft;
+
+/**
+ *
+ */
+class ColorFieldType extends BaseFieldType
+{
+	/**
+	 * Returns the type of field this is.
+	 *
+	 * @return string
+	 */
+	public function getName()
+	{
+		return Craft::t('Color');
+	}
+
+	/**
+	 * Returns the content attribute config.
+	 *
+	 * @return mixed
+	 */
+	public function defineContentAttribute()
+	{
+		return array(AttributeType::String, 'column' => ColumnType::Char, 'length' => 7);
+	}
+
+	/**
+	 * Returns the field's input HTML.
+	 *
+	 * @param string $name
+	 * @param mixed  $value
+	 * @return string
+	 */
+	public function getInputHtml($name, $value)
+	{
+		return craft()->templates->render('_includes/forms/color', array(
+			'name'  => $name,
+			'value' => $value,
+		));
+	}
+}
