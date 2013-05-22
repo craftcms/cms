475305ffb98edf9e21f90436d8113d74377db328
Brad Bell <brad@pixelandtonic.com>
2013-05-21 20:04:15 -0700

    Add a "Download Backup" checkbox to the db backup tool.

--- src/tools/DbBackupTool.php
+++ src/tools/DbBackupTool.php
@@ -27,6 +27,20 @@ class DbBackupTool extends BaseTool
 	}
 
 	/**
+	 * Returns the tool's options HTML.
+	 *
+	 * @return string
+	 */
+	public function getOptionsHtml()
+	{
+		return craft()->templates->render('_includes/forms/checkbox', array(
+			'name'    => 'downloadBackup',
+			'label'   => Craft::t('Download Backup?'),
+			'checked' => true,
+		));
+	}
+
+	/**
 	 * Performs the tool's action.
 	 *
 	 * @param array $params
@@ -34,6 +48,22 @@ class DbBackupTool extends BaseTool
 	 */
 	public function performAction($params = array())
 	{
-		craft()->db->backup();
+		$file = craft()->db->backup();
+
+		if (IOHelper::fileExists($file) && isset($params['downloadBackup']) && (bool)$params['downloadBackup'])
+		{
+			$destZip = craft()->path->getTempPath().IOHelper::getFileName($file, false).'.zip';
+			if (IOHelper::fileExists($destZip))
+			{
+				IOHelper::deleteFile($destZip, true);
+			}
+
+			IOHelper::createFile($destZip);
+
+			if (Zip::add($destZip, $file, craft()->path->getDbBackupPath()))
+			{
+				return array('backupFile' => IOHelper::getFileName($destZip, false));
+			}
+		}
 	}
 }
