aef07e26b5a0c32b242bfdf9e387698fab292763
andris-sevcenko <asevcenko@gmail.com>
2013-05-29 14:46:43 +0300

    Isn't ir marvelous, how a simple typo can ruin someones day? A typo and IE9, that is.

--- src/helpers/JsonHelper.php
+++ src/helpers/JsonHelper.php
@@ -13,6 +13,6 @@ class JsonHelper extends \CJSON
 	{
 		header('Cache-Control: no-cache, must-revalidate');
 		header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-		header('Content-Type: application/json; charset=utf=8');
+		header('Content-Type: application/json; charset=utf-8');
 	}
 }
