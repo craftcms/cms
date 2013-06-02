90b06cc4542c26355704aee99f3350e609ad9d55
Brandon Kelly <brandon@pixelandtonic.com>
2013-06-01 18:03:07 -0700

    Track migration

--- src/services/UpdatesService.php
+++ src/services/UpdatesService.php
@@ -197,6 +197,9 @@ class UpdatesService extends BaseApplicationComponent
 		$info->build       = $build;
 		$info->releaseDate = $releaseDate;
 
+		// TODO: Deprecate after next breakpoint release.
+		$info->track = '@@@track@@@';
+
 		return Craft::saveInfo($info);
 	}
 
