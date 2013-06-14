b1d937ca90780629d02e067cdac99cb9eb9f5f69
andris-sevcenko <asevcenko@gmail.com>
2013-06-14 15:49:54 +0300

    Some refactoring, context menus for sources and elements in Assets.

--- src/resources/js/classes/AssetIndex.js
+++ src/resources/js/classes/AssetIndex.js
@@ -6,15 +6,20 @@ Craft.AssetIndex = Craft.BaseElementIndex.extend({
     $buttons: null,
     $uploadButton: null,
     $progressBar: null,
+    $folders: null,
 
     uploader: null,
     promptHandler: null,
     progressBar: null,
+    indexMode: false,
 
     initialSourceKey: null,
     isIndexBusy: false,
     _uploadFileProgress: {},
     uploadedFileIds: [],
+
+    _singleFileMenu: null,
+    _multiFileMenu: null,
 
 	init: function(elementType, $container, settings)
 	{
@@ -25,6 +30,36 @@ Craft.AssetIndex = Craft.BaseElementIndex.extend({
         settings.onUpdateElements = this.addCallback(settings.onUpdateElements, $.proxy(this, '_onUpdateElements'));
 
         this.base(elementType, $container, settings);
+
+
+        if (this.settings.mode == "index")
+        {
+            this.indexMode = true;
+            this.initIndexMode();
+        }
+    },
+
+    /**
+     * Full blown Assets.
+     */
+    initIndexMode: function ()
+    {
+        // Context menus for the folders
+        var assetIndex = this;
+        this.$sources.each(function () {
+            var menuOptions = [{ label: Craft.t('New subfolder'), onClick: $.proxy(assetIndex, '_createSubfolder') }];
+
+            element = $(this);
+
+            // For all folders that are not top folders
+            if (element.parents('ul').length > 1)
+            {
+                menuOptions.push({ label: Craft.t('Rename folder'), onClick: $.proxy(assetIndex, '_renameFolder') });
+                menuOptions.push({ label: Craft.t('Delete folder'), onClick: $.proxy(assetIndex, '_deleteFolder') });
+            }
+            new Garnish.ContextMenu(element, menuOptions, {menuClass: 'menu assets-contextmenu'});
+        });
+
     },
 
     /**
@@ -126,7 +161,7 @@ Craft.AssetIndex = Craft.BaseElementIndex.extend({
 
         if (response.success || response.prompt) {
 
-            // TODO respect the select settings
+            // TODO respect the select settings regarding limits
             // Add the uploaded file to the selected ones, if appropriate
             this.uploadedFileIds.push(response.fileId);
 
@@ -137,7 +172,7 @@ Craft.AssetIndex = Craft.BaseElementIndex.extend({
             }
         }
 
-        // is this the last file?
+        // for the last file, display prompts, if any. If not - just update the element view.
         if (! this.uploader.getInProgress()) {
 
             this.setIndexAvailable();
@@ -222,6 +257,13 @@ Craft.AssetIndex = Craft.BaseElementIndex.extend({
      */
     _onUpdateElements: function ()
     {
+        if (this.indexMode)
+        {
+            $elements = this.$elementContainer.children(':not(.disabled)');
+            this._initElementSelect($elements);
+            this._attachElementEvents($elements);
+        }
+
         // See if we have freshly uploaded files to add to selection
         if (this.uploadedFileIds.length)
         {
@@ -235,7 +277,91 @@ Craft.AssetIndex = Craft.BaseElementIndex.extend({
             // Reset the list.
             this.uploadedFileIds = [];
         }
+    },
+
+    _initElementSelect: function ($children)
+    {
+        var elementSelect = new Garnish.Select(this.$elementContainer, $children, {
+            multi: true,
+            vertical: (this.getState('view') == 'table'),
+            waitForDblClick: true,
+            onSelectionChange: $.proxy(this, '_onElementSelectionChange')
+        });
+
+        this.setSelector(elementSelect);
+    },
+
+    _onElementSelectionChange: function ()
+    {
+        this._enableElementContextMenu();
+    },
+
+    _attachElementEvents: function ($elements)
+    {
+        // Doubleclick opens the HUD for editing
+        this.removeListener($elements, 'dlbclick');
+        this.addListener($elements, 'dblclick', $.proxy(this, '_editProperties'));
+
+        // Context menus
+        this._destroyElementContextMenus();
+        this._createElementContextMenus($elements);
+    },
+
+    _editProperties: function (event)
+    {
+        var $target = $(event.currentTarget);
+        if (!$target.data('AssetEditor'))
+        {
+            $target.data('AssetEditor', new Assets.AssetEditor($target.attr('data-id'), $target));
+        }
+
+        $target.data('AssetEditor').show();
+    },
+
+    _createElementContextMenus: function ($elements)
+    {
+        var settings = {menuClass: 'menu assets-contextmenu'};
+
+        var menuOptions = [{ label: Craft.t('View file'), onClick: $.proxy(this, '_viewFile') }];
+        menuOptions.push({ label: Craft.t('Edit properties'), onClick: $.proxy(this, '_showProperties') });
+        menuOptions.push({ label: Craft.t('Rename file'), onClick: $.proxy(this, '_renameFile') });
+        menuOptions.push('-');
+        menuOptions.push({ label: Craft.t('Delete file'), onClick: $.proxy(this, '_deleteFile') });
+        this._singleFileMenu = new Garnish.ContextMenu($elements, menuOptions, settings);
+
+        menuOptions = [{ label: Craft.t('Delete'), onClick: $.proxy(this, '_deleteFiles') }];
+        this._multiFileMenu = new Garnish.ContextMenu($elements, menuOptions, settings);
+
+        this._enableElementContextMenu();
+    },
+
+    _destroyElementContextMenus: function ()
+    {
+        if (this._singleFileMenu !== null)
+        {
+            this._singleFileMenu.destroy();
+        }
+        if (this._multiFileMenu !== null)
+        {
+            this._singleFileMenu.destroy();
+        }
+    },
+
+    _enableElementContextMenu: function ()
+    {
+        this._multiFileMenu.disable();
+        this._singleFileMenu.disable();
+
+        if (this.selector.getTotalSelected() == 1)
+        {
+            this._singleFileMenu.enable();
+        }
+        else if (this.selector.getTotalSelected() > 1)
+        {
+            this._multiFileMenu.enable();
+        }
     }
+
 });
 
 // Register it!
