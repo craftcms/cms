445a3bab136bf8eb5c55a9bfb4f29af4c76e54d5
andris-sevcenko <asevcenko@gmail.com>
2013-07-05 17:04:18 +0300

    Cloud icon!

--- src/resources/js/classes/Uploader.js
+++ src/resources/js/classes/Uploader.js
@@ -35,7 +35,7 @@ Craft.Uploader = Garnish.Base.extend({
         action:       Craft.actionUrl + '/assets/uploadFile',
         template:     '<div class="assets-qq-uploader">'
             +   '<div class="assets-qq-upload-drop-area"></div>'
-            +   '<a href="javascript:;" class="btn submit assets-qq-upload-button" data-icon="?" style="position: relative; overflow: hidden; direction: ltr; " role="button">' + Craft.t('Upload files') + '</a>'
+            +   '<a href="javascript:;" class="btn submit assets-qq-upload-button" data-icon="↑" style="position: relative; overflow: hidden; direction: ltr; " role="button">' + Craft.t('Upload files') + '</a>'
             +   '<ul class="assets-qq-upload-list hidden"></ul>'
             + '</div>',
 
