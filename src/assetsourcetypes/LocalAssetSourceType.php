2e6a63154239155199914b9e0e7ff655e76bd14c
andris-sevcenko <asevcenko@gmail.com>
2013-07-29 16:47:17 +0300

    Fixing a too relaxed requirement for variable type.

--- src/assetsourcetypes/LocalAssetSourceType.php
+++ src/assetsourcetypes/LocalAssetSourceType.php
@@ -137,7 +137,7 @@ class LocalAssetSourceType extends BaseAssetSourceType
 	 * @param BaseAssetSourceType $sourceType = null
 	 * @return string
 	 */
-	private function _getSourceFileSystemPath(BaseAssetSourceType $sourceType = null)
+	private function _getSourceFileSystemPath(LocalAssetSourceType $sourceType = null)
 	{
 		$path = is_null($sourceType) ? $this->getBasePath() : $sourceType->getBasePath();
 		$path = IOHelper::getRealPath($path);
