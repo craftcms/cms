99e9c8d0631dfb409387dcc83bf470af55de1f88
Brandon Kelly <brandon@pixelandtonic.com>
2013-08-04 21:02:35 -0700

    Use the proper template loader param names to match the interface

--- src/etc/templating/twigextensions/TemplateLoader.php
+++ src/etc/templating/twigextensions/TemplateLoader.php
@@ -9,47 +9,47 @@ class TemplateLoader implements \Twig_LoaderInterface
 	/**
 	 * Gets the source code of a template.
 	 *
-	 * @param  string $template The name of the template to load, or a StringTemplate object.
-	 * @return string           The template source code.
+	 * @param  string $name The name of the template to load, or a StringTemplate object.
+	 * @return string       The template source code.
 	 */
-	public function getSource($template)
+	public function getSource($name)
 	{
-		if (is_string($template))
+		if (is_string($name))
 		{
-			return IOHelper::getFileContents(craft()->templates->findTemplate($template));
+			return IOHelper::getFileContents(craft()->templates->findTemplate($name));
 		}
 		else
 		{
-			return $template->template;
+			return $name->template;
 		}
 	}
 
 	/**
 	 * Gets the cache key to use for the cache for a given template.
 	 *
-	 * @param string $template The name of the template to load, or a StringTemplate object.
-	 * @return string          The cache key (the path to the template)
+	 * @param string $name The name of the template to load, or a StringTemplate object.
+	 * @return string      The cache key (the path to the template)
 	 */
-	public function getCacheKey($template)
+	public function getCacheKey($name)
 	{
-		if (is_string($template))
+		if (is_string($name))
 		{
-			return craft()->templates->findTemplate($template);
+			return craft()->templates->findTemplate($name);
 		}
 		else
 		{
-			return $template->cacheKey;
+			return $name->cacheKey;
 		}
 	}
 
 	/**
 	 * Returns whether the cached template is still up-to-date with the latest template.
 	 *
-	 * @param string    $template           The template name, or a StringTemplate object.
-	 * @param timestamp $cachedModifiedTime The last modification time of the cached template
+	 * @param string    $name The template name, or a StringTemplate object.
+	 * @param timestamp $time The last modification time of the cached template
 	 * @return bool
 	 */
-	public function isFresh($template, $cachedModifiedTime)
+	public function isFresh($name, $time)
 	{
 		// If this is a CP request and a DB update is needed, force a recompile.
 		if (craft()->request->isCpRequest() && craft()->updates->isCraftDbUpdateNeeded())
@@ -57,10 +57,10 @@ class TemplateLoader implements \Twig_LoaderInterface
 			return false;
 		}
 
-		if (is_string($template))
+		if (is_string($name))
 		{
-			$sourceModifiedTime = IOHelper::getLastTimeModified(craft()->templates->findTemplate($template));
-			return $sourceModifiedTime->getTimestamp() <= $cachedModifiedTime;
+			$sourceModifiedTime = IOHelper::getLastTimeModified(craft()->templates->findTemplate($name));
+			return $sourceModifiedTime->getTimestamp() <= $time;
 		}
 		else
 		{
