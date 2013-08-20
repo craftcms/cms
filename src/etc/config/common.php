d73152af77997c8a24d62843a79eeddfe340a147
Brad Bell <brad@pixelandtonic.com>
2013-08-20 13:30:38 -0700

    Don't need this anymore.

--- src/etc/config/common.php
+++ src/etc/config/common.php
@@ -2,7 +2,6 @@
 
 Yii::setPathOfAlias('app', CRAFT_APP_PATH);
 Yii::setPathOfAlias('plugins', CRAFT_PLUGINS_PATH);
-Yii::setPathOfAlias('Imagine', CRAFT_APP_PATH.'/lib/Imagine');
 
 // Load the deafult configs
 $generalConfig = require_once(CRAFT_APP_PATH.'etc/config/defaults/general.php');
