99dd6275fe333afa44982f2eb2d88d22bf4a36e1
Brandon Kelly <brandon@pixelandtonic.com>
2013-08-22 17:51:15 -0700

    000000

--- src/migrations/m130910_000000_drop_users_enctype.php
+++ src/migrations/m130910_000000_drop_users_enctype.php
@@ -4,7 +4,7 @@ namespace Craft;
 /**
  * The class name is the UTC timestamp in the format of mYYMMDD_HHMMSS_migrationName
  */
-class m130910_000002_drop_users_enctype extends BaseMigration
+class m130910_000000_drop_users_enctype extends BaseMigration
 {
 	/**
 	 * Any migration code in here is wrapped inside of a transaction.
