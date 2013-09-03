539aa4cb80fc6c8e7a2da8a7222bd7876310b384
Brandon Kelly <brandon@pixelandtonic.com>
2013-09-03 07:18:05 -0700

    Well these guys were having fun.

--- src/controllers/EntriesController.php
+++ src/controllers/EntriesController.php
@@ -69,7 +69,7 @@ class EntriesController extends BaseController
 				{
 					$criteria = craft()->elements->getCriteria(ElementType::Entry);
 					$criteria->id = $variables['entryId'];
-					$criteria->status = '*';
+					$criteria->status = null;
 
 					if (Craft::hasPackage(CraftPackage::Localize))
 					{
@@ -360,7 +360,7 @@ class EntriesController extends BaseController
 		{
 			$criteria = craft()->elements->getCriteria(ElementType::Entry);
 			$criteria->id = $entryId;
-			$criteria->status = '*';
+			$criteria->status = null;
 			$entry = $criteria->first();
 
 			if (!$entry)
