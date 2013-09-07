926ba72f46f4eaeeaa99e0288383bdc855d47145
Brandon Kelly <brandon@pixelandtonic.com>
2013-09-07 07:05:19 -0700

    Show a "Check again" button in the Updates widget if no updates are available

--- src/resources/js/UpdatesWidget.js
+++ src/resources/js/UpdatesWidget.js
@@ -3,26 +3,59 @@
 Craft.UpdatesWidget = Garnish.Base.extend({
 
 	$widget: null,
+	$body: null,
+	$btn: null,
+	checking: false,
 
-	init: function(widgetId)
+	init: function(widgetId, cached)
 	{
 		this.$widget = $('#widget'+widgetId);
+		this.$body = this.$widget.find('.body:first');
+		this.initBtn();
+
+		if (!cached)
+		{
+			this.checkForUpdates();
+		}
+	},
+
+	initBtn: function()
+	{
+		this.$btn = this.$body.find('.btn:first');
+		this.addListener(this.$btn, 'click', function() {
+			this.checkForUpdates(true);
+		});
+	},
+
+	checkForUpdates: function(forceRefresh)
+	{
+		if (this.checking)
+		{
+			return;
+		}
+
+		this.checking = true;
 		this.$widget.addClass('loading');
+		this.$btn.addClass('disabled');
 
-		Craft.postActionRequest('dashboard/checkForUpdates', $.proxy(function(response, textStatus) {
+		var data = {
+			forceRefresh: forceRefresh
+		};
 
+		Craft.postActionRequest('dashboard/checkForUpdates', data, $.proxy(function(response, textStatus) {
+
+			this.checking = false;
 			this.$widget.removeClass('loading');
 
 			if (textStatus == 'success')
 			{
-				var text = response;
+				this.$body.html(response);
+				this.initBtn();
 			}
 			else
 			{
-				var text = Craft.t('An unknown error occurred.');
+				this.$body.find('p:first').text('An unknown error occurred.');
 			}
-
-			this.$widget.find('.body').html(text);
 
 		}, this), {
 			complete: $.noop
