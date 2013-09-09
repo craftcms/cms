eeabc5b56db1d6177762ad64c2a80a72eed63b3c
Brad Bell <brad@pixelandtonic.com>
2013-09-08 21:26:52 -0700

    If a user gets deleted, set any entryversion->creator columns to null and cascade delete any entrydraft's they may have created.
    Fixes a FK constraint violation when deleting a user that has versions or drafts.

--- src/records/EntryVersionRecord.php
+++ src/records/EntryVersionRecord.php
@@ -37,7 +37,7 @@ class EntryVersionRecord extends BaseRecord
 		return array(
 			'entry'   => array(static::BELONGS_TO, 'EntryRecord', 'required' => true, 'onDelete' => static::CASCADE),
 			'section' => array(static::BELONGS_TO, 'SectionRecord', 'required' => true, 'onDelete' => static::CASCADE),
-			'creator' => array(static::BELONGS_TO, 'UserRecord', 'required' => true),
+			'creator' => array(static::BELONGS_TO, 'UserRecord', 'required' => false, 'onDelete' => static::SET_NULL),
 			'locale' => array(static::BELONGS_TO, 'LocaleRecord', 'locale', 'required' => true, 'onDelete' => static::CASCADE, 'onUpdate' => static::CASCADE),
 		);
 	}
