a3273ead739d632b829d8a206b9e0450045e5086
Brandon Kelly <brandon@pixelandtonic.com>
2013-09-10 05:59:12 -0700

    Tabs

--- src/fieldtypes/RichTextData.php
+++ src/fieldtypes/RichTextData.php
@@ -4,54 +4,54 @@ namespace Craft;
 /**
  * Stores the data for Rich Text fields.
  */
- class RichTextData extends \Twig_Markup
- {
- 	private $_pages;
+class RichTextData extends \Twig_Markup
+{
+	private $_pages;
 
- 	/**
- 	 * Returns an array of the individual page contents.
- 	 *
- 	 * @return array
- 	 */
- 	public function getPages()
- 	{
- 		if (!isset($this->_pages))
- 		{
- 			$this->_pages = array();
- 			$pages = explode('<!--pagebreak-->', $this->content);
+	/**
+	 * Returns an array of the individual page contents.
+	 *
+	 * @return array
+	 */
+	public function getPages()
+	{
+		if (!isset($this->_pages))
+		{
+			$this->_pages = array();
+			$pages = explode('<!--pagebreak-->', $this->content);
 
- 			foreach ($pages as $page)
- 			{
- 				$this->_pages[] = new \Twig_Markup($page, $this->charset);
- 			}
- 		}
+			foreach ($pages as $page)
+			{
+				$this->_pages[] = new \Twig_Markup($page, $this->charset);
+			}
+		}
 
- 		return $this->_pages;
- 	}
+		return $this->_pages;
+	}
 
- 	/**
- 	 * Returns a specific page.
- 	 *
- 	 * @param int $pageNumber
- 	 * @return string|null
- 	 */
- 	public function getPage($pageNumber)
- 	{
- 		$pages = $this->getPages();
+	/**
+	 * Returns a specific page.
+	 *
+	 * @param int $pageNumber
+	 * @return string|null
+	 */
+	public function getPage($pageNumber)
+	{
+		$pages = $this->getPages();
 
- 		if (isset($pages[$pageNumber - 1]))
- 		{
- 			return $pages[$pageNumber - 1];
- 		}
- 	}
+		if (isset($pages[$pageNumber - 1]))
+		{
+			return $pages[$pageNumber - 1];
+		}
+	}
 
- 	/**
- 	 * Returns the total number of pages.
- 	 *
- 	 * @return int
- 	 */
- 	public function getTotalPages()
- 	{
- 		return count($this->getPages());
- 	}
- }
+	/**
+	 * Returns the total number of pages.
+	 *
+	 * @return int
+	 */
+	public function getTotalPages()
+	{
+		return count($this->getPages());
+	}
+}
