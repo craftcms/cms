c2bb95a62b5a5b89942f58e346e32f0ed48201be
Brandon Kelly <brandon@pixelandtonic.com>
2013-10-09 17:48:50 -0700

    Lightswitch fields

---
+++ src/fieldtypes/LightswitchFieldType.php
@@ -0,0 +1,43 @@
+<?php
+namespace Craft;
+
+/**
+ *
+ */
+class LightswitchFieldType extends BaseFieldType
+{
+	/**
+	 * Returns the type of field this is.
+	 *
+	 * @return string
+	 */
+	public function getName()
+	{
+		return Craft::t('Lightswitch');
+	}
+
+	/**
+	 * Returns the content attribute config.
+	 *
+	 * @return mixed
+	 */
+	public function defineContentAttribute()
+	{
+		return AttributeType::Bool;
+	}
+
+	/**
+	 * Returns the field's input HTML.
+	 *
+	 * @param string $name
+	 * @param mixed  $value
+	 * @return string
+	 */
+	public function getInputHtml($name, $value)
+	{
+		return craft()->templates->render('_includes/forms/Lightswitch', array(
+			'name'  => $name,
+			'on'    => (bool) $value,
+		));
+	}
+}
