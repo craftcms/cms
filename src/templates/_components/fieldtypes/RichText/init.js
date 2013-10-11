9185653d87e895b97a07bf9b0f7ab7cd016332e9
Brandon Kelly <brandon@pixelandtonic.com>
2013-10-11 05:21:33 -0700

    The selected source is now remembered in the "Choose image", "Link to an entry", and "Link to an asset" rich text field modals

--- src/templates/_components/fieldtypes/RichText/init.js
+++ src/templates/_components/fieldtypes/RichText/init.js
@@ -30,6 +30,7 @@ config.buttonsCustom.image = {
 				if (typeof this.assetSelectionModal == 'undefined')
 				{
 					this.assetSelectionModal = Craft.createElementSelectorModal('Asset', {
+						storageKey: 'RichTextFieldType.ChooseImage',
 						multiSelect: true,
 						criteria: { kind: 'image' },
 						onSelect: $.proxy(function(assets) {
@@ -74,6 +75,7 @@ config.buttonsCustom.link = {
 				if (typeof this.entrySelectionModal == 'undefined')
 				{
 					this.entrySelectionModal = Craft.createElementSelectorModal('Entry', {
+						storageKey: 'RichTextFieldType.LinkToEntry',
 						sources: {{sections|raw}},
 						onSelect: function(entries) {
 							if (entries.length)
@@ -109,6 +111,7 @@ config.buttonsCustom.link = {
 				if (typeof this.assetLinkSelectionModal == 'undefined')
 				{
 					this.assetLinkSelectionModal = Craft.createElementSelectorModal('Asset', {
+						storageKey: 'RichTextFieldType.LinkToAsset',
 						onSelect: function(assets) {
 							if (assets.length)
 							{
