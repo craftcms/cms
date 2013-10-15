e151ff7046d3077255624e716128c4fcf779b9bd
Brandon Kelly <brandon@pixelandtonic.com>
2013-10-15 11:10:35 -0700

    BaseOptionsFieldType no longer needs to pass 'jsId' and 'jsName'

--- src/fieldtypes/BaseOptionsFieldType.php
+++ src/fieldtypes/BaseOptionsFieldType.php
@@ -62,8 +62,6 @@ abstract class BaseOptionsFieldType extends BaseFieldType
 				'instructions' => Craft::t('Define the available options.'),
 				'id'           => 'options',
 				'name'         => 'options',
-				'jsId'         => 'types-'.$class.'-options',
-				'jsName'       => 'types['.$class.'][options]',
 				'addRowLabel'  => Craft::t('Add an option'),
 				'cols'         => array(
 					'label' => array(
