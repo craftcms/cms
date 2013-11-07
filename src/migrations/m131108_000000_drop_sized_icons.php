0e90291b3ed4593cab62b17f50e35d75adad27b2
Brandon Kelly <brandon@pixelandtonic.com>
2013-11-07 15:11:39 -0800

    Moved the new migration dates to tomorrow, just to be sure they're after the last release date

--- src/migrations/m131108_000000_drop_sized_icons.php
+++ src/migrations/m131108_000000_drop_sized_icons.php
@@ -4,7 +4,7 @@ namespace Craft;
 /**
  * The class name is the UTC timestamp in the format of mYYMMDD_HHMMSS_migrationName
  */
-class m131107_120000_drop_sized_icons extends BaseMigration
+class m131108_000000_drop_sized_icons extends BaseMigration
 {
 	/**
 	 * Any migration code in here is wrapped inside of a transaction.
