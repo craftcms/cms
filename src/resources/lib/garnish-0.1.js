ec0192b9e4715c6d15315331c8f7ae6109450fd4
Brandon Kelly <brandon@pixelandtonic.com>
2013-11-13 09:47:06 -0800

    Menus are now at least as wide as their buttons

--- src/resources/lib/garnish-0.1.js
+++ src/resources/lib/garnish-0.1.js
@@ -2690,10 +2690,6 @@ Garnish.Menu = Garnish.Base.extend({
 
 	setPositionRelativeToButton: function()
 	{
-		var css = {
-			minWidth: (btnWidth - 32)
-		};
-
 		var windowHeight = Garnish.$win.height(),
 			windowScrollTop = Garnish.$win.scrollTop(),
 
@@ -2707,6 +2703,10 @@ Garnish.Menu = Garnish.Base.extend({
 
 			bottomClearance = windowHeight + windowScrollTop - btnOffsetBottom,
 			topClearance = btnOffsetTop - windowScrollTop;
+
+		var css = {
+			minWidth: btnWidth - (this.$container.outerWidth() - this.$container.width())
+		};
 
 		// Is there room for the menu below the button?
 		if (bottomClearance >= menuHeight || bottomClearance >= topClearance)
@@ -2722,7 +2722,7 @@ Garnish.Menu = Garnish.Base.extend({
 		{
 			case 'right':
 			{
-				css.right = 1 + Garnish.$win.width() - (btnOffset.left + btnWidth);
+				css.right = Garnish.$win.width() - (btnOffset.left + btnWidth);
 				break;
 			}
 			case 'center':
@@ -2732,7 +2732,7 @@ Garnish.Menu = Garnish.Base.extend({
 			}
 			default:
 			{
-				css.left = 1 + btnOffset.left;
+				css.left = btnOffset.left;
 			}
 		}
 
