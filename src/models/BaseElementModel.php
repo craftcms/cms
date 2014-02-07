2dc545ff31660593c5a40c00df6cc9c4af4eb72e
Brandon Kelly <brandon@pixelandtonic.com>
2014-02-07 12:09:59 -0800

    BaseElementModel::getUrl() will now factor in the locale-specific site URL, if it's set

--- src/models/BaseElementModel.php
+++ src/models/BaseElementModel.php
@@ -149,14 +149,33 @@ abstract class BaseElementModel extends BaseModel
 	{
 		if ($this->uri !== null)
 		{
+			$useLocaleSiteUrl = (
+				($this->locale != craft()->language) &&
+				($localeSiteUrl = craft()->config->getLocalized('siteUrl', $this->locale))
+			);
+
+			if ($useLocaleSiteUrl)
+			{
+				// Temporarily set Craft to use this element's locale's site URL
+				$siteUrl = craft()->getSiteUrl();
+				craft()->setSiteUrl($localeSiteUrl);
+			}
+
 			if ($this->uri == '__home__')
 			{
-				return UrlHelper::getSiteUrl();
+				$url = UrlHelper::getSiteUrl();
 			}
 			else
 			{
-				return UrlHelper::getSiteUrl($this->uri);
+				$url = UrlHelper::getSiteUrl($this->uri);
 			}
+
+			if ($useLocaleSiteUrl)
+			{
+				craft()->setSiteUrl($siteUrl);
+			}
+
+			return $url;
 		}
 	}
 
