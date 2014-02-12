156db462113f1d84b30194c05f793a06895e2ca7
Brandon Kelly <brandon@pixelandtonic.com>
2014-02-12 06:51:26 -0800

    Redesigned the Edit User page
    - the Permissions tab is now visible for users with the assignUserPermissions permission
    - no more Admin tab; its contents have been moved to two new panes
    - all users with access to the page can see the user info pane, and resend verification emails
    - users with the deleteUsers permission, but not administrateUsers, are now able to actually delete users

--- src/controllers/UsersController.php
+++ src/controllers/UsersController.php
@@ -445,22 +445,13 @@ class UsersController extends BaseController
 				),
 			);
 
-			// If they can administrate users, show the admin tab.
-			if (craft()->userSession->getUser()->can('administrateUsers'))
+			// If they can assign user groups and permissions, show the Permissions tab
+			if (craft()->userSession->getUser()->can('assignUserPermissions'))
 			{
 				$variables['tabs']['perms'] = array(
 					'label' => Craft::t('Permissions'),
 					'url'   => '#perms',
 				);
-
-				if ($variables['account']->id)
-				{
-					$variables['tabs']['admin'] = array(
-						'label' => Craft::t('Admin'),
-						'url'   => '#admin',
-					);
-				}
-
 			}
 		}
 
@@ -864,7 +855,7 @@ class UsersController extends BaseController
 	public function actionSendActivationEmail()
 	{
 		$this->requirePostRequest();
-		craft()->userSession->requirePermission('administrateUsers');
+		craft()->userSession->requirePermission('editUsers');
 
 		$userId = craft()->request->getRequiredPost('userId');
 		$user = craft()->users->getUserById($userId);
