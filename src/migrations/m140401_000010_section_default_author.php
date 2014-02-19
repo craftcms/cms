c055b7af28530a80db8b1d03774fdc7048b7ea91
Brandon Kelly <brandon@pixelandtonic.com>
2014-02-18 21:07:42 -0800

    Missed one

--- src/migrations/m140401_000010_section_default_author.php
+++ src/migrations/m140401_000010_section_default_author.php
@@ -4,7 +4,7 @@ namespace Craft;
 /**
  * The class name is the UTC timestamp in the format of mYYMMDD_HHMMSS_migrationName
  */
-class m140204_000010_section_default_author extends BaseMigration
+class m140401_000010_section_default_author extends BaseMigration
 {
 	/**
 	 * Any migration code in here is wrapped inside of a transaction.
