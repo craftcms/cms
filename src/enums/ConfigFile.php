1ec0675263e42a18b16484a2501829af0b1c5bce
Brad Bell <brad@pixelandtonic.com>
2014-02-21 21:38:45 -0800

    Add ConfigFile enum

---
+++ src/enums/ConfigFile.php
@@ -0,0 +1,15 @@
+<?php
+namespace Craft;
+
+/**
+ *
+ */
+class ConfigFile
+{
+	const FileCache    = 'filecache';
+	const General      = 'general';
+	const Db           = 'db';
+	const DbCache      = 'dbcache';
+	const Memcache     = 'memcache';
+	const RedisCache   = 'rediscache';
+}
