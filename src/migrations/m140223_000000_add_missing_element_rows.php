95a67ea524a6eb7ffc31266a0172e8e5531e6be4
Brandon Kelly <brandon@pixelandtonic.com>
2014-02-23 07:39:03 -0800

    Re-running the add_missing_element_rows migration, but this time prune out any rows in Matrix content tables that don't map back to a Matrix block

--- src/migrations/m140223_000000_add_missing_element_rows.php
+++ src/migrations/m140223_000000_add_missing_element_rows.php
@@ -4,7 +4,7 @@ namespace Craft;
 /**
  * The class name is the UTC timestamp in the format of mYYMMDD_HHMMSS_migrationName
  */
-class m140126_000000_add_missing_content_rows extends BaseMigration
+class m140223_000000_add_missing_element_rows extends BaseMigration
 {
 	/**
 	 * Any migration code in here is wrapped inside of a transaction.
@@ -20,6 +20,37 @@ class m140126_000000_add_missing_content_rows extends BaseMigration
 		// Now make sure that neither of those tables have data for a locale that the other one doesn't
 		$this->_addMissingLocaleRows('content', 'elements_i18n');
 		$this->_addMissingLocaleRows('elements_i18n', 'content');
+
+		// Prune out any matrixcontent rows that aren't for an actual Matrix block
+		$matrixHandles = craft()->db->createCommand()
+			->select('handle')
+			->from('fields')
+			->where('type = "Matrix"')
+			->queryColumn();
+
+		if ($matrixHandles)
+		{
+			$blockIds = craft()->db->createCommand()
+				->select('id')
+				->from('elements')
+				->where('type = "MatrixBlock"')
+				->queryColumn();
+
+			if ($blockIds)
+			{
+				$condition = array('not in', 'elementId', $blockIds);
+			}
+			else
+			{
+				$condition = '';
+			}
+
+			foreach ($matrixHandles as $handle)
+			{
+				$table = 'matrixcontent_'.strtolower($handle);
+				$this->delete($table, $condition);
+			}
+		}
 
 		return true;
 	}
