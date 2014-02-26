88059529191aacdc7f2c548b1914d5703d9c2244
Brad Bell <brad@pixelandtonic.com>
2014-02-25 19:08:29 -0800

    Important.

--- src/etc/cache/ApcCache.php
+++ src/etc/cache/ApcCache.php
@@ -2,7 +2,10 @@
 namespace Craft;
 
 /**
+ * ApcCache provides APC caching in terms of an application component.
  *
+ * The caching is based on {@link http://www.php.net/apc APC}.
+ * To use this application component, the APC PHP extension must be loaded.
  */
 class ApcCache extends \CApcCache
 {
