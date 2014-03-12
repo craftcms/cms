5f6a19fa4043c98bbe0f7b676745530d9f0661d5
Brandon Kelly <brandon@pixelandtonic.com>
2014-03-11 17:05:55 -0700

    Changed the timestamp for the allow_temp_source_transforms migration, and stop worrying about avoiding a single ALTER TABLE query

--- src/migrations/m140401_000013_allow_temp_source_transforms.php
+++ src/migrations/m140401_000013_allow_temp_source_transforms.php
@@ -4,7 +4,7 @@ namespace Craft;
 /**
  * The class name is the UTC timestamp in the format of mYYMMDD_HHMMSS_migrationName
  */
-class m140401_100013_allow_temp_source_transforms extends BaseMigration
+class m140401_000013_allow_temp_source_transforms extends BaseMigration
 {
 	/**
 	 * Any migration code in here is wrapped inside of a transaction.
@@ -13,12 +13,6 @@ class m140401_100013_allow_temp_source_transforms extends BaseMigration
 	 */
 	public function safeUp()
 	{
-		// No need to torture the Dev track guys again
-		if (craft()->migrations->hasRun('m140401_100013_allow_temp_source_transforms'))
-		{
-			return true;
-		}
-
 		$this->alterColumn('assettransformindex', 'sourceId', array('column' => ColumnType::Int, 'required' => false));
 
 		return true;
