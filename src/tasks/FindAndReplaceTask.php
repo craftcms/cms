b438e098b441fe8c5f7aeaafdb2c626cf6f99f4b
Brandon Kelly <brandon@pixelandtonic.com>
2014-03-18 12:48:08 -0700

    Added a Find and Replace tool + task

---
+++ src/tasks/FindAndReplaceTask.php
@@ -0,0 +1,170 @@
+<?php
+namespace Craft;
+
+/**
+ * Find and Replace task
+ */
+class FindAndReplaceTask extends BaseTask
+{
+	private $_table;
+	private $_textColumns;
+	private $_matrixFieldIds;
+
+	/**
+	 * Defines the settings.
+	 *
+	 * @access protected
+	 * @return array
+	 */
+	protected function defineSettings()
+	{
+		return array(
+			'find'          => AttributeType::String,
+			'replace'       => AttributeType::String,
+			'matrixFieldId' => AttributeType::String,
+		);
+	}
+
+	/**
+	 * Returns the default description for this task.
+	 *
+	 * @return string
+	 */
+	public function getDescription()
+	{
+		$settings = $this->getSettings();
+
+		return Craft::t('Replacing “{find}” with “{replace}”', array(
+			'find'    => $settings->find,
+			'replace' => $settings->replace
+		));
+	}
+
+	/**
+	 * Gets the total number of steps for this task.
+	 *
+	 * @return int
+	 */
+	public function getTotalSteps()
+	{
+		$this->_textColumns = array();
+		$this->_matrixFieldIds = array();
+
+		// Is this for a Matrix field?
+		$matrixFieldId = $this->getSettings()->matrixFieldId;
+
+		if ($matrixFieldId)
+		{
+			$matrixField = craft()->fields->getFieldById($matrixFieldId);
+
+			if (!$matrixField || $matrixField->type != 'Matrix')
+			{
+				return 0;
+			}
+
+			$this->_table = craft()->matrix->getContentTableName($matrixField);
+
+			$blockTypes = craft()->matrix->getBlockTypesByFieldId($matrixFieldId);
+
+			foreach ($blockTypes as $blockType)
+			{
+				$fieldColumnPrefix = 'field_'.$blockType->handle.'_';
+
+				foreach ($blockType->getFields() as $field)
+				{
+					$this->_checkField($field, $fieldColumnPrefix);
+				}
+			}
+		}
+		else
+		{
+			$this->_table = 'content';
+
+			foreach (craft()->fields->getAllFields() as $field)
+			{
+				$this->_checkField($field, 'field_');
+			}
+		}
+
+		return count($this->_textColumns) + count($this->_matrixFieldIds);
+	}
+
+	/**
+	 * Runs a task step.
+	 *
+	 * @param int $step
+	 * @return bool
+	 */
+	public function runStep($step)
+	{
+		$settings = $this->getSettings();
+
+		if (isset($this->_textColumns[$step]))
+		{
+			craft()->db->createCommand()->replace($this->_table, $this->_textColumns[$step], $settings->find, $settings->replace);
+			return true;
+		}
+		else
+		{
+			$step -= count($this->_textColumns);
+
+			if (isset($this->_matrixFieldIds[$step]))
+			{
+				$field = craft()->fields->getFieldById($this->_matrixFieldIds[$step]);
+
+				if ($field)
+				{
+					return $this->runSubTask('FindAndReplace', Craft::t('Working in Matrix field “{field}”', array('field' => $field->name)), array(
+						'find'          => $settings->find,
+						'replace'       => $settings->replace,
+						'matrixFieldId' => $field->id
+					));
+				}
+				else
+				{
+					// Oh what the hell.
+					return true;
+				}
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+
+	/**
+	 * Checks whether the given field is saving data into a textual column, and saves it accordingly
+	 *
+	 * @access private
+	 * @param FieldModel $field
+	 * @param string     $fieldColumnPrefix
+	 * @return bool
+	 */
+	private function _checkField(FieldModel $field, $fieldColumnPrefix)
+	{
+		if ($field->type == 'Matrix')
+		{
+			$this->_matrixFieldIds[] = $field->id;
+		}
+		else
+		{
+			$fieldType = $field->getFieldType();
+
+			if ($fieldType)
+			{
+				$attributeConfig = $fieldType->defineContentAttribute();
+
+				if ($attributeConfig && $attributeConfig != AttributeType::Number)
+				{
+					$attributeConfig = ModelHelper::normalizeAttributeConfig($attributeConfig);
+
+					if ($attributeConfig['type'] == AttributeType::String)
+					{
+						$this->_textColumns[] = $fieldColumnPrefix.$field->handle;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ src/tools/FindAndReplaceTool.php
@@ -0,0 +1,66 @@
+<?php
+namespace Craft;
+
+/**
+ * Find and Replace tool
+ */
+class FindAndReplaceTool extends BaseTool
+{
+	/**
+	 * Returns the tool name.
+	 *
+	 * @return string
+	 */
+	public function getName()
+	{
+		return Craft::t('Find and Replace');
+	}
+
+	/**
+	 * Returns the tool's icon value.
+	 *
+	 * @return string
+	 */
+	public function getIconValue()
+	{
+		return 'wand';
+	}
+
+	/**
+	 * Returns the tool's options HTML.
+	 *
+	 * @return string
+	 */
+	public function getOptionsHtml()
+	{
+		return craft()->templates->renderMacro('_includes/forms', 'textField', array(
+			array(
+				'name'        => 'find',
+				'placeholder' => Craft::t('Find'),
+			)
+		)) .
+		craft()->templates->renderMacro('_includes/forms', 'textField', array(
+			array(
+				'name'        => 'replace',
+				'placeholder' => Craft::t('Replace'),
+			)
+		));
+	}
+
+	/**
+	 * Performs the tool's action.
+	 *
+	 * @param array $params
+	 * @return array
+	 */
+	public function performAction($params = array())
+	{
+		if (!empty($params['find']) && !empty($params['replace']))
+		{
+			craft()->tasks->createTask('FindAndReplace', null, array(
+				'find'    => $params['find'],
+				'replace' => $params['replace']
+			));
+		}
+	}
+}
