4c1e1f794c5f890975e4b299b380393cdc40e5e8
Brandon Kelly <brandon@pixelandtonic.com>
2014-03-24 14:56:06 -0700

    Moved the Requirement and PhpVersionRequirement classes into Requirements.php

--- src/etc/requirements/Requirements.php
+++ src/etc/requirements/Requirements.php
@@ -1,8 +1,10 @@
 <?php
-
 namespace Craft;
 
 
+/**
+ * Requirements class
+ */
 class Requirements
 {
 	public static function getRequirements()
@@ -180,5 +182,200 @@ class Requirements
 		}
 
 		return false;
+	}
+}
+
+/**
+ * Requirement class
+ */
+class Requirement extends \CComponent
+{
+	private $_name;
+	private $_condition;
+	private $_requiredBy;
+	private $_notes;
+	private $_required;
+	private $_result;
+
+	/**
+	 * Constructor
+	 *
+	 * @param string|null $name
+	 * @param bool|null   $condition
+	 * @param bool|null   $required
+	 * @param string|null $requiredBy
+	 * @param string|null $notes
+	 */
+	function __construct($name = null, $condition = null, $required = true, $requiredBy = null, $notes = null)
+	{
+		$this->_name = $name;
+		$this->_condition = $condition;
+		$this->_required = $required;
+		$this->_requiredBy = $requiredBy;
+		$this->_notes = $notes;
+	}
+
+	/**
+	 * Calculates the result of this requirement.
+	 *
+	 * @access protected
+	 * @return string
+	 */
+	protected function calculateResult()
+	{
+		if ($this->_condition)
+		{
+			return RequirementResult::Success;
+		}
+		else if ($this->_required)
+		{
+			return RequirementResult::Failed;
+		}
+		else
+		{
+			return RequirementResult::Warning;
+		}
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getName()
+	{
+		return $this->_name;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getResult()
+	{
+		if (!isset($this->_result))
+		{
+			$this->_result = $this->calculateResult();
+		}
+
+		return $this->_result;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function getRequired()
+	{
+		return $this->_required;
+	}
+
+	/**
+	 * @return null
+	 */
+	public function getRequiredBy()
+	{
+		return $this->_requiredBy;
+	}
+
+	/**
+	 * @return null
+	 */
+	public function getNotes()
+	{
+		return $this->_notes;
+	}
+}
+
+/**
+ * PHP version requirement class
+ */
+class PhpVersionRequirement extends Requirement
+{
+	const REQUIRED_PHP_VERSION = '@@@requiredPHPVersion@@@';
+
+	/**
+	 * @param      $name
+	 * @param      $condition
+	 * @param bool $required
+	 * @param null $requiredBy
+	 * @param null $notes
+	 */
+	function __construct()
+	{
+		parent::__construct(
+			Craft::t('PHP Version'),
+			null,
+			true,
+			'<a href="http://buildwithcraft.com">@@@appName@@@</a>'
+		);
+	}
+
+	/**
+	 * @return null
+	 */
+	public function getNotes()
+	{
+		if ($this->_isBadPhpVersion())
+		{
+			return Craft::t('PHP {version} has a known <a href="{url}">security vulnerability</a>. You should probably upgrade.', array(
+				'version' => PHP_VERSION,
+				'url'     => 'http://arstechnica.com/security/2014/03/php-bug-allowing-site-hijacking-still-menaces-internet-22-months-on'
+			));
+		}
+		else
+		{
+			return Craft::t('PHP {version} or higher is required.', array(
+				'version' => static::REQUIRED_PHP_VERSION,
+			));
+		}
+	}
+
+	/**
+	 * Calculates the result of this requirement.
+	 *
+	 * @access protected
+	 * @return string
+	 */
+	protected function calculateResult()
+	{
+		if ($this->_doesMinVersionPass())
+		{
+			// If it's 5.3 < 5.3.12, or 5.4 < 5.4.2, still issue a warning, due to the PHP hijack bug:
+			// http://arstechnica.com/security/2014/03/php-bug-allowing-site-hijacking-still-menaces-internet-22-months-on/
+			if ($this->_isBadPhpVersion())
+			{
+				return RequirementResult::Warning;
+			}
+			else
+			{
+				return RequirementResult::Success;
+			}
+		}
+		else
+		{
+			return RequirementResult::Failed;
+		}
+	}
+
+	/**
+	 * Returns whether this is past the min PHP version.
+	 *
+	 * @access private
+	 * @return bool
+	 */
+	private function _doesMinVersionPass()
+	{
+		return version_compare(PHP_VERSION, static::REQUIRED_PHP_VERSION, '>=');
+	}
+
+	/**
+	 * Returns whether this is one of the bad PHP versions.
+	 *
+	 * @access private
+	 * @return bool
+	 */
+	private function _isBadPhpVersion()
+	{
+		return (
+			(version_compare(PHP_VERSION, '5.3', '>=') && version_compare(PHP_VERSION, '5.3.12', '<')) ||
+			(version_compare(PHP_VERSION, '5.4', '>=') && version_compare(PHP_VERSION, '5.4.2', '<'))
+		);
 	}
 }
