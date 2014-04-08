2f3bbf21501aab154641a8943319ac3584db931a
Brad Bell <brad@pixelandtonic.com>
2014-04-08 15:09:15 -0700

    Fix a bug where the Recent Entries widget wouldn't show any entries if you're running Craft Personal Edition.

--- src/widgets/RecentEntriesWidget.php
+++ src/widgets/RecentEntriesWidget.php
@@ -124,7 +124,7 @@ class RecentEntriesWidget extends BaseWidget
 		}
 
 		// If they don't have publish pro, OR they have publish pro and have permission to edit sections in it.
-		if ((craft()->getEdition() == Craft::Client || (craft()->getEdition() >= Craft::Client && $somethingToDisplay)) && count($sectionIds) > 0)
+		if ((craft()->getEdition() == Craft::Personal || (craft()->getEdition() >= Craft::Client && $somethingToDisplay)) && count($sectionIds) > 0)
 		{
 			$criteria = $this->_getCriteria($sectionIds);
 			$entries = $criteria->find();
