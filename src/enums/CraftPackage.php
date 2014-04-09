6176f6b1ab83c4b4a72d4d67bd74b3be7d6dcc3d
Brad Bell <brad@pixelandtonic.com>
2014-04-08 22:41:43 -0700

    Revert "No longer needed."
    This reverts commit 942b7d89a1f68d770f94d8751057eecdbea154d5.

---
+++ src/enums/CraftPackage.php
@@ -0,0 +1,14 @@
+<?php
+namespace Craft;
+
+/**
+ *
+ */
+abstract class CraftPackage extends BaseEnum
+{
+	const PublishPro = 'PublishPro';
+	const Users      = 'Users';
+	const Localize   = 'Localize';
+	const Cloud      = 'Cloud';
+	const Rebrand    = 'Rebrand';
+}
