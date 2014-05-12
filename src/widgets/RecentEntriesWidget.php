0a4a429cba2ca40dece6b0417635abbc75ee7cc6
Brandon Kelly <brandon@pixelandtonic.com>
2014-05-12 12:50:22 -0700

    Refactored the Recent Entries widget, and fixed a bug where it would show entries in the current locale even if you didn't have permission to edit it.

--- src/widgets/RecentEntriesWidget.php
+++ src/widgets/RecentEntriesWidget.php
@@ -26,14 +26,10 @@ class RecentEntriesWidget extends BaseWidget
 	 */
 	protected function defineSettings()
 	{
-		if (craft()->getEdition() >= Craft::Client)
-		{
-			$settings['section'] = array(AttributeType::Mixed, 'default' => '*');
-		}
-
-		$settings['limit'] = array(AttributeType::Number, 'default' => 10);
-
-		return $settings;
+		return array(
+			'section' => array(AttributeType::Mixed, 'default' => '*'),
+			'limit'   => array(AttributeType::Number, 'default' => 10),
+		);
 	}
 
 	/**
@@ -106,41 +102,60 @@ class RecentEntriesWidget extends BaseWidget
 	}
 
 	/**
+	 * Returns the recent entries, based on the widget settings and user permissions.
 	 *
+	 * @access private
+	 * @return array
 	 */
 	private function _getEntries()
 	{
-		$sectionIds = $this->_getSectionIds();
+		// Make sure that the user is actually allowed to edit entries in the current locale.
+		// Otherwise grab entries in their first editable locale.
+		$editableLocaleIds = craft()->i18n->getEditableLocaleIds();
+		$targetLocale = craft()->language;
 
-		$somethingToDisplay = false;
-
-		// If they have Client or Pro installed, only display the sections they are allowed to edit.
-		if (craft()->getEdition() >= Craft::Client)
+		if (!$editableLocaleIds)
 		{
-			if ($this->getSettings()->section == '*' || in_array($this->getSettings()->section, $sectionIds))
-			{
-				$somethingToDisplay = true;
-			}
+			return array();
 		}
 
-		// If they don't have Client or Pro, OR they have Client/Pro and have permission to edit sections in it.
-		if ((craft()->getEdition() == Craft::Personal || (craft()->getEdition() >= Craft::Client && $somethingToDisplay)) && count($sectionIds) > 0)
+		if (!in_array($targetLocale, $editableLocaleIds))
 		{
-			$criteria = $this->_getCriteria($sectionIds);
-			$entries = $criteria->find();
-		}
-		else
-		{
-			$entries = array();
+			$targetLocale = $editableLocaleIds[0];
 		}
 
-		return $entries;
+		// Normalize the target section ID value.
+		$editableSectionIds = $this->_getEditableSectionIds();
+		$targetSectionId = $this->getSettings()->section;
+
+		if (!$targetSectionId || $targetSectionId == '*' || !in_array($targetSectionId, $editableSectionIds))
+		{
+			$targetSectionId = array_merge($editableSectionIds);
+		}
+
+		if (!$targetSectionId)
+		{
+			return array();
+		}
+
+		$criteria = craft()->elements->getCriteria(ElementType::Entry);
+		$criteria->status = null;
+		$criteria->localeEnabled = null;
+		$criteria->locale = $targetLocale;
+		$criteria->sectionId = $targetSectionId;
+		$criteria->limit = $this->getSettings()->limit;
+		$criteria->order = 'dateCreated desc';
+
+		return $criteria->find();
 	}
 
 	/**
+	 * Returns the Channel and Structure section IDs that the user is allowed to edit.
+	 *
+	 * @access private
 	 * @return array
 	 */
-	private function _getSectionIds()
+	private function _getEditableSectionIds()
 	{
 		$sectionIds = array();
 
@@ -153,38 +168,5 @@ class RecentEntriesWidget extends BaseWidget
 		}
 
 		return $sectionIds;
-	}
-
-	/**
-	 * @param $sectionIds
-	 * @throws Exception
-	 * @return ElementCriteriaModel
-	 */
-	private function _getCriteria($sectionIds)
-	{
-		$criteria = craft()->elements->getCriteria(ElementType::Entry);
-		$criteria->status = null;
-		$criteria->localeEnabled = null;
-		$criteria->limit = $this->getSettings()->limit;
-		$criteria->order = 'dateCreated DESC';
-
-		// Section is only defined if Client/Pro is installed.
-		if (craft()->getEdition() >= Craft::Client)
-		{
-			if ($this->getSettings()->section == '*')
-			{
-				$criteria->sectionId = $sectionIds;
-			}
-			else
-			{
-				$criteria->sectionId = $this->getSettings()->section;
-			}
-		}
-		else
-		{
-			$criteria->sectionId = $sectionIds;
-		}
-
-		return $criteria;
 	}
 }
