2b8ba9304d6f45caa1b3eb01c4cfcca07d1190ce
Brandon Kelly <brandon@pixelandtonic.com>
2014-05-22 17:28:05 -0700

    Timestamp shuffle

--- src/migrations/m140603_000001_draft_notes.php
+++ src/migrations/m140603_000001_draft_notes.php
@@ -4,7 +4,7 @@ namespace Craft;
 /**
  * The class name is the UTC timestamp in the format of mYYMMDD_HHMMSS_migrationName
  */
-class m140603_000003_draft_notes extends BaseMigration
+class m140603_000001_draft_notes extends BaseMigration
 {
 	/**
 	 * Any migration code in here is wrapped inside of a transaction.
--- src/migrations/m140603_000002_version_nums.php
+++ src/migrations/m140603_000002_version_nums.php
@@ -4,7 +4,7 @@ namespace Craft;
 /**
  * The class name is the UTC timestamp in the format of mYYMMDD_HHMMSS_migrationName
  */
-class m140603_000001_version_nums extends BaseMigration
+class m140603_000002_version_nums extends BaseMigration
 {
 	/**
 	 * Any migration code in here is wrapped inside of a transaction.
