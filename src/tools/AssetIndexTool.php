0aa6db1a67eca9ab63bc097b850976fb0e799d33
Andris Sevcenko <asevcenko@gmail.com>
2014-05-29 11:41:54 +0300

    Clean up stale indexing session data upon finishing indexing.

--- src/tools/AssetIndexTool.php
+++ src/tools/AssetIndexTool.php
@@ -158,6 +158,16 @@ class AssetIndexTool extends BaseTool
 						$responseArray['confirm'] = craft()->templates->render('assets/_missing_items', array('missingFiles' => $missingFiles, 'missingFolders' => $missingFolders));
 						$responseArray['params'] = array('finish' => 1);
 					}
+					// Clean up stale indexing data (all sessions that have all recordIds set)
+					$sessionsInProgress = craft()->db->createCommand()
+											->select('sessionId')
+											->from('assetindexdata')
+											->where('recordId IS NULL')
+											->group('sessionId')
+											->queryScalar();
+
+					craft()->db->createCommand()->delete('assetindexdata', array('not in', 'sessionId', $sessionsInProgress));
+
 
 					// Generate the HTML for missing files and folders
 					return array(
