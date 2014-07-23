9199f89658303d486e28ee2200c8b60583c92ccb
Brandon Kelly <brandon@pixelandtonic.com>
2014-07-23 06:35:24 -0700

    Edit Entry <title> tag was getting double-encoded

--- src/controllers/EntriesController.php
+++ src/controllers/EntriesController.php
@@ -208,6 +208,7 @@ class EntriesController extends BaseEntriesController
 		}
 		else
 		{
+			$variables['docTitle'] = Craft::t($variables['entry']->title);
 			$variables['title'] = HtmlHelper::encode(Craft::t($variables['entry']->title));
 
 			if (craft()->getEdition() >= Craft::Client && $variables['entry']->getClassHandle() != 'Entry')
