463b653ca13ab969f6c4c4488ff089f369b7d107
Brad Bell <brad@pixelandtonic.com>
2014-07-29 15:04:44 -0700

    Since 1.2

--- src/etc/templating/twigextensions/Nav_TokenParser.php
+++ src/etc/templating/twigextensions/Nav_TokenParser.php
@@ -9,7 +9,7 @@ namespace Craft;
  * @license   http://buildwithcraft.com/license Craft License Agreement
  * @link      http://buildwithcraft.com
  * @package   craft.app.etc.templating.twigextensions
- * @since     1.0
+ * @since     1.2
  */
 class Nav_TokenParser extends \Twig_TokenParser
 {
--- src/extensions/NestedSetBehavior.php
+++ src/extensions/NestedSetBehavior.php
@@ -7,7 +7,7 @@
  * @author  Alexander Kochetov <creocoder@gmail.com>
  * @link    https://github.com/yiiext/nested-set-behavior
  * @package craft.app.extensions
- * @since   1.0
+ * @since   1.2
  */
 class NestedSetBehavior extends CActiveRecordBehavior
 {
