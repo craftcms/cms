0723620d3d76e0a39f3f3ae8552ea8cf6576ed47
Brad Bell <brad@pixelandtonic.com>
2014-08-21 10:32:33 -0700

    Minor cleanup

--- src/assetsourcetypes/GoogleCloudAssetSourceType.php
+++ src/assetsourcetypes/GoogleCloudAssetSourceType.php
@@ -7,12 +7,12 @@ craft()->requireEdition(Craft::Pro);
  * The Google Cloud asset source type class. Handles the implementation of Google Cloud as an asset source type in
  * Craft.
  *
- * @author    Pixel & Tonic, Inc. <support@pixelandtonic.com>
- * @copyright Copyright (c) 2014, Pixel & Tonic, Inc.
- * @license   http://buildwithcraft.com/license Craft License Agreement
- * @see       http://buildwithcraft.com
- * @package   craft.app.assetsourcetypes
- * @since     1.0
+ * @author     Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @copyright  Copyright (c) 2014, Pixel & Tonic, Inc.
+ * @license    http://buildwithcraft.com/license Craft License Agreement
+ * @see        http://buildwithcraft.com
+ * @package    craft.app.assetsourcetypes
+ * @since      1.0
  * @deprecated This class will most likely be removed in Craft 3.0.
  */
 class GoogleCloudAssetSourceType extends BaseAssetSourceType
