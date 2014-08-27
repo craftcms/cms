62995311c36d5c86cff2d1d7673060e31d4ffdcc
Brandon Kelly <brandon@pixelandtonic.com>
2014-08-27 13:31:15 -0700

    Position Select field type

---
+++ src/resources/js/PositionSelectInput.js
@@ -0,0 +1,32 @@
+PositionSelectInput = Garnish.Base.extend({
+
+	$container: null,
+	$options: null,
+	$selectedOption: null,
+	$input: null,
+
+	init: function(id)
+	{
+		this.$container = $('#'+id);
+		this.$options = this.$container.find('.btn');
+		this.$selectedOption = this.$options.filter('.active');
+		this.$input = this.$container.next('input');
+
+		this.addListener(this.$options, 'click', 'onOptionSelect');
+	},
+
+	onOptionSelect: function(ev)
+	{
+		var $option = $(ev.currentTarget);
+
+		if ($option.hasClass('active'))
+		{
+			return;
+		}
+
+		this.$selectedOption.removeClass('active');
+		this.$selectedOption = $option.addClass('active');
+		this.$input.val($option.data('option'));
+	}
+
+});
