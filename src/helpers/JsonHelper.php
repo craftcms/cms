d8c2f8e5e9d882bbfd20412e96f18ff214ea08e0
Brandon Kelly <brandon@pixelandtonic.com>
2014-09-02 14:35:23 -0700

    As you wish

--- src/helpers/JsonHelper.php
+++ src/helpers/JsonHelper.php
@@ -21,12 +21,7 @@ class JsonHelper extends \CJSON
 	 */
 	public static function sendJsonHeaders()
 	{
-		// TODO: After next breakpoint release, replace with HeaderHelper code below
-		header('Cache-Control: no-cache, must-revalidate');
-		header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-		header('Content-Type: application/json; charset=utf-8');
-
-		//HeaderHelper::setNoCache();
-		//HeaderHelper::setContentTypeByExtension('json');
+		HeaderHelper::setNoCache();
+		HeaderHelper::setContentTypeByExtension('json');
 	}
 }
