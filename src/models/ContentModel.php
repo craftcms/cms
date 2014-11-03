a905b13c71542d6dd17dc67488c52b19ac0722cb
Brad Bell <brad@pixelandtonic.com>
2014-11-03 11:41:39 -0800

    Fix a bug where custom fields weren't being loaded when working with the elements service through a console command.

--- src/models/ContentModel.php
+++ src/models/ContentModel.php
@@ -150,33 +150,30 @@ class ContentModel extends BaseModel
 			'title'     => array(AttributeType::String, 'required' => $requiredTitle, 'maxLength' => 255, 'label' => 'Title'),
 		);
 
-		if (craft()->isInstalled() && !craft()->isConsole())
+		foreach (craft()->fields->getAllFields() as $field)
 		{
-			foreach (craft()->fields->getAllFields() as $field)
+			$fieldType = $field->getFieldType();
+
+			if ($fieldType)
 			{
-				$fieldType = $field->getFieldType();
-
-				if ($fieldType)
-				{
-					$attributeConfig = $fieldType->defineContentAttribute();
-				}
-
-				// Default to Mixed
-				if (!$fieldType || !$attributeConfig)
-				{
-					$attributeConfig = AttributeType::Mixed;
-				}
-
-				$attributeConfig = ModelHelper::normalizeAttributeConfig($attributeConfig);
-				$attributeConfig['label'] = $field->name;
-
-				if (isset($this->_requiredFields) && in_array($field->id, $this->_requiredFields))
-				{
-					$attributeConfig['required'] = true;
-				}
-
-				$attributes[$field->handle] = $attributeConfig;
+				$attributeConfig = $fieldType->defineContentAttribute();
 			}
+
+			// Default to Mixed
+			if (!$fieldType || !$attributeConfig)
+			{
+				$attributeConfig = AttributeType::Mixed;
+			}
+
+			$attributeConfig = ModelHelper::normalizeAttributeConfig($attributeConfig);
+			$attributeConfig['label'] = $field->name;
+
+			if (isset($this->_requiredFields) && in_array($field->id, $this->_requiredFields))
+			{
+				$attributeConfig['required'] = true;
+			}
+
+			$attributes[$field->handle] = $attributeConfig;
 		}
 
 		return $attributes;
