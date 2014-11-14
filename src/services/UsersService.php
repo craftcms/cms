60ce14f5323149d9c265c885767056e129330204
Brad Bell <brad@pixelandtonic.com>
2014-11-14 13:21:02 -0800

    Revert "Add 'convertFilenamesToAscii' config setting defaults to false. Applies to all places where files can be uploaded."
    This reverts commit dab905b5773540b7dbf6219bf4913ee918b36db4.

--- src/services/UsersService.php
+++ src/services/UsersService.php
@@ -521,7 +521,7 @@ class UsersService extends BaseApplicationComponent
 	 */
 	public function saveUserPhoto($fileName, Image $image, UserModel $user)
 	{
-		$userName = IOHelper::cleanFilename($user->username, craft()->config->get('convertFilenamesToAscii'));
+		$userName = IOHelper::cleanFilename($user->username);
 		$userPhotoFolder = craft()->path->getUserPhotosPath().$userName.'/';
 		$targetFolder = $userPhotoFolder.'original/';
 
