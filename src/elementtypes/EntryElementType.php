4c9428fb0de12f1fc37f822a2615f893835b5a49
Brandon Kelly <brandon@pixelandtonic.com>
2014-12-09 11:46:37 -0800

    Set null Post Dates when enabling entries from the Set Status action

--- src/elementtypes/EntryElementType.php
+++ src/elementtypes/EntryElementType.php
@@ -235,7 +235,20 @@ class EntryElementType extends BaseElementType
 		// Set Status
 		if ($canSetStatus)
 		{
-			$actions[] = 'SetStatus';
+			$setStatusAction = craft()->elements->getAction('SetStatus');
+			$setStatusAction->onSetStatus = function(Event $event)
+			{
+				if ($event->params['status'] == BaseElementModel::ENABLED)
+				{
+					// Set a Post Date as well
+					craft()->db->createCommand()->update(
+						'entries',
+						array('postDate' => DateTimeHelper::currentTimeForDb()),
+						array('and', array('in', 'id', $event->params['elementIds']), 'postDate is null')
+					);
+				}
+			};
+			$actions[] = $setStatusAction;
 		}
 
 		// Edit
