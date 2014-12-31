6a1348d88a33034d2929e6e88a6e304438f9b108
Brandon Kelly <brandon@pixelandtonic.com>
2014-12-31 14:40:01 -0500

    PHP 5.4

--- src/index.php
+++ src/index.php
@@ -1,11 +1,11 @@
 <?php
 
-// Make sure this is PHP 5.3 or later
+// Make sure this is PHP 5.4 or later
 // -----------------------------------------------------------------------------
 
-if (!defined('PHP_VERSION_ID') || PHP_VERSION_ID < 50300)
+if (!defined('PHP_VERSION_ID') || PHP_VERSION_ID < 50400)
 {
-	exit('@@@appName@@@ requires PHP 5.3.0 or later, but you&rsquo;re running '.PHP_VERSION.'. Please talk to your host/IT department about upgrading PHP or your server.');
+	exit('@@@appName@@@ requires PHP 5.4.0 or later, but you&rsquo;re running '.PHP_VERSION.'. Please talk to your host/IT department about upgrading PHP or your server.');
 }
 
 // Check for this early because Craft uses it before the requirements checker gets a chance to run.
