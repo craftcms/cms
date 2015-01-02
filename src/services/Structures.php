929311e9dfda5c3dfd2c7ab7253fdd20a8cac60a
Brad Bell <brad@pixelandtonic.com>
2015-01-02 15:57:38 -0500

    Missed one.

--- src/services/Structures.php
+++ src/services/Structures.php
@@ -20,7 +20,7 @@ use craft\app\web\Application;
  * @package   craft.app.services
  * @since     3.0
  */
-class StructuresService extends Component
+class Structures extends Component
 {
 	// Properties
 	// =========================================================================
