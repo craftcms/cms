2f02131080d83422fefe05354233ad6f78c507ea
Brandon Kelly <brandon@pixelandtonic.com>
2015-01-09 11:29:13 -0800

    http_response_code() came in PHP 5.4

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -37,10 +37,7 @@ function craft_createFolder($path)
 		if (!mkdir($path, 0755, true))
 		{
 			// Set a 503 response header so things like Varnish won't cache a bad page.
-			if (function_exists('http_response_code'))
-			{
-				http_response_code(503);
-			}
+			http_response_code(503);
 
 			exit('Tried to create a folder at '.$path.', but could not.');
 		}
@@ -59,10 +56,7 @@ function craft_ensureFolderIsReadable($path, $writableToo = false)
 	if ($realPath === false || !is_dir($realPath) || !@file_exists($realPath.'/.'))
 	{
 		// Set a 503 response header so things like Varnish won't cache a bad page.
-		if (function_exists('http_response_code'))
-		{
-			http_response_code(503);
-		}
+		http_response_code(503);
 
 		exit(($realPath !== false ? $realPath : $path).' doesn\'t exist or isn\'t writable by PHP. Please fix that.');
 	}
@@ -72,10 +66,7 @@ function craft_ensureFolderIsReadable($path, $writableToo = false)
 		if (!is_writable($realPath))
 		{
 			// Set a 503 response header so things like Varnish won't cache a bad page.
-			if (function_exists('http_response_code'))
-			{
-				http_response_code(503);
-			}
+			http_response_code(503);
 
 			exit($realPath.' isn\'t writable by PHP. Please fix that.');
 		}
