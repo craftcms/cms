575ae9b9b62106464e8bacc1d1410117da4b7b56
Brandon Kelly <brandon@pixelandtonic.com>
2015-01-10 06:42:47 -0800

    No more need for this

--- src/config/common.php
+++ src/config/common.php
@@ -3,11 +3,5 @@
 $components = require __DIR__.'/components/common.php';
 
 return [
-	'import' => [
-		'application.framework.cli.commands.*',
-		'application.framework.console.*',
-		'application.framework.logging.CLogger',
-	],
-
 	'components' => $components,
 ];
