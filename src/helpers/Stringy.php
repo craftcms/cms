6fe8aa0d23b6a074e76c04a2d65a7fe42f4ab361
Brad Bell <brad@pixelandtonic.com>
2015-01-13 16:49:06 -0800

    Add Stringy.

---
+++ src/helpers/Stringy.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @link http://buildwithcraft.com/
+ * @copyright Copyright (c) 2013 Pixel & Tonic, Inc.
+ * @license http://buildwithcraft.com/license
+ */
+
+namespace craft\app\helpers;
+
+/**
+ * The entire purpose of this class is so we can get at the charsArray in Stringy, which is a protected method
+ * and the creators did not want to expose as public.
+ *
+ * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @since 3.0
+ */
+class Stringy extends \Stringy
+{
+	/**
+	 * @return array
+	 */
+	public function getAsciiCharMap()
+	{
+		return parent::charsArray();
+	}
+}
