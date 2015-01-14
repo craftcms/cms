a5d96fb6803d82e3625443161dee8d403a435a0f
Brad Bell <brad@pixelandtonic.com>
2015-01-13 17:01:05 -0800

    Add craft/app/helpers/HtmlPurifier that extends \yii\helpers\HtmlPurifier so we can add some wrapping method.

---
+++ src/helpers/HtmlPurifier.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @link http://buildwithcraft.com/
+ * @copyright Copyright (c) 2013 Pixel & Tonic, Inc.
+ * @license http://buildwithcraft.com/license
+ */
+
+namespace craft\app\helpers;
+
+/**
+ * HtmlPurifier provides an ability to clean up HTML from any harmful code.
+ *
+ * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @since 3.0
+ */
+class HtmlPurifier extends \yii\helpers\HtmlPurifier
+{
+	public static function cleanUtf8($string)
+	{
+		return \HTMLPurifier_Encoder::cleanUTF8($string);
+	}
+
+	public static function convertToUtf8($string, $config)
+	{
+		return \HTMLPurifier_Encoder::convertToUTF8($string, $config, null);
+	}
+}
