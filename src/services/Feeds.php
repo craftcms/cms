f1b7849897809d6133cea85a528647c86c61a0d9
Brad Bell <brad@pixelandtonic.com>
2015-03-03 17:53:39 -0800

    Fix Feeds service error.

--- src/services/Feeds.php
+++ src/services/Feeds.php
@@ -36,7 +36,7 @@ class Feeds extends Component
 
 		// Import this here to ensure that libs like SimplePie are using our version of the class and not any server's
 		// random version.
-		require_once(Craft::getAlias('app.vendor.simplepie.simplepie.idn.').DIRECTORY_SEPARATOR.'idna_convert.class.php');
+		require_once(Craft::getAlias('@app/vendor/simplepie/simplepie/idn/idna_convert.class.php'));
 	}
 
 	/**
