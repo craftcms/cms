07ea4a75f8df7e10864ac12a2e6667a675ecc8e2
Brad Bell <brad@pixelandtonic.com>
2015-03-17 19:37:01 -0700

    A little console work.

--- src/config/console.php
+++ src/config/console.php
@@ -1,26 +1,7 @@
 <?php
 
 return [
-
-	'basePath' => dirname(__FILE__).'/../../',
-
-	// autoloading model and component classes
-	'import' => [
-		'application.*',
-		'application.migrations.*',
-	],
-
 	'components' => [
-		'db' => [
-			'emulatePrepare'    => true,
-			'driverMap'         => ['mysql' => 'Craft\MysqlSchema'],
-			'class'             => 'Craft\DbConnection',
-		],
-		'migrations' => [
-			'class'             => 'Craft\MigrationsService',
-		],
 		'request' => 'craft\app\console\Request',
 	],
-
-	'commandPath' => \Craft::getAlias('system.cli.commands.*'),
 ];
