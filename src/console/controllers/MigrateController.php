cfb36d66bd76ddf1db63aceabc1b7aa455aeff3f
Brad Bell <brad@pixelandtonic.com>
2015-03-19 18:46:52 -0700

    More migration work.

--- src/console/controllers/MigrateController.php
+++ src/console/controllers/MigrateController.php
@@ -7,7 +7,13 @@
 
 namespace craft\app\console\controllers;
 
+use Craft;
+use craft\app\base\BasePlugin;
+use craft\app\helpers\IOHelper;
+use craft\app\helpers\StringHelper;
 use yii\base\NotSupportedException;
+use yii\console\Exception;
+use yii\helpers\Console;
 
 /**
  * Manages Craft and plugin migrations.
@@ -26,10 +32,10 @@ use yii\base\NotSupportedException;
  *
  * ~~~
  * # creates a new migration named 'create_user_table'
- * yii migrate/create create_user_table
+ * craft migrate create create_user_table
  *
  * # applies ALL new migrations
- * yii migrate
+ * craft migrate
  * ~~~
  *
  * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
@@ -37,6 +43,11 @@ use yii\base\NotSupportedException;
  */
 class MigrateController extends \yii\console\controllers\MigrateController
 {
+	/**
+	 * @var
+	 */
+	public $pluginHandle;
+
 	/**
 	 * Craft doesn’t support down migrations.
 	 *
@@ -86,5 +97,88 @@ class MigrateController extends \yii\console\controllers\MigrateController
 	public function actionMark($version)
 	{
 		throw new NotSupportedException('Marking migrations is not supported.');
+	}
+
+	/**
+	 * Used for creating a new migration, for either Craft or a plugin.
+	 *
+	 *    craft migrate create MigrationDescription --pluginHandle=pluginHandle
+	 *
+	 * If PluginHandle is omitted, the migration is created for Craft in craft/app/migrations. If it is available, the
+	 * migration is created in craft/plugins/PluginHandle/migrations.
+	 *
+	 * The migration description can only contain letters, digits and/or underscore characters.
+	 *
+	 * @param string $name
+	 *
+	 * @return int
+	 * @throws Exception
+	 */
+	public function actionCreate($name)
+	{
+		if (!preg_match('/^\w+$/', $name))
+		{
+			throw new Exception("The migration name should contain letters, digits and/or underscore characters only.");
+		}
+
+		if ($this->pluginHandle)
+		{
+			if (!preg_match('/^\w+$/', $this->pluginHandle))
+			{
+				throw new Exception("The plugin handle should contain letters, digits and/or underscore characters only.");
+			}
+
+			// See if this is a valid plugin
+			$this->_validatePlugin($this->pluginHandle);
+
+			$name = 'm'.gmdate('ymd_His').'_'.StringHelper::toLowerCase($this->pluginHandle).'_'.$name;
+			$migrationNameDesc = 'mYYMMDD_HHMMSS_pluginHandle_migrationName';
+
+			$path = Craft::$app->path->getMigrationsPath($this->pluginHandle);
+
+			if (!IOHelper::folderExists($path))
+			{
+				IOHelper::createFolder($path);
+			}
+		}
+		else
+		{
+			$name = 'm'.gmdate('ymd_His').'_'.$name;
+			$migrationNameDesc = 'mYYMMDD_HHMMSS_migrationName';
+			$path = $this->migrationPath;
+		}
+
+		$migrationFile = $path.'/'.$name.'.php';
+
+		if ($this->confirm("Create new migration '$migrationFile'?"))
+		{
+			$content = $this->renderFile(Craft::$app->migrations->getTemplate(), ['className' => $name, 'migrationNameDesc' => $migrationNameDesc]);
+			IOHelper::writeToFile($migrationFile, $content);
+
+			$this->stdout("New migration created successfully.\n", Console::FG_GREEN);
+		}
+	}
+
+	// Private Methods
+	// =========================================================================
+
+	/**
+	 * Given a plugin handle, will retrieve it, or display an error if it doesn't exist or is disabled.
+	 *
+	 * @param string $pluginHandle The handle of the plugin to search for.
+	 *
+	 * @return BasePlugin|int
+	 * @throws Exception
+	 */
+	private function _validatePlugin($pluginHandle)
+	{
+		$plugin = Craft::$app->plugins->getPlugin($pluginHandle);
+
+		if (!$plugin)
+		{
+			throw new Exception("Could not find an installed and enabled plugin with the handle '$pluginHandle'");
+		}
+
+		return $plugin;
 	}
 }
