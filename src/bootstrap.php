2ed1c2baf6054405906e132e694c61e03ed5e889
Brandon Kelly <brandon@pixelandtonic.com>
2015-04-03 20:57:03 -0700

    It's 2015

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,7 +3,7 @@
  * Craft bootstrap file.
  *
  * @link http://buildwithcraft.com/
- * @copyright Copyright (c) 2013 Pixel & Tonic, Inc.
+ * @copyright Copyright (c) 2015 Pixel & Tonic, Inc.
  * @license http://buildwithcraft.com/license
  */
 
