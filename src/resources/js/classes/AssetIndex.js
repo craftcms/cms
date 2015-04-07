23fa721765e4387d1942142315e0bdaebf50f2d1
Andris Ševčenko <andris@pixelandtonic.com>
2015-04-07 15:21:21 +0300

    Fix an issue where files would disappear until reload when moving files and resolving conflict with "cancel"

--- src/resources/js/classes/AssetIndex.js
+++ src/resources/js/classes/AssetIndex.js
@@ -280,6 +280,7 @@ Craft.AssetIndex = Craft.BaseElementIndex.extend(
 
 					this.setIndexAvailable();
 					this.progressBar.hideProgressBar();
+					var reloadIndex = false;
 
 					var performAfterMoveActions = function ()
 					{
@@ -297,6 +298,11 @@ Craft.AssetIndex = Craft.BaseElementIndex.extend(
 
 						this.elementSelect.deselectAll();
 						this._collapseExtraExpandedFolders(targetFolderId);
+
+						if (reloadIndex)
+						{
+							this.updateElements();
+						}
 					};
 
 					if (this.promptHandler.getPromptCount())
@@ -311,6 +317,7 @@ Craft.AssetIndex = Craft.BaseElementIndex.extend(
 							{
 								if (returnData[i].choice == 'cancel')
 								{
+									reloadIndex = true;
 									continue;
 								}
 
