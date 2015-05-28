8508f6d4af77f0501c1e9b4e7841ffc865a497be
Brandon Kelly <brandon@pixelandtonic.com>
2015-05-28 14:13:10 -0700

    StringHelper::toKebabCase(), splitOnWords(), stripHtml(), and the |kebab Twig filter

--- src/etc/templating/twigextensions/CraftTwigExtension.php
+++ src/etc/templating/twigextensions/CraftTwigExtension.php
@@ -85,6 +85,7 @@ class CraftTwigExtension extends \Twig_Extension
 			't'                  => $translateFilter,
 			'ucfirst'            => new \Twig_Filter_Method($this, 'ucfirstFilter'),
 			'ucwords'            => new \Twig_Filter_Function('ucwords'),
+			'kebab'              => new \Twig_Filter_Method($this, 'kebabFilter'),
 			'without'            => new \Twig_Filter_Method($this, 'withoutFilter'),
 		);
 	}
@@ -111,6 +112,21 @@ class CraftTwigExtension extends \Twig_Extension
 	public function lcfirstFilter($string)
 	{
 		return StringHelper::lowercaseFirst($string);
+	}
+
+	/**
+	 * Kebab-cases a string.
+	 *
+	 * @param string $string The string
+	 * @param string $glue The string used to glue the words together (default is a hyphen)
+	 * @param boolean $lower Whether the string should be lowercased (default is true)
+	 * @param boolean $removePunctuation Whether punctuation marks should be removed (default is true)
+	 *
+	 * @return string
+	 */
+	public function kebabFilter($string, $glue = '-', $lower = true, $removePunctuation = true)
+	{
+		return StringHelper::toKebabCase($string, $glue, $lower, $removePunctuation);
 	}
 
 	/**
