5b6e983d288824ade3772e35e229a1a8f407ecf1
Brad Bell <brad@pixelandtonic.com>
2015-06-01 17:52:19 -0700

    Fix the ini_set check in RequirementsChecker so that it works on FastCGI / php-fpm installs.

--- src/requirements/RequirementsChecker.php
+++ src/requirements/RequirementsChecker.php
@@ -566,15 +566,16 @@ class RequirementsChecker
 		$result = ini_set('memory_limit', '442M');
 		restore_error_handler();
 
-		// ini_set can return false or an empty string depending on your php version / FastCGI.
-		// If it has been disabled in php.ini, the value will be null because of our muted error handler
-		if (!$result)
+		//  If ini_set has been disabled in php.ini, the value will be null because of our muted error handler
+		if ($result === null)
 		{
 			$this->iniSetMessage = 'It looks like <a href="http://php.net/manual/en/function.ini-set.php">ini_set</a> has been disabled in your php.ini file. Craft requires that to operate.';
 			return false;
 		}
 
-		if ($result !== $oldValue)
+		// ini_set can return false or an empty string or the current value of memory_limit depending on your php
+		// version and FastCGI. Regard, calling it didn't work, but there was no error.
+		if ($result === false || $result === '' || $result === $oldValue)
 		{
 			$this->iniSetMessage = 'It appears calls to <a href="http://php.net/manual/en/function.ini-set.php">ini_set</a> are not working for Craft. You may need to increase some settings in your php.ini file such as <a href="http://php.net/manual/en/ini.core.php#ini.memory-limit">memory_limit</a> and <a href="http://php.net/manual/en/info.configuration.php#ini.max-execution-time">max_execution_time</a> for long running operations like updating and asset transformations.';
 
@@ -584,7 +585,7 @@ class RequirementsChecker
 
 		// Resetting should work, but might as well be extra careful.
 		set_error_handler(array($this, 'muteErrorHandler'));
-		$result = ini_set('memory_limit', $oldValue);
+		ini_set('memory_limit', $oldValue);
 		restore_error_handler();
 
 		$this->iniSetMessage = 'Calls to <a href="http://php.net/manual/en/function.ini-set.php">ini_set</a> are working correctly.';
