2e796092f543eccedda8e826beefd15133e96408
Brandon Kelly <brandon@pixelandtonic.com>
2015-06-09 13:44:19 -0700

    Empty DateTime attributes should stay empty

--- src/base/Model.php
+++ src/base/Model.php
@@ -132,7 +132,11 @@ abstract class Model extends \yii\base\Model
         // Have all DateTime attributes converted to ISO-8601 strings
         foreach ($this->datetimeAttributes() as $attribute) {
             $fields[$attribute] = function($model, $attribute) {
-                return DateTimeHelper::toIso8601($model->$attribute);
+                if (!empty($model->$attribute)) {
+                    return DateTimeHelper::toIso8601($model->$attribute);
+                } else {
+                    return $model->$attribute;
+                }
             };
         }
 
