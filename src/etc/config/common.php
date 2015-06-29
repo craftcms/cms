d0a32f25dcbf6abbca96ab98ab7bd8eee883c4d2
Brad Bell <brad@pixelandtonic.com>
2015-06-29 14:35:01 -0700

    Fix a bug where PHP warnings/notices/fatal errors could display the full path to the error even if devMode was disabled.

--- src/etc/config/common.php
+++ src/etc/config/common.php
@@ -1,12 +1,5 @@
 <?php
 
-// Initially set it here.  WebApp->init() will check devMode and override appropriately.
-error_reporting(E_ALL & ~E_STRICT);
-ini_set('display_errors', 1);
-
-ini_set('log_errors', 1);
-ini_set('error_log', CRAFT_STORAGE_PATH.'runtime/logs/phperrors.log');
-
 $configArray = array(
 
 	// autoloading model and component classes
