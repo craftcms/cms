ab5a7272f4a794188db89248bb77df41092b0f65
Brad Bell <brad@pixelandtonic.com>
2015-07-03 11:17:36 -0500

    Fix error reporting regression for yiic from c78576f2a5a4970e30fba556ceecacdbe7c7df33

--- src/etc/console/yiic.php
+++ src/etc/console/yiic.php
@@ -21,6 +21,7 @@ if (isset($_SERVER['argv']))
 
 defined('CRAFT_BASE_PATH')         || define('CRAFT_BASE_PATH', str_replace('\\', '/', realpath(dirname(__FILE__).'/../../../')).'/');
 defined('CRAFT_APP_PATH')          || define('CRAFT_APP_PATH',          CRAFT_BASE_PATH.'app/');
+
 if ($frontConfigPath)
 {
 	defined('CRAFT_CONFIG_PATH')   || define('CRAFT_CONFIG_PATH',       $frontConfigPath);
@@ -42,6 +43,14 @@ defined('CRAFT_ENVIRONMENT')       || define('CRAFT_ENVIRONMENT',       'console
 
 // fix for fcgi
 defined('STDIN') or define('STDIN', fopen('php://stdin', 'r'));
+
+ini_set('log_errors', 1);
+ini_set('error_log', CRAFT_STORAGE_PATH.'runtime/logs/phperrors.log');
+
+error_reporting(E_ALL & ~E_STRICT);
+ini_set('display_errors', 1);
+defined('YII_DEBUG') || define('YII_DEBUG', true);
+defined('YII_TRACE_LEVEL') || define('YII_TRACE_LEVEL', 3);
 
 require_once dirname(__FILE__).'/../../framework/yii.php';
 require_once CRAFT_APP_PATH.'Craft.php';
