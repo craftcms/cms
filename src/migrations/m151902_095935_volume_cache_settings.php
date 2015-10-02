b1c4a05767383b8a9c07f9d4f6fa515dbc91d2db
Andris Ševčenko <andris@pixelandtonic.com>
2015-10-02 12:45:39 +0300

    Update Volume cache header setting storage format from 2.x.

---
+++ src/migrations/m151902_095935_volume_cache_settings.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace craft\app\migrations;
+
+use Craft;
+use craft\app\db\Migration;
+use craft\app\db\Query;
+use craft\app\helpers\Json;
+use craft\app\helpers\Migration as MigrationHelper;
+
+/**
+ * m150403_185142_volumes migration.
+ */
+class m151902_095935_volume_cache_settings extends Migration
+{
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        // Update permissions
+        $volumes = (new Query())
+            ->select('id, settings')
+            ->from('{{%volumes}}')
+            ->where(['like', 'type', '%AwsS3', false])
+            ->orWhere(['like', 'type', '%GoogleCloud', false])
+            ->all();
+
+        foreach ($volumes as $volume) {
+            $settings = Json::decode($volume['settings']);
+
+            if (!empty($settings['expires']) && preg_match('/([0-9]+)([a-z]+)/', $settings['expires'], $matches))
+            {
+                $settings['expires'] = $matches[1].' '.$matches[2];
+
+                Craft::$app->getDb()->createCommand()->update(
+                    '{{%volumes}}',
+                    array('settings' => Json::encode($settings)),
+                    array('id' => $volume['id'])
+                )->execute();
+            }
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m151902_095935_volume_cache_settings cannot be reverted.\n";
+
+        return false;
+    }
+}
