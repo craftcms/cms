faf572d8bb446d728f7fbb9f0ee19ce39de83308
Brad Bell <brad@pixelandtonic.com>
2015-10-21 14:02:17 -0700

    Try and alter the column *before* saving teh field record, so if an error occurs altering the column, the transaction can roll back all the way. Fixes a bug where the CP would be unusable if you saved a field chaning its type so that you'd get a MySQL data truncation error *and* changed the field's handle all at once.

--- src/services/FieldsService.php
+++ src/services/FieldsService.php
@@ -467,38 +467,8 @@ class FieldsService extends BaseApplicationComponent
 				$fieldRecord = $this->_getFieldRecord($field);
 				$isNewField = $fieldRecord->isNewRecord();
 
-				$fieldRecord->groupId      = $field->groupId;
-				$fieldRecord->name         = $field->name;
-				$fieldRecord->handle       = $field->handle;
-				$fieldRecord->context      = $field->context;
-				$fieldRecord->instructions = $field->instructions;
-				$fieldRecord->translatable = $field->translatable;
-				$fieldRecord->type         = $field->type;
-
 				// Get the field type
 				$fieldType = $field->getFieldType();
-
-				// Give the field type a chance to prep the settings from post
-				$preppedSettings = $fieldType->prepSettings($field->settings);
-
-				// Set the prepped settings on the FieldRecord, FieldModel, and the field type
-				$fieldRecord->settings = $field->settings = $preppedSettings;
-				$fieldType->setSettings($preppedSettings);
-
-				if ($fieldRecord->settings instanceof BaseModel)
-				{
-					// Call getAttributes() without passing 'true' so the __model__ isn't saved
-					$fieldRecord->settings = $fieldRecord->settings->getAttributes();
-				}
-
-				$fieldType->onBeforeSave();
-				$fieldRecord->save(false);
-
-				// Now that we have a field ID, save it on the model
-				if ($isNewField)
-				{
-					$field->id = $fieldRecord->id;
-				}
 
 				// Create/alter the content table column
 				$columnType = $fieldType->defineContentAttribute();
@@ -537,6 +507,36 @@ class FieldsService extends BaseApplicationComponent
 							craft()->db->createCommand()->dropColumn($contentTable, $oldColumnName);
 						}
 					}
+				}
+
+				$fieldRecord->groupId      = $field->groupId;
+				$fieldRecord->name         = $field->name;
+				$fieldRecord->handle       = $field->handle;
+				$fieldRecord->context      = $field->context;
+				$fieldRecord->instructions = $field->instructions;
+				$fieldRecord->translatable = $field->translatable;
+				$fieldRecord->type         = $field->type;
+
+				// Give the field type a chance to prep the settings from post
+				$preppedSettings = $fieldType->prepSettings($field->settings);
+
+				// Set the prepped settings on the FieldRecord, FieldModel, and the field type
+				$fieldRecord->settings = $field->settings = $preppedSettings;
+				$fieldType->setSettings($preppedSettings);
+
+				if ($fieldRecord->settings instanceof BaseModel)
+				{
+					// Call getAttributes() without passing 'true' so the __model__ isn't saved
+					$fieldRecord->settings = $fieldRecord->settings->getAttributes();
+				}
+
+				$fieldType->onBeforeSave();
+				$fieldRecord->save(false);
+
+				// Now that we have a field ID, save it on the model
+				if ($isNewField)
+				{
+					$field->id = $fieldRecord->id;
 				}
 
 				if (!$isNewField)
