b17bfe42b418953cad632a73353fa581caa9ca6f
Andris Ševčenko <andris@pixelandtonic.com>
2015-10-26 10:59:27 +0200

    Fix an issue that prevented opening the cropping modal twice in a row without reloading the page.

--- src/resources/js/classes/ImageUpload.js
+++ src/resources/js/classes/ImageUpload.js
@@ -288,8 +288,8 @@ Craft.ImageModal = Garnish.Modal.extend(
 	cancel: function()
 	{
 		this.hide();
-		this.areaSelect.setOptions({remove: true, hide: true, disable: true});
-		this.$container.empty();
+		this.$container.remove();
+		this.destroy();
 	},
 
 	saveImage: function()
@@ -320,10 +320,8 @@ Craft.ImageModal = Garnish.Modal.extend(
 			}
 
 			this.hide();
-			this.$container.empty();
-			this.areaSelect.setOptions({remove: true, hide: true, disable: true});
-
-
+			this.$container.remove();
+			this.destroy();
 		}, this));
 
 		this.areaSelect.setOptions({disable: true});
