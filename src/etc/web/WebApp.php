330fb72cce50a3c055a5523e6c11d5f3ba23014b
Brad Bell <brad@pixelandtonic.com>
2015-11-04 20:59:04 -0800

    Fix a bug where a user had "access the site the site when the system of off" permissions and they were trying to log into the site from the front-end, they couldn't

--- src/etc/web/WebApp.php
+++ src/etc/web/WebApp.php
@@ -988,7 +988,10 @@ class WebApp extends \CWebApplication
 
 			// Special case because we hide the cpTrigger in emails.
 			$this->request->getPath() === craft()->config->get('actionTrigger').'/users/setpassword' ||
-			$this->request->getPath() === craft()->config->get('actionTrigger').'/users/verifyemail'
+			$this->request->getPath() === craft()->config->get('actionTrigger').'/users/verifyemail' ||
+			// Special case because this might be a request with a user that has "Access the site when the system is off"
+			// permissions and is in the process of logging in while the system is off.
+			$this->request->getPost('action') === 'users/login'
 		)
 		{
 			if ($this->userSession->checkPermission('accessCpWhenSystemIsOff'))
