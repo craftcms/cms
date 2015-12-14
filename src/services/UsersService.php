a5df31fa035bd2227252a875950b25f7498dde2e
Brandon Kelly <brandon@pixelandtonic.com>
2015-12-14 11:52:31 -0800

    Send verification/password reset emails with the primary locale if the target user doesn't have a preferred locale

--- src/services/UsersService.php
+++ src/services/UsersService.php
@@ -529,7 +529,8 @@ class UsersService extends BaseApplicationComponent
 				'id' => $userRecord->uid
 			);
 			$protocol = craft()->request->isSecureConnection() ? 'https' : 'http';
-			$url = UrlHelper::getSiteUrl($path, $params, $protocol, $user->preferredLocale);
+			$locale = $user->preferredLocale ?: craft()->i18n->getPrimarySiteLocaleId();
+			$url = UrlHelper::getSiteUrl($path, $params, $protocol, $locale);
 		}
 
 		return $url;
@@ -561,7 +562,8 @@ class UsersService extends BaseApplicationComponent
 		}
 		else
 		{
-			return UrlHelper::getSiteUrl($path, $params, $scheme, $user->preferredLocale);
+			$locale = $user->preferredLocale ?: craft()->i18n->getPrimarySiteLocaleId();
+			return UrlHelper::getSiteUrl($path, $params, $scheme, $locale);
 		}
 	}
 
