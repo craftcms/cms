da8b839f01eb977749ff11774fc7575f7263d84b
Brad Bell <brad@pixelandtonic.com>
2016-01-07 15:39:33 -0800

    Probably fixes a rare bug where you'd get invalid MySQL syntax when creating a database backup on some systems.

--- src/etc/db/DbBackup.php
+++ src/etc/db/DbBackup.php
@@ -430,8 +430,8 @@ class DbBackup
 							$insertStatement .= ' ('.implode(', ', $row).'),'.PHP_EOL;
 						}
 
-						// Nuke that last command and add a ;
-						$insertStatement[strlen($insertStatement) - 2] = ';';
+						// Nuke that last comma and add a ;
+						$insertStatement = mb_substr($insertStatement, 0, -2).';';
 
 						IOHelper::writeToFile($this->_filePath, $insertStatement . PHP_EOL, true, true);
 					}
