162e41f30a4fb8654effcf8233378eac2b959b61
Brandon Kelly <brandon@pixelandtonic.com>
2016-01-25 17:16:00 -0800

    User::loginByUserId()

--- src/web/User.php
+++ src/web/User.php
@@ -78,6 +78,28 @@ class User extends \yii\web\User
     // -------------------------------------------------------------------------
 
     /**
+     * Logs in a user by their ID
+     *
+     * @param integer $userId The user’s ID
+     * @param integer $duration The number of seconds that the user can remain in logged-in status.
+     * Defaults to 0, meaning login till the user closes the browser or the session is manually destroyed.
+     * If greater than 0 and [[enableAutoLogin]] is true, cookie-based login will be supported.
+     * Note that if [[enableSession]] is false, this parameter will be ignored.
+     *
+     * @return boolean Whether the user is logged in
+     */
+    public function loginByUserId($userId, $duration = 0)
+    {
+        $user = Craft::$app->getUsers()->getUserById($userId);
+
+        if (!$user) {
+            return false;
+        }
+
+        return $this->login($user, $duration);
+    }
+
+    /**
      * Sends a username cookie.
      *
      * This method is used after a user is logged in. It saves the logged-in user's username in a cookie,
