ffcbbfa9bc4c18373153813d529d6e30a045d213
Brandon Kelly <brandon@pixelandtonic.com>
2016-01-28 10:58:04 -0800

    Added new reserved field handles for Struture attributes and some element type-specific ones

--- src/records/FieldRecord.php
+++ src/records/FieldRecord.php
@@ -17,7 +17,9 @@ class FieldRecord extends BaseRecord
 	// =========================================================================
 
 	/**
-	 * @var array
+	 * @var array Reserved field handles.
+	 *
+	 * Some of these are element type-specific, but necessary to prevent 'order' criteria param conflicts
 	 */
 	protected $reservedHandleWords = array(
 		'archived',
@@ -26,14 +28,22 @@ class FieldRecord extends BaseRecord
 		'dateUpdated',
 		'enabled',
 		'id',
+		'level',
+		'lft',
 		'link',
 		'locale',
+		'localeEnabled',
+		'name', // global set-specific
 		'parents',
+		'postDate', // entry-specific
+		'rgt',
+		'root',
 		'siblings',
 		'sortOrder',
 		'uid',
 		'uri',
 		'url',
+		'username', // user-specific
 		'ref',
 		'status',
 		'title',
