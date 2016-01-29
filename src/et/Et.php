cead3a5c4bae332532cb4603b208a756ee259152
Brandon Kelly <brandon@pixelandtonic.com>
2016-01-29 13:31:55 -0800

    Default to 30 second connection timeout

--- src/et/Et.php
+++ src/et/Et.php
@@ -69,7 +69,7 @@ class Et
      *
      * @return Et
      */
-    public function __construct($endpoint, $timeout = 30, $connectTimeout = 2)
+    public function __construct($endpoint, $timeout = 30, $connectTimeout = 30)
     {
         $endpoint .= Craft::$app->getConfig()->get('endpointSuffix');
 
