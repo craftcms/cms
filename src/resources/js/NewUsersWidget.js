c0580973a9d022916c8fec1b84a059c8ffc00277
Benjamin David <ben@pixelandtonic.com>
2016-02-08 13:29:14 +0100

    Added New Users dashboard widget

---
+++ src/resources/js/NewUsersWidget.js
@@ -0,0 +1,83 @@
+(function($) {
+
+
+Craft.NewUsersWidget = Garnish.Base.extend(
+{
+    settings: null,
+    data: null,
+
+    $widget: null,
+    $body: null,
+
+    init: function(widgetId, settings)
+    {
+        this.setSettings(settings);
+
+        this.$widget = $('#widget'+widgetId);
+        this.$body = this.$widget.find('.body:first');
+
+        // Add the chart to the body
+        this.$chartContainer = $('.chart', this.$widget);
+
+        // Error
+        this.$error = $('<div class="error"/>').prependTo(this.$body);
+
+        // Request orders report
+        var requestData = {
+            dateRange: this.settings.dateRange,
+            userGroupId: this.settings.userGroupId,
+            elementType: 'Commerce_Order'
+        };
+
+        Craft.postActionRequest('reports/getNewUsersReport', requestData, $.proxy(function(response, textStatus)
+        {
+            if(textStatus == 'success' && typeof(response.error) == 'undefined')
+            {
+                // Create chart
+                this.chart = new Craft.charts.Area(this.$chartContainer);
+
+                var chartDataTable = new Craft.charts.DataTable(response.reportDataTable);
+
+                this.chart.draw(chartDataTable);
+
+                // Resize chart when grid is refreshed
+                window.dashboard.grid.on('refreshCols', $.proxy(this, 'handleGridRefresh'));
+            }
+            else
+            {
+                // Error
+
+                var msg = 'An unknown error occured.';
+
+                if(typeof(response) != 'undefined' && response && typeof(response.error) != 'undefined')
+                {
+                    msg = response.error;
+                }
+
+                this.$error.html(msg);
+                this.$error.removeClass('hidden');
+            }
+
+        }, this));
+
+        this.$widget.data('widget').on('destroy', $.proxy(this, 'destroy'));
+
+        Craft.NewUsersWidget.instances.push(this);
+    },
+
+    handleGridRefresh: function()
+    {
+        this.chart.resize();
+    },
+
+    destroy: function()
+    {
+        Craft.NewUsersWidget.instances.splice($.inArray(this, Craft.NewUsersWidget.instances), 1);
+        this.base();
+    }
+}, {
+    instances: []
+});
+
+
+})(jQuery);
---
+++ src/widgets/NewUsersWidget.php
@@ -0,0 +1,77 @@
+<?php
+namespace Craft;
+
+/**
+ * Class NewUsersWidget
+ *
+ * @author    Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @copyright Copyright (c) 2014, Pixel & Tonic, Inc.
+ * @license   http://craftcms.com/license Craft License Agreement
+ * @see       http://craftcms.com
+ * @package   craft.app.widgets
+ * @since     1.0
+ */
+class NewUsersWidget extends BaseWidget
+{
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * @inheritDoc IComponentType::getName()
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return Craft::t('New Users');
+    }
+
+    /**
+     * @inheritDoc IWidget::getBodyHtml()
+     *
+     * @return string|false
+     */
+    public function getBodyHtml()
+    {
+        $settings = $this->getSettings();
+
+        $groupId = $settings->userGroupId;
+        $userGroup = craft()->userGroups->getGroupById($groupId);
+
+        craft()->templates->includeJsResource('js/NewUsersWidget.js');
+        craft()->templates->includeJs('new Craft.NewUsersWidget('.$this->model->id.', '.JsonHelper::encode($settings).');');
+
+
+        return craft()->templates->render('_components/widgets/NewUsers/body', array(
+            'userGroup' => $userGroup
+        ));
+    }
+
+    /**
+     * @inheritDoc ISavableComponentType::getSettingsHtml()
+     *
+     * @return string
+     */
+    public function getSettingsHtml()
+    {
+        return craft()->templates->render('_components/widgets/NewUsers/settings', array(
+            'settings' => $this->getSettings()
+        ));
+    }
+
+    // Protected Methods
+    // =========================================================================
+
+    /**
+     * @inheritDoc BaseSavableComponentType::defineSettings()
+     *
+     * @return array
+     */
+    protected function defineSettings()
+    {
+        return array(
+            'userGroupId'   => AttributeType::Number,
+            'dateRange'   => AttributeType::String,
+        );
+    }
+}
