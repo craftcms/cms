ee89460eb7a09efaf7d768b6422025fca73fc0c5
Brandon Kelly <brandon@pixelandtonic.com>
2016-05-03 16:16:46 -0700

    FileHelper::getMimeTypeFromMagicBytes()

--- src/helpers/FileHelper.php
+++ src/helpers/FileHelper.php
@@ -54,4 +54,22 @@ class FileHelper extends \CFileHelper
 
 		return parent::getExtensionByMimeType($file, $magicFile);
 	}
+
+	/**
+	 * Returns a file’s MIME type, based on its magic bytes.
+	 *
+	 * @param string $file The file path
+	 *
+	 * @return string The file’s MIME type
+	 *
+	 * @see https://designshack.net/articles/php-articles/smart-file-type-detection-using-php/
+	 */
+	public static function getMimeTypeFromMagicBytes($file)
+	{
+		// Can't just use CFileHelper::getMimeType() here because that will fall back on mime_content_type() if it's available
+		$fileInfo = new \finfo(\FILEINFO_MIME);
+		$mimeType = $file_info->buffer(file_get_contents($file));
+
+		return $mimeType;
+	}
 }
