7484293377ed081e6aec24c958de56b2f841ec15
Brandon Kelly <brandon@pixelandtonic.com>
2016-07-27 17:35:20 -0700

    Fixed custom debug toolbar panels' styling

--- src/views/debug/info/summary.php
+++ src/views/debug/info/summary.php
@@ -1,12 +1,11 @@
 <?php
 /** @var $panel craft\app\debug\InfoPanel */
 ?>
-<div class="yii-debug-toolbar-block">
+<div class="yii-debug-toolbar__block">
     <a href="<?= $panel->getUrl() ?>">
         Craft
-        <span
-            class="label"><?= $panel->data['craftVersion'].'.'.$panel->data['craftBuild'] ?></span>
+        <span class="yii-debug-toolbar__label"><?= $panel->data['craftVersion'].'.'.$panel->data['craftBuild'] ?></span>
         PHP
-        <span class="label"><?= $panel->data['phpVersion'] ?></span>
+        <span class="yii-debug-toolbar__label"><?= $panel->data['phpVersion'] ?></span>
     </a>
 </div>
