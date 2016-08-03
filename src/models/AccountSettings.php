7ab5a49a7c8c81ae9d4810bcee0ece4c16616866
Brad Bell <brad@pixelandtonic.com>
2016-08-03 14:03:41 -0700

    Fix username requirement in models/AccountSettings.

--- src/models/AccountSettings.php
+++ src/models/AccountSettings.php
@@ -44,8 +44,15 @@ class AccountSettings extends Model
      */
     public function rules()
     {
+        $requireUsername = !Craft::$app->getConfig()->get('useEmailAsUsername');
+        $requiredAttributes = [['email', 'password'], 'required'];
+
+        if ($requireUsername) {
+            $requiredAttributes[0][] = 'username';
+        }
+
         return [
-            [['username', 'email', 'password'], 'required'],
+            $requiredAttributes,
             [['email'], 'email'],
             [['email'], 'string', 'min' => 5],
             [['password'], 'string', 'min' => 6],
