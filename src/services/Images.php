4de4534245b3fefb57d53db39c50928a9bc529d8
Brandon Kelly <brandon@pixelandtonic.com>
2016-08-04 06:03:38 -0700

    Suppress static method warning

--- src/services/Images.php
+++ src/services/Images.php
@@ -118,6 +118,8 @@ class Images extends Component
             // Taken from Imagick\Imagine() constructor.
             // Imagick::getVersion() is static only since Imagick PECL extension 3.2.0b1, so instantiate it.
             $imagick = new \Imagick();
+            /** @noinspection PhpStaticAsDynamicMethodCallInspection */
+            // getVersion() is only static as of imagick 3.2.0b1
             $v = $imagick->getVersion();
             /** @noinspection PhpUnusedLocalVariableInspection */
             list($version, $year, $month, $day, $q, $website) = sscanf($v['versionString'], 'ImageMagick %s %04d-%02d-%02d %s %s');
