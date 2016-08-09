35117b755bb76a6cfcf315521b8bd8b01d5f5750
Brandon Kelly <brandon@pixelandtonic.com>
2016-08-09 05:34:25 -0700

    web\Application::getRequest() will always return web\Request

--- src/web/Application.php
+++ src/web/Application.php
@@ -332,10 +332,6 @@ class Application extends \yii\web\Application
      */
     public function getTranslatedBrowserLanguage()
     {
-        if ($this->getRequest()->getIsConsoleRequest()) {
-            return false;
-        }
-
         $browserLanguages = $this->getRequest()->getAcceptableLanguages();
 
         if ($browserLanguages) {
