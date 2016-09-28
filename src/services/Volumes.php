80d040c13204c89721f0981ab9501967ba0f3937
Brandon Kelly <brandon@pixelandtonic.com>
2016-09-28 05:19:07 -0700

    Deleted DeleteVolumeEvent

--- src/services/Volumes.php
+++ src/services/Volumes.php
@@ -7,7 +7,6 @@ use craft\app\base\VolumeInterface;
 use craft\app\db\Query;
 use craft\app\errors\VolumeException;
 use craft\app\errors\MissingComponentException;
-use craft\app\events\DeleteVolumeEvent;
 use craft\app\events\VolumeEvent;
 use craft\app\helpers\Component as ComponentHelper;
 use craft\app\records\Volume as AssetVolumeRecord;
@@ -60,7 +59,7 @@ class Volumes extends Component
     const EVENT_BEFORE_DELETE_VOLUME = 'beforeDeleteVolume';
 
     /**
-     * @event EntryEvent The event that is triggered after a Asset volume is deleted.
+     * @event VolumeEvent The event that is triggered after a Asset volume is deleted.
      */
     const EVENT_AFTER_DELETE_VOLUME = 'afterDeleteVolume';
 
@@ -584,7 +583,7 @@ class Volumes extends Component
             $volume = $this->getVolumeById($volumeId);
 
             // Fire a 'beforeDeleteVolume' event
-            $event = new DeleteVolumeEvent([
+            $event = new VolumeEvent([
                 'volume' => $volume
             ]);
 
@@ -619,7 +618,9 @@ class Volumes extends Component
 
         if ($success) {
             // Fire an 'afterDeleteVolume' event
-            $this->trigger(self::EVENT_AFTER_DELETE_VOLUME, new DeleteVolumeEvent(['volume' => $volume]));
+            $this->trigger(self::EVENT_AFTER_DELETE_VOLUME, new VolumeEvent([
+                'volume' => $volume
+            ]));
         }
 
         return $success;
