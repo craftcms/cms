c9f3c232a2e4e63de589c9923157f43b7aa6d94e
Benjamin David <ben@dukt.net>
2016-10-06 10:26:58 -0700

    Fixed a bug where the table name for `usergroups_users` was wrong when requesting New Users data

--- src/controllers/ChartsController.php
+++ src/controllers/ChartsController.php
@@ -51,8 +51,8 @@ class ChartsController extends Controller
 
         if ($userGroupId)
         {
-            $query->innerJoin('usergroups_users userGroupUsers', 'userGroupUsers.userId = users.id');
-            $query->where('userGroupUsers.groupId = :userGroupId', array(':userGroupId' => $userGroupId));
+            $query->innerJoin('{{%usergroups_users}} usergroups_users', 'usergroups_users.userId = users.id');
+            $query->where('usergroups_users.groupId = :userGroupId', array(':userGroupId' => $userGroupId));
         }
 
         // Get the chart data table
