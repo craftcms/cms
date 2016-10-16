aafc65d4589031e2d89e0a7456076ec7549ff4ba
Brad Bell <brad@pixelandtonic.com>
2016-10-15 17:25:01 -0700

    Fix the schemaVersion not null migration so it works with Postgres.

--- src/migrations/m161006_205918_schemaVersion_not_null.php
+++ src/migrations/m161006_205918_schemaVersion_not_null.php
@@ -4,6 +4,7 @@ namespace craft\app\migrations;
 
 use Craft;
 use craft\app\db\Migration;
+use craft\app\services\Config;
 
 /**
  * m161006_205918_schemaVersion_not_null migration.
@@ -17,9 +18,18 @@ class m161006_205918_schemaVersion_not_null extends Migration
     {
         $this->update('{{%plugins}}', [
             'schemaVersion' => '1.0.0'
-        ], 'schemaVersion is null', [], false);
+        ], Craft::$app->getDb()->getSchema()->quoteColumnName('schemaVersion').' IS NULL', [], false);
 
-        $this->alterColumn('{{%plugins}}', 'schemaVersion', $this->string(15)->notNull());
+        $database = Craft::$app->getConfig()->get('driver', 'db');
+
+        // Two statements here because of a Yii 2/PostgreSQL bug:
+        // https://github.com/yiisoft/yii2/issues/12077
+        if ($database == 'mysql') {
+            $this->alterColumn('{{%plugins}}', 'schemaVersion', $this->string(15)->notNull());
+        } else if ($database == 'pgsql') {
+            $pluginTable = $this->db->getSchema()->defaultSchema.'.'.$this->db->getSchema()->getRawTableName('plugins');
+            $this->db->createCommand()->setSql('ALTER TABLE '.$pluginTable.' ALTER COLUMN "schemaVersion" TYPE varchar(15), ALTER COLUMN "schemaVersion" SET NOT NULL')->execute();
+        }
     }
 
     /**
