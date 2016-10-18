eccf7cc2bd29b8d432e38083a305eeffcf9fda22
Brad Bell <brad@pixelandtonic.com>
2016-10-18 15:44:19 -0700

    Get ChartHelper into Craft 3 code style.

--- src/helpers/ChartHelper.php
+++ src/helpers/ChartHelper.php
@@ -13,295 +13,273 @@ use craft\app\dates\DateTime;
 
 
 /**
- * Class Assets
+ * Class ChartHelper
  *
  * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
  * @since  3.0
  */
 class ChartHelper
 {
-	// Public Methods
-	// =========================================================================
+    // Public Methods
+    // =========================================================================
 
-	/**
-	 * Returns the data for a run chart, based on a given DB query, start/end dates, and the desired time interval unit.
-	 *
-	 * The query’s SELECT clause should already be set to a column aliased as `value`.
-	 *
-	 * The $options array can override the following defaults:
-	 *
-	 *  - `intervalUnit` - The time interval unit to use ('hour', 'day', 'month', or 'year').
-	 *                     By default, a unit will be decided automatically based on the start/end date duration.
-	 *  - `categoryLabel` - The label to use for the chart categories (times). Defaults to "Date".
-	 *  - `valueLabel` - The label to use for the chart values. Defaults to "Value".
-	 *  - `valueType` - The type of values that are being plotted ('number', 'currency', 'percent', 'time'). Defaults to 'number'.
-	 *
-	 * @param Query      $query        The DB query that should be used
-	 * @param DateTime   $startDate    The start of the time duration to select (inclusive)
-	 * @param DateTime   $endDate      The end of the time duratio to select (exclusive)
-	 * @param string     $dateColumn   The column that represents the date
-	 * @param array|null $options      Any customizations that should be made over the default options
-	 *
-	 * @return array
-	 */
-	public static function getRunChartDataFromQuery(Query $query, DateTime $startDate, DateTime $endDate, $dateColumn, $options = array())
-	{
-		// Setup
-		$options = array_merge(array(
-			'intervalUnit' => null,
-			'categoryLabel' => Craft::t('app', 'Date'),
-			'valueLabel' => Craft::t('app', 'Value'),
-			'valueType' => 'number',
-		), $options);
+    /**
+     * Returns the data for a run chart, based on a given DB query, start/end dates, and the desired time interval unit.
+     *
+     * The query’s SELECT clause should already be set to a column aliased as `value`.
+     *
+     * The $options array can override the following defaults:
+     *
+     *  - `intervalUnit` - The time interval unit to use ('hour', 'day', 'month', or 'year').
+     *                     By default, a unit will be decided automatically based on the start/end date duration.
+     *  - `categoryLabel` - The label to use for the chart categories (times). Defaults to "Date".
+     *  - `valueLabel` - The label to use for the chart values. Defaults to "Value".
+     *  - `valueType` - The type of values that are being plotted ('number', 'currency', 'percent', 'time'). Defaults to 'number'.
+     *
+     * @param Query      $query      The DB query that should be used
+     * @param DateTime   $startDate  The start of the time duration to select (inclusive)
+     * @param DateTime   $endDate    The end of the time duration to select (exclusive)
+     * @param string     $dateColumn The column that represents the date
+     * @param array|null $options    Any customizations that should be made over the default options
+     *
+     * @return array
+     */
+    public static function getRunChartDataFromQuery(Query $query, DateTime $startDate, DateTime $endDate, $dateColumn, $options = [])
+    {
+        // Setup
+        $options = array_merge([
+            'intervalUnit' => null,
+            'categoryLabel' => Craft::t('app', 'Date'),
+            'valueLabel' => Craft::t('app', 'Value'),
+            'valueType' => 'number',
+        ], $options);
 
-		$craftTimezone = new \DateTimeZone(Craft::$app->getTimeZone());
+        $craftTimezone = new \DateTimeZone(Craft::$app->getTimeZone());
 
-		if ($options['intervalUnit'] && in_array($options['intervalUnit'], array('year', 'month', 'day', 'hour')))
-		{
-			$intervalUnit = $options['intervalUnit'];
-		}
-		else
-		{
-			$intervalUnit = self::getRunChartIntervalUnit($startDate, $endDate);
-		}
+        if ($options['intervalUnit'] && in_array($options['intervalUnit'], ['year', 'month', 'day', 'hour'])) {
+            $intervalUnit = $options['intervalUnit'];
+        } else {
+            $intervalUnit = self::getRunChartIntervalUnit($startDate, $endDate);
+        }
 
-		switch ($intervalUnit)
-		{
-			case 'year':
-			{
-				$sqlDateFormat = '%Y-01-01';
-				$phpDateFormat = 'Y-01-01';
-				$sqlGroup = "YEAR({$dateColumn})";
-				$cursorDate = new DateTime($startDate->format('Y-01-01'), $craftTimezone);
-				break;
-			}
-			case 'month':
-			{
-				$sqlDateFormat = '%Y-%m-01';
-				$phpDateFormat = 'Y-m-01';
-				$sqlGroup = "YEAR({$dateColumn}), MONTH({$dateColumn})";
-				$cursorDate = new DateTime($startDate->format('Y-m-01'), $craftTimezone);
-				break;
-			}
-			case 'day':
-			{
-				$sqlDateFormat = '%Y-%m-%d';
-				$phpDateFormat = 'Y-m-d';
-				$sqlGroup = "YEAR({$dateColumn}), MONTH({$dateColumn}), DAY({$dateColumn})";
-				$cursorDate = new DateTime($startDate->format('Y-m-d'), $craftTimezone);
-				break;
-			}
-			case 'hour':
-			{
-				$sqlDateFormat = '%Y-%m-%d %H:00:00';
-				$phpDateFormat = 'Y-m-d H:00:00';
-				$sqlGroup = "YEAR({$dateColumn}), MONTH({$dateColumn}), DAY({$dateColumn}), HOUR({$dateColumn})";
-				$cursorDate = new DateTime($startDate->format('Y-m-d'), $craftTimezone);
-				break;
-			}
-		}
+        switch ($intervalUnit) {
+            case 'year': {
+                $sqlDateFormat = '%Y-01-01';
+                $phpDateFormat = 'Y-01-01';
+                $sqlGroup = "YEAR({$dateColumn})";
+                $cursorDate = new DateTime($startDate->format('Y-01-01'), $craftTimezone);
+                break;
+            }
+            case 'month': {
+                $sqlDateFormat = '%Y-%m-01';
+                $phpDateFormat = 'Y-m-01';
+                $sqlGroup = "YEAR({$dateColumn}), MONTH({$dateColumn})";
+                $cursorDate = new DateTime($startDate->format('Y-m-01'), $craftTimezone);
+                break;
+            }
+            case 'day': {
+                $sqlDateFormat = '%Y-%m-%d';
+                $phpDateFormat = 'Y-m-d';
+                $sqlGroup = "YEAR({$dateColumn}), MONTH({$dateColumn}), DAY({$dateColumn})";
+                $cursorDate = new DateTime($startDate->format('Y-m-d'), $craftTimezone);
+                break;
+            }
+            case 'hour': {
+                $sqlDateFormat = '%Y-%m-%d %H:00:00';
+                $phpDateFormat = 'Y-m-d H:00:00';
+                $sqlGroup = "YEAR({$dateColumn}), MONTH({$dateColumn}), DAY({$dateColumn}), HOUR({$dateColumn})";
+                $cursorDate = new DateTime($startDate->format('Y-m-d'), $craftTimezone);
+                break;
+            }
+        }
 
-		// Execute the query
-		$results = $query
-			->addSelect(["DATE_FORMAT({$dateColumn},'{$sqlDateFormat}') as date"])
-			->andWhere(
-				array('and', $dateColumn.' >= :startDate', $dateColumn.' < :endDate'),
-				array(':startDate' => $startDate->mySqlDateTime(), ':endDate' => $endDate->mySqlDateTime()))
-			->groupBy($sqlGroup)
-			->orderBy($dateColumn.' asc')
-			->all();
+        // Execute the query
+        $results = $query
+            ->addSelect(["DATE_FORMAT({$dateColumn},'{$sqlDateFormat}') as date"])
+            ->andWhere(
+                ['and', $dateColumn.' >= :startDate', $dateColumn.' < :endDate'],
+                [':startDate' => $startDate->mySqlDateTime(), ':endDate' => $endDate->mySqlDateTime()])
+            ->groupBy($sqlGroup)
+            ->orderBy($dateColumn.' asc')
+            ->all();
 
-		// Assemble the data
-		$rows = array();
+        // Assemble the data
+        $rows = [];
 
-		$endTimestamp = $endDate->getTimestamp();
+        $endTimestamp = $endDate->getTimestamp();
 
-		while ($cursorDate->getTimestamp() < $endTimestamp)
-		{
-			// Do we have a record for this date?
-			// $formattedCursorDate = $cursorDate->format($phpDateFormat, $craftTimezone);
-			$formattedCursorDate = $cursorDate->format($phpDateFormat);
+        while ($cursorDate->getTimestamp() < $endTimestamp) {
+            // Do we have a record for this date?
+            // $formattedCursorDate = $cursorDate->format($phpDateFormat, $craftTimezone);
+            $formattedCursorDate = $cursorDate->format($phpDateFormat);
 
-			if (isset($results[0]) && $results[0]['date'] == $formattedCursorDate)
-			{
-				$value = (float) $results[0]['value'];
-				array_shift($results);
-			}
-			else
-			{
-				$value = 0;
-			}
+            if (isset($results[0]) && $results[0]['date'] == $formattedCursorDate) {
+                $value = (float)$results[0]['value'];
+                array_shift($results);
+            } else {
+                $value = 0;
+            }
 
-			$rows[] = array($formattedCursorDate, $value);
-			$cursorDate->modify('+1 '.$intervalUnit);
-		}
+            $rows[] = [$formattedCursorDate, $value];
+            $cursorDate->modify('+1 '.$intervalUnit);
+        }
 
-		return array(
-			'columns' => array(
-				array(
-					'type' => ($intervalUnit == 'hour' ? 'datetime' : 'date'),
-					'label' => $options['categoryLabel']
-				),
-				array(
-					'type' => $options['valueType'],
-					'label' => $options['valueLabel']
-				)
-			),
-			'rows' => $rows,
-		);
-	}
+        return [
+            'columns' => [
+                [
+                    'type' => ($intervalUnit == 'hour' ? 'datetime' : 'date'),
+                    'label' => $options['categoryLabel']
+                ],
+                [
+                    'type' => $options['valueType'],
+                    'label' => $options['valueLabel']
+                ]
+            ],
+            'rows' => $rows,
+        ];
+    }
 
-	/**
-	 * Returns the interval unit that should be used in a run chart, based on the given start and end dates.
-	 *
-	 * @param DateTime $startDate
-	 * @param DateTime $endDate
-	 *
-	 * @return string The unit that the chart should use ('hour', 'day', 'month', or 'year')
-	 */
-	public static function getRunChartIntervalUnit(DateTime $startDate, DateTime $endDate)
-	{
-		// Get the total number of days between the two dates
-		$days = floor(($endDate->getTimestamp() - $startDate->getTimestamp()) / 86400);
+    /**
+     * Returns the interval unit that should be used in a run chart, based on the given start and end dates.
+     *
+     * @param DateTime $startDate
+     * @param DateTime $endDate
+     *
+     * @return string The unit that the chart should use ('hour', 'day', 'month', or 'year')
+     */
+    public static function getRunChartIntervalUnit(DateTime $startDate, DateTime $endDate)
+    {
+        // Get the total number of days between the two dates
+        $days = floor(($endDate->getTimestamp() - $startDate->getTimestamp()) / 86400);
 
-		if ($days >= 730)
-		{
-			return 'year';
-		}
+        if ($days >= 730) {
+            return 'year';
+        }
 
-		if ($days >= 60)
-		{
-			return 'month';
-		}
+        if ($days >= 60) {
+            return 'month';
+        }
 
-		if ($days >= 2)
-		{
-			return 'day';
-		}
+        if ($days >= 2) {
+            return 'day';
+        }
 
-		return 'hour';
-	}
+        return 'hour';
+    }
 
-	/**
-	 * Returns the short date, decimal, percent and currency D3 formats based on Craft's locale settings
-	 *
-	 * @return array
-	 */
-	public static function getFormats()
-	{
-		return array(
-			'shortDateFormats' => self::getShortDateFormats(),
-			'decimalFormat' => self::getDecimalFormat(),
-			'percentFormat' => self::getPercentFormat(),
-			'currencyFormat' => self::getCurrencyFormat(),
-		);
-	}
+    /**
+     * Returns the short date, decimal, percent and currency D3 formats based on Craft's locale settings
+     *
+     * @return array
+     */
+    public static function getFormats()
+    {
+        return [
+            'shortDateFormats' => self::getShortDateFormats(),
+            'decimalFormat' => self::getDecimalFormat(),
+            'percentFormat' => self::getPercentFormat(),
+            'currencyFormat' => self::getCurrencyFormat(),
+        ];
+    }
 
-	/**
-	 * Returns the D3 short date formats based on Yii's short date format
-	 *
-	 * @return array
-	 */
-	public static function getShortDateFormats()
-	{
-		$format = Craft::$app->getLocale()->getDateFormat('short');
+    /**
+     * Returns the D3 short date formats based on Yii's short date format
+     *
+     * @return array
+     */
+    public static function getShortDateFormats()
+    {
+        $format = Craft::$app->getLocale()->getDateFormat('short');
 
-		// Some of these are RTL versions
-		$removals = array(
-			'day' => array('y'),
-			'month' => array('d', 'd‏'),
-			'year' => array('d', 'd‏', 'm', 'M‏'),
-		);
+        // Some of these are RTL versions
+        $removals = [
+            'day' => ['y'],
+            'month' => ['d', 'd‏'],
+            'year' => ['d', 'd‏', 'm', 'M‏'],
+        ];
 
-		$shortDateFormats = array();
+        $shortDateFormats = [];
 
-		foreach($removals as $unit => $chars)
-		{
-			$shortDateFormats[$unit] = $format;
+        foreach ($removals as $unit => $chars) {
+            $shortDateFormats[$unit] = $format;
 
-			foreach($chars as $char)
-			{
-				$shortDateFormats[$unit] = preg_replace("/(^[{$char}]+\W+|\W+[{$char}]+)/iu", '', $shortDateFormats[$unit]);
-			}
-		}
+            foreach ($chars as $char) {
+                $shortDateFormats[$unit] = preg_replace("/(^[{$char}]+\W+|\W+[{$char}]+)/iu", '', $shortDateFormats[$unit]);
+            }
+        }
 
 
-		// yii formats to d3 formats
+        // yii formats to d3 formats
 
-		$yiiToD3Formats = array(
-			'day' => array('dd' => '%-d','d' => '%-d'),
-			'month' => array('MM' => '%-m','M' => '%-m'),
-			'year' => array('yyyy' => '%Y','yy' => '%y','y' => '%y')
-		);
+        $yiiToD3Formats = [
+            'day' => ['dd' => '%-d', 'd' => '%-d'],
+            'month' => ['MM' => '%-m', 'M' => '%-m'],
+            'year' => ['yyyy' => '%Y', 'yy' => '%y', 'y' => '%y']
+        ];
 
-		foreach($shortDateFormats as $unit => $format)
-		{
-			foreach($yiiToD3Formats as $_unit => $_formats)
-			{
-				foreach($_formats as $yiiFormat => $d3Format)
-				{
-					$pattern = "/({$yiiFormat})/i";
+        foreach ($shortDateFormats as $unit => $format) {
+            foreach ($yiiToD3Formats as $_unit => $_formats) {
+                foreach ($_formats as $yiiFormat => $d3Format) {
+                    $pattern = "/({$yiiFormat})/i";
 
-					preg_match($pattern, $shortDateFormats[$unit], $matches);
+                    preg_match($pattern, $shortDateFormats[$unit], $matches);
 
-					if(count($matches) > 0)
-					{
-						$shortDateFormats[$unit] = preg_replace($pattern, $d3Format, $shortDateFormats[$unit]);
+                    if (count($matches) > 0) {
+                        $shortDateFormats[$unit] = preg_replace($pattern, $d3Format, $shortDateFormats[$unit]);
 
-						break;
-					}
+                        break;
+                    }
+                }
+            }
+        }
 
-				}
-			}
-		}
+        return $shortDateFormats;
+    }
 
-		return $shortDateFormats;
-	}
+    /**
+     * Returns the decimal format for D3
+     *
+     * @return string
+     */
+    public static function getDecimalFormat()
+    {
+        return ',.3f';
+    }
 
-	/**
-	 * Returns the decimal format for D3
-	 *
-	 * @return string
-	 */
-	public static function getDecimalFormat()
-	{
-		return ',.3f';
-	}
+    /**
+     * Returns the percent format for D3
+     *
+     * @return string
+     */
+    public static function getPercentFormat()
+    {
+        return ',.2%';
+    }
 
-	/**
-	 * Returns the percent format for D3
-	 *
-	 * @return string
-	 */
-	public static function getPercentFormat()
-	{
-		return ',.2%';
-	}
+    /**
+     * Returns the currency format for D3
+     *
+     * @return string
+     */
+    public static function getCurrencyFormat()
+    {
+        return '$,.2f';
+    }
 
-	/**
-	 * Returns the currency format for D3
-	 *
-	 * @return string
-	 */
-	public static function getCurrencyFormat()
-	{
-		return '$,.2f';
-	}
+    /**
+     * Returns the predefined date ranges with their label, start date and end date.
+     *
+     * @return array
+     */
+    public static function getDateRanges()
+    {
+        $dateRanges = [
+            'd7' => ['label' => Craft::t('app', 'Last 7 days'), 'startDate' => '-7 days', 'endDate' => null],
+            'd30' => ['label' => Craft::t('app', 'Last 30 days'), 'startDate' => '-30 days', 'endDate' => null],
+            'lastweek' => ['label' => Craft::t('app', 'Last Week'), 'startDate' => '-2 weeks', 'endDate' => '-1 week'],
+            'lastmonth' => ['label' => Craft::t('app', 'Last Month'), 'startDate' => '-2 months', 'endDate' => '-1 month'],
+        ];
 
-	/**
-	 * Returns the predefined date ranges with their label, start date and end date.
-	 *
-	 * @return array
-	 */
-	public static function getDateRanges()
-	{
-		$dateRanges = array(
-			'd7' => array('label' => Craft::t('app', 'Last 7 days'), 'startDate' => '-7 days', 'endDate' => null),
-			'd30' => array('label' => Craft::t('app', 'Last 30 days'), 'startDate' => '-30 days', 'endDate' => null),
-			'lastweek' => array('label' => Craft::t('app', 'Last Week'), 'startDate' => '-2 weeks', 'endDate' => '-1 week'),
-			'lastmonth' => array('label' => Craft::t('app', 'Last Month'), 'startDate' => '-2 months', 'endDate' => '-1 month'),
-		);
-
-		return $dateRanges;
-	}
+        return $dateRanges;
+    }
 }
