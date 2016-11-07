e9cab50b5e911ccddfab37e7922e852efef4b439
Brandon Kelly <brandon@pixelandtonic.com>
2016-11-07 05:25:49 -0800

    include autoload.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
 defined('CRAFT_BASE_PATH') || define('CRAFT_BASE_PATH', dirname(__DIR__).'/craft');
-
+require dirname(__DIR__).'/vendor/autoload.php';
 $app = require dirname(__DIR__).'/vendor/craftcms/craft/bootstrap/web.php';
 $app->run();
