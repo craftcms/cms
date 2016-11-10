6690466deec12b36833c1b7d4a95760564e25ce4
Brad Bell <brad@pixelandtonic.com>
2016-11-09 21:15:35 -0800

    Fix that last templatecache index shuffle migration so it actually runs this time.

--- src/migrations/m161109_000000_index_shuffle.php
+++ src/migrations/m161109_000000_index_shuffle.php
@@ -4,7 +4,7 @@ namespace Craft;
 /**
  * The class name is the UTC timestamp in the format of mYYMMDD_HHMMSS_migrationName
  */
-class m1601026_000000_index_shuffle extends BaseMigration
+class m161109_000000_index_shuffle extends BaseMigration
 {
 	/**
 	 * Any migration code in here is wrapped inside of a transaction.
@@ -15,7 +15,7 @@ class m1601026_000000_index_shuffle extends BaseMigration
 	{
 		// Order is important.
 		Craft::log('Dropping `expiryDate,cacheKey,locale,path` index on the templatecaches table.', LogLevel::Info, true);
-		$this->dropIndex('templatecaches', 'expiryDate,cacheKey,locale,path');
+		MigrationHelper::dropIndexIfExists('templatecaches', array('expiryDate', 'cacheKey', 'locale', 'path'));
 
 		Craft::log('Creating `locale,cacheKey,path,expiryDate` index on the templatecaches table.', LogLevel::Info, true);
 		$this->createIndex('templatecaches', 'locale,cacheKey,path,expiryDate');
