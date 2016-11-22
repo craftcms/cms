cb5386dc5ce7d8ac5b1a921076b4a0be3ad09be1
brandonkelly <brandon@pixelandtonic.com>
2016-11-22 13:09:45 -0800

    Drop the PHP 5.6 check from bootstarp/web
    Moved the check to craft/app/index.php in the craftcms.com zip packager. And Composer is already enforcing PHP >= 5.6 for us, for people installing with that.

--- bootstrap/web.php
+++ bootstrap/web.php
@@ -7,13 +7,6 @@
  * @license   https://craftcms.com/license
  */
 
-// Make sure this is PHP 5.6 or later
-// -----------------------------------------------------------------------------
-
-if (!defined('PHP_VERSION_ID') || PHP_VERSION_ID < 50600) {
-    exit('Craft CMS requires PHP 5.6 or later, but you&rsquo;re running '.PHP_VERSION.'. Please talk to your host/IT department about upgrading PHP or your server.');
-}
-
 // Check for this early because Craft uses it before the requirements checker gets a chance to run.
 if (!extension_loaded('mbstring') || (extension_loaded('mbstring') && ini_get('mbstring.func_overload') != 0)) {
     exit('Craft CMS requires the <a href="http://php.net/manual/en/book.mbstring.php" target="_blank">PHP multibyte string</a> extension in order to run. Please talk to your host/IT department about enabling it on your server.');
