74a185fe60ed33980e36c453b63de0e1e0bbf1ca
brandonkelly <brandon@pixelandtonic.com>
2016-11-25 07:10:59 -0800

    Avoid that “missing craft\app\web\UrlRule” error

---
+++ src/migrations/m161125_150752_clear_urlmanager_cache.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace craft\migrations;
+
+use Craft;
+use craft\db\Migration;
+
+/**
+ * m161125_150752_clear_urlmanager_cache migration.
+ */
+class m161125_150752_clear_urlmanager_cache extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        Craft::$app->getCache()->delete('yii\web\UrlManager');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m161125_150752_clear_urlmanager_cache cannot be reverted.\n";
+        return false;
+    }
+}
