5e5f7424b8ebde16561f08a1358a96bdca87ef57
Brad Bell <brad@pixelandtonic.com>
2016-12-12 19:53:19 -0800

    Get rid of SimplePie in favor of ZendFeed and use our own Guzzle PSR-7 client.

---
+++ src/base/GuzzleClient.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @link      https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license   https://craftcms.com/license
+ */
+
+namespace craft\base;
+
+use Craft;
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface as GuzzleClientInterface;
+use Zend\Feed\Reader\Http\ClientInterface as FeedReaderHttpClientInterface;
+use Zend\Feed\Reader\Http\Psr7ResponseDecorator;
+
+/**
+ * Craft PSR-7 Guzzle client.
+ *
+ * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @since  3.0
+ */
+class GuzzleClient implements FeedReaderHttpClientInterface
+{
+    /**
+     * @var GuzzleClientInterface
+     */
+    private $_client;
+
+    /**
+     * @param GuzzleClientInterface|null $client
+     */
+    public function __construct(GuzzleClientInterface $client = null)
+    {
+        $this->_client = $client ?: new Client([
+            'headers' => [
+                'User-Agent' => 'Craft/'.Craft::$app->version.' '.\GuzzleHttp\default_user_agent()
+            ],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get($uri)
+    {
+        return new Psr7ResponseDecorator(
+            $this->_client->request('GET', $uri)
+        );
+    }
+}
