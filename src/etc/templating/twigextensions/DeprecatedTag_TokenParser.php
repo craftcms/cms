feba03e45549edd08925b57ce115a66bd55295f0
brandonkelly <brandon@pixelandtonic.com>
2016-12-27 07:46:26 -0800

    Removed constructor @return

--- src/etc/templating/twigextensions/DeprecatedTag_TokenParser.php
+++ src/etc/templating/twigextensions/DeprecatedTag_TokenParser.php
@@ -28,8 +28,6 @@ class DeprecatedTag_TokenParser extends \Twig_TokenParser
 	 * Constructor
 	 *
 	 * @param string $tag
-	 *
-	 * @return DeprecatedTag_TokenParser
 	 */
 	public function __construct($tag)
 	{
