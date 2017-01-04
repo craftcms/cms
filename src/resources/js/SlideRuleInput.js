9bc5dc9a5a38483d344e798ed03f73fdc3ef98fa
andris-sevcenko <asevcenko@gmail.com>
2017-01-04 11:50:20 +0200

    Add setValue to Slide Rule.

--- src/resources/js/SlideRuleInput.js
+++ src/resources/js/SlideRuleInput.js
@@ -1,183 +1,189 @@
 SlideRuleInput = Garnish.Base.extend({
 
-	$container: null,
-	$options: null,
-	$selectedOption: null,
-	$input: null,
-	value: null,
+    $container: null,
+    $options: null,
+    $selectedOption: null,
+    $input: null,
+    value: null,
 
-	startPositionX: null,
+    startPositionX: null,
 
-	init: function(id, settings)
-	{
-		this.setSettings(settings, SlideRuleInput.defaultSettings);
+    init: function(id, settings)
+    {
+        this.setSettings(settings, SlideRuleInput.defaultSettings);
 
-		this.value = 0;
-		this.graduationsMin = -70;
-		this.graduationsMax = 70;
-		this.slideMin = -45;
-		this.slideMax = 45;
+        this.value = 0;
+        this.graduationsMin = -70;
+        this.graduationsMax = 70;
+        this.slideMin = -45;
+        this.slideMax = 45;
 
-		this.$container = $('#'+id);
-		this.$overlay = $('<div class="overlay"></div>').appendTo(this.$container);
-		this.$cursor = $('<div class="cursor"></div>').appendTo(this.$container);
-		this.$graduations = $('<div class="graduations"></div>').appendTo(this.$container);
-		this.$graduationsUl = $('<ul></ul>').appendTo(this.$graduations);
+        this.$container = $('#'+id);
+        this.$overlay = $('<div class="overlay"></div>').appendTo(this.$container);
+        this.$cursor = $('<div class="cursor"></div>').appendTo(this.$container);
+        this.$graduations = $('<div class="graduations"></div>').appendTo(this.$container);
+        this.$graduationsUl = $('<ul></ul>').appendTo(this.$graduations);
 
-		for(var i = this.graduationsMin; i <= this.graduationsMax; i++)
-		{
-			var $li = $('<li class="graduation" data-graduation="'+i+'"><div class="label">'+i+'</div></li>').appendTo(this.$graduationsUl);
+        for(var i = this.graduationsMin; i <= this.graduationsMax; i++)
+        {
+            var $li = $('<li class="graduation" data-graduation="'+i+'"><div class="label">'+i+'</div></li>').appendTo(this.$graduationsUl);
 
-			if((i % 5) == 0)
-			{
-				$li.addClass('main-graduation');
-			}
+            if((i % 5) == 0)
+            {
+                $li.addClass('main-graduation');
+            }
 
-			if(i == 0)
-			{
-				$li.addClass('selected');
-			}
-		}
+            if(i == 0)
+            {
+                $li.addClass('selected');
+            }
+        }
 
-		this.$options = this.$container.find('.graduation');
+        this.$options = this.$container.find('.graduation');
 
-		this.addListener(this.$container, 'resize',  $.proxy(this, '_handleResize'));
-		this.addListener(this.$container, 'tapstart',  $.proxy(this, '_handleTapStart'));
-		this.addListener(Garnish.$bod, 'tapmove', $.proxy(this, '_handleTapMove'));
-		this.addListener(Garnish.$bod, 'tapend', $.proxy(this, '_handleTapEnd'));
+        this.addListener(this.$container, 'resize',  $.proxy(this, '_handleResize'));
+        this.addListener(this.$container, 'tapstart',  $.proxy(this, '_handleTapStart'));
+        this.addListener(Garnish.$bod, 'tapmove', $.proxy(this, '_handleTapMove'));
+        this.addListener(Garnish.$bod, 'tapend', $.proxy(this, '_handleTapEnd'));
 
-		// Set to zero
+        // Set to zero
 
-		// this.setValue(0);
+        // this.setValue(0);
 
-		setTimeout($.proxy(function()
-		{
-			// (n -1) options because the border is placed on the left of the 10px box
-			this.graduationsCalculatedWidth = (this.$options.length - 1) * 10;
-			this.$graduationsUl.css('left', (-this.graduationsCalculatedWidth / 2) + this.$container.width() / 2);
-		}, this), 50);
-	},
+        setTimeout($.proxy(function()
+        {
+            // (n -1) options because the border is placed on the left of the 10px box
+            this.graduationsCalculatedWidth = (this.$options.length - 1) * 10;
+            this.$graduationsUl.css('left', (-this.graduationsCalculatedWidth / 2) + this.$container.width() / 2);
+        }, this), 50);
+    },
 
-	_handleResize: function()
-	{
-		var left = this.valueToPosition(this.value);
-		this.$graduationsUl.css('left', left);
-	},
+    _handleResize: function()
+    {
+        var left = this.valueToPosition(this.value);
+        this.$graduationsUl.css('left', left);
+    },
 
-	_handleTapStart: function(ev, touch)
-	{
-		ev.preventDefault();
+    _handleTapStart: function(ev, touch)
+    {
+        ev.preventDefault();
 
-		this.startPositionX = touch.position.x;
-		this.startLeft = this.$graduationsUl.position().left;
+        this.startPositionX = touch.position.x;
+        this.startLeft = this.$graduationsUl.position().left;
 
-		this.dragging = true;
-		this.onStart();
-	},
+        this.dragging = true;
+        this.onStart();
+    },
 
-	_handleTapMove: function(ev, touch)
-	{
-		if(this.dragging)
-		{
-			ev.preventDefault();
+    _handleTapMove: function(ev, touch)
+    {
+        if(this.dragging)
+        {
+            ev.preventDefault();
 
-			var curX = this.startPositionX - touch.position.x;
-			var left = this.startLeft - curX;
-			var value = this.positionToValue(left);
+            var curX = this.startPositionX - touch.position.x;
+            var left = this.startLeft - curX;
+            var value = this.positionToValue(left);
 
-			if(value < this.slideMin)
-			{
-				value = this.slideMin;
-				left = this.valueToPosition(value);
+            this.setValue(value);
 
-			}
-			else if(value > this.slideMax)
-			{
-				value = this.slideMax;
-				left = this.valueToPosition(value);
-			}
+            this.onChange();
+        }
+    },
 
-			this.$graduationsUl.css('left', left);
+    setValue: function (value) {
+        var left = this.valueToPosition(value);
+        if(value < this.slideMin)
+        {
+            value = this.slideMin;
+            left = this.valueToPosition(value);
 
-			if(value >= this.slideMin && value <= this.slideMax)
-			{
-				this.$options.removeClass('selected');
+        }
+        else if(value > this.slideMax)
+        {
+            value = this.slideMax;
+            left = this.valueToPosition(value);
+        }
 
-				$.each(this.$options, function(key, option) {
-					if($(option).data('graduation') > 0 )
-					{
-						if($(option).data('graduation') <= value)
-						{
-							$(option).addClass('selected');
-						}
-					}
-					if($(option).data('graduation') < 0 )
-					{
-						if($(option).data('graduation') >= value)
-						{
-							$(option).addClass('selected');
-						}
-					}
+        this.$graduationsUl.css('left', left);
 
-					if($(option).data('graduation') == 0)
-					{
-						$(option).addClass('selected');
-					}
-				});
-			}
+        if(value >= this.slideMin && value <= this.slideMax)
+        {
+            this.$options.removeClass('selected');
 
-			this.value = value;
-			this.onChange();
-		}
-	},
+            $.each(this.$options, function(key, option) {
+                if($(option).data('graduation') > 0 )
+                {
+                    if($(option).data('graduation') <= value)
+                    {
+                        $(option).addClass('selected');
+                    }
+                }
+                if($(option).data('graduation') < 0 )
+                {
+                    if($(option).data('graduation') >= value)
+                    {
+                        $(option).addClass('selected');
+                    }
+                }
 
-	_handleTapEnd: function(ev, touch)
-	{
-		if(this.dragging)
-		{
-			ev.preventDefault();
-			this.dragging = false;
-			this.onEnd();
-		}
-	},
+                if($(option).data('graduation') == 0)
+                {
+                    $(option).addClass('selected');
+                }
+            });
+        }
 
-	positionToValue: function (position)
-	{
-		var scaleMin = (this.graduationsMin * -1);
-		var scaleMax = (this.graduationsMin - this.graduationsMax) * -1;
+        this.value = value;
+    },
 
-		return (( ( this.$graduations.width() / 2 ) + (position * -1) ) / this.graduationsCalculatedWidth) * scaleMax - scaleMin;
-	},
+    _handleTapEnd: function(ev, touch)
+    {
+        if(this.dragging)
+        {
+            ev.preventDefault();
+            this.dragging = false;
+            this.onEnd();
+        }
+    },
 
-	valueToPosition: function(value)
-	{
-		var scaleMin = (this.graduationsMin * -1);
-		var scaleMax = (this.graduationsMin - this.graduationsMax) * -1;
+    positionToValue: function (position)
+    {
+        var scaleMin = (this.graduationsMin * -1);
+        var scaleMax = (this.graduationsMin - this.graduationsMax) * -1;
 
-		return -((value + scaleMin) * this.graduationsCalculatedWidth / scaleMax - this.$graduations.width() / 2);
-	},
+        return (( ( this.$graduations.width() / 2 ) + (position * -1) ) / this.graduationsCalculatedWidth) * scaleMax - scaleMin;
+    },
 
-	onStart: function () {
-		if (typeof this.settings.onChange == "function") {
-			this.settings.onStart(this);
-		}
-	},
+    valueToPosition: function(value)
+    {
+        var scaleMin = (this.graduationsMin * -1);
+        var scaleMax = (this.graduationsMin - this.graduationsMax) * -1;
 
-	onChange: function () {
-		if (typeof this.settings.onChange == "function") {
-			this.settings.onChange(this);
-		}
-	},
+        return -((value + scaleMin) * this.graduationsCalculatedWidth / scaleMax - this.$graduations.width() / 2);
+    },
 
-	onEnd: function () {
-		if (typeof this.settings.onChange == "function") {
-			this.settings.onEnd(this);
-		}
-	},
+    onStart: function () {
+        if (typeof this.settings.onChange == "function") {
+            this.settings.onStart(this);
+        }
+    },
 
-	defaultSettings: {
-		onStart: $.noop,
-		onChange: $.noop,
-		onEnd: $.noop
-	}
+    onChange: function () {
+        if (typeof this.settings.onChange == "function") {
+            this.settings.onChange(this);
+        }
+    },
+
+    onEnd: function () {
+        if (typeof this.settings.onChange == "function") {
+            this.settings.onEnd(this);
+        }
+    },
+
+    defaultSettings: {
+        onStart: $.noop,
+        onChange: $.noop,
+        onEnd: $.noop
+    }
 });
