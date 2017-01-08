92673a1a4db78d3076df12ad52f804c2ae79fef2
brandonkelly <brandon@pixelandtonic.com>
2017-01-08 08:15:28 -0800

    @inheritdoc

--- src/helpers/ArrayHelper.php
+++ src/helpers/ArrayHelper.php
@@ -19,43 +19,7 @@ class ArrayHelper extends \yii\helpers\ArrayHelper
     // =========================================================================
 
     /**
-     * Converts an object, an array of objects, or a comma-delimited string into an array.
-     *
-     *     ArrayHelper::toArray('one, two, three') => ['one', 'two', 'three']
-     *
-     * @param array|object|string $object     The object, array or string to be converted into an array.
-     * @param array               $properties A mapping from object class names to the properties that need to put into
-     *                                        the resulting arrays. The properties specified for each class is an array
-     *                                        of the following format:
-     *
-     * ~~~
-     * [
-     *     'app\models\Post' => [
-     *         'id',
-     *         'title',
-     *         // the key name in array result => property name
-     *         'createTime' => 'created_at',
-     *         // the key name in array result => anonymous function
-     *         'length' => function ($post) {
-     *             return strlen($post->content);
-     *         },
-     *     ],
-     * ]
-     * ~~~
-     *
-     * The result of `ArrayHelper::toArray($post, $properties)` could be like the following:
-     *
-     * ~~~
-     * [
-     *     'id' => 123,
-     *     'title' => 'test',
-     *     'createTime' => '2013-01-01 12:00AM',
-     *     'length' => 301,
-     * ]
-     * ~~~
-     * @param bool                $recursive  Whether to recursively converts properties which are objects into arrays.
-     *
-     * @return array The array representation of the given object.
+     * @inheritdoc
      */
     public static function toArray($object, $properties = [], $recursive = true): array
     {
