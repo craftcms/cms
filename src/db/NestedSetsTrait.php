903d58c77ca7a3058bb555792ee0472dc4d00cbd
brandonkelly <brandon@pixelandtonic.com>
2017-01-09 16:25:55 -0800

    YiiActiveRecord

--- src/db/NestedSetsTrait.php
+++ src/db/NestedSetsTrait.php
@@ -8,26 +8,25 @@
 namespace craft\db;
 
 use yii\db\ActiveQuery;
-
-/** @noinspection PhpUnnecessaryFullyQualifiedNameInspection */
+use yii\db\ActiveRecord as YiiActiveRecord;
 
 /**
  * Trait NestedSetsTrait.
  *
- * @method bool       makeRoot(bool $runValidation = true, array $attributes = null)
- * @method bool       prependTo(\yii\db\ActiveRecord $node, bool $runValidation = true, array $attributes = null)
- * @method bool       appendTo(\yii\db\ActiveRecord $node, bool $runValidation = true, array $attributes = null)
- * @method bool       insertBefore(\yii\db\ActiveRecord $node, bool $runValidation = true, array $attributes = null)
- * @method bool       insertAfter(\yii\db\ActiveRecord $node, bool $runValidation = true, array $attributes = null)
- * @method int|false deleteWithChildren()
- * @method ActiveQuery   parents(int $depth = null)
- * @method ActiveQuery   children(int $depth = null)
- * @method ActiveQuery   leaves()
- * @method ActiveQuery   prev()
- * @method ActiveQuery   next()
- * @method bool       isRoot()
- * @method bool       isChildOf(\yii\db\ActiveRecord $node)
- * @method bool       isLeaf()
+ * @method bool        makeRoot(bool $runValidation = true, array $attributes = null)
+ * @method bool        prependTo(YiiActiveRecord $node, bool $runValidation = true, array $attributes = null)
+ * @method bool        appendTo(YiiActiveRecord $node, bool $runValidation = true, array $attributes = null)
+ * @method bool        insertBefore(YiiActiveRecord $node, bool $runValidation = true, array $attributes = null)
+ * @method bool        insertAfter(YiiActiveRecord $node, bool $runValidation = true, array $attributes = null)
+ * @method int|false   deleteWithChildren()
+ * @method ActiveQuery parents(int $depth = null)
+ * @method ActiveQuery children(int $depth = null)
+ * @method ActiveQuery leaves()
+ * @method ActiveQuery prev()
+ * @method ActiveQuery next()
+ * @method bool        isRoot()
+ * @method bool        isChildOf(YiiActiveRecord $node)
+ * @method bool        isLeaf()
  *
  * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
  * @since  3.0
