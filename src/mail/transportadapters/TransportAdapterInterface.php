f6d1775b29aa1b0cf1189224bc06142a3190122c
brandonkelly <brandon@pixelandtonic.com>
2017-01-16 14:07:03 -0800

    Not true anymore

--- src/mail/transportadapters/TransportAdapterInterface.php
+++ src/mail/transportadapters/TransportAdapterInterface.php
@@ -23,9 +23,6 @@ interface TransportAdapterInterface extends SavableComponentInterface
     /**
      * Returns the config array that should be passed to [[\craft\mail\Mailer::setTransport()]].
      *
-     * Note that the return value will be stored in the database as JSON,
-     * so you cannot create an actual \Swift_Transport object here.
-     *
      * @return array The transport config
      */
     public function getTransportConfig(): array;
