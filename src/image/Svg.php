9f8f01fad28be7209d1c733c0a4b7b62eba06658
brandonkelly <brandon@pixelandtonic.com>
2017-01-19 16:39:55 -0800

    Thought I fixed this :-/

--- src/image/Svg.php
+++ src/image/Svg.php
@@ -170,7 +170,7 @@ class Svg extends Image
     /**
      * @inheritdoc
      */
-    public function scaleAndCrop(int $targetWidth, int $targetHeight = null, bool $scaleIfSmaller = true, string $cropPositions = 'center-center')
+    public function scaleAndCrop(int $targetWidth = null, int $targetHeight = null, bool $scaleIfSmaller = true, string $cropPositions = 'center-center')
     {
         $this->normalizeDimensions($targetWidth, $targetHeight);
 
