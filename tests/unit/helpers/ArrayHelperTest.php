423e8b87c9cb26c29bfc0bb48907a7eec4a5d876
Brad Bell <brad@pixelandtonic.com>
2017-01-25 14:22:46 -0800

    Scrutinizer

--- tests/unit/helpers/ArrayHelperTest.php
+++ tests/unit/helpers/ArrayHelperTest.php
@@ -21,18 +21,18 @@ class ArrayHelperTest extends \Codeception\TestCase\Test
     public function testToArray()
     {
         $this->assertEquals([], ArrayHelper::toArray(null));
-        $this->assertEquals([1,1,1,2,2,2,3,3,3], ArrayHelper::toArray("1,1,1,2,2,2,3,3,3"));
+        $this->assertEquals([1, 1, 1, 2, 2, 2, 3, 3, 3], ArrayHelper::toArray('1,1,1,2,2,2,3,3,3'));
     }
 
     public function testPrependOrAppend()
     {
-        $array = [1,2,3];
-        ArrayHelper::prependOrAppend($array,4,false);
-        $this->assertSame([1,2,3,4],$array);
+        $array = [1, 2, 3];
+        ArrayHelper::prependOrAppend($array, 4, false);
+        $this->assertSame([1, 2, 3, 4], $array);
 
-        $array = [1,2,3];
-        ArrayHelper::prependOrAppend($array,4,true);
-        $this->assertSame([4,1,2,3], $array);
+        $array = [1, 2, 3];
+        ArrayHelper::prependOrAppend($array, 4, true);
+        $this->assertSame([4, 1, 2, 3], $array);
 
     }
 }
