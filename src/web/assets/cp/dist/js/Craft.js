dca62601045411de39588f06fccfb7ebaffb3127
Benjamin David <ben@dukt.net>
2017-01-30 15:15:29 +0100

    Compressed JS

--- src/web/assets/cp/dist/js/Craft.js
+++ src/web/assets/cp/dist/js/Craft.js
@@ -7041,6 +7041,9 @@ Craft.charts.BaseChart = Garnish.Base.extend(
         formatLocaleDefinition: null,
         timeFormatLocaleDefinition: null,
         formats: {
+            numberFormat: ',.2f',
+            percentFormat: ',.2%',
+            currencyFormat: '$,.2f',
             shortDateFormats: {
                 day: "%-m/%-d",
                 month: "%-m/%y",
@@ -7114,7 +7117,7 @@ Craft.charts.Area = Craft.charts.BaseChart.extend(
         var x = this.getX(true);
         var xTicks = 3;
         var xAxis = d3.axisBottom(x)
-            .tickFormat(this.getXFormatter())
+            .tickFormat(this.getXTicksFormatter())
             .ticks(xTicks);
 
         this.drawingArea.append("g")
@@ -7130,7 +7133,7 @@ Craft.charts.Area = Craft.charts.BaseChart.extend(
 
         if (this.orientation != 'rtl') {
             var yAxis = d3.axisLeft(y)
-                .tickFormat(this.getYFormatter())
+                .tickFormat(this.getYTicksFormatter())
                 .tickValues(this.getYTickValues())
                 .ticks(yTicks);
 
@@ -7139,7 +7142,7 @@ Craft.charts.Area = Craft.charts.BaseChart.extend(
                 .call(yAxis);
         } else {
             var yAxis = d3.axisRight(y)
-                .tickFormat(this.getYFormatter())
+                .tickFormat(this.getYTicksFormatter())
                 .tickValues(this.getYTickValues())
                 .ticks(yTicks);
 
@@ -7391,7 +7394,7 @@ Craft.charts.Area = Craft.charts.BaseChart.extend(
         $.each(values, $.proxy(function(key, value) {
             var characterWidth = 8;
 
-            var formatter = this.getYFormatter();
+            var formatter = this.getYTicksFormatter();
 
             var formattedValue = formatter(value);
             var computedTickWidth = formattedValue.length * characterWidth;
@@ -7472,6 +7475,30 @@ Craft.charts.Area = Craft.charts.BaseChart.extend(
         return formatter;
     },
 
+    getXTicksFormatter: function() {
+        var formatter;
+
+        if (this.settings.xAxis.ticksFormatter != $.noop) {
+            formatter = this.settings.xAxis.ticksFormatter(this);
+        } else {
+            formatter = this.getXFormatter();
+        }
+
+        return formatter;
+    },
+
+    getYTicksFormatter: function() {
+        var formatter;
+
+        if (this.settings.yAxis.ticksFormatter != $.noop) {
+            formatter = this.settings.yAxis.ticksFormatter(this);
+        }  else {
+            formatter = this.getYFormatter();
+        }
+
+        return formatter;
+    },
+
     getYMaxValue: function() {
         return d3.max(this.dataTable.rows, function(d) {
             return d[1];
@@ -7497,12 +7524,14 @@ Craft.charts.Area = Craft.charts.BaseChart.extend(
         xAxis: {
             gridlines: false,
             showAxis: true,
-            formatter: $.noop
+            formatter: $.noop,
+            ticksFormatter: $.noop
         },
         yAxis: {
             gridlines: true,
             showAxis: false,
-            formatter: $.noop
+            formatter: $.noop,
+            ticksFormatter: $.noop
         }
     }
 });
@@ -7564,9 +7593,6 @@ Craft.charts.utils = {
 
             case 'time':
                 return Craft.charts.utils.getDuration;
-
-            case 'decimal':
-                return formatLocale.format(chartSettings.formats.decimalFormat);
 
             case 'number':
                 return formatLocale.format(chartSettings.formats.numberFormat);
