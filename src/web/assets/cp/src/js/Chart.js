c2e5159975e96b5b86a9abbc80e9bbf187502bd8
Benjamin David <ben@dukt.net>
2017-01-30 11:12:32 +0100

    Removed `Craft.charts.utils.applyShadowFilter()`

--- src/web/assets/cp/src/js/Chart.js
+++ src/web/assets/cp/src/js/Chart.js
@@ -562,10 +562,6 @@ Craft.charts.Area = Craft.charts.BaseChart.extend(
                     this.tip.hide();
                 }, this));
         }
-
-
-        // Apply shadow filter
-        Craft.charts.utils.applyShadowFilter('drop-shadow', this.g);
     },
 
     getChartMargin: function() {
@@ -724,45 +720,6 @@ Craft.charts.utils = {
         var time = hours + ':' + minutes + ':' + seconds;
 
         return time;
-    },
-
-    applyShadowFilter: function(id, svg) {
-        // filters go in defs element
-        var defs = svg.append("defs");
-
-        // create filter with id #{id}
-        // height=130% so that the shadow is not clipped
-        var filter = defs.append("filter")
-            .attr("id", id)
-            .attr("width", "200%")
-            .attr("height", "200%")
-            .attr("x", "-50%")
-            .attr("y", "-50%");
-
-        // SourceAlpha refers to opacity of graphic that this filter will be applied to
-        // convolve that with a Gaussian with standard deviation 3 and store result
-        // in blur
-        filter.append("feGaussianBlur")
-            .attr("in", "SourceAlpha")
-            .attr("stdDeviation", 1)
-            .attr("result", "blur");
-
-        // translate output of Gaussian blur to the right and downwards with 2px
-        // store result in offsetBlur
-        filter.append("feOffset")
-            .attr("in", "blur")
-            .attr("dx", 0)
-            .attr("dy", 0)
-            .attr("result", "offsetBlur");
-
-        // overlay original SourceGraphic over translated blurred opacity by using
-        // feMerge filter. Order of specifying inputs is important!
-        var feMerge = filter.append("feMerge");
-
-        feMerge.append("feMergeNode")
-            .attr("in", "offsetBlur");
-        feMerge.append("feMergeNode")
-            .attr("in", "SourceGraphic");
     }
 };
 
