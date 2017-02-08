1f49d80125dd6b871b59412f238163baabc4adf2
brandonkelly <brandon@pixelandtonic.com>
2017-02-08 05:39:05 -0800

    changelog*

--- src/services/Updates.php
+++ src/services/Updates.php
@@ -377,7 +377,7 @@ class Updates extends Component
 
             return (string)$response->getBody();
         } catch (\Exception $e) {
-            Craft::error('There was a problem getting the update feed for “'.$plugin->name.'”, so it was skipped: '.$e->getMessage(), __METHOD__);
+            Craft::error('There was a problem getting the changelog for “'.$plugin->name.'”, so it was skipped: '.$e->getMessage(), __METHOD__);
 
             return null;
         }
