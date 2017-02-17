craftcms/cms

commit 249b2bbe1c8b6683981862db58542e47d15b4c82

Fixes 1380

src/base/Element.php
@@ -1799,7 +1799,8 @@ abstract class Element extends Component implements ElementInterface
         $contentService = Craft::$app->getContent();
         $originalFieldContext = $contentService->fieldContext;
         $contentService->fieldContext = $this->getFieldContext();
-        $this->_fieldsByHandle[$handle] = Craft::$app->getFields()->getFieldByHandle($handle);
+        $fieldLayout = $this->getFieldLayout();
+        $this->_fieldsByHandle[$handle] = $fieldLayout ? $fieldLayout->getFieldByHandle($handle) : null;
         $contentService->fieldContext = $originalFieldContext;
 
         return $this->_fieldsByHandle[$handle];

src/models/FieldLayout.php
@@ -111,6 +111,25 @@ class FieldLayout extends Model
     }
 
     /**
+     * Returns a field by its handle.
+     *
+     * @param string $handle The field handle.
+     *
+     * @return Field|FieldInterface|null
+     */
+    public function getFieldByHandle(string $handle)
+    {
+        foreach ($this->getFields() as $field) {
+            /** @var Field $field */
+            if ($field->handle === $handle) {
+                return $field;
+            }
+        }
+
+        return null;
+    }
+
+    /**
      * Sets the layout’s tabs.
      *
      * @param array|FieldLayoutTab[] $tabs An array of the layout’s tabs, which can either be FieldLayoutTab

commit 1947ed1d8c08dbe6aa0b414eb50dc8a40f898835

Fixed a PHP error caused by 249b2bbe1c8b6683981862db58542e47d15b4c82

src/base/Element.php
@@ -1863,7 +1863,12 @@ abstract class Element extends Component implements ElementInterface
                             if ($field instanceof EagerLoadingFieldInterface && $this->hasEagerLoadedElements($field->handle)) {
                                 $value = $this->getEagerLoadedElements($field->handle);
                             } else {
-                                $value = $this->getFieldValue($field->handle);
+                                // The field might not actually belong to this element
+                                try {
+                                    $value = $this->getFieldValue($field->handle);
+                                } catch (\Exception $e) {
+                                    $value = $field->normalizeValue(null);
+                                }
                             }
 
                             return $field->getTableAttributeHtml($value, $this);