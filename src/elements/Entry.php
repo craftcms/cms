cf6e190d5182e7a39d9a7f23cd204285ecf09c2a
brandonkelly <brandon@pixelandtonic.com>
2017-03-06 16:38:17 -0800

    No more need for this

--- src/elements/Entry.php
+++ src/elements/Entry.php
@@ -538,15 +538,6 @@ class Entry extends Element
     public function rules()
     {
         $rules = parent::rules();
-
-        if (!$this->getType()->hasTitleField) {
-            // Don't validate the title
-            $key = array_search([['title'], 'required'], $rules, true);
-            if ($key !== -1) {
-                array_splice($rules, $key, 1);
-            }
-        }
-
         $rules[] = [['sectionId', 'typeId', 'authorId', 'newParentId'], 'number', 'integerOnly' => true];
         $rules[] = [['postDate', 'expiryDate'], DateTimeValidator::class];
 
