6ac12354de6e5872f4d7f5c3e6626a09b7bb4e5c
Brad Bell <brad@pixelandtonic.com>
2017-04-05 16:28:30 -0700

    Do some UTF-8 normalization and set a default timezone for console bootstrap.

--- bootstrap/console.php
+++ bootstrap/console.php
@@ -2,6 +2,20 @@
 
 use craft\helpers\Console;
 
+mb_detect_order('auto');
+
+// Normalize how PHP's string methods (strtoupper, etc) behave.
+setlocale(
+    LC_CTYPE,
+    'C.UTF-8', // libc >= 2.13
+    'C.utf8', // different spelling
+    'en_US.UTF-8', // fallback to lowest common denominator
+    'en_US.utf8' // different spelling for fallback
+);
+
+// Set default timezone to UTC
+date_default_timezone_set('UTC');
+
 $appType = 'console';
 
 /** @var \craft\console\Application $app */
