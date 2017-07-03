93d2758eae2ea463efaebe3b6fda405983bed088
Benjamin David <ben@dukt.net>
2017-07-03 10:22:13 +0200

    PluginGrid

--- src/web/assets/pluginstorevue/resources/assets/js/PluginGrid.vue
+++ src/web/assets/pluginstorevue/resources/assets/js/PluginGrid.vue
@@ -2,7 +2,7 @@
 
     <div>
         <div class="row plugin-grid" v-if="plugins.length > 0">
-            <div class="col-xs-12 col-sm-6 col-lg-6 plugin" v-for="plugin in plugins">
+            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 plugin" v-for="plugin in plugins">
                 <plugin-card :plugin="plugin" @click="openModal(plugin)"></plugin-card>
             </div>
         </div>
