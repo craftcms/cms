888332a9666d69db1af030b2228799632bc4cc56
Benjamin David <ben@dukt.net>
2017-07-03 10:22:03 +0200

    Index

--- src/web/assets/pluginstorevue/resources/assets/js/Index.vue
+++ src/web/assets/pluginstorevue/resources/assets/js/Index.vue
@@ -2,14 +2,26 @@
     <div>
         <plugin-search @showResults="showingSearchResults = true" @hideResults="showingSearchResults = false"></plugin-search>
 
-        <div class="row">
+        <div v-if="!showingSearchResults" class="row">
             <div class="col-xs-12 col-sm-8">
                 <h2>Staff Picks</h2>
-                <plugin-grid :plugins="plugins" v-if="!showingSearchResults"></plugin-grid>
+                <plugin-grid :plugins="plugins"></plugin-grid>
+                <h2>Active Trials</h2>
+                <plugin-grid :plugins="plugins"></plugin-grid>
             </div>
             <div class="col-xs-12 col-sm-4">
                 <h2>Categories</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto asperiores beatae corporis, doloremque est explicabo facilis illo inventore iusto libero magnam maxime modi, mollitia nulla perferendis placeat quisquam reiciendis rem.</p>
+                <ul>
+                    <li><a href="#">Analytics</a></li>
+                    <li><a href="#">Customer Support</a></li>
+                    <li><a href="#">Developer Tools</a></li>
+                    <li><a href="#">E-commerce</a></li>
+                    <li><a href="#">File Management</a></li>
+                    <li><a href="#">Sales</a></li>
+                    <li><a href="#">Marteking</a></li>
+                    <li><a href="#">Security</a></li>
+                    <li><a href="#">Templates</a></li>
+                </ul>
             </div>
         </div>
     </div>
