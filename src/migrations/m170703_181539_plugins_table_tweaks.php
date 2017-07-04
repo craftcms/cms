7216f071dae7fca71ae71af3d7171aaa190c8f4e
Brad Bell <brad@pixelandtonic.com>
2017-07-03 21:19:40 -0700

    Fixed the plugins_table_tweaks migration so it works with PostgreSQL

--- src/migrations/m170703_181539_plugins_table_tweaks.php
+++ src/migrations/m170703_181539_plugins_table_tweaks.php
@@ -16,9 +16,19 @@ class m170703_181539_plugins_table_tweaks extends Migration
     public function safeUp()
     {
         // Remove lengths
-        $this->alterColumn('{{%plugins}}', 'handle', $this->string()->notNull());
-        $this->alterColumn('{{%plugins}}', 'version', $this->string()->notNull());
-        $this->alterColumn('{{%plugins}}', 'schemaVersion', $this->string()->notNull());
+        // Two statements here because of a Yii 2/PostgreSQL bug:
+        // https://github.com/yiisoft/yii2/issues/12077
+        if ($this->db->getIsMysql()) {
+            $this->alterColumn('{{%plugins}}', 'handle', $this->string()->notNull());
+            $this->alterColumn('{{%plugins}}', 'version', $this->string()->notNull());
+            $this->alterColumn('{{%plugins}}', 'schemaVersion', $this->string()->notNull());
+        } else {
+            $pluginTable = $this->db->getSchema()->defaultSchema.'.'.$this->db->getSchema()->getRawTableName('plugins');
+
+            $this->db->createCommand()->setSql('ALTER TABLE '.$pluginTable.' ALTER COLUMN "handle" TYPE varchar(255), ALTER COLUMN "handle" SET NOT NULL')->execute();
+            $this->db->createCommand()->setSql('ALTER TABLE '.$pluginTable.' ALTER COLUMN "version" TYPE varchar(255), ALTER COLUMN "version" SET NOT NULL')->execute();
+            $this->db->createCommand()->setSql('ALTER TABLE '.$pluginTable.' ALTER COLUMN "schemaVersion" TYPE varchar(255), ALTER COLUMN "schemaVersion" SET NOT NULL')->execute();
+        }
     }
 
     /**
