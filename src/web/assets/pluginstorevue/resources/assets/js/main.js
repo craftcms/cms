a20e5d9babdb79fc600c998636ebe737d62cc318
Benjamin David <ben@dukt.net>
2017-07-17 01:37:45 +0200

    Clean up components in main.js

--- src/web/assets/pluginstorevue/resources/assets/js/main.js
+++ src/web/assets/pluginstorevue/resources/assets/js/main.js
@@ -4,10 +4,6 @@ import lodash from 'lodash'
 import VueLodash from 'vue-lodash/dist/vue-lodash.min'
 
 import App from './App';
-import Index from './Index';
-import Category from './Category';
-import Developer from './Developer';
-import AllPlugins from './components/AllPlugins';
 import CartButton from './components/CartButton';
 import router from './router';
 import store from './store'
@@ -19,7 +15,7 @@ const app = new Vue({
     el: '#container',
     router,
     store,
-    components: { App, Index, AllPlugins, CartButton, Category, Developer },
+    components: { App, CartButton },
     data() {
       return {
           showCrumbs: false,
