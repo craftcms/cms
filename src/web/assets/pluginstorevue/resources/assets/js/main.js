0b6a97ee56f1c1804631c056ce1d2b29a1641ca4
Benjamin David <ben@dukt.net>
2017-07-26 16:05:28 +0200

    Added notifications support

--- src/web/assets/pluginstorevue/resources/assets/js/main.js
+++ src/web/assets/pluginstorevue/resources/assets/js/main.js
@@ -27,6 +27,39 @@ const app = new Vue({
       }
     },
 
+    methods: {
+        displayNotification(type, message) {
+            var $notificationContainer = $('#notifications');
+            var notificationDuration = Craft.CP.notificationDuration;
+
+            if (type == 'error') {
+                notificationDuration *= 2;
+            }
+
+            var $notification = $('<div class="notification ' + type + '">' + message + '</div>')
+                .appendTo($notificationContainer);
+
+            var fadedMargin = -($notification.outerWidth() / 2) + 'px';
+
+            $notification
+                .hide()
+                .css({opacity: 0, 'margin-left': fadedMargin, 'margin-right': fadedMargin})
+                .velocity({opacity: 1, 'margin-left': '2px', 'margin-right': '2px'}, {display: 'inline-block', duration: 'fast'})
+                .delay(notificationDuration)
+                .velocity({opacity: 0, 'margin-left': fadedMargin, 'margin-right': fadedMargin}, {
+                    complete: function() {
+                        $notification.remove();
+                    }
+                });
+        },
+        displayNotice(message) {
+            this.displayNotification('notice', message);
+        },
+        displayError(message) {
+            this.displayNotification('error', message);
+        }
+    },
+
     created() {
         this.$store.dispatch('getAllPlugins')
         this.$store.dispatch('getAllCategories')
