7b88dc158e3440e9afc98a56b39a0a7dc834ca9c
Benjamin David <ben@dukt.net>
2017-08-02 00:51:51 +0200

    Added TextField component

---
+++ src/web/assets/pluginstorevue/resources/assets/js/components/TextField.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="field first" id="name-field">
+        <div class="heading">
+            <label :id="id+'-label'" :for="id">{{ label }}</label>
+        </div>
+        <div class="input ltr">
+            <input class="text fullwidth" type="text" :id="id" :name="id" :placeholder="placeholder" autocomplete="off">
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'textField',
+        props: ['label', 'id', 'placeholder']
+    }
+</script>
