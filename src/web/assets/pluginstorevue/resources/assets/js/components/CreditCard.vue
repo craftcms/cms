891477b75d55ef3937d60a27f2cfedf44349803c
Benjamin David <ben@dukt.net>
2017-08-05 03:46:58 +0200

    Improved CreditCard styles

--- src/web/assets/pluginstorevue/resources/assets/js/components/CreditCard.vue
+++ src/web/assets/pluginstorevue/resources/assets/js/components/CreditCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="credit-card">
         <text-fieldtype label="Card Number" id="card-number" v-model="value.number"></text-fieldtype>
         <text-fieldtype label="Card Expiry" id="card-expiry" placeholder="MM/YY"  v-model="value.expiry"></text-fieldtype>
         <text-fieldtype label="Card CVC" id="card-cvc" placeholder="XXX"  v-model="value.cvc"></text-fieldtype>
