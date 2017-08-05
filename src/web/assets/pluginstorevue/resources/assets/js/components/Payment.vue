b6fcea12bd806056da383980fb0ac4db23aed67f
Benjamin David <ben@dukt.net>
2017-08-05 03:47:53 +0200

    Improved payment flow

--- src/web/assets/pluginstorevue/resources/assets/js/components/Payment.vue
+++ src/web/assets/pluginstorevue/resources/assets/js/components/Payment.vue
@@ -1,123 +1,129 @@
 <template>
 
-    <div>
-            <div class="block">
-                    <h2>Identity</h2>
+	<div>
+		<div class="block">
+			<h2>Identity</h2>
 
-                    <a class="block-toggle" v-if="!showIdentity" @click="showIdentity=true">Edit</a>
-                    <a class="block-toggle" v-else @click="showIdentity=false">Done</a>
+			<a class="block-toggle" v-if="!showIdentity" @click="showIdentity=true">Edit</a>
+			<a class="block-toggle" v-else @click="showIdentity=false">Done</a>
 
-                    <template v-if="showIdentity">
+			<template v-if="showIdentity">
+				<p><label><input type="radio" value="craftid" v-model="identityMode" /> Use your Craft ID</label></p>
+				<p><label><input type="radio" value="guest" v-model="identityMode" /> Continue as guest</label></p>
 
-                            <p><label><input type="radio" value="craftid" v-model="identityMode" /> Use your Craft ID</label></p>
-                            <p><label><input type="radio" value="guest" v-model="identityMode" /> Continue as guest</label></p>
+				<template v-if="identityMode == 'craftid'">
+					<p><a class="btn submit" @click="showIdentity=false">Connect to your Craft ID</a></p>
+				</template>
+				<template v-else-if="identityMode == 'guest'">
+					<text-fieldtype label="Full Name" id="fullName" placeholder="John Smith" v-model="guestIdentity.fullName"></text-fieldtype>
+					<text-fieldtype label="Email" id="email" placeholder="john@example.com" v-model="guestIdentity.email"></text-fieldtype>
 
-                            <template v-if="identityMode == 'craftid'">
-                                    <p><a href="#" class="btn submit">Connect to your Craft ID</a></p>
-                            </template>
-                            <template v-else-if="identityMode == 'guest'">
-                                    <text-fieldtype label="Full Name" id="fullName" placeholder="John Smith" v-model="guestIdentity.fullName"></text-fieldtype>
-                                    <text-fieldtype label="Email" id="email" placeholder="john@example.com" v-model="guestIdentity.email"></text-fieldtype>
-                            </template>
-                    </template>
-                    <template v-else>
-                            <div v-if="identityMode == 'craftid'">
-                                    <ul>
-                                            <li>{{ craftIdIdentity.fullName }} <em>(Craft ID)</em></li>
-                                            <li>{{ craftIdIdentity.email }}</li>
-                                    </ul>
-                            </div>
-                            <div v-if="identityMode == 'guest'">
-                                    <ul>
-                                            <li>{{ guestIdentity.fullName }} <em>(Guest)</em></li>
-                                            <li>{{ guestIdentity.email }}</li>
-                                    </ul>
-                            </div>
-                    </template>
-            </div>
+					<a class="btn submit" @click="showIdentity=false">Continue</a>
+				</template>
+			</template>
+			<template v-else>
+				<div v-if="identityMode == 'craftid'">
+					<ul>
+						<li>{{ craftIdIdentity.fullName }} <em>(Craft ID)</em></li>
+						<li>{{ craftIdIdentity.email }}</li>
+					</ul>
+				</div>
+				<div v-if="identityMode == 'guest'">
+					<ul>
+						<li>{{ guestIdentity.fullName }} <em>(Guest)</em></li>
+						<li>{{ guestIdentity.email }}</li>
+					</ul>
+				</div>
+			</template>
+		</div>
 
-            <hr>
+		<hr>
 
-            <div class="block">
-                    <h2>Payment Method</h2>
+		<div class="block">
+			<h2>Payment Method</h2>
 
-                    <a class="block-toggle" v-if="!showPaymentMethod" @click="showPaymentMethod=true">Edit</a>
-                    <a class="block-toggle" v-else @click="showPaymentMethod=false">Done</a>
+			<a class="block-toggle" v-if="!showPaymentMethod" @click="showPaymentMethod=true">Edit</a>
+			<a class="block-toggle" v-else @click="showPaymentMethod=false">Done</a>
 
-                    <template v-if="showPaymentMethod">
-                            <template v-if="identityMode == 'craftid'">
-                                    <p><label><input type="radio" value="existingCard" v-model="paymentMode"> Use card {{ existingCard.number }}</label></p>
-                                    <p><label><input type="radio" value="newCard" v-model="paymentMode"> Or use a different credit card</label></p>
+			<template v-if="showPaymentMethod">
+				<template v-if="identityMode == 'craftid'">
+					<p><label><input type="radio" value="existingCard" v-model="paymentMode" /> Use card {{ craftIdIdentity.creditCard.number }}</label></p>
+					<p><label><input type="radio" value="newCard" v-model="paymentMode" /> Or use a different credit card</label></p>
 
-                                    <template v-if="paymentMode == 'newCard'">
-                                            <credit-card v-model="card"></credit-card>
-                                    </template>
-                            </template>
-                            <template v-else>
-                                <credit-card v-model="card"></credit-card>
-                            </template>
-                    </template>
-                    <template v-else>
-                            <ul>
-                                    <template v-if="identityMode == 'craftid' && paymentMode == 'existingCard'">
-                                            <li>{{ existingCard.number }}</li>
-                                            <li>{{ existingCard.expiry }}</li>
-                                            <li>{{ existingCard.cvc }}</li>
-                                    </template>
-                                    <template v-else>
-                                            <li>{{ card.number }}</li>
-                                            <li>{{ card.expiry }}</li>
-                                            <li>{{ card.cvc }}</li>
-                                    </template>
-                            </ul>
-                    </template>
-            </div>
+					<template v-if="paymentMode == 'newCard'">
+						<credit-card v-model="creditCard"></credit-card>
+					</template>
+				</template>
+				<template v-else>
+					<credit-card v-model="creditCard"></credit-card>
+				</template>
 
-            <hr>
+				<a class="btn submit" @click="showPaymentMethod=false">Continue</a>
+			</template>
+			<template v-else>
+				<ul>
+					<template v-if="identityMode == 'craftid' && paymentMode == 'existingCard'">
+						<li>{{ craftIdIdentity.creditCard.number }}</li>
+						<li>{{ craftIdIdentity.creditCard.expiry }}</li>
+						<li>{{ craftIdIdentity.creditCard.cvc }}</li>
+					</template>
+					<template v-else>
+						<li>{{ creditCard.number }}</li>
+						<li>{{ creditCard.expiry }}</li>
+						<li>{{ creditCard.cvc }}</li>
+					</template>
+				</ul>
+			</template>
+		</div>
 
-            <div class="block">
-                    <h2>Billing</h2>
+		<hr>
 
-                    <a class="block-toggle" v-if="!showBilling" @click="showBilling=true">Edit</a>
-                    <a class="block-toggle" v-else @click="showBilling=false">Done</a>
+		<div class="block">
+			<h2>Billing</h2>
 
-                    <template v-if="showBilling">
-                            <text-fieldtype label="Business Name" id="business-name" v-model="billing.businessName"></text-fieldtype>
-                            <text-fieldtype label="Business Tax ID" id="business-tax-id" v-model="billing.businessTaxId"></text-fieldtype>
-                            <text-fieldtype label="Address Line 1" id="address-line-1" v-model="billing.addressLine1"></text-fieldtype>
-                            <text-fieldtype label="Address Line 2" id="address-line-2" v-model="billing.addressLine2"></text-fieldtype>
-                            <text-fieldtype label="Country" id="country" v-model="billing.country"></text-fieldtype>
-                            <text-fieldtype label="State" id="state" v-model="billing.state"></text-fieldtype>
-                            <text-fieldtype label="City" id="city" v-model="billing.city"></text-fieldtype>
-                            <text-fieldtype label="Zip Code" id="zip-code" v-model="billing.zipCode"></text-fieldtype>
-                    </template>
-                    <template v-else>
-                            <ul>
-                                    <li>{{ billing.businessName }}</li>
-                                    <li>{{ billing.businessTaxId }}</li>
-                                    <li>{{ billing.addressLine1 }}</li>
-                                    <li>{{ billing.addressLine2 }}</li>
-                                    <li><span v-if="billing.city">{{ billing.city }}, </span>{{ billing.state }} {{ billing.zipCode }}</li>
-                                    <li>{{ billing.country }}</li>
-                            </ul>
-                    </template>
-            </div>
+			<a class="block-toggle" v-if="!showBilling"
+			   @click="showBilling=true">Edit</a>
+			<a class="block-toggle" v-else @click="showBilling=false">Done</a>
 
-            <hr>
+			<template v-if="showBilling">
+				<text-fieldtype label="Business Name" id="business-name" v-model="billing.businessName"></text-fieldtype>
+				<text-fieldtype label="Business Tax ID" id="business-tax-id" v-model="billing.businessTaxId"></text-fieldtype>
+				<text-fieldtype label="Address Line 1" id="address-line-1" v-model="billing.addressLine1"></text-fieldtype>
+				<text-fieldtype label="Address Line 2" id="address-line-2" v-model="billing.addressLine2"></text-fieldtype>
+				<text-fieldtype label="Country" id="country" v-model="billing.country"></text-fieldtype>
+				<text-fieldtype label="State" id="state" v-model="billing.state"></text-fieldtype>
+				<text-fieldtype label="City" id="city" v-model="billing.city"></text-fieldtype>
+				<text-fieldtype label="Zip Code" id="zip-code" v-model="billing.zipCode"></text-fieldtype>
 
-            <div class="buttons">
-                <a href="#" class="btn submit">Pay {{ cartTotal()|currency }}</a>
-            </div>
+				<a class="btn submit" @click="showBilling=false">Continue</a>
+			</template>
+			<template v-else>
+				<ul>
+					<li>{{ billing.businessName }}</li>
+					<li>{{ billing.businessTaxId }}</li>
+					<li>{{ billing.addressLine1 }}</li>
+					<li>{{ billing.addressLine2 }}</li>
+					<li><span v-if="billing.city">{{ billing.city }}, </span>{{ billing.state }} {{ billing.zipCode }}</li>
+					<li>{{ billing.country }}</li>
+				</ul>
+			</template>
+		</div>
 
-            <p>Your payment is safe and secure with Stripe.</p>
-    </div>
+		<hr>
+
+		<div class="buttons">
+			<a class="btn submit" :class="{ disabled: !readyToPay }">Pay {{ cartTotal() | currency }}</a>
+		</div>
+
+		<p>Your payment is safe and secure with Stripe.</p>
+	</div>
 
 </template>
 
 <script>
     import TextFieldtype from './fieldtypes/TextFieldtype';
     import CreditCard from './CreditCard';
-    import { mapGetters, mapActions } from 'vuex'
+    import {mapGetters, mapActions} from 'vuex'
 
     export default {
         components: {
@@ -129,11 +135,18 @@
             ...mapGetters({
                 cartTotal: 'cartTotal',
             }),
+			readyToPay() {
+                if(!this.showIdentity && !this.showPaymentMethod && !this.showBilling) {
+                    return true;
+				}
+
+				return false;
+			}
         },
 
         data() {
             return {
-                identityMode: 'guest',
+                identityMode: 'craftid',
                 paymentMode: 'existingCard',
                 showIdentity: false,
                 showPaymentMethod: false,
@@ -141,6 +154,11 @@
                 craftIdIdentity: {
                     fullName: "Brandon Kelly",
                     email: "brandon@pixelandtonic.com",
+					creditCard: {
+                        number: 'YYYY EXIS TING YYYY',
+                        expiry: '01/20',
+                        cvc: '123',
+                    },
                 },
                 guestIdentity: {
                     fullName: "",
@@ -151,20 +169,20 @@
                     expiry: '01/20',
                     cvc: '123',
                 },
-                card: {
+                creditCard: {
                     number: 'XXXX XXXX XXXX XXXX',
                     expiry: '01/20',
                     cvc: '123',
                 },
                 billing: {
-                        businessName: 'Pixel & Tonic',
-                        businessTaxId: '123456789',
-                        addressLine1: 'address line 1',
-                        addressLine2: 'address line 2',
-                        country: 'USA',
-                        state: 'OR',
-                        city: 'Bend',
-                        zipCode: '97700',
+                    businessName: 'Pixel & Tonic',
+                    businessTaxId: '123456789',
+                    addressLine1: 'address line 1',
+                    addressLine2: 'address line 2',
+                    country: 'USA',
+                    state: 'OR',
+                    city: 'Bend',
+                    zipCode: '97700',
                 }
             }
         }
