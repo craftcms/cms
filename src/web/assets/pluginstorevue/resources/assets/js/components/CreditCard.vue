1315a1bfa6ea25ff7bf0593c4bf1006c5664f4a6
Benjamin David <ben@dukt.net>
2017-08-14 13:27:30 +0200

    Improved CreditCard component’s fields

--- src/web/assets/pluginstorevue/resources/assets/js/components/CreditCard.vue
+++ src/web/assets/pluginstorevue/resources/assets/js/components/CreditCard.vue
@@ -1,19 +1,28 @@
 <template>
     <div class="credit-card">
-        <text-fieldtype label="Card Number" id="card-number" v-model="value.number"></text-fieldtype>
-        <text-fieldtype label="Card Expiry" id="card-expiry" placeholder="MM/YY"  v-model="value.expiry"></text-fieldtype>
-        <text-fieldtype label="Card CVC" id="card-cvc" placeholder="XXX"  v-model="value.cvc"></text-fieldtype>
+
+        <div class="multitext">
+            <div class="multitextrow">
+                <text-fieldtype id="card-number" placeholder="Card Number" v-model="value.number"></text-fieldtype>
+            </div>
+            <div class="multitextrow">
+                <text-input id="card-expiry" placeholder="MM/YY" v-model="value.expiry"></text-input>
+                <text-input id="card-cvc" placeholder="CVC" v-model="value.cvc"></text-input>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import TextFieldtype from './fieldtypes/TextFieldtype';
+    import TextInput from './inputs/TextInput';
 
     export default {
         props: ['value'],
 
         components: {
             TextFieldtype,
+            TextInput,
         },
     }
 </script>
