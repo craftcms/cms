238c818d4857d62763233e503b56d2db526ed156
Benjamin David <ben@dukt.net>
2017-08-15 13:28:47 +0200

    Added CheckboxField

---
+++ src/web/assets/pluginstorevue/resources/assets/js/components/fields/CheckboxField.vue
@@ -0,0 +1,14 @@
+<template>
+
+	<div class="field">
+		<input :id="id" type="checkbox" class="checkbox">
+		<label :id="id+'-label'" :for="id">{{ label }}</label>
+	</div>
+
+</template>
+
+<script>
+    export default {
+        props: ['label', 'id', 'value'],
+    }
+</script>
