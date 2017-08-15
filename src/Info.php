113b16620588b9f66bb55ae06f4b2a5d44ceda3c
brandonkelly <brandon@pixelandtonic.com>
2017-08-15 11:34:19 -0700

    Finish 2.6.2989

--- src/Info.php
+++ src/Info.php
@@ -1,6 +1,6 @@
 <?php
 namespace Craft;
 
-define('CRAFT_VERSION', '2.6.2988');
+define('CRAFT_VERSION', '2.6.2989');
 define('CRAFT_SCHEMA_VERSION', '2.6.10');
 define('CRAFT_MIN_VERSION_REQUIRED', '2.6.2922');
