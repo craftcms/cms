61bd3a2cbce0135c4b9ac1de2cafe3914a089ec7
Benjamin David <ben@dukt.net>
2017-08-17 17:21:54 +0200

    Improved Craft component

--- src/web/assets/pluginstorevue/resources/assets/js/Craft.vue
+++ src/web/assets/pluginstorevue/resources/assets/js/Craft.vue
@@ -1,38 +1,126 @@
 <template>
-    <div v-if="craftClient && craftPro">
+    <div v-if="craftClientPlugin && craftProPlugin">
 
-        <div class="cssgrid-plugins">
-            <div class="box">
-                <h2>Personal</h2>
-                <p class="light">For sites built by and for the developer.</p>
-                <p>Free</p>
-            </div>
-            <div class="box">
-                <h2>Client</h2>
-                <p class="light">For sites built for clients with only one content manager.</p>
-                <p>{{ craftClient.price|currency }}</p>
-                
-                <div>
-                    <a v-if="isInCart(craftClient)" class="btn submit disabled">Added to cart</a>
-                    <a v-else  @click="buy(craftClient)" class="btn submit">Buy now</a>
 
-                    <a v-if="isInTrial(craftClient)" class="btn disabled">Try for free</a>
-                    <a v-else @click="installPlugin(craftClient)" class="btn">Try for free</a>
-                </div>
-            </div>
-            <div class="box">
-                <h2>Pro</h2>
-                <p class="light">For everything else.</p>
-                <p>{{ craftPro.price|currency }}</p>
+        <div id="upgrademodal-compare" class="body">
+            <table class="data fullwidth">
+                <thead>
+                <tr class="logos">
+                    <td></td>
+                    <th scope="col">
+                        <h1 class="logo">Personal</h1>
+                        <p>{{ "For sites built by and for the developer." }}</p>
+                    </th>
+                    <th scope="col">
+                        <h1 class="logo">Client</h1>
+                        <p>{{ "For sites built for clients with only one content manager." }}</p>
+                    </th>
+                    <th scope="col">
+                        <h1 class="logo">Pro</h1>
+                        <p>{{ "For everything else." }}</p>
+                    </th>
+                </tr>
+                <tr class="license-statuses">
+                    <td></td>
 
-                <div>
-                    <a v-if="isInCart(craftPro)" class="btn submit disabled">Added to cart</a>
-                    <a v-else  @click="buy(craftPro)" class="btn submit">Buy now</a>
+                    <!--<td>{{ "statusBadge(CraftPersonal, licensedEdition)" }}</td>-->
+                    <td><craft-status-badge :edition="craftData.CraftPersonal" /></td>
 
-                    <a v-if="isInTrial(craftPro)" class="btn disabled">Try for free</a>
-                    <a v-else @click="installPlugin(craftPro)" class="btn">Try for free</a>
-                </div>
-            </div>
+                    <!--<td>{{ "statusBadge(CraftClient, licensedEdition)" }}</td>-->
+                    <td><craft-status-badge :edition="craftData.CraftClient" /></td>
+
+                    <!--<td>{{ "statusBadge(CraftPro, licensedEdition)" }}</td>-->
+                    <td><craft-status-badge :edition="craftData.CraftPro" /></td>
+                </tr>
+                <tr class="price">
+                    <th scope="row" class="feature">{{ "One-Time Price" }}</th>
+                    <td>{{ "Free" }}</td>
+
+                    <!--<td>{{ "price(CraftClient, editions[CraftClient])" }}</td>-->
+                    <td>{{ craftData.editions[1].formattedPrice }}</td>
+
+                    <!--<td>{{ "price(CraftPro, editions[CraftPro])" }}</td>-->
+                    <td>{{ craftData.editions[2].formattedPrice }}</td>
+                </tr>
+                <tr class="buybtns">
+                    <td></td>
+                    <td></td>
+
+                    <!--<td>{{ "buybtn(CraftClient, editions[CraftClient], licensedEdition, canTestEditions)" }}</td>-->
+                    <td>
+                        <div class="btngroup">
+                            <a v-if="isInCart(craftClientPlugin)" class="btn submit disabled">Added to cart</a>
+                            <a v-else  @click="buy(craftClientPlugin)" class="btn submit">Buy now</a>
+
+                            <a v-if="isInTrial(craftClientPlugin)" class="btn disabled">Try for free</a>
+                            <a v-else @click="installPlugin(craftClientPlugin)" class="btn">Try for free</a>
+                        </div>
+                    </td>
+
+                    <!--<td>{{ "buybtn(CraftPro, editions[CraftPro], licensedEdition, canTestEditions)" }}</td>-->
+                    <td>
+                        <div class="btngroup">
+                            <div v-if="isInCart(craftProPlugin)" class="btn submit disabled">Added to cart</div>
+                            <div v-else  @click="buy(craftProPlugin)" class="btn submit">Buy now</div>
+
+                            <div v-if="isInTrial(craftProPlugin)" class="btn disabled">Try for free</div>
+                            <div v-else @click="installPlugin(craftProPlugin)" class="btn">Try for free</div>
+                        </div>
+                    </td>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th class="group" colspan="4">{{ "User Accounts" }}</th>
+                </tr>
+                <tr>
+                    <th class="feature" scope="row">{{ "Additional user accounts" }}</th>
+                    <td>{{ "One Admin account" }}</td>
+                    <td>{{ "One “Client” account" }}</td>
+                    <td>{{ "Unlimited" }}</td>
+                </tr>
+                <tr>
+                    <th class="feature" scope="row">{{ "User groups" }}</th>
+                    <td></td>
+                    <td></td>
+                    <td><span data-icon="check"></span></td>
+                </tr>
+                <tr>
+                    <th class="feature" scope="row">{{ "User permissions" }}</th>
+                    <td></td>
+                    <td></td>
+                    <td><span data-icon="check"></span></td>
+                </tr>
+                <tr>
+                    <th class="feature" scope="row">{{ "Public user registration" }}</th>
+                    <td></td>
+                    <td></td>
+                    <td><span data-icon="check"></span></td>
+                </tr>
+
+                <tr>
+                    <th class="group" colspan="4">{{ "System Branding" }}</th>
+                </tr>
+                <tr>
+                    <th class="feature" scope="row">{{ "Custom login screen logo" }}</th>
+                    <td></td>
+                    <td><span data-icon="check"></span></td>
+                    <td><span data-icon="check"></span></td>
+                </tr>
+                <tr>
+                    <th class="feature" scope="row">{{ "Custom HTML email template" }}</th>
+                    <td></td>
+                    <td><span data-icon="check"></span></td>
+                    <td><span data-icon="check"></span></td>
+                </tr>
+                <tr>
+                    <th class="feature" scope="row">{{ "Custom email message wording" }}</th>
+                    <td></td>
+                    <td><span data-icon="check"></span></td>
+                    <td><span data-icon="check"></span></td>
+                </tr>
+                </tbody>
+            </table>
         </div>
 
     </div>
@@ -41,17 +129,22 @@
 
 <script>
     import { mapGetters, mapActions } from 'vuex'
+    import CraftStatusBadge from './components/CraftStatusBadge';
 
     export default {
+        components: {
+            CraftStatusBadge
+        },
         computed: {
             ...mapGetters({
+                craftData: 'craftData',
                 isInTrial: 'isInTrial',
                 isInCart: 'isInCart',
             }),
-            craftClient() {
+            craftClientPlugin() {
                 return this.$store.getters.getPluginById(152);
             },
-            craftPro() {
+            craftProPlugin() {
                 return this.$store.getters.getPluginById(153);
             },
         },
