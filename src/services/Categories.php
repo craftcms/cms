65dd9b5b49af30f0816f78e4b8233d923dbd2ac2
Sebastian Weyrauch <git@dev-lab.de>
2017-08-23 07:17:06 +0200

    added integer casts for all maxLevel comparisons

--- src/services/Categories.php
+++ src/services/Categories.php
@@ -314,7 +314,7 @@ class Categories extends Component
         }
 
         // If they've set maxLevels to 0 (don't ask why), then pretend like there are none.
-        if ($group->maxLevels === 0) {
+        if ((int)$group->maxLevels === 0) {
             $group->maxLevels = null;
         }
 
