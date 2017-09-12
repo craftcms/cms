64d6326a3d3b3fa3de9d68656e8c5173acd02dad
Benjamin David <ben@dukt.net>
2017-09-12 10:20:46 +0200

    v-model support for CheckboxField

--- src/web/assets/pluginstore/resources/assets/js/components/fields/CheckboxField.vue
+++ src/web/assets/pluginstore/resources/assets/js/components/fields/CheckboxField.vue
@@ -1,7 +1,7 @@
 <template>
 
 	<div class="field">
-		<input :id="id" type="checkbox" class="checkbox">
+		<input :id="id" type="checkbox" class="checkbox" value="1" :checked="checked" @change="change">
 		<label :id="id+'-label'" :for="id">{{ label }}</label>
 	</div>
 
@@ -10,5 +10,18 @@
 <script>
     export default {
         props: ['label', 'id', 'value'],
+
+		data() {
+          	return {
+          	    checked: this.value,
+			}
+		},
+
+		methods: {
+          	change($event) {
+          	    this.checked = !this.checked;
+                this.$emit('input', this.checked)
+			}
+		},
     }
 </script>
