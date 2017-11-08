d968bcfc38a26c542b7603192a71195e0d4ac7ea
brandonkelly <brandon@pixelandtonic.com>
2017-11-08 08:42:01 -0800

    Typo + PHP 5.3 arary syntax

--- src/controllers/UsersController.php
+++ src/controllers/UsersController.php
@@ -603,10 +603,10 @@ class UsersController extends BaseController
 					if (craft()->userSession->isAdmin())
 					{
 						// If they already have a password (like from a front-end user registration), no
-						// need to show the Copy Password Reset URL option
+						// need to show the "Copy activation URL" option
 						if (!$variables['account']->password)
 						{
-							$statusActions[] = ['id' => 'copy-passwordreset-url', 'label' => Craft::t('Copy activation URL')];
+							$statusActions[] = array('id' => 'copy-passwordreset-url', 'label' => Craft::t('Copy activation URL'));
 						}
 
 						$statusActions[] = array('action' => 'users/activateUser', 'label' => Craft::t('Activate account'));
