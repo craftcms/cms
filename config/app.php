735b03a1e6761181ecf8f82d7820d9d7ea02253d
brandonkelly <brandon@pixelandtonic.com>
2017-11-17 06:46:04 -0800

    Add custom module scaffolding

---
+++ config/app.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Yii Application Config
+ *
+ * Edit this file at your own risk!
+ *
+ * The array returned by this file will get merged with
+ * vendor/craftcms/cms/src/config/app/main.php and [web|console].php, when
+ * the Craft's bootstrap script is defining the configuration for the entire
+ * application.
+ *
+ * You can define custom modules and system components, and even override the
+ * built-in system components.
+ */
+
+return [
+    'modules' => [
+        'my-module' => \modules\Module::class,
+    ],
+    //'bootstrap' => ['my-module'],
+];
---
+++ modules/Module.php
@@ -0,0 +1,32 @@
+<?php
+namespace modules;
+
+/**
+ * Custom module class.
+ *
+ * This class will be available throughout the system via:
+ * `Craft::$app->getModule('my-module')`.
+ *
+ * You can change its module ID ("my-module") to something else from
+ * config/app.php.
+ *
+ * If you want the module to get loaded on every request, uncomment this line
+ * in config/app.php:
+ *
+ *     'bootstrap' => ['my-module']
+ *
+ * Learn more about Yii module development in Yii's documentation:
+ * http://www.yiiframework.com/doc-2.0/guide-structure-modules.html
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * Initializes the module.
+     */
+    public function init()
+    {
+        parent::init();
+
+        // Custom initialization code goes here...
+    }
+}
