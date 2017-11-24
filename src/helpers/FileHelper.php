2c329478aebb2f49b5ae7e61af60ca337d654a06
brandonkelly <brandon@pixelandtonic.com>
2017-11-24 03:24:06 -0800

    FileHelper::isSvg()

--- src/helpers/FileHelper.php
+++ src/helpers/FileHelper.php
@@ -271,6 +271,25 @@ class FileHelper extends \yii\helpers\FileHelper
     }
 
     /**
+     * Returns whether the given file path is an SVG image.
+     *
+     *
+     * @param string $file the file name.
+     * @param string $magicFile name of the optional magic database file (or alias), usually something like `/path/to/magic.mime`.
+     * This will be passed as the second parameter to [finfo_open()](http://php.net/manual/en/function.finfo-open.php)
+     * when the `fileinfo` extension is installed. If the MIME type is being determined based via [[getMimeTypeByExtension()]]
+     * and this is null, it will use the file specified by [[mimeMagicFile]].
+     * @param bool $checkExtension whether to use the file extension to determine the MIME type in case
+     * `finfo_open()` cannot determine it.
+     *
+     * @return bool
+     */
+    public static function isSvg(string $file, string $magicFile = null, bool $checkExtension = true): bool
+    {
+        return self::getMimeType($file, $magicFile, $checkExtension) === 'image/svg+xml';
+    }
+
+    /**
      * Writes contents to a file.
      *
      * @param string $file     the file path
