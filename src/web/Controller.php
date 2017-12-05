8de3940eab353cd4dee88ce8876bc3a9ce52f2d3
brandonkelly <brandon@pixelandtonic.com>
2017-12-05 07:10:28 -0800

    Log exceptions

--- src/web/Controller.php
+++ src/web/Controller.php
@@ -80,6 +80,7 @@ abstract class Controller extends \yii\web\Controller
             return parent::runAction($id, $params);
         }
         catch (\Throwable $e) {
+            Craft::$app->getErrorHandler()->logException($e);
             if (Craft::$app->getRequest()->getAcceptsJson()) {
                 $statusCode = $e instanceof HttpException && $e->statusCode ? $e->statusCode : 500;
                 return $this->asErrorJson($e->getMessage())
