9ce98ea8697ca220cc536547cc25926410e75937
brandonkelly <brandon@pixelandtonic.com>
2017-12-11 11:20:19 -0800

    attempt at this min-height fix

--- src/web/assets/cp/src/js/CP.js
+++ src/web/assets/cp/src/js/CP.js
@@ -262,7 +262,7 @@ Craft.CP = Garnish.Base.extend(
 
         handleResize: function() {
             // #main-content height = viewport height - #alerts - #crumbs - #header
-            this.$mainContent.css('max-height', Garnish.$win.height() - (this.$alerts.outerHeight() || 0) - (this.$crumbs.outerHeight() || 0) - this.$header.outerHeight());
+            //this.$mainContent.css('max-height', Garnish.$win.height() - (this.$alerts.outerHeight() || 0) - (this.$crumbs.outerHeight() || 0) - this.$header.outerHeight());
 
             // Update responsive tables
             this.updateResponsiveTables();
