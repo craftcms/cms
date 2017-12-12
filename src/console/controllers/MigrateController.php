084e27c8c3e2375011bccc860d85b7aabc0c1448
brandonkelly <brandon@pixelandtonic.com>
2017-12-12 12:11:38 -0800

    Cleaner fix for #1907

--- src/console/controllers/MigrateController.php
+++ src/console/controllers/MigrateController.php
@@ -198,65 +198,26 @@ class MigrateController extends BaseMigrateController
         }
     }
 
+    /**
+     * @inheritdoc
+     */
     public function actionUp($limit = 0)
     {
-        $migrations = $this->getNewMigrations();
-        if (empty($migrations)) {
-            $this->stdout("No new migrations found. Your system is up-to-date.\n", Console::FG_GREEN);
+        $res = parent::actionUp($limit);
 
-            return ExitCode::OK;
-        }
-
-        $total = count($migrations);
-        $limit = (int) $limit;
-        if ($limit > 0) {
-            $migrations = array_slice($migrations, 0, $limit);
-        }
-
-        $n = count($migrations);
-        if ($n === $total) {
-            $this->stdout("Total $n new " . ($n === 1 ? 'migration' : 'migrations') . " to be applied:\n", Console::FG_YELLOW);
-        } else {
-            $this->stdout("Total $n out of $total new " . ($total === 1 ? 'migration' : 'migrations') . " to be applied:\n", Console::FG_YELLOW);
-        }
-
-        foreach ($migrations as $migration) {
-            $nameLimit = $this->getMigrationNameLimit();
-            if ($nameLimit !== null && strlen($migration) > $nameLimit) {
-                $this->stdout("\nThe migration name '$migration' is too long. Its not possible to apply this migration.\n", Console::FG_RED);
-                return ExitCode::UNSPECIFIED_ERROR;
+        if (in_array($res, [ExitCode::OK, null], true) && empty($this->getNewMigrations())) {
+            // Update any schema versions.
+            switch ($this->type) {
+                case MigrationManager::TYPE_APP:
+                    Craft::$app->getUpdates()->updateCraftVersionInfo();
+                    break;
+                case MigrationManager::TYPE_PLUGIN:
+                    Craft::$app->getUpdates()->setNewPluginInfo($this->plugin);
+                    break;
             }
-            $this->stdout("\t$migration\n");
         }
-        $this->stdout("\n");
 
-        $applied = 0;
-        if ($this->confirm('Apply the above ' . ($n === 1 ? 'migration' : 'migrations') . '?')) {
-            foreach ($migrations as $migration) {
-                if (!$this->migrateUp($migration)) {
-                    $this->stdout("\n$applied from $n " . ($applied === 1 ? 'migration was' : 'migrations were') . " applied.\n", Console::FG_RED);
-                    $this->stdout("\nMigration failed. The rest of the migrations are canceled.\n", Console::FG_RED);
-
-                    return ExitCode::UNSPECIFIED_ERROR;
-                }
-                $applied++;
-            }
-
-            if ($limit === 0) {
-                // Update any schema versions.
-                switch ($this->type) {
-                    case MigrationManager::TYPE_APP:
-                        Craft::$app->getUpdates()->updateCraftVersionInfo();
-                        break;
-                    case MigrationManager::TYPE_PLUGIN:
-                        Craft::$app->getUpdates()->setNewPluginInfo($this->plugin);
-                        break;
-                }
-            }
-
-            $this->stdout("\n$n " . ($n === 1 ? 'migration was' : 'migrations were') . " applied.\n", Console::FG_GREEN);
-            $this->stdout("\nMigrated up successfully.\n", Console::FG_GREEN);
-        }
+        return $res;
     }
 
     // Protected Methods
