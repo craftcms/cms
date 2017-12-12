9a62b42e5cf9e918943464f607f16db5a13527ad
brandonkelly <brandon@pixelandtonic.com>
2017-12-12 06:49:51 -0800

    Remove unknown inspection

--- src/elements/Asset.php
+++ src/elements/Asset.php
@@ -885,7 +885,6 @@ class Asset extends Element
     {
         switch ($attribute) {
             case 'filename':
-                /** @noinspection CssInvalidPropertyValue - FP */
                 return Html::encodeParams('<span style="word-break: break-word;">{filename}</span>', [
                     'filename' => $this->filename,
                 ]);
