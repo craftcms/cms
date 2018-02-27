e32836809498f58eff792ce41821529b867b8ef8
brandonkelly <brandon@pixelandtonic.com>
2018-02-27 09:45:20 -0800

    Update config/app.php docblock

--- config/app.php
+++ config/app.php
@@ -5,12 +5,16 @@
  * Edit this file at your own risk!
  *
  * The array returned by this file will get merged with
- * vendor/craftcms/cms/src/config/app/main.php and [web|console].php, when
+ * vendor/craftcms/cms/src/config/app.php and app.[web|console].php, when
  * Craft's bootstrap script is defining the configuration for the entire
  * application.
  *
  * You can define custom modules and system components, and even override the
  * built-in system components.
+ *
+ * If you want to modify the application config for *only* web requests or
+ * *only* console requests, create an app.web.php or app.console.php file in
+ * your config/ folder, alongside this one.
  */
 
 return [
