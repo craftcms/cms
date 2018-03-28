542cdac3b280e6313615aa048265a52b263784ab
Benjamin David <ben@dukt.net>
2018-03-28 09:41:14 -0700

    Made text inputs support errors

--- src/web/assets/pluginstore/src/js/components/inputs/TextInput.vue
+++ src/web/assets/pluginstore/src/js/components/inputs/TextInput.vue
@@ -1,11 +1,11 @@
 <template>
-	<input class="text fullwidth" type="text" :id="id" :placeholder="placeholder" autocomplete="off" :value="value" @input="$emit('input', $event.target.value)">
+	<input class="text fullwidth" :class="{'error': error }" type="text" :id="id" :placeholder="placeholder" autocomplete="off" :value="value" @input="$emit('input', $event.target.value)">
 </template>
 
 <script>
     export default {
 
-        props: ['id', 'placeholder', 'value'],
+        props: ['id', 'placeholder', 'value', 'error'],
 
     }
 </script>
