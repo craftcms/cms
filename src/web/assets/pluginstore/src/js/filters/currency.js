83f54fcab1899913768367e37377737d62276a50
Benjamin David <ben@dukt.net>
2018-04-11 15:29:14 +0200

    Improved currency filter

--- src/web/assets/pluginstore/src/js/filters/currency.js
+++ src/web/assets/pluginstore/src/js/filters/currency.js
@@ -1,25 +1,16 @@
-const digitsRE = /(\d{3})(?=\d)/g
+import Accounting from 'accounting'
 
-export function currency(value, currency, decimals) {
-    value = parseFloat(value)
-    if (!isFinite(value) || (!value && value !== 0)) {
-        return ''
+/**
+ * Formats a value as a currency value
+ */
+export function currency(value) {
+    let precision = 2;
+    let floatValue = parseFloat(value);
+
+    // Auto precision
+    if(Math.round(floatValue) === floatValue) {
+        precision = 0;
     }
-    currency = currency != null ? currency : '$'
-    decimals = decimals != null ? decimals : 2
-    var stringified = Math.abs(value).toFixed(decimals)
-    var _int = decimals
-        ? stringified.slice(0, -1 - decimals)
-        : stringified
-    var i = _int.length % 3
-    var head = i > 0
-        ? (_int.slice(0, i) + (_int.length > 3 ? ',' : ''))
-        : ''
-    var _float = decimals
-        ? stringified.slice(-1 - decimals)
-        : ''
-    var sign = value < 0 ? '-' : ''
-    return sign + currency + head +
-        _int.slice(i).replace(digitsRE, '$1,') +
-        _float
+
+    return Accounting.formatMoney(floatValue, '$', precision);
 }
