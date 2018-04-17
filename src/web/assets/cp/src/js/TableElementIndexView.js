45590b8554b1ef56b0f708779d77dc2029b5553e
Nate Iler <nate@flipboxdigital.com>
2018-04-17 10:05:00 -0600

    allow get more elements action to be set via settings

--- src/web/assets/cp/src/js/TableElementIndexView.js
+++ src/web/assets/cp/src/js/TableElementIndexView.js
@@ -240,7 +240,7 @@ Craft.TableElementIndexView = Craft.BaseElementIndexView.extend(
                     var params = $.extend(true, {}, this.settings.params);
                     params.criteria.descendantOf = id;
 
-                    Craft.postActionRequest('element-indexes/get-more-elements', params, $.proxy(function(response, textStatus) {
+                    Craft.postActionRequest(this.settings.loadMoreAction, params, $.proxy(function(response, textStatus) {
                         // Do we even care about this anymore?
                         if (!$spinnerRow.parent().length) {
                             return;
