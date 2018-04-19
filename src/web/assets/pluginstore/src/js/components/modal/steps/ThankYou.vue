2ffdf69920b8cf073b7e621e0793b5ac927d4b48
Benjamin David <ben@dukt.net>
2018-04-19 21:07:53 +0200

    Revert "Added “Auto-renew your licenses” section on thank you page"
    This reverts commit c93dc646f50735ec00419ba47a2a88ab5f7ff163.

--- src/web/assets/pluginstore/src/js/components/modal/steps/ThankYou.vue
+++ src/web/assets/pluginstore/src/js/components/modal/steps/ThankYou.vue
@@ -9,49 +9,6 @@
 					<a :href="managePluginsUrl" class="btn submit">{{ "Manage plugins"|t('app') }}</a>
 				</p>
 			</div>
-
-			<div id="thank-you-renewals">
-				<h2>Auto-renew your licenses</h2>
-				<table class="data fullwidth">
-					<thead>
-					<tr>
-						<th></th>
-						<th>Item</th>
-						<th>Auto Renew</th>
-						<th>Price</th>
-					</tr>
-					</thead>
-					<tbody>
-					<tr v-for="(item, itemKey) in cartItems">
-						<template v-if="item.lineItem.purchasable.type === 'cms-edition'">
-							<td class="thin">
-								<div class="plugin-icon">
-									<img :src="craftLogo" width="32" height="32" />
-								</div>
-							</td>
-							<td>Craft {{ item.lineItem.purchasable.name }}</td>
-						</template>
-
-						<template v-else="item.lineItem.purchasable.type === 'plugin-edition'">
-							<td class="thin">
-								<div class="plugin-icon">
-									<img v-if="item.plugin.iconUrl" :src="item.plugin.iconUrl" height="32" />
-								</div>
-							</td>
-							<td>
-								{{ item.plugin.name}}
-							</td>
-						</template>
-						<td>
-							<lightswitch-input :id="'auto-renew-'+itemKey" v-model="autoRenew[itemKey]" />
-						</td>
-						<td>
-							{{ item.lineItem.purchasable.renewalPrice|currency }}/year
-						</td>
-					</tr>
-					</tbody>
-				</table>
-			</div>
 		</template>
 	</step>
 </template>
@@ -61,15 +18,8 @@
 
     export default {
 
-        data() {
-            return {
-                autoRenew: {},
-            }
-        },
-
         components: {
             Step: require('../Step'),
-            LightswitchInput: require('../../inputs/LightswitchInput'),
         },
 
         computed: {
