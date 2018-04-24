c04a35d638585e376f00829a5fa189a7f090e17c
andris-sevcenko <asevcenko@gmail.com>
2018-04-24 10:55:43 +0300

    Fix docbloc.

--- src/controllers/AssetsController.php
+++ src/controllers/AssetsController.php
@@ -38,8 +38,8 @@ use yii\web\Response;
 /**
  * The AssetsController class is a controller that handles various actions related to asset tasks, such as uploading
  * files and creating/deleting/renaming files and folders.
- * Note that all actions in the controller except [[actionGenerateTransform()]] require an authenticated Craft session
- * via [[allowAnonymous]].
+ * Note that all actions in the controller except for [[actionGenerateTransform()]] and [[actionGenerateThumb()]]
+ * require an authenticated Craft session via [[allowAnonymous]].
  *
  * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
  * @since 3.0
