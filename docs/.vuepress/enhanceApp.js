852759489d924bd2357a7df45a66cc4baa3b2f8f
brandonkelly <brandon@pixelandtonic.com>
2018-05-14 15:16:24 -0700

    GA take 3

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,9 +1,9 @@
-import Vue from 'vue'
 import VueAnalytics from 'vue-analytics'
 
-Vue.use(VueAnalytics, {
-    id: 'UA-39036834-7',
-    checkDuplicatedScript: true
-})
-
-export default ({ Vue, options, router, siteData }) => {}
+export default ({ Vue, options, router, siteData }) => {
+    Vue.use(VueAnalytics, {
+        // $vue: this,
+        id: 'UA-39036834-7',
+        router
+    })
+}
