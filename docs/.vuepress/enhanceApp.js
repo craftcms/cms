35a076fd91a185c15d979942fadf16460b78a517
brandonkelly <brandon@pixelandtonic.com>
2018-05-14 15:19:58 -0700

    Remove comment

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -2,7 +2,6 @@ import VueAnalytics from 'vue-analytics'
 
 export default ({ Vue, options, router, siteData }) => {
     Vue.use(VueAnalytics, {
-        // $vue: this,
         id: 'UA-39036834-7',
         router
     })
