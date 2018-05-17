951c57066b2b8f70573ec76f27f9dcae446075be
Makeila Lundy <makeila@pixelandtonic.com>
2018-05-17 11:17:26 -0700

    delete class creation blurb

--- tests/unit/ViewTest.php
+++ tests/unit/ViewTest.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: makeilalundy
- * Date: 5/17/18
- * Time: 9:24 AM
- */
-
 
 class ViewTest extends \Codeception\Test\Unit
 {
@@ -21,27 +14,13 @@ class ViewTest extends \Codeception\Test\Unit
         $this->assertEquals( '{{ object.titleWithHyphens|replace({\'-\': \'!\'}) }}', $view->normalizeObjectTemplate('{{ object.titleWithHyphens|replace({\'-\': \'!\'}) }}'));
         $this->assertEquals( '{{object.foo|raw}}', $view->normalizeObjectTemplate('{foo}'));
         $this->assertEquals( '{{object.foo.bar|raw}}', $view->normalizeObjectTemplate('{foo.bar}'));
-       // $this->assertEquals( '{foo : \'bar\'}', $view->normalizeObjectTemplate('{foo : \'bar\'}'));
+        $this->assertEquals( '{foo : \'bar\'}', $view->normalizeObjectTemplate('{foo : \'bar\'}'));
         $this->assertEquals( '{{foo}}', $view->normalizeObjectTemplate('{{foo}}'));
         $this->assertEquals( '{% foo %}', $view->normalizeObjectTemplate('{% foo %}'));
         $this->assertEquals( '{{object.foo.fn({bar: baz})|raw}}', $view->normalizeObjectTemplate('{foo.fn({bar: baz})}'));
         $this->assertEquals( '{{object.foo.fn({bar: {baz: 1}})|raw}}', $view->normalizeObjectTemplate('{foo.fn({bar: {baz: 1}})}'));
         $this->assertEquals( '{{object.foo.fn(\'bar:baz\')|raw}}', $view->normalizeObjectTemplate('{foo.fn(\'bar:baz\')}'));
         $this->assertEquals( '{{object.foo.fn({\'bar\': baz})|raw}}', $view->normalizeObjectTemplate('{foo.fn({\'bar\': baz})}'));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     }
 
