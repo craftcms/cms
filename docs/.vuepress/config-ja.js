6fcc63e6dd4a70b7063c1674bb3b2b732f663edc
brandonkelly <brandon@pixelandtonic.com>
2018-06-25 17:18:56 -0700

    Updated config for Japanese translation

---
+++ docs/.vuepress/config-ja.js
@@ -0,0 +1,172 @@
+module.exports = {
+    selectText: 'Language', // todo: translate
+    label: 'Japanese', // todo: translate
+    editLinkText: 'Edit this page on GitHub', // todo: translate
+    // algolia: {
+    //     apiKey: '1014b55e7f916b20c5d6834bf7666dc3',
+    //     indexName: 'craftcms',
+    //     algoliaOptions: {
+    //         facetFilters: ['version:v3', 'tags:doc']
+    //     }
+    // },
+    sidebar: {
+        '/ja/element-query-params/': [
+            ['../element-queries', '← エレメントクエリ'],
+            {
+                title: 'エレメントのクエリパラメータ',
+                collapsable: false,
+                children: [
+                    ['asset-query-params', 'アセット'],
+                    ['category-query-params', 'カテゴリ'],
+                    ['entry-query-params', 'エントリ'],
+                    ['matrix-block-query-params', '行列ブロック'],
+                    ['tag-query-params', 'タグ'],
+                    ['user-query-params', 'ユーザー'],
+                ]
+            }
+        ],
+        '/ja/templating/': [
+            ['../twig-primer', '← Twig 入門書'],
+            {
+                title: 'タグ',
+                collapsable: false,
+                children: [
+                    'tags/cache',
+                    'tags/css',
+                    'tags/exit',
+                    'tags/header',
+                    'tags/js',
+                    'tags/nav',
+                    'tags/paginate',
+                    'tags/redirect',
+                    'tags/requirelogin',
+                    'tags/requirepermission',
+                    'tags/switch',
+                ]
+            },
+            'filters',
+            'functions',
+            'global-variables',
+            'tests',
+            'elements',
+            {
+                title: 'テンプレートの実例',
+                collapsable: false,
+                children: [
+                    'examples/integrating-disqus',
+                    'examples/rss-feed',
+                    'examples/atom-feed',
+                    'examples/entry-form',
+                    'examples/search-form',
+                    'examples/login-form',
+                    'examples/user-profile-form',
+                    'examples/user-registration-form',
+                    'examples/forgot-password-form',
+                    'examples/set-password-form',
+                ]
+            }
+        ],
+        '/ja/': [
+            {
+                title: '導入',
+                collapsable: false,
+                children: [
+                    '',
+                    'coc',
+                    ['how-to-use-the-documentation', 'このドキュメントの使い方'],
+                ]
+            },
+            {
+                title: 'Craft のインストール',
+                collapsable: false,
+                children: [
+                    'requirements',
+                    'installation',
+                ]
+            },
+            {
+                title: 'Craft のアップデート',
+                collapsable: false,
+                children: [
+                    'upgrade',
+                    'updating',
+                    'changes-in-craft-3',
+                ]
+            },
+            {
+                title: 'はじめに',
+                collapsable: false,
+                children: [
+                    'the-pieces-of-craft',
+                    'directory-structure',
+                ]
+            },
+            {
+                title: 'コアコンセプト',
+                collapsable: false,
+                children: [
+                    '/ja/sections-and-entries',
+                    'fields',
+                    'categories',
+                    'assets',
+                    'users',
+                    'globals',
+                    'tags',
+                    'routing',
+                    'searching',
+                    'sites',
+                    ['localization', 'ローカライゼーション'],
+                    'element-queries',
+                    'content-migrations',
+                    'configuration',
+                ]
+            },
+            {
+                title: 'テンプレート記法',
+                collapsable: false,
+                children: [
+                    'twig-primer',
+                    'templating/tags',
+                    'templating/filters',
+                    'templating/functions',
+                    'templating/global-variables',
+                    'templating/tests',
+                    'templating/elements',
+                    'templating/examples/',
+                ]
+            },
+            {
+                title: '高度なトピックス',
+                collapsable: false,
+                children: [
+                    'relations',
+                    'reference-tags',
+                    'eager-loading-elements',
+                ]
+            },
+            {
+                title: 'プラグイン開発',
+                collapsable: false,
+                children: [
+                    'plugin-intro',
+                    // 'coding-guidelines',
+                    // 'updating-plugins',
+                    // 'changelogs-and-updates',
+                    // 'plugin-settings',
+                    // 'cp-section',
+                    // 'asset-bundles',
+                    // 'services',
+                    // 'extending-twig',
+                    // 'widget-types',
+                    // 'field-types',
+                    // 'volume-types',
+                    // 'utility-types',
+                    // 'element-types',
+                    // 'element-action-types',
+                    // 'plugin-migrations',
+                    // 'plugin-store'
+                ]
+            }
+        ]
+    }
+};
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,26 @@
 module.exports = {
-    title: 'Craft 3 Documentation',
-    description: 'Craft 3 Documentation',
     theme: 'craftdocs',
-    base: '/v3/',
     ga: 'UA-39036834-9',
+    base: '/v3/',
+    locales: {
+        '/': {
+            lang: 'en-US',
+            title: 'Craft 3 Documentation',
+        },
+        '/ja/': {
+            lang: 'ja',
+            title: 'Craft 3 Documentation', // todo: translate
+        }
+    },
     themeConfig: {
         docsRepo: 'craftcms/docs',
         docsDir: 'docs',
         docsBranch: 'v3',
         editLinks: true,
+        locales: {
+            '/': require('./config-en'),
+            '/ja/': require('./config-ja')
+        },
         nav: [
             {
                 text: 'Craft CMS',
@@ -21,176 +33,9 @@ module.exports = {
             },
             { text: 'Craftnet API', link: 'https://docs.api.craftcms.com/' },
         ],
-        sidebar: {
-            '/element-query-params/': [
-                ['../element-queries', '← Element Queries'],
-                {
-                    title: 'Element Query Params',
-                    collapsable: false,
-                    children: [
-                        ['asset-query-params', 'Assets'],
-                        ['category-query-params', 'Categories'],
-                        ['entry-query-params', 'Entries'],
-                        ['matrix-block-query-params', 'Matrix Blocks'],
-                        ['tag-query-params', 'Tags'],
-                        ['user-query-params', 'Users'],
-                    ]
-                }
-            ],
-            '/templating/': [
-                ['../twig-primer', '← Twig Primer'],
-                {
-                    title: 'Tags',
-                    collapsable: false,
-                    children: [
-                        'tags/cache',
-                        'tags/css',
-                        'tags/exit',
-                        'tags/header',
-                        'tags/js',
-                        'tags/nav',
-                        'tags/paginate',
-                        'tags/redirect',
-                        'tags/requirelogin',
-                        'tags/requirepermission',
-                        'tags/switch',
-                    ]
-                },
-                'filters',
-                'functions',
-                'global-variables',
-                'tests',
-                'elements',
-                {
-                    title: 'Examples',
-                    collapsable: false,
-                    children: [
-                        'examples/integrating-disqus',
-                        'examples/rss-feed',
-                        'examples/atom-feed',
-                        'examples/entry-form',
-                        'examples/search-form',
-                        'examples/login-form',
-                        'examples/user-profile-form',
-                        'examples/user-registration-form',
-                        'examples/forgot-password-form',
-                        'examples/set-password-form',
-                    ]
-                }
-            ],
-            '/': [
-                {
-                    title: 'Introduction',
-                    collapsable: false,
-                    children: [
-                        '',
-                        'coc',
-                        'how-to-use-the-documentation'
-                    ]
-                },
-                {
-                    title: 'Installing Craft',
-                    collapsable: false,
-                    children: [
-                        'requirements',
-                        'installation'
-                    ]
-                },
-                {
-                    title: 'Updating Craft',
-                    collapsable: false,
-                    children: [
-                        'upgrade',
-                        'updating',
-                        'changes-in-craft-3'
-                    ]
-                },
-                {
-                    title: 'Getting Started',
-                    collapsable: false,
-                    children: [
-                        'the-pieces-of-craft',
-                        'directory-structure'
-                    ]
-                },
-                {
-                    title: 'Core Concepts',
-                    collapsable: false,
-                    children: [
-                        'sections-and-entries',
-                        'fields',
-                        'categories',
-                        'assets',
-                        'users',
-                        'globals',
-                        'tags',
-                        'routing',
-                        'searching',
-                        'sites',
-                        ['localization', 'Localization'],
-                        'element-queries',
-                        'content-migrations',
-                        'configuration'
-                    ]
-                },
-                {
-                    title: 'Templating',
-                    collapsable: false,
-                    children: [
-                        'twig-primer',
-                        'templating/tags',
-                        'templating/filters',
-                        'templating/functions',
-                        'templating/global-variables',
-                        'templating/tests',
-                        'templating/elements',
-                        'templating/examples/'
-                    ]
-                },
-                {
-                    title: 'Advanced Topics',
-                    collapsable: false,
-                    children: [
-                        'relations',
-                        'reference-tags',
-                        'eager-loading-elements',
-                    ]
-                },
-                {
-                    title: 'Plugin Development',
-                    collapsable: false,
-                    children: [
-                        'plugin-intro',
-                        'coding-guidelines',
-                        'updating-plugins',
-                        'changelogs-and-updates',
-                        'plugin-settings',
-                        'cp-section',
-                        'asset-bundles',
-                        'services',
-                        'extending-twig',
-                        'widget-types',
-                        'field-types',
-                        'volume-types',
-                        'utility-types',
-                        'element-types',
-                        'element-action-types',
-                        'plugin-migrations',
-                        'plugin-store'
-                    ]
-                }
-            ]
-        },
         codeLanguages: {
             twig: 'Twig',
             php: 'PHP',
-        },
-        algolia: {
-            apiKey: '1014b55e7f916b20c5d6834bf7666dc3',
-            indexName: 'craftcms',
-            algoliaOptions: {
-                facetFilters: ['version:v3', 'tags:doc']
-            }
         }
     },
     markdown: {
