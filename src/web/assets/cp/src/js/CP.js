d4843ef695be19200d6e9afdbe103059a47781ae
XhmikosR <xhmikosr@gmail.com>
2018-08-15 00:20:08 +0300

    Add `rel="noopener"` for external links.

--- src/web/assets/cp/src/js/CP.js
+++ src/web/assets/cp/src/js/CP.js
@@ -124,7 +124,7 @@ Craft.CP = Garnish.Base.extend(
             // hat tip: https://stackoverflow.com/a/2911045/1688568
             $('a').each(function() {
                 if (this.hostname.length && this.hostname !== location.hostname && typeof $(this).attr('target') === 'undefined') {
-                    $(this).attr('target', '_blank')
+                    $(this).attr('rel', 'noopener').attr('target', '_blank')
                 }
             });
         },
