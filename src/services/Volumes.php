cbb3bda74900da2d55f3cc20e542b7f08b7ef484
andris-sevcenko <asevcenko@gmail.com>
2018-08-16 13:22:09 +0300

    Cleanup.

--- src/services/Volumes.php
+++ src/services/Volumes.php
@@ -418,6 +418,7 @@ class Volumes extends Component
         }
 
         $projectConfig = Craft::$app->getProjectConfig();
+
         $configData = [
             'name' => $volume->name,
             'handle' => $volume->handle,
