a0d79d43a06e62d2531fddf231f89a21506d43ef
andris-sevcenko <asevcenko@gmail.com>
2018-08-17 13:10:57 +0300

    Migrate matrixBlockType field contexts to UIDs.

---
+++ src/migrations/m180520_173000_matrix_context_to_uids.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace craft\migrations;
+
+use Craft;
+use craft\db\Migration;
+use craft\db\Query;
+use craft\helpers\FileHelper;
+use craft\helpers\Json;
+use craft\helpers\ProjectConfig as ProjectConfigHelper;
+use craft\services\ProjectConfig;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * m180520_173000_matrix_context_to_uids migration.
+ */
+class m180520_173000_matrix_context_to_uids extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        // Get map of IDs to UIDs
+        $blockPairs = (new Query())
+            ->select(['id', 'uid'])
+            ->from('{{%matrixblocktypes}}')
+            ->pairs();
+
+        // Get all
+        $fields = (new Query())
+            ->select(['id', 'context'])
+            ->from('{{%fields}}')
+            ->where(['like', 'context', 'matrixBlockType'])
+            ->all();
+
+        // Switch out ids for UIDs
+        foreach ($fields as $row) {
+            $context = explode(':', $row['context']);
+            $newContext = $context[0].':'.$blockPairs[$context[1]];
+            $this->update('{{%fields}}', ['context' => $newContext], ['id' => $row['id']]);
+        }
+
+        return true;
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m180520_173000_matrix_context_to_uids cannot be reverted.\n";
+
+        return false;
+    }
+
+}
