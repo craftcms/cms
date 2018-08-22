fa96669188ed43e323e77f9f65adae32f93813ae
brandonkelly <brandon@pixelandtonic.com>
2018-08-22 15:52:14 -0700

    Fix hyperlink

--- src/config/GeneralConfig.php
+++ src/config/GeneralConfig.php
@@ -400,7 +400,7 @@ class GeneralConfig extends BaseObject
      * @var string The maximum amount of memory Craft will try to reserve during memory intensive operations such as zipping,
      * unzipping and updating. Defaults to an empty string, which means it will use as much memory as it possibly can.
      *
-     * See http://php.net/manual/en/faq.using.php#faq.using.shorthandbytes for a list of acceptable values.
+     * See <http://php.net/manual/en/faq.using.php#faq.using.shorthandbytes> for a list of acceptable values.
      */
     public $phpMaxMemoryLimit = '';
     /**
