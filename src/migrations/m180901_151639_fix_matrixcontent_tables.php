097c01e380682538c6d24c28f043518059c89595
brandonkelly <brandon@pixelandtonic.com>
2018-09-04 06:56:38 -0700

    Disable FK checks while duplicating Matrix content tables

--- src/migrations/m180901_151639_fix_matrixcontent_tables.php
+++ src/migrations/m180901_151639_fix_matrixcontent_tables.php
@@ -46,54 +46,63 @@ class m180901_151639_fix_matrixcontent_tables extends Migration
             ], [], false);
         }
 
-        foreach (array_keys($duplicateHandles) as $handle) {
-            $originalField = array_shift($fieldsByHandle[$handle]);
-            $originalTableName = $originalField['settings']['contentTable'];
-            $originalSchema = $this->db->getTableSchema($originalTableName);
+        if (!empty($duplicateHandles)) {
+            // Disable FK checks
+            $queryBuilder = $this->db->getSchema()->getQueryBuilder();
+            $this->execute($queryBuilder->checkIntegrity(false));
 
-            $originalFieldColumns = array_filter($originalSchema->getColumnNames(), function($columnName) {
-                return strpos($columnName, 'field_') === 0;
-            });
+            foreach (array_keys($duplicateHandles) as $handle) {
+                $originalField = array_shift($fieldsByHandle[$handle]);
+                $originalTableName = $originalField['settings']['contentTable'];
+                $originalSchema = $this->db->getTableSchema($originalTableName);
 
-            $seqStart = 1 + (new Query())
-                ->from([$originalTableName])
-                ->max('[[id]]');
+                $originalFieldColumns = array_filter($originalSchema->getColumnNames(), function($columnName) {
+                    return strpos($columnName, 'field_') === 0;
+                });
 
-            foreach ($fieldsByHandle[$handle] as $i => $field) {
-                // duplicate the table for this field, and clean it up
-                $tableName = $field['settings']['contentTable'];
-                $this->execute("create table {$tableName} as select * from {$originalTableName}");
+                $seqStart = 1 + (new Query())
+                        ->from([$originalTableName])
+                        ->max('[[id]]');
 
-                if ($this->db->getIsPgsql()) {
-                    // Manually construct the SQL for Postgres
-                    // (see https://github.com/yiisoft/yii2/issues/12077)
-                    $this->addPrimaryKey(null, $tableName, ['id']);
-                    $rawTableName = $this->db->tablePrefix . trim($tableName, '{}%');
-                    $seqName = $rawTableName . '_id_seq';
-                    $this->execute("create sequence [[{$seqName}]] start with {$seqStart} owned by [[{$rawTableName}.id]]");
-                    $this->execute("alter table {$tableName} alter column [[id]] set default nextval('{$seqName}')");
-                    $this->execute("alter table {$tableName} alter column [[elementId]] set not null");
-                    $this->execute("alter table {$tableName} alter column [[siteId]] set not null");
-                    $this->execute("alter table {$tableName} alter column [[dateCreated]] set not null");
-                    $this->execute("alter table {$tableName} alter column [[dateUpdated]] set not null");
-                    $this->execute("alter table {$tableName} alter column [[uid]] set not null");
-                } else {
-                    $this->alterColumn($tableName, 'id', $this->primaryKey());
-                    $this->alterColumn($tableName, 'elementId', $this->integer()->notNull());
-                    $this->alterColumn($tableName, 'siteId', $this->integer()->notNull());
-                    $this->alterColumn($tableName, 'dateCreated', $this->dateTime()->notNull());
-                    $this->alterColumn($tableName, 'dateUpdated', $this->dateTime()->notNull());
-                    $this->alterColumn($tableName, 'uid', $this->uid());
+                foreach ($fieldsByHandle[$handle] as $i => $field) {
+                    // duplicate the table for this field, and clean it up
+                    $tableName = $field['settings']['contentTable'];
+                    $this->execute("create table {$tableName} as select * from {$originalTableName}");
+
+                    if ($this->db->getIsPgsql()) {
+                        // Manually construct the SQL for Postgres
+                        // (see https://github.com/yiisoft/yii2/issues/12077)
+                        $this->addPrimaryKey(null, $tableName, ['id']);
+                        $rawTableName = $this->db->tablePrefix . trim($tableName, '{}%');
+                        $seqName = $rawTableName . '_id_seq';
+                        $this->execute("create sequence [[{$seqName}]] start with {$seqStart} owned by [[{$rawTableName}.id]]");
+                        $this->execute("alter table {$tableName} alter column [[id]] set default nextval('{$seqName}')");
+                        $this->execute("alter table {$tableName} alter column [[elementId]] set not null");
+                        $this->execute("alter table {$tableName} alter column [[siteId]] set not null");
+                        $this->execute("alter table {$tableName} alter column [[dateCreated]] set not null");
+                        $this->execute("alter table {$tableName} alter column [[dateUpdated]] set not null");
+                        $this->execute("alter table {$tableName} alter column [[uid]] set not null");
+                    } else {
+                        $this->alterColumn($tableName, 'id', $this->primaryKey());
+                        $this->alterColumn($tableName, 'elementId', $this->integer()->notNull());
+                        $this->alterColumn($tableName, 'siteId', $this->integer()->notNull());
+                        $this->alterColumn($tableName, 'dateCreated', $this->dateTime()->notNull());
+                        $this->alterColumn($tableName, 'dateUpdated', $this->dateTime()->notNull());
+                        $this->alterColumn($tableName, 'uid', $this->uid());
+                    }
+
+                    $this->createIndex(null, $tableName, ['elementId', 'siteId'], true);
+                    $this->addForeignKey(null, $tableName, ['elementId'], '{{%elements}}', ['id'], 'CASCADE', null);
+                    $this->addForeignKey(null, $tableName, ['siteId'], '{{%sites}}', ['id'], 'CASCADE', 'CASCADE');
+                    $this->_cleanUpTable($field['id'], $tableName, $originalFieldColumns);
                 }
 
-                $this->createIndex(null, $tableName, ['elementId', 'siteId'], true);
-                $this->addForeignKey(null, $tableName, ['elementId'], '{{%elements}}', ['id'], 'CASCADE', null);
-                $this->addForeignKey(null, $tableName, ['siteId'], '{{%sites}}', ['id'], 'CASCADE', 'CASCADE');
-                $this->_cleanUpTable($field['id'], $tableName, $originalFieldColumns);
+                // now clean up the original table
+                $this->_cleanUpTable($originalField['id'], $originalTableName, $originalFieldColumns);
             }
 
-            // now clean up the original table
-            $this->_cleanUpTable($originalField['id'], $originalTableName, $originalFieldColumns);
+            // Re-enable FK checks
+            $this->execute($queryBuilder->checkIntegrity(true));
         }
     }
 
