b404cd5fb8b50a02185d751cfb537f4f5e151406
Giel Tettelaar <water.giel@gmail.com>
2018-10-10 21:31:48 +0200

    Finalized the baseline test and 100% coverage* on configHelper
    * This doesnt mean we might not have missed something

--- tests/unit/helpers/ConfigHelperTest.php
+++ tests/unit/helpers/ConfigHelperTest.php
@@ -11,6 +11,7 @@ namespace craftunit\helpers;
 
 use Codeception\Test\Unit;
 use craft\helpers\ConfigHelper;
+use craftunit\support\mockclasses\models\ExampleModel;
 use PHPUnit\Util\ConfigurationGenerator;
 use yii\base\ErrorException;
 use yii\base\InvalidConfigException;
@@ -43,8 +44,15 @@ class ConfigHelperTest extends Unit
     public function sizeInBytesData()
     {
         return [
+            [5368709120.0, '5G'],
+            [5242880, '5M'],
+            [5120, '5K'],
+            [5120, 'ABCDEFHIJFLKNOPQRSTUVWXYZ5K'],
+            [5, '5ABCDEFHIJFKLKNOPQRSTUVWXYZ'],
+            [5120, '!@#$%^&*()5K'],
             [4, '4'],
             [5, 5],
+            [0, 'M5'],
         ];
     }
 
@@ -90,5 +98,40 @@ class ConfigHelperTest extends Unit
         $this->tester->expectException(ErrorException::class, function (){
             ConfigHelper::durationInSeconds(new \stdClass('2018-08-08 20:0:00'));
         });
+    }
+
+    /**
+     * @dataProvider localizedValueData
+     *
+     * @param $result
+     * @param $input
+     */
+    public function testLocalizedValue($result, $input, $handle = null)
+    {
+        $this->assertSame($result, ConfigHelper::localizedValue($input, $handle));
+    }
+
+    public function localizedValueData()
+    {
+        $exampleModel = new ExampleModel();
+        $exampleModel->exampleParam = 'imaparam';
+
+        return [
+            // Ensure if array that it is accesed by the handle and returns the value of the index.
+            ['imavalue', ['imahandle' => 'imavalue'], 'imahandle'],
+
+            // If variable is callable.  Ensure the handle gets passed into the callable.
+            ['imahandle', function($handle){  return $handle; }, 'imahandle'],
+            ['imaparam', $exampleModel, null],
+            [reset($exampleModel), $exampleModel, null],
+            ['imnotavalue', ['imnotahandle' => 'imnotavalue'], 'imahandle'],
+            ['string', 'string', null],
+            ['', '', null],
+            [123, 123, null],
+            [false, false, null],
+            [true, true, null],
+            [12345678901234567890,12345678901234567890 , null],
+
+        ];
     }
 }
