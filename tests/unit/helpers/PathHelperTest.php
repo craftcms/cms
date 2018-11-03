a7e11e06057be1b5e54bb8a3b0b2db14847b45f2
Giel Tettelaar <water.giel@gmail.com>
2018-11-03 13:20:57 +0100

    Added baseline pathHelperTest

---
+++ tests/unit/helpers/PathHelperTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @link      https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license   https://craftcms.github.io/license/
+ */
+
+namespace craftunit\helpers;
+
+use Codeception\Test\Unit;
+
+
+/**
+ * Class PathHelperTest.
+ *
+ * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @author Global Network Group | Giel Tettelaar <giel@yellowflash.net>
+ * @since  3.0
+ */
+class PathHelperTest extends Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+}
