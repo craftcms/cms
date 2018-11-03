9a7339f98d658f0adbefe70be9b5546c4d6ac708
Giel Tettelaar <water.giel@gmail.com>
2018-11-03 12:25:19 +0100

    Added Baseline numberHelperTest

---
+++ tests/unit/helpers/NumberHelperTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @link      https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license   https://craftcms.github.io/license/
+ */
+
+
+namespace craftunit\helpers;
+
+
+use Codeception\Test\Unit;
+use craft\helpers\Number;
+
+/**
+ * Class NumberHelperTest.
+ *
+ * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @author Global Network Group | Giel Tettelaar <giel@yellowflash.net>
+ * @since  3.0
+ */
+class NumberHelperTest extends Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+}
