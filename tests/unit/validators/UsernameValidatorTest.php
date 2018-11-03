72d4f693add59e5add970546a01b76e9f747f33e
Giel Tettelaar <water.giel@gmail.com>
2018-11-03 15:29:40 +0100

    Added tests for the UsernameValidator

---
+++ tests/unit/validators/UsernameValidatorTest.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @link https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license https://craftcms.github.io/license/
+ */
+
+namespace craftunit\validators;
+
+use Codeception\Test\Unit;
+use craft\validators\UsernameValidator;
+
+
+/**
+ * Class UsernameValidatorTest.
+ *
+ * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @author Global Network Group | Giel Tettelaar <giel@yellowflash.net>
+ * @since  3.0
+ */
+class UsernameValidatorTest extends Unit
+{
+    /**
+     * @var UsernameValidator
+     */
+    protected $usernameValidator;
+
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    public function _before()
+    {
+        $this->usernameValidator = new UsernameValidator();
+    }
+
+    /**
+     * @dataProvider validateValueData
+     * @param $result
+     * @param $input
+     */
+    public function testValidateValue($result, $input)
+    {
+        $validated = $this->usernameValidator->validateValue($input);
+        $this->assertSame($result, $validated);
+    }
+    public function validateValueData()
+    {
+        return [
+            [null, 'asdfghjkl1234567890'],
+            [['{attribute} cannot contain spaces.', []], '  '],
+            [null, null],
+            [null, '!@#$%^&*()'],
+            [['{attribute} cannot contain spaces.', []], 'dsasadsdasasad    adsdasdassdasad'],
+        ];
+    }
+}
