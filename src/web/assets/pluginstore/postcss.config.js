ec56ac5468f2c26b711266c95019d6944033eab9
Benjamin David <ben@dukt.net>
2018-11-20 01:50:43 +0100

    Tailwind CSS

--- src/web/assets/pluginstore/postcss.config.js
+++ src/web/assets/pluginstore/postcss.config.js
@@ -1,5 +1,8 @@
+var tailwindcss = require('tailwindcss');
+
 module.exports = {
-  plugins: {
-    autoprefixer: {}
-  }
+    plugins: [
+        tailwindcss('./tailwind.config.js'),
+        require('autoprefixer')
+    ],
 }
