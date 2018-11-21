46daafffaac1627bb38ae748388379956ab867b2
Benjamin David <ben@dukt.net>
2018-11-21 23:34:17 +0100

    Added Sidebar and CategorySelector components

--- src/web/assets/pluginstore/src/App.vue
+++ src/web/assets/pluginstore/src/App.vue
@@ -1,7 +1,17 @@
 <template>
     <div>
         <template v-if="$root.pluginStoreDataLoaded && !$root.pluginStoreDataError">
-            <router-view></router-view>
+            <plugin-search @showResults="showingSearchResults = true" @hideResults="showingSearchResults = false" :plugins="plugins"></plugin-search>
+
+            <category-selector></category-selector>
+
+            <div v-if="!showingSearchResults" class="ps-grid-wrapper has-sidebar">
+                <sidebar></sidebar>
+
+                <div class="ps-grid-main">
+                    <router-view></router-view>
+                </div>
+            </div>
         </template>
         <template v-else>
             <status-message :error="$root.pluginStoreDataError" :message="$root.statusMessage"></status-message>
@@ -13,16 +23,36 @@
 
 <script>
     import './sass/main.scss'
-
+    import {mapState} from 'vuex'
+    import CategorySelector from './js/components/CategorySelector'
+    import Sidebar from './js/components/Sidebar'
+    import PluginSearch from './js/components/PluginSearch'
     import Modal from './js/components/modal/Modal'
     import StatusMessage from './js/components/StatusMessage'
 
     export default {
 
         components: {
+            CategorySelector,
+            Sidebar,
+            PluginSearch,
             Modal,
             StatusMessage,
-        }
+        },
 
+        data() {
+            return {
+                showingSearchResults: false,
+            }
+        },
+
+
+        computed: {
+
+            ...mapState({
+                categories: state => state.pluginStore.categories,
+                plugins: state => state.pluginStore.plugins,
+            }),
+        }
     }
 </script>
