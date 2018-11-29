90abb2a5223e3ca56e60daf44e7ed9da00f23148
Benjamin David <ben@dukt.net>
2018-11-29 14:36:24 +0100

    HMR

--- src/web/assets/pluginstore/vue.config.js
+++ src/web/assets/pluginstore/vue.config.js
@@ -2,21 +2,27 @@ var fs = require('fs');
 
 module.exports = {
      filenameHashing: false,
+    // only set this when hmr is on?
+    baseUrl: 'https://192.168.1.72:8080',
      configureWebpack: {
          externals: {
              'vue': 'Vue',
              'vue-router': 'VueRouter',
              'vuex': 'Vuex',
              'axios': 'axios'
-         }
+         },
      },
      devServer: {
+         hotOnly: true,
+         proxy: 'https://192.168.1.72:8080',
+         public: '192.168.1.72:8080',
          headers: { "Access-Control-Allow-Origin": "*" },
          https: {
              key: fs.readFileSync('../../../../../../ssl/pluginstore.dev.key'),
              cert: fs.readFileSync('../../../../../../ssl/pluginstore.dev.crt'),
          },
-     }
+     },
+
     chainWebpack: config => {
          config.module
              .rule('images')
