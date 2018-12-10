5ac9e5d6e777903f6e94a37e971314b472267539
Benjamin David <ben@dukt.net>
2018-12-10 14:22:38 +0100

    Fixed missing link icon

--- src/web/assets/pluginstore/src/js/plugins/font-awesome.js
+++ src/web/assets/pluginstore/src/js/plugins/font-awesome.js
@@ -3,8 +3,8 @@ import Vue from 'vue'
 // Font Awesome
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheck, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
-library.add([ faCheck, faInfoCircle ])
+import { faCheck, faInfoCircle, faLink } from '@fortawesome/free-solid-svg-icons'
+library.add([ faCheck, faInfoCircle, faLink ])
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
