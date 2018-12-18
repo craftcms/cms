d0022670e789bdcb66fe467ab606439015407bde
Giel Tettelaar <giel@yellowflash.net>
2018-12-18 12:03:24 +0100

    Revert "Revert "Merge branch 'dev-tests' of https://github.com/lemiwinkz/cms into develop""
    This reverts commit 9dc1f903890e6f0ce7432f8ea1029fbbc7d00c02.

---
+++ tests/unit/validators/UriValidatorTest.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * @link      https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license   https://craftcms.github.io/license/
+ */
+
+namespace craftunit\validators;
+
+
+use Codeception\Test\Unit;
+use craft\test\mockclasses\models\ExampleModel;
+use craft\validators\UriValidator;
+
+/**
+ * Class UriValidatorTest.
+ *
+ * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @author Global Network Group | Giel Tettelaar <giel@yellowflash.net>
+ * @since  3.0
+ */
+class UriValidatorTest extends Unit
+{
+    /**
+     * @var UriValidator
+     */
+    protected $uriValidator;
+
+    /**
+     * @var ExampleModel
+     */
+    protected $model;
+    /*
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    public function _before()
+    {
+        $this->model = new ExampleModel();
+        $this->uriValidator = new UriValidator();
+    }
+
+    /**
+     * @dataProvider validateValueData
+     * @param      $mustValidate
+     * @param      $input
+     * @param null $pattern
+     */
+    public function testValidateValue($mustValidate, $input, $pattern = null)
+    {
+        if ($pattern) {
+            $this->uriValidator->pattern = $pattern;
+        }
+
+        $validates = $this->uriValidator->validate($input);
+
+        if ($mustValidate) {
+            $this->assertTrue($validates);
+        } else {
+            $this->assertFalse($validates);
+        }
+    }
+    public function validateValueData()
+    {
+        return [
+            [true, 'test'],
+            [true, ' '],
+            [false, 'integer', '/^\w+\((\d+)\)/'],
+            [true, 'integer(9)', '/^\w+\((\d+)\)/'],
+        ];
+    }
+}
