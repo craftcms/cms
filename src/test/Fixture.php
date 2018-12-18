5ede6b9486dcd3f42419b1a8d84935ba025ea4c3
Giel Tettelaar <giel@yellowflash.net>
2018-12-18 11:48:31 +0100

    Better explanation and added a base fixture file

---
+++ src/test/Fixture.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Giel Tettelaar
+ * Date: 18/12/2018
+ * Time: 11:40
+ */
+
+namespace craft\test;
+
+
+use yii\test\ActiveFixture;
+
+class Fixture extends ActiveFixture
+{
+    public function unload() {
+
+    }
+}
