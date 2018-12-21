3ae39d22a8b42aaa6ccf42331ff0c4b826a28a3d
Giel Tettelaar <giel@yellowflash.net>
2018-12-21 16:56:31 +0100

    Added a note and basic mailer test.

---
+++ tests/unit/mail/MailerTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @link https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license https://craftcms.github.io/license/
+ */
+
+
+/**
+ * Unit tests for MailerTest
+ *
+ * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @author Global Network Group | Giel Tettelaar <giel@yellowflash.net>
+ * @since 3.0
+ */
+class MailerTest
+{
+
+}
