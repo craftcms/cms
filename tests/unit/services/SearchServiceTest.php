c16ee0c677208c4178b4227d9a618c33dda777da
Giel Tettelaar <giel@yellowflash.net>
2018-12-22 14:32:58 +0100

    Added more tests to the search service

--- tests/unit/services/SearchServiceTest.php
+++ tests/unit/services/SearchServiceTest.php
@@ -47,28 +47,71 @@ class SearchServiceTest extends Unit
         $result = $this->usernameEmailArrayToIdList($usernameOrEmailsForResult);
         $forQuery = $this->usernameEmailArrayToIdList($usernameOrEmailsForQuery);
 
-
-        $filtered = \Craft::$app->getSearch()->filterElementIdsByQuery($forQuery, $query, $scoreResults, $siteId. $returnScores);
+        $filtered = \Craft::$app->getSearch()->filterElementIdsByQuery($forQuery, $query, $scoreResults, $siteId, $returnScores);
 
         $this->assertSame($result, $filtered);
     }
+
+    /**
+     * Provide an array with input usernames
+     * @return array
+     */
     public function filterElementIdByQueryData()
     {
         return [
+            [['user4', 'user1', 'user2', 'user3'], ['user1', 'user2', 'user3', 'user4'], 'user', true, 1, false],
+            [['user4', 'user1', 'user2', 'user3'], [], 'user', true, 1, false],
+            [['user1', 'user2', 'user3'], ['user1', 'user2', 'user3'], 'user', true, 1, false],
+            [['user4'], ['user1', 'user2', 'user3', 'user4'], 'user someemail', true, 1, false],
+            [[], ['user1', 'user2', 'user3'], 'user someemail', true, 1, false ],
+
+            // User4 goes first as it has both user and someemail keywords
+            [['user4', 'user1', 'user2', 'user3'], ['user1', 'user2', 'user3', 'user4'], 'user OR someemail', true, 1, false],
+            // TODO: [[], ['user1', 'user2', 'user3', 'user4'],'user OR someemail', false, 1, false],
+            [['user4', 'user1'], ['user1', 'user2', 'user3', 'user4'],'someemail OR -firstName:*', true, 1, false],
+        ];
+    }
+
+    /**
+     * @dataProvider filterScoresData
+     * @param $result
+     * @param $elementIds
+     * @param $query
+     * @param bool $scoreResults
+     * @param null $siteId
+     * @param bool $returnScores
+     */
+    public function testFilterScores($scoresAndNames, $usernameOrEmailsForQuery, $query, $scoreResults = true, $siteId = null)
+    {
+        // Repackage the given emails/username into
+        $result = $this->scoreList($scoresAndNames, true, true);
+        $forQuery = $this->usernameEmailArrayToIdList($usernameOrEmailsForQuery);
+
+        $filtered = \Craft::$app->getSearch()->filterElementIdsByQuery($forQuery, $query, $scoreResults, $siteId, true);
+
+        $this->assertSame($result, $filtered);
+    }
+    public function filterScoresData()
+    {
+        return [
+            [[['identifier' => 'user1', 'score' => 13.333333333333332]], ['user1'], 'user', true, 1],
             [
-                ['user1', 'user2', 'user3'],
-                ['user1', 'user2', 'user3'],
-                'user',
-                true,
-                1
+                [
+                    ['identifier' => 'user4', 'score' => 118.33333333333333],
+                    ['identifier' => 'user1', 'score' => 13.333333333333332],
+                    ['identifier' => 'user2', 'score' => 13.333333333333332],
+                    ['identifier' => 'user3', 'score' => 13.333333333333332]
+                ], ['user1', 'user2', 'user3', 'user4'], 'user', true, 1
             ],
             [
-                ['user4'],
-                ['user1', 'user2', 'user3', 'user4'],
-                'user someemail',
-                true,
-                1
-            ]
+                [
+                    ['identifier' => 'user4', 'score' => 118.33333333333333],
+                    ['identifier' => 'user1', 'score' => 13.333333333333332],
+                    ['identifier' => 'user2', 'score' => 13.333333333333332],
+                    ['identifier' => 'user3', 'score' => 13.333333333333332]
+                ], [], 'user', true, 1
+            ],
+
         ];
     }
 
@@ -88,6 +131,18 @@ class SearchServiceTest extends Unit
         foreach ($usernameOrEmails as $usernameOrEmail) {
             $userId = $this->getUserIdByEmailOrUserName($usernameOrEmail)->id;
             $ids[] = $typecastToInt === true ? (int)$userId : $userId;
+
+        }
+
+        return $ids;
+    }
+
+    private function scoreList(array $usernameOrEmailsAndScores)
+    {
+        $ids = [];
+        foreach ($usernameOrEmailsAndScores as $usernameOrEmailAndScore) {
+            $userId = $this->getUserIdByEmailOrUserName($usernameOrEmailAndScore['identifier'])->id;
+            $ids[$userId] = $usernameOrEmailAndScore['score'];
         }
 
         return $ids;
