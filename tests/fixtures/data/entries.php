c7682aa7f993bce499a9219620384326efc0c62c
Giel Tettelaar <giel@yellowflash.net>
2018-12-22 11:12:20 +0100

    Dont need this anymore

--- tests/fixtures/data/entries.php
+++ tests/fixtures/data/entries.php
@@ -1,15 +1,9 @@
 <?php
 
-if ($this->sectionIds && $this->typeIds) {
-    return [
-        [
-            'sectionId' => '1000',
-            'typeId' => '1000',
-            'title' => 'Theories of life',
-        ],
-    ];
-} else {
-    return [
-
-    ];
-}
+return [
+    [
+        'sectionId' => '1000',
+        'typeId' => '1000',
+        'title' => 'Theories of life',
+    ],
+];
