1294d47a3d0777741be0704281cb3613b98aed61
Giel Tettelaar <giel@yellowflash.net>
2018-12-29 11:21:31 +0100

    More tests for the ControllerTest methods

--- tests/unit/web/ControllerTest.php
+++ tests/unit/web/ControllerTest.php
@@ -10,10 +10,10 @@ namespace craftunit\web;
 
 
 use Codeception\Test\Unit;
-use craft\helpers\ArrayHelper;
-use craft\test\Craft;
+use craft\helpers\UrlHelper;
 use craft\test\mockclasses\controllers\TestController;
-use craft\web\Application;
+use craft\web\Response;
+use craft\web\View;
 use yii\base\Action;
 use yii\base\ExitException;
 use yiiunit\TestCase;
@@ -42,7 +42,7 @@ class ControllerTest extends Unit
         $_SERVER['REQUEST_URI'] = 'https://craftcms.com/admin/dashboard';
         $this->controller = new TestController('test', \Craft::$app);
     }
-    public function testStuff()
+    public function testBeforeAction()
     {
         $this->tester->expectThrowable(ExitException::class, function () {
             // AllowAnonymous should redirect and Craft::$app->exit(); I.E. An exit exception
@@ -52,5 +52,54 @@ class ControllerTest extends Unit
         $this->assertTrue($this->controller->beforeAction(new Action('allow-anonymous', $this->controller)));
     }
 
+    public function testRunActionJsonError()
+    {
+        // We accept JSON.
+        \Craft::$app->getRequest()->setAcceptableContentTypes(['application/json' => true]);
+        \Craft::$app->getRequest()->headers->set('Accept', 'application/json');
 
+        /* @var Response $resp */
+        $resp = $this->controller->runAction('me-dont-exist');
+
+        // As long as this is set. We can expect yii to do its thing.
+        $this->assertSame(Response::FORMAT_JSON, $resp->format);
+    }
+
+    public function testTemplateRendering()
+    {
+        // We need to render a template from the site dir.
+        \Craft::$app->getView()->setTemplateMode(View::TEMPLATE_MODE_SITE);
+
+        $response = $this->controller->renderTemplate('template');
+
+        // Again. If this is all good. We can expect Yii to do its thing.
+        $this->assertSame('Im a template!', $response->data);
+        $this->assertSame(Response::FORMAT_RAW, $response->format);
+        $this->assertSame('text/html; charset=UTF-8', $response->getHeaders()->get('content-type'));
+    }
+
+    /**
+     * If the content-type headers are already set. Render Template should ignore attempting to set them.
+     * @throws \yii\base\Exception
+     */
+    public function testTemplateRenderingIfHeadersAlreadySet()
+    {
+        // We need to render a template from the site dir.
+        \Craft::$app->getView()->setTemplateMode(View::TEMPLATE_MODE_SITE);
+        \Craft::$app->getResponse()->getHeaders()->set('content-type', 'HEADERS');
+
+        $response = $this->controller->renderTemplate('template');
+
+        // Again. If this is all good. We can expect Yii to do its thing.
+        $this->assertSame('Im a template!', $response->data);
+        $this->assertSame(Response::FORMAT_RAW, $response->format);
+        $this->assertSame('HEADERS', $response->getHeaders()->get('content-type'));
+    }
+
+    private function setMockUser()
+    {
+        \Craft::$app->getUser()->setIdentity(
+            \Craft::$app->getUsers()->getUserById('1')
+        );
+    }
 }
