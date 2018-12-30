9e968f8a4f641355d8b47f8eb4e7f93ecbe91057
Brad Bell <brad@pixelandtonic.com>
2018-12-30 10:31:03 -0800

    Update UID route token to use StringHelper::UUID_V4_REGEX

--- src/config/cproutes/common.php
+++ src/config/cproutes/common.php
@@ -1,5 +1,7 @@
 <?php
 
+use craft\helpers\StringHelper;
+
 return [
     'categories' => 'categories/category-index',
     'categories/<groupHandle:{handle}>' => 'categories/category-index',
@@ -49,7 +51,7 @@ return [
                 'day' => '(?:0?[1-9]|[12][0-9]|3[01])',
                 'number' => '\d+',
                 'page' => '\d+',
-                'uid' => '[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}',
+                'uid' => StringHelper::UUID_V4_REGEX,
                 'slug' => '[^\/]+',
                 'tag' => '[^\/]+',
                 '*' => '[^\/]+',
