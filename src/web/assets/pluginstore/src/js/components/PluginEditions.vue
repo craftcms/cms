1644c5037f0b8a2d39ff8e3ec6f2c5a92fba3109
Benjamin David <ben@dukt.net>
2018-12-31 14:49:25 +0100

    Improved plugin editions for small screens

--- src/web/assets/pluginstore/src/js/components/PluginEditions.vue
+++ src/web/assets/pluginstore/src/js/components/PluginEditions.vue
@@ -20,10 +20,26 @@
 
 <style lang="scss">
     .plugin-editions {
-        @apply .flex .-mx-4 .justify-center;
+        @apply .py-6;
 
         .plugin-editions-edition {
-            @apply .w-1/3 .mx-4;
+            &:not(:last-child) {
+                @apply .mb-6;
+            }
+        }
+    }
+
+    @media (min-width: 992px) {
+        .plugin-editions {
+            @apply .flex .-mx-4 .justify-center;
+
+            .plugin-editions-edition {
+                @apply .w-1/3 .mx-4;
+
+                &:not(:last-child) {
+                    @apply .mb-0;
+                }
+            }
         }
     }
 </style>
