89210bd24cb77a5491ef685f433a88e007456b06
Benjamin David <ben@dukt.net>
2019-01-02 01:56:35 +0100

    Config based on `.env` file

--- src/web/assets/pluginstore/vue.config.js
+++ src/web/assets/pluginstore/vue.config.js
@@ -1,35 +1,34 @@
 var fs = require('fs');
 
 module.exports = {
-     filenameHashing: false,
-    // only set this when hmr is on?
-    baseUrl: 'https://192.168.1.72:8080',
-     configureWebpack: {
-         externals: {
-             'vue': 'Vue',
-             'vue-router': 'VueRouter',
-             'vuex': 'Vuex',
-             'axios': 'axios'
-         },
-     },
-     devServer: {
-         proxy: 'https://192.168.1.72:8080',
-         public: '192.168.1.72:8080',
-         headers: { "Access-Control-Allow-Origin": "*" },
-         https: {
-             key: fs.readFileSync('../../../../../../ssl/pluginstore.dev.key'),
-             cert: fs.readFileSync('../../../../../../ssl/pluginstore.dev.crt'),
-         },
-     },
+    filenameHashing: false,
+    baseUrl: process.env.NODE_ENV === 'development' ? process.env.DEV_BASE_URL : process.env.PROD_BASE_URL,
+    configureWebpack: {
+        externals: {
+            'vue': 'Vue',
+            'vue-router': 'VueRouter',
+            'vuex': 'Vuex',
+            'axios': 'axios'
+        },
+    },
+    devServer: {
+        hotOnly: true,
+        port: process.env.DEV_SERVER_PORT,
+        headers: {"Access-Control-Allow-Origin": "*"},
+        https: {
+            key: process.env.DEV_SERVER_SSL_KEY ? fs.readFileSync(process.env.DEV_SERVER_SSL_KEY) : null,
+            cert: process.env.DEV_SERVER_SSL_CERT ? fs.readFileSync(process.env.DEV_SERVER_SSL_CERT) : null,
+        },
+    },
 
     chainWebpack: config => {
-         config.module
-             .rule('images')
-             .use('url-loader')
-                .tap(options => {
-                    options.limit = -1
-                    options.fallback.options.name = 'images/[name].[ext]'
-                    return options
-                })
+        config.module
+            .rule('images')
+            .use('url-loader')
+            .tap(options => {
+                options.limit = -1
+                options.fallback.options.name = 'images/[name].[ext]'
+                return options
+            })
     }
- }
+}
