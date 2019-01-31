dddb01a4c9f244a0f988466793dee28a13b4428a
brandonkelly <brandon@pixelandtonic.com>
2019-01-30 17:19:02 -0800

    Updated changelog + docs

--- src/elements/db/UserQuery.php
+++ src/elements/db/UserQuery.php
@@ -514,10 +514,10 @@ class UserQuery extends ElementQuery
      * | Value | Fetches {elements}…
      * | - | -
      * | `'active'` _(default)_ | with active accounts.
-     * | `'locked'` | with locked accounts.
      * | `'suspended'` | with suspended accounts.
      * | `'pending'` | with accounts that are still pending activation.
-     * | `['active', 'locked']` | with active or locked accounts.
+     * | `'locked'` | with locked accounts (regardless of whether they’re active or suspended).
+     * | `['active', 'suspended']` | with active or suspended accounts.
      *
      * ---
      *
