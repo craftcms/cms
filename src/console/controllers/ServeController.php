55289c4f78d7383fa61a02506b3f1d6e16c3908d
Mark Huot <mark@markhuot.com>
2019-02-01 10:49:23 -0500

    Switching to better alias

--- src/console/controllers/ServeController.php
+++ src/console/controllers/ServeController.php
@@ -9,6 +9,6 @@ class ServeController extends BaseServeController {
     /**
      * @var string path or [path alias](guide:concept-aliases) to directory to serve
      */
-    public $docroot = '@root/web';
+    public $docroot = '@webroot';
 
 }
