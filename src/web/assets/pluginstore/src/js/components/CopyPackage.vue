f86d50bba1cfd963088f5b322fed89088008d499
Benjamin David <ben@dukt.net>
2019-03-07 18:41:04 +0100

    Added a “Package Name” section to the Plugin Store’s plugin screens.
    Fixed #2757

---
+++ src/web/assets/pluginstore/src/js/components/CopyPackage.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="copy-package">
+        <input ref="input" type="text" class="text w-full" :value="plugin.packageName" @focus="select" readonly="readonly" />
+        <btn @click="copy"><icon icon="copy" /></btn>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['plugin'],
+
+        methods: {
+            select() {
+                this.$refs.input.select()
+            },
+
+            copy() {
+                this.select();
+
+                window.document.execCommand('copy');
+            },
+        }
+    }
+</script>
+
+<style lang="scss">
+    .copy-package {
+        @apply .flex;
+
+        input {
+            @apply .font-mono;
+            border-top-right-radius: 0 !important;
+            border-bottom-right-radius: 0 !important;
+        }
+
+        button {
+            @apply .text-black .border .rounded-l-none .border-l-0;
+        }
+    }
+</style>
