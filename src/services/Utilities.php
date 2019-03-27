0a7e0897ebf3c2576485eb2d8720a7b9a5cda0b5
andris-sevcenko <asevcenko@gmail.com>
2019-03-27 09:34:33 +0200

    Initial work on project config rebuilder.

--- src/services/Utilities.php
+++ src/services/Utilities.php
@@ -17,6 +17,7 @@ use craft\utilities\DeprecationErrors;
 use craft\utilities\FindAndReplace;
 use craft\utilities\Migrations;
 use craft\utilities\PhpInfo;
+use craft\utilities\RebuildProjectConfig;
 use craft\utilities\SearchIndexes;
 use craft\utilities\SystemMessages as SystemMessagesUtility;
 use craft\utilities\SystemReport;
@@ -88,6 +89,7 @@ class Utilities extends Component
         $utilityTypes[] = DbBackup::class;
         $utilityTypes[] = FindAndReplace::class;
         $utilityTypes[] = Migrations::class;
+        $utilityTypes[] = RebuildProjectConfig::class;
 
         $event = new RegisterComponentTypesEvent([
             'types' => $utilityTypes
