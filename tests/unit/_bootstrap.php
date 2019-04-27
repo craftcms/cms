bb14d4af9d5bb4000c2d41f4624113213609fd48
lemiwinkz <giel@yellowflash.net>
2019-04-27 19:07:42 +0200

    Git commit from Tower

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -1,9 +1,5 @@
 <?php
 // Here you can initialize variables that will be available to your tests
-
-
-
-
 // Autoload the fixtures
 \Codeception\Util\Autoload::addNamespace('', dirname(__DIR__).'/fixtures');
 
