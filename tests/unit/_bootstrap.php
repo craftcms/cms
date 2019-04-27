c19d5633dc48cf1935402f5af77d0d7aecaf0fdc
Giel Tettelaar <giel@yellowflash.net>
2019-04-27 17:05:43 +0000

    Test commit from CMD

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -2,6 +2,8 @@
 // Here you can initialize variables that will be available to your tests
 
 
+
+
 // Autoload the fixtures
 \Codeception\Util\Autoload::addNamespace('', dirname(__DIR__).'/fixtures');
 
