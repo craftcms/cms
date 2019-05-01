d40b4d0864b9eeec823487e03a44fa72c63a9b5b
lemiwinkz <giel@yellowflash.net>
2019-05-01 17:38:23 +0200

    Save route testing

--- tests/unit/services/RoutesTest.php
+++ tests/unit/services/RoutesTest.php
@@ -9,6 +9,8 @@ namespace craftunit\services;
 
 
 use Codeception\Test\Unit;
+use craft\helpers\StringHelper;
+use craft\services\Routes;
 
 /**
  * Unit tests for RoutesTest
@@ -20,4 +22,116 @@ use Codeception\Test\Unit;
  */
 class RoutesTest extends Unit
 {
+    /**
+     * @var \UnitTester $tester
+     */
+    protected $tester;
+
+    /**
+     * @var Routes $routes
+     */
+    protected $routes;
+
+    public function _before()
+    {
+        parent::_before();
+        $this->routes = \Craft::$app->getRoutes();
+    }
+
+    /**
+     * @dataProvider saveRouteDataProvider
+     * @param $result
+     * @param array $uriParts
+     * @param string $template
+     * @param string|null $siteUid
+     * @param string|null $routeUid
+     */
+    public function testSaveRoute($result, array $uriParts, string $template, string $siteUid = null, string $routeUid = null)
+    {
+        $routeUUID = $this->routes->saveRoute($uriParts, $template, $siteUid, $routeUid);
+
+        $this->assertSame(
+            $result,
+            \Craft::$app->getProjectConfig()->get(Routes::CONFIG_ROUTES_KEY.'.'.$routeUUID)
+        );
+
+        $this->assertTrue(StringHelper::isUUID($routeUUID));
+    }
+
+    public function saveRouteDataProvider()
+    {
+        return [
+            [
+                [
+                    'template' => '_test',
+                    'uriPattern' => '',
+                    'sortOrder' => 1,
+                    'siteUid' => null
+                ],
+                [], '_test'
+            ],
+            [
+                [
+                    'template' => '_test',
+                    'uriParts' => ['test1', 'test2'],
+                    'uriPattern' => 'test1test2',
+                    'sortOrder' => 1,
+                    'siteUid' => null
+                ],
+                ['test1', 'test2'], '_test'
+            ],
+            [
+                [
+                    'template' => '_test',
+                    'uriParts' => [['validHandle', 'date'], ['someHandle', 'slug']],
+                    'uriPattern' => '<validHandle:date><someHandle:slug>',
+                    'sortOrder' => 1,
+                    'siteUid' => null
+                ],
+                [['validHandle', 'date'], ['someHandle', 'slug']], '_test'
+            ],
+            [
+                [
+                    'template' => '_test',
+                    'uriParts' => [['validHandle', 'date'], ['!@#$%^&*(', 'validHandle'], ['validHandle', '!@#$%^&*(']],
+                    'uriPattern' => '<validHandle:date><any:validHandle><validHandle2:!@#$%^&*(>',
+                    'sortOrder' => 1,
+                    'siteUid' => null
+                ],
+                [['validHandle', 'date'], ['!@#$%^&*(', 'validHandle'],  ['validHandle', '!@#$%^&*(']], '_test'
+            ],
+            [
+                [
+                    'template' => '_test',
+                    'uriParts' => [['validHandle', 'date', 'extraParamThatIsntUsed'], ['!@#$%^&*(', 'validHandle']],
+                    'uriPattern' => '<validHandle:date><any:validHandle>',
+                    'sortOrder' => 1,
+                    'siteUid' => null
+                ],
+                [['validHandle', 'date', 'extraParamThatIsntUsed'], ['!@#$%^&*(', 'validHandle']], '_test'
+            ],
+            [
+                [
+                    'template' => '_test',
+                    'uriParts' => [['validHandle', 'date'], 'noArray'],
+                    'uriPattern' => '<validHandle:date>noArray',
+                    'sortOrder' => 1,
+                    'siteUid' => null
+                ],
+                [['validHandle', 'date'], 'noArray'], '_test'
+            ],
+
+            // TODO: Well more a question.Shouldnt emojis (UTF-8) be allowed in routes?
+            [
+                [
+                    'template' => '_test',
+                    'uriParts' => [['😎', 'date'], ['😎', 'emoji']],
+                    'uriPattern' => '<any:date><any2:emoji>',
+                    'sortOrder' => 1,
+                    'siteUid' => null
+                ],
+                [['😎', 'date'], ['😎', 'emoji']], '_test'
+            ],
+        ];
+    }
 }
