3cb2e0164f61dcefb89a45af0ea999736fded066
brandonkelly <brandon@pixelandtonic.com>
2019-05-20 11:34:27 -0700

    Factor descendant depth into New Parent options
    Fixes #4279

--- src/controllers/EntriesController.php
+++ src/controllers/EntriesController.php
@@ -153,7 +153,20 @@ class EntriesController extends BaseEntriesController
             ];
 
             if ($section->maxLevels) {
-                $variables['parentOptionCriteria']['level'] = '< ' . $section->maxLevels;
+                if ($entry->id) {
+                    // Figure out how deep the ancestors go
+                    $maxDepth = Entry::find()
+                        ->select('level')
+                        ->descendantOf($entry)
+                        ->anyStatus()
+                        ->leaves()
+                        ->scalar();
+                    $depth = 1 + ($maxDepth ?: $entry->level) - $entry->level;
+                } else {
+                    $depth = 1;
+                }
+
+                $variables['parentOptionCriteria']['level'] = '<= ' . ($section->maxLevels - $depth);
             }
 
             if ($entry->id !== null) {
