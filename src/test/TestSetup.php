10a9d4817ec6e19261964c525884e3a0e68a5400
lemiwinkz <giel@yellowflash.net>
2019-05-23 11:33:45 +0200

    Implement the service map via the TestSetup class
    Need this for a mocking feature.

--- src/test/TestSetup.php
+++ src/test/TestSetup.php
@@ -7,6 +7,8 @@
 namespace craft\test;
 
 use Craft;
+use craft\mail\Mailer;
+use craft\services\ProjectConfig;
 use craft\test\Craft as CraftTest;
 use craft\db\Connection;
 use craft\db\Migration;
@@ -23,6 +25,46 @@ use yii\base\Event;
 use yii\base\InvalidArgumentException;
 use yii\base\InvalidConfigException;
 use yii\db\Exception;
+use craft\db\MigrationManager;
+use craft\feeds\Feeds;
+use craft\i18n\Locale;
+use craft\queue\Queue;
+use craft\services\Api;
+use craft\services\AssetIndexer;
+use craft\services\Assets;
+use craft\services\AssetTransforms;
+use craft\services\Categories;
+use craft\services\Composer;
+use craft\services\Content;
+use craft\services\Dashboard;
+use craft\services\Deprecator;
+use craft\services\ElementIndexes;
+use craft\services\Elements;
+use craft\services\Entries;
+use craft\services\EntryRevisions;
+use craft\services\Globals;
+use craft\services\Images;
+use craft\services\Matrix;
+use craft\services\Path;
+use craft\services\Plugins;
+use craft\services\PluginStore;
+use craft\services\Relations;
+use craft\services\Routes;
+use craft\services\Search;
+use craft\services\Sections;
+use craft\services\Sites;
+use craft\services\Structures;
+use craft\services\SystemMessages;
+use craft\services\Tags;
+use craft\services\TemplateCaches;
+use craft\services\Tokens;
+use craft\services\Updates;
+use craft\services\UserGroups;
+use craft\services\UserPermissions;
+use craft\services\Users;
+use craft\services\Utilities;
+use craft\services\Volumes;
+use yii\mutex\Mutex;
 
 /**
  * Class TestSetup.
@@ -316,5 +358,58 @@ class TestSetup
         ]);
 
         $migration->safeUp();
+    }
+
+    /**
+     * @return array
+     */
+    public function getCraftServiceMap() : array
+    {
+        return [
+            [Api::class, ['getApi', 'api']],
+            [Assets::class, ['getAssets', 'assets']],
+            [AssetIndexer::class, ['getAssetIndexer', 'assetIndexer']],
+            [AssetTransforms::class, ['getAssetTransforms', 'assetTransforms']],
+            [Categories::class, ['getCategories', 'categories']],
+            [Composer::class, ['getComposer', 'composer']],
+            [Config::class, ['getConfig', 'config']],
+            [Content::class, ['getContent', 'content']],
+            [MigrationManager::class, ['getContentMigrator', 'contentMigrator']],
+            [Dashboard::class, ['getDashboard', 'dashboard']],
+            [Deprecator::class, ['getDeprecator', 'deprecator']],
+            [ElementIndexes::class, ['getElementIndexes', 'elementIndexes']],
+            [Elements::class, ['getElements', 'elements']],
+            [SystemMessages::class, ['getSystemMessages', 'systemMessages']],
+            [Entries::class, ['getEntries', 'entries']],
+            [EntryRevisions::class, ['getEntryRevisions', 'entryRevisions']],
+            [Feeds::class, ['getFeeds', 'feeds']],
+            [Globals::class, ['getGlobals', 'globals']],
+            [Images::class, ['getImages', 'images']],
+            [Locale::class, ['getLocale', 'locale']],
+            [Mailer::class, ['getMailer', 'mailer']],
+            [Matrix::class, ['getMatrix', 'matrix']],
+            [MigrationManager::class, ['getMigrator', 'migrator']],
+            [Mutex::class, ['getMutex', 'mutex']],
+            [Path::class, ['getPath', 'path']],
+            [Plugins::class, ['getPlugins', 'plugins']],
+            [PluginStore::class, ['getPluginStore', 'pluginStore']],
+            [ProjectConfig::class, ['getProjectConfig', 'projectConfig']],
+            [Queue::class, ['getQueue', 'queue']],
+            [Relations::class, ['getRelations', 'relations']],
+            [Routes::class, ['getRoutes', 'routes']],
+            [Search::class, ['getSearch', 'search']],
+            [Sections::class, ['getSections', 'sections']],
+            [Sites::class, ['getSites', 'sites']],
+            [Structures::class, ['getStructures', 'structures']],
+            [Tags::class, ['getTags', 'tags']],
+            [TemplateCaches::class, ['getTemplateCaches', 'templateCaches']],
+            [Tokens::class, ['getTokens', 'tokens']],
+            [Updates::class, ['getUpdates', 'updates']],
+            [UserGroups::class, ['getUserGroups', 'userGroups']],
+            [UserPermissions::class, ['getUserPermissions', 'userPermissions']],
+            [Users::class, ['getUsers', 'users']],
+            [Utilities::class, ['getUtilities', 'utilities']],
+            [Volumes::class, ['getVolumes', 'volumes']],
+        ];
     }
 }
