61575dbd45d35e672f2ed491ec9e4eb6e539d195
andris-sevcenko <asevcenko@gmail.com>
2019-05-27 13:41:26 +0300

    Resolve user fields.

--- src/fields/Users.php
+++ src/fields/Users.php
@@ -9,6 +9,10 @@ namespace craft\fields;
 
 use Craft;
 use craft\elements\User;
+use craft\gql\arguments\elements\User as UserArguments;
+use craft\gql\interfaces\elements\User as UserInterface;
+use craft\gql\resolvers\elements\User as UserResolver;
+use GraphQL\Type\Definition\Type;
 
 /**
  * Users represents a Users field.
@@ -43,5 +47,18 @@ class Users extends BaseRelationField
     public static function defaultSelectionLabel(): string
     {
         return Craft::t('app', 'Add a user');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getContentGqlType()
+    {
+        return [
+            'name' => $this->handle,
+            'type' => Type::listOf(UserInterface::getType()),
+            'args' => UserArguments::getArguments(),
+            'resolve' => UserResolver::class . '::resolve',
+        ];
     }
 }
