515fffac594ce4e9d358cb5d094106b0f8516090
Brad Bell <brad@pixelandtonic.com>
2019-05-27 17:06:58 -0700

    Fix the product.jpg leak.

--- tests/fixtures/AssetsFixture.php
+++ tests/fixtures/AssetsFixture.php
@@ -8,6 +8,7 @@
 
 namespace craftunit\fixtures;
 
+use craft\helpers\FileHelper;
 use craft\test\fixtures\elements\AssetFixture;
 
 /**
@@ -19,7 +20,7 @@ use craft\test\fixtures\elements\AssetFixture;
  */
 class AssetsFixture extends AssetFixture
 {
-    // Properties
+    // Public Properties
     // =========================================================================
 
     /**
@@ -31,4 +32,64 @@ class AssetsFixture extends AssetFixture
      * @inheritdoc
      */
     public $depends = [VolumesFixture::class, VolumesFolderFixture::class];
+
+    // Private Properties
+    // =========================================================================
+
+    /**
+     * @var array Used to track the files the fixture data file defines.
+     */
+    private $_files;
+
+    /**
+     * @var $string
+     */
+    private $_sourceAssetPath;
+
+    /**
+     * @var $string
+     */
+    private $_destinationAssetPath;
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * @inheritDoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        $this->_sourceAssetPath = dirname(__FILE__,2).'/_craft/assets/';
+        $this->_destinationAssetPath = dirname(__FILE__,2).'/_craft/storage/runtime/temp/';
+
+        $data = require $this->dataFile;
+
+        foreach ($data as $fileName) {
+            $this->_files[] = $fileName;
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function beforeLoad()
+    {
+        parent::beforeLoad();
+
+        foreach ($this->_files as $key => $fileInfo) {
+            copy($this->_sourceAssetPath.$fileInfo['filename'], $this->_destinationAssetPath.$fileInfo['filename']);
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function beforeUnload()
+    {
+        parent::beforeUnload();
+
+        FileHelper::clearDirectory($this->_destinationAssetPath);
+    }
 }
