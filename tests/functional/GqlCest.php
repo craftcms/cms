c5092c652d03b2a19a44c3d7e886ae2b8fb0b846
andris-sevcenko <asevcenko@gmail.com>
2019-05-31 20:16:56 +0300

    Some GQL functional tests.

--- tests/functional/GqlCest.php
+++ tests/functional/GqlCest.php
@@ -17,9 +17,53 @@ class GqlCest
     }
 
     // tests
-    public function tryToForgetQuery(FunctionalTester $I)
+    public function forgetQueryParameter(FunctionalTester $I)
     {
+        // If this suite is ran separately, somethis this test fails for no reason.
+        // ¯\_(ツ)_/¯
         $I->amOnPage('?action=gql');
         $I->see('Request missing required param');
     }
+
+    public function provideMalformedQueryParameter(FunctionalTester $I)
+    {
+        $I->amOnPage('?action=gql&query=bogus}');
+        $I->see('Syntax Error');
+    }
+
+    public function testQuerying(FunctionalTester $I)
+    {
+        $queryTypes = [
+            'Entries',
+            'MatrixBlocks',
+            'Users',
+            'Assets',
+            'GlobalSets',
+        ];
+
+        foreach ($queryTypes as $queryType) {
+            $I->amOnPage('?action=gql&query={query' . $queryType . '{title}}');
+            $I->see('"query' . $queryType . '":[');
+        }
+    }
+
+    public function testWrongGqlField(FunctionalTester $I)
+    {
+        $parameter = 'bogus';
+        $I->amOnPage('?action=gql&query={queryEntries{' . $parameter . '}}');
+        $I->see('"Cannot query field \"' . $parameter . '\"');
+    }
+
+    public function testWrongGqlQueryParameter(FunctionalTester $I)
+    {
+        $resp = $I->amOnPage('?action=gql&query={queryEntries(limit:[5,2]){title}}');
+        $I->see('"debugMessage":"Expected');
+    }
+
+    // @todo: figure out how to test paging. Probably after we have fixtures for a lot of stuff.
+//    public function testPaging(FunctionalTester $I)
+//    {
+//        $I->amOnPage('?action=gql&query={queryEntries(limit:2){title}}');
+//        $I->see('"debugMessage":"Expected');
+//    }
 }
