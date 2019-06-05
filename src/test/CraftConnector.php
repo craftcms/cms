3fca8a387430595a8dc461e7456d2fcad31cd329
lemiwinkz <giel@yellowflash.net>
2019-06-05 19:58:22 +0200

    Improves upon d11dc32ecf4f5ae4b65dbebb6cd762038a482f75
    We now reload all modules before each functional test request - so that their init() function can be called using the newest request parameters.

--- src/test/CraftConnector.php
+++ src/test/CraftConnector.php
@@ -9,6 +9,8 @@ namespace craft\test;
 
 use Codeception\Lib\Connector\Yii2;
 use Craft;
+use craft\base\Plugin;
+use yii\base\Module;
 use yii\mail\MessageInterface;
 use yii\web\Application;
 
@@ -56,10 +58,29 @@ class CraftConnector extends Yii2
 
     /**
      * @param Application $app
+     * @throws \craft\errors\InvalidPluginException
      */
     public function resetRequest(Application $app)
     {
         parent::resetRequest($app);
         $app->getRequest()->setIsConsoleRequest(false);
+
+        $modules = Craft::$app->getModules();
+        /* @var Module $module */
+        foreach ($modules as $module) {
+            $moduleClass = get_class($module);
+            $moduleId = $module->id;
+
+            if ($module instanceof Plugin) {
+                $module = Craft::$app->getPlugins()->createPlugin($moduleId);
+            } else {
+                $module = new $moduleClass($moduleId, Craft::$app);
+            }
+
+            $moduleClass::setInstance(
+                $module
+            );
+            Craft::$app->setModule($moduleId, $module);
+        }
     }
 }
