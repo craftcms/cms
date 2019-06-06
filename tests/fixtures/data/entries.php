c902da55eb3b0434ad9f9cf589ff1c2f7b8a4980
lemiwinkz <giel@yellowflash.net>
2019-06-06 17:13:10 +0200

    Resort the elements with matrix fixture into it's own class.

--- tests/fixtures/data/entries.php
+++ tests/fixtures/data/entries.php
@@ -73,27 +73,5 @@ return [
         'typeId' => '1005',
         'title' => 'Single entry'
     ],
-    [
-        'authorId' => '1',
-        'sectionId' => '1000',
-        'typeId' => '1000',
-        'title' => 'Theories of matrix',
-        'fieldLayoutType' => 'field_layout_with_matrix_and_normal_fields',
-        'matrixFirst' => [
-            'new1' => [
-                'type' => 'aBlock',
-                'fields' => [
-                    'firstSubfield' => 'Some text'
-
-                ],
-            ],
-            'new2' => [
-                'type' => 'aBlock',
-                'fields' => [
-                    'firstSubfield' => 'Some text'
-                ],
-            ],
-        ],
-    ],
 
 ];
