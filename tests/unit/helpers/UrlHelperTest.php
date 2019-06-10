7eb80f34b582091bae4f5dc949c88604d4fd6df3
lemiwinkz <giel@yellowflash.net>
2019-06-10 18:50:13 +0200

    Fixes failing tests bcause of codeception.yml changes

--- tests/unit/helpers/UrlHelperTest.php
+++ tests/unit/helpers/UrlHelperTest.php
@@ -10,6 +10,7 @@ namespace crafttests\unit\helpers;
 use Codeception\Test\Unit;
 use Craft;
 use craft\errors\SiteNotFoundException;
+use craft\helpers\StringHelper;
 use craft\helpers\UrlHelper;
 use craft\test\Craft as CraftTest;
 use UnitTester;
@@ -225,13 +226,24 @@ class UrlHelperTest extends Unit
      */
     public function testBaseTesting()
     {
-        $this->assertSame($this->baseUrl, UrlHelper::baseUrl());
-        $this->assertSame($this->baseUrl, UrlHelper::baseSiteUrl());
-        $this->assertSame(rtrim($this->baseUrl, '/'), UrlHelper::host());
+        $baseSiteUrl = Craft::$app->getSites()->getCurrentSite()->getBaseUrl();
+        $host = rtrim($this->baseUrl, '/');
+        if (mb_strpos($host, '/index.php') !== false) {
+            $host = StringHelper::replace($host, '/index.php', '');
+        }
+
+        $this->assertSame($baseSiteUrl, UrlHelper::baseUrl());
+        $this->assertSame($baseSiteUrl, UrlHelper::baseSiteUrl());
+        $this->assertSame($host, UrlHelper::host());
 
         $this->assertSame('/', UrlHelper::baseCpUrl());
         $this->assertSame('/', UrlHelper::baseRequestUrl());
+
+        // @todo: This right?
         $this->assertSame('', UrlHelper::cpHost());
+
+        Craft::$app->getConfig()->getGeneral()->baseCpUrl = 'https://craftcms.com/test/test';
+        $this->assertSame('https://craftcms.com', UrlHelper::cpHost());
     }
 
     /**
@@ -667,7 +679,7 @@ class UrlHelperTest extends Unit
 
         $this->baseUrl = $configSiteUrl;
 
-        // Add the entry script. This  is for the withScript variable.
+        // Add the entry script. This is for the withScript variable.
         if (strpos($this->entryScript, $configSiteUrl) === false) {
             $configSiteUrl .= $this->entryScript;
         }
