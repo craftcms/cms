fd273b325a60d26a3e08dadf1db936b328e584c3
andris-sevcenko <asevcenko@gmail.com>
2019-06-11 11:43:51 +0300

    Grammar.

--- src/test/fixtures/elements/ElementFixture.php
+++ src/test/fixtures/elements/ElementFixture.php
@@ -104,7 +104,7 @@ abstract class ElementFixture extends ActiveFixture
                 if ($fieldLayout) {
                     $element->fieldLayoutId = $fieldLayout->id;
                 } else {
-                    codecept_debug("Field layout with type: $fieldLayoutType but this was not findable");
+                    codecept_debug("Field layout with type: $fieldLayoutType could not be found");
                 }
             }
 
