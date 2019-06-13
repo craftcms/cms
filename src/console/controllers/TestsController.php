7bee133e2fb8bc54fe6883e9ffb950ca4a083387
lemiwinkz <giel@yellowflash.net>
2019-06-13 23:30:51 +0200

    Initial draft of Testing email settings console command. To resolve #4020

--- src/console/controllers/TestsController.php
+++ src/console/controllers/TestsController.php
@@ -9,10 +9,19 @@ namespace craft\console\controllers;
 
 use Craft;
 use craft\console\Controller;
+use craft\helpers\App;
 use craft\helpers\Console;
 use craft\helpers\FileHelper;
+use craft\helpers\MailerHelper;
+use craft\mail\Message;
+use craft\mail\transportadapters\BaseTransportAdapter;
+use craft\mail\transportadapters\Gmail;
+use craft\mail\transportadapters\Sendmail;
+use craft\mail\transportadapters\Smtp;
+use craft\models\MailSettings;
 use yii\base\InvalidArgumentException;
 use yii\console\ExitCode;
+use craft\elements\User as UserElement;
 
 /**
  * Various support resources for testing Craft.
@@ -27,6 +36,101 @@ class TestsController extends Controller
     // =========================================================================
 
     /**
+     * @return int
+     * @throws \craft\errors\MissingComponentException
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function actionTestEmailSettings()
+    {
+        $recieverEmail = $this->prompt('Which email address must we send this test email to?');
+
+        $mailParams = [
+            'user' => new UserElement([
+                'email' => $recieverEmail,
+                'username' => $recieverEmail
+            ])
+        ];
+
+        $settingsModel = App::mailSettings();
+
+        // Default settings?
+        if ($this->confirm('Do you want to test Crafts default email settings?')) {
+            $adapter = MailerHelper::createTransportAdapter(
+                $settingsModel->transportType,
+                $settingsModel->transportSettings
+            );
+
+            $mailParams['settings'] = $this->_renderMailSettingsString(
+                App::mailSettings(),
+                $adapter
+            );
+
+            $message = Craft::$app->getMailer()
+                ->composeFromKey('test_email', $mailParams)
+                ->setTo($recieverEmail);
+
+            return $this->_testEmailSending($message);
+        }
+
+        // Otherwise we let the user decide....
+        $transportAdapters = [
+            Smtp::class,
+            Gmail::class,
+            Sendmail::class,
+            'Other'
+        ];
+        $selectedOption = null;
+
+        foreach ($transportAdapters as $transportAdapter) {
+            if ($this->confirm("Do you want to user {$transportAdapter}?".PHP_EOL)) {
+                $selectedOption = $transportAdapter;
+                break;
+            }
+        }
+
+        if ($selectedOption === 'Other') {
+            $selectedOption = $this->prompt("Which transport type do you want to use?");
+        }
+
+        /* @var BaseTransportAdapter $transport */
+        $transport = new $selectedOption();
+
+        if (!$transport instanceof BaseTransportAdapter) {
+            $this->stderr("$selectedOption is not an instance of " . BaseTransportAdapter::class . "");
+            return ExitCode::OK;
+        }
+
+        // What do they want to use?
+        foreach ($transport->settingsAttributes() as $property) {
+            $transport->$property = $this->prompt("What must $property be set to?");
+        }
+
+        // Save the new stuff to the settings
+        $settingsModel->transportType = $transport::displayName();
+        $settingsModel->transportSettings = $transport->getSettings();
+
+        // Too easy?
+        if (!$transport->validate()) {
+            $this->stderr('Your email settings are invalid.');
+            return ExitCode::OK;
+        }
+
+        // Setup the mailer.
+        $mailer = Craft::$app->getMailer();
+        $mailer->transport = $transport->defineTransport();
+
+        // For the template
+        $mailParams['settings'] = $this->_renderMailSettingsString($settingsModel, $transport);
+
+        $message = $mailer
+            ->composeFromKey('test_email', $mailParams)
+            ->setTo($recieverEmail);
+
+        // FOR... SPARTAAA!
+        return $this->_testEmailSending($message);
+    }
+
+    /**
      * Sets up a test suite for the current project.
      *
      * @param string|null $dst The folder that the test suite should be generated in.
@@ -39,7 +143,7 @@ class TestsController extends Controller
             $dst = getcwd();
         }
 
-        $src = dirname(__DIR__, 2). DIRECTORY_SEPARATOR . 'test' . DIRECTORY_SEPARATOR . 'internal' . DIRECTORY_SEPARATOR . 'example-test-suite';
+        $src = dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . 'test' . DIRECTORY_SEPARATOR . 'internal' . DIRECTORY_SEPARATOR . 'example-test-suite';
 
         // Figure out the plan and check for conflicts
         $plan = [];
@@ -129,6 +233,55 @@ class TestsController extends Controller
         }
 
         $this->outputCommand('An output command');
+
+        return ExitCode::OK;
+    }
+
+    // Protected functions
+    // =========================================================================
+
+    /**
+     * @param MailSettings $settings
+     * @param $adapter
+     * @return string
+     */
+    protected function _renderMailSettingsString(MailSettings $settings, $adapter) : string
+    {
+        // Compose the settings list as HTML
+        $settingsList = '';
+
+        foreach (['fromEmail', 'fromName', 'template'] as $name) {
+            if (!empty($settings->$name)) {
+                $settingsList .= '- **' . $settings->getAttributeLabel($name) . ':** ' . $settings->$name . "\n";
+            }
+        }
+
+        $settingsList .= '- **' . 'Transport Type' . ':** ' . $adapter::displayName() . "\n";
+
+        $security = Craft::$app->getSecurity();
+
+        foreach ($adapter->settingsAttributes() as $name) {
+            if (!empty($adapter->$name)) {
+                $label = $adapter->getAttributeLabel($name);
+                $value = $security->redactIfSensitive($name, $adapter->$name);
+                $settingsList .= "- **{$label}:** {$value}\n";
+            }
+        }
+
+        return $settingsList;
+    }
+
+    /**
+     * @param Message $message
+     * @return int
+     */
+    protected function _testEmailSending(Message $message) : int
+    {
+        if ($message->send()) {
+            $this->stdout('Email sent successfully! Check your inbox.'.PHP_EOL.PHP_EOL);
+        } else {
+            $this->stderr('There was an error testing your email settings.'.PHP_EOL.PHP_EOL);
+        }
 
         return ExitCode::OK;
     }
