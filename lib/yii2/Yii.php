be1a1aa66d32b0704e41e5d3eebec0f61797514f
Brandon Kelly <brandon@pixelandtonic.com>
2019-06-17 15:01:58 -0700

    Fixed another Windows bug with Craft::alias()
    #4405

--- lib/yii2/Yii.php
+++ lib/yii2/Yii.php
@@ -58,7 +58,7 @@ class Yii extends BaseYii
         $path = FileHelper::normalizePath($path);
         foreach (self::$_aliasPaths as $alias => $aliasPath) {
             if (strpos($path . DIRECTORY_SEPARATOR, $aliasPath . DIRECTORY_SEPARATOR) === 0) {
-                return $alias . substr($path, strlen($aliasPath));
+                return $alias . str_replace('\\', '/', substr($path, strlen($aliasPath)));
             }
         }
         return $path;
