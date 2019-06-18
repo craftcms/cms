67e24d8cd8cacdc8ed42c795f4bb105e6b9e329a
lemiwinkz <giel@yellowflash.net>
2019-06-18 22:03:27 +0200

    Console actions.

--- src/console/controllers/QueueController.php
+++ src/console/controllers/QueueController.php
@@ -7,12 +7,9 @@
 
 namespace craft\console\controllers;
 
-use Craft;
-use craft\db\Table;
-use craft\helpers\Console;
-use craft\services\Plugins;
 use craft\console\Controller;
 use yii\console\ExitCode;
+use Craft;
 
 /**
  * Console based Queue actions
@@ -22,5 +19,109 @@ use yii\console\ExitCode;
  */
 class QueueController extends Controller
 {
+    /**
+     * @var bool Whether every entry change should be force-synced.
+     */
+    public $force = false;
 
+    /**
+     * @return int
+     */
+    public function actionRun()
+    {
+        $this->stdout('Running queue'.PHP_EOL);
+
+        Craft::$app->getQueue()->run();
+
+        $this->stdout('Queue successfully executed'.PHP_EOL);
+
+        return ExitCode::OK;
+    }
+
+    /**
+     * @param null $id
+     * @return int
+     */
+    public function actionRetry($id = null)
+    {
+        if ($this->force = true) {
+            $this->stdout('Retrying all jobs'. PHP_EOL);
+
+            \Craft::$app->getQueue()->retryAll();
+            \Craft::$app->getQueue()->run();
+
+            $this->stdout('All jobs retried'. PHP_EOL);
+
+            return ExitCode::OK;
+        }
+
+        // Fail-safe
+        if (!$id) {
+            $id = $this->prompt('You have not entered an ID. Enter one now.'.PHP_EOL);
+            if (!$id) {
+                $this->stderr('Invalid id'.PHP_EOL);
+                return ExitCode::OK;
+            }
+        }
+
+        // Retry with specific id
+        $this->stdout("Retrying job with id: $id". PHP_EOL);
+
+        Craft::$app->getQueue()->retry($id);
+        Craft::$app->getQueue()->run();
+
+        $this->stdout("Job with id: $id retried". PHP_EOL);
+
+        return ExitCode::OK;
+    }
+
+    /**
+     * @param null $id
+     * @return int
+     * @throws \yii\db\Exception
+     */
+    public function actionRelease($id = null)
+    {
+        if ($this->force = true) {
+            $this->stdout('Releasing all jobs'. PHP_EOL);
+
+            \Craft::$app->getQueue()->releaseAll();
+            \Craft::$app->getQueue()->run();
+
+            $this->stdout('All jobs released'. PHP_EOL);
+
+            return ExitCode::OK;
+        }
+
+        // Fail-safe
+        if (!$id) {
+            $id = $this->prompt('You have not entered an ID. Enter one now.'.PHP_EOL);
+            if (!$id) {
+                $this->stderr('Invalid id'.PHP_EOL);
+                return ExitCode::OK;
+            }
+        }
+
+        // Retry with specific id
+        $this->stdout("Releasing job with id: $id". PHP_EOL);
+
+        Craft::$app->getQueue()->release($id);
+        Craft::$app->getQueue()->run();
+
+        $this->stdout("Job with id: $id released". PHP_EOL);
+
+        return ExitCode::OK;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function options($actionID)
+    {
+        $options = parent::options($actionID);
+
+        $options[] = 'all';
+
+        return $options;
+    }
 }
