abf50df1290f51d52daf4cfc544e30fec63c9831
andris-sevcenko <asevcenko@gmail.com>
2019-06-21 16:10:54 +0300

    Fix gql token fixture data.

--- tests/fixtures/data/gql-tokens.php
+++ tests/fixtures/data/gql-tokens.php
@@ -13,7 +13,7 @@ return [
         'enabled' => true,
         'expiryDate' => null,
         'lastUsed' => null,
-        'permissions' => '["globalsets.globalset-1000-uid:read","globalsets.globalset-1001-uid:read","globalsets.globalset-1002-uid:read","volumes.volume-1000-uid:read","volumes.volume-1001-uid:read","volumes.volume-1002-uid:read","usergroups.usergroup-1000-uid:read","usergroups.usergroup-1001-uid:read","usergroups.usergroup-1002-uid:read","sections.section-1000-uid:read","sections.section-1001-uid:read","sections.section-1002-uid:read","sections.section-1003-uid:read","sections.section-1004-uid:read","sections.section-1005-uid:read","entrytypes.entrytype-1000-uid:read","entrytypes.entrytype-1001-uid:read","entrytypes.entrytype-1002-uid:read","entrytypes.entrytype-1003-uid:read","entrytypes.entrytype-1004-uid:read","entrytypes.entrytype-1005-uid:read"]',
+        'permissions' => '["globalsets.globalset-1000-uid:read","globalsets.globalset-1001-uid:read","globalsets.globalset-1002-uid:read","volumes.volume-1000-uid:read","volumes.volume-1001-uid:read","volumes.volume-1002-uid:read","usergroups.usergroup-1000-uid:read","usergroups.usergroup-1001-uid:read","usergroups.usergroup-1002-uid:read","sections.section-1000-uid:read","sections.section-1001-uid:read","sections.section-1002-uid:read","sections.section-1003-uid:read","sections.section-1004-uid:read","sections.section-1005-uid:read","entrytypes.entry-type-1000-uid:read","entrytypes.entry-type-1001-uid:read","entrytypes.entry-type-1002-uid:read","entrytypes.entry-type-1003-uid:read","entrytypes.entry-type-1004-uid:read","entrytypes.entry-type-1005-uid:read"]',
         'dateCreated' => '2018-08-08 20:00:00',
         'dateUpdated' => '2018-08-08 20:00:00',
         'uid' => 'gql-master-token-uid'
