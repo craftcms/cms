e9c359da21585e87a29d326ce03226d92fba67c2
andris-sevcenko <asevcenko@gmail.com>
2019-06-21 17:36:28 +0300

    Add another global set to the mix.

--- tests/fixtures/data/global-sets.php
+++ tests/fixtures/data/global-sets.php
@@ -22,11 +22,19 @@ return [
         'uid' => 'globalset-1001-uid',
     ],
 
+    [
+        'name' => 'A third global set',
+        'handle' => 'aThirdGlobalSet',
+        'fieldLayoutType' => 'field_layout_with_matrix_and_normal_fields',
+        'plainTextField' => 'No more information to give.',
+        'uid' => 'globalset-1002-uid',
+    ],
+
     // Deleted
     [
         'name' => 'A deleted global set',
         'handle' => 'aDeletedGlobalSet',
         'dateDeleted' => (new DateTime('now'))->format('Y-m-d H:i:s'),
-        'uid' => 'globalset-1002-uid',
+        'uid' => 'globalset-1003-uid',
     ],
 ];
