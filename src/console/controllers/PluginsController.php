7c4906bf3badfedff5a3d1893cc31f92e84434f2
lemiwinkz <giel@yellowflash.net>
2019-06-24 13:17:58 +0200

    Cleanup

--- src/console/controllers/PluginsController.php
+++ src/console/controllers/PluginsController.php
@@ -10,6 +10,7 @@ namespace craft\console\controllers;
 use Craft;
 use craft\base\Plugin;
 use craft\console\Controller;
+use craft\errors\InvalidPluginException;
 use craft\helpers\ArrayHelper;
 use craft\helpers\Console;
 use craft\helpers\StringHelper;
@@ -34,31 +35,35 @@ class PluginsController extends Controller
     public $defaultAction = 'view';
 
     /**
+     * @param string|null $pluginHandle
      * @return int
+     * @throws InvalidPluginException
      */
     public function actionView(string $pluginHandle = null) : int
     {
         $plugins = $this->_assemblePlugins();
 
+        // Single plugin display
         if ($pluginHandle) {
-            $plugin = ArrayHelper::where(
-                $plugins,
-                'handle',
-                $pluginHandle
-            );
-            $plugin = ArrayHelper::firstValue($plugin);
+            $plugin = Craft::$app->getPlugins()->getComposerPluginInfo($pluginHandle);
+            if (!$plugin) {
+                $this->stderr('No plugin exists by that handle.'.PHP_EOL);
+                return ExitCode::UNSPECIFIED_ERROR;
+            }
+
+            $this->stdout('Plugin with name: '.$plugin['name'].''.PHP_EOL.PHP_EOL);
 
             foreach ($plugin as $propName => $value) {
                 if (is_array($value)) {
                     $value = implode(', ', $value);
                 }
 
-                if (is_object($value) && !method_exists($value, '__toString')) {
-                    $value = 'Unkown';
-                }
-                
                 $this->stdout("$propName: $value" . PHP_EOL);
             }
+
+            $isInstalled = Craft::$app->getPlugins()->isPluginInstalled($pluginHandle) ? 'Yes': 'No';
+            $this->stdout("Is installed: $isInstalled".PHP_EOL);
+            $this->stdout(PHP_EOL);
 
             return ExitCode::OK;
         }
@@ -96,14 +101,15 @@ class PluginsController extends Controller
      * Enabling them
      *
      * @return int
+     * @throws InvalidPluginException
      */
     public function actionManage() : int
     {
         $pluginsService = Craft::$app->getPlugins();
 
-        if (($actionablePlugins = $this->prompt(
-            'Enter plugins seperated by comma. Enter "No" if you want a list of plugins and the option to select from that list.'.PHP_EOL)
-            ) !== 'No') {
+        if ($actionablePlugins = $this->prompt(
+            'Enter plugins seperated by comma. Enter nothing if you want a list of plugins and the option to select from that list.')
+            ) {
             $actionablePlugins = StringHelper::split($actionablePlugins, ',');
 
             // Hmmm.
@@ -138,7 +144,7 @@ class PluginsController extends Controller
         }
 
         foreach ($actionablePlugins as $actionablePlugin) {
-            $isInstalled = Craft::$app->getPlugins()->isPluginInstalled($actionablePlugin);
+            $isInstalled = $pluginsService->isPluginInstalled($actionablePlugin);
 
             // Hmmm.
             if (!$isInstalled) {
@@ -157,23 +163,23 @@ class PluginsController extends Controller
             try {
                 switch ($action) {
                     case 'Uninstall':
-                        if (!Craft::$app->getPlugins()->uninstallPlugin($actionablePlugin)) {
+                        if (!$pluginsService->uninstallPlugin($actionablePlugin)) {
                             return $this->_handleFailedPluginAction($actionablePlugin, $action);
                         }
                         break;
                     case 'Install':
                         $edition = $this->prompt('Which edition must the plugin be installed?');
-                        if (!Craft::$app->getPlugins()->installPlugin($actionablePlugin, $edition)) {
+                        if (!$pluginsService->installPlugin($actionablePlugin, $edition)) {
                             return $this->_handleFailedPluginAction($actionablePlugin, $action);
                         }
                         break;
                     case 'Disable':
-                        if (!Craft::$app->getPlugins()->disablePlugin($actionablePlugin)) {
+                        if (!$pluginsService->disablePlugin($actionablePlugin)) {
                             return $this->_handleFailedPluginAction($actionablePlugin, $action);
                         }
                         break;
                     case 'Enable':
-                        if (!Craft::$app->getPlugins()->enablePlugin($actionablePlugin)) {
+                        if (!$pluginsService->enablePlugin($actionablePlugin)) {
                             return $this->_handleFailedPluginAction($actionablePlugin, $action);
                         }
                         break;
