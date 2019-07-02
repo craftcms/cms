56c66b46e1187968dc95b2187b73ed86363ed776
andris-sevcenko <asevcenko@gmail.com>
2019-07-02 15:37:31 +0300

    Update matrix block type generator to return a subset of block types if needed.

--- src/gql/types/generators/MatrixBlockType.php
+++ src/gql/types/generators/MatrixBlockType.php
@@ -4,6 +4,7 @@ namespace craft\gql\types\generators;
 use Craft;
 use craft\base\Field;
 use craft\elements\MatrixBlock as MatrixBlockElement;
+use craft\fields\Matrix;
 use craft\gql\interfaces\elements\MatrixBlock as MatrixBlockInterface;
 use craft\gql\GqlEntityRegistry;
 use craft\gql\types\MatrixBlock;
@@ -19,29 +20,41 @@ class MatrixBlockType implements BaseGenerator
      */
     public static function generateTypes($context = null): array
     {
-        $matrixBlockTypes = Craft::$app->getMatrix()->getAllBlockTypes();
+        // If we need matrix block types for a specific Matrix field, fetch those.
+        if ($context) {
+            /** @var Matrix $context */
+            $matrixBlockTypes = $context->getBlockTypes();
+        } else {
+            $matrixBlockTypes = Craft::$app->getMatrix()->getAllBlockTypes();
+        }
+
         $gqlTypes = [];
 
         foreach ($matrixBlockTypes as $matrixBlockType) {
             /** @var MatrixBlockTypeModel $matrixBlockType */
             $typeName = MatrixBlockElement::getGqlTypeNameByContext($matrixBlockType);
-            $contentFields = $matrixBlockType->getFields();
-            $contentFieldGqlTypes = [];
 
-            /** @var Field $contentField */
-            foreach ($contentFields as $contentField) {
-                $contentFieldGqlTypes[$contentField->handle] = $contentField->getContentGqlType();
+            if (!($entity = GqlEntityRegistry::getEntity($typeName))) {
+                $contentFields = $matrixBlockType->getFields();
+                $contentFieldGqlTypes = [];
+
+                /** @var Field $contentField */
+                foreach ($contentFields as $contentField) {
+                    $contentFieldGqlTypes[$contentField->handle] = $contentField->getContentGqlType();
+                }
+
+                $blockTypeFields = array_merge(MatrixBlockInterface::getFields(), $contentFieldGqlTypes);
+
+                // Generate a type for each entry type
+                $entity = GqlEntityRegistry::createEntity($typeName, new MatrixBlock([
+                    'name' => $typeName,
+                    'fields' => function() use ($blockTypeFields) {
+                        return $blockTypeFields;
+                    }
+                ]));
             }
 
-            $blockTypeFields = array_merge(MatrixBlockInterface::getFields(), $contentFieldGqlTypes);
-
-            // Generate a type for each entry type
-            $gqlTypes[$typeName] = GqlEntityRegistry::getEntity($typeName) ?: GqlEntityRegistry::createEntity($typeName, new MatrixBlock([
-                'name' => $typeName,
-                'fields' => function () use ($blockTypeFields) {
-                    return $blockTypeFields;
-                }
-            ]));
+            $gqlTypes[$typeName] = $entity;
         }
 
         return $gqlTypes;
