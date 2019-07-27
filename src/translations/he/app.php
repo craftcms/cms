75df17c27a37743807a745a291fb2b9642dcdcfc
Brad Bell <brad@pixelandtonic.com>
2019-07-26 17:09:16 -0700

    New translations app.php (Hebrew)

--- src/translations/he/app.php
+++ src/translations/he/app.php
@@ -1144,13 +1144,13 @@ return [
     'Your system isn’t set up to save content for the site “{site}”.' => 'Your system isn’t set up to save content for the site “{site}”.',
     'You’re all up-to-date!' => 'You’re all up-to-date!',
     'You’re running Craft {edition} with a Craft {licensedEdition} license.' => 'You’re running Craft {edition} with a Craft {licensedEdition} license.',
-    'account_activation_body' => 'Hey {{user.friendlyName}},
+    'account_activation_body' => 'שלום {{user.friendlyName}},
 
-Thanks for creating an account with {{siteName}}! To activate your account, click the following link:
+תודה שיצרת חשבון ב{{siteName}}! להפעל החשבון שלך, לחץ על הלינק הבא:
 
 <{{link}}>
 
-If you were not expecting this email, just ignore it.',
+אם לא ציפית לקבל את האימייל הזה, פשוט התעלם ממנו.',
     'account_activation_heading' => 'When someone creates an account:',
     'account_activation_subject' => 'Activate your account',
     'by {author}' => 'by {author}',
@@ -1162,13 +1162,13 @@ If you were not expecting this email, just ignore it.',
     'five' => 'five',
     'folders' => 'folders',
     'folders and files' => 'folders and files',
-    'forgot_password_body' => 'Hey {{user.friendlyName}},
+    'forgot_password_body' => 'שלום {{user.friendlyName}},
 
-To reset your {{siteName}} password, click on this link:
+על מנת לשחזר את הסיסמא שלך ל{{siteName}}, לחץ על הלינק:
 
 <{{link}}>
 
-If you were not expecting this email, just ignore it.',
+אם לא ציפית לקבל את האימייל הזה, פשוט התעלם ממנו.',
     'forgot_password_heading' => 'When someone forgets their password:',
     'forgot_password_subject' => 'Reset your password',
     'four' => 'four',
@@ -1184,24 +1184,24 @@ If you were not expecting this email, just ignore it.',
     'seconds' => 'seconds',
     'seven' => 'seven',
     'six' => 'six',
-    'test_email_body' => 'Hey {{user.friendlyName}},
+    'test_email_body' => 'שלום {{user.friendlyName}},
 
-Congratulations! Craft was successfully able to send an email.
+ברכותינו! Craft הצליחהה לשלוח לך אימייל בהצלחה.
 
-Here are the settings you used:
+להלן ההגדרות בהן השתמשת:
 
 {{ settings }}',
     'test_email_heading' => 'When you are testing your email settings:',
     'test_email_subject' => 'This is a test email from Craft',
     'three' => 'three',
     'two' => 'two',
-    'verify_new_email_body' => 'Hey {{user.friendlyName}},
+    'verify_new_email_body' => 'שלום {{user.friendlyName}},
 
-Please verify your new email address by clicking on this link:
+בבקשה אשר את כתובת האימייל החדשה שלך על-ידי לחיצה על הלינק:
 
 <{{link}}>
 
-If you were not expecting this email, just ignore it.',
+אם לא ציפית לקבל את האימייל הזה, פשוט התעלם ממנו.',
     'verify_new_email_heading' => 'When someone changes their email address:',
     'verify_new_email_subject' => 'Verify your new email address',
     'week' => 'week',
