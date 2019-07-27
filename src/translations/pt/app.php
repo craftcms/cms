1cb011ed53300433b1c3f011b3bfc9ca29d597f3
Brad Bell <brad@pixelandtonic.com>
2019-07-26 17:08:17 -0700

    New translations app.php (Portuguese)

--- src/translations/pt/app.php
+++ src/translations/pt/app.php
@@ -1144,13 +1144,13 @@ return [
     'Your system isn’t set up to save content for the site “{site}”.' => 'Your system isn’t set up to save content for the site “{site}”.',
     'You’re all up-to-date!' => 'You’re all up-to-date!',
     'You’re running Craft {edition} with a Craft {licensedEdition} license.' => 'You’re running Craft {edition} with a Craft {licensedEdition} license.',
-    'account_activation_body' => 'Hey {{user.friendlyName}},
+    'account_activation_body' => 'Oi {{user.friendlyName}},
 
-Thanks for creating an account with {{siteName}}! To activate your account, click the following link:
+Obrigado por criar uma conta em {{siteName}}! Para ativar sua conta, clique no link a seguir:
 
 <{{link}}>
 
-If you were not expecting this email, just ignore it.',
+Se você não estava esperando esse e-mail, ignore-o.',
     'account_activation_heading' => 'When someone creates an account:',
     'account_activation_subject' => 'Activate your account',
     'by {author}' => 'by {author}',
@@ -1162,13 +1162,13 @@ If you were not expecting this email, just ignore it.',
     'five' => 'five',
     'folders' => 'folders',
     'folders and files' => 'folders and files',
-    'forgot_password_body' => 'Hey {{user.friendlyName}},
+    'forgot_password_body' => 'Oi {{user.friendlyName}},
 
-To reset your {{siteName}} password, click on this link:
+Para resetar sua senha em {{siteName}}, clique nesse link:
 
 <{{link}}>
 
-If you were not expecting this email, just ignore it.',
+Se você não estava esperando esse e-mail, ignore-o.',
     'forgot_password_heading' => 'When someone forgets their password:',
     'forgot_password_subject' => 'Reset your password',
     'four' => 'four',
@@ -1184,24 +1184,24 @@ If you were not expecting this email, just ignore it.',
     'seconds' => 'seconds',
     'seven' => 'seven',
     'six' => 'six',
-    'test_email_body' => 'Hey {{user.friendlyName}},
+    'test_email_body' => 'Oi {{user.friendlyName}},
 
-Congratulations! Craft was successfully able to send an email.
+Parabéns! Craft conseguiu enviar um e-mail com sucesso.
 
-Here are the settings you used:
+Aqui estão as configurações que você usou:
 
 {{ settings }}',
     'test_email_heading' => 'When you are testing your email settings:',
     'test_email_subject' => 'This is a test email from Craft',
     'three' => 'three',
     'two' => 'two',
-    'verify_new_email_body' => 'Hey {{user.friendlyName}},
+    'verify_new_email_body' => 'Olá {{user.friendlyName}},
 
-Please verify your new email address by clicking on this link:
+Por favor, verifique o seu novo endereço de e-mail, clicando neste link:
 
 <{{link}}>
 
-If you were not expecting this email, just ignore it.',
+Se você não solicitou este e-mail, simplesmente ignore.',
     'verify_new_email_heading' => 'When someone changes their email address:',
     'verify_new_email_subject' => 'Verify your new email address',
     'week' => 'week',
