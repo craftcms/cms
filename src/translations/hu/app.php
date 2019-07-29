91361008355f334defd1df2d830ac350e3255eeb
Brad Bell <brad@pixelandtonic.com>
2019-07-29 16:04:45 -0700

    Latest source strings for translations.

--- src/translations/hu/app.php
+++ src/translations/hu/app.php
@@ -86,7 +86,6 @@ return [
     'Are you sure you want to delete this block type?' => 'Biztosan törölni szeretnéd ez a blokktípust?',
     'Are you sure you want to delete this category?' => 'Biztosan törlöd ezt a kategóriát?',
     'Are you sure you want to delete this draft?' => 'Biztosan törölni szeretnéd ezt a piszkozatot?',
-    'Are you sure you want to delete this entry?' => 'Biztosan törli a bejegyzést?',
     'Are you sure you want to delete this field?' => 'Biztosan törölni szeretnéd ezt a mezőt?',
     'Are you sure you want to delete this group and all its fields?' => 'Biztosan törlöd a csoportot és annak minden mezőjét?',
     'Are you sure you want to delete this group?' => 'Biztosan törölni szeretnéd ezt a csoportot?',
@@ -797,7 +796,6 @@ return [
     'Save and add another' => 'Ment és hozzáad másikat',
     'Save and continue editing' => 'Ment és szerkesztés folytatása',
     'Save as a new category' => 'Ment új kategóriaként.',
-    'Save as a new entry' => 'Mentés új bejegyzésként',
     'Scale and crop' => 'Mér és szab',
     'Scale to fit' => 'Arányosan méretez',
     'Schema Name' => 'Séma neve',
@@ -1381,7 +1379,6 @@ Amennyiben nem neked szól ez a levél, egyszerűen csak hagyd figyelmen kívül
     'Learn how' => 'Learn how',
     'Price includes 1 year of updates.' => 'Price includes 1 year of updates.',
     '{name} should be at least {value}.' => '{name} should be at least {value}.',
-    'Entry updated.' => 'Entry updated.',
     'Business Tax ID' => 'Business Tax ID',
     'Save {type}' => 'Save {type}',
     'Save entries to other sites in the same site group' => 'Save entries to other sites in the same site group',
@@ -1390,7 +1387,6 @@ Amennyiben nem neked szól ez a levél, egyszerűen csak hagyd figyelmen kívül
     'This can be set to an environment variable.' => 'This can be set to an environment variable.',
     'Invalid verification code. Please login or reset your password.' => 'Invalid verification code. Please login or reset your password.',
     'Impersonate users' => 'Impersonate users',
-    'Delete entry' => 'Delete entry',
     'Update {type}' => 'Update {type}',
     'Please fix the following in your {file} file before proceeding:' => 'Please fix the following in your {file} file before proceeding:',
     'Label' => 'Label',
@@ -1462,5 +1458,10 @@ Amennyiben nem neked szól ez a levél, egyszerűen csak hagyd figyelmen kívül
     'Matrix Blocks' => 'Matrix Blocks',
     'Address Line 1' => 'Address Line 1',
     'Choose a site' => 'Choose a site',
-    'Address Line 2' => 'Address Line 2'
+    'Address Line 2' => 'Address Line 2',
+    'Save as a new {type}' => 'Save as a new {type}',
+    'Are you sure you want to delete this {type}?' => 'Are you sure you want to delete this {type}?',
+    'Delete {type}' => 'Delete {type}',
+    'Save as a Draft' => 'Save as a Draft',
+    '<strong>{cause}</strong> currently installed has the schema version of {existing} (expected {incoming})' => '<strong>{cause}</strong> currently installed has the schema version of {existing} (expected {incoming})'
 ];
