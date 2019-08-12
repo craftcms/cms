81241a78a19cdbb32221e72b4e4203cd87279873
lemiwinkz <giel@yellowflash.net>
2019-08-12 15:49:48 +0200

    Fix a bug where the section is not yet available

--- src/test/fixtures/elements/EntryFixture.php
+++ src/test/fixtures/elements/EntryFixture.php
@@ -76,9 +76,19 @@ abstract class EntryFixture extends ElementFixture
 
         // Ensure we match the titleFormat - https://github.com/craftcms/cms/issues/4663
         if (isset($data['typeId'])) {
-            $entry = new Entry($data);
-            $entry->updateTitle();
-            $query->title = $entry->title;
+            $section = Craft::$app->sections->getSectionById($data['sectionId']);
+            $type = Craft::$app->sections->getEntryTypeById($data['typeId']);
+
+            // Ensure we have a section and type to work with...
+            if ($type && $section) {
+                $entry = new Entry([
+                    'typeId' => $data['typeId'],
+                    'sectionId' => $data['sectionId']
+                ]);
+                $entry->setAttributes($data);
+                $entry->updateTitle();
+                $query->title = $entry->title;
+            }
         }
 
         return $query->one();
