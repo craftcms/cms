37d5e5b9791025a6eaf89d008683a5827228b9d9
lemiwinkz <giel@yellowflash.net>
2019-08-17 16:50:27 +0200

    Cleanup

--- src/controllers/UsersController.php
+++ src/controllers/UsersController.php
@@ -1135,7 +1135,7 @@ class UsersController extends Controller
             $preferences = array_merge($preferences, [
                 'enableDebugToolbarForSite' => (bool)$request->getBodyParam('enableDebugToolbarForSite', $user->getPreference('enableDebugToolbarForSite')),
                 'enableDebugToolbarForCp' => (bool)$request->getBodyParam('enableDebugToolbarForCp', $user->getPreference('enableDebugToolbarForCp')),
-                'showExceptionDetail' => (bool)$request->getBodyParams('showExceptionDetail', $user->getPreference('showExceptionDetail'))
+                'showExceptionDetail' => (bool)$request->getBodyParam('showExceptionDetail', $user->getPreference('showExceptionDetail'))
             ]);
         }
 
