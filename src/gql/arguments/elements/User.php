2b368ccd18abd5883677727ead5f609aee83c2c1
andris-sevcenko <asevcenko@gmail.com>
2019-08-21 19:18:47 +0300

    Reduce the available options for querying users.

--- src/gql/arguments/elements/User.php
+++ src/gql/arguments/elements/User.php
@@ -16,26 +16,6 @@ class User extends ElementArguments
     public static function getArguments(): array
     {
         return array_merge(parent::getArguments(), [
-            'admin' => [
-                'name' => 'admin',
-                'type' => Type::boolean(),
-                'description' => 'Narrows the query results to only users that have admin accounts.'
-            ],
-            'can' => [
-                'name' => 'can',
-                'type' => Type::string(),
-                'description' => 'Narrows the query results to only users that have a certain user permission, either directly on the user account or through one of their user groups.'
-            ],
-            'groupId' => [
-                'name' => 'groupId',
-                'type' => Type::listOf(Type::int()),
-                'description' => 'Narrows the query results based on the user group the users belong to, per the groups’ IDs.'
-            ],
-            'group' => [
-                'name' => 'group',
-                'type' => Type::listOf(Type::string()),
-                'description' => 'Narrows the query results based on the user group the users belong to.'
-            ],
             'email' => [
                 'name' => 'email',
                 'type' => Type::listOf(Type::string()),
@@ -55,11 +35,6 @@ class User extends ElementArguments
                 'name' => 'lastName',
                 'type' => Type::listOf(Type::string()),
                 'description' => 'Narrows the query results based on the users’ last names.'
-            ],
-            'lastLoginDate' => [
-                'name' => 'lastLoginDate',
-                'type' => Type::string(),
-                'description' => 'Narrows the query results based on the users’ last login dates.'
             ],
         ]);
     }
