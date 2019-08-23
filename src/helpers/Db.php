09f7f9b8ab2bc13dacaa27d3b2f0a12eda85a076
Brandon Kelly <brandon@pixelandtonic.com>
2019-08-23 10:59:18 -0700

    Cleanup

--- src/helpers/Db.php
+++ src/helpers/Db.php
@@ -434,10 +434,10 @@ class Db
      * @param string $defaultOperator The default operator to apply to the values
      * (can be `not`, `!=`, `<=`, `>=`, `<`, `>`, or `=`)
      * @param bool $caseInsensitive Whether the resulting condition should be case-insensitive
-     * @param null $columnType The database column type the param is targeting.
+     * @param string|null $columnType The database column type the param is targeting
      * @return mixed
      */
-    public static function parseParam(string $column, $value, string $defaultOperator = '=', $caseInsensitive = false, $columnType = null)
+    public static function parseParam(string $column, $value, string $defaultOperator = '=', bool $caseInsensitive = false, string $columnType = null)
     {
         if (is_string($value) && preg_match('/^not\s*$/', $value)) {
             return '';
