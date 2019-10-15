7ec2011bc477a64e15a5c4476ab505c3c5a95da0
Brandon Kelly <brandon@pixelandtonic.com>
2019-10-15 05:01:45 -0700

    Plain Text field improvements
    Resolves #5099

--- CHANGELOG-v3.4.md
+++ CHANGELOG-v3.4.md
@@ -18,6 +18,8 @@
 - Sections’ entry URI format settings are now shown when running Craft in headless mode. ([#4934](https://github.com/craftcms/cms/issues/4934))
 - The “Primary entry page” preview target is now user-customizable alongside all other preview targets in sections’ settings. ([#4520](https://github.com/craftcms/cms/issues/4520))
 - Control panel requests are now always set to the primary site, regardless of the URL they were accessed from.
+- Plain Text fields can now specify a maximum size in bytes. ([#5099](https://github.com/craftcms/cms/issues/5099))
+- Plain Text fields’ Column Type settings now have an “Automatic” option, which is selected by default for new fields. ([#5099](https://github.com/craftcms/cms/issues/5099))
 - Set Password and Verify Email links now use the `setPasswordPath` and `verifyEmailPath` config settings. ([#4925](https://github.com/craftcms/cms/issues/4925))
 - `resave/*` commands now have an `--update-search-index` argument (defaults to `false`). ([#4840](https://github.com/craftcms/cms/issues/4840))
 - Plugins can now modify the GraphQL schema by listening for the `defineGqlTypeFields` event.
--- src/fields/PlainText.php
+++ src/fields/PlainText.php
@@ -71,9 +71,15 @@ class PlainText extends Field implements PreviewableFieldInterface
     public $charLimit;
 
     /**
-     * @var string The type of database column the field should have in the content table
+     * @var int|null The maximum number of bytes allowed in the field
+     * @since 3.4.0
      */
-    public $columnType = Schema::TYPE_TEXT;
+    public $byteLimit;
+
+    /**
+     * @var string|null The type of database column the field should have in the content table
+     */
+    public $columnType;
 
     // Public Methods
     // =========================================================================
@@ -83,10 +89,21 @@ class PlainText extends Field implements PreviewableFieldInterface
      */
     public function __construct(array $config = [])
     {
-        // This existed at one point way back in the day.
-        if (isset($config['maxLengthUnit'])) {
-            unset($config['maxLengthUnit']);
+        if (isset($config['limitUnit'], $config['fieldLimit'])) {
+            if ($config['limitUnit'] === 'chars') {
+                $config['charLimit'] = (int)$config['fieldLimit'] ?: null;
+            } else {
+                $config['byteLimit'] = (int)$config['fieldLimit'] ?: null;
+            }
+            unset($config['limitUnit'], $config['fieldLimit']);
         }
+
+        if (isset($config['columnType']) && $config['columnType'] === 'auto') {
+            unset($config['columnType']);
+        }
+
+        // This existed at one point way back in the day.
+        unset($config['maxLengthUnit']);
 
         parent::__construct($config);
     }
@@ -97,8 +114,8 @@ class PlainText extends Field implements PreviewableFieldInterface
     public function rules()
     {
         $rules = parent::rules();
-        $rules[] = [['initialRows', 'charLimit'], 'integer', 'min' => 1];
-        $rules[] = [['charLimit'], 'validateCharLimit'];
+        $rules[] = [['initialRows', 'charLimit', 'byteLimit'], 'integer', 'min' => 1];
+        $rules[] = [['charLimit', 'byteLimit'], 'validateFieldLimit'];
         return $rules;
     }
 
@@ -107,13 +124,15 @@ class PlainText extends Field implements PreviewableFieldInterface
      *
      * @param string $attribute
      */
-    public function validateCharLimit(string $attribute)
+    public function validateFieldLimit(string $attribute)
     {
-        if ($this->charLimit) {
-            $columnTypeMax = Db::getTextualColumnStorageCapacity($this->columnType);
-
-            if ($columnTypeMax && $columnTypeMax < $this->charLimit) {
-                $this->addError($attribute, Craft::t('app', 'Character Limit is too big for your chosen Column Type.'));
+        if ($bytes = $this->$attribute) {
+            if ($attribute === 'charLimit') {
+                $bytes *= 4;
+            }
+            $columnTypeMax = Db::getTextualColumnStorageCapacity($this->getContentColumnType());
+            if ($columnTypeMax && $columnTypeMax < $bytes) {
+                $this->addError($attribute, Craft::t('app', 'Field Limit is too big for your chosen Column Type.'));
             }
         }
     }
@@ -134,7 +153,19 @@ class PlainText extends Field implements PreviewableFieldInterface
      */
     public function getContentColumnType(): string
     {
-        return $this->columnType;
+        if ($this->columnType) {
+            return $this->columnType;
+        }
+
+        if ($this->byteLimit) {
+            $bytes = $this->byteLimit;
+        } else if ($this->charLimit) {
+            $bytes = $this->charLimit * 4;
+        } else {
+            return Schema::TYPE_TEXT;
+        }
+
+        return Schema::TYPE_STRING . "({$bytes})";
     }
 
     /**
@@ -169,7 +200,11 @@ class PlainText extends Field implements PreviewableFieldInterface
     public function getElementValidationRules(): array
     {
         return [
-            ['string', 'max' => $this->charLimit ?: null],
+            [
+                'string',
+                'max' => $this->byteLimit ?? $this->charLimit ?? null,
+                'encoding' => $this->byteLimit ? '8bit' : 'UTF-8',
+            ],
         ];
     }
 
