1db6f8e17b67e40933d7fbdb7301ffcd3b9553c3
Benjamin David <ben@dukt.net>
2019-11-13 15:58:58 +0100

    Install the latest compatible version instead of just the latest version

--- src/web/assets/pluginstore/src/js/components/PluginActions.vue
+++ src/web/assets/pluginstore/src/js/components/PluginActions.vue
@@ -32,7 +32,7 @@
                     <input type="hidden" name="packageName" :value="plugin.packageName">
                     <input type="hidden" name="handle" :value="plugin.handle">
                     <input type="hidden" name="edition" :value="edition.handle">
-                    <input type="hidden" name="version" :value="plugin.version">
+                    <input type="hidden" name="version" :value="plugin.latestCompatibleVersion">
                 </template>
 
                 <!-- Install (Free) -->
