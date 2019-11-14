3d4ce8e5c21d2497694e1ceef975f42963a414e3
Benjamin David <ben@dukt.net>
2019-11-14 08:07:32 +0100

    Improved styles for the Thank you page

--- src/web/assets/pluginstore/src/js/components/modal/steps/ThankYou.vue
+++ src/web/assets/pluginstore/src/js/components/modal/steps/ThankYou.vue
@@ -5,9 +5,9 @@
                 <icon icon="check-circle" size="4xl" />
                 <h2>{{ "Thank You!"|t('app') }}</h2>
                 <p class="light">{{ "Your order has been processed successfully."|t('app') }}</p>
-                <p>
+                <div class="mt-6">
                     <btn kind="primary" :href="managePluginsUrl">{{ "Manage plugins"|t('app') }}</btn>
-                </p>
+                </div>
             </div>
         </template>
     </step>
