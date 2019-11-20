85802b0b36bc4c923aa0ddb03681d96c7874c97e
Nathaniel Hammond <nathanielhammond@gmail.com>
2019-11-20 11:28:52 +0000

    Added Admin Table helper class

---
+++ src/helpers/AdminTable.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @link https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license https://craftcms.github.io/license/
+ */
+
+namespace craft\helpers;
+
+use Craft;
+
+/**
+ * Admin Table helper
+ *
+ * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @since 3.4
+ */
+class AdminTable
+{
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * @param int $page
+     * @param int $total
+     * @param int $limit
+     * @return array
+     */
+    public static function paginationLinks(int $page, int $total, int $limit): array
+    {
+        $lastPage = ceil($total / $limit);
+        $from = ($page * $limit) - $limit;
+        $to = $from + $limit;
+        $to = $to > $total ? $total : $to;
+        $from++;
+
+        return [
+            'total' => $total,
+            'per_page' => $limit,
+            'current_page' => $page,
+            'last_page' => (int)$lastPage,
+            'next_page_url' => '?next',
+            'prev_page_url' => '?prev',
+            'from' => (int)$from,
+            'to' => (int)$to,
+        ];
+    }
+}
