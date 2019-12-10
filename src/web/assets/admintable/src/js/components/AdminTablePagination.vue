d11c96d502217ac7f20ba02d799f17ad750019bc
Nathaniel Hammond <nathanielhammond@gmail.com>
2019-12-09 16:43:06 -0800

    Update pagination styling

--- src/web/assets/admintable/src/js/components/AdminTablePagination.vue
+++ src/web/assets/admintable/src/js/components/AdminTablePagination.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="tablePagination" class="flex pagination">
+  <div v-if="tablePagination" class="vue-admin-table-pagination flex pagination">
     <div
             @click="loadPage('prev')"
             class="page-link" data-icon="leftangle"
@@ -10,19 +10,39 @@
             class="page-link" data-icon="rightangle"
             :class="[isOnLastPage ? 'disabled' : '']"
             title="Next Page"></div>
-    <div v-show="tablePagination" class="page-info">{{tablePagination.from}}-{{tablePagination.to}} of {{tablePagination.total}}</div>
+    <div v-show="tablePagination" class="page-info">{{tablePagination.from}}-{{tablePagination.to}} of {{tablePagination.total}} {{countWording}}</div>
   </div>
 </template>
 
 <script>
+    /* global Craft */
     import PaginationMixin from 'vuetable-2/src/components/VuetablePaginationMixin'
 
     export default {
         name: 'AdminTablePagination',
         mixins: [PaginationMixin],
+        props: {
+          singular: {
+              type: String,
+              default: Craft.t('app', 'Item')
+          },
+          plural: {
+              type: String,
+              default: Craft.t('app', 'Items')
+          }
+        },
+        computed: {
+            countWording() {
+                return this.tablePagination.total == 1 ? this.singular : this.plural;
+            }
+        }
     }
 </script>
 
-<style scoped>
-
+<style land="scss">
+  .vue-admin-table-pagination {
+    border-top: 1px solid #f3f7fc;
+    margin-top: 14px;
+    padding-top: 14px;
+  }
 </style>
