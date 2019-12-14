82d25d3538bba762bc329f1f0171414a57eec102
andris-sevcenko <asevcenko@gmail.com>
2019-12-14 11:13:48 -0800

    Test for eager loading with count for GraphQL.

--- tests/unit/gql/ExtractEagerLoadingParameterTest.php
+++ tests/unit/gql/ExtractEagerLoadingParameterTest.php
@@ -185,6 +185,12 @@ GQL;
                 ],
             ],
             [
+                '{ entries { __count(field: "assetField") assetField { filename }}}',
+                [
+                    ['assetField', ['volumeId' => [5, 7], 'count' => true]]
+                ],
+            ],
+            [
                 '{ entries { assetField { filename }}}',
                 [
                     ['assetField', ['volumeId' => [5, 7]]]
