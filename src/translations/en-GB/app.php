a4df9d98758bcb3712ca7d95a28ea771806a9af5
Brad Bell <brad@pixelandtonic.com>
2020-01-02 13:23:29 -0800

    New translations app.php (English, United Kingdom)

--- src/translations/en-GB/app.php
+++ src/translations/en-GB/app.php
@@ -180,7 +180,7 @@ return [
     'Clear Caches' => 'Clear Caches',
     'Clear all' => 'Clear all',
     'Close' => 'Close',
-    'Close Live Preview' => 'Close Live Preview',
+    'Close Preview' => 'Close Preview',
     'Cloud Storage Integration' => 'Cloud storage integration',
     'Collapse' => 'Collapse',
     'Color' => 'Colour',
