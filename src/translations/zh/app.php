3c982bcfae8c993496883d9a1e9fab7d03ba65d6
Brad Bell <brad@pixelandtonic.com>
2020-01-02 13:24:08 -0800

    New translations app.php (Chinese Simplified)

--- src/translations/zh/app.php
+++ src/translations/zh/app.php
@@ -180,7 +180,7 @@ return [
     'Clear Caches' => '清除缓存',
     'Clear all' => '全部清除',
     'Close' => '关闭',
-    'Close Live Preview' => '关闭实时预览',
+    'Close Preview' => 'Close Preview',
     'Cloud Storage Integration' => '云存储集成',
     'Collapse' => '折叠',
     'Color' => '颜色',
