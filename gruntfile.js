531c8054291ff687eb21338a9c58cc4eb48aba1c
Brandon Kelly <brandon@pixelandtonic.com>
2020-01-06 10:48:00 -0800

    Use the harmony branch for grunt-contrib-uglify

--- gruntfile.js
+++ gruntfile.js
@@ -142,7 +142,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-postcss');
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-uglify-es');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
 
