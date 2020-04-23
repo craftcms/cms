bb4dc0e159f9815b33fc35285856ee4b9beeecec
Brandon Kelly <brandon@pixelandtonic.com>
2020-04-23 13:45:19 -0700

    Fixed eager-loaded getDescendants() if $dist is passed

--- src/base/Element.php
+++ src/base/Element.php
@@ -1847,7 +1847,12 @@ abstract class Element extends Component implements ElementInterface
     {
         // Eager-loaded?
         if (($descendants = $this->getEagerLoadedElements('descendants')) !== null) {
-            return $descendants;
+            if ($dist === null) {
+                return $descendants;
+            }
+            return ArrayHelper::where($descendants, function(self $element) use ($dist) {
+                return $element->level <= $this->level + $dist;
+            });
         }
 
         return static::find()
