976dceb71e4ee736c989de737063a22122b1839e
Brandon Kelly <brandon@pixelandtonic.com>
2020-04-23 06:29:23 -0700

    Reset the memoized volume after moving an asset

--- src/elements/Asset.php
+++ src/elements/Asset.php
@@ -1841,6 +1841,7 @@ class Asset extends Element
         $this->folderId = $folderId;
         $this->folderPath = $newFolder->path;
         $this->filename = $filename;
+        $this->_volume = $newVolume;
 
         // If there was a new file involved, update file data.
         if ($tempPath) {
