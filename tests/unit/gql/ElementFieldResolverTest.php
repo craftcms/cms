0cf71279ee30730bb4ff840e0327f7416799a5d4
andris-sevcenko <asevcenko@gmail.com>
2020-04-24 11:30:03 +0300

    Keep on improving that code coverage.

--- tests/unit/gql/ElementFieldResolverTest.php
+++ tests/unit/gql/ElementFieldResolverTest.php
@@ -11,18 +11,22 @@ use Codeception\Test\Unit;
 use Craft;
 use craft\elements\Asset;
 use craft\elements\Asset as AssetElement;
+use craft\elements\Category as CategoryElement;
 use craft\elements\Entry as EntryElement;
 use craft\elements\GlobalSet as GlobalSetElement;
 use craft\elements\MatrixBlock as MatrixBlockElement;
 use craft\elements\User as UserElement;
 use craft\errors\GqlException;
 use craft\gql\types\elements\Asset as AssetGqlType;
+use craft\gql\types\elements\Category as CategoryGqlType;
 use craft\gql\types\elements\Entry as EntryGqlType;
 use craft\gql\types\elements\GlobalSet as GlobalSetGqlType;
 use craft\gql\types\elements\MatrixBlock as MatrixBlockGqlType;
+use craft\gql\types\elements\Tag as TagGqlType;
 use craft\gql\types\elements\User as UserGqlType;
 use craft\helpers\Json;
 use craft\helpers\StringHelper;
+use craft\models\CategoryGroup;
 use craft\models\EntryType;
 use craft\models\GqlSchema;
 use craft\models\MatrixBlockType;
@@ -65,7 +69,7 @@ class ElementFieldResolverTest extends Unit
      * @dataProvider entryFieldTestDataProvider
      *
      * @param string $gqlTypeClass The Gql type class
-     * @param string $propertyName The propery being tested
+     * @param string $propertyName The property being tested
      * @param mixed $result True for exact match, false for non-existing or a callback for fetching the data
      */
     public function testEntryFieldResolving(string $gqlTypeClass, string $propertyName, $result)
@@ -98,7 +102,7 @@ class ElementFieldResolverTest extends Unit
      * @dataProvider assetFieldTestDataProvider
      *
      * @param string $gqlTypeClass The Gql type class
-     * @param string $propertyName The propery being tested
+     * @param string $propertyName The property being tested
      * @param mixed $result True for exact match, false for non-existing or a callback for fetching the data
      */
     public function testAssetFieldResolving(string $gqlTypeClass, string $propertyName, $result)
@@ -121,7 +125,7 @@ class ElementFieldResolverTest extends Unit
      * @dataProvider globalSetFieldTestDataProvider
      *
      * @param string $gqlTypeClass The Gql type class
-     * @param string $propertyName The propery being tested
+     * @param string $propertyName The property being tested
      * @param mixed $result True for exact match, false for non-existing or a callback for fetching the data
      */
     public function testGlobalSetFieldResolving(string $gqlTypeClass, string $propertyName, $result)
@@ -140,12 +144,68 @@ class ElementFieldResolverTest extends Unit
     }
 
     /**
+     * Test resolving fields on categories
+     *
+     * @dataProvider categoryFieldTestDataProvider
+     *
+     * @param string $gqlTypeClass The Gql type class
+     * @param string $propertyName The property being tested
+     * @param mixed $result True for exact match, false for non-existing or a callback for fetching the data
+     */
+    public function testCategoryFieldResolving(string $gqlTypeClass, string $propertyName, $result)
+    {
+        $groupHandle = StringHelper::UUID();
+
+        $mockElement = $this->make(
+            CategoryElement::class, [
+                '__get' => function($property) {
+                    // Assume a content field named 'plainTextField'
+                    return $property == 'plainTextField' ? 'ok' : $this->$property;
+                },
+                'getGroup' => function() use ($groupHandle) {
+                    return $this->make(CategoryGroup::class, ['handle' =>$groupHandle]);
+                },
+            ]
+        );
+
+        $this->_runTest($mockElement, $gqlTypeClass, $propertyName, $result);
+    }
+
+    /**
+     * Test resolving fields on tags
+     *
+     * @dataProvider tagFieldTestDataProvider
+     *
+     * @param string $gqlTypeClass The Gql type class
+     * @param string $propertyName The property being tested
+     * @param mixed $result True for exact match, false for non-existing or a callback for fetching the data
+     */
+    public function testTagFieldResolving(string $gqlTypeClass, string $propertyName, $result)
+    {
+        $groupHandle = StringHelper::UUID();
+
+        $mockElement = $this->make(
+            CategoryElement::class, [
+                '__get' => function($property) {
+                    // Assume a content field named 'plainTextField'
+                    return $property == 'plainTextField' ? 'ok' : $this->$property;
+                },
+                'getGroup' => function() use ($groupHandle) {
+                    return $this->make(CategoryGroup::class, ['handle' => $groupHandle]);
+                },
+            ]
+        );
+
+        $this->_runTest($mockElement, $gqlTypeClass, $propertyName, $result);
+    }
+
+    /**
      * Test resolving fields on matrix blocks.
      *
      * @dataProvider matrixBlockFieldTestDataProvider
      *
      * @param string $gqlTypeClass The Gql type class
-     * @param string $propertyName The propery being tested
+     * @param string $propertyName The property being tested
      * @param mixed $result True for exact match, false for non-existing or a callback for fetching the data
      */
     public function testMatrixBlockFieldResolving(string $gqlTypeClass, string $propertyName, $result)
@@ -176,7 +236,7 @@ class ElementFieldResolverTest extends Unit
      * @dataProvider userFieldTestDataProvider
      *
      * @param string $gqlTypeClass The Gql type class
-     * @param string $propertyName The propery being tested
+     * @param string $propertyName The property being tested
      * @param mixed $result True for exact match, false for non-existing or a callback for fetching the data
      */
     public function testUserFieldResolving(string $gqlTypeClass, string $propertyName, $result)
@@ -238,7 +298,7 @@ class ElementFieldResolverTest extends Unit
      * Run the test on an element for a type class with the property name.
      *
      * @param string $gqlTypeClass The Gql type class
-     * @param string $propertyName The propery being tested
+     * @param string $propertyName The property being tested
      * @param mixed $result True for exact match, false for non-existing or a callback for fetching the data
      */
     public function _runTest($element, string $gqlTypeClass, string $propertyName, $result)
@@ -295,6 +355,32 @@ class ElementFieldResolverTest extends Unit
             [GlobalSetGqlType::class, 'missingProperty', false],
             [GlobalSetGqlType::class, 'plainTextField', true],
             [GlobalSetGqlType::class, 'handle', true],
+        ];
+    }
+
+    public function categoryFieldTestDataProvider(): array
+    {
+        return [
+            [CategoryGqlType::class, 'missingProperty', false],
+            [CategoryGqlType::class, 'plainTextField', true],
+            [
+                CategoryGqlType::class, 'groupHandle', function($source) {
+                    return $source->getGroup()->handle;
+                }
+            ],
+        ];
+    }
+
+    public function tagFieldTestDataProvider(): array
+    {
+        return [
+            [TagGqlType::class, 'missingProperty', false],
+            [TagGqlType::class, 'plainTextField', true],
+            [
+                TagGqlType::class, 'groupHandle', function($source) {
+                    return $source->getGroup()->handle;
+                }
+            ],
         ];
     }
 
