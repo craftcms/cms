0e4ec2cd087b53f1eba3ddce672578fc545c2cac
andris-sevcenko <asevcenko@gmail.com>
2020-05-05 13:25:08 +0300

    Add `MockElementQuery` for improved testability.

---
+++ src/test/mockclasses/elements/MockElementQuery.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * @link      https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license   https://craftcms.github.io/license/
+ */
+
+namespace craft\test\mockclasses\elements;
+
+use craft\base\Element;
+
+/**
+ * MockElementQuery is used to mimic element query and help mocking element query results.
+ *
+ * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @since  3.5
+ */
+class MockElementQuery
+{
+    /**
+     * The "elements" to return when invoking `one()` or `all()`
+     * @var array
+     */
+    protected $returnValues = [];
+
+    /**
+     * The element query properties
+     * @var array
+     */
+    protected $properties = [];
+
+    /**
+     * MockElementQuery constructor.
+     *
+     * @param array $returnValues
+     */
+    public function __construct($returnValues = [])
+    {
+        if (!empty($returnValues)) {
+            $this->returnValues = $returnValues;
+        }
+    }
+
+    /**
+     * Set the return values.
+     *
+     * @param array $values
+     */
+    public function setReturnValues(array $values = []) {
+        $this->returnValues = $values;
+    }
+
+    /**
+     * Setter for mock query arguments.
+     *
+     * @param $name
+     * @param $value
+     */
+    public function __set($name, $value) {
+        $this->properties[$name] = $value;
+    }
+
+    /**
+     * Check if a property has been set already.
+     *
+     * @param $name
+     * @return bool
+     */
+    public function __isset($name)
+    {
+        return array_key_exists($name, $this->properties);
+    }
+
+    /**
+     * Getter for mock query arguments.
+     *
+     * @param $name
+     */
+    public function __get($name) {
+        return $this->properties[$name] ?? null;
+    }
+
+    /**
+     * Mock setting query arguments via a method call.
+     *
+     * @param $method
+     * @param $arguments
+     * @return static
+     */
+    public function __call($method, $arguments) {
+        $this->properties[$method] = reset($arguments);
+
+        return $this;
+    }
+
+    /**
+     * Return all the return values.
+     *
+     * @return array
+     */
+    public function all(): array {
+        return $this->returnValues;
+    }
+
+    /**
+     * Return a return value.
+     *
+     * @return mixed|null
+     */
+    public function one() {
+        return !empty($this->returnValues) ? reset($this->returnValues) : null;
+    }
+}
