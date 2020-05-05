29bc568ebc365196399883ccc383f33ac8c8f8e5
Brandon Kelly <brandon@pixelandtonic.com>
2020-05-05 08:30:51 -0700

    Ignore disabled/soft-deleted elements for :empty: / :notempty:
    Fixes #6026

--- CHANGELOG-v3.md
+++ CHANGELOG-v3.md
@@ -364,6 +364,8 @@
 - Fixed a bug where element exporting would redirect the browser window if the export request didn’t immediately return the export data. ([#5558](https://github.com/craftcms/cms/issues/5558))
 - Fixed a “Division by zero” error that occurred if an image transform didn’t specify a width or a height. ([#5590](https://github.com/craftcms/cms/issues/5590))
 - Fixed a bug where elements weren’t always retaining their positions in element indexes between pages.
+- Fixed a bug where relational fields weren’t ignoring disabled and soft-deleted elements when `:empty:` or `:notempty:` were passed to their element query params. ([#6026](https://github.com/craftcms/cms/issues/6026))
+- Fixed a bug where Matrix fields weren’t ignoring disabled blocks when `:empty:` or `:notempty:` were passed to their element query params. ([#6026](https://github.com/craftcms/cms/issues/6026))
 
 ## 3.4.3 - 2020-02-03
 
--- src/fields/BaseRelationField.php
+++ src/fields/BaseRelationField.php
@@ -449,14 +449,30 @@ abstract class BaseRelationField extends Field implements PreviewableFieldInterf
         }
 
         if ($value === ':notempty:' || $value === ':empty:') {
-            $alias = 'relations_' . $this->handle;
-            $operator = ($value === ':notempty:' ? '!=' : '=');
-            $paramHandle = ':fieldId' . StringHelper::randomString(8);
+            $ns = $this->handle . '_' . StringHelper::randomString(5);
+            $condition = [
+                'exists', (new Query())
+                    ->from(["relations_$ns" => TableName::RELATIONS])
+                    ->innerJoin(["elements_$ns" => TableName::ELEMENTS], "[[elements_$ns.id]] = [[relations_$ns.targetId]]")
+                    ->leftJoin(["elements_sites_$ns" => TableName::ELEMENTS_SITES], [
+                        'and',
+                        "[[elements_sites_$ns.elementId]] = [[elements_$ns.id]]",
+                        ["elements_sites_$ns.siteId" => $query->siteId],
+                    ])
+                    ->where("[[relations_$ns.sourceId]] = [[elements.id]]")
+                    ->andWhere([
+                        "relations_$ns.fieldId" => $this->id,
+                        "elements_$ns.enabled" => true,
+                        "elements_$ns.dateDeleted" => null,
+                    ])
+                    ->andWhere(['not', ["elements_sites_$ns.enabled" => false]])
+            ];
 
-            $query->subQuery->andWhere(
-                "(select count([[{$alias}.id]]) from {{%relations}} {{{$alias}}} where [[{$alias}.sourceId]] = [[elements.id]] and [[{$alias}.fieldId]] = {$paramHandle}) {$operator} 0",
-                [$paramHandle => $this->id]
-            );
+            if ($value === ':notempty:') {
+                $query->subQuery->andWhere($condition);
+            } else {
+                $query->subQuery->andWhere(['not', $condition]);
+            }
         } else {
             $parser = new ElementRelationParamParser([
                 'fields' => [
