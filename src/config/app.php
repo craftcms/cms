d2140016b513d8eafcb5a4bac8fbc0e48bb4c23b
Brandon Kelly <brandon@pixelandtonic.com>
2020-05-28 12:22:46 -0700

    Finish 3.4.21

--- src/config/app.php
+++ src/config/app.php
@@ -3,7 +3,7 @@
 return [
     'id' => 'CraftCMS',
     'name' => 'Craft CMS',
-    'version' => '3.4.20',
+    'version' => '3.4.21',
     'schemaVersion' => '3.4.10',
     'minVersionRequired' => '2.6.2788',
     'basePath' => dirname(__DIR__), // Defines the @app alias
