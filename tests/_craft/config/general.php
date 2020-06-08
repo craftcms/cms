f4bf71ebbb2c5cab87d7a2070737bf3469cd31ee
Brandon Kelly <brandon@pixelandtonic.com>
2020-06-08 13:15:30 -0700

    Fixed useProjectConfigFile test bug

--- tests/_craft/config/general.php
+++ tests/_craft/config/general.php
@@ -1,22 +1,19 @@
 <?php
 
+use craft\test\Craft;
+
 /**
  * General Configuration
  *
  * All of your system's general configuration settings go in here.
  * You can see a list of the default settings in craft/app/config/defaults/general.php
  */
-$config = [
+return [
     'devMode' => true,
     'cpTrigger' => 'adminustriggerus',
     'siteUrl' => 'https://test.craftcms.test/index.php',
     'slugWordSeparator' => '--',
     'allowUppercaseInSlug' => true,
-    'securityKey' => getenv('SECURITY_KEY')
+    'securityKey' => getenv('SECURITY_KEY'),
+    'useProjectConfigFile' => isset(Craft::$instance) && Craft::$instance->_getConfig('projectConfig') !== null,
 ];
-
-
-$testConfig = \craft\test\Craft::$testConfig;
-$config['useProjectConfigFile'] = !empty($testConfig['projectConfig']);
-
-return $config;
