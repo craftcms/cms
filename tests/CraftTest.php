6b7170625b21cf69af946bd3d6b269addb2982ee
Jason McCallister <jason@craftcms.com>
2020-07-06 13:13:44 -0400

    Add tests for bool
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- tests/CraftTest.php
+++ tests/CraftTest.php
@@ -14,5 +14,34 @@ class CraftTest extends TestCase
 
         // Assert
         $this->assertEquals('testing', $env);
+        putenv('CRAFT_TEST');
+    }
+
+    public function testParseEnvReturnsTrue()
+    {
+        // Arrange
+        putenv("CRAFT_TEST=true");
+
+        // Act
+        $env = Craft::parseEnv('$CRAFT_TEST');
+
+        // Assert
+        $this->assertEquals(true, $env);
+        $this->assertIsBool($env);
+        putenv('CRAFT_TEST');
+    }
+
+    public function testParseEnvReturnsFalse()
+    {
+        // Arrange
+        putenv("CRAFT_TEST=false");
+
+        // Act
+        $env = Craft::parseEnv('$CRAFT_TEST');
+
+        // Assert
+        $this->assertEquals(false, $env);
+        $this->assertIsBool($env);
+        putenv('CRAFT_TEST');
     }
 }
