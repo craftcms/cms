1c5f1f9c61b26cb226e85725e2ff4bdc9367381c
qrazi <qrazi.sivlingworkz@gmail.com>
2020-09-04 14:00:43 +0200

    Add expected constant to example test suite, bringing it in line with official docs

--- src/test/internal/example-test-suite/tests/_bootstrap.php
+++ src/test/internal/example-test-suite/tests/_bootstrap.php
@@ -5,6 +5,7 @@ use craft\test\TestSetup;
 ini_set('date.timezone', 'UTC');
 
 // Use the current installation of Craft
+define('CRAFT_TESTS_PATH', __DIR__);
 define('CRAFT_STORAGE_PATH', __DIR__ . DIRECTORY_SEPARATOR . '_craft' . DIRECTORY_SEPARATOR . 'storage');
 define('CRAFT_TEMPLATES_PATH', __DIR__ . DIRECTORY_SEPARATOR . '_craft' . DIRECTORY_SEPARATOR . 'templates');
 define('CRAFT_CONFIG_PATH', __DIR__ . DIRECTORY_SEPARATOR . '_craft' . DIRECTORY_SEPARATOR . 'config');
