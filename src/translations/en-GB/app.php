498a6f1e1eb28064ff9d26b971cc09151db21fe2
Brad Bell <brad@pixelandtonic.com>
2020-09-11 17:57:24 -0700

    New translations app.php (English, United Kingdom)

--- src/translations/en-GB/app.php
+++ src/translations/en-GB/app.php
@@ -1709,5 +1709,6 @@ If you weren’t expecting this email, just ignore it.',
     'Retry Duration' => 'Retry Duration',
     'Online' => 'Online',
     'This field’s values are used as search keywords.' => 'This field’s values are used as search keywords.',
-    'The allowed values are [{values}]' => 'The allowed values are [{values}]'
+    'The allowed values are [{values}]' => 'The allowed values are [{values}]',
+    'Save and edit entry types' => 'Save and edit entry types',
 ];
