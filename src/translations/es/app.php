aa28a84929e1f97abdeec3d3ecefe601263e9946
Brad Bell <brad@pixelandtonic.com>
2020-09-11 17:57:43 -0700

    New translations app.php (Spanish)

--- src/translations/es/app.php
+++ src/translations/es/app.php
@@ -1709,5 +1709,6 @@ Si no estabas esperando este correo electrónico, ignóralo.',
     'Retry Duration' => 'Reintentar duración',
     'Online' => 'En línea',
     'This field’s values are used as search keywords.' => 'This field’s values are used as search keywords.',
-    'The allowed values are [{values}]' => 'The allowed values are [{values}]'
+    'The allowed values are [{values}]' => 'The allowed values are [{values}]',
+    'Save and edit entry types' => 'Save and edit entry types',
 ];
