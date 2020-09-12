baa24be1bc1f08f196d87ec41baf0191f278f461
Brad Bell <brad@pixelandtonic.com>
2020-09-11 17:57:45 -0700

    New translations app.php (Dutch)

--- src/translations/nl/app.php
+++ src/translations/nl/app.php
@@ -1709,5 +1709,6 @@ Als je deze email niet verwachtte, kun je hem gewoon negeren.',
     'Retry Duration' => 'Duur nieuwe poging',
     'Online' => 'Online',
     'This field’s values are used as search keywords.' => 'This field’s values are used as search keywords.',
-    'The allowed values are [{values}]' => 'The allowed values are [{values}]'
+    'The allowed values are [{values}]' => 'The allowed values are [{values}]',
+    'Save and edit entry types' => 'Save and edit entry types',
 ];
