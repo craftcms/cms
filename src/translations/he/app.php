ef270e1ad78cd6964f63005d4e92a1e6f898a589
Brad Bell <brad@pixelandtonic.com>
2020-09-11 17:57:56 -0700

    New translations app.php (Hebrew)

--- src/translations/he/app.php
+++ src/translations/he/app.php
@@ -1709,5 +1709,6 @@ return [
     'Retry Duration' => 'משך הניסוי מחדש',
     'Online' => 'מקוון',
     'This field’s values are used as search keywords.' => 'This field’s values are used as search keywords.',
-    'The allowed values are [{values}]' => 'The allowed values are [{values}]'
+    'The allowed values are [{values}]' => 'The allowed values are [{values}]',
+    'Save and edit entry types' => 'Save and edit entry types',
 ];
