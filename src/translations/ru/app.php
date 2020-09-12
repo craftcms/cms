3ab17c997f5f03a6fd34b7d4d15362195a6cbd31
Brad Bell <brad@pixelandtonic.com>
2020-09-11 17:57:39 -0700

    New translations app.php (Russian)

--- src/translations/ru/app.php
+++ src/translations/ru/app.php
@@ -1710,5 +1710,6 @@ return [
     'Retry Duration' => 'Длительность попытки повторного подключения',
     'Online' => 'Интерактивный режим',
     'This field’s values are used as search keywords.' => 'This field’s values are used as search keywords.',
-    'The allowed values are [{values}]' => 'The allowed values are [{values}]'
+    'The allowed values are [{values}]' => 'The allowed values are [{values}]',
+    'Save and edit entry types' => 'Save and edit entry types',
 ];
