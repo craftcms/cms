7876bdfedf9974fd52c718fd8569af1c2d106ac2
Brad Bell <brad@pixelandtonic.com>
2020-09-11 17:57:37 -0700

    New translations app.php (Slovak)

--- src/translations/sk/app.php
+++ src/translations/sk/app.php
@@ -1709,5 +1709,6 @@ Ak si tento email neočakával, jednoducho ho ignoruj.',
     'Retry Duration' => 'Opakovať trvanie',
     'Online' => 'Online',
     'This field’s values are used as search keywords.' => 'This field’s values are used as search keywords.',
-    'The allowed values are [{values}]' => 'The allowed values are [{values}]'
+    'The allowed values are [{values}]' => 'The allowed values are [{values}]',
+    'Save and edit entry types' => 'Save and edit entry types',
 ];
