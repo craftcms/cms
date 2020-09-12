43848214f3acd4ae755385c8c8114eecd223001d
Brad Bell <brad@pixelandtonic.com>
2020-09-11 17:57:33 -0700

    New translations app.php (Turkish)

--- src/translations/tr/app.php
+++ src/translations/tr/app.php
@@ -1705,5 +1705,6 @@ Bu epostayı beklemiyorduysanız, görmezden gelin.',
     'Retry Duration' => 'Yeniden Deneme Süresi',
     'Online' => 'Çevrimiçi',
     'This field’s values are used as search keywords.' => 'This field’s values are used as search keywords.',
-    'The allowed values are [{values}]' => 'The allowed values are [{values}]'
+    'The allowed values are [{values}]' => 'The allowed values are [{values}]',
+    'Save and edit entry types' => 'Save and edit entry types',
 ];
