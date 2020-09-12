fcab50d37409605f54c65a79fb4f2af67edc20a0
Brad Bell <brad@pixelandtonic.com>
2020-09-11 17:57:21 -0700

    New translations app.php (French, Canada)

--- src/translations/fr-CA/app.php
+++ src/translations/fr-CA/app.php
@@ -1709,5 +1709,6 @@ Si vous n’attendiez aucun courriel de ce type, ignorez-le, tout simplement.',
     'Retry Duration' => 'Durée des tentatives successives',
     'Online' => 'En ligne',
     'This field’s values are used as search keywords.' => 'This field’s values are used as search keywords.',
-    'The allowed values are [{values}]' => 'The allowed values are [{values}]'
+    'The allowed values are [{values}]' => 'The allowed values are [{values}]',
+    'Save and edit entry types' => 'Save and edit entry types',
 ];
