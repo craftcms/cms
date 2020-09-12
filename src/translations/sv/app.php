ec2516144e35ef47b23ef8590247ed1fe8945f63
Brad Bell <brad@pixelandtonic.com>
2020-09-11 17:57:35 -0700

    New translations app.php (Swedish)

--- src/translations/sv/app.php
+++ src/translations/sv/app.php
@@ -1709,5 +1709,6 @@ Om du inte väntade dig att få detta e-postmeddelande kan du ignorera det.',
     'Retry Duration' => 'Prova varaktighet på nytt',
     'Online' => 'Online',
     'This field’s values are used as search keywords.' => 'This field’s values are used as search keywords.',
-    'The allowed values are [{values}]' => 'The allowed values are [{values}]'
+    'The allowed values are [{values}]' => 'The allowed values are [{values}]',
+    'Save and edit entry types' => 'Save and edit entry types',
 ];
