c1502d8546e6ac6f312a31876feecd8f7b6788da
Brad Bell <brad@pixelandtonic.com>
2020-09-14 14:37:57 -0700

    New translations app.php (Chinese Simplified)

--- src/translations/zh/app.php
+++ src/translations/zh/app.php
@@ -1711,5 +1711,5 @@ return [
     'This field’s values are used as search keywords.' => 'This field’s values are used as search keywords.',
     'The allowed values are [{values}]' => 'The allowed values are [{values}]',
     'Save and edit entry types' => 'Save and edit entry types',
-    'Notes about your changes' => 'Notes about your changes',
+    'Notes about your changes' => '变更注释',
 ];
