db7c23fd03e7def9d84f7764eb211e1f7a95bd49
Brad Bell <brad@pixelandtonic.com>
2020-09-14 14:17:41 -0700

    New translations app.php (Norwegian Bokmal)

--- src/translations/nb/app.php
+++ src/translations/nb/app.php
@@ -1707,4 +1707,5 @@ Har du ikke bedt om denne e-posten, vennligst se bort fra den.',
     'This field’s values are used as search keywords.' => 'This field’s values are used as search keywords.',
     'The allowed values are [{values}]' => 'The allowed values are [{values}]',
     'Save and edit entry types' => 'Save and edit entry types',
+    'Notes about your changes' => 'Notes about your changes',
 ];
