353a72e83a07d02bac2bb3e52c607b0417690f91
Brad Bell <brad@pixelandtonic.com>
2020-09-14 14:37:50 -0700

    New translations app.php (Portuguese)

--- src/translations/pt/app.php
+++ src/translations/pt/app.php
@@ -1711,5 +1711,5 @@ Se você não solicitou este e-mail, simplesmente ignore.',
     'This field’s values are used as search keywords.' => 'This field’s values are used as search keywords.',
     'The allowed values are [{values}]' => 'The allowed values are [{values}]',
     'Save and edit entry types' => 'Save and edit entry types',
-    'Notes about your changes' => 'Notes about your changes',
+    'Notes about your changes' => 'Notas sobre suas alterações',
 ];
