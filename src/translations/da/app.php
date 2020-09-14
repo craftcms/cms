1803f6e3c93f3e3e9f0b265382d1491a8a7730da
Brad Bell <brad@pixelandtonic.com>
2020-09-14 14:18:28 -0700

    New translations app.php (Danish)

--- src/translations/da/app.php
+++ src/translations/da/app.php
@@ -1708,4 +1708,5 @@ Hvis du ikke forventede denne e-mail, bare ignorere den.',
     'This field’s values are used as search keywords.' => 'This field’s values are used as search keywords.',
     'The allowed values are [{values}]' => 'The allowed values are [{values}]',
     'Save and edit entry types' => 'Save and edit entry types',
+    'Notes about your changes' => 'Notes about your changes',
 ];
