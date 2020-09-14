faf6d4a1ed2fab78acf8f21d3501e71638d9e114
Brad Bell <brad@pixelandtonic.com>
2020-09-14 14:38:01 -0700

    New translations app.php (Turkish)

--- src/translations/tr/app.php
+++ src/translations/tr/app.php
@@ -1707,5 +1707,5 @@ Bu epostayı beklemiyorduysanız, görmezden gelin.',
     'This field’s values are used as search keywords.' => 'This field’s values are used as search keywords.',
     'The allowed values are [{values}]' => 'The allowed values are [{values}]',
     'Save and edit entry types' => 'Save and edit entry types',
-    'Notes about your changes' => 'Notes about your changes',
+    'Notes about your changes' => 'Değişikliklerinizle ilgili notlar',
 ];
