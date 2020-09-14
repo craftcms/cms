32afa68b7ffa9ce1031eff93a7532d2af006f0f1
Brad Bell <brad@pixelandtonic.com>
2020-09-14 14:18:07 -0700

    New translations app.php (Russian)

--- src/translations/ru/app.php
+++ src/translations/ru/app.php
@@ -1712,4 +1712,5 @@ return [
     'This field’s values are used as search keywords.' => 'This field’s values are used as search keywords.',
     'The allowed values are [{values}]' => 'The allowed values are [{values}]',
     'Save and edit entry types' => 'Save and edit entry types',
+    'Notes about your changes' => 'Notes about your changes',
 ];
