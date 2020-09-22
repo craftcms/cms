a9cb97ae1672ae90c56d9074e3a40aa21be24394
Brad Bell <brad@pixelandtonic.com>
2020-09-22 16:15:50 -0700

    New translations app.php (Hebrew)

--- src/translations/he/app.php
+++ src/translations/he/app.php
@@ -1723,4 +1723,5 @@ return [
     'Show thumbnails and titles' => 'Show thumbnails and titles',
     'Show thumbnails only' => 'Show thumbnails only',
     'Are you sure you want to close the editor? Any changes will be lost.' => 'Are you sure you want to close the editor? Any changes will be lost.',
+    'Upload a file' => 'Upload a file',
 ];
