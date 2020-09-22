7429db4b568a2a593932bc07056aa3b9e999931f
Brad Bell <brad@pixelandtonic.com>
2020-09-22 16:15:38 -0700

    New translations app.php (Spanish)

--- src/translations/es/app.php
+++ src/translations/es/app.php
@@ -1723,4 +1723,5 @@ Si no estabas esperando este correo electrónico, ignóralo.',
     'Show thumbnails and titles' => 'Show thumbnails and titles',
     'Show thumbnails only' => 'Show thumbnails only',
     'Are you sure you want to close the editor? Any changes will be lost.' => 'Are you sure you want to close the editor? Any changes will be lost.',
+    'Upload a file' => 'Upload a file',
 ];
