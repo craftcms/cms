f1f646e53d26e51bd839a3a6046392180b6ace9e
Brad Bell <brad@pixelandtonic.com>
2020-09-22 16:15:58 -0700

    New translations app.php (Czech)

--- src/translations/cs/app.php
+++ src/translations/cs/app.php
@@ -1719,4 +1719,5 @@ Pokud nevíte, o co se jedná, tuto zprávu klidně ignorujte.',
     'Show thumbnails and titles' => 'Show thumbnails and titles',
     'Show thumbnails only' => 'Show thumbnails only',
     'Are you sure you want to close the editor? Any changes will be lost.' => 'Are you sure you want to close the editor? Any changes will be lost.',
+    'Upload a file' => 'Upload a file',
 ];
