d18bf50932acaa7a9f4d9effa48e594d5bbc2bd7
Brad Bell <brad@pixelandtonic.com>
2020-09-22 16:15:31 -0700

    New translations app.php (Slovak)

--- src/translations/sk/app.php
+++ src/translations/sk/app.php
@@ -1723,4 +1723,5 @@ Ak si tento email neočakával, jednoducho ho ignoruj.',
     'Show thumbnails and titles' => 'Show thumbnails and titles',
     'Show thumbnails only' => 'Show thumbnails only',
     'Are you sure you want to close the editor? Any changes will be lost.' => 'Are you sure you want to close the editor? Any changes will be lost.',
+    'Upload a file' => 'Upload a file',
 ];
