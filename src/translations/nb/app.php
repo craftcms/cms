e4db1cc7d2d54146260ce7e0b1b07aa000fe8b03
Brad Bell <brad@pixelandtonic.com>
2020-09-22 12:06:43 -0700

    New translations app.php (Norwegian Bokmal)

--- src/translations/nb/app.php
+++ src/translations/nb/app.php
@@ -1718,4 +1718,5 @@ Har du ikke bedt om denne e-posten, vennligst se bort fra den.',
     'How the related {type} should be displayed within element indexes.' => 'How the related {type} should be displayed within element indexes.',
     'Show thumbnails and titles' => 'Show thumbnails and titles',
     'Show thumbnails only' => 'Show thumbnails only',
+    'Are you sure you want to close the editor? Any changes will be lost.' => 'Are you sure you want to close the editor? Any changes will be lost.',
 ];
