0ebc2338fe322be1beb4e298cbc0b531a1b9c396
Brad Bell <brad@pixelandtonic.com>
2020-09-22 12:07:20 -0700

    New translations app.php (Hungarian)

--- src/translations/hu/app.php
+++ src/translations/hu/app.php
@@ -1720,4 +1720,5 @@ Amennyiben nem neked szól ez a levél, egyszerűen csak hagyd figyelmen kívül
     'How the related {type} should be displayed within element indexes.' => 'How the related {type} should be displayed within element indexes.',
     'Show thumbnails and titles' => 'Show thumbnails and titles',
     'Show thumbnails only' => 'Show thumbnails only',
+    'Are you sure you want to close the editor? Any changes will be lost.' => 'Are you sure you want to close the editor? Any changes will be lost.',
 ];
