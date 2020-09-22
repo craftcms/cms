90f0f3872b8337a3f94abf01e66fb84568a364c0
Brad Bell <brad@pixelandtonic.com>
2020-09-22 16:15:40 -0700

    New translations app.php (Dutch)

--- src/translations/nl/app.php
+++ src/translations/nl/app.php
@@ -1723,4 +1723,5 @@ Als je deze email niet verwachtte, kun je hem gewoon negeren.',
     'Show thumbnails and titles' => 'Show thumbnails and titles',
     'Show thumbnails only' => 'Show thumbnails only',
     'Are you sure you want to close the editor? Any changes will be lost.' => 'Are you sure you want to close the editor? Any changes will be lost.',
+    'Upload a file' => 'Upload a file',
 ];
