6eee2d636f3917d1d32e45cd2031777cde961dcb
Brad Bell <brad@pixelandtonic.com>
2020-09-22 12:07:17 -0700

    New translations app.php (Italian)

--- src/translations/it/app.php
+++ src/translations/it/app.php
@@ -1718,4 +1718,5 @@ Se non aspettavi questa email, ignorala semplicemente.',
     'How the related {type} should be displayed within element indexes.' => 'How the related {type} should be displayed within element indexes.',
     'Show thumbnails and titles' => 'Show thumbnails and titles',
     'Show thumbnails only' => 'Show thumbnails only',
+    'Are you sure you want to close the editor? Any changes will be lost.' => 'Are you sure you want to close the editor? Any changes will be lost.',
 ];
