26d60df5fbcf93ee9bccdda7e22e461818dca048
Brandon Kelly <brandon@pixelandtonic.com>
2020-10-06 09:14:03 -0700

    "Buy Craft Pro"

--- src/translations/en/app.php
+++ src/translations/en/app.php
@@ -1732,6 +1732,6 @@ If you were not expecting this email, just ignore it.',
     'Time fields are better suited for managing Time-only values.' => 'Time fields are better suited for managing Time-only values.',
     '{edition} edition' => '{edition} edition',
     '(trial)' => '(trial)',
-    'Buy Craft Pro or downgrade' => 'Buy Craft Pro or downgrade',
+    'Buy Craft Pro' => 'Buy Craft Pro',
     'Upgrade to Craft Pro' => 'Upgrade to Craft Pro',
 ];
