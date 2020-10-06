9122571d29591c255ce656f2b7f0557bef6d9b85
Brad Bell <brad@pixelandtonic.com>
2020-10-05 17:13:20 -0700

    New translations app.php (Portuguese)

--- src/translations/pt/app.php
+++ src/translations/pt/app.php
@@ -1730,4 +1730,7 @@ Se você não solicitou este e-mail, simplesmente ignore.',
     'Min Time' => 'Min Time',
     'Max Time' => 'Max Time',
     'Time fields are better suited for managing Time-only values.' => 'Time fields are better suited for managing Time-only values.',
+    '{edition} edition' => '{edition} edition',
+    '(trial)' => '(trial)',
+    'Upgrade' => 'Upgrade',
 ];
