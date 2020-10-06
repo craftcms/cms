4a8e36f874c7840f5a909c0176b8398054716b0f
Brad Bell <brad@pixelandtonic.com>
2020-10-05 17:45:13 -0700

    New translations app.php (Swedish)

--- src/translations/sv/app.php
+++ src/translations/sv/app.php
@@ -1732,5 +1732,6 @@ Om du inte väntade dig att få detta e-postmeddelande kan du ignorera det.',
     'Time fields are better suited for managing Time-only values.' => 'Time fields are better suited for managing Time-only values.',
     '{edition} edition' => '{edition} edition',
     '(trial)' => '(trial)',
-    'Upgrade' => 'Upgrade',
+    'Buy or downgrade' => 'Buy or downgrade',
+    'Upgrade to Pro' => 'Upgrade to Pro',
 ];
