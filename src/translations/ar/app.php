a87e8845df12118836ea8e8c2aa5412befdf4b48
Brad Bell <brad@pixelandtonic.com>
2020-10-05 17:44:47 -0700

    New translations app.php (Arabic)

--- src/translations/ar/app.php
+++ src/translations/ar/app.php
@@ -1732,5 +1732,6 @@ return [
     'Time fields are better suited for managing Time-only values.' => 'Time fields are better suited for managing Time-only values.',
     '{edition} edition' => '{edition} edition',
     '(trial)' => '(trial)',
-    'Upgrade' => 'Upgrade',
+    'Buy or downgrade' => 'Buy or downgrade',
+    'Upgrade to Pro' => 'Upgrade to Pro',
 ];
