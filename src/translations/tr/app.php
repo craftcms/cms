5ecd12c458af64f57ebaf95ed1b3aeeafceb215f
Brad Bell <brad@pixelandtonic.com>
2020-10-06 09:18:47 -0700

    New translations app.php (Turkish)

--- src/translations/tr/app.php
+++ src/translations/tr/app.php
@@ -1728,6 +1728,6 @@ Bu epostayı beklemiyorduysanız, görmezden gelin.',
     'Time fields are better suited for managing Time-only values.' => 'Time fields are better suited for managing Time-only values.',
     '{edition} edition' => '{edition} edition',
     '(trial)' => '(trial)',
-    'Buy Craft Pro or downgrade' => 'Buy Craft Pro or downgrade',
+    'Buy Craft Pro' => 'Buy Craft Pro',
     'Upgrade to Craft Pro' => 'Upgrade to Craft Pro',
 ];
