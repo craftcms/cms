fed9e5cfb2211151a5ee6944b5d21b2d014a9dc9
Brad Bell <brad@pixelandtonic.com>
2020-10-05 17:45:23 -0700

    New translations app.php (Dutch)

--- src/translations/nl/app.php
+++ src/translations/nl/app.php
@@ -1732,5 +1732,6 @@ Als je deze email niet verwachtte, kun je hem gewoon negeren.',
     'Time fields are better suited for managing Time-only values.' => 'Time fields are better suited for managing Time-only values.',
     '{edition} edition' => '{edition} edition',
     '(trial)' => '(trial)',
-    'Upgrade' => 'Upgrade',
+    'Buy or downgrade' => 'Buy or downgrade',
+    'Upgrade to Pro' => 'Upgrade to Pro',
 ];
