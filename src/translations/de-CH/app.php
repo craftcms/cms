97afbc500b94c6e4305a5a1f1133d8f8fc780f65
Brad Bell <brad@pixelandtonic.com>
2020-10-06 09:19:26 -0700

    New translations app.php (German, Switzerland)

--- src/translations/de-CH/app.php
+++ src/translations/de-CH/app.php
@@ -1728,6 +1728,6 @@ Wenn Sie diese E-Mail nicht erwartet haben, ignorieren Sie sie bitte einfach.',
     'Time fields are better suited for managing Time-only values.' => 'Time fields are better suited for managing Time-only values.',
     '{edition} edition' => '{edition} edition',
     '(trial)' => '(trial)',
-    'Buy Craft Pro or downgrade' => 'Buy Craft Pro or downgrade',
+    'Buy Craft Pro' => 'Buy Craft Pro',
     'Upgrade to Craft Pro' => 'Upgrade to Craft Pro',
 ];
