6ec2f5e88be25b326f6ae36301297ca4b71abe01
Brandon Kelly <brandon@pixelandtonic.com>
2020-11-05 08:30:48 -0800

    @since tags

--- src/helpers/Gql.php
+++ src/helpers/Gql.php
@@ -425,6 +425,7 @@ class Gql
      * Shorthand for returning the complexity function for an eager-loaded field.
      *
      * @return callable
+     * @since 3.6.0
      */
     public static function eagerLoadComplexity(): callable
     {
@@ -437,6 +438,7 @@ class Gql
      * Shorthand for returning the complexity function for a field that will add a single query to execution.
      *
      * @return callable
+     * @since 3.6.0
      */
     public static function singleQueryComplexity(): callable
     {
@@ -449,6 +451,7 @@ class Gql
      * Shorthand for returning the complexity function for a field that will generate a single query for every iteration.
      *
      * @return callable
+     * @since 3.6.0
      */
     public static function nPlus1Complexity(): callable
     {
