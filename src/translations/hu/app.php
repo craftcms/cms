224b23a3ad30f38978998f2fdf9d171899178aac
Brad Bell <brad@pixelandtonic.com>
2020-11-17 16:25:09 -0800

    New translations app.php (Hungarian)

--- src/translations/hu/app.php
+++ src/translations/hu/app.php
@@ -1742,4 +1742,5 @@ Amennyiben nem neked szól ez a levél, egyszerűen csak hagyd figyelmen kívül
     'Generate YAML Files' => 'Generate YAML Files',
     'Save the loaded project config data to YAML files in your <code>{folder}</code> folder.' => 'Save the loaded project config data to YAML files in your <code>{folder}</code> folder.',
     'Generate' => 'Generate',
+    'Delete their content' => 'Delete their content',
 ];
