f212f42eaecd6d7da4a4a63ce0f6ed3377efddac
Brad Bell <brad@pixelandtonic.com>
2020-11-19 14:29:10 -0800

    New translations app.php (Slovak)

--- src/translations/sk/app.php
+++ src/translations/sk/app.php
@@ -1745,4 +1745,6 @@ Ak si tento email neočakával, jednoducho ho ignoruj.',
     'Save the loaded project config data to YAML files in your <code>{folder}</code> folder.' => 'Save the loaded project config data to YAML files in your <code>{folder}</code> folder.',
     'Generate' => 'Generate',
     'Delete their content' => 'Delete their content',
+    'Show nested sources',
+    'Hide nested sources',
 ];
