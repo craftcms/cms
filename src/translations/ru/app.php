27d5f19e829fb46b132d92fabbcd6f4ddaace439
Brad Bell <brad@pixelandtonic.com>
2020-11-19 14:28:49 -0800

    New translations app.php (Russian)

--- src/translations/ru/app.php
+++ src/translations/ru/app.php
@@ -1746,4 +1746,6 @@ return [
     'Save the loaded project config data to YAML files in your <code>{folder}</code> folder.' => 'Save the loaded project config data to YAML files in your <code>{folder}</code> folder.',
     'Generate' => 'Generate',
     'Delete their content' => 'Delete their content',
+    'Show nested sources',
+    'Hide nested sources',
 ];
