056aaff578c4375c445b7186cc662243fe74072c
Brad Bell <brad@pixelandtonic.com>
2020-11-19 14:29:06 -0800

    New translations app.php (Turkish)

--- src/translations/tr/app.php
+++ src/translations/tr/app.php
@@ -1741,4 +1741,6 @@ Bu epostayı beklemiyorduysanız, görmezden gelin.',
     'Save the loaded project config data to YAML files in your <code>{folder}</code> folder.' => 'Save the loaded project config data to YAML files in your <code>{folder}</code> folder.',
     'Generate' => 'Generate',
     'Delete their content' => 'Delete their content',
+    'Show nested sources',
+    'Hide nested sources',
 ];
