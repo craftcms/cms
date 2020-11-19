486593743ce8b1990cd88d7cc128e156c9dae834
Brad Bell <brad@pixelandtonic.com>
2020-11-19 14:29:26 -0800

    New translations app.php (Italian)

--- src/translations/it/app.php
+++ src/translations/it/app.php
@@ -1741,4 +1741,6 @@ Se non aspettavi questa email, ignorala semplicemente.',
     'Save the loaded project config data to YAML files in your <code>{folder}</code> folder.' => 'Save the loaded project config data to YAML files in your <code>{folder}</code> folder.',
     'Generate' => 'Generate',
     'Delete their content' => 'Delete their content',
+    'Show nested sources',
+    'Hide nested sources',
 ];
