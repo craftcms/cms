8621343ea96beb22379c347f395bddce8bd9291a
Brandon Kelly <brandon@pixelandtonic.com>
2020-12-01 14:17:40 -0800

    Fixed #7213

--- src/controllers/AssetsController.php
+++ src/controllers/AssetsController.php
@@ -314,7 +314,12 @@ class AssetsController extends Controller
                     throw new BadRequestHttpException('The field provided is not an Assets field');
                 }
 
-                $element = $elementId ? Craft::$app->getElements()->getElementById((int)$elementId) : null;
+                if ($elementId) {
+                    $siteId = $this->request->getBodyParam('siteId') ?: null;
+                    $element = Craft::$app->getElements()->getElementById($elementId, null, $siteId);
+                } else {
+                    $element = null;
+                }
                 $folderId = $field->resolveDynamicPathToFolderId($element);
             }
 
