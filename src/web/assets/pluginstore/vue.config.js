c03d75e2f5a9d18c2346909c0f96c95697cd1706
Benjamin David <ben@dukt.net>
2020-12-05 14:53:02 +0100

    Fixed bug where dev server https couldn’t be disabled

--- src/web/assets/pluginstore/vue.config.js
+++ src/web/assets/pluginstore/vue.config.js
@@ -1,6 +1,18 @@
 var fs = require('fs');
 const ManifestPlugin = require('webpack-manifest-plugin')
 
+let https = null
+
+const httpsKey = process.env.DEV_SERVER_SSL_KEY ? fs.readFileSync(process.env.DEV_SERVER_SSL_KEY) : null
+const httpsCert = process.env.DEV_SERVER_SSL_CERT ? fs.readFileSync(process.env.DEV_SERVER_SSL_CERT) : null
+
+if (httpsKey || httpsCert) {
+    https = {
+        key: httpsKey,
+        cert: httpsCert,
+    }
+}
+
 module.exports = {
     filenameHashing: false,
     publicPath: process.env.NODE_ENV === 'development' ? process.env.DEV_PUBLIC_PATH : '/',
@@ -20,10 +32,7 @@ module.exports = {
     devServer: {
         port: process.env.DEV_SERVER_PORT,
         headers: {"Access-Control-Allow-Origin": "*"},
-        https: {
-            key: process.env.DEV_SERVER_SSL_KEY ? fs.readFileSync(process.env.DEV_SERVER_SSL_KEY) : null,
-            cert: process.env.DEV_SERVER_SSL_CERT ? fs.readFileSync(process.env.DEV_SERVER_SSL_CERT) : null,
-        },
+        https,
 
         // Fix bug caused by webpack-dev-server 3.1.11.
         // https://github.com/vuejs/vue-cli/issues/3173#issuecomment-449573901
