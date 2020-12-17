craftcms/cms

commit 8621343ea96beb22379c347f395bddce8bd9291a

Fixed #7213

src/controllers/AssetsController.php
@@ -314,7 +314,12 @@ class AssetsController extends Controller
                     throw new BadRequestHttpException('The field provided is not an Assets field');
                 }
 
-                $element = $elementId ? Craft::$app->getElements()->getElementById((int)$elementId) : null;
+                if ($elementId) {
+                    $siteId = $this->request->getBodyParam('siteId') ?: null;
+                    $element = Craft::$app->getElements()->getElementById($elementId, null, $siteId);
+                } else {
+                    $element = null;
+                }
                 $folderId = $field->resolveDynamicPathToFolderId($element);
             }
 

src/web/assets/cp/src/js/AssetSelectInput.js
@@ -105,7 +105,8 @@ Craft.AssetSelectInput = Craft.BaseElementSelectInput.extend(
                 dropZone: this.$container,
                 formData: {
                     fieldId: this.settings.fieldId,
-                    elementId: this.settings.sourceElementId
+                    elementId: this.settings.sourceElementId,
+                    siteId: this.settings.criteria.siteId,
                 }
             };
 

commit c1df884318cb4e33d007ebe52a69d8d96024ca70

Fixed uploading assets to new Matrix blocks w/out an ID yet
Fixes #7284

Caused by 8621343ea96beb22379c347f395bddce8bd9291a, due to removal of `(int)` before $elementId. `elementId` was getting passed as the string "null" which would get typecast to 0.

Other actionUpload changes due to that "null" value being what enabled the condition on previous line 297 to pass, even though it wasn’t actually a proper int value.

src/controllers/AssetsController.php
@@ -290,16 +290,16 @@ class AssetsController extends Controller
     public function actionUpload(): Response
     {
         $uploadedFile = UploadedFile::getInstanceByName('assets-upload');
-        $folderId = $this->request->getBodyParam('folderId');
-        $fieldId = $this->request->getBodyParam('fieldId');
-        $elementId = $this->request->getBodyParam('elementId');
 
-        if (empty($folderId) && (empty($fieldId) || empty($elementId))) {
-            throw new BadRequestHttpException('No target destination provided for uploading');
+        if (!$uploadedFile) {
+            throw new BadRequestHttpException('No file was uploaded');
         }
 
-        if ($uploadedFile === null) {
-            throw new BadRequestHttpException('No file was uploaded');
+        $folderId = $this->request->getBodyParam('folderId');
+        $fieldId = $this->request->getBodyParam('fieldId');
+
+        if (!$folderId && !$fieldId) {
+            throw new BadRequestHttpException('No target destination provided for uploading');
         }
 
         try {
@@ -314,7 +314,7 @@ class AssetsController extends Controller
                     throw new BadRequestHttpException('The field provided is not an Assets field');
                 }
 
-                if ($elementId) {
+                if ($elementId = $this->request->getBodyParam('elementId')) {
                     $siteId = $this->request->getBodyParam('siteId') ?: null;
                     $element = Craft::$app->getElements()->getElementById($elementId, null, $siteId);
                 } else {

src/web/assets/cp/src/js/AssetSelectInput.js
@@ -105,10 +105,16 @@ Craft.AssetSelectInput = Craft.BaseElementSelectInput.extend(
                 dropZone: this.$container,
                 formData: {
                     fieldId: this.settings.fieldId,
-                    elementId: this.settings.sourceElementId,
-                    siteId: this.settings.criteria.siteId,
                 }
             };
+
+            if (this.settings.sourceElementId) {
+                options.formData.elementId = this.settings.sourceElementId;
+            }
+
+            if (this.settings.criteria.siteId) {
+                options.formData.siteId = this.settings.criteria.siteId;
+            }
 
             if (this.$addElementBtn) {
                 this.$uploadBtn = $('<button/>', {