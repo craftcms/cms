23a4cc21b81e80ef44e2b9eb4a9ef473ffd31b23
Brad Bell <brad@pixelandtonic.com>
2020-12-23 06:49:00 -0800

    New translations app.php (Japanese)

--- src/translations/ja/app.php
+++ src/translations/ja/app.php
@@ -1575,6 +1575,7 @@ return [
     'XML' => 'XML',
     'Years' => '年',
     'Yesterday' => '昨日',
+    'Yes' => 'Yes',
     'You can only delete groups that have no sites.' => '削除できるのはサイトがないグループのみです。',
     'You cannot access the control panel while the system is offline with that account.' => 'システムがそのアカウントでオフラインの間は、コントロールパネルにアクセスすることはできません。',
     'You cannot access the control panel with that account.' => 'このアカウントではコントロールパネルにアクセスできません。',
