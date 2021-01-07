41b2c62ba50981141f9843a97ac54e66993d92e1
Brandon Kelly <brandon@pixelandtonic.com>
2021-01-07 11:52:48 -0800

    Twig filter tests

--- tests/unit/web/twig/ExtensionTest.php
+++ tests/unit/web/twig/ExtensionTest.php
@@ -11,10 +11,13 @@ use ArrayObject;
 use Codeception\Test\Unit;
 use Craft;
 use craft\elements\User;
+use craft\fields\MissingField;
+use craft\fields\PlainText;
 use craft\test\TestSetup;
 use craft\web\View;
 use crafttests\fixtures\GlobalSetFixture;
 use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
 use UnitTester;
 use yii\base\ErrorException;
@@ -142,6 +145,620 @@ class ExtensionTest extends Unit
     }
 
     /**
+     *
+     */
+    public function testInstanceOfTest()
+    {
+        $this->testRenderResult(
+            'yes',
+            '{{ foo is instance of(class) ? "yes" : "no" }}',
+            [
+                'foo' => new PlainText(),
+                'class' => PlainText::class,
+            ]
+        );
+        $this->testRenderResult(
+            'no',
+            '{{ foo is instance of(class) ? "yes" : "no" }}',
+            [
+                'foo' => new PlainText(),
+                'class' => 'foo\\bar\\Baz',
+            ]
+        );
+    }
+
+    /**
+     *
+     */
+    public function testMissingTest()
+    {
+        $this->testRenderResult(
+            'yes',
+            '{{ foo is missing ? "yes" : "no" }}',
+            [
+                'foo' => new MissingField(),
+            ]
+        );
+        $this->testRenderResult(
+            'no',
+            '{{ foo is missing ? "yes" : "no" }}',
+            [
+                'foo' => new PlainText(),
+            ]
+        );
+    }
+
+    /**
+     *
+     */
+    public function testTranslateFilter()
+    {
+        $this->testRenderResult(
+            'Translated message',
+            '{{ "Source message"|t("site") }}'
+        );
+        $this->testRenderResult(
+            'Translated message with foo',
+            '{{ "Source message with {var}"|t("site", {var: myVar}) }}',
+            [
+                'myVar' => 'foo',
+            ]
+        );
+
+        // 'site' category is optional
+        $this->testRenderResult(
+            'Translated message',
+            '{{ "Source message"|t }}'
+        );
+        $this->testRenderResult(
+            'Translated message with foo',
+            '{{ "Source message with {var}"|t({var: myVar}) }}',
+            [
+                'myVar' => 'foo',
+            ]
+        );
+
+        // |translate should swallow the InvalidConfigException here
+        $this->testRenderResult(
+            'Source message',
+            '{{ "Source message"|t("invalidCategory") }}'
+        );
+
+        $this->expectException(InvalidConfigException::class);
+        Craft::t('invalidCategory', 'Source message');
+        $this->view->renderString('{{ "Source message"|t("invalidCategory") }}');
+    }
+
+    /**
+     *
+     */
+    public function testTruncateFilter()
+    {
+        $this->testRenderResult(
+            '',
+            '{{ ""|truncate(8) }}'
+        );
+        $this->testRenderResult(
+            'Test...',
+            '{{ "Test foo bar"|truncate(8, "...") }}'
+        );
+    }
+
+    /**
+     *
+     */
+    public function testUcfirstFilter()
+    {
+        $this->testRenderResult(
+            'Foo bar',
+            '{{ "foo bar"|ucfirst }}'
+        );
+    }
+
+    /**
+     * @deprecated
+     */
+    public function testUcwordsFilter()
+    {
+        $this->testRenderResult(
+            'Foo Bar',
+            '{{ "foo bar"|ucwords }}'
+        );
+    }
+
+    /**
+     *
+     */
+    public function testLcfirstFilter()
+    {
+        $this->testRenderResult(
+            'foo Bar',
+            '{{ "Foo Bar"|lcfirst }}'
+        );
+    }
+
+    /**
+     *
+     */
+    public function testKebabFilter()
+    {
+        $this->testRenderResult(
+            'foo-bar',
+            '{{ "foo bar"|kebab }}'
+        );
+    }
+
+    /**
+     *
+     */
+    public function testCamelFilter()
+    {
+        $this->testRenderResult(
+            'fooBar',
+            '{{ "foo bar"|camel }}'
+        );
+    }
+
+    /**
+     *
+     */
+    public function testPascalFilter()
+    {
+        $this->testRenderResult(
+            'FooBar',
+            '{{ "foo bar"|pascal }}'
+        );
+    }
+
+    /**
+     *
+     */
+    public function testSnakeFilter()
+    {
+        $this->testRenderResult(
+            'foo_bar',
+            '{{ "foo bar"|snake }}'
+        );
+    }
+
+    /**
+     *
+     */
+    public function testJsonEncodeFilter()
+    {
+        $this->testRenderResult(
+            '{"foo":true}',
+            '{{ myVar|json_encode }}',
+            [
+                'myVar' => ['foo' => true],
+            ]
+        );
+    }
+
+    /**
+     * @throws LoaderError
+     * @throws SyntaxError
+     */
+    public function testWithoutFilter()
+    {
+        $this->testRenderResult(
+            'foo,bar',
+            '{{ ["foo","bar","baz"]|without("baz")|join(",") }}'
+        );
+        $this->testRenderResult(
+            'foo',
+            '{{ ["foo","bar","baz"]|without(["bar","baz"])|join(",") }}'
+        );
+    }
+
+    /**
+     * @throws LoaderError
+     * @throws SyntaxError
+     */
+    public function testWithoutKeyFilter()
+    {
+        $this->testRenderResult(
+            'foo,bar',
+            '{{ {a:"foo",b:"bar",c:"baz"}|withoutKey("c")|join(",") }}'
+        );
+        $this->testRenderResult(
+            'foo',
+            '{{ {a:"foo",b:"bar",c:"baz"}|withoutKey(["b","c"])|join(",") }}'
+        );
+    }
+
+    /**
+     *
+     */
+    public function testAttrFilter()
+    {
+        $this->testRenderResult(
+            '<p class="foo">Hey</p>',
+            '{{ "<p>Hey</p>"|attr({class: "foo"}) }}'
+        );
+
+        // |attr should swallow the InvalidArgumentException here
+        $this->testRenderResult(
+            'Hey',
+            '{{ "Hey"|attr({class: "foo"}) }}'
+        );
+    }
+
+    /**
+     *
+     */
+    public function testParseAttrFilter()
+    {
+        $this->testRenderResult(
+            '{"id":"foo","class":["bar","baz"]}',
+            '{{ \'<p id="foo" class="bar baz">Hello</p>\'|parseAttr|json_encode }}'
+        );
+
+        // |parseAttr should swallow the InvalidArgumentException here
+        $this->testRenderResult(
+            '[]',
+            '{{ "foo"|parseAttr|json_encode }}'
+        );
+    }
+
+    /**
+     *
+     */
+    public function testParseRefsFilter()
+    {
+        $this->testRenderResult(
+            TestSetup::USERNAME,
+            '{{ "{user:1:username}"|parseRefs }}'
+        );
+    }
+
+    /**
+     *
+     */
+    public function testAppendFilter()
+    {
+        $this->testRenderResult(
+            '<p><span>foo</span><span>bar</span></p>',
+            '{{ "<p><span>foo</span></p>"|append("<span>bar</span>") }}'
+        );
+        $this->testRenderResult(
+            '<p><span>foo</span></p>',
+            '{{ "<p><span>bar</span></p>"|append("<span>foo</span>", "replace") }}'
+        );
+    }
+
+    /**
+     *
+     */
+    public function testPrependFilter()
+    {
+        $this->testRenderResult(
+            '<p><span>foo</span><span>bar</span></p>',
+            '{{ "<p><span>bar</span></p>"|prepend("<span>foo</span>") }}'
+        );
+        $this->testRenderResult(
+            '<p><span>foo</span></p>',
+            '{{ "<p><span>bar</span></p>"|prepend("<span>foo</span>", "replace") }}'
+        );
+    }
+
+    /**
+     *
+     */
+    public function testPurifyFilter()
+    {
+        $this->testRenderResult(
+            '<p>foo</p>',
+            '{{ \'<p bad-attr="bad-value">foo</p>\'|purify }}'
+        );
+    }
+
+    /**
+     *
+     */
+    public function testPushFilter()
+    {
+        $this->testRenderResult(
+            '["foo","bar","baz"]',
+            '{{ ["foo"]|push("bar", "baz")|json_encode }}'
+        );
+    }
+
+    /**
+     *
+     */
+    public function testUnshiftFilter()
+    {
+        $this->testRenderResult(
+            '["foo","bar","baz"]',
+            '{{ ["baz"]|unshift("foo", "bar")|json_encode }}'
+        );
+    }
+
+    /**
+     *
+     */
+    public function testReplaceFilter()
+    {
+        $this->testRenderResult(
+            'qux quux corge',
+            '{{ "foo bar baz"|replace({foo: "qux", bar: "quux", baz: "corge"}) }}'
+        );
+
+        $this->testRenderResult(
+            'qux',
+            '{{ "foo bar baz"|replace("/f.*z/", "qux") }}'
+        );
+
+        $this->testRenderResult(
+            'foo qux baz',
+            '{{ "foo bar baz"|replace("bar", "qux") }}'
+        );
+    }
+
+    /**
+     *
+     */
+    public function testDateFilter()
+    {
+        // DateInterval
+        $this->testRenderResult(
+            '4 days',
+            '{{ d|date("%d days") }}',
+            [
+                'd' => new \DateInterval('P2Y4DT6H8M')
+            ]
+        );
+
+        $d = new \DateTime('2021-01-20 10:00:00');
+
+        // ICU format
+        $this->testRenderResult(
+            '2021-01-20',
+            '{{ d|date("icu:YYYY-MM-dd") }}',
+            compact('d')
+        );
+
+        // PHP format
+        $this->testRenderResult(
+            '2021-01-20',
+            '{{ d|date("Y-m-d") }}',
+            compact('d')
+        );
+        $this->testRenderResult(
+            '2021-01-20',
+            '{{ d|date("php:Y-m-d") }}',
+            compact('d')
+        );
+    }
+
+    /**
+     *
+     */
+    public function testTimeFilter()
+    {
+        $d = new \DateTime('2021-01-20 10:00:00');
+
+        // ICU format
+        $this->testRenderResult(
+            '10:00:00',
+            '{{ d|time("icu:HH:mm:ss") }}',
+            compact('d')
+        );
+
+        // PHP format
+        $this->testRenderResult(
+            '10:00:00',
+            '{{ d|time("h:i:s") }}',
+            compact('d')
+        );
+        $this->testRenderResult(
+            '10:00:00',
+            '{{ d|time("php:h:i:s") }}',
+            compact('d')
+        );
+    }
+
+    /**
+     *
+     */
+    public function testDatetimeFilter()
+    {
+        $d = new \DateTime('2021-01-20 10:00:00');
+
+        // ICU format
+        $this->testRenderResult(
+            '2021-01-20 10:00:00',
+            '{{ d|datetime("icu:YYYY-MM-dd HH:mm:ss") }}',
+            compact('d')
+        );
+
+        // PHP format
+        $this->testRenderResult(
+            '2021-01-20 10:00:00',
+            '{{ d|datetime("Y-m-d h:i:s") }}',
+            compact('d')
+        );
+        $this->testRenderResult(
+            '2021-01-20 10:00:00',
+            '{{ d|datetime("php:Y-m-d h:i:s") }}',
+            compact('d')
+        );
+    }
+
+    /**
+     *
+     */
+    public function testAtomFilter()
+    {
+        $d = new \DateTime();
+        $this->testRenderResult(
+            $d->format(\DateTime::ATOM),
+            '{{ d|atom }}',
+            compact('d')
+        );
+    }
+
+    /**
+     *
+     */
+    public function testRssFilter()
+    {
+        $d = new \DateTime();
+        $this->testRenderResult(
+            $d->format(\DateTime::RSS),
+            '{{ d|rss }}',
+            compact('d')
+        );
+    }
+
+    /**
+     *
+     */
+    public function testEncencFilter()
+    {
+        $enc = $this->view->renderString('{{ "foo"|encenc }}');
+        self::assertStringStartsWith('base64:', $enc);
+    }
+
+    /**
+     *
+     */
+    public function testFilterFilter()
+    {
+        $this->testRenderResult(
+            'foo bar baz',
+            '{{ ["foo", "", "bar", "", "baz"]|filter|join(" ") }}'
+        );
+
+        $this->testRenderResult(
+            'foo bar',
+            '{{ ["foo", "bar", "baz"]|filter(i => i != "baz")|join(" ") }}'
+        );
+    }
+
+    /**
+     *
+     */
+    public function testGroupFilter()
+    {
+        // deprecated element query
+        $this->testRenderResult(
+            TestSetup::USERNAME,
+            '{{ craft.users().id(1)|group("username")|keys|join(",") }}'
+        );
+
+        $this->testRenderResult(
+            TestSetup::USERNAME,
+            '{{ craft.users().id(1).all()|group("username")|keys|join(",") }}'
+        );
+
+        $this->testRenderResult(
+            TestSetup::USERNAME,
+            '{{ craft.users().id(1).all()|group(u => u.username)|keys|join(",") }}'
+        );
+
+        // invalid value
+        self::expectException(RuntimeError::class);
+        $this->view->renderString('{% do "foo"|group("bar") %}');
+    }
+
+    /**
+     * @throws LoaderError
+     * @throws SyntaxError
+     */
+    public function testIndexOfFilter()
+    {
+        $array = new ArrayObject(['John', 'Smith']);
+
+        $this->testRenderResult(
+            '3',
+            '{{ "Im a string"|indexOf("a") }}'
+        );
+
+        $this->testRenderResult(
+            '1',
+            '{{ [2, 3, 4, 5]|indexOf(3) }}'
+        );
+
+        $this->testRenderResult(
+            '1',
+            '{{ array|indexOf("Smith") }}',
+            compact('array')
+        );
+
+        $this->testRenderResult(
+            '-1',
+            '{{ array|indexOf("Doe") }}',
+            compact('array')
+        );
+    }
+
+    /**
+     *
+     */
+    public function testLiteralFilter()
+    {
+        $this->testRenderResult(
+            '\\*foo\\*',
+            '{{ "*foo*"|literal }}'
+        );
+    }
+
+    /**
+     *
+     */
+    public function testMarkdownFilter()
+    {
+        $this->testRenderResult(
+            "<p><strong>Hello</strong></p>\n",
+            '{{ "**Hello**"|md }}'
+        );
+
+        $this->testRenderResult(
+            "<p><strong>Hello</strong></p>\n",
+            '{{ "**Hello**"|markdown }}'
+        );
+
+        $this->testRenderResult(
+            '<strong>Hello</strong>',
+            '{{ "**Hello**"|md(inlineOnly=true) }}'
+        );
+    }
+
+    /**
+     *
+     */
+    public function testMergeFilter()
+    {
+        $this->testRenderResult(
+            'foo bar baz',
+            '{{ ["foo"]|merge(["bar", "baz"])|join(" ") }}'
+        );
+
+        $this->testRenderResult(
+            '{"f":"foo","b":["baz"]}',
+            '{{ {f: "foo", b: ["bar"]}|merge({b: ["baz"]})|json_encode }}'
+        );
+
+        $this->testRenderResult(
+            '{"f":"foo","b":["bar","baz"]}',
+            '{{ {f: "foo", b: ["bar"]}|merge({b: ["baz"]}, recursive=true)|json_encode }}'
+        );
+    }
+
+    /**
+     *
+     */
+    public function testMultisortFilter()
+    {
+        $this->testRenderResult(
+            'bar baz foo',
+            '{{ [{k:"foo"},{k:"bar"},{k:"baz"}]|multisort("k")|column("k")|join(" ") }}'
+        );
+    }
+
+    /**
      * @throws LoaderError
      * @throws SyntaxError
      */
@@ -251,69 +868,12 @@ class ExtensionTest extends Unit
      * @throws LoaderError
      * @throws SyntaxError
      */
-    public function testIndexOfFilter()
-    {
-        $arrayObject = new ArrayObject(['John', 'Smith']);
-
-        $this->testRenderResult(
-            '3',
-            '{{ "Im a string"|indexOf("a") }}'
-        );
-
-        $this->testRenderResult(
-            '1',
-            '{{ [2, 3, 4, 5]|indexOf(3) }}'
-        );
-
-        $this->testRenderResult(
-            '1',
-            '{{ ArrayObject|indexOf("Smith") }}',
-            ['ArrayObject' => $arrayObject]
-        );
-    }
-
-    /**
-     * @throws LoaderError
-     * @throws SyntaxError
-     */
     public function testShuffleFunction()
     {
         // 1 means true (string version of bool)
         $this->testRenderResult(
             '1',
             '{% set a = [0,1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f"] %}{{ a != shuffle(a) or a != shuffle(a) }}'
-        );
-    }
-
-    /**
-     * @throws LoaderError
-     * @throws SyntaxError
-     */
-    public function testWithoutFilter()
-    {
-        $this->testRenderResult(
-            'foo,bar',
-            '{{ ["foo","bar","baz"]|without("baz")|join(",") }}'
-        );
-        $this->testRenderResult(
-            'foo',
-            '{{ ["foo","bar","baz"]|without(["bar","baz"])|join(",") }}'
-        );
-    }
-
-    /**
-     * @throws LoaderError
-     * @throws SyntaxError
-     */
-    public function testWithoutKeyFilter()
-    {
-        $this->testRenderResult(
-            'foo,bar',
-            '{{ {a:"foo",b:"bar",c:"baz"}|withoutKey("c")|join(",") }}'
-        );
-        $this->testRenderResult(
-            'foo',
-            '{{ {a:"foo",b:"bar",c:"baz"}|withoutKey(["b","c"])|join(",") }}'
         );
     }
 
