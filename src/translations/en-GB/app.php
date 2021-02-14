85e7781cee5d0beeed9c243c43622312fef59043
Brad Bell <brad@pixelandtonic.com>
2021-02-14 05:26:04 -0800

    New translations app.php (English, United Kingdom)

--- src/translations/en-GB/app.php
+++ src/translations/en-GB/app.php
@@ -356,6 +356,7 @@ return [
     'Craft CMS is running in Dev Mode.' => 'Craft CMS is running in Dev Mode.',
     'Craft Support' => 'Craft Support',
     'Craft isn’t installed yet.' => 'Craft isn’t installed yet.',
+    'Create a draft' => 'Create a draft',
     'Create a new GraphQL Schema' => 'Create a new GraphQL Schema',
     'Create a new GraphQL token' => 'Create a new GraphQL token',
     'Create a new asset volume' => 'Create a new asset volume',
