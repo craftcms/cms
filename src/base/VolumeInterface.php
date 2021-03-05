624cda185c238f86ba38f858414b9b2598717643
andris-sevcenko <andris@pixeland>
2021-03-05 15:17:22 +0200

    @since.

--- src/base/VolumeInterface.php
+++ src/base/VolumeInterface.php
@@ -171,6 +171,7 @@ interface VolumeInterface extends SavableComponentInterface
      *
      * @param string $path The folder path to check
      * @return bool
+     * @since 3.6.9
      */
     public function directoryExists(string $path): bool;
 
@@ -190,6 +191,7 @@ interface VolumeInterface extends SavableComponentInterface
      * @param string $path The path of the directory, relative to the source’s root
      * @throws VolumeObjectExistsException if a directory with such name already exists
      * @throws VolumeException if something else goes wrong
+     * @since 3.6.0
      */
     public function createDirectory(string $path);
 
@@ -207,6 +209,7 @@ interface VolumeInterface extends SavableComponentInterface
      *
      * @param string $path The path of the directory, relative to the source’s root
      * @throws VolumeException if something goes wrong
+     * @since 3.6.0
      */
     public function deleteDirectory(string $path);
 
@@ -230,6 +233,7 @@ interface VolumeInterface extends SavableComponentInterface
      * @throws VolumeObjectNotFoundException if a directory with such name already exists
      * @throws VolumeObjectExistsException if a directory with such name already exists
      * @throws VolumeException if something else goes wrong
+     * @since 3.6.0
      */
     public function renameDirectory(string $path, string $newName);
 }
