31065d8910c708bbf7b25f3e78f3c494a7e5e624
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2021-03-16 09:12:23 +0000

    Add root webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+/*jshint esversion: 6 */
+/* globals __dirname, module, require, process */
+'use strict';
+const webpack = require('webpack');
+const path = require('path');
+const fs = require('fs');
+
+// Constants
+const ASSETS_PATH = path.join(__dirname, 'src/web/assets');
+
+// Setup configs
+let configs = [];
+
+// Import asset configs
+let assetWebpackConfigs = [];
+let assetWebPackConfigFiles = fs.readdirSync(ASSETS_PATH).filter(f => {
+    let dirPath = path.join(ASSETS_PATH, f);
+    let filePath = path.join(dirPath, 'webpack.config.js');
+    return fs.statSync(dirPath).isDirectory() && fs.existsSync(filePath);
+}).map(p => path.join(ASSETS_PATH, p, 'webpack.config'));
+
+let loop = 1;
+assetWebPackConfigFiles.forEach(asset => {
+    let assetConfig = require(asset);
+    configs.push(assetConfig);
+});
+
+module.exports = configs;
