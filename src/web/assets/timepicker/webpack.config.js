ef71ae3d3f5d19f6941de3c9db5c70744599934a
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2021-03-16 09:48:54 +0000

    Update timepicker asset

---
+++ src/web/assets/timepicker/webpack.config.js
@@ -0,0 +1,22 @@
+/* jshint esversion: 6 */
+/* globals module, require, __dirname */
+const CraftWebpackConfig = require('../../../../CraftWebpackConfig');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const NODE_MODULES = __dirname + '/../../../../node_modules/';
+
+module.exports = new CraftWebpackConfig({
+    config: {
+        entry: {'entry': './entry.js'},
+        plugins: [
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        context: NODE_MODULES + 'timepicker',
+                        from: 'jquery.timepicker.min.js',
+                        to: 'jquery.timepicker.js'
+                    }
+                ]
+            })
+        ]
+    }
+});
