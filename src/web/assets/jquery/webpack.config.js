b5d91dffbb521b174abc6ad1630f8a051f89cc4a
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2021-03-16 09:25:32 +0000

    Added jquery asset

---
+++ src/web/assets/jquery/webpack.config.js
@@ -0,0 +1,20 @@
+/* jshint esversion: 6 */
+/* globals module, require, __dirname */
+const CraftWebpackConfig = require('../../../../CraftWebpackConfig');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const NODE_MODULES = __dirname + '/../../../../node_modules/';
+
+module.exports = new CraftWebpackConfig({
+    config: {
+        entry: {'entry': './entry.js'},
+        plugins: [
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: './jquery.*',
+                    }
+                ]
+            }),
+        ]
+    }
+});
