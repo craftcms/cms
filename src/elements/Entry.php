1346562afba8ecbaf6874e231d6c85be129ba984
brandonkelly <brandon@pixelandtonic.com>
2021-04-14 06:44:07 -0700

    Show all author statuses on entry indexes

--- src/elements/Entry.php
+++ src/elements/Entry.php
@@ -637,7 +637,7 @@ class Entry extends Element
     {
         switch ($attribute) {
             case 'author':
-                $elementQuery->andWith('author');
+                $elementQuery->andWith(['author', ['status' => null]]);
                 break;
             case 'revisionNotes':
                 $elementQuery->andWith('currentRevision');
