d64c1c4e0963a2c0c1b51837a5b1f69f42a1366e
andris-sevcenko <andris@pixelandtonic.com>
2021-04-16 13:31:57 +0300

    Lol.

--- src/controllers/AuthenticationController.php
+++ src/controllers/AuthenticationController.php
@@ -9,42 +9,8 @@ namespace craft\controllers;
 
 use Craft;
 use craft\authentication\base\Step;
-use craft\base\Element;
-use craft\elements\Asset;
-use craft\elements\Entry;
-use craft\elements\User;
-use craft\errors\UploadFailedException;
-use craft\errors\UserLockedException;
-use craft\events\DefineUserContentSummaryEvent;
-use craft\events\InvalidUserTokenEvent;
-use craft\events\LoginFailureEvent;
-use craft\events\RegisterUserActionsEvent;
-use craft\events\UserEvent;
-use craft\helpers\ArrayHelper;
-use craft\helpers\Assets;
-use craft\helpers\FileHelper;
-use craft\helpers\Html;
-use craft\helpers\Image;
-use craft\helpers\Json;
-use craft\helpers\UrlHelper;
-use craft\helpers\User as UserHelper;
-use craft\i18n\Locale;
-use craft\models\UserGroup;
-use craft\services\Users;
-use craft\web\assets\edituser\EditUserAsset;
 use craft\web\Controller;
-use craft\web\Request;
-use craft\web\ServiceUnavailableHttpException;
-use craft\web\UploadedFile;
-use craft\web\View;
-use DateTime;
-use yii\base\InvalidArgumentException;
-use yii\web\BadRequestHttpException;
-use yii\web\ForbiddenHttpException;
-use yii\web\HttpException;
-use yii\web\NotFoundHttpException;
 use yii\web\Response;
-use yii\web\ServerErrorHttpException;
 
 /** @noinspection ClassOverridesFieldOfSuperClassInspection */
 
