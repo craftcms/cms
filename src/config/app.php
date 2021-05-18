c3ec3c8698076afc6c710835d61a350c3ba76d2d
brandonkelly <brandon@pixelandtonic.com>
2021-05-18 17:05:48 -0500

    Bump version to 3.7.0-alpha

--- src/config/app.php
+++ src/config/app.php
@@ -3,7 +3,7 @@
 return [
     'id' => 'CraftCMS',
     'name' => 'Craft CMS',
-    'version' => '3.6.15',
+    'version' => '3.7.0-alpha',
     'schemaVersion' => '3.7.3',
     'minVersionRequired' => '2.6.2788',
     'basePath' => dirname(__DIR__), // Defines the @app alias
