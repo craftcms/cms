46ee0dc4cdd970ee46b00cd13cbbb16fe4597aca
andris-sevcenko <andris@pixelandtonic.com>
2021-06-18 13:59:33 +0300

    Improve volume order sorting.

--- src/helpers/Assets.php
+++ src/helpers/Assets.php
@@ -275,14 +275,7 @@ class Assets
      */
     public static function sortFolderTree(array &$tree)
     {
-        $sort = [];
-
-        foreach ($tree as $topFolder) {
-            $volume = $topFolder->getVolume();
-            $sort[] = $volume->sortOrder;
-        }
-
-        array_multisort($sort, $tree);
+        ArrayHelper::multisort($tree, function ($folder) { return $folder->getVolume()->sortOrder;});
     }
 
     /**
