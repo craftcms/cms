a2ca931c44b4e9e80d023ae696e1708674c98de8
Benjamin David <ben@dukt.net>
2021-07-07 19:02:38 +0200

    Stop using tailwindcss JIT

--- src/web/assets/tailwindcss/postcss.config.js
+++ src/web/assets/tailwindcss/postcss.config.js
@@ -1,11 +1,10 @@
 /* jshint esversion: 6 */
 /* globals module, require, __dirname */
 const path = require('path');
-const tailwindcssJit = require('@tailwindcss/jit');
 
 module.exports = {
     plugins: [
-        tailwindcssJit(path.resolve(__dirname, 'tailwind.config.js')),
+        require('tailwindcss')(path.resolve(__dirname, 'tailwind.config.js')),
         require('autoprefixer')
     ]
 }
