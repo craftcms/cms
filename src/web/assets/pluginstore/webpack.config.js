52c0afd521f287e3e6cda14efc71bb4c276323fd
Benjamin David <ben@dukt.net>
2021-07-09 21:39:55 +0200

    Fixed an issue where the Plugin Store’s postcss.config.js was not being called

--- src/web/assets/pluginstore/webpack.config.js
+++ src/web/assets/pluginstore/webpack.config.js
@@ -1,10 +1,13 @@
 /* jshint esversion: 6 */
 /* globals module, require */
+
+const path = require('path')
 const CraftWebpackConfig = require('../../../../CraftWebpackConfig');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = new CraftWebpackConfig({
     type: 'vue',
+    postCssConfig: path.resolve(__dirname, 'postcss.config.js'),
     config: {
         entry: { app: './main.js'},
         output: {
