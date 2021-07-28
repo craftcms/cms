e26f4aad82713dc9a16be55071fa4905aa9e6b7e
Brad Bell <brad@pixelandtonic.com>
2021-07-27 17:06:51 -0700

    Manually included any classes in compiled_templates for for tests and phpstan

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -16,4 +16,11 @@ define('CRAFT_VENDOR_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor');
 
 $devMode = true;
 
+$compiledTemplates = CRAFT_STORAGE_PATH.DIRECTORY_SEPARATOR.'runtime'.DIRECTORY_SEPARATOR.'compiled_classes';
+foreach (new DirectoryIterator($compiledTemplates) as $file) {
+    if (!$file->isDot() && $file->getExtension() === 'php') {
+        include $compiledTemplates.DIRECTORY_SEPARATOR.$file;
+    }
+}
+
 TestSetup::configureCraft();
