b47ffea9b039c2d9103f12d8ab80bbba5edd2b5e
brandonkelly <brandon@pixelandtonic.com>
2021-07-30 17:06:06 -0700

    Probably fixed tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -16,10 +16,12 @@ define('CRAFT_VENDOR_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor');
 
 $devMode = true;
 
-$compiledTemplates = CRAFT_STORAGE_PATH.DIRECTORY_SEPARATOR.'runtime'.DIRECTORY_SEPARATOR.'compiled_classes';
-foreach (new DirectoryIterator($compiledTemplates) as $file) {
-    if (!$file->isDot() && $file->getExtension() === 'php') {
-        include $compiledTemplates.DIRECTORY_SEPARATOR.$file;
+$compiledTemplates = CRAFT_STORAGE_PATH . DIRECTORY_SEPARATOR . 'runtime' . DIRECTORY_SEPARATOR . 'compiled_classes';
+if (is_dir($compiledTemplates)) {
+    foreach (new DirectoryIterator($compiledTemplates) as $file) {
+        if (!$file->isDot() && $file->getExtension() === 'php') {
+            include $compiledTemplates . DIRECTORY_SEPARATOR . $file;
+        }
     }
 }
 
