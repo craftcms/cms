craftcms/cms

commit d990cd2107fa8f263cfde5928a9ae340b0ea0bd1

“Show Time Zone” setting for Date fields

src/fields/Date.php
@@ -17,10 +17,12 @@ use craft\elements\db\ElementQueryInterface;
 use craft\gql\types\DateTime as DateTimeType;
 use craft\helpers\DateTimeHelper;
 use craft\helpers\Db;
+use craft\helpers\ElementHelper;
 use craft\helpers\Html;
 use craft\i18n\Locale;
 use craft\validators\DateTimeValidator;
 use DateTime;
+use DateTimeZone;
 use yii\db\Schema;
 
 /**
@@ -56,6 +58,13 @@ class Date extends Field implements PreviewableFieldInterface, SortableFieldInte
      * @var bool Whether a timepicker should be shown as part of the input
      */
     public $showTime = false;
+
+    /**
+     * @var bool Whether the selected time zone should be stored with the field data. Otherwise the system
+     * time zone will always be used.
+     * @since 3.7.0
+     */
+    public $showTimeZone = false;
 
     /**
      * @var DateTime|null The minimum allowed date
@@ -132,6 +141,10 @@ class Date extends Field implements PreviewableFieldInterface, SortableFieldInte
         if (!$this->showDate && !$this->showTime) {
             $this->showDate = true;
         }
+
+        if (!$this->showTime) {
+            $this->showTimeZone = false;
+        }
     }
 
     /**
@@ -160,8 +173,15 @@ class Date extends Field implements PreviewableFieldInterface, SortableFieldInte
     /**
      * @inheritdoc
      */
-    public function getContentColumnType(): string
+    public function getContentColumnType()
     {
+        if ($this->showTimeZone) {
+            return [
+                'date' => Schema::TYPE_DATETIME,
+                'tz' => Schema::TYPE_STRING,
+            ];
+        }
+
         return Schema::TYPE_DATETIME;
     }
 
@@ -214,6 +234,7 @@ class Date extends Field implements PreviewableFieldInterface, SortableFieldInte
      */
     protected function inputHtml($value, ElementInterface $element = null): string
     {
+        /* @var DateTime|null $value */
         $id = Html::id($this->handle);
         $variables = [
             'id' => $id,
@@ -224,20 +245,30 @@ class Date extends Field implements PreviewableFieldInterface, SortableFieldInte
         ];
 
         $input = '';
+        $wrap = $this->showTime && ($this->showDate || $this->showTimeZone);
+        $view = Craft::$app->getView();
 
-        if ($this->showDate && $this->showTime) {
+        if ($wrap) {
             $input .= '<div class="datetimewrapper">';
         }
 
         if ($this->showDate) {
-            $input .= Craft::$app->getView()->renderTemplate('_includes/forms/date', $variables);
+            $input .= $view->renderTemplate('_includes/forms/date', $variables);
         }
 
         if ($this->showTime) {
-            $input .= ' ' . Craft::$app->getView()->renderTemplate('_includes/forms/time', $variables);
+            $input .= ' ' . $view->renderTemplate('_includes/forms/time', $variables);
         }
 
-        if ($this->showDate && $this->showTime) {
+        if ($this->showTimeZone) {
+            $input .= ' ' . $view->renderTemplate('_includes/forms/timeZone', [
+                    'instructionsId' => "$id-instructions",
+                    'name' => "$this->handle[timezone]",
+                    'value' => $value ? $value->getTimezone()->getName() : Craft::$app->getTimeZone(),
+                ]);
+        }
+
+        if ($wrap) {
             $input .= '</div>';
         }
 
@@ -291,11 +322,51 @@ class Date extends Field implements PreviewableFieldInterface, SortableFieldInte
      */
     public function normalizeValue($value, ElementInterface $element = null)
     {
-        if ($value && ($date = DateTimeHelper::toDateTime($value)) !== false) {
-            return $date;
+        if ($value instanceof DateTime) {
+            return $value;
         }
 
-        return null;
+        // Is this coming from the DB?
+        if (is_array($value) && array_key_exists('tz', $value)) {
+            $timeZone = $value['tz'];
+            $value = $value['date'];
+        }
+
+        if (!$value || (is_array($value) && empty($value['date']))) {
+            return null;
+        }
+
+        $date = DateTimeHelper::toDateTime($value);
+
+        if ($date === false) {
+            return null;
+        }
+
+        if ($this->showTimeZone && (isset($timeZone) || (is_array($value) && isset($value['timezone'])))) {
+            $date->setTimezone(new DateTimeZone($timeZone ?? $value['timezone']));
+        }
+
+        return $date;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function serializeValue($value, ElementInterface $element = null)
+    {
+        if (!$value) {
+            return null;
+        }
+
+        /* @var DateTime $value */
+        if (!$this->showTimeZone) {
+            return Db::prepareDateForDb($value);
+        }
+
+        return [
+            'date' => Db::prepareDateForDb($value),
+            'tz' => $value->getTimezone()->getName(),
+        ];
     }
 
     /**

src/templates/_components/fieldtypes/Date/settings.html
@@ -26,6 +26,19 @@
     value: field.minuteIncrement ? field.minuteIncrement : 30,
 }) }}
 
+{{ forms.lightswitchField({
+    fieldClass: [
+        "#{targetPrefix}showTime",
+        "#{targetPrefix}showBoth",
+        value == 'showDate' ? 'hidden',
+    ]|filter,
+    label: 'Show Time Zone'|t('app'),
+    instructions: 'Whether authors should be able to choose which time zone the time is in.'|t('app'),
+    id: 'show-time-zone',
+    name: 'showTimeZone',
+    on: field.showTimeZone,
+}) }}
+
 {{ forms.dateField({
     fieldClass: [
         "#{targetPrefix}showDate",

src/templates/_includes/forms/time.html
@@ -1,6 +1,7 @@
 {% set id = (id ?? "time#{random()}") ~ '-time' -%}
 {% set name = name ?? null -%}
 {% set value = value ?? null -%}
+{% set outputTzParam = outputTzParam ?? true %}
 
 {% do view.registerAssetBundle('craft\\web\\assets\\timepicker\\TimepickerAsset') -%}
 
@@ -21,7 +22,7 @@
         value: (value ? value|time('short') : '')
     } -%}
     <div data-icon="time"></div>
-    {% if name -%}
+    {% if name and outputTzParam -%}
         {{ hiddenInput("#{name}[timezone]", craft.app.getTimeZone()) }}
     {%- endif -%}
 {% endtag %}

src/translations/en/app.php
@@ -1185,6 +1185,7 @@ return [
     'Setup your site' => 'Setup your site',
     'Should new email addresses be verified before getting saved to user accounts? (This also affects new user registration.)' => 'Should new email addresses be verified before getting saved to user accounts? (This also affects new user registration.)',
     'Should users who register their own accounts be suspended by default? This will prevent them from receiving an activation email.' => 'Should users who register their own accounts be suspended by default? This will prevent them from receiving an activation email.',
+    'Show Time Zone' => 'Show Time Zone',
     'Show all changes' => 'Show all changes',
     'Show date and time' => 'Show date and time',
     'Show date' => 'Show date',
@@ -1586,6 +1587,7 @@ return [
     'Where do you want to store temporary asset uploads?' => 'Where do you want to store temporary asset uploads?',
     'Where do you want to store user photos? Note that the subfolder path can contain variables like <code>{username}</code>.' => 'Where do you want to store user photos? Note that the subfolder path can contain variables like <code>{username}</code>.',
     'Where selected assets should be stored.' => 'Where selected assets should be stored.',
+    'Whether authors should be able to choose which time zone the time is in.' => 'Whether authors should be able to choose which time zone the time is in.',
     'Whether authors should be able to upload files directly to the field, rather than requiring them to select/upload assets via the selection modal.' => 'Whether authors should be able to upload files directly to the field, rather than requiring them to select/upload assets via the selection modal.',
     'Whether preview frames should be automatically refreshed when content changes.' => 'Whether preview frames should be automatically refreshed when content changes.',
     'Whether remotely-stored images should be downloaded and stored locally, to speed up transform generation.' => 'Whether remotely-stored images should be downloaded and stored locally, to speed up transform generation.',

src/web/assets/cp/dist/css/craft.css
@@ -2358,9 +2358,9 @@ body.rtl .passwordwrapper .password-toggle { padding-left: 7px; }
 
 .datetimewrapper { display: -webkit-flex; display: flex; -webkit-flex-direction: row; flex-direction: row; -webkit-align-items: center; align-items: center; }
 
-body.ltr .datetimewrapper > .datewrapper + .timewrapper { margin-left: 5px; }
+body.ltr .datetimewrapper > .datewrapper + .timewrapper, body.ltr .datetimewrapper > .timewrapper + .timezone { margin-left: 5px; }
 
-body.rtl .datetimewrapper > .datewrapper + .timewrapper { margin-right: 5px; }
+body.rtl .datetimewrapper > .datewrapper + .timewrapper, body.rtl .datetimewrapper > .timewrapper + .timezone { margin-right: 5px; }
 
 .clear-btn { display: -webkit-flex; display: flex; -webkit-justify-content: center; justify-content: center; -webkit-align-items: center; align-items: center; width: 24px; cursor: pointer; color: rgba(123, 135, 147, 0.5); border: none; padding: 0; background: transparent; }
 
@@ -2498,7 +2498,7 @@ body .selectize-dropdown .optgroup-header { margin: 14px 0 3px; font-size: 10px;
 body .selectize-dropdown .active { color: #3f4d5a; background-color: #f3f7fc; }
 
 /* time zones */
-.timezone { max-width: 25em; }
+.timezone { height: 34px; max-width: 25em; }
 
 /* datepicker */
 .ui-datepicker { position: fixed; top: -300px; -padding: 10px; width: 210px; height: 242px; z-index: 101 !important; }

src/web/assets/cp/src/css/_main.scss
@@ -4703,7 +4703,8 @@ fieldset {
   flex-direction: row;
   align-items: center;
 
-  & > .datewrapper + .timewrapper {
+  & > .datewrapper + .timewrapper,
+  & > .timewrapper + .timezone {
     @include margin-left(5px);
   }
 }
@@ -4944,6 +4945,7 @@ body .selectize-dropdown .active {
 
 /* time zones */
 .timezone {
+  height: 34px;
   max-width: 25em;
 }
 

commit 43f2b9ab0782827ffdc40bd839fee1b43fca7d4e

Fixed #9708
Caused by d990cd2107fa8f263cfde5928a9ae340b0ea0bd1

src/fields/Date.php
@@ -334,7 +334,7 @@ class Date extends Field implements PreviewableFieldInterface, SortableFieldInte
 
         if (
             !$value ||
-            (is_array($value) && empty($value['date']) && empty($value['time']))
+            (is_array($value) && empty($value['date']) && empty($value['time']) && empty($value['datetime']))
         ) {
             return null;
         }

tests/unit/helpers/DateTimeHelperTest.php
@@ -521,6 +521,12 @@ class DateTimeHelperTest extends Unit
                 },
                 '2021',
             ],
+            'datetime-with-timezone' => [
+                function() {
+                    return new DateTime('2021-09-01T12:00', new DateTimeZone('Europe/Berlin'));
+                },
+                ['datetime' => '2021-09-01T12:00', 'timezone' => 'Europe/Berlin'],
+            ],
         ];
     }
 