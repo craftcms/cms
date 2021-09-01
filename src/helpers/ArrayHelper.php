bea6fbb4c3018a36374bc106948c7d3e6e99569a
brandonkelly <brandon@pixelandtonic.com>
2021-09-01 11:29:51 -0700

    Allow pasing UserGroup[] into EntryQuery::authorGroup()

--- src/helpers/ArrayHelper.php
+++ src/helpers/ArrayHelper.php
@@ -8,7 +8,12 @@
 namespace craft\helpers;
 
 use Craft;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Contracts\Support\Jsonable;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Enumerable;
+use JsonSerializable;
+use Traversable;
 
 /**
  * Class ArrayHelper
