8ddf99fed32b062f5b496965905b20419d3ee93d
Benjamin David <ben@dukt.net>
2021-09-30 17:03:10 +0200

    Clean up unused code

--- src/web/assets/pluginstore/tailwind.config.js
+++ src/web/assets/pluginstore/tailwind.config.js
@@ -1,18 +1,13 @@
 const colors = require('../../../../packages/craftui-tailwind/colors/colors')
 const semanticColors = require('./semanticColors')(colors)
-// const tailwindConf = require('../../../../packages/craftui-tailwind/config')
 const path = require('path')
 
 module.exports = {
     prefix: 'tw-',
-    // ...tailwindConf,
     purge: [
         path.resolve(__dirname, './src/**/*.{js,jsx,ts,tsx,vue}'),
-        // '../../../templates/**/*.{html,twig}',
     ],
     plugins: [
-        // ...tailwindConf.plugins,
-
         require('../../../../packages/craftui-tailwind')({
             semanticColors,
             darkModeSupport: false,
--- src/web/assets/tailwindcss/tailwind.config.js
+++ src/web/assets/tailwindcss/tailwind.config.js
@@ -1,21 +1,14 @@
 const path = require('path')
-// const tailwindConf = require('../../../../packages/craftui-tailwind/config')
 const colors = require('../../../../packages/craftui-tailwind/colors/colors')
 const semanticColors = require('../pluginstore/semanticColors')(colors)
 
 module.exports = {
     prefix: 'tw-',
-    corePlugins: {
-        preflight: true,
-    },
-    // ...tailwindConf,
     purge: [
         path.resolve(__dirname, '../../../templates/**/*.{html,twig}'),
         path.resolve(__dirname, '../pluginstore/src/**/*.{vue,js}'),
     ],
     plugins: [
-        // ...tailwindConf.plugins,
-
         require('../../../../packages/craftui-tailwind')({
             semanticColors,
             darkModeSupport: false,
