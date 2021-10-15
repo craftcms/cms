0c0c4bea8502fcff3e5c44e8d99ee2e5b7c17108
brandonkelly <brandon@pixelandtonic.com>
2021-10-15 13:00:05 -0700

    Deprecated properties last

--- src/elements/db/MatrixBlockQuery.php
+++ src/elements/db/MatrixBlockQuery.php
@@ -108,11 +108,11 @@ class MatrixBlockQuery extends ElementQuery
     public function __set($name, $value)
     {
         switch ($name) {
-            case 'ownerSite':
-                Craft::$app->getDeprecator()->log('MatrixBlockQuery::ownerSite()', 'The `ownerSite` Matrix block query param has been deprecated. Use `site` or `siteId` instead.');
-                break;
             case 'type':
                 $this->type($value);
+                break;
+            case 'ownerSite':
+                Craft::$app->getDeprecator()->log('MatrixBlockQuery::ownerSite()', 'The `ownerSite` Matrix block query param has been deprecated. Use `site` or `siteId` instead.');
                 break;
             case 'ownerLocale':
                 Craft::$app->getDeprecator()->log('MatrixBlockQuery::ownerLocale()', 'The `ownerLocale` Matrix block query param has been deprecated. Use `site` or `siteId` instead.');
