cf216236a62539bd47fe564be3c67b79ae7bf851
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2021-11-09 16:12:19 +0000

    CP webpack config

---
+++ src/web/assets/cp/src/Craft.js
@@ -0,0 +1,69 @@
+/* jshint esversion: 6 */
+// Import CSS
+import '../../cp/src/css/craft.scss';
+
+// Import JS
+// Load Craft first
+import '../../cp/src/js/Craft.js';
+// Then load all base scripts
+import '../../cp/src/js/BaseElementEditor.js';
+import '../../cp/src/js/BaseElementIndex.js';
+import '../../cp/src/js/BaseElementIndexView.js';
+import '../../cp/src/js/BaseElementSelectInput.js';
+import '../../cp/src/js/BaseElementSelectorModal.js';
+import '../../cp/src/js/BaseInputGenerator.js';
+
+// Tabs
+import '../../cp/src/js/Tabs.js';
+
+// Finally load the remaining files
+import '../../cp/src/js/AdminTable.js';
+import '../../cp/src/js/AssetEditor.js';
+import '../../cp/src/js/AssetImageEditor.js';
+import '../../cp/src/js/AssetIndex.js';
+import '../../cp/src/js/AssetSelectInput.js';
+import '../../cp/src/js/AssetSelectorModal.js';
+import '../../cp/src/js/AuthManager.js';
+import '../../cp/src/js/CP.js';
+import '../../cp/src/js/CategoryIndex.js';
+import '../../cp/src/js/CategorySelectInput.js';
+import '../../cp/src/js/Chart.js';
+import '../../cp/src/js/ColorInput.js';
+import '../../cp/src/js/CustomizeSourcesModal.js';
+import '../../cp/src/js/DataTableSorter.js';
+import '../../cp/src/js/DeleteUserModal.js';
+import '../../cp/src/js/DraftEditor.js';
+import '../../cp/src/js/DynamicGenerator.js';
+import '../../cp/src/js/EditableTable.js';
+import '../../cp/src/js/ElementActionTrigger.js';
+import '../../cp/src/js/ElementThumbLoader.js';
+import '../../cp/src/js/ElevatedSessionForm.js';
+import '../../cp/src/js/ElevatedSessionManager.js';
+import '../../cp/src/js/EntryIndex.js';
+import '../../cp/src/js/FieldLayoutDesigner.js';
+import '../../cp/src/js/FieldToggle.js';
+import '../../cp/src/js/Grid.js';
+import '../../cp/src/js/HandleGenerator.js';
+import '../../cp/src/js/ImageUpload.js';
+import '../../cp/src/js/InfoIcon.js';
+import '../../cp/src/js/LightSwitch.js';
+import '../../cp/src/js/Listbox.js';
+import '../../cp/src/js/LivePreview.js';
+import '../../cp/src/js/PasswordInput.js';
+import '../../cp/src/js/Preview.js';
+import '../../cp/src/js/PreviewFileModal.js';
+import '../../cp/src/js/ProgressBar.js';
+import '../../cp/src/js/PromptHandler.js';
+import '../../cp/src/js/Slideout.js';
+import '../../cp/src/js/SlidePicker.js';
+import '../../cp/src/js/SlideRuleInput.js';
+import '../../cp/src/js/SlugGenerator.js';
+import '../../cp/src/js/Structure.js';
+import '../../cp/src/js/StructureDrag.js';
+import '../../cp/src/js/StructureTableSorter.js';
+import '../../cp/src/js/TableElementIndexView.js';
+import '../../cp/src/js/TagSelectInput.js';
+import '../../cp/src/js/ThumbsElementIndexView.js';
+import '../../cp/src/js/UI.js';
+import '../../cp/src/js/Uploader.js';
+import '../../cp/src/js/UriFormatGenerator.js';
