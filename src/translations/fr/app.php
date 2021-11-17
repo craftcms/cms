edcdbe9bf5c9960aab8c8116c407ac44d8248b97
Yohann Bianchi <yohann.b@lahautesociete.com>
2021-11-17 23:34:22 +0100

    Improve the french translation of "Which section do you want to save entries to?"

--- src/translations/fr/app.php
+++ src/translations/fr/app.php
@@ -1640,7 +1640,7 @@ return [
     'Which group should this field be displayed in?' => 'Dans quel groupe ce champ devrait-il être affiché ?',
     'Which group should this site belong to?' => 'À quel groupe ce site devrait-il appartenir ?',
     'Which section do you want to pull recent entries from?' => 'De quelle section voulez-vous récupérer les entrées récentes ?',
-    'Which section do you want to save entries to?' => 'Dans quelle section voulez-vous enregistrer des entrées ?',
+    'Which section do you want to save entries to?' => 'Dans quelle section voulez-vous enregistrer les entrées ?',
     'Which site should {type} be related from?' => 'À quel site {type} devrait être relié(e) ?',
     'Which sites should blocks be saved to?' => 'Sur quels sites les blocs doivent-ils être enregistrés ?',
     'Which source do you want to select {type} from?' => 'À partir de quel emplacement souhaitez-vous sélectionner {type} ?',
