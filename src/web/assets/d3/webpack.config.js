1c1bb555c702f8274a1f2d300dce72fa6c154e6e
Tim Kelty <tim@pixelandtonic.com>
2021-11-18 12:50:24 -0500

    d3 cleanup

--- src/web/assets/d3/webpack.config.js
+++ src/web/assets/d3/webpack.config.js
@@ -3,7 +3,7 @@
 const CraftWebpackConfig = require('@craftcms/webpack/CraftWebpackConfig');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const JSONMinifyPlugin = require('node-json-minify');
-const NODE_MODULES = __dirname + '/../../../../node_modules/';
+const path = require('path');
 
 module.exports = new CraftWebpackConfig({
     type: 'lib',
@@ -13,24 +13,25 @@ module.exports = new CraftWebpackConfig({
             new CopyWebpackPlugin({
                 patterns: [
                     {
-                        context: NODE_MODULES,
-                        from: 'd3/build/d3.min.js',
+                        from: require.resolve('d3/build/d3.min.js'),
                         to: 'd3.js',
                     },
                     {
-                        context: NODE_MODULES,
-                        from: 'd3-format/locale/*.json',
+                        context: path.dirname(require.resolve('d3-format/package.json')),
+                        from: 'locale/*.json',
+                        to: 'd3-format/',
                         transform: function(content) {
                             return JSONMinifyPlugin(content.toString());
                         }
                     },
                     {
-                        context: NODE_MODULES,
-                        from: 'd3-time-format/locale/*.json',
+                        context: path.dirname(require.resolve('d3-time-format/package.json')),
+                        from: 'locale/*.json',
+                        to: 'd3-time-format/',
                         transform: function(content) {
                             return JSONMinifyPlugin(content.toString());
                         }
-                    }
+                    },
                 ],
             }),
         ]
