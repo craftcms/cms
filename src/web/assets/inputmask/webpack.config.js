95aacb2c5b6795e25dad6ba7d8a4078045d9c56d
Tim Kelty <tim@pixelandtonic.com>
2021-11-24 23:39:35 -0500

    CopyPlugin

--- src/web/assets/inputmask/webpack.config.js
+++ src/web/assets/inputmask/webpack.config.js
@@ -1,12 +1,22 @@
 /* jshint esversion: 6 */
 /* globals module, require */
 const {getConfig} = require('@craftcms/webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const pkgDir = require('pkg-dir');
+const path = require('path');
 
 module.exports = getConfig({
     context: __dirname,
     config: {
-        entry: {
-            'jquery.inputmask.bundle': require.resolve('inputmask/dist/jquery.inputmask.bundle.js')
-        },
+        plugins: [
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        context: path.join(pkgDir.sync(require.resolve('inputmask')), 'dist'),
+                        from: './jquery.inputmask.bundle.js*',
+                    }
+                ]
+            }),
+        ]
     }
 });
