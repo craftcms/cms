6aa48e81e52998215dacb9129f034fed43f3d031
Matt Stein <matt@pixelandtonic.com>
2021-12-03 15:48:51 -0800

    Add link.

--- src/elements/db/ElementQuery.php
+++ src/elements/db/ElementQuery.php
@@ -1740,7 +1740,7 @@ class ElementQuery extends Query implements ElementQueryInterface
     }
 
     /**
-     * Clears the cached result.
+     * Clears the [cached result](https://craftcms.com/docs/3.x/element-queries.html#cache).
      *
      * @see getCachedResult()
      * @see setCachedResult()
