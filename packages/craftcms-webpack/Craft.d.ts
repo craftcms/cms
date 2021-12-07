a03d774caa1d10da2bac294fd10bc512b5709b19
andris-sevcenko <andris@pixelandtonic.com>
2021-12-07 11:30:47 -0500

    Migrate TS def changes.

--- packages/craftcms-webpack/Craft.d.ts
+++ packages/craftcms-webpack/Craft.d.ts
@@ -13,13 +13,24 @@ interface ProgressBarInterface {
     showProgressBar(): void
 }
 
+interface ElevatedSessionManagerInterface
+{
+    fetchingTimeout: boolean,
+    requireElevatedSession(cb: () => void)
+}
+
 // Declare existing variables, mock the things we'll use.
 declare var Craft: {
     ProgressBar: ProgressBarInterface,
     t(category: string, message: string, params?: object): string,
     postActionRequest(action: string, data?: object, callback?: (response: object, textStatus: string) => void): void,
     initUiElements($container: JQuery): void,
-    expandPostArray(arr: object): any
+    expandPostArray(arr: object): any,
+    AuthenticationChainHandler: AuthenticationChainHandler,
+    cpLoginChain: string,
+    PasswordInput: any,
+    appendFootHtml: (html?: string) => void
+    elevatedSessionManager: ElevatedSessionManagerInterface
 };
 
 declare var Garnish: any;
