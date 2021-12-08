craftcms/cms

commit f8beee9625809026b71ed247c5abbc286ecd9cac

Fixed #9910

src/services/Elements.php
@@ -1233,6 +1233,8 @@ class Elements extends Component
             static::$duplicatedElementIds[$element->id] = $mainClone->id;
             static::$duplicatedElementSourceIds[$mainClone->id] = $element->id;
 
+            $mainClone->newSiteIds = [];
+
             // Propagate it
             foreach ($supportedSites as $siteInfo) {
                 if ($siteInfo['siteId'] != $mainClone->siteId) {
@@ -1309,6 +1311,10 @@ class Elements extends Component
 
                     if (!$this->_saveElementInternal($siteClone, false, false)) {
                         throw new InvalidElementException($siteClone, "Element {$element->id} could not be duplicated for site {$siteInfo['siteId']}: " . implode(', ', $siteClone->getFirstErrors()));
+                    }
+
+                    if ($siteClone->isNewForSite) {
+                        $mainClone->newSiteIds[] = $siteClone->siteId;
                     }
                 }
             }
@@ -2628,7 +2634,7 @@ class Elements extends Component
                 ]);
             }
 
-            if ($isNewSiteElement = empty($siteSettingsRecord)) {
+            if ($element->isNewForSite = empty($siteSettingsRecord)) {
                 // First time we've saved the element for this site
                 $siteSettingsRecord = new Element_SiteSettingsRecord();
                 $siteSettingsRecord->elementId = $element->id;
@@ -2645,7 +2651,7 @@ class Elements extends Component
             }
 
             // Update our list of dirty attributes
-            if ($trackChanges && !$isNewSiteElement) {
+            if ($trackChanges && !$element->isNewForSite) {
                 ArrayHelper::append($dirtyAttributes, ...array_keys($siteSettingsRecord->getDirtyAttributes([
                     'slug',
                     'uri',

src/base/ElementTrait.php
@@ -177,6 +177,12 @@ trait ElementTrait
     public $newSiteIds = [];
 
     /**
+     * @var bool Whether the element is being saved to the current site for the first time.
+     * @since 3.7.15
+     */
+    public $isNewForSite = false;
+
+    /**
      * @var bool Whether the element is being resaved by a ResaveElement job or a `resave` console command.
      * @since 3.1.22
      */

src/services/Matrix.php
@@ -856,7 +856,7 @@ class Matrix extends Component
                 ];
 
                 if ($target->updatingFromDerivative && $block->getIsDerivative()) {
-                    if ($block->getOwner()->isFieldModified($field->handle)) {
+                    if (!empty($target->newSiteIds) || $source->isFieldModified($field->handle)) {
                         /** @var MatrixBlock $newBlock */
                         $newBlock = $elementsService->updateCanonicalElement($block, $newAttributes);
                         $newBlockId = $newBlock->id;

commit f03bffe2741b470d4265bdf9d9bf89cc218bab7e

Fixed Preparse/Neo issue
Caused by changes in f8beee9625809026b71ed247c5abbc286ecd9cac

src/services/Elements.php
@@ -2542,6 +2542,9 @@ class Elements extends Component
         $oldUpdateSearchIndex = $this->_updateSearchIndex;
         $updateSearchIndex = $this->_updateSearchIndex = $updateSearchIndex ?? $this->_updateSearchIndex ?? true;
 
+        $newSiteIds = $element->newSiteIds;
+        $element->newSiteIds = [];
+
         $transaction = Craft::$app->getDb()->beginTransaction();
 
         try {
@@ -2695,8 +2698,6 @@ class Elements extends Component
 
             // Update the element across the other sites?
             if ($propagate) {
-                $element->newSiteIds = [];
-
                 foreach ($supportedSites as $siteInfo) {
                     // Skip the initial site
                     if ($siteInfo['siteId'] != $element->siteId) {
@@ -2722,6 +2723,7 @@ class Elements extends Component
             throw $e;
         } finally {
             $this->_updateSearchIndex = $oldUpdateSearchIndex;
+            $element->newSiteIds = $newSiteIds;
         }
 
         if (!$element->propagating) {