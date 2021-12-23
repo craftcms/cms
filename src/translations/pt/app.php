54a38e11bff47e4e2d442df3f6524c457aba940f
shinybrad <83238715+shinybrad@users.noreply.github.com>
2021-12-23 14:25:38 -0500

    New translations app.php (Portuguese) [ci skip]

--- src/translations/pt/app.php
+++ src/translations/pt/app.php
@@ -1180,7 +1180,6 @@ return [
     'Saving' => 'A guardar',
     'Scale and crop' => 'Redimensionar e cortar',
     'Scale to fit' => 'Redimensionar para ajustar',
-    'Schema Name' => 'Nome do esquema',
     'Schema saved.' => 'Esquema guardado.',
     'Schemas' => 'Esquemas',
     'Score' => 'Ponto',
