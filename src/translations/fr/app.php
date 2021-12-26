782b1d996c965ba3be78aec0350c8dd1e8d2405b
shinybrad <83238715+shinybrad@users.noreply.github.com>
2021-12-26 11:02:50 -0500

    New translations app.php (French) [ci skip]

--- src/translations/fr/app.php
+++ src/translations/fr/app.php
@@ -1204,7 +1204,6 @@ return [
     'Saving…' => 'Saving…',
     'Scale and crop' => 'Réduire et recadrer',
     'Scale to fit' => 'Réduire pour faire correspondre',
-    'Schema Name' => 'Nom du schéma',
     'Schema saved.' => 'Schéma enregistré.',
     'Schemas' => 'Schémas',
     'Score' => 'Score',
