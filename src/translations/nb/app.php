955c6d3807cdb91b28ff3f552467d752205d7064
shinybrad <83238715+shinybrad@users.noreply.github.com>
2021-12-26 11:02:53 -0500

    New translations app.php (Norwegian Bokmal) [ci skip]

--- src/translations/nb/app.php
+++ src/translations/nb/app.php
@@ -1204,7 +1204,6 @@ return [
     'Saving…' => 'Saving…',
     'Scale and crop' => 'Skaler og beskjær',
     'Scale to fit' => 'Skaler til å passe',
-    'Schema Name' => 'Skjemanavn',
     'Schema saved.' => 'Skjema lagret.',
     'Schemas' => 'Skjemaer',
     'Score' => 'Resultat',
