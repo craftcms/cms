5f3ac9214d4c06c7ee5b9f3551e360ed834d3f06
shinybrad <83238715+shinybrad@users.noreply.github.com>
2022-01-21 03:26:53 -0500

    New translations app.php (Czech) [ci skip]

--- src/translations/cs/app.php
+++ src/translations/cs/app.php
@@ -1009,7 +1009,7 @@ return [
     'One of {name}’s migrations failed.' => 'Jedna z migrací {name} se nezdařila.',
     'One update available!' => 'Jedna aktualizace je k dispozici!',
     'Online' => 'Online',
-    'Only allow {type} to be selected if they match following rules:' => 'Only allow {type} to be selected if they match following rules:',
+    'Only allow {type} to be selected if they match the following rules:' => 'Only allow {type} to be selected if they match the following rules:',
     'Only save blocks to the site they were created in' => 'Uložte bloky pouze na web, na kterém byly vytvořeny',
     'Only save entries to the site they were created in' => 'Uložte záznamy pouze na web, na kterém byly vytvořeny',
     'Only show for users who match the following rules:' => 'Only show for users who match the following rules:',
