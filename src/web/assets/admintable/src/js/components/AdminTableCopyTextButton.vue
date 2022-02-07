3969360b37656b156f0243b3589e060f51f19053
Tim Kelty <tim@pixelandtonic.com>
2022-02-07 16:47:02 -0500

    Add copy text buttons to all handles in admintable

---
+++ src/web/assets/admintable/src/js/components/AdminTableCopyTextButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex">
+    <div class="flex-grow"></div>
+  </div>
+</template>
+
+<script>
+    /* global Craft */
+    /* global $ */
+
+    export default {
+        name: 'AdminTableCopyTextButton',
+
+        props: {
+            value: String,
+        },
+
+        mounted() {
+          $(this.$el).prepend(Craft.ui.createCopyTextBtn({
+            value: this.value,
+            class: 'code small light',
+          }));
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
