0087c3c56a1c9cd9d76525a18507d8e7eb629a20
Lupe <lupe@pixelandtonic.com>
2022-02-18 10:41:22 -0800

    Update tablist to use tag function and set label name at top of template

--- src/controllers/ElementsController.php
+++ src/controllers/ElementsController.php
@@ -1168,6 +1168,7 @@ JS;
             $tabHtml = count($tabs) > 1 ? $view->namespaceInputs(fn() => $view->renderTemplate('_includes/tabs', [
                 'tabs' => $tabs,
                 'selectedTab' => $this->_selectedTab,
+                'tablistLabel' => Craft::t('app', 'Fields'),
             ], View::TEMPLATE_MODE_CP), $namespace) : null;
 
             // Make sure the user is authorized to preview the draft
