2fc81d630b53b956e0369a83682fc7f5b2ac957b
Benjamin David <ben@pixelandtonic.com>
2022-02-22 16:44:45 +0100

    Clean up unused code

--- src/web/assets/pluginstore/src/js/api/developerIndex.js
+++ src/web/assets/pluginstore/src/js/api/developerIndex.js
@@ -1,18 +1,6 @@
-import axios from 'axios';
 import {getElementIndexParams} from '../utils/elementIndex'
 
-let CancelToken = axios.CancelToken
-let cancelTokenSource = CancelToken.source()
-
 export default  {
-  cancelRequests() {
-    // cancel requests
-    cancelTokenSource.cancel()
-
-    // create new cancel token
-    cancelTokenSource = CancelToken.source()
-  },
-
   searchDevelopers({searchQuery, developerIndexParams}) {
     return new Promise((resolve) => {
       const params = getElementIndexParams(developerIndexParams)
