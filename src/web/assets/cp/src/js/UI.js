b901b072397f6255add1d463d7a74ee1874e54ee
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2022-02-25 15:00:20 +0000

    Update warning to be consistent with field macro

--- src/web/assets/cp/src/js/UI.js
+++ src/web/assets/cp/src/js/UI.js
@@ -923,15 +923,19 @@ Craft.ui =
             $('<div class="input"/>').append(input).appendTo($field);
 
             if (config.tip) {
-              const $tip = $('<p class="notice has-icon"/>');
-              $('<span class="icon" aria-hidden="true"/>').appendTo($tip);
-              $('<span class="visually-hidden" aria-hidden="true"/>').text(Craft.t('app', 'Tip') + ' ').appendTo($tip);
-              $('<span/>').text(config.tip).appendTo($tip);
-              $tip.appendTo($field);
+                const $tip = $('<p class="notice has-icon"/>');
+                $('<span class="icon" aria-hidden="true"/>').appendTo($tip);
+                $('<span class="visually-hidden"/>').text(Craft.t('app', 'Tip') + ': ').appendTo($tip);
+                $('<span/>').text(config.tip).appendTo($tip);
+                $tip.appendTo($field);
             }
 
             if (config.warning) {
-                $('<p class="warning"/>').text(config.warning).appendTo($field);
+                const $warning = $('<p class="warning has-icon"/>');
+                $('<span class="icon" aria-hidden="true"/>').appendTo($warning);
+                $('<span class="visually-hidden"/>').text(Craft.t('app', 'Warning') + ': ').appendTo($warning);
+                $('<span/>').text(config.warning).appendTo($warning);
+                $warning.appendTo($field);
             }
 
             if (config.errors) {
