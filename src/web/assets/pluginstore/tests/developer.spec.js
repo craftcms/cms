74450d5e9cab8fa28201093071c7c85ff07b3923
Benjamin David <ben@pixelandtonic.com>
2022-03-01 17:51:35 +0100

    Fixed bug where tests were waiting for responses that didn’t come

--- src/web/assets/pluginstore/tests/developer.spec.js
+++ src/web/assets/pluginstore/tests/developer.spec.js
@@ -3,6 +3,9 @@ const {test, expect} = require('@playwright/test');
 test('Should show developer details', async ({ page, baseURL }) => {
   await page.goto(baseURL + '/plugin-store/developer/70');
 
+  // Wait for category request listing plugins to be done
+  await page.waitForResponse(response => response.url().includes('//api.craftcms.com/v1/plugin-store/plugins'))
+
   // Developer name
   const title = page.locator('.ps-wrapper h1');
   await expect(title).toContainText('Pixel & Tonic');
