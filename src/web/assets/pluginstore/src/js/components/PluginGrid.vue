4e0ffab3c8d9350c778a04e0bf6bbd1aaf4ddd7d
Benjamin David <ben@pixelandtonic.com>
2022-03-07 09:06:11 +0100

    Fixed a bug where plugin cards were not the same height in plugin indexes

--- src/web/assets/pluginstore/src/js/components/PluginGrid.vue
+++ src/web/assets/pluginstore/src/js/components/PluginGrid.vue
@@ -4,10 +4,11 @@
       class="tw-grid-plugins tw-grid tw-grid-cols-1 sm:tw-grid-cols-2 xl:tw-grid-cols-3 2xl:tw-grid-cols-4 tw-gap-x-8"
       v-if="plugins && plugins.length > 0">
       <div
-        class="tw-grid-box"
+        class="tw-grid-box sm:tw-flex"
         v-for="(plugin, key) in computedPlugins"
         :key="key">
         <plugin-card
+          class="sm:tw-flex-1"
           :plugin="plugin"
           :trialMode="trialMode"></plugin-card>
       </div>
