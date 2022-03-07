a26c37cb3838c7cd7a6019190f8ed52237e7b0c6
andris-sevcenko <andris@pixelandtonic.com>
2022-03-07 12:25:40 +0200

    Avoid deprecation errors

--- src/base/Fs.php
+++ src/base/Fs.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 namespace craft\base;
 
 use Craft;
+use craft\helpers\App;
 use craft\validators\HandleValidator;
 
 /**
@@ -41,7 +42,7 @@ abstract class Fs extends SavableComponent implements FsInterface
             return null;
         }
 
-        return rtrim(Craft::parseEnv($this->url), '/') . '/';
+        return rtrim(App::parseEnv($this->url), '/') . '/';
     }
 
     /**
