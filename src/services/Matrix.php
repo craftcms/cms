2ccbe5a875cbcb3767aaead3e293aeb48fd50e1c
brandonkelly <brandon@pixelandtonic.com>
2022-03-28 06:20:32 -0700

    Don't validate field layouts when applying project config changes

--- src/services/Matrix.php
+++ src/services/Matrix.php
@@ -316,7 +316,7 @@ class Matrix extends Component
                     $layout->id = $blockTypeRecord->fieldLayoutId;
                     $layout->type = MatrixBlock::class;
                     $layout->uid = key($data['fieldLayouts']);
-                    $fieldsService->saveLayout($layout);
+                    $fieldsService->saveLayout($layout, false);
                     $blockTypeRecord->fieldLayoutId = $layout->id;
                 } elseif ($blockTypeRecord->fieldLayoutId) {
                     // Delete the field layout
