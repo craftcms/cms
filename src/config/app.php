ac4beea698569f5637c8400322e223864f3f07ca
brandonkelly <brandon@pixelandtonic.com>
2022-04-02 09:53:21 -0700

    Bump the version

--- src/config/app.php
+++ src/config/app.php
@@ -3,7 +3,7 @@
 return [
     'id' => 'CraftCMS',
     'name' => 'Craft CMS',
-    'version' => '3.7.37',
+    'version' => '3.7.38',
     'schemaVersion' => '3.7.33',
     'minVersionRequired' => '2.6.2788',
     'basePath' => dirname(__DIR__), // Defines the @app alias
