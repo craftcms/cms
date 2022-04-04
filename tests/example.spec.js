41bb4a43d39b3262bc43740712b54b86ef32ce91
Guadalupe Camacho <lupe@pixelandtonic.com>
2022-04-04 10:07:45 -0700

    Install playwright and add example test spec

---
+++ tests/example.spec.js
@@ -0,0 +1,7 @@
+const { test, expect } = require('@playwright/test');
+
+test('basic test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await expect(title).toHaveText('Playwright');
+});
