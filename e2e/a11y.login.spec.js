138f009e4af8e69573875ef58312e7342dc46fb0
Guadalupe Camacho <lupe@pixelandtonic.com>
2022-04-05 08:45:55 -0700

    Rewrite login test to use checkA11y insted of getViolations, as WCAG tags were not being honored

--- e2e/a11y.login.spec.js
+++ e2e/a11y.login.spec.js
@@ -1,33 +1,17 @@
 const { test, expect } = require('@playwright/test');
-const { injectAxe, checkA11y, getAxeResults, getViolations } = require('axe-playwright');
-const { createHtmlReport } = require("axe-html-reporter");
+const { injectAxe, checkA11y } = require('axe-playwright');
 const baseUrl = 'https://craft3.nitro/admin';
 
 test('Login accessibility test', async ({ page }) => {
   await page.goto(`${baseUrl}/login`);
   await injectAxe(page);
 
-  const violations = await getViolations(page, null, {
+  await checkA11y(page, null, {
     axeOptions: {
       runOnly: {
         type: 'tag',
-        values: ['wcag2a', 'wcag2aa'],
+        values: ['wcag21a', 'wcag21aa'],
       },
     },
   });
-
-  const axeResults = await getAxeResults(page);
-  createHtmlReport({
-    results: axeResults,
-    options: {
-      outputDir: "report/axe",
-      reportFileName: `axe.html`,
-    },
-  });
-
-  // Trying to filter out Best Practice issues since tag config doesn't seem to be working
-  // const finalViolations = violations.filter(violation => violation.tags.indexOf('best-practice') < 0 );
-  // expect(finalViolations.length).toEqual(0);
-
-  expect(violations.length).toEqual(0);
 });
