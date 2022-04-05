57b0ef237f5006c1eb22b3c0a5f12e4731eccde7
Guadalupe Camacho <lupe@pixelandtonic.com>
2022-04-05 09:19:15 -0700

    Add html reporter back in

--- e2e/a11y.cp.spec.js
+++ e2e/a11y.cp.spec.js
@@ -1,7 +1,18 @@
 const { test, expect } = require('@playwright/test');
-const {injectAxe, checkA11y} = require('axe-playwright');
+const {injectAxe, checkA11y, getAxeResults} = require('axe-playwright');
+const {createHtmlReport} = require('axe-html-reporter');
 
 const baseUrl = 'https://craft3.nitro/admin';
+
+const createReport = (axeResults, pageName) => {
+  createHtmlReport({
+    results: axeResults,
+    options: {
+      outputDir: "report/axe",
+      reportFileName: `a11y-results-${pageName}.html`,
+    },
+  });
+};
 
 test.beforeEach(async ({ page }) => {
   // Runs before each test and signs in each page.
@@ -16,6 +27,9 @@ test('Dashboard', async ({ page }) => {
   await page.goto(`${baseUrl}/dashboard`);
   await expect(page.locator('h1')).toHaveText('Dashboard');
   await injectAxe(page);
+
+  const axeResults = await getAxeResults(page);
+  createReport(axeResults, 'dashboard');
   await checkA11y(page);
 });
 
@@ -25,5 +39,7 @@ test('Entries', async ({ page }) => {
 
   /* Wait for entries table to load */
   await expect(page.locator('.main .centeralign')).toHaveClass(/hidden/);
+  const axeResults = await getAxeResults(page);
+  createReport(axeResults, 'entries');
   await checkA11y(page);
 });
--- e2e/a11y.login.spec.js
+++ e2e/a11y.login.spec.js
@@ -1,10 +1,21 @@
 const { test, expect } = require('@playwright/test');
-const { injectAxe, checkA11y } = require('axe-playwright');
+const { injectAxe, checkA11y, getAxeResults } = require('axe-playwright');
+const { createHtmlReport } = require('axe-html-reporter');
 const baseUrl = 'https://craft3.nitro/admin';
 
 test('Login accessibility test', async ({ page }) => {
   await page.goto(`${baseUrl}/login`);
   await injectAxe(page);
+
+  const axeResults = await getAxeResults(page);
+
+  createHtmlReport({
+    results: axeResults,
+    options: {
+      outputDir: "report/axe",
+      reportFileName: `a11y-results-login.html`,
+    },
+  });
 
   await checkA11y(page);
 });
