12c90e18516311b4c2d8c810c82d9df4de5bd60e
Tim Kelty <tim@pixelandtonic.com>
2022-04-07 09:45:59 -0400

    Add d3-selection

--- src/web/assets/d3/src/index.js
+++ src/web/assets/d3/src/index.js
@@ -1,11 +1,13 @@
 import {d3Format} from "d3-format";
-import {d3TimeFormat} from "d3-format";
+import {d3TimeFormat} from "d3-time-format";
+import {d3Selection} from "d3-selection";
 // import * as d3 from "d3";
 
 const d3 = Object.assign(
     {},
     d3Format,
-    d3TimeFormat
+    d3TimeFormat,
+    d3Selection,
 );
 
 export default d3;
