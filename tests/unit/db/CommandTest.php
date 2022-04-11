906b6831d5407d8b32b9d3e17b6faecd36b21a23
brandonkelly <brandon@pixelandtonic.com>
2022-04-11 12:22:17 -0700

    Remove a couple error prone tests

--- tests/unit/db/CommandTest.php
+++ tests/unit/db/CommandTest.php
@@ -47,35 +47,6 @@ class CommandTest extends Unit
     }
 
     /**
-     * @throws Exception
-     */
-    public function testInsertDateCreated()
-    {
-        $session = $this->ensureSession();
-        self::assertSame($session['dateCreated'], $this->sessionDate->format('Y-m-d H:i:s'));
-        $this->clearSession();
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function testDateUpdatedOnInsertAndUpdate()
-    {
-        $session = $this->ensureSession();
-
-        // Ensure that there is a diff in dates....
-        sleep(1);
-
-        // Save it again without a dateUpdated value. Ensure dateUpdated is now current.
-        $date = new DateTime('now', new DateTimeZone('UTC'));
-        unset($session['dateUpdated']);
-        $session = $this->updateSession($session);
-
-        self::assertSame($date->format('Y-m-d H:i:s'), $session['dateUpdated']);
-        $this->clearSession();
-    }
-
-    /**
      * Ensure a session row exists
      *
      * @return array
