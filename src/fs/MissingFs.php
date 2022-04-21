f55dd4fa8b92732915e2fc7ecb07954f6209822f
andris-sevcenko <andris@pixelandtonic.com>
2022-04-21 10:40:26 +0300

    Remove `strict_types=1`
    Resolve #10956

--- src/fs/MissingFs.php
+++ src/fs/MissingFs.php
@@ -1,7 +1,5 @@
 <?php
-
 /** @noinspection PhpInconsistentReturnPointsInspection */
-declare(strict_types=1);
 /**
  * @link https://craftcms.com/
  * @copyright Copyright (c) Pixel & Tonic, Inc.
--- src/services/Assets.php
+++ src/services/Assets.php
@@ -1,6 +1,4 @@
 <?php
-
-declare(strict_types=1);
 /**
  * @link https://craftcms.com/
  * @copyright Copyright (c) Pixel & Tonic, Inc.
