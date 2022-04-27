e18a16ab56ae69bca29d8a1ab0f5dac1195b5707
Benjamin David <ben@pixelandtonic.com>
2022-04-27 15:42:53 +0200

    Improved styles of the small edition badge

--- src/web/assets/pluginstore/src/js/components/EditionBadge.vue
+++ src/web/assets/pluginstore/src/js/components/EditionBadge.vue
@@ -48,7 +48,7 @@
     }
 
     .edition-badge-name {
-      @apply tw-inline-block tw-uppercase tw-border tw-border-solid tw-border-gray-400 tw-px-2 tw-py-0 tw-text-gray-500 tw-rounded tw-text-xs;
+      @apply tw-inline-block tw-uppercase tw-border tw-border-solid tw-border-gray-400 tw-px-2 tw-py-1 tw-text-gray-500 tw-rounded tw-text-xs;
       letter-spacing: 1.5px;
     }
   }
