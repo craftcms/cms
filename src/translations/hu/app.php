732992e9da20b8430376e64e167c0a4e58b76e56
shinybrad <83238715+shinybrad@users.noreply.github.com>
2022-04-29 06:16:45 -0700

    New translations app.php (Hungarian) [ci skip]

--- src/translations/hu/app.php
+++ src/translations/hu/app.php
@@ -636,7 +636,7 @@ return [
     'Files in this filesystem have public URLs' => 'A fájlrendszerben lévő fájloknak nyilvános URL-címük van',
     'Filesystem saved.' => 'Fájlrendszer elmentve.',
     'Filesystems' => 'Fájlrendszerek',
-    'Filesytem Type' => 'Fájlrendszer típusa',
+    'Filesystem Type' => 'Filesystem Type',
     'Filter results' => 'Eredmények szűrése',
     'Find Text' => 'Szöveg keresése',
     'Find an official Craft Partner' => 'Hivatalos Craft Partner keresése',
