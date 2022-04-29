fcd4c4134c082ea8eb2ae92b2b4631c5f40f5958
shinybrad <83238715+shinybrad@users.noreply.github.com>
2022-04-29 06:38:20 -0700

    New translations app.php (Dutch) [ci skip]

--- src/translations/nl/app.php
+++ src/translations/nl/app.php
@@ -636,7 +636,7 @@ return [
     'Files in this filesystem have public URLs' => 'Bestanden in dit bestandssysteem hebben openbare URL\'s',
     'Filesystem saved.' => 'Bestandssysteem opgeslagen.',
     'Filesystems' => 'Bestandssystemen',
-    'Filesystem Type' => 'Filesystem Type',
+    'Filesystem Type' => 'Type bestandssysteem',
     'Filter results' => 'Resultaten filteren',
     'Find Text' => 'Tekst zoeken',
     'Find an official Craft Partner' => 'Zoek een officiële Craft-partner',
