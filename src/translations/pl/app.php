1dab6c87feec94d0a611fba9e9be5dd04d014102
shinybrad <83238715+shinybrad@users.noreply.github.com>
2022-04-29 06:38:19 -0700

    New translations app.php (Polish) [ci skip]

--- src/translations/pl/app.php
+++ src/translations/pl/app.php
@@ -636,7 +636,7 @@ return [
     'Files in this filesystem have public URLs' => 'Pliki w tym systemie plików mają publiczne adresy URL',
     'Filesystem saved.' => 'System plików został zapisany.',
     'Filesystems' => 'Systemy plików',
-    'Filesystem Type' => 'Filesystem Type',
+    'Filesystem Type' => 'Typ systemu plików',
     'Filter results' => 'Filtruj wyniki',
     'Find Text' => 'Znajdź tekst',
     'Find an official Craft Partner' => 'Znajdź oficjalnego partnera Craft',
