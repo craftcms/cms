6c9e0441f50b9086ad42df1e1ef14dbd4412eff0
shinybrad <83238715+shinybrad@users.noreply.github.com>
2022-05-07 10:48:44 -0700

    New translations app.php (Swedish) [ci skip]

--- src/translations/sv/app.php
+++ src/translations/sv/app.php
@@ -684,7 +684,6 @@ return [
     'GraphQL Schemas' => 'GraphQL-scheman',
     'GraphQL Tokens' => 'GraphQL-tokens',
     'GraphQL queries' => 'GraphQL-förfrågan',
-    'GraphQL' => 'GraphQL',
     'Group Name' => 'Gruppnamn',
     'Group deleted.' => 'Gruppen har raderats.',
     'Group renamed.' => 'Gruppens namn ändrades.',
