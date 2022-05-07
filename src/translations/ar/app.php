5f4a02b559cd37be768f0353d73917fe41133103
shinybrad <83238715+shinybrad@users.noreply.github.com>
2022-05-07 10:49:45 -0700

    New translations app.php (Arabic) [ci skip]

--- src/translations/ar/app.php
+++ src/translations/ar/app.php
@@ -668,7 +668,6 @@ return [
     'GraphQL Schemas' => 'مخططات GraphQL',
     'GraphQL Tokens' => 'رموز GraphQL',
     'GraphQL queries' => 'استفسارات GraphQL',
-    'GraphQL' => 'GraphQL',
     'Group Name' => 'اسم المجموعة',
     'Group deleted.' => 'تم حذف المجموعة.',
     'Group renamed.' => 'تمت إعادة تسمية المجموعة.',
