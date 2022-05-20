ea847f49629f8f9205596a8bb36325847cd8d8e0
shinybrad <83238715+shinybrad@users.noreply.github.com>
2022-05-20 02:33:09 -0700

    New translations app.php (Thai) [ci skip]

--- src/translations/th/app.php
+++ src/translations/th/app.php
@@ -947,7 +947,7 @@ return [
     'No blocks.' => 'No blocks.',
     'No category groups exist yet.' => 'No category groups exist yet.',
     'No content migrations.' => 'No content migrations.',
-    'No deprecation warnings to report!' => 'No deprecation warnings to report!',
+    'No deprecation warnings to report!' => 'ไม่มีคำเตือนการหลีกเลี่ยงใช้งานที่จะรายงาน!',
     'No entries exist yet.' => 'No entries exist yet.',
     'No entry types exist for this section.' => 'No entry types exist for this section.',
     'No fields exist yet.' => 'No fields exist yet.',
