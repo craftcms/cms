60ca5d0727d7888be85d6f603fba9e6462834ef9
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2022-05-24 17:23:55 +0100

    WIP db restore

--- packages/craftcms-playwright/_craft.js
+++ packages/craftcms-playwright/_craft.js
@@ -1,22 +1,15 @@
-const dbBackupPath = './backup/db.sql';
-
-const dbBackup = async () => {
-  console.log('Creating DB backup');
-  try {
-  } catch (e) {
-    console.error(e);
-  }
-};
+const nodeExec = util.promisify(require('child_process').exec);
 
 const dbRestore = async () => {
   console.log('Restoring DB');
   try {
+    const {stdout, stderr} = await nodeExec(`../../craft db/restore db.sql`);
+    return {stdout, stderr};
   } catch (e) {
     console.error(e);
   }
 };
 
 module.exports = {
-  dbBackup,
   dbRestore,
 };
