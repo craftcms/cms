190b3e90dcadc1befbf1ac422052f4fa28f6cef1
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2022-05-26 10:00:34 +0100

    Update plugin actions test with fixtures

--- tests/playwright/pluginstore/plugin-actions.test.js
+++ tests/playwright/pluginstore/plugin-actions.test.js
@@ -1,79 +1,86 @@
-const {expect} = require('@playwright/test');
-const {test} = require('@craftcms/playwright');
+const {test, expect} = require('@craftcms/playwright');
 test.describe.configure({mode: 'serial'});
 
-test('Install the plugin', async ({page, baseURL}) => {
-  await page.goto('./plugin-store/seomatic');
+test.describe('Plugin Actions', () => {
+  test.afterAll(async ({cleanAll}) => {});
 
-  // Wait plugin request to be done
-  await page.waitForResponse((response) =>
-    response
-      .url()
-      .includes('//api.craftcms.com/v1/plugin-store/plugin/seomatic')
-  );
+  test('Install the plugin', async ({page, baseURL}) => {
+    await page.goto('./plugin-store/seomatic');
 
-  // Developer name
-  const title = page.locator('.plugin-details-header h1');
-  await expect(title).toContainText('SEOmatic');
+    // Wait plugin request to be done
+    await page.waitForResponse((response) =>
+      response
+        .url()
+        .includes('//api.craftcms.com/v1/plugin-store/plugin/seomatic')
+    );
 
-  // Try the plugin
-  await page.click('.plugin-editions-edition button:has-text("Try")');
+    // Developer name
+    const title = page.locator('.plugin-details-header h1');
+    await expect(title).toContainText('SEOmatic');
 
-  // Go through the install process
-  const status = page.locator('#status');
-  await expect(status).toContainText('Checking environment');
-  await page.waitForResponse((response) => response.url().includes('precheck')); //craft4.test/index.php?p=admin%2Factions%2Fpluginstore%2Finstall%2Fprecheck
-  await expect(status).toContainText(
-    'Updating Composer dependencies (this may take a minute)…'
-  );
-  await page.waitForResponse((response) =>
-    response.url().includes('composer-install')
-  ); //craft4.test/index.php?p=admin%2Factions%2Fpluginstore%2Finstall%2Fcomposer-install
-  await expect(status).toContainText('Installing the plugin');
-  await page.waitForResponse((response) =>
-    response.url().includes('craft-install')
-  ); //craft4.test/index.php?p=admin%2Factions%2Fpluginstore%2Finstall%2Fcraft-install
-  await expect(status).toContainText('All done!');
-});
+    // Try the plugin
+    await page.click('.plugin-editions-edition button:has-text("Try")');
 
-test('Uninstall and remove the plugin', async ({page, baseURL}) => {
-  page.goto('./settings/plugins');
+    // Go through the install process
+    const status = page.locator('#status');
+    await expect(status).toContainText('Checking environment');
+    await page.waitForResponse((response) =>
+      response.url().includes('precheck')
+    ); //craft4.test/index.php?p=admin%2Factions%2Fpluginstore%2Finstall%2Fprecheck
+    await expect(status).toContainText(
+      'Updating Composer dependencies (this may take a minute)…'
+    );
+    await page.waitForResponse((response) =>
+      response.url().includes('composer-install')
+    ); //craft4.test/index.php?p=admin%2Factions%2Fpluginstore%2Finstall%2Fcomposer-install
+    await expect(status).toContainText('Installing the plugin');
+    await page.waitForResponse((response) =>
+      response.url().includes('craft-install')
+    ); //craft4.test/index.php?p=admin%2Factions%2Fpluginstore%2Finstall%2Fcraft-install
+    await expect(status).toContainText('All done!');
+  });
 
-  // Uninstall the plugin
-  const menuBtnSelector = 'tr[data-handle="seomatic"] button.menubtn';
-  const menuBtn = await page.waitForSelector(menuBtnSelector);
+  test('Uninstall and remove the plugin', async ({page, baseURL}) => {
+    page.goto('./settings/plugins');
 
-  await page.click(menuBtnSelector);
+    // Uninstall the plugin
+    const menuBtnSelector = 'tr[data-handle="seomatic"] button.menubtn';
+    const menuBtn = await page.waitForSelector(menuBtnSelector);
 
-  let menuId = await menuBtn.getAttribute('aria-controls');
-  menuId = menuId.replace('.', '\\.');
+    await page.click(menuBtnSelector);
 
-  page.on('dialog', (dialog) => dialog.accept());
+    let menuId = await menuBtn.getAttribute('aria-controls');
+    menuId = menuId.replace('.', '\\.');
 
-  const uninstallBtnSelector = '#' + menuId + ' a:has-text("Uninstall")';
-  await page.waitForSelector(uninstallBtnSelector);
-  await page.click(uninstallBtnSelector);
+    page.on('dialog', (dialog) => dialog.accept());
 
-  // Remove the plugin
-  const menuBtn2 = await page.waitForSelector(menuBtnSelector);
-  await page.click(menuBtnSelector);
+    const uninstallBtnSelector = '#' + menuId + ' a:has-text("Uninstall")';
+    await page.waitForSelector(uninstallBtnSelector);
+    await page.click(uninstallBtnSelector);
 
-  let menuId2 = await menuBtn2.getAttribute('aria-controls');
-  menuId2 = menuId2.replace('.', '\\.');
+    // Remove the plugin
+    const menuBtn2 = await page.waitForSelector(menuBtnSelector);
+    await page.click(menuBtnSelector);
 
-  const removeBtnSelector = '#' + menuId2 + ' a:has-text("Remove")';
-  await page.waitForSelector(removeBtnSelector);
-  await page.click(removeBtnSelector);
+    let menuId2 = await menuBtn2.getAttribute('aria-controls');
+    menuId2 = menuId2.replace('.', '\\.');
 
-  const status = page.locator('#status');
-  await expect(status).toContainText('Checking environment');
-  await page.waitForResponse((response) => response.url().includes('precheck')); //craft4.test/index.php?p=admin%2Factions%2Fpluginstore%2Fremove%2Fprecheck
-  await expect(status).toContainText(
-    'Updating Composer dependencies (this may take a minute)…'
-  );
-  await page.waitForResponse((response) =>
-    response.url().includes('composer-remove')
-  ); //craft4.test/index.php?p=admin%2Factions%2Fpluginstore%2Fremove%2Fcomposer-remove
-  await expect(status).toContainText('The plugin was removed successfully.');
-  await page.waitForResponse((response) => response.url().includes('finish')); //craft4.test/index.php?p=admin%2Factions%2Fpluginstore%2Fremove%2Ffinish
+    const removeBtnSelector = '#' + menuId2 + ' a:has-text("Remove")';
+    await page.waitForSelector(removeBtnSelector);
+    await page.click(removeBtnSelector);
+
+    const status = page.locator('#status');
+    await expect(status).toContainText('Checking environment');
+    await page.waitForResponse((response) =>
+      response.url().includes('precheck')
+    ); //craft4.test/index.php?p=admin%2Factions%2Fpluginstore%2Fremove%2Fprecheck
+    await expect(status).toContainText(
+      'Updating Composer dependencies (this may take a minute)…'
+    );
+    await page.waitForResponse((response) =>
+      response.url().includes('composer-remove')
+    ); //craft4.test/index.php?p=admin%2Factions%2Fpluginstore%2Fremove%2Fcomposer-remove
+    await expect(status).toContainText('The plugin was removed successfully.');
+    await page.waitForResponse((response) => response.url().includes('finish')); //craft4.test/index.php?p=admin%2Factions%2Fpluginstore%2Fremove%2Ffinish
+  });
 });
