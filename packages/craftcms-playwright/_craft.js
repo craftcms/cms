3bdc8fe6b52f0516ade076af365bca92a6bf0780
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2022-05-26 09:58:23 +0100

    Rework craft helper methods

--- packages/craftcms-playwright/_craft.js
+++ packages/craftcms-playwright/_craft.js
@@ -1,9 +1,37 @@
+const util = require('util');
 const nodeExec = util.promisify(require('child_process').exec);
+const craftCli = '/app/craft';
 
 const dbRestore = async () => {
   console.log('Restoring DB');
   try {
-    const {stdout, stderr} = await nodeExec(`../../craft db/restore db.sql`);
+    const {stdout, stderr} = await nodeExec(
+      `${craftCli} db/restore /app/backup/db.sql`
+    );
+    return {stdout, stderr};
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const projectConfigRestore = async () => {
+  console.log('Restoring Project Config');
+  try {
+    const {stdout, stderr} = await nodeExec(
+      `cp -vfrp /app/backup/project /app/config/.`
+    );
+    return {stdout, stderr};
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const composerRestore = async () => {
+  console.log('Restoring Composer');
+  try {
+    const {stdout, stderr} = await nodeExec(
+      `cp -vfrp /app/backup/composer.* /app/. && composer install --working-dir=/app`
+    );
     return {stdout, stderr};
   } catch (e) {
     console.error(e);
@@ -12,4 +40,6 @@ const dbRestore = async () => {
 
 module.exports = {
   dbRestore,
+  projectConfigRestore,
+  composerRestore,
 };
