e00c9c01f485fa215e2e7ae6ae6cd48961c7de21
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2022-05-27 16:00:57 +0100

    Try to reset as much as we can on teardown of tests

--- packages/craftcms-playwright/_global-teardown.js
+++ packages/craftcms-playwright/_global-teardown.js
@@ -1,3 +1,8 @@
+const craft = require('./_craft');
+
 module.exports = async (config) => {
   console.log('Tearing down');
+  await craft.dbRestore();
+  await craft.projectConfigRestore();
+  await craft.composerRestore();
 };
