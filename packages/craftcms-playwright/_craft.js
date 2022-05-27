d8875cb98532a04ab4f4b83c4847e569b2479999
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2022-05-27 16:00:20 +0100

    Update craft specific commands

--- packages/craftcms-playwright/_craft.js
+++ packages/craftcms-playwright/_craft.js
@@ -1,12 +1,26 @@
 const util = require('util');
 const nodeExec = util.promisify(require('child_process').exec);
+const dockerCli =
+  'docker compose --file=./node_modules/@craftcms/playwright/docker-compose.yaml exec --user appuser playwright';
 const craftCli = '/app/craft';
 
 const dbRestore = async () => {
   console.log('Restoring DB');
   try {
     const {stdout, stderr} = await nodeExec(
-      `${craftCli} db/restore /app/backup/db.sql`
+      `${dockerCli} ${craftCli} db/restore --interactive=0 /app/backup/db.sql`
+    );
+    return {stdout, stderr};
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const dbBackup = async () => {
+  console.log('Backing up DB');
+  try {
+    const {stdout, stderr} = await nodeExec(
+      `${dockerCli} ${craftCli} db/backup --interactive=0 --overwrite=1 /app/backup/db.sql`
     );
     return {stdout, stderr};
   } catch (e) {
@@ -18,7 +32,7 @@ const projectConfigRestore = async () => {
   console.log('Restoring Project Config');
   try {
     const {stdout, stderr} = await nodeExec(
-      `cp -vfrp /app/backup/project /app/config/.`
+      `${dockerCli} cp -vfrp /app/backup/project /app/config/.`
     );
     return {stdout, stderr};
   } catch (e) {
@@ -29,9 +43,12 @@ const projectConfigRestore = async () => {
 const composerRestore = async () => {
   console.log('Restoring Composer');
   try {
-    const {stdout, stderr} = await nodeExec(
-      `cp -vfrp /app/backup/composer.* /app/. && composer install --working-dir=/app`
+    let {stdout, stderr} = await nodeExec(
+      `${dockerCli} cp -vfrp /app/backup/composer.json /app/.`
     );
+    await nodeExec(`${dockerCli} cp -vfrp /app/backup/composer.lock /app/.`);
+    await nodeExec(`${dockerCli} composer install --working-dir=/app`);
+    await nodeExec(`${dockerCli} composer dump-autoload --working-dir=/app`);
     return {stdout, stderr};
   } catch (e) {
     console.error(e);
@@ -39,6 +56,7 @@ const composerRestore = async () => {
 };
 
 module.exports = {
+  dbBackup,
   dbRestore,
   projectConfigRestore,
   composerRestore,
