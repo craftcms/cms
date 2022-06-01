2d551314a0e12d1b8e1c2bc4a0bad9673c463c2c
Lupe <lupe@pixelandtonic.com>
2022-06-01 10:56:46 -0700

    Update link label in footer to match visible text

--- src/translations/en/app.php
+++ src/translations/en/app.php
@@ -387,6 +387,7 @@ return [
     'Couldn’t update password.' => 'Couldn’t update password.',
     'Country' => 'Country',
     'County' => 'County',
+    'Craft CMS' => 'Craft CMS',
     'Craft CMS does not support backtracking to this version. Please update to Craft CMS {version} or later.' => 'Craft CMS does not support backtracking to this version. Please update to Craft CMS {version} or later.',
     'Craft CMS is running in Dev Mode.' => 'Craft CMS is running in Dev Mode.',
     'Craft Support' => 'Craft Support',
