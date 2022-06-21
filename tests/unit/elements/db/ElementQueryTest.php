4662cc9b7b52b21a1e7ca1d47b83afa960fe3e9e
brandonkelly <brandon@pixelandtonic.com>
2022-06-20 23:45:43 -0700

    Allow other orderBy columns alongside `score`
    Resolves #11457

--- tests/unit/elements/db/ElementQueryTest.php
+++ tests/unit/elements/db/ElementQueryTest.php
@@ -39,6 +39,18 @@ class ElementQueryTest extends TestCase
     }
 
     /**
+     * @dataProvider normalizeOrderByDataProvider
+     * @param array $expected
+     * @param mixed $columns
+     * @return void
+     */
+    public function testNormalizeOrderBy(array $expected, mixed $columns): void
+    {
+        $query = Entry::find();
+        self::assertSame($expected, $this->invokeMethod($query, 'normalizeOrderBy', [$columns]));
+    }
+
+    /**
      * @return array
      */
     public function relatedToDataProvider(): array
@@ -47,6 +59,21 @@ class ElementQueryTest extends TestCase
             [false, ['or', ['targetElement' => 1], ['targetElement' => 2]], 3],
             [['or', 1, 2], null, ['or', 1, 2]],
             [['and', ['field' => null, 'sourceSite' => null, 'element' => ['or', 1]], ['field' => null, 'sourceSite' => null, 'element' => ['or', 1, 2]]], ['or', 1], [1, 2]],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function normalizeOrderByDataProvider(): array
+    {
+        return [
+            [['score' => SORT_DESC], 'score'],
+            [['score' => SORT_DESC], ' score '],
+            [['score' => SORT_ASC], 'score asc'],
+            [['score' => SORT_ASC], ' score asc '],
+            [['foo' => SORT_ASC, 'score' => SORT_DESC, 'bar' => SORT_ASC], 'foo, score, bar'],
+            [['foo' => SORT_DESC, 'score' => SORT_ASC, 'bar' => SORT_DESC], 'foo desc, score asc, bar desc'],
         ];
     }
 }
