9c64037e67bcd1697844eb4b9c49f8793a8cc75d
shinybrad <83238715+shinybrad@users.noreply.github.com>
2022-06-30 11:13:18 -0400

    New translations app.php (Hebrew) [ci skip]

--- src/translations/he/app.php
+++ src/translations/he/app.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    '(Ungrouped)' => '(Ungrouped)',
     '(blank)' => '(ריק)',
     '(trial)' => '(ניסיון)',
     '({currencyCode}) {currencySymbol}' => '({currencyCode}) {currencySymbol}',
