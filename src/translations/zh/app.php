386a6882e0a90306eba62981c18f56d91c9f5f6a
shinybrad <83238715+shinybrad@users.noreply.github.com>
2022-06-30 11:12:50 -0400

    New translations app.php (Chinese Simplified) [ci skip]

--- src/translations/zh/app.php
+++ src/translations/zh/app.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    '(Ungrouped)' => '(Ungrouped)',
     '(blank)' => '（空）',
     '(trial)' => '（试用版）',
     '({currencyCode}) {currencySymbol}' => '({currencyCode}) {currencySymbol}',
