8b7313705cdaa448dd47517f39616154767dce05
Lupe <lupe@pixelandtonic.com>
2022-07-06 07:52:27 -0700

    Hide draft icon from assistive tech

--- src/base/Element.php
+++ src/base/Element.php
@@ -4525,7 +4525,10 @@ JS,
                     return false;
                 }
                 if ($this->getIsUnpublishedDraft()) {
-                    $icon = Html::tag('span', '', ['data' => ['icon' => 'draft']]);
+                    $icon = Html::tag('span', '', [
+                        'data' => ['icon' => 'draft'],
+                        'aria' => ['hidden' => 'true'],
+                    ]);
                     $label = Craft::t('app', 'Draft');
                 } else {
                     $status = $this->getStatus();
