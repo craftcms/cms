cd8de56f54a80664bb4b9bb59c8a0839f3e811a3
shinybrad <83238715+shinybrad@users.noreply.github.com>
2022-07-08 11:25:46 -0400

    New translations app.php (Slovak) [ci skip]

--- src/translations/sk/app.php
+++ src/translations/sk/app.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    '(Ungrouped)' => '(Ungrouped)',
     '(blank)' => '(prázdne)',
     '(trial)' => '(skúšobná verzia)',
     '({currencyCode}) {currencySymbol}' => '({currencyCode}) {currencySymbol}',
