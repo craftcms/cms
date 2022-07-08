3f892c47964212ef5e060dad7dc0d8849865f62a
shinybrad <83238715+shinybrad@users.noreply.github.com>
2022-07-08 11:26:08 -0400

    New translations app.php (Arabic) [ci skip]

--- src/translations/ar/app.php
+++ src/translations/ar/app.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    '(Ungrouped)' => '(Ungrouped)',
     '(blank)' => '(فراغ)',
     '(trial)' => '(نسخة تجريبية)',
     '({currencyCode}) {currencySymbol}' => '({currencyCode}) {currencySymbol}',
