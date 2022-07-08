10c2261590ed118b106f992d902177a85f74a4d0
shinybrad <83238715+shinybrad@users.noreply.github.com>
2022-07-08 11:25:36 -0400

    New translations app.php (English, United Kingdom) [ci skip]

--- src/translations/en-GB/app.php
+++ src/translations/en-GB/app.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    '(Ungrouped)' => '(Ungrouped)',
     '(blank)' => '(blank)',
     '(trial)' => '(trial)',
     '({currencyCode}) {currencySymbol}' => '({currencyCode}) {currencySymbol}',
