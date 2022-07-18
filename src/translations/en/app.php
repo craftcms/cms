d1fee884cb9e4829a516b5c378a301bb90aeeebd
brandonkelly <brandon@pixelandtonic.com>
2022-07-18 14:29:44 -0700

    DateTimeHelper::humanDuration() + show remembered session duration on the login page
    Resolves #11594

--- src/translations/en/app.php
+++ src/translations/en/app.php
@@ -774,7 +774,7 @@ return [
     'Job retried.' => 'Job retried.',
     'Job' => 'Job',
     'Keep both' => 'Keep both',
-    'Keep me logged in' => 'Keep me logged in',
+    'Keep me signed in for {duration}' => 'Keep me signed in for {duration}',
     'Keep me signed in' => 'Keep me signed in',
     'Keep them' => 'Keep them',
     'Knowledge Base' => 'Knowledge Base',
--- tests/unit/helpers/DateTimeHelperTest.php
+++ tests/unit/helpers/DateTimeHelperTest.php
@@ -97,17 +97,6 @@ class DateTimeHelperTest extends TestCase
     }
 
     /**
-     * @dataProvider secondsToHumanTimeDurationDataProvider
-     * @param string $expected
-     * @param int $seconds
-     * @param bool $showSeconds
-     */
-    public function testSecondsToHumanTimeDuration(string $expected, int $seconds, bool $showSeconds = true): void
-    {
-        self::assertSame($expected, DateTimeHelper::secondsToHumanTimeDuration($seconds, $showSeconds));
-    }
-
-    /**
      * What we are testing here is that if we tell the DtHelper to not assume a timezone and set it to system.
      * That all formats are converted to the system timezone from the inputted system timezone. ie an array like this:
      *
@@ -243,16 +232,19 @@ class DateTimeHelperTest extends TestCase
     }
 
     /**
-     * @dataProvider humanIntervalFromDurationDataProvider
+     * @dataProvider humanDurationDataProvider
      * @param string $expected
-     * @param string $duration
-     * @param bool $showSeconds
+     * @param string|int $duration
+     * @param bool|null $showSeconds
      * @throws Exception
      */
-    public function testHumanIntervalFromDuration(string $expected, string $duration, bool $showSeconds = true): void
+    public function testHumanDuration(string $expected, string|int $duration, ?bool $showSeconds = null): void
     {
-        $dateInterval = new DateInterval($duration);
-        self::assertSame($expected, DateTimeHelper::humanDurationFromInterval($dateInterval, $showSeconds));
+        if (is_string($duration)) {
+            $duration = new DateInterval($duration);
+        }
+
+        self::assertSame($expected, DateTimeHelper::humanDuration($duration, $showSeconds));
     }
 
     /**
@@ -446,25 +438,6 @@ class DateTimeHelperTest extends TestCase
             [60, DateTimeHelper::SECONDS_MINUTE],
             [2629740, DateTimeHelper::SECONDS_MONTH],
             [31556874, DateTimeHelper::SECONDS_YEAR],
-        ];
-    }
-
-    /**
-     * @return array
-     */
-    public function secondsToHumanTimeDurationDataProvider(): array
-    {
-        return [
-            ['22 seconds', 22],
-            ['1 second', 1],
-            ['2 minutes', 120],
-            ['2 minutes, 5 seconds', 125],
-            ['2 minutes, 1 second', 121],
-            ['2 minutes', 121, false],
-            ['3 minutes', 179, false],
-            ['1 hour', 3600],
-            ['1 day', 86400],
-            ['1 week', 604800],
         ];
     }
 
@@ -686,7 +659,7 @@ class DateTimeHelperTest extends TestCase
     /**
      * @return array
      */
-    public function humanIntervalFromDurationDataProvider(): array
+    public function humanDurationDataProvider(): array
     {
         return [
             ['1 day', 'P1D'],
@@ -697,7 +670,22 @@ class DateTimeHelperTest extends TestCase
             ['2 months, 1 day, and 1 hour', 'P2M1DT1H'],
             ['1 hour and 1 minute', 'PT1H1M25S', false],
             ['1 hour and 2 minutes', 'PT1H1M55S', false],
-            ['less than a minute', 'PT1S', false],
+            ['Less than a minute', 'PT1S', false],
+            ['1 minute', 82],
+            ['1 minute', 82, false],
+            ['1 minute and 22 seconds', 82, true],
+            ['22 seconds', 22, true],
+            ['22 seconds', 22],
+            ['Less than a minute', 22, false],
+            ['1 second', 1],
+            ['2 minutes', 120],
+            ['2 minutes and 5 seconds', 125],
+            ['2 minutes and 1 second', 121],
+            ['2 minutes', 121, false],
+            ['3 minutes', 179, false],
+            ['1 hour', 3600],
+            ['1 day', 86400],
+            ['1 week', 604800],
         ];
     }
 
@@ -710,7 +698,6 @@ class DateTimeHelperTest extends TestCase
             [10, 10000, 10],
             [0, 0000, 0],
             [928172, 928172000, 928172],
-
         ];
     }
 
