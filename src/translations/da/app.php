91e46d239cd3045185a0d50a8ad07b7f024ef258
shinybrad <83238715+shinybrad@users.noreply.github.com>
2022-07-20 17:27:55 -0400

    New translations app.php (Danish) [ci skip]

--- src/translations/da/app.php
+++ src/translations/da/app.php
@@ -889,6 +889,7 @@ return [
     'More Resources' => 'Flere ressourcer',
     'More' => 'Mere',
     'More…' => 'Mere…',
+    'More actions' => 'More actions',
     'Move down' => 'Flyt ned',
     'Move to the left' => 'Flyt til venstre',
     'Move to the right' => 'Flyt til højre',
