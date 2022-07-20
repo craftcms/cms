e1b874cd0a516f7509c7e2a42ec5944d1ebaa5f1
shinybrad <83238715+shinybrad@users.noreply.github.com>
2022-07-20 16:54:38 -0400

    New translations app.php (German) [ci skip]

--- src/translations/de/app.php
+++ src/translations/de/app.php
@@ -304,6 +304,7 @@ return [
     'Connecting to CraftCMS.com…' => 'Verbindung mit CraftCMS.com wird hergestellt…',
     'Content' => 'Inhalt',
     'Contents of {path}' => 'Inhalt von {path}',
+    'Context' => 'Context',
     'Continue anyway' => 'Trotzdem fortfahren',
     'Continue shopping' => 'Weiter einkaufen',
     'Continue' => 'Weiter',
