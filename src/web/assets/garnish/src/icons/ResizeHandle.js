a49c1506504e31b45185060bbb2dc51e3e821b68
Lupe <lupe@pixelandtonic.com>
2022-07-27 12:18:57 -0700

    Add resize handle icon to Garnish

---
+++ src/web/assets/garnish/src/icons/ResizeHandle.js
@@ -0,0 +1,4 @@
+const ResizeHandle =
+  '<svg fill="none" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" aria-labelledby="resizeTitle"><title id="resizeTitle">Resize</title><path d="M20 0 0 20h2L20 2V0ZM20 6 6 20h2L20 8V6ZM20 12l-8 8h2l6-6v-2Z" fill="#000"/></svg>';
+
+export default ResizeHandle;
