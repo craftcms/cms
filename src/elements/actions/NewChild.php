08d7546db0ad7c7606295701348820cecc0de3b8
brandonkelly <brandon@pixelandtonic.com>
2022-08-16 16:22:07 -0700

    Fixed PHP errors

--- src/elements/actions/NewChild.php
+++ src/elements/actions/NewChild.php
@@ -37,13 +37,11 @@ class NewChild extends ElementAction
     /**
      * @inheritdoc
      */
-    public function init(): void
+    public function setElementType(string $elementType): void
     {
-        parent::init();
-
         /** @var string|ElementInterface $elementType */
         /** @phpstan-var class-string<ElementInterface> $elementType */
-        $elementType = $this->elementType;
+        parent::setElementType($elementType);
 
         if (!isset($this->label)) {
             $this->label = Craft::t('app', 'Create a new child {type}', [
