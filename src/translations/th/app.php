6bed8d78fcbecca2b1a5bd1e19d7997f872ebac3
shinybrad <83238715+shinybrad@users.noreply.github.com>
2022-09-01 13:20:53 -0400

    New translations app.php (Thai) [ci skip]

--- src/translations/th/app.php
+++ src/translations/th/app.php
@@ -892,7 +892,6 @@ return [
     'No GraphQL tokens exist yet.' => 'No GraphQL tokens exist yet.',
     'No blocks.' => 'No blocks.',
     'No category groups exist yet.' => 'No category groups exist yet.',
-    'No content migrations.' => 'No content migrations.',
     'No deprecation errors to report!' => 'No deprecation errors to report!',
     'No entries exist yet.' => 'No entries exist yet.',
     'No entry types exist for this section.' => 'No entry types exist for this section.',
