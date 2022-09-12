dd84d3ed2fd91b7853c98b3290f083193d863811
Brian Hanson <brian@brianhanson.net>
2022-09-12 12:23:15 -0500

    Allow animated webp
    #11889

--- src/image/Raster.php
+++ src/image/Raster.php
@@ -209,6 +209,7 @@ class Raster extends Image
             $gif = $this->_instance->create($newSize);
             $gif->layers()->remove(0);
 
+            $this->_image->layers()->coalesce();
             foreach ($this->_image->layers() as $layer) {
                 $croppedLayer = $layer->crop($startingPoint, $newSize);
                 $gif->layers()->add($croppedLayer);
@@ -703,6 +704,7 @@ class Raster extends Image
                 return ['jpeg_quality' => $quality, 'flatten' => true];
 
             case 'gif':
+            case 'webp':
                 return ['animated' => $this->_isAnimatedGif];
 
             case 'png':
