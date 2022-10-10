037c06e572691a4e9beb354cbc00396ec5cea476
brandonkelly <brandon@pixelandtonic.com>
2022-10-10 10:58:55 -0700

    Bring back FocusVisibleAsset

---
+++ src/web/assets/focusvisible/FocusVisibleAsset.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @link https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license https://craftcms.github.io/license/
+ */
+
+namespace craft\web\assets\focusvisible;
+
+use craft\web\AssetBundle;
+
+/**
+ * Focus visible asset bundle.
+ *
+ * @deprecated in 4.2.6.
+ */
+class FocusVisibleAsset extends AssetBundle
+{
+}
