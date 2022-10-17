fc83211cb5eb325c16e72c5cbeb6db803eca054e
brandonkelly <brandon@pixelandtonic.com>
2022-10-17 14:05:50 -0700

    Explicitly check for an empty value
    Same difference as in_array(['']) but clearer

--- src/console/controllers/SetupController.php
+++ src/console/controllers/SetupController.php
@@ -183,7 +183,7 @@ EOD;
     {
         $didSomething = false;
 
-        if (in_array(Craft::$app->id, ['CraftCMS', '']) && !App::env('CRAFT_APP_ID')) {
+        if ((!Craft::$app->id || Craft::$app->id === 'CraftCMS') && !App::env('CRAFT_APP_ID')) {
             $this->run('app-id');
             $didSomething = true;
         }
