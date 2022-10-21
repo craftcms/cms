775f01d5ce1b072660fc710af8c78144728f30df
shinybrad <83238715+shinybrad@users.noreply.github.com>
2022-10-21 09:56:09 -0400

    New translations app.php (German) [ci skip]

--- src/translations/de/app.php
+++ src/translations/de/app.php
@@ -435,7 +435,7 @@ return [
     'Current User Condition' => 'Aktueller Benutzerzustand',
     'Current' => 'Aktuell',
     'Currently SVG images do not support focal point.' => 'Derzeit unterstützen SVG-Bilder keinen Blickpunkt.',
-    'Custom' => 'Custom',
+    'Custom' => 'Benutzerdefiniert',
     'Customize' => 'Anpassen',
     'Custom…' => 'Benutzerdefiniert…',
     'Dashboard' => 'Übersicht',
