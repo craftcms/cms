77cd870b9e8d7742222234f35c62430ba1751887
Brian Hanson <brian@brianhanson.net>
2022-11-11 09:05:14 -0600

    Respect siteId in graphql structure mutations

--- src/gql/base/StructureMutationTrait.php
+++ src/gql/base/StructureMutationTrait.php
@@ -30,19 +30,20 @@ trait StructureMutationTrait
         }
 
         $structureService = Craft::$app->getStructures();
+        $siteId = $arguments['siteId'] ?? null;
 
         if (!empty($arguments['prependTo'])) {
-            $structureService->prepend($structureId, $element, $this->getRelatedElement($arguments['prependTo']));
+            $structureService->prepend($structureId, $element, $this->getRelatedElement($arguments['prependTo'], $siteId));
         } elseif (!empty($arguments['appendTo'])) {
-            $structureService->append($structureId, $element, $this->getRelatedElement($arguments['appendTo']));
+            $structureService->append($structureId, $element, $this->getRelatedElement($arguments['appendTo'], $siteId));
         } elseif (!empty($arguments['prependToRoot'])) {
             $structureService->prependToRoot($structureId, $element);
         } elseif (!empty($arguments['appendToRoot'])) {
             $structureService->appendToRoot($structureId, $element);
         } elseif (!empty($arguments['insertBefore'])) {
-            $structureService->moveBefore($structureId, $element, $this->getRelatedElement($arguments['insertBefore']));
+            $structureService->moveBefore($structureId, $element, $this->getRelatedElement($arguments['insertBefore'], $siteId));
         } elseif (!empty($arguments['insertAfter'])) {
-            $structureService->moveAfter($structureId, $element, $this->getRelatedElement($arguments['insertAfter']));
+            $structureService->moveAfter($structureId, $element, $this->getRelatedElement($arguments['insertAfter'], $siteId));
         }
     }
 
@@ -50,11 +51,12 @@ trait StructureMutationTrait
      * Get the related element.
      *
      * @param $elementId
+     * @param int|null $siteId
      * @return ElementInterface
      */
-    protected function getRelatedElement($elementId)
+    protected function getRelatedElement($elementId, int $siteId = null): ElementInterface
     {
-        $relatedElement = Craft::$app->getElements()->getElementById($elementId);
+        $relatedElement = Craft::$app->getElements()->getElementById($elementId, null, $siteId);
 
         if (!$relatedElement) {
             throw new Error('Unable to move element in a structure');
