7ee2074d30ad92e165c14467c26a1af0f346f1c4
brandonkelly <brandon@pixelandtonic.com>
2022-12-08 06:24:34 -0800

    Filesystem type generator

---
+++ src/console/generators/FilesystemType.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace craft\console\generators;
+
+use Craft;
+use craft\base\Fs;
+use craft\models\FsListing;
+use Nette\PhpGenerator\PhpNamespace;
+use yii\helpers\Inflector;
+use yii\web\Application;
+
+/**
+ * Creates a new filesystem type.
+ */
+class FilesystemType extends BaseGenerator
+{
+    private string $className;
+    private string $namespace;
+    private string $displayName;
+
+    public function run(): bool
+    {
+        $this->className = $this->classNamePrompt('Filesystem type name:', [
+            'required' => true,
+        ]);
+
+        $this->namespace = $this->namespacePrompt('Filesystem type namespace:', [
+            'default' => "$this->baseNamespace\\fs",
+        ]);
+
+        $this->displayName = Inflector::camel2words($this->className);
+
+        $namespace = (new PhpNamespace($this->namespace))
+            ->addUse(Craft::class)
+            ->addUse(Fs::class)
+            ->addUse(FsListing::class);
+
+        $class = $this->createClass($this->className, Fs::class, [
+            self::CLASS_METHODS => $this->methods(),
+        ]);
+        $namespace->add($class);
+
+        $class->addComment("$this->displayName filesystem type");
+
+        $this->writePhpClass($namespace);
+
+        $message = "**Filesystem type created!**";
+        if (!$this->module instanceof Application) {
+            $moduleFile = $this->moduleFile();
+            $message .= "\n" . <<<MD
+Add the following code to `$moduleFile` to register the filesystem type:
+
+```php
+use craft\\events\\RegisterComponentTypesEvent;
+use craft\\services\\Fs;
+use yii\\base\\Event;
+use $this->namespace\\$this->className;
+
+Event::on(
+    Fs::class,
+    Fs::EVENT_REGISTER_FILESYSTEM_TYPES,
+    function(RegisterComponentTypesEvent \$event) {
+        \$event->types[] = $this->className::class;
+    }
+);
+```
+MD;
+        }
+
+        $this->controller->success($message);
+        return true;
+    }
+
+    private function methods(): array
+    {
+        return [
+            'displayName' => sprintf('return %s;', $this->messagePhp($this->displayName)),
+            'attributeLabels' => <<<PHP
+return array_merge(parent::attributeLabels(), [
+    // ...
+]);
+PHP,
+            'defineRules' => <<<PHP
+return array_merge(parent::defineRules(), [
+    // ...
+]);
+PHP,
+            'getSettingsHtml' => 'return null;',
+            'getFileList' => <<<PHP
+// Loop through the files and directories in \$directory and yield FsListing models representing them
+// ...
+PHP,
+            'getFileSize' => <<<PHP
+// Return the file size of \$uri in bytes
+// ...
+PHP,
+            'getDateModified' => <<<PHP
+// Return the date modified for \$uri as a Unix timestamp
+// ...
+PHP,
+            'read' => '// ...',
+            'write' => '// ...',
+            'writeFileFromStream' => '// ...',
+            'fileExists' => '// ...',
+            'deleteFile' => '// ...',
+            'renameFile' => '// ...',
+            'copyFile' => '// ...',
+            'getFileStream' => '// ...',
+            'directoryExists' => '// ...',
+            'createDirectory' => '// ...',
+            'deleteDirectory' => '// ...',
+            'renameDirectory' => '// ...',
+        ];
+    }
+}
