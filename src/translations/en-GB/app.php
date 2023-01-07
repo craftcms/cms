7702524aaf6b99e2896c3a43998a2e99f957734f
shinybrad <83238715+shinybrad@users.noreply.github.com>
2023-01-07 11:34:37 -0500

    New translations app.php (English, United Kingdom) [ci skip]

--- src/translations/en-GB/app.php
+++ src/translations/en-GB/app.php
@@ -900,6 +900,7 @@ return [
     'Months' => 'Months',
     'More Resources' => 'More Resources',
     'More actions' => 'More actions',
+    'More info' => 'More info',
     'More' => 'More',
     'More…' => 'More…',
     'Move down' => 'Move down',
