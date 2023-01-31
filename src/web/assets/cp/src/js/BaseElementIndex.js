75da3653446225db4a02a85700d3d94e49cb128a
brandonkelly <brandon@pixelandtonic.com>
2023-01-31 10:27:06 -0800

    Fixed more bugs

--- src/web/assets/cp/src/js/BaseElementIndex.js
+++ src/web/assets/cp/src/js/BaseElementIndex.js
@@ -607,7 +607,7 @@ Craft.BaseElementIndex = Garnish.Base.extend(
           ((i) => {
             const step = sourcePath[i];
 
-            if ($overflowUl && i < sourcePath.length - 2) {
+            if ($overflowUl && i < sourcePath.length - 1) {
               step.$overflowLi = $('<li/>', {
                 class: 'hidden',
               }).appendTo($overflowUl);
@@ -787,7 +787,7 @@ Craft.BaseElementIndex = Garnish.Base.extend(
         this.$sourcePathOverflowBtnContainer.removeClass('hidden');
         firstStep.$li.removeClass('first-step');
 
-        for (let i = 0; i < this.sourcePath.length - 2; i++) {
+        for (let i = 0; i < this.sourcePath.length - 1; i++) {
           const step = this.sourcePath[i];
           step.$overflowLi.removeClass('hidden');
           step.$li.addClass('hidden');
@@ -1105,6 +1105,7 @@ Craft.BaseElementIndex = Garnish.Base.extend(
       return new Promise((resolve, reject) => {
         // Ignore if we're not fully initialized yet
         if (!this.initialized) {
+          debugger;
           reject('The element index isn’t initialized yet.');
           return;
         }
@@ -1945,7 +1946,9 @@ Craft.BaseElementIndex = Garnish.Base.extend(
       var $option = $(ev.selectedOption).addClass('sel');
       this.$siteMenuBtn.html($option.html());
       this._setSite($option.data('site-id'));
-      this.updateElements();
+      if (this.initialized) {
+        this.updateElements();
+      }
       this.onSelectSite();
     },
 
