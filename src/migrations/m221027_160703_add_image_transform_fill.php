d81e15692d3d8c251d3c0b851fd5645302920b95
brandonkelly <brandon@pixelandtonic.com>
2023-02-03 17:43:22 -0800

    Update existing transforms' configs

--- src/migrations/m221027_160703_add_image_transform_fill.php
+++ src/migrations/m221027_160703_add_image_transform_fill.php
@@ -5,6 +5,7 @@ namespace craft\migrations;
 use Craft;
 use craft\db\Migration;
 use craft\db\Table;
+use craft\services\ProjectConfig;
 
 /**
  * m221027_160703_add_image_transform_fill migration.
@@ -18,15 +19,14 @@ class m221027_160703_add_image_transform_fill extends Migration
     {
         // Place migration code here...
         $this->addColumn(Table::IMAGETRANSFORMS, 'fill', $this->string(11)->null()->after('interlace'));
-        $allowUpscale = Craft::$app->getConfig()->getGeneral()->upscaleImages;
-        $this->addColumn(Table::IMAGETRANSFORMS, 'upscale', $this->boolean()->notNull()->defaultValue($allowUpscale)->after('fill'));
+        $this->addColumn(Table::IMAGETRANSFORMS, 'upscale', $this->boolean()->notNull()->defaultValue(true)->after('fill'));
 
-        $values = ['stretch', 'fit', 'crop', 'letterbox'];
+        $modeOptions = ['stretch', 'fit', 'crop', 'letterbox'];
         if ($this->db->getIsPgsql()) {
             // Manually construct the SQL for Postgres
             $check = '[[mode]] in (';
-            foreach ($values as $i => $value) {
-                if ($i != 0) {
+            foreach ($modeOptions as $i => $value) {
+                if ($i !== 0) {
                     $check .= ',';
                 }
                 $check .= $this->db->quoteValue($value);
@@ -34,13 +34,22 @@ class m221027_160703_add_image_transform_fill extends Migration
             $check .= ')';
             $this->execute("alter table {{%imagetransforms}} drop constraint {{%imagetransforms_mode_check}}, add check ({$check})");
         } else {
-            $this->alterColumn(
-                Table::IMAGETRANSFORMS,
-                'mode',
-                $this->enum('mode', ['stretch', 'fit', 'crop', 'letterbox'])->notNull()->defaultValue('crop'),
-            );
+            $this->alterColumn(Table::IMAGETRANSFORMS, 'mode', $this->enum('mode', $modeOptions)->notNull()->defaultValue('crop'));
         }
 
+        $projectConfig = Craft::$app->getProjectConfig();
+        $schemaVersion = $projectConfig->get('system.schemaVersion', true);
+
+        if (version_compare($schemaVersion, '4.4.0.3', '<')) {
+            // Hard-code the existing transforms with the current upscaleImages config value
+            $generalConfig = Craft::$app->getConfig()->getGeneral();
+            $transforms = $projectConfig->get(ProjectConfig::PATH_IMAGE_TRANSFORMS) ?? [];
+            foreach ($transforms as $uid => $config) {
+                $config['upscale'] = $generalConfig->upscaleImages;
+                $path = sprintf('%s.%s', ProjectConfig::PATH_IMAGE_TRANSFORMS, $uid);
+                $projectConfig->set($path, $config);
+            }
+        }
 
         return true;
     }
