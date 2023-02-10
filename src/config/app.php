48bef51d36d7a87f1f32fe115de2cae2da55efd7
brandonkelly <brandon@pixelandtonic.com>
2023-02-10 10:45:46 -0800

    Bump the schema version again

--- src/config/app.php
+++ src/config/app.php
@@ -4,7 +4,7 @@ return [
     'id' => 'CraftCMS',
     'name' => 'Craft CMS',
     'version' => '4.3.8.2',
-    'schemaVersion' => '4.4.0.3',
+    'schemaVersion' => '4.4.0.4',
     'minVersionRequired' => '3.7.11',
     'basePath' => dirname(__DIR__), // Defines the @app alias
     'runtimePath' => '@storage/runtime', // Defines the @runtime alias
