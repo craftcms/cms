craftcms/cms

commit 5850dd3693bef134ec680e99904e2b58805d5295

Image transform cleanup

src/base/Fs.php
@@ -30,9 +30,7 @@ abstract class Fs extends SavableComponent implements FsInterface
     public const VISIBILITY_PUBLIC = 'public';
 
     /**
-     * Return the root URL for a filesystem or `null` if the filesystem does not have any.
-     *
-     * @return string|null
+     * @inheritdoc
      */
     public function getRootUrl(): ?string
     {
@@ -40,8 +38,7 @@ abstract class Fs extends SavableComponent implements FsInterface
             return null;
         }
 
-        $url = App::parseEnv($this->url);
-        return $url ? rtrim($url, '/') . '/' : null;
+        return App::parseEnv($this->url) ?: null;
     }
 
     /**

src/base/FsInterface.php
@@ -22,6 +22,13 @@ use Generator;
 interface FsInterface extends SavableComponentInterface
 {
     /**
+     * Returns the root URL for a filesystem, if it has one.
+     *
+     * @return string|null
+     */
+    public function getRootUrl(): ?string;
+
+    /**
      * List files.
      *
      * @param string $directory The path of the directory to list files of

src/elements/Asset.php
@@ -1522,33 +1522,30 @@ JS;
 
     private function _url(mixed $transform = null, ?bool $immediately = null): ?string
     {
-        $volume = $this->getVolume();
-
-        $transform = $transform ?? $this->_transform;
-
-        if ($transform === null || !Image::canManipulateAsImage(pathinfo($this->getFilename(), PATHINFO_EXTENSION))) {
-            return Html::encodeSpaces(Assets::generateUrl($volume->getFs(), $this));
-        }
-
-        $fsNoUrls = !$transform && !$volume->getFs()->hasUrls;
-        $noFolder = !$this->folderId;
-        $transformNoUrl = $transform && !$volume->getTransformFs()->hasUrls;
-
-        if ($fsNoUrls || $noFolder || $transformNoUrl) {
+        if (!$this->folderId) {
             return null;
         }
 
-        $mimeType = $this->getMimeType();
-        $generalConfig = Craft::$app->getConfig()->getGeneral();
+        $volume = $this->getVolume();
+        $transform = $transform ?? $this->_transform;
 
-        if (
-            ($mimeType === 'image/gif' && !$generalConfig->transformGifs) ||
-            ($mimeType === 'image/svg+xml' && !$generalConfig->transformSvgs)
-        ) {
-            return Html::encodeSpaces(Assets::generateUrl($volume->getFs(), $this));
+        if ($transform) {
+            $mimeType = $this->getMimeType();
+            $generalConfig = Craft::$app->getConfig()->getGeneral();
+            if (
+                ($mimeType === 'image/gif' && !$generalConfig->transformGifs) ||
+                ($mimeType === 'image/svg+xml' && !$generalConfig->transformSvgs) ||
+                !Image::canManipulateAsImage(pathinfo($this->getFilename(), PATHINFO_EXTENSION))
+            ) {
+                $transform = null;
+            }
         }
 
         if ($transform) {
+            if (!$volume->getTransformFs()->hasUrls) {
+                return null;
+            }
+
             if (is_array($transform)) {
                 if (isset($transform['width'])) {
                     $transform['width'] = round((float)$transform['width']);
@@ -1600,7 +1597,12 @@ JS;
             }
         }
 
-        return Html::encodeSpaces(Assets::generateUrl($volume->getFs(), $this));
+        $fs = $volume->getFs();
+        if (!$fs->hasUrls) {
+            return null;
+        }
+
+        return Html::encodeSpaces(Assets::generateUrl($fs, $this));
     }
 
     /**

src/helpers/Assets.php
@@ -77,21 +77,21 @@ class Assets
     }
 
     /**
-     * Generates a URL for a given Assets file on a filesystem.
+     * Generates the URL for an asset.
      *
      * @param FsInterface $fs
      * @param Asset $asset
      * @param string|null $uri Asset URI to use. Defaults to the filename.
      * @param DateTime|null $dateUpdated last datetime the target of the url was updated, if known
      * @return string
-     * @throws InvalidConfigException
+     * @throws InvalidConfigException if the asset doesn’t have a filename.
      */
     public static function generateUrl(FsInterface $fs, Asset $asset, ?string $uri = null, ?DateTime $dateUpdated = null): string
     {
-        $revParams = self::revParams($asset, $dateUpdated);
         $pathParts = explode('/', $asset->folderPath . ($uri ?? $asset->getFilename()));
         $path = implode('/', array_map('rawurlencode', $pathParts));
-        return UrlHelper::urlWithParams($fs->getRootUrl() . $path, $revParams);
+        $url = sprintf('%s/%s', StringHelper::ensureRight($fs->getRootUrl() ?? '', '/'), $path);
+        return UrlHelper::urlWithParams($url, self::revParams($asset, $dateUpdated));
     }
 
     /**

src/imagetransforms/ImageTransformer.php
@@ -72,35 +72,80 @@ class ImageTransformer extends Component implements ImageTransformerInterface, E
     protected array $imageEditorData = [];
 
     /**
-     * Returns the URL for an image asset transform.
-     *
-     * @return string The URL for the transform
+     * @inheritdoc
      */
     public function getTransformUrl(Asset $asset, ImageTransform $imageTransform, bool $immediately): string
     {
-        if (!$immediately) {
-            return $this->_deferredTransformUrl($asset, $imageTransform);
+        $fs = $asset->getVolume()->getTransformFs();
+        $index = $this->getTransformIndex($asset, $imageTransform);
+        $uri = str_replace('\\', '/', $this->getTransformBasePath($asset)) . $this->getTransformUri($asset, $index);
+
+        // If it's a local filesystem, double-check that the transform exists
+        if ($fs instanceof LocalFsInterface && $index->fileExists && !$fs->fileExists($uri)) {
+            $index->fileExists = false;
+            $this->storeTransformIndexData($index);
         }
 
-        $imageTransformIndex = $this->getTransformIndex($asset, $imageTransform);
+        if (!$index->fileExists) {
+            if (!$immediately) {
+                // Add a Generate Image Transform job to the queue, in case the temp URL never gets requested
+                Queue::push(new GenerateImageTransform([
+                    'transformId' => $index->id,
+                ]));
 
-        if ($imageTransformIndex->fileExists) {
-            $fs = $asset->getVolume()->getTransformFs();
-            $uri = str_replace('\\', '/', $this->getTransformBasePath($asset)) . $this->getTransformUri($asset, $imageTransformIndex);
+                // Return the temporary transform URL
+                return UrlHelper::actionUrl('assets/generate-transform', [
+                    'transformId' => $index->id,
+                ], showScriptName: false);
+            }
 
-            // Check if it really exists
-            if ($fs instanceof LocalFsInterface && !$fs->fileExists($uri)) {
-                $imageTransformIndex->fileExists = false;
-                $this->storeTransformIndexData($imageTransformIndex);
-            } else {
-                return UrlHelper::urlWithParams(
-                    $fs->getRootUrl() . $uri,
-                    AssetsHelper::revParams($asset, $imageTransformIndex->dateUpdated),
-                );
+            // Is the transform being generated by another request?
+            if ($index->inProgress) {
+                for ($try = 1; $try <= 30; $try++) {
+                    if ($index->error) {
+                        throw new ImageTransformException(Craft::t('app', 'Failed to generate transform with id of {id}.', [
+                            'id' => $index->id,
+                        ]));
+                    }
+
+                    // Wait a second and check again
+                    App::maxPowerCaptain();
+                    sleep(1);
+                    $index = $this->getTransformIndexModelById($index->id);
+                    if (!$index->inProgress) {
+                        break;
+                    }
+                }
+            }
+
+            // No file, then
+            if (!$index->fileExists) {
+                // Mark the transform as in progress
+                $index->inProgress = true;
+                $this->storeTransformIndexData($index);
+
+                // Generate the transform
+                try {
+                    $this->generateTransform($index);
+                } catch (Exception $e) {
+                    $index->inProgress = false;
+                    $index->fileExists = false;
+                    $index->error = true;
+                    $this->storeTransformIndexData($index);
+
+                    throw new ImageTransformException(Craft::t('app', 'Failed to generate transform with id of {id}.', [
+                        'id' => $index->id,
+                    ]), previous: $e);
+                }
+
+                $index->inProgress = false;
+                $index->fileExists = true;
+                $this->storeTransformIndexData($index);
             }
         }
 
-        return $this->ensureTransformUrlByIndexModel($asset, $imageTransformIndex);
+        $url = sprintf('%s/%s', sprintf(StringHelper::ensureRight($fs->getRootUrl() ?? '', '/')), $uri);
+        return UrlHelper::urlWithParams($url, AssetsHelper::revParams($asset, $index->dateUpdated));
     }
 
     /**
@@ -139,27 +184,6 @@ class ImageTransformer extends Component implements ImageTransformerInterface, E
         } catch (InvalidConfigException) {
             // nbd
         }
-    }
-
-    /**
-     * @inheritdoc
-     */
-    private function _deferredTransformUrl(Asset $asset, ImageTransform $imageTransform): string
-    {
-        $index = $this->getTransformIndex($asset, $imageTransform);
-
-        // Does the file actually exist?
-        if ($index->fileExists) {
-            return $this->getTransformUrl($asset, $imageTransform, true);
-        }
-
-        // Add a Generate Image Transform job to the queue, in case the temp URL never gets requested
-        Queue::push(new GenerateImageTransform([
-            'transformId' => $index->id,
-        ]));
-
-        // Return the temporary transform URL
-        return UrlHelper::actionUrl('assets/generate-transform', ['transformId' => $index->id], null, false);
     }
 
     /**
@@ -422,8 +446,18 @@ class ImageTransformer extends Component implements ImageTransformerInterface, E
      * @param ImageTransformIndex $index
      * @return bool true if transform exists for the index
      * @throws ImageTransformException
+     * @deprecated in 4.4.0. [[generateTransform()]] should be used instead.
      */
     protected function procureTransformedImage(ImageTransformIndex $index): bool
+    {
+        $this->generateTransform($index);
+        return true;
+    }
+
+    /**
+     * @throws ImageTransformException
+     */
+    private function generateTransform(ImageTransformIndex $index): void
     {
         $asset = Craft::$app->getAssets()->getAssetById($index->assetId);
 
@@ -448,7 +482,7 @@ class ImageTransformer extends Component implements ImageTransformerInterface, E
             // Sanity check
             try {
                 if ($fs->fileExists($target)) {
-                    return true;
+                    return;
                 }
 
                 $fs->copyFile($from, $target);
@@ -459,7 +493,9 @@ class ImageTransformer extends Component implements ImageTransformerInterface, E
             $this->generateTransformedImage($asset, $index);
         }
 
-        return $fs->fileExists($target);
+        if (!$fs->fileExists($target)) {
+            throw new ImageTransformException('There was a problem generating the image transform.');
+        }
     }
 
     /**
@@ -469,74 +505,10 @@ class ImageTransformer extends Component implements ImageTransformerInterface, E
      * @param ImageTransformIndex $index
      * @return string
      * @throws ImageTransformException If there was an error generating the transform.
+     * @deprecated in 4.4.0. [[getTransformUrl()]] should be used instead.
      */
     protected function ensureTransformUrlByIndexModel(Asset $asset, ImageTransformIndex $index): string
     {
-        // Make sure we're not in the middle of working on this transform from a separate request
-        if ($index->inProgress) {
-            for ($safety = 0; $safety < 100; $safety++) {
-                if ($index->error) {
-                    throw new ImageTransformException(Craft::t('app',
-                        'Failed to generate transform with id of {id}.',
-                        ['id' => $index->id]));
-                }
-
-                // Wait a second!
-                sleep(1);
-                App::maxPowerCaptain();
-
-                /** @noinspection CallableParameterUseCaseInTypeContextInspection */
-                $index = $this->getTransformIndexModelById($index->id);
-
-                // Is it being worked on right now?
-                if ($index->inProgress) {
-                    // Make sure it hasn't been working for more than 30 seconds. Otherwise give up on the other request.
-                    $time = new \DateTime();
-
-                    if ($time->getTimestamp() - $index->dateUpdated->getTimestamp() < 30) {
-                        continue;
-                    }
-
-                    $this->storeTransformIndexData($index);
-                    break;
-                }
-
-                // Must be done now!
-                break;
-            }
-        }
-
-        // No file, then
-        if (!$index->fileExists) {
-            // Mark the transform as in progress
-            $index->inProgress = true;
-            $this->storeTransformIndexData($index);
-
-            // Generate the transform
-            try {
-                if ($this->procureTransformedImage($index)) {
-                    // Update the index
-                    $index->inProgress = false;
-                    $index->fileExists = true;
-                } else {
-                    $index->inProgress = false;
-                    $index->fileExists = false;
-                    $index->error = true;
-                }
-                $this->storeTransformIndexData($index);
-            } catch (Exception $e) {
-                $index->inProgress = false;
-                $index->fileExists = false;
-                $index->error = true;
-                $this->storeTransformIndexData($index);
-                Craft::$app->getErrorHandler()->logException($e);
-
-                throw new ImageTransformException(Craft::t('app',
-                    'Failed to generate transform with id of {id}.',
-                    ['id' => $index->id]));
-            }
-        }
-
         return $this->getTransformUrl($asset, $index->getTransform(), true);
     }
 
@@ -599,7 +571,7 @@ class ImageTransformer extends Component implements ImageTransformerInterface, E
         }
 
         // Create a new record
-        $newIndex = new ImageTransformIndex([
+        $index = new ImageTransformIndex([
             'assetId' => $asset->id,
             'format' => $transform->format,
             'transformer' => $transform->getTransformer(),
@@ -607,14 +579,11 @@ class ImageTransformer extends Component implements ImageTransformerInterface, E
             'transformString' => $transformString,
             'fileExists' => false,
             'inProgress' => false,
+            'transform' => $transform,
         ]);
 
-
-        if ($transform instanceof ImageTransform) {
-            $newIndex->setTransform($transform);
-        }
-
-        return $this->storeTransformIndexData($newIndex);
+        $this->storeTransformIndexData($index);
+        return $index;
     }
 
     /**
@@ -684,6 +653,7 @@ class ImageTransformer extends Component implements ImageTransformerInterface, E
             $index->id = (int)$db->getLastInsertID(Table::IMAGETRANSFORMINDEX);
         }
 
+        // todo: this should return void
         return $index;
     }
 

src/models/ImageTransform.php
@@ -77,7 +77,7 @@ class ImageTransform extends Model
     public string $position = 'center-center';
 
     /**
-     * @var 'none'|'line'|'plane'|'partition' Position
+     * @var 'none'|'line'|'plane'|'partition' Interlace
      */
     public string $interlace = 'none';
 

src/models/ImageTransformIndex.php
@@ -97,6 +97,22 @@ class ImageTransformIndex extends Model
     /**
      * @inheritdoc
      */
+    public function init(): void
+    {
+        parent::init();
+
+        // Only respect inProgress if it's been less than 30 seconds since the last time the index was updated
+        if ($this->inProgress) {
+            $duration = time() - ($this->dateUpdated?->getTimestamp() ?? 0);
+            if ($duration > 30) {
+                $this->inProgress = false;
+            }
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
     protected function defineRules(): array
     {
         $rules = parent::defineRules();

commit 8412b1b5778ef2c48caf1cc259c9769d23a00cd6

Fixed #12725
Bug caused by 5850dd3693bef134ec680e99904e2b58805d5295

src/base/Fs.php
@@ -38,7 +38,12 @@ abstract class Fs extends SavableComponent implements FsInterface
             return null;
         }
 
-        return App::parseEnv($this->url) ?: null;
+        $url = App::parseEnv($this->url);
+        if (is_string($url)) {
+            $url = rtrim($url, '/');
+        }
+
+        return $url ? "$url/" : null;
     }
 
     /**