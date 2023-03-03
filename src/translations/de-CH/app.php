0497a41c34daa663b9625c5346af84fbe7b4170e
shinybrad <83238715+shinybrad@users.noreply.github.com>
2023-03-03 09:30:35 -0500

    New translations app.php (German, Switzerland) [ci skip]

--- src/translations/de-CH/app.php
+++ src/translations/de-CH/app.php
@@ -771,7 +771,7 @@ return [
     'License transferred.' => 'Lizenz übertragen.',
     'License' => 'Lizenz',
     'Licensed' => 'Lizenziert',
-    'Licensing Issues' => 'Lizenzierungsprobleme',
+    'Licensing Issues' => 'Probleme mit der Lizenz',
     'Lightswitch' => 'Lichtschalter',
     'Limit the number of selectable category branches.' => 'Begrenzen Sie die Anzahl der auswählbaren Kategoriezweige.',
     'Limit the number of selectable {type}.' => 'Die Anzahl der auswählbaren {type} begrenzen.',
