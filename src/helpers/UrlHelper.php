f84f03a07995332863f74a33d91f0f59c90b7630
Leevi Graham <leevi@newism.com.au>
2023-03-06 14:28:32 +1100

    Fixes #12797

--- src/helpers/UrlHelper.php
+++ src/helpers/UrlHelper.php
@@ -84,7 +84,6 @@ class UrlHelper
         $params = [];
         foreach (explode('&', $query) as $param) {
             [$n, $v] = array_pad(explode('=', $param, 2), 2, '');
-            $n = urldecode($n);
             $v = str_replace(['%2F', '%7B', '%7D'], ['/', '{', '}'], $v);
             $params[] = $v !== '' ? "$n=$v" : $n;
         }
