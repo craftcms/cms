28220d693416dab7e6db68d36ba033ca1a69e792
shinybrad <83238715+shinybrad@users.noreply.github.com>
2023-04-19 19:36:37 -0400

    New translations app.php (French, Canada) [ci skip]

--- src/translations/fr-CA/app.php
+++ src/translations/fr-CA/app.php
@@ -8,7 +8,7 @@ return [
     '/path/to/folder' => '/chemin/vers/dossier',
     '1 job' => '1 tâche',
     '<a>Renew now</a> for another year of updates.' => '<a>Renouvelez dès maintenant</a> pour une autre année de mises à jour.',
-    '<span class="visually-hidden">Characters left:</span> {chars, number}' => '<span class="visually-hidden">Characters left:</span> {chars, number}',
+    '<span class="visually-hidden">Characters left:</span> {chars, number}' => '<span class="visually-hidden">Caractères restants :</span> {chars, number}',
     '<strong>Your license has expired!</strong> Renew your {name} license for another year of amazing updates.' => '<strong>Votre licence a expiré!</strong> Renouvelez votre licence {name} pour une année supplémentaire de mises à jour incroyables.',
     '<strong>You’ve reached a breakpoint!</strong> More updates will become available after you install {update}.' => '<strong>Vous avez atteint un point d’interruption!</strong> Des mises à jour supplémentaires seront disponibles une fois que vous aurez installé {update}.',
     'A critical update is available.' => 'Une mise à jour critique est disponible.',
