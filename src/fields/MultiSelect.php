9c9002dc53d8445e65c2ac0c6a1fcef7e92d683f
Iwona Just <iwona@pixelandtonic.com>
2023-05-10 10:41:16 +0100

    styling of selectize menus

--- src/fields/MultiSelect.php
+++ src/fields/MultiSelect.php
@@ -59,16 +59,6 @@ class MultiSelect extends BaseOptionsField
 
         $id = $this->getInputId();
 
-        $view = Craft::$app->getView();
-        $view->registerJsWithVars(fn($id) => <<<JS
-$('#' + $id).selectize({
-  plugins: ['remove_button'],
-  dropdownParent: 'body',
-});
-JS, [
-            $view->namespaceInputId($id),
-        ]);
-
         return Cp::multiSelectHtml([
             'id' => $id,
             'describedBy' => $this->describedBy,
@@ -80,6 +70,9 @@ JS, [
                 'style' => [
                     'display' => 'none', // Hide it before selectize does its thing
                 ],
+            ],
+            'selectizeOptions' => [
+                'plugins' => ['remove_button'],
             ],
         ]);
     }
