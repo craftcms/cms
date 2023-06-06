2d2fcd1be404ff4ce0410c17a47eeacf5f66fc00
brandonkelly <brandon@pixelandtonic.com>
2023-06-06 11:00:07 -0700

    Ensure all field layout element UUIDs are unique

--- src/migrations/m230511_215903_content_refactor.php
+++ src/migrations/m230511_215903_content_refactor.php
@@ -9,6 +9,7 @@ use craft\db\Table;
 use craft\elements\User;
 use craft\fields\Matrix;
 use craft\services\ProjectConfig;
+use yii\console\Exception;
 
 /**
  * m230511_215903_content_refactor migration.
@@ -20,6 +21,21 @@ class m230511_215903_content_refactor extends BaseContentRefactorMigration
      */
     public function safeUp(): bool
     {
+        // Before anything else, be absolutely certain that all custom fields' layout elements have unique UUIDs
+        $uids = [];
+        $fieldsService = Craft::$app->getFields();
+        foreach ($fieldsService->getAllLayouts() as $fieldLayout) {
+            foreach ($fieldLayout->getCustomFieldElements() as $layoutElement) {
+                if (!isset($layoutElement->uid)) {
+                    throw new Exception('A field layout element is missing its UUID. Reinstall Craft CMS ^4.4.14 and run `utils/fix-field-layout-uids` before upgrading to Craft CMS 5.');
+                }
+                if (isset($uids[$layoutElement->uid])) {
+                    throw new Exception('A field layout element has a duplicate UUID. Reinstall Craft CMS ^4.4.14 and run `utils/fix-field-layout-uids` before upgrading to Craft CMS 5.');
+                }
+                $uids[$layoutElement->uid] = true;
+            }
+        }
+
         if (!$this->db->columnExists(Table::ELEMENTS_SITES, 'title')) {
             $this->addColumn(Table::ELEMENTS_SITES, 'title', $this->string()->after('siteId'));
             $this->addColumn(Table::ELEMENTS_SITES, 'content', $this->json()->after('uri'));
@@ -29,8 +45,6 @@ class m230511_215903_content_refactor extends BaseContentRefactorMigration
         $projectConfig = Craft::$app->getProjectConfig();
         $schemaVersion = $projectConfig->get('system.schemaVersion', true);
         $updateProjectConfig = version_compare($schemaVersion, '5.0.0', '<');
-
-        $fieldsService = Craft::$app->getFields();
 
         // update addresses
         $this->updateElements(
