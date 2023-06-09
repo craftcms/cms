e443fb51a98d79de2f36cfebbe08037d4f72255f
brandonkelly <brandon@pixelandtonic.com>
2023-06-09 08:21:28 -0700

    No need for the columnExists() check

--- src/migrations/m230511_215903_content_refactor.php
+++ src/migrations/m230511_215903_content_refactor.php
@@ -20,11 +20,9 @@ class m230511_215903_content_refactor extends BaseContentRefactorMigration
      */
     public function safeUp(): bool
     {
-        if (!$this->db->columnExists(Table::ELEMENTS_SITES, 'title')) {
-            $this->addColumn(Table::ELEMENTS_SITES, 'title', $this->string()->after('siteId'));
-            $this->addColumn(Table::ELEMENTS_SITES, 'content', $this->json()->after('uri'));
-            $this->createIndex(null, Table::ELEMENTS_SITES, ['title', 'siteId']);
-        }
+        $this->addColumn(Table::ELEMENTS_SITES, 'title', $this->string()->after('siteId'));
+        $this->addColumn(Table::ELEMENTS_SITES, 'content', $this->json()->after('uri'));
+        $this->createIndex(null, Table::ELEMENTS_SITES, ['title', 'siteId']);
 
         $projectConfig = Craft::$app->getProjectConfig();
         $schemaVersion = $projectConfig->get('system.schemaVersion', true);
