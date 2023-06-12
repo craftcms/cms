a7993bcfdba2aa7b9fbe823036a4338e129f4e90
brandonkelly <brandon@pixelandtonic.com>
2023-06-12 14:43:52 -0700

    Cleanup and use $userVariable

--- src/controllers/UsersController.php
+++ src/controllers/UsersController.php
@@ -1561,16 +1561,17 @@ JS,
         $returnCsrfToken = $returnCsrfToken || $loggedIn;
 
         if ($this->request->getAcceptsJson()) {
-            $return = [
-                'id' => $user->id,
-            ];
-            if ($returnCsrfToken && $generalConfig->enableCsrfProtection) {
-                $return['csrfTokenValue'] = $this->request->getCsrfToken();
-            }
-
-            return $this->asModelSuccess($user, Craft::t('app', '{type} saved.', [
-                'type' => User::displayName(),
-            ]), data: $return);
+            return $this->asModelSuccess(
+                $user,
+                Craft::t('app', '{type} saved.', ['type' => User::displayName()]),
+                $userVariable,
+                array_filter([
+                    'id' => $user->id, // todo: remove
+                    'csrfTokenValue' => $returnCsrfToken && $generalConfig->enableCsrfProtection
+                        ? $this->request->getCsrfToken()
+                        : null,
+                ]),
+            );
         }
 
         if ($isPublicRegistration) {
