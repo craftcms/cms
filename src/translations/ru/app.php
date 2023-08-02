2299036414d4e790c26743a7755cdde01ca78541
shinybrad <83238715+shinybrad@users.noreply.github.com>
2023-08-02 13:19:23 -0400

    New translations app.php (Russian) [ci skip]

--- src/translations/ru/app.php
+++ src/translations/ru/app.php
@@ -1286,7 +1286,6 @@ return [
     'The field type “{type}” could not be found.' => 'Поле типа «{type}» не обнаружено.',
     'The file failed to upload to the server properly.' => 'Файл не был загружен на сервер должным образом.',
     'The file “{name}” does not appear to be an image.' => 'Файл «{name}», по видимому, не является изображением.',
-    'The file “{path}” does not appear to be an image.' => 'Вероятно, файл «{path}» не является изображением.',
     'The following <a href="{url}">aliases</a> are defined:' => 'Заданы следующие <a href="{url}">псевдонимы</a>:',
     'The following items were not indexed.' => 'Следующие объекты не были проиндексированы.',
     'The following licensing issues can be resolved with a single purchase on Craft Console.' => 'Чтобы устранить указанные проблемы с лицензированием, необходимо приобрести лицензию в Craft Console.',
