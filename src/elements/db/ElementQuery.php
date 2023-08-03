0eeec5fbec4c4d5cec5a6e76175692c9c384b84d
brandonkelly <brandon@pixelandtonic.com>
2023-08-03 16:22:31 -0700

    Fixed `@event` type
    [ci skip]

--- src/elements/db/ElementQuery.php
+++ src/elements/db/ElementQuery.php
@@ -60,7 +60,7 @@ class ElementQuery extends Query implements ElementQueryInterface
     const EVENT_BEFORE_PREPARE = 'beforePrepare';
 
     /**
-     * @event Event An event that is triggered at the end of preparing an element query for the query builder.
+     * @event CancelableEvent An event that is triggered at the end of preparing an element query for the query builder.
      */
     const EVENT_AFTER_PREPARE = 'afterPrepare';
 
