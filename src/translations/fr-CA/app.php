ef78ac4047cb9b20164598292ce6ffbb1398f6cb
shinybrad <83238715+shinybrad@users.noreply.github.com>
2023-08-08 19:53:56 -0400

    New translations app.php (French, Canada) [ci skip]

--- src/translations/fr-CA/app.php
+++ src/translations/fr-CA/app.php
@@ -1337,6 +1337,7 @@ return [
     'Show the debug toolbar in the control panel' => 'Afficher la barre d’outils de débogage dans le panneau de configuration',
     'Show the debug toolbar on the front end' => 'Afficher la barre d’outils de débogage sur le frontal',
     'Show the site menu' => 'Afficher le menu du site',
+    'Show the Status field' => 'Show the Status field',
     'Show them indefinitely' => 'Les afficher indéfiniment',
     'Show thumbnails and titles' => 'Afficher les vignettes et les titres',
     'Show thumbnails only' => 'Afficher uniquement les vignettes',
