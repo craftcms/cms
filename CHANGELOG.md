dbf0da3f7cfcae87d6a82903392750bba9a4c7da
brandonkelly <brandon@pixelandtonic.com>
2023-08-23 11:34:15 -0700

    Don't let notifications block previously-registered keyboard shortcuts
    Resolves #13591

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 ## Unreleased
 
+- Control panel notifications no longer block page keyboard shortcuts. ([#13591](https://github.com/craftcms/cms/issues/13591))
+- `Garnish.uiLayerManager.addLayer()` now supports a `bubble` option, which allows non-matching keyboard shortcuts to bubble up to the parent layer.
 - Fixed an error that could occur when Craft was performing a Composer operation, if no `HOME` environment variable was set for PHP. ([#13590](https://github.com/craftcms/cms/issues/13590))
 - Fixed a bug where `craft\fields\Matrix::serializeValue()` was setting `fields` keys to a closure. ([#13592](https://github.com/craftcms/cms/issues/13592))
 
--- src/web/assets/garnish/src/UiLayerManager.js
+++ src/web/assets/garnish/src/UiLayerManager.js
@@ -40,16 +40,31 @@ export default Base.extend({
    * Registers a new UI layer.
    *
    * @param {jQuery|HTMLElement} [container]
+   * @param {Object} [options]
    */
-  addLayer: function (container) {
+  addLayer: function (container, options) {
+    if ($.isPlainObject(container)) {
+      options = container;
+      container = null;
+    }
+
+    options = Object.assign(
+      {
+        bubble: false,
+      },
+      options || {}
+    );
+
     this.layers.push({
       $container: container ? $(container) : null,
       shortcuts: [],
       isModal: container ? $(container).attr('aria-modal') === 'true' : false,
+      options: options,
     });
     this.trigger('addLayer', {
       layer: this.layer,
       $container: this.currentLayer.$container,
+      options: options,
     });
     return this;
   },
@@ -72,14 +87,11 @@ export default Base.extend({
   },
 
   getLayerIndex: function (layer) {
-    const $layer = $(layer);
+    layer = $(layer).get(0);
     let layerIndex;
 
     $(this.layers).each(function (index) {
-      if (
-        this.$container !== null &&
-        this.$container.get(0) === $(layer).get(0)
-      ) {
+      if (this.$container !== null && this.$container.get(0) === layer) {
         layerIndex = index;
         return false;
       }
@@ -137,8 +149,12 @@ export default Base.extend({
     };
   },
 
-  triggerShortcut: function (ev) {
-    const shortcut = this.layers[this.layer].shortcuts.find(
+  triggerShortcut: function (ev, layerIndex) {
+    if (typeof layerIndex === 'undefined') {
+      layerIndex = this.layer;
+    }
+    const layer = this.layers[layerIndex];
+    const shortcut = layer.shortcuts.find(
       (s) =>
         s.shortcut.keyCode === ev.keyCode &&
         s.shortcut.ctrl === Garnish.isCtrlKeyPressed(ev) &&
@@ -149,6 +165,8 @@ export default Base.extend({
     if (shortcut) {
       ev.preventDefault();
       shortcut.callback(ev);
+    } else if (layer.options.bubble && layerIndex > 0) {
+      this.triggerShortcut(ev, layerIndex - 1);
     }
   },
 });
