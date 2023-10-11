7fb2feaf9168029e6e2ae371618f383205a4cc5b
shinybrad <83238715+shinybrad@users.noreply.github.com>
2023-10-10 20:27:34 -0400

    New translations app.php (Slovak) [ci skip]

--- src/translations/sk/app.php
+++ src/translations/sk/app.php
@@ -868,6 +868,7 @@ return [
     'Manage relations on a per-site basis' => 'Spravovať vzťahy podľa konkrétnych webov',
     'Manage your Craft Console account' => 'Manage your Craft Console account',
     'Manipulated SVG image rasterizing is unreliable. See \\craft\\services\\Images::loadImage()' => 'Rastrovanie upraveného obrázka SVG je nespoľahlivé. Pozrite \\craft\\services\\Images::loadImage()',
+    'Matrix field' => 'Matrix field',
     'Matrix' => 'Matrix',
     'Max Date' => 'Maximálny dátum',
     'Max Length' => 'Max. dĺžka',
