870a9b486c5d257006aeee7332d157aeddb45d44
shinybrad <83238715+shinybrad@users.noreply.github.com>
2023-10-10 20:27:15 -0400

    New translations app.php (Arabic) [ci skip]

--- src/translations/ar/app.php
+++ src/translations/ar/app.php
@@ -868,6 +868,7 @@ return [
     'Manage relations on a per-site basis' => 'إدارة العلاقات على أساس الموقع الواحد',
     'Manage your Craft Console account' => 'Manage your Craft Console account',
     'Manipulated SVG image rasterizing is unreliable. See \\craft\\services\\Images::loadImage()' => 'تحويل الصور الخطية إلى نقطية الذي تمت معالجته لصورة SVG غير موثوق فيه. راجع \\craft\\services\\Images::loadImage()',
+    'Matrix field' => 'Matrix field',
     'Matrix' => 'مصفوفة',
     'Max Date' => 'تاريخ الحد الأقصى',
     'Max Length' => 'الحد الأقصى للطول',
