79d2b3e7f4f6d8d482e848e2be01de71915c95dc
brandonkelly <brandon@pixelandtonic.com>
2023-10-16 11:11:58 -0700

    Fixed defaults

--- tests/_craft/config/db.php
+++ tests/_craft/config/db.php
@@ -12,6 +12,6 @@ return [
     'password' => App::env('DB_PASSWORD'),
     'schema' => App::env('DB_SCHEMA'),
     'tablePrefix' => App::env('DB_TABLE_PREFIX'),
-    'charset' => App::env('DB_CHARSET') ?? 'utf8',
-    'collation' => App::env('DB_COLLATION'),
+    'charset' => App::env('DB_CHARSET') ?: 'utf8',
+    'collation' => App::env('DB_COLLATION') ?: null,
 ];
