10f835cdd1e7430ef72dfcb1bb0c6fe50c536c91
brandonkelly <brandon@pixelandtonic.com>
2023-10-16 11:00:19 -0700

    Recommend utf8mb3 for MySQL tests

--- tests/unit/db/QueryBuilderTest.php
+++ tests/unit/db/QueryBuilderTest.php
@@ -34,7 +34,16 @@ class QueryBuilderTest extends Unit
             $this->markTestSkipped('MySQL only');
         }
 
+        $dbConfig = Craft::$app->getConfig()->getDb();
+        $charset = $dbConfig->charset;
+        $collation = $dbConfig->collation;
+        $dbConfig->charset = 'utf8';
+        $dbConfig->collation = null;
+
         $sql = $db->getQueryBuilder()->createTable('foo', ['id' => Schema::TYPE_PK], $options);
+
+        $dbConfig->charset = $charset;
+        $dbConfig->collation = $collation;
 
         if ($contains) {
             $this->assertStringContainsString($needle, $sql);
