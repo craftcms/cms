13fa6e45d10586177307dfcef4dc28220e72dbb2
shinybrad <83238715+shinybrad@users.noreply.github.com>
2023-10-20 15:08:05 -0400

    New translations app.php (Czech) [ci skip]

--- src/translations/cs/app.php
+++ src/translations/cs/app.php
@@ -1846,6 +1846,7 @@ return [
     'Your system isn’t set up to save content for the site “{site}”.' => 'Váš systém není u webu „{site}“ nastaven na ukládání obsahu.',
     'Your {folder} folder isn’t writable.' => 'Do vaší složky {folder} nelze zapisovat.',
     'You’re all up to date!' => 'Vše je aktuální!',
+    'You’re viewing a revision. None of the {type}’s fields are editable.' => 'You’re viewing a revision. None of the {type}’s fields are editable.',
     'Zip Code' => 'PSČ',
     'account_activation_body' => "Ahoj {{user.friendlyName|e}},\n\nděkujeme za vytvoření uživatelského účtu na webu {{siteName}}! Pro aktivaci svého účtu prosím otevři následující odkaz:\n\n<{{link}}>\n\nPokud nevíte, o co se jedná, klidně tuto zprávu ignorujte.",
     'account_activation_heading' => 'Když někdo vytvoří účet:',
