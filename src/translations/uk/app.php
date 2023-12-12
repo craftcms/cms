6be06f40b8ce54dabb1d0580f2f21b783b957f4d
shinybrad <83238715+shinybrad@users.noreply.github.com>
2023-12-12 02:30:40 -0500

    New translations app.php (Ukrainian) [ci skip]

--- src/translations/uk/app.php
+++ src/translations/uk/app.php
@@ -1331,6 +1331,7 @@ return [
     'See all' => 'Переглянути все',
     'See logs' => 'Див. журнали',
     'Select All' => 'Вибрати все',
+    'Select a filesystem' => 'Select a filesystem',
     'Select a volume' => 'Виберіть том',
     'Select all' => 'Вибрати все',
     'Select context' => 'Select context',
