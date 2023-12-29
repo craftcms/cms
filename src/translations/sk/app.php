4b264e91ef4473a83b149c665dad6e13449fde7c
shinybrad <83238715+shinybrad@users.noreply.github.com>
2023-12-29 07:39:52 -0500

    New translations app.php (Slovak) [ci skip]

--- src/translations/sk/app.php
+++ src/translations/sk/app.php
@@ -269,7 +269,7 @@ return [
     'Choose the available content for querying with this schema:' => 'Vyberte dostupný obsah pre dotazovanie pomocou tejto schémy:',
     'Choose the available mutations for this schema:' => 'Vyberte dostupné mutácie pre túto schému:',
     'Choose the site-specific settings for nested entries.' => 'Choose the site-specific settings for nested entries.',
-    'Choose the type of entry this section’s entry will have.' => 'Choose the type of entry this section’s entry will have.',
+    'Choose the type of entry this section will have.' => 'Choose the type of entry this section will have.',
     'Choose the types of entries that can be created in this field.' => 'Choose the types of entries that can be created in this field.',
     'Choose the types of entries that can be included in this field.' => 'Choose the types of entries that can be included in this field.',
     'Choose the types of entries that can be included in this section.' => 'Choose the types of entries that can be included in this section.',
