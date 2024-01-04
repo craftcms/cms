d2e95803eed0878efbd687432db2d3e21f9047ef
brandonkelly <brandon@pixelandtonic.com>
2024-01-03 16:08:44 -0800

    Always establish a second connection

--- src/db/NonTransactionalConnection.php
+++ src/db/NonTransactionalConnection.php
@@ -8,9 +8,6 @@
 namespace craft\db;
 
 use yii\base\NotSupportedException;
-use yii\db\Command as YiiCommand;
-use yii\db\Connection as YiiConnection;
-use yii\di\Instance;
 
 /**
  * Non-transactional database connection.
@@ -20,37 +17,16 @@ use yii\di\Instance;
  */
 class NonTransactionalConnection extends Connection
 {
-    public YiiConnection|string $primaryDb = 'db';
-
-    /**
-     * @inheritdoc
-     */
-    public function init()
-    {
-        $this->primaryDb = Instance::ensure($this->primaryDb, YiiConnection::class);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function createCommand($sql = null, $params = []): YiiCommand
-    {
-        // stick with the primary connection as long as we can
-        if (!$this->getIsActive() && $this->primaryDb->getTransaction() === null) {
-            return $this->primaryDb->useMaster(
-                fn() => $this->primaryDb->createCommand($sql, $params),
-            );
-        }
-
-        return parent::createCommand($sql, $params);
-    }
-
     /**
      * @inheritdoc
      * @throws NotSupportedException
      */
     public function beginTransaction($isolationLevel = null)
     {
-        throw new NotSupportedException(sprintf('Transactions aren’t supported by %s', __CLASS__));
+        if ($this->getTransaction() !== null) {
+            throw new NotSupportedException(sprintf('Nested transactions aren’t supported by %s', __CLASS__));
+        }
+
+        return parent::beginTransaction($isolationLevel);
     }
 }
