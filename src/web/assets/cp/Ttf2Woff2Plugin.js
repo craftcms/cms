dd6e88eb28f7db1e5bbc5333b8fb40354102c717
Brian Hanson <brian@brianhanson.net>
2024-01-16 11:39:38 -0600

    Cleanup

--- src/web/assets/cp/Ttf2Woff2Plugin.js
+++ src/web/assets/cp/Ttf2Woff2Plugin.js
@@ -3,26 +3,28 @@ const fs = require('fs');
 const tff2woff2 = require('ttf2woff2');
 
 class Ttf2Woff2Plugin {
+  static defaultOptions = {
+    src: './src/fonts',
+  };
+
   constructor(options = {}) {
     this.options = {
-      src: './src/fonts',
-      dest: './dist/fonts',
+      ...Ttf2Woff2Plugin.defaultOptions,
       ...options,
     };
 
     this.src = path.resolve(__dirname, this.options.src);
-    this.dest = path.resolve(__dirname, this.options.dest);
   }
 
   apply(compiler) {
-    compiler.hooks.done.tap('Ttf2Woff2Plugin', () => {
+    compiler.hooks.beforeRun.tap('Ttf2Woff2Plugin', () => {
       const files = fs.readdirSync(this.src);
 
       files.forEach((file) => {
         if (path.extname(file) === '.ttf') {
           const input = fs.readFileSync(path.resolve(this.src, file));
           fs.writeFileSync(
-            path.resolve(this.dest, file.replace('.ttf', '.woff2')),
+            path.resolve(this.src, file.replace('.ttf', '.woff2')),
             tff2woff2(input)
           );
         }
