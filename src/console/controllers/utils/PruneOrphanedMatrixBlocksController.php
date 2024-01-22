b9498242cee14b661191d480d465a03391b204c0
brandonkelly <brandon@pixelandtonic.com>
2024-01-22 12:47:50 -0800

    Prune command cleanup

--- src/console/controllers/utils/PruneOrphanedMatrixBlocksController.php
+++ src/console/controllers/utils/PruneOrphanedMatrixBlocksController.php
@@ -30,45 +30,51 @@ class PruneOrphanedMatrixBlocksController extends Controller
      */
     public function actionIndex(): int
     {
-        if (Craft::$app->getIsMultiSite()) {
-            // get all sites
-            $sites = Craft::$app->getSites()->getAllSites();
-
-            // for each site get all matrix blocks with owner that doesn't exist for this site
-            foreach ($sites as $site) {
-                $this->stdout('Finding orphaned matrix blocks for site `' . $site->getName() . '`... ' . PHP_EOL);
-
-                $esSubQuery = (new Query())
-                    ->from(['es' => Table::ELEMENTS_SITES])
-                    ->where([
-                        'and',
-                        '[[es.elementId]] = [[matrixblocks.primaryOwnerId]]',
-                        ['es.siteId' => $site->id],
-                    ]);
-
-                $matrixBlocks = MatrixBlock::find()
-                    ->status(null)
-                    ->siteId($site->id)
-                    ->where(['not exists', $esSubQuery])
-                    ->all();
-
-                $this->stdout(' Found ' . count($matrixBlocks) . ' orphaned blocks' . PHP_EOL, Console::FG_RED);
-
-                $elementsService = Craft::$app->getElements();
-
-                // delete the ones we found
-                foreach ($matrixBlocks as $block) {
-                    $block->siteId = $site->id;
-                    $block->deletedWithOwner = false;
-                    $elementsService->deleteElementForSite($block);
-                }
-            }
-
-            $this->stdout(PHP_EOL . 'Finished pruning orphaned matrix blocks' . PHP_EOL . PHP_EOL, Console::FG_GREEN);
-        } else {
-            $this->stdout('Nothing to prune' . PHP_EOL . PHP_EOL, Console::FG_GREEN);
+        if (!Craft::$app->getIsMultiSite()) {
+            $this->stdout("This command should only be run for multi-site installs.\n", Console::FG_YELLOW);
+            return ExitCode::OK;
         }
 
+        $elementsService = Craft::$app->getElements();
+
+        // get all sites
+        $sites = Craft::$app->getSites()->getAllSites();
+
+        // for each site get all matrix blocks with owner that doesn't exist for this site
+        foreach ($sites as $site) {
+            $this->stdout(sprintf('Finding orphaned matrix blocks for site "%s" ... ', $site->getName()));
+
+            $esSubQuery = (new Query())
+                ->from(['es' => Table::ELEMENTS_SITES])
+                ->where([
+                    'and',
+                    '[[es.elementId]] = [[matrixblocks.primaryOwnerId]]',
+                    ['es.siteId' => $site->id],
+                ]);
+
+            $matrixBlocks = MatrixBlock::find()
+                ->status(null)
+                ->siteId($site->id)
+                ->where(['not exists', $esSubQuery])
+                ->all();
+
+            if (empty($matrixBlocks)) {
+                $this->stdout("none found\n", Console::FG_GREEN);
+                continue;
+            }
+
+            $this->stdout(sprintf("%s found\n", count($matrixBlocks)), Console::FG_RED);
+
+            // delete the ones we found
+            foreach ($matrixBlocks as $block) {
+                $this->do(sprintf('Deleting block %s in %s', $block->id, $site->getName()), function() use ($block, $elementsService) {
+                    $block->deletedWithOwner = false;
+                    $elementsService->deleteElementForSite($block);
+                });
+            }
+        }
+
+        $this->stdout("\nFinished pruning orphaned Matrix blocks.\n", Console::FG_GREEN);
         return ExitCode::OK;
     }
 }
