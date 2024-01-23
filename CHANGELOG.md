2bcd199fce87ea10abff4b00eba90af29c75d37e
brandonkelly <brandon@pixelandtonic.com>
2024-01-23 15:46:29 -0800

    Finish 5.0.0-alpha.8

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Release Notes for Craft CMS 5
 
-## Unreleased
+## 5.0.0-alpha.8 - 2024-01-23
 
 - Fixed a PHP error that occurred when the Debug Toolbar was enabled. ([#14183](https://github.com/craftcms/cms/issues/14183))
 
--- src/config/app.php
+++ src/config/app.php
@@ -3,7 +3,7 @@
 return [
     'id' => 'CraftCMS',
     'name' => 'Craft CMS',
-    'version' => '5.0.0-alpha.7',
+    'version' => '5.0.0-alpha.8',
     'schemaVersion' => '5.0.0.15',
     'minVersionRequired' => '4.4.0',
     'basePath' => dirname(__DIR__), // Defines the @app alias
