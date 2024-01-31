0c343d4ff365989dd68a9dd1b4c8ef6f6920d878
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-01-30 21:31:12 -0500

    New translations app.php (Slovak) [ci skip]

--- src/translations/sk/app.php
+++ src/translations/sk/app.php
@@ -537,6 +537,7 @@ return [
     'Device type' => 'Typ zariadenia',
     'Dimensions' => 'Rozmery',
     'Directories cannot be deleted while moving assets.' => 'Adresáre nie je možné odstrániť počas presunu prostriedkov.',
+    'Disable autofocus' => 'Disable autofocus',
     'Disable focal point' => 'Zakázať ohniskový bod',
     'Disable' => 'Zakázať',
     'Disabled' => 'Zakázané',
