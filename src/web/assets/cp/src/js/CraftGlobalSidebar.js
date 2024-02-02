63d3c0cd655559c551fb78fb4378a4d225bc653e
Brian Hanson <brian@brianhanson.net>
2024-02-02 11:56:25 -0600

    New sidebar styling

---
+++ src/web/assets/cp/src/js/CraftGlobalSidebar.js
@@ -0,0 +1,27 @@
+class CraftGlobalSidebar extends HTMLElement {
+  connectedCallback() {
+    this.trigger = this.querySelector('#sidebar-trigger');
+
+    if (this.trigger) {
+      this.trigger.addEventListener('click', this.toggle.bind(this));
+    }
+  }
+
+  toggle() {
+    if (document.body.getAttribute('data-sidebar') === 'expanded') {
+      this.collapse();
+    } else {
+      this.expand();
+    }
+  }
+
+  expand() {
+    document.body.setAttribute('data-sidebar', 'expanded');
+  }
+
+  collapse() {
+    document.body.setAttribute('data-sidebar', 'collapsed');
+  }
+}
+
+customElements.define('craft-global-sidebar', CraftGlobalSidebar);
