905759eff0bf4eb65d3ba33fa65f0f9c29e879d5
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-02-16 23:18:58 -0500

    New translations app.php (French) [ci skip]

--- src/translations/fr/app.php
+++ src/translations/fr/app.php
@@ -61,6 +61,7 @@ return [
     'Add an option' => 'Ajouter une option',
     'Add to cart' => 'Ajouter au panier',
     'Add {type} above' => 'Ajouter {type} au-dessus',
+    'Add' => 'Add',
     'Added to cart' => 'Ajouté au panier',
     'Address Fields' => 'Champs d’adresse',
     'Address Line 1' => 'Adresse ligne 1',
