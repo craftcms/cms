4b5ecf268834209396dabbcd071f4c247b48b2ab
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-02-19 16:11:29 -0500

    New translations app.php (Thai) [ci skip]

--- src/translations/th/app.php
+++ src/translations/th/app.php
@@ -1739,7 +1739,7 @@ return [
     'Unlock' => 'Unlock',
     'Unread announcements' => 'ประกาศที่ไม่ได้อ่าน',
     'Unsuspend' => 'Unsuspend',
-    'Untitled entry' => 'Untitled entry',
+    'Untitled {type}' => 'Untitled {type}',
     'Update YAML Files' => 'Update YAML Files',
     'Update YAML files' => 'Update YAML files',
     'Update aborted.' => 'Update aborted.',
