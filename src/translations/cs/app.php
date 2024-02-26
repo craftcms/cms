e6b768ae9b0b487e57187ddb96b14e2f75913964
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-02-26 01:22:08 -0500

    New translations app.php (Czech) [ci skip]

--- src/translations/cs/app.php
+++ src/translations/cs/app.php
@@ -1724,6 +1724,7 @@ return [
     'URL Format' => 'Formát URL',
     'URL type' => 'Typ URL',
     'URL' => 'URL',
+    'Unable to fetch updates at this time.' => 'Unable to fetch updates at this time.',
     'Unable to fetch upgrade info at this time.' => 'V tuto chvíli nelze načíst informace o aktualizaci.',
     'Unable to find the template “{template}”.' => 'Nepodařilo se najít šablonu "{template}".',
     'Unauthorized' => 'Neautorizován',
