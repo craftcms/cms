c0d76b1f41df769a4ba3e7848f5d9dd3b1d0b113
Brad Bell <brad@pixelandtonic.com>
2024-03-04 14:34:10 -0800

    Fix a potential SSTI vulnerability

--- src/web/twig/Extension.php
+++ src/web/twig/Extension.php
@@ -1632,6 +1632,8 @@ class Extension extends AbstractExtension implements GlobalsInterface
             'system',
             'passthru',
             'exec',
+            'file_get_contents',
+            'file_put_contents',
         ])) {
             throw new RuntimeError('Not supported in this filter.');
         }
