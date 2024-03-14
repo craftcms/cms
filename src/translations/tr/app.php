00498194bc4956ac170a2f1f19d38b0297f3addd
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-03-14 00:54:19 -0400

    New translations app.php (Turkish) [ci skip]

--- src/translations/tr/app.php
+++ src/translations/tr/app.php
@@ -1589,7 +1589,7 @@ return [
     'The table name prefix' => 'Tablo adı ön eki',
     'The template Craft CMS will use for HTML emails' => 'Craft CMS\'in HTML epostaları için kullanacağı şablon',
     'The template will be rendered with an `element` variable.' => 'Şablon bir `element` değişkeni ile oluşturulur.',
-    'The text label for entry creation buttons.' => 'The text label for entry creation buttons.',
+    'The text label for the entry creation button.' => 'The text label for the entry creation button.',
     'The text label for {type} selection buttons.' => 'The text label for {type} selection buttons.',
     'The text that will be shown if the field doesn’t have a value.' => 'Alanın bir değeri yoksa gösterilecek olan metin.',
     'The total number of {type} to display per page within the element index.' => 'The total number of {type} to display per page within the element index.',
