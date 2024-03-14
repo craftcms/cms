721454396b1d14d0e812f659c88dc3e156c700bd
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-03-14 00:54:01 -0400

    New translations app.php (Danish) [ci skip]

--- src/translations/da/app.php
+++ src/translations/da/app.php
@@ -1589,7 +1589,7 @@ return [
     'The table name prefix' => 'Tabelnavnets præfiks',
     'The template Craft CMS will use for HTML emails' => 'Skabelonen som Craft CMS vil bruge til HTML e-mails',
     'The template will be rendered with an `element` variable.' => 'Skabelonen gengives med en `element`-varibel.',
-    'The text label for entry creation buttons.' => 'The text label for entry creation buttons.',
+    'The text label for the entry creation button.' => 'The text label for the entry creation button.',
     'The text label for {type} selection buttons.' => 'The text label for {type} selection buttons.',
     'The text that will be shown if the field doesn’t have a value.' => 'Teksten der vil blive vist hvis felten ikke har en værdi.',
     'The total number of {type} to display per page within the element index.' => 'The total number of {type} to display per page within the element index.',
