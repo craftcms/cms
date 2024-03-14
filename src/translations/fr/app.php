a27bb06f080248f04fbffeed60f805e7fbf56068
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-03-14 00:53:56 -0400

    New translations app.php (French) [ci skip]

--- src/translations/fr/app.php
+++ src/translations/fr/app.php
@@ -1589,7 +1589,7 @@ return [
     'The table name prefix' => 'Le préfixe des noms de tables',
     'The template Craft CMS will use for HTML emails' => 'Le template qu’utilisera Craft CMS pour les emails en HTML',
     'The template will be rendered with an `element` variable.' => 'Le template sera rendu avec une variable `element`.',
-    'The text label for entry creation buttons.' => 'The text label for entry creation buttons.',
+    'The text label for the entry creation button.' => 'The text label for the entry creation button.',
     'The text label for {type} selection buttons.' => 'The text label for {type} selection buttons.',
     'The text that will be shown if the field doesn’t have a value.' => 'Le texte qui sera affiché si le champ ne contient pas de valeur.',
     'The total number of {type} to display per page within the element index.' => 'The total number of {type} to display per page within the element index.',
