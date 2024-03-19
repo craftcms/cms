f3a296bfa007a22b4c6fe4affaef6cacf201dff4
brandonkelly <brandon@pixelandtonic.com>
2024-03-19 09:53:25 -0700

    Finish 3.9.12

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Release Notes for Craft CMS 3.x
 
-## Unreleased
+## 3.9.12 - 2024-03-19
 
 - Craft no longer shows an alert when an installed Craft/plugin edition is a lower tier than the licensed edition.
 
--- src/config/app.php
+++ src/config/app.php
@@ -3,7 +3,7 @@
 return [
     'id' => 'CraftCMS',
     'name' => 'Craft CMS',
-    'version' => '3.9.11',
+    'version' => '3.9.12',
     'schemaVersion' => '3.8.0.1',
     'minVersionRequired' => '2.6.2788',
     'basePath' => dirname(__DIR__), // Defines the @app alias
