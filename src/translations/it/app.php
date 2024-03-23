34cd813c7be631697504f70b7f5e8d03e2b62a19
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-03-23 19:09:23 -0400

    New translations app.php (Italian) [ci skip]

--- src/translations/it/app.php
+++ src/translations/it/app.php
@@ -1636,7 +1636,7 @@ return [
     'URL Format' => 'Formato URL',
     'URL type' => 'Tipo URL',
     'URL' => 'URL',
-    'Unable to fetch updates at this time.' => 'Unable to fetch updates at this time.',
+    'Unable to fetch updates at this time.' => 'Impossibile recuperare gli aggiornamenti al momento.',
     'Unable to fetch upgrade info at this time.' => 'Impossibile recuperare le info di aggiornamento al momento.',
     'Unable to find the template “{template}”.' => 'Impossibile trovare il template “{template}”.',
     'Unauthorized' => 'Non autorizzato',
