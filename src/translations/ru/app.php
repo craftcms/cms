2cca13a33b511a7b23186b5940e19871c75fc70f
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-04-12 17:20:05 -0700

    New translations app.php (Russian) [ci skip]

--- src/translations/ru/app.php
+++ src/translations/ru/app.php
@@ -1983,7 +1983,7 @@ return [
     '{num, number} {num, plural, =1{minute} other{minutes}}' => '{num, number} {num, plural, =1{минута} other{минуты}}',
     '{num, number} {num, plural, =1{month} other{months}}' => '{num, number} {num, plural, =1{месяц} other{месяцы}}
 ',
-    '{num, number} {num, plural, =1{notification} other{notifications}}' => '{num, number} {num, plural, one {уведомление} few {уведомления} many {уведомлений} other{уведомления}}',
+    '{num, number} {num, plural, =1{notification} other{notifications}}' => '{num, number} {num, plural, =1{notification} other{notifications}}',
     '{num, number} {num, plural, =1{second} other{seconds}}' => '{num, number} {num, plural, =1{секунда} other{секунды}}',
     '{num, number} {num, plural, =1{week} other{weeks}}' => '{num, number} {num, plural, =1{неделя} other{недели}}',
     '{num, number} {num, plural, =1{year} other{years}} of updates' => '{num, number} {num, plural, one {} few {года} many {лет}=1{год} other{года}} обновлений',
