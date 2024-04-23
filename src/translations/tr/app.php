eba77839b879f6928b195e85f868a27b32955970
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-04-23 11:09:33 -0700

    New translations app.php (Turkish) [ci skip]

--- src/translations/tr/app.php
+++ src/translations/tr/app.php
@@ -2014,6 +2014,7 @@ return [
     'on {date}' => '{date}',
     'on {day}' => '{day}',
     'one' => 'bir',
+    'or <a href="{url}">try before buying</a>' => 'or <a href="{url}">try before buying</a>',
     'or <a>send to Developer Support</a>' => 'veya <a>Geliştirici Destek Bölümü\'ne gönderin</a>',
     'path/to/subfolder' => 'altklasore/giden/yol',
     'revision' => 'revizyon',
