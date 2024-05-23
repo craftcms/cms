750bc0045110fd8be9c2bedc28c5826f53126299
Iwona Just <iwona@pixelandtonic.com>
2024-05-23 13:48:42 +0100

    allow entries and product variants in related to rules

--- src/elements/conditions/RelatedToConditionRule.php
+++ src/elements/conditions/RelatedToConditionRule.php
@@ -5,9 +5,9 @@ namespace craft\elements\conditions;
 use Craft;
 use craft\base\conditions\BaseElementSelectConditionRule;
 use craft\base\ElementInterface;
-use craft\base\NestedElementInterface;
 use craft\elements\db\ElementQueryInterface;
 use craft\elements\Entry;
+use craft\fields\BaseRelationField;
 use craft\helpers\Cp;
 use craft\helpers\Html;
 use craft\helpers\UrlHelper;
@@ -94,15 +94,13 @@ class RelatedToConditionRule extends BaseElementSelectConditionRule implements E
     private function _elementTypeOptions(): array
     {
         $options = [];
-        foreach (Craft::$app->getElements()->getAllElementTypes() as $elementType) {
-            /** @var string|ElementInterface $elementType */
-            /** @phpstan-var class-string<ElementInterface>|ElementInterface $elementType */
-            if (!is_subclass_of($elementType, NestedElementInterface::class)) {
-                $options[] = [
-                    'value' => $elementType,
-                    'label' => $elementType::displayName(),
-                ];
-            }
+        /** @var string|BaseRelationField $field */
+        foreach (Craft::$app->getFields()->getAllRelationalFieldTypes() as $field) {
+            $elementType = $field::elementType();
+            $options[] = [
+                'value' => $elementType,
+                'label' => $elementType::displayName(),
+            ];
         }
         return $options;
     }
