df8ed8aa256f07b673a832571b08a27fafd5f5eb
brandonkelly <brandon@pixelandtonic.com>
2024-06-11 13:52:08 -0700

    Clear `errors` class when refreshing an element chip/card's HTML
    [ci skip]

--- src/web/assets/cp/src/js/Craft.js
+++ src/web/assets/cp/src/js/Craft.js
@@ -2412,7 +2412,7 @@ $.extend(Craft, {
             )
             .detach();
           const $inputs = $element.find('input,button').detach();
-          $element.html($replacement.html());
+          $element.html($replacement.html()).removeClass('error');
 
           if ($actions.length) {
             const $oldStatus = $actions.find('span.status');
