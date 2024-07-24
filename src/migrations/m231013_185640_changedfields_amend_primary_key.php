ed3485588d9459801576eb1a632d7e8eab2d2775
Kenneth Ormandy <hello@kennethormandy.com>
2024-07-24 11:41:43 -0700

    Fixes check for primary key name in Postgres

--- src/migrations/m231013_185640_changedfields_amend_primary_key.php
+++ src/migrations/m231013_185640_changedfields_amend_primary_key.php
@@ -19,7 +19,7 @@ class m231013_185640_changedfields_amend_primary_key extends Migration
             $this->dropPrimaryKey('elementId', Table::CHANGEDFIELDS);
             $this->addPrimaryKey('layoutUid', Table::CHANGEDFIELDS, ['elementId', 'siteId', 'fieldId', 'layoutElementUid']);
         } else {
-            $pkName = $this->db->getSchema()->getRawTableName(Table::CHANGEDFIELDS) . '_pkey';
+            $pkName = $this->db->getSchema()->getTablePrimaryKey(Table::CHANGEDFIELDS)->name;
 
             $this->dropPrimaryKey($pkName, Table::CHANGEDFIELDS);
             $this->addPrimaryKey($pkName, Table::CHANGEDFIELDS, ['elementId', 'siteId', 'fieldId', 'layoutElementUid']);
