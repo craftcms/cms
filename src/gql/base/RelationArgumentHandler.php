213f6e5fda8c232125c3de0c7dabecb6973e5718
brandonkelly <brandon@pixelandtonic.com>
2024-08-08 14:50:45 -0700

    relatedViaField & relatedViaSite

--- src/gql/base/RelationArgumentHandler.php
+++ src/gql/base/RelationArgumentHandler.php
@@ -37,6 +37,7 @@ abstract class RelationArgumentHandler extends ArgumentHandler
         $idSets = [];
 
         foreach ($criteriaList as $criteria) {
+            unset($criteria['relatedViaField'], $criteria['relatedViaSite']);
             /** @var ElementQuery $elementQuery */
             $elementQuery = Craft::configure(Craft::$app->getElements()->createElementQuery($elementType), $criteria);
             $idSets[] = $elementQuery->ids();
@@ -55,6 +56,16 @@ abstract class RelationArgumentHandler extends ArgumentHandler
         }
 
         $argumentValue = $argumentList[$this->argumentName];
+
+        // Extract relatedViaField and relatedViaSite values
+        $relationParams = [];
+        foreach ($argumentValue as &$value) {
+            $relationParams[] = [
+                'field' => ArrayHelper::remove($value, 'relatedViaField'),
+                'site' => ArrayHelper::remove($value, 'relatedViaSite'),
+            ];
+        }
+
         $hash = md5(serialize($argumentValue));
 
         // See if we have done something exactly like this already.
@@ -76,7 +87,8 @@ abstract class RelationArgumentHandler extends ArgumentHandler
         }
 
         foreach ($idSets as $idSet) {
-            $relatedTo[] = ['element' => $idSet];
+            $relationParams = array_shift($relationParams) ?? [];
+            $relatedTo[] = ['element' => $idSet] + $relationParams;
         }
 
         $argumentList['relatedTo'] = $relatedTo;
