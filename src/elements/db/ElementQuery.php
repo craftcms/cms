5b71f0caaab693e13c65de1b99f3c71b6f0ccfe7
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2024-08-15 14:48:18 +0100

    Fix potentially criteria values

--- src/elements/db/ElementQuery.php
+++ src/elements/db/ElementQuery.php
@@ -1146,10 +1146,10 @@ class ElementQuery extends Query implements ElementQueryInterface
     /**
      * @param $value
      * @param $currentValue
-     * @return array|false
+     * @return array|string|false
      * @throws NotSupportedException
      */
-    private function _andRelatedToCriteria($value, $currentValue): array|false
+    private function _andRelatedToCriteria($value, $currentValue): array|string|false
     {
         if (!$value) {
             return false;
