2a1d8ea94b310bee6fe76ecf9a5951810cc6153a
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-08-29 19:42:56 -0400

    New translations app.php (Swedish) [ci skip]

--- src/translations/sv/app.php
+++ src/translations/sv/app.php
@@ -2114,6 +2114,7 @@ return [
     '{name} should be at least {value}.' => '{name} ska vara minst {value}.',
     '{name} sorted by {attribute}, {direction}' => '{name} sorterat efter {attribute}, {direction}',
     '{nestedType} can only be created after the {ownerType} has been saved.' => '{nestedType} kan bara upprättas efter att {ownerType} har sparats.',
+    '{num, number} {num, plural, =1{result} other{results}}' => '{num, number} {num, plural, =1{result} other{results}}',
     '{num, number} {num, plural, =1{Available Update} other{Available Updates}}' => '{num, number} {num, plural, one {} =1{tillgänglig uppdatering} other{tillgängliga uppdateringar}}',
     '{num, number} {num, plural, =1{column} other{columns}}' => '{num, number} {num, plural, one {} =1{kolumn} other{kolumner}}',
     '{num, number} {num, plural, =1{day} other{days}}' => '{num, number} {num, plural, one {} =1{dag} other{dagar}}',
