21ee027d1c9870a1c692baf32b3c2d64a5ae6682
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-08-29 19:42:44 -0400

    New translations app.php (Italian) [ci skip]

--- src/translations/it/app.php
+++ src/translations/it/app.php
@@ -2116,6 +2116,7 @@ Congratulazioni! Craft ti ha inviato una email con successo. Ecco le impostazion
     '{name} should be at least {value}.' => '{name} deve essere almeno {value}.',
     '{name} sorted by {attribute}, {direction}' => '{name} ordinato per {attribute}, {direction}',
     '{nestedType} can only be created after the {ownerType} has been saved.' => '{nestedType} può essere creato solo dopo il salvataggio di {ownerType}.',
+    '{num, number} {num, plural, =1{result} other{results}}' => '{num, number} {num, plural, =1{result} other{results}}',
     '{num, number} {num, plural, =1{Available Update} other{Available Updates}}' => '{num, number} {num, plural, one {} =1{Aggiornamento disponibile} other{Aggiornamenti disponibili}}',
     '{num, number} {num, plural, =1{column} other{columns}}' => '{num, number} {num, plural, one {} =1{colonna} other{colonne}}',
     '{num, number} {num, plural, =1{day} other{days}}' => '{num, number} {num, plural, one {} =1{giorno} other{giorni}}',
