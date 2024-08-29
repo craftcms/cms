6941ecf8899ffbc016fc075fada7816b80e5118f
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-08-29 19:42:54 -0400

    New translations app.php (Slovak) [ci skip]

--- src/translations/sk/app.php
+++ src/translations/sk/app.php
@@ -2114,6 +2114,7 @@ return [
     '{name} should be at least {value}.' => '{name} musí byť minimálne {value}.',
     '{name} sorted by {attribute}, {direction}' => '{name} triedené podľa {attribute}, {direction}',
     '{nestedType} can only be created after the {ownerType} has been saved.' => '{nestedType} je možné vytvoriť až po uložení {ownerType}.',
+    '{num, number} {num, plural, =1{result} other{results}}' => '{num, number} {num, plural, =1{result} other{results}}',
     '{num, number} {num, plural, =1{Available Update} other{Available Updates}}' => '{num, number} {num, plural, =1{dostupná aktualizácia} other{dostupných aktualizácií}}',
     '{num, number} {num, plural, =1{column} other{columns}}' => '{num, number} {num, plural, =1{stĺpec} other{stĺpcov}}',
     '{num, number} {num, plural, =1{day} other{days}}' => '{num, number} {num, plural, =1{deň} other{dní}}',
