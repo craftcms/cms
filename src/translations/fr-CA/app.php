8b6f6a2de01a03917acf478105b617c72c14a9cf
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-08-29 19:43:07 -0400

    New translations app.php (French, Canada) [ci skip]

--- src/translations/fr-CA/app.php
+++ src/translations/fr-CA/app.php
@@ -2115,6 +2115,7 @@ return [
     '{name} should be at least {value}.' => '{name} doit être au moins {value}.',
     '{name} sorted by {attribute}, {direction}' => '{name} trié par {attribute}, {direction}',
     '{nestedType} can only be created after the {ownerType} has been saved.' => '{nestedType} ne peut être créé qu\'après l\'enregistrement du {ownerType}.',
+    '{num, number} {num, plural, =1{result} other{results}}' => '{num, number} {num, plural, =1{result} other{results}}',
     '{num, number} {num, plural, =1{Available Update} other{Available Updates}}' => '{num, number} {num, plural, =1{Available Update} other{Available Updates}}',
     '{num, number} {num, plural, =1{column} other{columns}}' => '{num, number} {num, plural, =1{column} other{columns}}',
     '{num, number} {num, plural, =1{day} other{days}}' => '{num, number} {num, plural, =1{jour} =1{jour} other{jours}}',
