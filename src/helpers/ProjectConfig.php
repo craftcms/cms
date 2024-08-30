e3ebfd6b0093411d36b0f7d7a60c2a4651d24b9a
brandonkelly <brandon@pixelandtonic.com>
2024-08-30 05:50:45 -0700

    Don't ignore `null` assoc array values
    https://github.com/craftcms/cms/issues/10512#issuecomment-1053830465

--- src/helpers/ProjectConfig.php
+++ src/helpers/ProjectConfig.php
@@ -418,7 +418,7 @@ class ProjectConfig
             $associative = [];
             if (!empty($array[ProjectConfigService::ASSOC_KEY])) {
                 foreach ($array[ProjectConfigService::ASSOC_KEY] as $items) {
-                    if (!isset($items[0], $items[1])) {
+                    if (!array_key_exists(0, $items) || !array_key_exists(1, $items)) {
                         Craft::warning('Skipping incomplete packed associative array data', __METHOD__);
                         continue;
                     }
