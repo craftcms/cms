a8627cc5299a6d9b658727172e03b65d330fe41f
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-09-09 19:43:14 -0400

    New translations app.php (Swedish) [ci skip]

--- src/translations/sv/app.php
+++ src/translations/sv/app.php
@@ -1458,7 +1458,7 @@ return [
     'Sign in as {user}' => 'Logga in som {user}',
     'Sign in unsuccessful' => 'Inloggningen misslyckades',
     'Sign in with a passkey' => 'Logga in med en passnyckel',
-    'Sign in with {name}' => 'Sign in with {name}',
+    'Sign in with {name}' => 'Logga in med {name}',
     'Sign in' => 'Logga in',
     'Sign out now' => 'Logga ut nu',
     'Sign out' => 'Logga ut',
