2f0fcb15d25d610c8da8dbd2fdcff09b059a9f03
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-09-09 18:46:36 -0400

    New translations app.php (Thai) [ci skip]

--- src/translations/th/app.php
+++ src/translations/th/app.php
@@ -1286,6 +1286,7 @@ return [
     'Replace files' => 'Replace files',
     'Replace it' => 'Replace it',
     'Replace the folder (all existing files will be deleted)' => 'Replace the folder (all existing files will be deleted)',
+    'Replace' => 'Replace',
     'Replacing “{find}” with “{replace}”' => 'Replacing “{find}” with “{replace}”',
     'Reply To' => 'Reply To',
     'Reply-To Address' => 'Reply-To Address',
