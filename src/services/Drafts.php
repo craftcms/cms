02c8020025680dce0af6c3b6e995c5fd771d6b81
Iwona Just <iwona@pixelandtonic.com>
2024-09-13 09:30:35 +0100

    set saveContent to true so the default values get saved

--- src/services/Drafts.php
+++ src/services/Drafts.php
@@ -258,7 +258,8 @@ SQL,
         ]));
 
         // Try to save and return the result
-        return Craft::$app->getElements()->saveElement($element);
+        // set saveContent to true so that the default values get saved
+        return Craft::$app->getElements()->saveElement($element, saveContent: true);
     }
 
     /**
