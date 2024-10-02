5c9bf94b0d9e8d705cba49f84b078ec9ffe84126
shinybrad <83238715+shinybrad@users.noreply.github.com>
2024-10-02 11:53:08 -0400

    New translations app.php (German, Switzerland) [ci skip]

--- src/translations/de-CH/app.php
+++ src/translations/de-CH/app.php
@@ -927,7 +927,7 @@ return [
     'Matrix' => 'Matrix',
     'Max Authors' => 'Max. Autoren',
     'Max Date' => 'Maximales Datum',
-    'Max Length' => 'Maixmale Länge',
+    'Max Length' => 'Maximale Länge',
     'Max Levels' => 'Max Stufen',
     'Max Relations' => 'Maximale Anzahl Beziehungen',
     'Max Rows' => 'Max. Zeilen',
