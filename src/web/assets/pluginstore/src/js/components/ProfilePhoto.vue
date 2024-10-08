88109eba95c033a2f733af244537cbd99f52b75b
Brian Hanson <brian@brianhanson.net>
2024-10-08 09:24:53 -0500

    Fix profile avatar

--- src/web/assets/pluginstore/src/js/components/ProfilePhoto.vue
+++ src/web/assets/pluginstore/src/js/components/ProfilePhoto.vue
@@ -22,7 +22,7 @@
       class="tw-w-full"
     />
     <div v-else class="tw-flex tw-h-full tw-justify-center tw-items-center">
-      <span class="w-5">
+      <span class="tw-w-5">
         <svg
           aria-hidden="true"
           focusable="false"
@@ -33,7 +33,7 @@
           viewBox="0 0 448 512"
         >
           <path
-            fill="tw-currentColor"
+            fill="currentColor"
             d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z"
           ></path>
         </svg>
