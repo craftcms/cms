b1df3d31c00b9d9ba1d8e5122f77cf0d1c4cdf6e
i-just <iwona@pixelandtonic.com>
2025-04-17 14:26:07 +0100

    add serializeValueForDb method to the time field

--- src/fields/Time.php
+++ src/fields/Time.php
@@ -251,6 +251,14 @@ class Time extends Field implements InlineEditableFieldInterface, SortableFieldI
     /**
      * @inheritdoc
      */
+    public function serializeValueForDb(mixed $value, ?ElementInterface $element): mixed
+    {
+        return $this->serializeValue($value, $element);
+    }
+
+    /**
+     * @inheritdoc
+     */
     public function getElementConditionRuleType(): array|string|null
     {
         return EmptyFieldConditionRule::class;
