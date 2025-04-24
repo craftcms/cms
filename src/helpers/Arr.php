f54d0f097f9c6688e930473c62932468a26d72ed
Rias <hey@rias.be>
2025-04-24 19:27:34 +0200

    Make ::merge() more readable

--- src/helpers/Arr.php
+++ src/helpers/Arr.php
@@ -14,40 +14,40 @@ class Arr extends \Illuminate\Support\Arr
 {
     /**
      * Merges two or more arrays into one recursively.
+     *
      * If each array has an element with the same string key value, the latter
      * will overwrite the former (different from array_merge_recursive).
+     *
      * Recursive merging will be conducted if both arrays have an element of array
      * type and are having the same key.
+     *
      * For integer-keyed elements, the elements from the latter array will
      * be appended to the former array.
-     * You can use [[UnsetArrayValue]] object to unset value from previous array or
-     * [[ReplaceArrayValue]] to force replace former value instead of recursive merging.
-     * @param array $a array to be merged to
-     * @param array $b array to be merged from. You can specify additional
-     * arrays via third argument, fourth argument etc.
+     *
+     * @param array ...$arrays The arrays to merge. The first array will be merged to.
      * @return array the merged array (the original arrays are not changed.)
      */
-    public static function merge($a, $b)
+    public static function merge(array ...$arrays): array
     {
-        $args = func_get_args();
-        $res = array_shift($args);
-        while (!empty($args)) {
-            foreach (array_shift($args) as $k => $v) {
-                if (is_int($k)) {
-                    if (array_key_exists($k, $res)) {
-                        $res[] = $v;
-                    } else {
-                        $res[$k] = $v;
-                    }
-                } elseif (is_array($v) && isset($res[$k]) && is_array($res[$k])) {
-                    $res[$k] = static::merge($res[$k], $v);
-                } else {
-                    $res[$k] = $v;
+        $result = array_shift($arrays);
+
+        while (!empty($arrays)) {
+            foreach (array_shift($arrays) as $key => $value) {
+                if (is_int($key) && array_key_exists($key, $result)) {
+                    $result[] = $value;
+                    continue;
                 }
+
+                if (!is_int($key) && is_array($value) && isset($result[$key]) && is_array($result[$key])) {
+                    $result[$key] = static::merge($result[$key], $value);
+                    continue;
+                }
+
+                $result[$key] = $value;
             }
         }
 
-        return $res;
+        return $result;
     }
 
     /**
