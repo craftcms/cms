73b53deca521066279add2a510dbdc6e8c892115
Rias <hey@rias.be>
2025-07-13 11:49:44 +0200

    Update facade docblocks

--- cms/src/Support/Facades/Announcements.php
+++ cms/src/Support/Facades/Announcements.php
@@ -12,9 +12,9 @@ namespace Craft\Cms\Support\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static void push(string $heading, string $body, ?string $pluginHandle = null, bool $adminsOnly = false)
+ * @method static void push(string $heading, string $body, string|null $pluginHandle = null, bool $adminsOnly = false)
  * @method static array get()
- * @method static void markAsRead(array $ids)
+ * @method static void markAsRead(int[] $ids)
  *
  * @see \Craft\Cms\Announcement\Announcements
  */
