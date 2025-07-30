e0b4cc2c29dfdb960865295902c2672055c48b11
Rias <hey@rias.be>
2025-07-30 19:47:53 +0200

    Missed one

--- cms/tests/TestCase.php
+++ cms/tests/TestCase.php
@@ -14,6 +14,7 @@ use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\RefreshDatabaseState;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
@@ -28,7 +29,7 @@ class TestCase extends Orchestra
     {
         parent::setUp();
 
-        Craft::$app->mutex->release(ProjectConfig::MUTEX_NAME);
+        Cache::lock(ProjectConfig::MUTEX_NAME)->forceRelease();
 
         Factory::guessFactoryNamesUsing(
             fn (string $modelName) => 'CraftCms\\Cms\\Database\\Factories\\'.class_basename($modelName).'Factory'
