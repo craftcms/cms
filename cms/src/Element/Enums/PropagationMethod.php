b3c6117692e5ef730e60b369d63e42cff2646a1b
Rias <hey@rias.be>
2025-08-11 11:52:49 +0200

    Move PropagationMethod

--- cms/src/Element/Enums/PropagationMethod.php
+++ cms/src/Element/Enums/PropagationMethod.php
@@ -1,17 +1,11 @@
 <?php
-/**
- * @link https://craftcms.com/
- * @copyright Copyright (c) Pixel & Tonic, Inc.
- * @license https://craftcms.github.io/license/
- */
 
-namespace craft\enums;
+namespace CraftCms\Cms\Element\Enums;
 
 /**
  * PropagationMethod defines all possible site propagation methods for element values.
  *
- * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
- * @since 5.0.0
+ * @since 6.0.0
  */
 enum PropagationMethod: string
 {
