bde59db75af83914c2732ab83be1b848531fba58
Rias <hey@rias.be>
2025-08-14 08:27:47 +0200

    Set default charset & collation to Laravel defaults

--- cms/tests/TestCase.php
+++ cms/tests/TestCase.php
@@ -126,8 +126,9 @@ class TestCase extends Orchestra
                 'database' => env('DB_DATABASE'),
                 'username' => env('DB_USERNAME'),
                 'password' => env('DB_PASSWORD'),
+                'charset' => env('DB_CHARSET', 'utf8mb4'),
+                'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
                 'prefix' => env('DB_PREFIX'),
-                'charset' => env('DB_CHARSET'),
             ]),
         );
 
