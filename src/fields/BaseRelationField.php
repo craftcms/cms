d68c4740220f542cc2c4786a7db186351eeed64d
brandonkelly <brandon@pixelandtonic.com>
2025-09-05 11:34:39 -0700

    Sort lines + remove redundant setting

--- src/fields/BaseRelationField.php
+++ src/fields/BaseRelationField.php
@@ -391,7 +391,9 @@ abstract class BaseRelationField extends Field implements PreviewableFieldInterf
     {
         $attributes = parent::settingsAttributes();
         $attributes[] = 'allowSelfRelations';
+        $attributes[] = 'branchLimit';
         $attributes[] = 'localizeRelations';
+        $attributes[] = 'maintainHierarchy';
         $attributes[] = 'maxRelations';
         $attributes[] = 'minRelations';
         $attributes[] = 'selectionLabel';
@@ -401,9 +403,6 @@ abstract class BaseRelationField extends Field implements PreviewableFieldInterf
         $attributes[] = 'targetSiteId';
         $attributes[] = 'validateRelatedElements';
         $attributes[] = 'viewMode';
-        $attributes[] = 'allowSelfRelations';
-        $attributes[] = 'maintainHierarchy';
-        $attributes[] = 'branchLimit';
 
         return $attributes;
     }
