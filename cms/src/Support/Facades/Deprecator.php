b480e656ceada076c784100c8edc849f57f4136a
riasvdv <3626559+riasvdv@users.noreply.github.com>
2025-09-23 07:41:22 +0000

    Update facade docblocks

--- cms/src/Support/Facades/Deprecator.php
+++ cms/src/Support/Facades/Deprecator.php
@@ -7,9 +7,9 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @method static void log(string $key, string $message, string|null $file = null, int|null $line = null)
  * @method static void storeLogs()
- * @method static \CraftCms\Cms\Deprecator\Models\DeprecationError[] getRequestLogs()
+ * @method static array getRequestLogs()
  * @method static int getTotalLogs()
- * @method static \CraftCms\Cms\Deprecator\Models\DeprecationError[] getLogs(int|null $limit = null)
+ * @method static array getLogs(int|null $limit = null)
  * @method static \CraftCms\Cms\Deprecator\Models\DeprecationError|null getLogById(int $logId)
  * @method static bool deleteLogById(int $id)
  * @method static bool deleteAllLogs()
