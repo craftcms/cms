766af7165d183a08f4610c63b953f357686592d2
Rias <hey@rias.be>
2025-09-27 21:57:01 +0200

    Run rector only on cms and not adapter

--- rector.php
+++ rector.php
@@ -4,16 +4,11 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Php71\Rector\FuncCall\RemoveExtraParametersRector;
-use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__.'/yii2-adapter/lib',
-        __DIR__.'/yii2-adapter/legacy',
         __DIR__.'/src',
         __DIR__.'/tests',
-        __DIR__.'/yii2-adapter/legacy-tests/functional',
-        __DIR__.'/yii2-adapter/legacy-tests/unit',
     ])
     ->withSkip([
         __DIR__.'/resources/icons/index.php',
@@ -22,10 +17,5 @@ return RectorConfig::configure()
         // somehow craft\web AssetManager refer with Yii parent AssetManager class
         // autoload may need to be bootstrapped to early load some child classes
         RemoveExtraParametersRector::class,
-
-        // macro usage, make phpstan notice
-        ClosureToArrowFunctionRector::class => [
-            __DIR__.'/yii2-adapter/legacy/base/ApplicationTrait.php',
-        ],
     ])
     ->withPhpSets(php74: true);
