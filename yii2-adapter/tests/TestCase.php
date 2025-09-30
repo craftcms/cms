038875383fa08be5040ac4060b95f7420642c1ca
Rias <hey@rias.be>
2025-09-30 10:55:44 +0200

    Get phpstan working for adapter

--- yii2-adapter/tests/TestCase.php
+++ yii2-adapter/tests/TestCase.php
@@ -2,19 +2,10 @@
 
 namespace CraftCms\Yii2Adapter\Tests;
 
-use CraftCms\Aliases\AliasesServiceProvider;
-use CraftCms\Cms\Providers\CraftServiceProvider;
-use CraftCms\Yii2Adapter\Yii2ServiceProvider;
+use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    protected function getPackageProviders($app): array
-    {
-        return [
-            AliasesServiceProvider::class,
-            CraftServiceProvider::class,
-            Yii2ServiceProvider::class,
-        ];
-    }
+    use WithWorkbench;
 }
