e1c2f3eb3d430a3c1ad4e81dbe446045cc3bd8f6
Rias <hey@rias.be>
2025-10-20 10:25:21 +0200

    Test for DeleteStaleElementActivity

---
+++ tests/GarbageCollection/Actions/DeleteStaleElementActivityTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use CraftCms\Cms\Database\Table;
+use CraftCms\Cms\GarbageCollection\Actions\DeleteStaleElementActivity;
+
+it('deletes stale activity', function () {
+    DB::table(Table::ELEMENTACTIVITY)->insert([
+        'elementId' => 1,
+        'userId' => 1,
+        'siteId' => 1,
+        'type' => 'foo',
+        'timestamp' => now(),
+    ]);
+
+    DB::table(Table::ELEMENTACTIVITY)->insert([
+        'elementId' => 1,
+        'userId' => 1,
+        'siteId' => 1,
+        'type' => 'foo-2',
+        'timestamp' => now()->subMinute()->subSecond(),
+    ]);
+
+    expect(DB::table(Table::ELEMENTACTIVITY)->count())->toBe(2);
+
+    app(DeleteStaleElementActivity::class)();
+
+    expect(DB::table(Table::ELEMENTACTIVITY)->count())->toBe(1);
+});
