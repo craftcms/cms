6b664982c800b80792184c868fb96b9abdb876f0
Rias <hey@rias.be>
2025-10-20 10:19:15 +0200

    Test for DeleteStaleAnnouncements

---
+++ tests/GarbageCollection/Actions/DeleteStaleAnnouncementsTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use CraftCms\Cms\Announcement\Models\Announcement;
+use CraftCms\Cms\GarbageCollection\Actions\DeleteStaleAnnouncements;
+use CraftCms\Cms\User\Models\User;
+
+it('deletes stale announcements', function () {
+    // Valid announcement
+    Announcement::factory()->create([
+        'userId' => User::first()->id,
+        'dateRead' => now(),
+    ]);
+
+    // Stale announcement
+    Announcement::factory()->create([
+        'userId' => User::first()->id,
+        'dateRead' => now()->subDays(7)->subSecond(),
+    ]);
+
+    expect(Announcement::count())->toBe(2);
+
+    app(DeleteStaleAnnouncements::class)();
+
+    expect(Announcement::count())->toBe(1);
+});
