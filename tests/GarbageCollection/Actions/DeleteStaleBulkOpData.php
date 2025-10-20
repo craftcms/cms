ae74ca29112311646d7c79c413f60c03638571f7
Rias <hey@rias.be>
2025-10-20 10:22:51 +0200

    Test for DeleteStaleBulkOpData

---
+++ tests/GarbageCollection/Actions/DeleteStaleBulkOpData.php
@@ -0,0 +1,30 @@
+<?php
+
+use CraftCms\Cms\Database\Table;
+use CraftCms\Cms\GarbageCollection\Actions\DeleteStaleBulkOpData;
+
+it('deletes stale bulk op data', function (string $table, array $attributes) {
+    DB::table($table)->insert(array_merge($attributes, [
+        'key' => 'op1',
+        'timestamp' => now(),
+    ]));
+
+    DB::table($table)->insert(array_merge($attributes, [
+        'key' => 'op2',
+        'timestamp' => now()->subWeeks(2)->subSecond(),
+    ]));
+
+    expect(DB::table($table)->count())->toBe(2);
+
+    app(DeleteStaleBulkOpData::class)();
+
+    expect(DB::table($table)->count())->toBe(1);
+})->with([
+    [Table::BULKOPEVENTS, [
+        'senderClass' => 'foo',
+        'eventName' => 'foo',
+    ]],
+    [Table::ELEMENTS_BULKOPS, [
+        'elementId' => 1,
+    ]],
+]);
