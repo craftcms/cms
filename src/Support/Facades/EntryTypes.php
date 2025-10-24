bce4f9243e83e040afb9bc42521e3c46f9ba6d70
brandonkelly <47792+brandonkelly@users.noreply.github.com>
2025-10-24 23:31:48 +0000

    Update facade docblocks

--- src/Support/Facades/EntryTypes.php
+++ src/Support/Facades/EntryTypes.php
@@ -5,6 +5,19 @@ namespace CraftCms\Cms\Support\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static \Illuminate\Support\Collection getEntryTypesBySectionId(int $sectionId)
+ * @method static \Illuminate\Support\Collection getAllEntryTypes()
+ * @method static \CraftCms\Cms\EntryType\Data\EntryType|null getEntryTypeById(int $entryTypeId, bool $withTrashed = false)
+ * @method static \CraftCms\Cms\EntryType\Data\EntryType|null getEntryTypeByUid(string $uid)
+ * @method static \CraftCms\Cms\EntryType\Data\EntryType|null getEntryTypeByHandle(string $entryTypeHandle)
+ * @method static \CraftCms\Cms\EntryType\Data\EntryType|null getEntryType(\CraftCms\Cms\EntryType\Data\EntryType|int|string|array $entryType)
+ * @method static bool saveEntryType(\CraftCms\Cms\EntryType\Data\EntryType $entryType)
+ * @method static void handleChangedEntryType(\CraftCms\Cms\ProjectConfig\Events\ConfigEvent $event)
+ * @method static bool deleteEntryTypeById(int $entryTypeId)
+ * @method static bool deleteEntryType(\CraftCms\Cms\EntryType\Data\EntryType $entryType)
+ * @method static void handleDeletedEntryType(\CraftCms\Cms\ProjectConfig\Events\ConfigEvent $event)
+ * @method static void refreshEntryTypes()
+ *
  * @see \CraftCms\Cms\EntryType\EntryTypes
  */
 final class EntryTypes extends Facade
