2a3eca68414907d43b45a8c705e3cf61d0a6e6b2
Rias <hey@rias.be>
2025-10-27 10:57:30 +0100

    Update changelog

--- src/Entry/Commands/MergeEntryTypesCommand.php
+++ src/Entry/Commands/MergeEntryTypesCommand.php
@@ -27,7 +27,7 @@ use Illuminate\Support\Facades\File;
 use function Laravel\Prompts\confirm;
 use function Laravel\Prompts\select;
 
-final class MergeEntryTypes extends Command implements PromptsForMissingInput
+final class MergeEntryTypesCommand extends Command implements PromptsForMissingInput
 {
     use CraftCommand;
 
--- src/Entry/EntryServiceProvider.php
+++ src/Entry/EntryServiceProvider.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace CraftCms\Cms\Entry;
 
-use CraftCms\Cms\Entry\Commands\MergeEntryTypes;
+use CraftCms\Cms\Entry\Commands\MergeEntryTypesCommand;
 use Illuminate\Support\ServiceProvider;
 
 final class EntryServiceProvider extends ServiceProvider
@@ -12,7 +12,7 @@ final class EntryServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->commands([
-            MergeEntryTypes::class,
+            MergeEntryTypesCommand::class,
         ]);
     }
 }
