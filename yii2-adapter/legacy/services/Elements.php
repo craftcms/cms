d4b59162f0d0e36a84ffb929875e5b9bfbe827ac
brandonkelly <brandon@pixelandtonic.com>
2025-11-02 13:03:28 -0800

    Bring this back

--- yii2-adapter/legacy/services/Elements.php
+++ yii2-adapter/legacy/services/Elements.php
@@ -3009,6 +3009,11 @@ class Elements extends Component
     public function getElementTypeByRefHandle(string $refHandle): ?string
     {
         if (!isset($this->_elementTypesByRefHandle[$refHandle])) {
+            // Special cases for categories/tags/globals
+            if (in_array($refHandle, ['category', 'tag', 'globalset'])) {
+                $refHandle = Entry::refHandle();
+            }
+
             $class = $this->elementTypeByRefHandle($refHandle);
             $this->_elementTypesByRefHandle[$refHandle] = $class;
         }
