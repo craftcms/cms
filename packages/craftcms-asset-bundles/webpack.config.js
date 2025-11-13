572f7ca0a08adc0e5747b41c67a19b9112e6ef70
Brian Hanson <brian@brianhanson.net>
2025-11-13 14:22:40 -0600

    Fix installing via composer

--- packages/craftcms-asset-bundles/webpack.config.js
+++ packages/craftcms-asset-bundles/webpack.config.js
@@ -1,3 +1,19 @@
 const {getConfigs} = require('@craftcms/webpack');
+const pkgDir = require('pkg-dir');
 
-module.exports = getConfigs('bundles/**/*/webpack.config.js');
+const configs = getConfigs('bundles/**/*/webpack.config.js');
+
+const root = pkgDir.sync();
+const outBase = `${root}/../../resources/build/legacy`;
+
+const modifiedConfigs = configs.map((config) => {
+  return {
+    ...config,
+    output: {
+      ...config.output,
+      path: `${outBase}/${config.name}/dist`,
+    },
+  };
+});
+
+module.exports = modifiedConfigs;
