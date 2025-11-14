04eb59a0bbe154536e16c17500619cabb249beb9
brandonkelly <brandon@pixelandtonic.com>
2025-11-14 11:58:01 -0800

    Allow bulk actions on mobile

--- src/controllers/ElementIndexesController.php
+++ src/controllers/ElementIndexesController.php
@@ -874,10 +874,6 @@ class ElementIndexesController extends BaseElementsController
      */
     protected function availableActions(): ?array
     {
-        if ($this->request->isMobileBrowser()) {
-            return null;
-        }
-
         $actions = $this->elementType::actions($this->sourceKey);
 
         foreach ($actions as $i => $action) {
