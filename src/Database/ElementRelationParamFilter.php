fb2f0f32a8c9dbdc7e51af3495f236858f111667
Rias <hey@rias.be>
2025-11-18 12:26:55 +0100

    Avoid extra groups when there is only 1 param

--- src/Database/ElementRelationParamFilter.php
+++ src/Database/ElementRelationParamFilter.php
@@ -203,6 +203,10 @@ final class ElementRelationParamFilter
             return $query;
         }
 
+        if (count($relatedToParam) === 1) {
+            return $this->subparse($query, $relatedToParam[0]);
+        }
+
         return $query->where(function (Builder $query) use ($relatedToParam, $glue) {
             foreach ($relatedToParam as $relCriteria) {
                 $query->where(fn (Builder $query) => $this->subparse($query, $relCriteria), boolean: $glue);
