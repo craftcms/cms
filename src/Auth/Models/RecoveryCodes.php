00a1bd457271c143739fd08aa49a27e3322aa91b
Rias <hey@rias.be>
2025-11-26 10:12:59 +0100

    RecoveryCodes

---
+++ src/Auth/Models/RecoveryCodes.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CraftCms\Cms\Auth\Models;
+
+use CraftCms\Cms\Database\Table;
+use CraftCms\Cms\Shared\BaseModel;
+use CraftCms\Cms\User\Models\User;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+final class RecoveryCodes extends BaseModel
+{
+    protected $table = Table::RECOVERYCODES;
+
+    protected function casts(): array
+    {
+        return [
+            'recoveryCodes' => 'array',
+        ];
+    }
+
+    /**
+     * @return BelongsTo<User, $this>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'userId');
+    }
+}
