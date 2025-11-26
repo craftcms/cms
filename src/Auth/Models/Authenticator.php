0b7bddf012ff979522ff94c96b6558c261594a3a
Rias <hey@rias.be>
2025-11-26 09:24:48 +0100

    Authenticator

---
+++ src/Auth/Models/Authenticator.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CraftCms\Cms\Auth\Models;
+
+use CraftCms\Cms\Database\Table;
+use CraftCms\Cms\Shared\BaseModel;
+use CraftCms\Cms\User\Models\User;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+final class Authenticator extends BaseModel
+{
+    protected $table = Table::AUTHENTICATOR;
+
+    protected function casts(): array
+    {
+        return [
+            'oldTimestamp' => 'int',
+        ];
+    }
+
+    /**
+     * @return BelongsTo<User, $this>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'userId');
+    }
+}
