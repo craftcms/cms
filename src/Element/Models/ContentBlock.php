ee13d8ec751f6cc293aa3c131e4bee5588cb3d04
Rias <hey@rias.be>
2025-11-26 09:29:20 +0100

    ContentBlock

---
+++ src/Element/Models/ContentBlock.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CraftCms\Cms\Element\Models;
+
+use CraftCms\Cms\Database\Table;
+use CraftCms\Cms\Field\Models\Field;
+use CraftCms\Cms\Shared\BaseModel;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+final class ContentBlock extends BaseModel
+{
+    protected $table = Table::CONTENTBLOCKS;
+
+    public $incrementing = false;
+
+    /**
+     * @return BelongsTo<Element, $this>
+     */
+    public function element(): BelongsTo
+    {
+        return $this->belongsTo(Element::class, 'id');
+    }
+
+    /**
+     * @return BelongsTo<Element, $this>
+     */
+    public function primaryOwner(): BelongsTo
+    {
+        return $this->belongsTo(Element::class, 'primaryOwnerId');
+    }
+
+    /**
+     * @return BelongsTo<Field, $this>
+     */
+    public function field(): BelongsTo
+    {
+        return $this->belongsTo(Field::class, 'fieldId');
+    }
+}
