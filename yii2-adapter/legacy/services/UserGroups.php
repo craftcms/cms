17c44bde3ecb8a20c49d6f3f2709e3fc9055e006
Rias <hey@rias.be>
2025-12-01 16:00:42 +0100

    Fix phpstan

--- yii2-adapter/legacy/services/UserGroups.php
+++ yii2-adapter/legacy/services/UserGroups.php
@@ -237,7 +237,6 @@ class UserGroups extends Component
      * @param int $groupId The user group's ID
      *
      * @return bool Whether the user group was deleted successfully
-     * @throws WrongEditionException if this is called from Craft Solo edition
      */
     public function deleteGroupById(int $groupId): bool
     {
