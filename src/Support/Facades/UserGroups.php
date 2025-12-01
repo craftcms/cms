5b62627c50835fa9dfbae0d8d7dd7ad465347114
brandonkelly <47792+brandonkelly@users.noreply.github.com>
2025-12-01 14:08:53 +0000

    Update facade docblocks

--- src/Support/Facades/UserGroups.php
+++ src/Support/Facades/UserGroups.php
@@ -8,6 +8,20 @@ use Illuminate\Support\Facades\Facade;
 use Override;
 
 /**
+ * @method static \Illuminate\Support\Collection getAllGroups()
+ * @method static \Illuminate\Support\Collection getAssignableGroups(\craft\elements\User|null $user = null)
+ * @method static \CraftCms\Cms\User\Data\UserGroup|null getGroupById(int $groupId)
+ * @method static \CraftCms\Cms\User\Data\UserGroup|null getGroupByUid(string $uid)
+ * @method static \CraftCms\Cms\User\Data\UserGroup|null getGroupByHandle(string $groupHandle)
+ * @method static \CraftCms\Cms\User\Data\UserGroup getTeamGroup()
+ * @method static \Illuminate\Support\Collection getGroupsByUserId(int $userId)
+ * @method static void eagerLoadGroups(\craft\elements\User[] $users)
+ * @method static bool saveGroup(\CraftCms\Cms\User\Data\UserGroup $group)
+ * @method static void handleChangedUserGroup(\CraftCms\Cms\ProjectConfig\Events\ConfigEvent $event)
+ * @method static void handleDeletedUserGroup(\CraftCms\Cms\ProjectConfig\Events\ConfigEvent $event)
+ * @method static bool deleteGroupById(int $groupId)
+ * @method static bool deleteGroup(\CraftCms\Cms\User\Data\UserGroup $group)
+ *
  * @see \CraftCms\Cms\User\UserGroups
  */
 final class UserGroups extends Facade
