ba2d57da28e750c6bdf245fec78878bb140b26d0
Brian Hanson <brian@brianhanson.net>
2025-12-05 12:13:43 -0600

    Update controller

--- src/Http/Controllers/Settings/GeneralSettingsController.php
+++ src/Http/Controllers/Settings/GeneralSettingsController.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace CraftCms\Cms\Http\Controllers\Settings;
 
 use CraftCms\Cms\Config\GeneralConfig;
-use CraftCms\Cms\Cp\DateTime;
+use CraftCms\Cms\Cp\SelectOptions;
 use CraftCms\Cms\Http\RespondsWithFlash;
 use CraftCms\Cms\ProjectConfig\ProjectConfig;
+use CraftCms\Cms\Shared\Rules\TimezoneRule;
+use CraftCms\Cms\Support\Env;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -25,7 +28,12 @@ final readonly class GeneralSettingsController
     {
         return Inertia::render('SettingsGeneralPage', [
             'system' => $this->projectConfig->get('system') ?? [],
-            'timezones' => DateTime::getTimeZoneOptions(),
+            'nameOptions' => SelectOptions::getEnvSuggestions(),
+            'timezoneOptions' => [
+                ...SelectOptions::getTimeZoneOptions(),
+                ...SelectOptions::getEnvOptions(),
+            ],
+            'systemStatusOptions' => SelectOptions::getBooleanEnvOptions(),
             'readOnly' => ! $generalConfig->allowAdminChanges,
             'saveUrl' => route('craft.cp.settings.general.store'),
         ]);
@@ -33,6 +41,25 @@ final readonly class GeneralSettingsController
 
     public function store(Request $request)
     {
+        $resolvedValues = [];
+        foreach ($request->all() as $key => $value) {
+            if (is_string($value)) {
+                $resolvedValues[$key] = str_starts_with($value, '$') ? Env::parse($value) : $value;
+            } else {
+                $resolvedValues[$key] = $value;
+            }
+        }
+
+        /**
+         * We want to validate against the resolved values, but we'll store what the user provided
+         */
+        Validator::make($resolvedValues, [
+            'name' => ['required', 'string'],
+            'live' => ['required', 'boolean'],
+            'retryDuration' => ['nullable', 'integer'],
+            'timeZone' => ['required', 'string', new TimezoneRule],
+        ])->validate();
+
         $systemSettings = $this->projectConfig->get('system');
         $systemSettings['name'] = $request->input('name');
         $systemSettings['live'] = $request->input('live');
