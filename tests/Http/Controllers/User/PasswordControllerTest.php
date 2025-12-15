9f36097c47fee57356da47739093eda7f1d88162
Rias <hey@rias.be>
2025-12-15 11:29:26 +0100

    PasswordControllerTest

---
+++ tests/Http/Controllers/User/PasswordControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use CraftCms\Cms\Http\Controllers\Users\PasswordController;
+use CraftCms\Cms\User\Elements\User;
+
+use function CraftCms\Cms\t;
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\get;
+
+beforeEach(function () {
+    actingAs(User::findOne());
+});
+
+it('requires login', function () {
+    auth()->logout();
+
+    get(action([PasswordController::class, 'index']))->assertRedirect('admin/login');
+});
+
+test('index', function () {
+    get(action([PasswordController::class, 'index']))
+        ->assertOk()
+        ->assertSee(t('Password'));
+});
