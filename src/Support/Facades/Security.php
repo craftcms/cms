fdaff4cdffde94decbcfa3cf3c4129ce5aea6906
brandonkelly <47792+brandonkelly@users.noreply.github.com>
2026-01-21 15:14:22 +0000

    Update facade docblocks

--- src/Support/Facades/Security.php
+++ src/Support/Facades/Security.php
@@ -7,6 +7,10 @@ namespace CraftCms\Cms\Support\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static bool isSensitive(string $key)
+ * @method static mixed redactIfSensitive(string $key, mixed $value)
+ * @method static bool isSystemDir(string $path)
+ *
  * @see \CraftCms\Cms\Support\Security
  */
 final class Security extends Facade
