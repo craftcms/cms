ca5ff31248d0f11cf8ec4c7fce7901c339fe1071
shinybrad <83238715+shinybrad@users.noreply.github.com>
2026-01-25 20:22:11 -0500

    New translations app.php (Hungarian) [ci skip]

--- src/translations/hu/app.php
+++ src/translations/hu/app.php
@@ -243,6 +243,7 @@ return [
     'Change icon' => 'Ikon megváltoztatása',
     'Change logo' => 'Logo megváltoztatása',
     'Change photo' => 'Kép csere',
+    'Change the author of other users’ entries' => 'Change the author of other users’ entries',
     'Changelog' => 'Változásnapló',
     'Changes discarded.' => 'Változtatások elvetve.',
     'Changing this may result in data loss.' => 'Ennek megváltoztatása adatveszteséggel járhat.',
