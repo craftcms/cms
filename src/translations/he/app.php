f0b815ede650bd427856534849e5369d964a61ef
shinybrad <83238715+shinybrad@users.noreply.github.com>
2026-01-25 20:22:57 -0500

    New translations app.php (Hebrew) [ci skip]

--- src/translations/he/app.php
+++ src/translations/he/app.php
@@ -275,6 +275,7 @@ return [
     'Change icon' => 'שנה אייקון',
     'Change logo' => 'שנה לוגו',
     'Change photo' => 'החלף תצלום',
+    'Change the author of other users’ entries' => 'Change the author of other users’ entries',
     'Change your Password' => 'שנה את הסיסמה שלך',
     'Change' => 'שינוי',
     'Changelog' => 'שנה יומן',
