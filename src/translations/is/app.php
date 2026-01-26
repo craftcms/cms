9e87bde923821ef9dc7451391d8c342c4b744a9a
shinybrad <83238715+shinybrad@users.noreply.github.com>
2026-01-25 20:22:31 -0500

    New translations app.php (Icelandic) [ci skip]

--- src/translations/is/app.php
+++ src/translations/is/app.php
@@ -243,6 +243,7 @@ return [
     'Change icon' => 'Breyta tákni',
     'Change logo' => 'Breyta lógói',
     'Change photo' => 'Skipta um mynd',
+    'Change the author of other users’ entries' => 'Change the author of other users’ entries',
     'Changelog' => 'Breytingaskrá',
     'Changes discarded.' => 'Breytingum hent.',
     'Changing this may result in data loss.' => 'Ef þessu er breytt getur það leitt til taps á gögnum.',
