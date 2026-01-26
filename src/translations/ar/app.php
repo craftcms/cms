b50f7f0243353ae9c4fe3d3d36eef7ec782231c7
shinybrad <83238715+shinybrad@users.noreply.github.com>
2026-01-25 20:22:51 -0500

    New translations app.php (Arabic) [ci skip]

--- src/translations/ar/app.php
+++ src/translations/ar/app.php
@@ -275,6 +275,7 @@ return [
     'Change icon' => 'تغيير الأيقونة',
     'Change logo' => 'تغيير الشعار',
     'Change photo' => 'تغيير الصورة',
+    'Change the author of other users’ entries' => 'Change the author of other users’ entries',
     'Change your Password' => 'تغيير كلمة مرورك',
     'Change' => 'تغيير',
     'Changelog' => 'سجل التغييرات',
