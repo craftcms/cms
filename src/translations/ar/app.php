cb54e11019be9bfb2447c3f04f16ef582c73e29c
shinybrad <83238715+shinybrad@users.noreply.github.com>
2026-01-25 20:22:02 -0500

    New translations app.php (Arabic) [ci skip]

--- src/translations/ar/app.php
+++ src/translations/ar/app.php
@@ -243,6 +243,7 @@ return [
     'Change icon' => 'تغيير الأيقونة',
     'Change logo' => 'تغيير الشعار',
     'Change photo' => 'تغيير الصورة',
+    'Change the author of other users’ entries' => 'Change the author of other users’ entries',
     'Changelog' => 'سجل التغييرات',
     'Changes discarded.' => 'تم تجاهل التغييرات.',
     'Changing this may result in data loss.' => 'تغيير هذا قد يتسبب في فقدان البيانات.',
