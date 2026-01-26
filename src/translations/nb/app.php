b96ccbab0f132172c2c66390f209d25ff14b59ef
shinybrad <83238715+shinybrad@users.noreply.github.com>
2026-01-25 20:22:39 -0500

    New translations app.php (Norwegian Bokmal) [ci skip]

--- src/translations/nb/app.php
+++ src/translations/nb/app.php
@@ -243,6 +243,7 @@ return [
     'Change icon' => 'Bytt ikon',
     'Change logo' => 'Bytt logo',
     'Change photo' => 'Bytt bilde',
+    'Change the author of other users’ entries' => 'Change the author of other users’ entries',
     'Changelog' => 'Endringslogg',
     'Changes discarded.' => 'Endringer forkastet.',
     'Changing this may result in data loss.' => 'Å endre denne kan føre til tap av data.',
