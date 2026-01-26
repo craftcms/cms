dcaf61ccfd27b4c3d10ad44a71f1aba098a79e7f
shinybrad <83238715+shinybrad@users.noreply.github.com>
2026-01-25 20:22:04 -0500

    New translations app.php (Czech) [ci skip]

--- src/translations/cs/app.php
+++ src/translations/cs/app.php
@@ -243,6 +243,7 @@ return [
     'Change icon' => 'Změnit ikonu',
     'Change logo' => 'Změnit logo',
     'Change photo' => 'Změnit fotku',
+    'Change the author of other users’ entries' => 'Change the author of other users’ entries',
     'Changelog' => 'Protokol změn',
     'Changes discarded.' => 'Změny byly zrušeny.',
     'Changing this may result in data loss.' => 'Změna tohoto údaje může způsobit ztrátu dat.',
