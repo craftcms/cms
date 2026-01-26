4c8d3e5296a95a2cceda3785545702c47533b990
shinybrad <83238715+shinybrad@users.noreply.github.com>
2026-01-25 20:23:17 -0500

    New translations app.php (Chinese Simplified) [ci skip]

--- src/translations/zh/app.php
+++ src/translations/zh/app.php
@@ -275,6 +275,7 @@ return [
     'Change icon' => '更改图标',
     'Change logo' => '更改 logo',
     'Change photo' => '更改照片',
+    'Change the author of other users’ entries' => 'Change the author of other users’ entries',
     'Change your Password' => '更改密码',
     'Change' => '更改',
     'Changelog' => '更改日志',
