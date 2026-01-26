e446271706ed299e63f0dd3914b1ad21876d740f
shinybrad <83238715+shinybrad@users.noreply.github.com>
2026-01-25 20:23:25 -0500

    New translations app.php (French, Canada) [ci skip]

--- src/translations/fr-CA/app.php
+++ src/translations/fr-CA/app.php
@@ -275,6 +275,7 @@ return [
     'Change icon' => 'Modifier l’icône',
     'Change logo' => 'Modifier le logo',
     'Change photo' => 'Modifier la photo',
+    'Change the author of other users’ entries' => 'Change the author of other users’ entries',
     'Change your Password' => 'Modifier votre mot de passe',
     'Change' => 'Modifier',
     'Changelog' => 'Changelog',
