d339a96b3de0a65bbbef8f24a80dc0efd98cc269
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2026-01-30 16:25:06 +0000

    Don’t change the gql link data type name to avoid breaking changes

--- src/gql/types/generators/LinkDataType.php
+++ src/gql/types/generators/LinkDataType.php
@@ -38,11 +38,8 @@ class LinkDataType implements GeneratorInterface, SingleGeneratorInterface
     /**
      * Returns the generator name.
      */
-    public static function getName($context = null): string
+    public static function getName(): string
     {
-        if ($context instanceof Link) {
-            return "{$context->handle}_LinkData";
-        }
         return 'LinkData';
     }
 
@@ -51,7 +48,7 @@ class LinkDataType implements GeneratorInterface, SingleGeneratorInterface
      */
     public static function generateType(mixed $context): ObjectType
     {
-        $typeName = self::getName($context);
+        $typeName = self::getName();
         return GqlEntityRegistry::getOrCreate($typeName, fn() => new LinkData([
             'name' => $typeName,
             'fields' => function() use ($context, $typeName) {
@@ -80,8 +77,8 @@ class LinkDataType implements GeneratorInterface, SingleGeneratorInterface
                 if ($context instanceof Link) {
                     $hasElementType = false;
 
-                    foreach ($context->getLinkTypes() as $linkType) {
-                        if ($linkType instanceof BaseElementLinkType) {
+                    foreach (Link::types() as $linkType) {
+                        if (is_subclass_of($linkType, BaseElementLinkType::class)) {
                             $hasElementType = true;
                             $fields[$linkType::id()] = [
                                 'name' => $linkType::id(),
