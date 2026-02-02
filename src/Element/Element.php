44b01a357e4f5c25b39bf9b57197ba529aca10bf
Rias <hey@rias.be>
2026-02-02 13:14:49 +0100

    Queryable

--- src/Element/Element.php
+++ src/Element/Element.php
@@ -20,8 +20,6 @@ use craft\elements\actions\Duplicate;
 use craft\elements\actions\Edit;
 use craft\elements\actions\SetStatus;
 use craft\elements\actions\View as ViewAction;
-use craft\elements\conditions\ElementCondition;
-use craft\elements\conditions\ElementConditionInterface;
 use craft\elements\db\NestedElementQueryInterface;
 use craft\events\AuthorizationCheckEvent;
 use craft\events\DefineAttributeKeywordsEvent;
@@ -152,6 +150,7 @@ abstract class Element extends Component implements ElementInterface
     use Concerns\Exportable;
     use Concerns\HasControlPanelUI;
     use Concerns\HasCustomFields;
+    use Concerns\Queryable;
     use Concerns\Revisionable;
     use Concerns\Structurable;
     use ElementTrait {
@@ -817,54 +816,6 @@ abstract class Element extends Component implements ElementInterface
             self::STATUS_ENABLED => t('Enabled'),
             self::STATUS_DISABLED => t('Disabled'),
         ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public static function find(): ElementQueryInterface
-    {
-        return new Queries\ElementQuery(static::class);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public static function findOne(mixed $criteria = null): ?static
-    {
-        return static::findByCondition($criteria, true);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public static function findAll(mixed $criteria = null): array
-    {
-        return static::findByCondition($criteria, false);
-    }
-
-    /**
-     * @interitdoc
-     */
-    public static function get(int|string $id): ?static
-    {
-        /** @phpstan-ignore return.type */
-        return static::find()
-            ->id($id)
-            ->fixedOrder()
-            ->drafts(null)
-            ->provisionalDrafts(null)
-            ->revisions(null)
-            ->status(null)
-            ->one();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public static function createCondition(): ElementConditionInterface
-    {
-        return Craft::createObject(ElementCondition::class, [static::class]);
     }
 
     /**
@@ -3673,34 +3624,6 @@ abstract class Element extends Component implements ElementInterface
         if ($this->hasEventHandlers(self::EVENT_AFTER_RESTORE)) {
             $this->trigger(self::EVENT_AFTER_RESTORE);
         }
-    }
-
-    /**
-     * Finds Element instance(s) by the given condition.
-     *
-     * This method is internally called by [[findOne()]] and [[findAll()]].
-     *
-     * @param  mixed  $criteria  Refer to [[findOne()]] and [[findAll()]] for the explanation of this parameter
-     * @param  bool  $one  Whether this method is called by [[findOne()]] or [[findAll()]]
-     * @return static|static[]|null
-     */
-    protected static function findByCondition(mixed $criteria, bool $one): array|static|null
-    {
-        /** @var \CraftCms\Cms\Element\Queries\ElementQuery<static> $query */
-        $query = static::find();
-
-        if ($criteria !== null) {
-            if (! is_array($criteria) || Arr::isList($criteria)) {
-                $criteria = ['id' => $criteria];
-            }
-            Craft::configure($query, $criteria);
-        }
-
-        if ($one) {
-            return $query->first();
-        }
-
-        return $query->all();
     }
 
     /**
