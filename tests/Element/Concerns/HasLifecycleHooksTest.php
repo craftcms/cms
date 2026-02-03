85642f7bb60da1cf34fdaf6245ab610e951ba3cb
Rias <hey@rias.be>
2026-02-03 16:32:21 +0100

    Fix test?

--- tests/Element/Concerns/HasLifecycleHooksTest.php
+++ tests/Element/Concerns/HasLifecycleHooksTest.php
@@ -47,11 +47,12 @@ test('beforeSave event can prevent save', function () {
 });
 
 test('beforeSave event receives isNew parameter', function () {
+    // Make sure no other listeners are active
+    Event::forget(BeforeSave::class);
+
     $receivedIsNew = null;
     Event::listen(function (BeforeSave $event) use (&$receivedIsNew) {
-        if ($event->element->id === $this->entry->id) {
-            $receivedIsNew = $event->isNew;
-        }
+        $receivedIsNew = $event->isNew;
     });
 
     $this->entry->beforeSave(true);
