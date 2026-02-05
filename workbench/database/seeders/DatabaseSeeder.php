1b1dd57851b920e4c79c20416073d8570d870d0f
Rias <hey@rias.be>
2026-02-05 10:07:03 +0100

    Cleanup

--- workbench/database/seeders/DatabaseSeeder.php
+++ workbench/database/seeders/DatabaseSeeder.php
@@ -6,7 +6,6 @@ namespace Workbench\Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Workbench\Database\Factories\UserFactory;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,10 +17,5 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // UserFactory::new()->times(10)->create();
-
-        UserFactory::new()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 }
