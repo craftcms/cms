8cac2010d0047dc6a2e204570be41743b77f0c18
Rias <hey@rias.be>
2026-02-05 11:31:30 +0100

    Run install automatically, log in automatically

--- workbench/database/seeders/DatabaseSeeder.php
+++ workbench/database/seeders/DatabaseSeeder.php
@@ -4,8 +4,16 @@ declare(strict_types=1);
 
 namespace Workbench\Database\Seeders;
 
+use CraftCms\Cms\Database\Migrations\Install;
+use CraftCms\Cms\Database\Table;
+use CraftCms\Cms\ProjectConfig\ProjectConfig;
+use CraftCms\Cms\Site\Data\Site;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Context;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +24,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // UserFactory::new()->times(10)->create();
+        if (! Schema::hasTable(Table::ELEMENTS)) {
+            Context::forgetHidden('craft.info');
+            Context::forgetHidden('craft.isInstalled');
+
+            File::cleanDirectory(config_path('craft/project'));
+            File::cleanDirectory(storage_path('runtime/compiled_classes'));
+
+            Cache::lock(ProjectConfig::MUTEX_NAME)->forceRelease();
+
+            $site = new Site(
+                name: 'Craft test site',
+                handle: 'defaultSite',
+                language: 'en-US',
+                baseUrl: config('app.url'),
+                primary: true,
+                hasUrls: true,
+            );
+
+            $migration = new Install(
+                username: 'craftcms',
+                password: 'craftcms2018!!',
+                email: 'support@craftcms.com',
+                site: $site,
+            );
+
+            $migration->up();
+        }
     }
 }
--- workbench/routes/web.php
+++ workbench/routes/web.php
@@ -1,5 +1,1 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', fn () => view('welcome'));
