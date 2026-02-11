11b9b38bf131c4317e9625738aeaa58b057d5ff3
Rias <hey@rias.be>
2026-02-11 09:51:16 +0100

    RouteToken

--- src/Http/Controllers/PreviewController.php
+++ src/Http/Controllers/PreviewController.php
@@ -23,8 +23,11 @@ final readonly class PreviewController
 {
     use EnforcesPermissions;
 
-    public function createToken(Request $request, RouteTokens $tokens, RouteToken $tokenData): JsonResponse|RedirectResponse
+    public function createToken(Request $request, RouteTokens $tokens): JsonResponse|RedirectResponse
     {
+        $tokenData = new RouteToken($request->all());
+        $tokenData->validate(throw: true);
+
         match (true) {
             isset($tokenData->draftId) => $this->requireSessionAuthorization("previewDraft:{$tokenData->draftId}"),
             isset($tokenData->revisionId) => $this->requireSessionAuthorization("previewRevision:{$tokenData->revisionId}"),
@@ -51,8 +54,11 @@ final readonly class PreviewController
         return new JsonResponse(compact('token'));
     }
 
-    public function preview(Request $request, Kernel $kernel, RouteToken $tokenData): mixed
+    public function preview(Request $request, Kernel $kernel): mixed
     {
+        $tokenData = new RouteToken($request->all());
+        $tokenData->validate(throw: true);
+
         $query = $tokenData->elementType::find()
             ->siteId($tokenData->siteId)
             ->status(null);
