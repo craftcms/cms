74eb5d3a9ffa935df1a4fc2b6ddb1042d9f8e582
brandonkelly <brandon@pixelandtonic.com>
2026-02-11 17:13:27 -0800

    Finish 5.9.9

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Release Notes for Craft CMS 5
 
-## Unreleased
+## 5.9.9 - 2026-02-11
 
 > [!WARNING]  
 > Relational condition rules’ element ID templates are now rendered in a sandboxed Twig environment, when `enableTwigSandbox` is enabled.
--- src/config/app.php
+++ src/config/app.php
@@ -3,7 +3,7 @@
 return [
     'id' => 'CraftCMS',
     'name' => 'Craft CMS',
-    'version' => '5.9.8',
+    'version' => '5.9.9',
     'schemaVersion' => '5.9.0.8',
     'minVersionRequired' => '4.5.0',
     'basePath' => dirname(__DIR__), // Defines the @app alias
