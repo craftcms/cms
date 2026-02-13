5d0a939acfb17afb2e99b03854f88c75faaf4c01
brandonkelly <brandon@pixelandtonic.com>
2026-02-13 15:19:15 -0800

    Fixed `0` icons

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@
 - Element indexes now show “Paste” buttons alongside bulk element action buttons. ([#18427](https://github.com/craftcms/cms/issues/18427))
 - Impoved the performance of `craft\helpers\Typecast`. ([#18426](https://github.com/craftcms/cms/pull/18426))
 - Fixed a bug where some Matrix bulk action labels weren’t getting translated.
+- Fixed a bug where global nav items weren’t showing an icon if the icon was set to `0`.
 
 ## 5.9.10 - 2026-02-12
 
--- src/controllers/ElementIndexSettingsController.php
+++ src/controllers/ElementIndexSettingsController.php
@@ -352,7 +352,9 @@ class ElementIndexSettingsController extends BaseElementsController
         $sourcesService = Craft::$app->getElementSources();
         $sourcesService->saveSources($elementType, $newSourceConfigs);
         if ($multiPage) {
-            $sourcesService->savePageSettings($elementType, array_map('array_filter', $pageSettings));
+            $sourcesService->savePageSettings($elementType, array_map(function(array $settings) {
+                return array_filter($settings, fn($setting) => $setting !== null && $setting !== '');
+            }, $pageSettings));
         }
 
         Craft::$app->getSession()->setSuccess(Craft::t('app', 'Source settings saved'));
