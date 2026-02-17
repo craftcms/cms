2670bc26472b0dfcb0d372055544d01ae85be321
Brian Hanson <brian@brianhanson.net>
2026-02-17 13:50:21 -0600

    Allow monospace text area

--- packages/craftcms-cp/src/components/textarea/textarea.ts
+++ packages/craftcms-cp/src/components/textarea/textarea.ts
@@ -1,11 +1,15 @@
 import styles from './textarea.styles.js';
 import {inputStyles} from '../../styles/form.styles.js';
 import {LionTextarea} from '@lion/ui/textarea.js';
+import {property} from 'lit/decorators.js';
 
 export default class CraftTextarea extends LionTextarea {
   static override get styles() {
     return [...super.styles, inputStyles, styles];
   }
+
+  @property({type: Boolean, reflect: true})
+  monospace: boolean = false;
 }
 
 if (!customElements.get('craft-textarea')) {
