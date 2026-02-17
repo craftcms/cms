8596030464b1c21d8d1d69ad8e1918e35fcf0d98
Rias <hey@rias.be>
2026-02-17 10:01:15 +0100

    [6.x] Tests refactor + memory improvements (#18435)
    * Move tests into Feature & Unit
    
    * Move eligible tests to Unit
    
    * Move remaining tests
    
    * Fix legacy test
    
    * Display errors
    
    * Memory limit
    
    * Run ArchTest separately
    
    * Reduce output in GitHub actions
    
    * Move LanguageRuleTest to Unit
    
    * Update testing guidelines
    
    * phpstan
    
    * Fix unit tests
    
    * Make sure to set CRAFT_NO_TRIALS to false in test

---
+++ tests/Feature/User/PasswordResetTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CraftCms\Cms\Tests\Feature\User;
+
+use CraftCms\Cms\Notifications\Channels\CraftChannel;
+use CraftCms\Cms\Support\Facades\Users;
+use CraftCms\Cms\User\Elements\User;
+use CraftCms\Cms\User\Models\User as UserModel;
+use CraftCms\Cms\User\Notifications\ResetPasswordNotification;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Password;
+
+beforeEach(function () {
+    Notification::fake();
+});
+
+test('sendPasswordResetEmail dispatches ResetPasswordNotification', function () {
+    $user = UserModel::factory()->createElement();
+    $user = User::find()->id($user->id)->one();
+
+    Users::sendPasswordResetEmail($user);
+
+    Notification::assertSentTo(
+        $user,
+        ResetPasswordNotification::class,
+        fn ($notification, $channels) => in_array(CraftChannel::class, $channels)
+    );
+});
+
+test('isVerificationCodeValidForUser handles Laravel tokens', function () {
+    $user = UserModel::factory()->createElement();
+    $user = User::find()->id($user->id)->one();
+
+    $token = Password::broker('craft')->createToken($user);
+
+    expect(Password::broker('craft')->tokenExists($user, $token))->toBeTrue();
+});
