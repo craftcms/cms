647fa470074c413b93b1ac6f086f37cf71e26c06
Rias <hey@rias.be>
2026-02-17 11:29:25 +0100

    Add alias

---
+++ yii2-adapter/legacy/web/twig/TemplateLoader.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace craft\web\twig;
+
+/** @phpstan-ignore-next-line */
+if (false) {
+    /**
+     * Loads Craft templates into Twig.
+     *
+     * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
+     * @since 3.0.0
+     * @deprecated 6.0.0 use {@see \CraftCms\Cms\Twig\TemplateLoader} instead.
+     */
+    class TemplateLoader
+    {
+    }
+}
+
+class_alias(\CraftCms\Cms\Twig\Exceptions\TemplateLoaderException::class, TemplateLoader::class);
