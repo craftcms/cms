fe9da679d782b7a74542859a269ec369d2046919
Brian Hanson <brian@brianhanson.net>
2026-02-17 22:10:37 -0600

    Remove unnecessary middleware

--- routes/cp.php
+++ routes/cp.php
@@ -33,7 +33,6 @@ use CraftCms\Cms\Http\Controllers\Users\PermissionsController;
 use CraftCms\Cms\Http\Controllers\Users\PreferencesController;
 use CraftCms\Cms\Http\Controllers\Users\UsersController;
 use CraftCms\Cms\Http\Controllers\Utilities\UtilitiesController;
-use CraftCms\Cms\Http\Middleware\HandleInertiaRequests;
 use CraftCms\Cms\Http\Middleware\RequireAdmin;
 use CraftCms\Cms\Http\Middleware\RequireAdminChanges;
 use CraftCms\Cms\Http\Middleware\RequireEdition;
@@ -61,7 +60,6 @@ Route::middleware(['auth:craft', 'can:accessCp'])->group(function () {
 
     Route::get('utilities', [UtilitiesController::class, 'index']);
     Route::get('utilities/{id}/{extra?}', [UtilitiesController::class, 'show'])
-        ->middleware([HandleInertiaRequests::class])
         ->where('extra', '.*')
         ->name('utilities.show');
 
