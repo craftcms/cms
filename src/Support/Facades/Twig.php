b3e92347f16031d52f84e6c9edd238418b15c4a7
Rias <hey@rias.be>
2026-02-18 13:09:01 +0100

    Update Facade docblock

--- src/Support/Facades/Twig.php
+++ src/Support/Facades/Twig.php
@@ -4,18 +4,28 @@ declare(strict_types=1);
 
 namespace CraftCms\Cms\Support\Facades;
 
-use CraftCms\Cms\Twig\Environment;
 use CraftCms\Cms\Twig\TemplateRenderer;
-use CraftCms\Cms\View\TemplateMode;
 use Illuminate\Support\Facades\Facade;
 use Override;
-use Twig\Extension\ExtensionInterface;
 
 /**
- * @method static Environment get(?TemplateMode $mode = null)
- * @method static Environment create()
- * @method static void registerExtension(ExtensionInterface $extension, ?TemplateMode $mode = null)
- * @method static void set(Environment $twig, ?TemplateMode $mode = null)
+ * @method static \CraftCms\Cms\Twig\Environment get(\CraftCms\Cms\View\TemplateMode|null $mode = null)
+ * @method static void set(\CraftCms\Cms\Twig\Environment $twig, \CraftCms\Cms\View\TemplateMode|null $mode = null)
+ * @method static \CraftCms\Cms\Twig\Environment create()
+ * @method static void registerExtension(\Twig\Extension\ExtensionInterface $extension, \CraftCms\Cms\View\TemplateMode|null $mode = null)
+ * @method static void head()
+ * @method static void beginBody()
+ * @method static void endBody()
+ * @method static bool isRenderingTemplate()
+ * @method static string renderTemplate(string $template, array $variables = [], ?\CraftCms\Cms\View\TemplateMode $templateMode = null)
+ * @method static string renderSandboxedTemplate(string $template, array $variables = [], \CraftCms\Cms\View\TemplateMode|null $templateMode = null)
+ * @method static string renderPageTemplate(string $template, array $variables = [], \CraftCms\Cms\View\TemplateMode|null $templateMode = null)
+ * @method static bool isRenderingPageTemplate()
+ * @method static string renderString(string $template, array $variables = [], \CraftCms\Cms\View\TemplateMode $templateMode = 'site', bool $escapeHtml = false)
+ * @method static string renderSandboxedString(string $template, array $variables = [], \CraftCms\Cms\View\TemplateMode $templateMode = 'site', bool $escapeHtml = false)
+ * @method static string renderObjectTemplate(string $template, mixed $object, array $variables = [], \CraftCms\Cms\View\TemplateMode $templateMode = 'site')
+ * @method static string renderSandboxedObjectTemplate(string $template, mixed $object, array $variables = [], \CraftCms\Cms\View\TemplateMode $templateMode = 'site')
+ * @method static string normalizeObjectTemplate(string $template)
  *
  * @see \CraftCms\Cms\Twig\Twig
  * @see \CraftCms\Cms\Twig\TemplateRenderer
