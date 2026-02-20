de2bb891f26d1fe90deaf7b5ea7c70d30cca5d1e
Brian Hanson <brian@brianhanson.net>
2026-02-20 10:59:15 -0600

    Cleanup

---
+++ resources/js/components/Text.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  import {type Component, computed} from 'vue';
+  import {t} from '@craftcms/cp/utilities/translate.ts';
+
+  const props = withDefaults(
+    defineProps<{
+      as?: string | Component;
+      template: string;
+      params?: Record<string, string | number>;
+    }>(),
+    {as: 'div'}
+  );
+
+  const text = computed(() => t(props.template, props.params));
+</script>
+
+<template>
+  <component :is="as" v-bind="$attrs">{{ text }}</component>
+</template>
+
+<style scoped lang="scss"></style>
