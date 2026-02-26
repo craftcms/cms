8cce07750d03706c8f42bca7745338855702af3f
i-just <iwona@pixelandtonic.com>
2026-02-26 16:20:30 +0100

    redirect to referral URL on element save again + adjustment

--- src/helpers/UrlHelper.php
+++ src/helpers/UrlHelper.php
@@ -584,13 +584,22 @@ class UrlHelper
     {
         $referrer = Craft::$app->getRequest()->getReferrer();
 
-        // Make sure it didn't refer itself
-        if ($referrer === Craft::$app->getRequest()->getFullUri()) {
+        if ($referrer === null) {
             return null;
         }
 
         // Make sure the CP referred it
         if (!str_starts_with($referrer, self::baseCpUrl())) {
+            return null;
+        }
+
+        // to ensure we're comparing uris strip base cp url and query string from the referrer first
+        $referrerFullUri = ltrim(str_replace(self::baseCpUrl(), '', $referrer), '/');
+        $referrerFullUri = substr($referrerFullUri, 0, strpos($referrerFullUri, '?') ?: null);
+
+        // Make sure it didn't refer itself
+        if ($referrerFullUri === Craft::$app->getRequest()->getFullUri()) {
+//        if ($referrer === Craft::$app->getRequest()->getFullUri()) {
             return null;
         }
 
