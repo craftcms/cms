05c1444c93f3b5853289d9b0e038a90af7605ae0
Brandon Kelly <brandon@pixelandtonic.com>
2026-03-06 09:43:50 -0800

    New translations app.php (Hebrew) [ci skip]

--- src/translations/he/app.php
+++ src/translations/he/app.php
@@ -2333,6 +2333,7 @@ return [
     '{type} deleted for site.' => '{type} נמחק עבור האתר.',
     '{type} deleted.' => '{type} נמחק.',
     '{type} duplicated.' => '{type} שוכפל.',
+    '{type} entries are no longer allowed in this field. Please choose a different entry type.' => '{type} entries are no longer allowed in this field. Please choose a different entry type.',
     '{type} entries are no longer allowed in this section. Please choose a different entry type.' => 'רשומות {type} אינן מותרות עוד בסקשן הזה. בחר סוג רשומה אחר.',
     '{type} not restored.' => '{type} לא שוחזרו.',
     '{type} restored.' => '{type} שוחזרו.',
