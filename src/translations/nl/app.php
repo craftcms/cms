f76bb9d58db1c2e19029a3caeba1b67271c563bb
Brandon Kelly <brandon@pixelandtonic.com>
2026-03-06 09:44:01 -0800

    New translations app.php (Dutch) [ci skip]

--- src/translations/nl/app.php
+++ src/translations/nl/app.php
@@ -2333,6 +2333,7 @@ return [
     '{type} deleted for site.' => '{type} verwijderd voor site.',
     '{type} deleted.' => '{type} verwijderd.',
     '{type} duplicated.' => '{type} gedupliceerd.',
+    '{type} entries are no longer allowed in this field. Please choose a different entry type.' => '{type} entries are no longer allowed in this field. Please choose a different entry type.',
     '{type} entries are no longer allowed in this section. Please choose a different entry type.' => '{type} zijn niet langer toegestaan in deze sectie. Kies een ander type.',
     '{type} not restored.' => '{type} niet hersteld.',
     '{type} restored.' => '{type} hersteld.',
