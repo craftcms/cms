eaf192f080f0d7c71d4c7b2060e2fdf10113a502
brandonkelly <brandon@pixelandtonic.com>
2026-03-06 11:01:35 -0800

    Fixed cross-site element validation
    Fixes the initial issue reported in #18292

--- src/services/Elements.php
+++ src/services/Elements.php
@@ -1242,16 +1242,17 @@ class Elements extends Component
      * callback function.
      *
      * @param callable $callback
+     * @return mixed
      * @since 5.3.0
      */
-    public function ensureBulkOp(callable $callback): void
+    public function ensureBulkOp(callable $callback): mixed
     {
         if (empty($this->bulkKeys)) {
             $bulkKey = $this->beginBulkOp();
         }
 
         try {
-            $callback();
+            return $callback();
         } finally {
             if (isset($bulkKey)) {
                 $this->endBulkOp($bulkKey);
@@ -3923,7 +3924,7 @@ class Elements extends Component
             }
         }
 
-        $this->ensureBulkOp(function() use (
+        $success = $this->ensureBulkOp(function() use (
             $element,
             $isNewElement,
             $originalFirstSave,
@@ -4340,7 +4341,13 @@ class Elements extends Component
                     }
                 }
             }
+
+            return true;
         });
+
+        if (!$success) {
+            return false;
+        }
 
         // Fire an 'afterSaveElement' event
         if ($this->hasEventHandlers(self::EVENT_AFTER_SAVE_ELEMENT)) {
