3f41712cbf3dc73bf8c75319b50b62ed0881b037
Brandon Kelly <brandon@pixelandtonic.com>
2026-03-06 09:44:07 -0800

    New translations app.php (Russian) [ci skip]

--- src/translations/ru/app.php
+++ src/translations/ru/app.php
@@ -2334,6 +2334,7 @@ return [
     '{type} deleted for site.' => '{type} для сайта удален.',
     '{type} deleted.' => '{type} удален.',
     '{type} duplicated.' => 'Элемент «{type}» дублирован.',
+    '{type} entries are no longer allowed in this field. Please choose a different entry type.' => '{type} entries are no longer allowed in this field. Please choose a different entry type.',
     '{type} entries are no longer allowed in this section. Please choose a different entry type.' => 'Записи типа {type} больше нельзя использовать в этом разделе. Выберите другой тип записи.',
     '{type} not restored.' => 'Восстановление не выполнено: {type}.',
     '{type} restored.' => 'Восстановление выполнено: {type}.',
