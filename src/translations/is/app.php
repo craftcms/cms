995c6c4b7af5e2a4652b51b7a3985f5686babb0c
Brandon Kelly <brandon@pixelandtonic.com>
2026-03-06 09:44:19 -0800

    New translations app.php (Icelandic) [ci skip]

--- src/translations/is/app.php
+++ src/translations/is/app.php
@@ -2333,6 +2333,7 @@ return [
     '{type} deleted for site.' => '{type} eytt fyrir síðuna.',
     '{type} deleted.' => '{type} eytt.',
     '{type} duplicated.' => '{type} tvítekið.',
+    '{type} entries are no longer allowed in this field. Please choose a different entry type.' => '{type} entries are no longer allowed in this field. Please choose a different entry type.',
     '{type} entries are no longer allowed in this section. Please choose a different entry type.' => '{type} færslur eru ekki lengur leyfðar í þessu svæði. Vinsamlegast veldu aðra færslutegund.',
     '{type} not restored.' => '{type} ekki endurheimt.',
     '{type} restored.' => '{type} endurheimt.',
