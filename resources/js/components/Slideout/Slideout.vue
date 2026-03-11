bd9610570da89627a3bba33fd10910000d9b8b44
Brian Hanson <brian@brianhanson.net>
2026-03-11 13:29:09 -0500

    Improve width

--- resources/js/components/Slideout/Slideout.vue
+++ resources/js/components/Slideout/Slideout.vue
@@ -262,7 +262,8 @@
     flex-grow: 1;
     position: fixed;
     inset-block: 0;
-    width: minmax(320px, 55%);
+    min-width: 320px;
+    width: 55%;
     max-width: 100%;
     background-color: var(--c-surface-overlay);
     box-shadow: var(--c-shadow-overlay);
