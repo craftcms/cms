b586b6852c39c7aa599f00d88c9e4e008979f36f
brandonkelly <47792+brandonkelly@users.noreply.github.com>
2026-03-30 22:10:28 +0000

    Update facade docblocks

--- src/Support/Facades/ElementSources.php
+++ src/Support/Facades/ElementSources.php
@@ -8,11 +8,24 @@ use Illuminate\Support\Facades\Facade;
 use Override;
 
 /**
- * @method static \Illuminate\Support\Collection getSources(string $elementType, string $context = \CraftCms\Cms\Element\ElementSources::CONTEXT_INDEX, bool $withDisabled = false, ?string $page = null)
- * @method static array|null findSource(string $elementType, string $sourceKey, string $context = \CraftCms\Cms\Element\ElementSources::CONTEXT_INDEX, bool $withDisabled = false, ?string $page = null)
- * @method static bool sourceExists(string $elementType, string $sourceKey, string $context = \CraftCms\Cms\Element\ElementSources::CONTEXT_INDEX, bool $withDisabled = false, ?string $page = null)
- * @method static array getPages(string $elementType)
- * @method static \CraftCms\Cms\FieldLayout\FieldLayout|null getSourceFieldLayout(string $elementType, string $sourceKey, string $context = \CraftCms\Cms\Element\ElementSources::CONTEXT_INDEX, ?string $page = null)
+ * @method static \Illuminate\Support\Collection filterExtraHeadings(array[]|\Illuminate\Support\Collection $sources)
+ * @method static \Illuminate\Support\Collection getSources(string $elementType, string $context = 'index', bool $withDisabled = false, string|null $page = null)
+ * @method static bool sourceExists(string $elementType, string $sourceKey, string $context = 'index', bool $withDisabled = false, string|null $page = null)
+ * @method static array|null findSource(string $elementType, string $sourceKey, string $context = 'index', bool $withDisabled = false, string|null $page = null)
+ * @method static \Illuminate\Support\Collection getPages(string $elementType)
+ * @method static string|null getFirstPage(string $elementType, string $context = 'index', bool $withDisabled = false)
+ * @method static bool pageExists(string $elementType, string $page, string $context = 'index', bool $withDisabled = false)
+ * @method static string pageNameId(string $page)
+ * @method static void saveSources(string $elementType, array $sources)
+ * @method static \Illuminate\Support\Collection getAvailableTableAttributes(string $elementType)
+ * @method static \Illuminate\Support\Collection getTableAttributes(string $elementType, string $sourceKey, string[]|null $customAttributes = null)
+ * @method static \Illuminate\Support\Collection getFieldLayoutsForSource(string $elementType, string $sourceKey)
+ * @method static \Illuminate\Support\Collection getSourceSortOptions(string $elementType, string $sourceKey)
+ * @method static \Illuminate\Support\Collection getSortOptionsForFieldLayouts(\CraftCms\Cms\FieldLayout\FieldLayout[]|\Illuminate\Support\Collection $fieldLayouts)
+ * @method static \Illuminate\Support\Collection getSourceTableAttributes(string $elementType, string $sourceKey)
+ * @method static \Illuminate\Support\Collection getTableAttributesForFieldLayouts(\CraftCms\Cms\FieldLayout\FieldLayout[]|\Illuminate\Support\Collection $fieldLayouts)
+ * @method static array getPageSettings(string $elementType)
+ * @method static void savePageSettings(string $elementType, array $pageSettings)
  *
  * @see \CraftCms\Cms\Element\ElementSources
  */
