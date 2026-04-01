0d33cf55a71a1e8ca4450d02ff1a84c683993cfa
brandonkelly <brandon@pixelandtonic.com>
2026-04-01 09:44:05 -0700

    Fixed types

--- src/records/Section.php
+++ src/records/Section.php
@@ -22,8 +22,8 @@ use yii2tech\ar\softdelete\SoftDeleteBehavior;
  * @property string $handle Handle
  * @property string $type Type
  * @property bool $enableVersioning Enable versioning
- * @property bool $maxAuthors The maximum number of authors
- * @property bool $propagationMethod Propagation method
+ * @property int|null $maxAuthors The maximum number of authors
+ * @property string $propagationMethod Propagation method
  * @property string $defaultPlacement Default placement
  * @property array|null $previewTargets Preview targets
  * @property Section_SiteSettings[] $siteSettings Site settings
