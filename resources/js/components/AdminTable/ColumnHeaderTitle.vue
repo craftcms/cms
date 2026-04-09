8479fc4df9897d18019aa4a4c10046a10e6ca6e0
Guadalupe Camacho <lupe@pixelandtonic.com>
2026-04-09 11:11:38 -0700

    Unset button styles and get to span entire cell

--- resources/js/components/AdminTable/ColumnHeaderTitle.vue
+++ resources/js/components/AdminTable/ColumnHeaderTitle.vue
@@ -15,4 +15,17 @@
   <slot v-else />
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+  button {
+    all: unset;
+
+    &::after {
+      position: absolute;
+      top: 0;
+      left: 0;
+      content: '';
+      width: 100%;
+      height: 100%;
+    }
+  }
+</style>
