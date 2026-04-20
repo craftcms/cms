74928d1e6e503c329abd244bd7b0fae5e82743e5
Rias <hey@rias.be>
2026-04-20 11:30:33 +0200

    Fix tests

--- src/Plugin/Testing/PluginTestCase.php
+++ src/Plugin/Testing/PluginTestCase.php
@@ -6,6 +6,7 @@ namespace CraftCms\Cms\Plugin\Testing;
 
 use CraftCms\Cms\Database\LaravelMigrations;
 use CraftCms\Cms\Database\Migrations\Install;
+use CraftCms\Cms\Database\Migrator;
 use CraftCms\Cms\Providers\CraftServiceProvider;
 use CraftCms\Cms\Site\Data\Site;
 use CraftCms\Yii2Adapter\Yii2ServiceProvider;
@@ -40,10 +41,17 @@ abstract class PluginTestCase extends BaseTestCase
             password: 'craftcms2018!!',
             email: 'support@craftcms.com',
             site: $site,
-        );
+        )->silent();
 
-        $migration->up();
-        app(LaravelMigrations::class)->ensureSessionsTable();
+        $migrator = app(Migrator::class)->track('craft');
+        $migrator->runMigration($migration, 'up');
+        $migrator->getRepository()->log('Install', 1);
+
+        foreach ($migrator->getPendingMigrations() as $file) {
+            $migrator->getRepository()->log($migrator->getMigrationName($file), 1);
+        }
+
+        app(LaravelMigrations::class)->install($migrator);
     }
 
     #[Override]
