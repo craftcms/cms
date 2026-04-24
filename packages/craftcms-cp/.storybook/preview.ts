8b05b7c0a50ee6e4e8a7f72386334a0471bc34ad
Guadalupe Camacho <lupe@pixelandtonic.com>
2026-04-24 09:14:30 -0700

    Change default theme back to light

--- packages/craftcms-cp/.storybook/preview.ts
+++ packages/craftcms-cp/.storybook/preview.ts
@@ -57,7 +57,7 @@ const preview: Preview = {
         light: 'light',
         dark: 'dark',
       },
-      defaultTheme: 'dark',
+      defaultTheme: 'light',
       attributeName: 'data-theme',
     }),
   ],
