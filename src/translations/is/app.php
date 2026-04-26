a22453de39af7f67f57e3885c52cb8c9b68cf7a6
Brandon Kelly <brandon@pixelandtonic.com>
2026-04-26 16:21:02 -0700

    New translations app.php (Icelandic) [ci skip]

--- src/translations/is/app.php
+++ src/translations/is/app.php
@@ -162,7 +162,6 @@ return [
     'Are you sure you want to delete this image?' => 'Ertu viss um að þú viljir eyða þessari mynd?',
     'Are you sure you want to delete this route?' => 'Ertu viss um að þú viljir eyða þessari leið?',
     'Are you sure you want to delete this {type}?' => 'Ertu viss um að þú viljir eyða þessari {type}?',
-    'Are you sure you want to delete {numElements, plural, =1{this} other{these}} {type} along with {numElements, plural, =1{its} other{their}} descendants?' => 'Are you sure you want to delete {numElements, plural, =1{this} other{these}} {type} along with {numElements, plural, =1{its} other{their}} descendants?',
     'Are you sure you want to delete {numElements, plural, =1{this} other{these}} {type}?' => 'Are you sure you want to delete {numElements, plural, =1{this} other{these}} {type}?',
     'Are you sure you want to delete “{name}” and all entries of that type?' => 'Ertu viss um að þú viljir eyða „{name}“ og öllum færslum af þeirri gerð?',
     'Are you sure you want to delete “{name}” and all its entries?' => 'Ertu viss um að þú viljir eyða „{name}“ og öllum færslum þess?',
