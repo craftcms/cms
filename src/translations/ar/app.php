39aa4bf9b42e5f30fd954f49b6b1aecb3774a615
Brandon Kelly <brandon@pixelandtonic.com>
2026-04-26 16:20:28 -0700

    New translations app.php (Arabic) [ci skip]

--- src/translations/ar/app.php
+++ src/translations/ar/app.php
@@ -162,7 +162,6 @@ return [
     'Are you sure you want to delete this image?' => 'هل تريد بالتأكيد حذف هذه الصورة؟',
     'Are you sure you want to delete this route?' => 'هل أنت متأكد أنك تريد حذف هذا الطريق؟',
     'Are you sure you want to delete this {type}?' => 'هل أنت متأكد أنك تريد حذف هذا {type}؟',
-    'Are you sure you want to delete {numElements, plural, =1{this} other{these}} {type} along with {numElements, plural, =1{its} other{their}} descendants?' => 'Are you sure you want to delete {numElements, plural, =1{this} other{these}} {type} along with {numElements, plural, =1{its} other{their}} descendants?',
     'Are you sure you want to delete {numElements, plural, =1{this} other{these}} {type}?' => 'Are you sure you want to delete {numElements, plural, =1{this} other{these}} {type}?',
     'Are you sure you want to delete “{name}” and all entries of that type?' => 'هل تريد بالتأكيد حذف "{name}" وكل الإدخالات من هذا النوع؟',
     'Are you sure you want to delete “{name}” and all its entries?' => 'هل تريد بالتأكيد حذف ”{name}“ وكل الإدخالات المرتبطة به؟',
