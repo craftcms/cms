38af62d86faa2fc3f06ac898dd3bbf28a311a6d2
Brandon Kelly <brandon@pixelandtonic.com>
2026-04-26 16:20:52 -0700

    New translations app.php (Russian) [ci skip]

--- src/translations/ru/app.php
+++ src/translations/ru/app.php
@@ -162,7 +162,6 @@ return [
     'Are you sure you want to delete this image?' => 'Вы уверены, что хотите удалить данное изображение?',
     'Are you sure you want to delete this route?' => 'Вы уверены, что хотите удалить этот маршрут?',
     'Are you sure you want to delete this {type}?' => 'Вы уверены, что хотите удалить этот {type}?',
-    'Are you sure you want to delete {numElements, plural, =1{this} other{these}} {type} along with {numElements, plural, =1{its} other{their}} descendants?' => 'Are you sure you want to delete {numElements, plural, =1{this} other{these}} {type} along with {numElements, plural, =1{its} other{their}} descendants?',
     'Are you sure you want to delete {numElements, plural, =1{this} other{these}} {type}?' => 'Are you sure you want to delete {numElements, plural, =1{this} other{these}} {type}?',
     'Are you sure you want to delete “{name}” and all entries of that type?' => 'Вы уверены, что хотите удалить «{name}» и все записи этого типа?',
     'Are you sure you want to delete “{name}” and all its entries?' => 'Вы уверены, что хотите удалить «{name}» и все его записи?',
