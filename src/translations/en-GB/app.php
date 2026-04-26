9e2d246f893de363a04a404b20fe8e401af5e5bd
Brandon Kelly <brandon@pixelandtonic.com>
2026-04-26 16:21:08 -0700

    New translations app.php (English, United Kingdom) [ci skip]

--- src/translations/en-GB/app.php
+++ src/translations/en-GB/app.php
@@ -162,7 +162,6 @@ return [
     'Are you sure you want to delete this image?' => 'Are you sure you want to delete this image?',
     'Are you sure you want to delete this route?' => 'Are you sure you want to delete this route?',
     'Are you sure you want to delete this {type}?' => 'Are you sure you want to delete this {type}?',
-    'Are you sure you want to delete {numElements, plural, =1{this} other{these}} {type} along with {numElements, plural, =1{its} other{their}} descendants?' => 'Are you sure you want to delete {numElements, plural, =1{this} other{these}} {type} along with {numElements, plural, =1{its} other{their}} descendants?',
     'Are you sure you want to delete {numElements, plural, =1{this} other{these}} {type}?' => 'Are you sure you want to delete {numElements, plural, =1{this} other{these}} {type}?',
     'Are you sure you want to delete “{name}” and all entries of that type?' => 'Are you sure you want to delete “{name}” and all entries of that type?',
     'Are you sure you want to delete “{name}” and all its entries?' => 'Are you sure you want to delete “{name}” and all its entries?',
