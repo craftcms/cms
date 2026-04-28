b2a768b7877c1c6b2f00138b59c0e9146900aff8
Rias <hey@rias.be>
2026-04-28 13:20:56 +0200

    Normalize in trait as well

--- src/Plugin/Concerns/HasConfig.php
+++ src/Plugin/Concerns/HasConfig.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace CraftCms\Cms\Plugin\Concerns;
 
+use CraftCms\Cms\Support\File;
+
 trait HasConfig
 {
     /** @var bool Whether the config file should be automatically registered */
@@ -16,7 +18,7 @@ trait HasConfig
         }
 
         $handle = self::getInstance()->handle;
-        $source = sprintf('%s/config/%s.php', dirname($this->getBasePath()), $handle);
+        $source = File::normalizePath(sprintf('%s/config/%s.php', dirname($this->getBasePath()), $handle));
 
         if (! file_exists($source)) {
             return;
