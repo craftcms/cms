346cfb9748fac18086578a7215131ce8d2346841
Rias <hey@rias.be>
2026-05-05 10:41:14 +0200

    RegisterTemplateGlobals -> TemplateGlobalsResolving

--- src/View/Events/TemplateGlobalsResolving.php
+++ src/View/Events/TemplateGlobalsResolving.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace CraftCms\Cms\View\Events;
 
-class RegisterTemplateGlobals
+class TemplateGlobalsResolving
 {
     public function __construct(
         public array $globals,
