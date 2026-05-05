3550b40ba0fa90ef73a85abff62a553104802dfe
Rias <hey@rias.be>
2026-05-05 10:23:40 +0200

    BeforeResaveElements -> ElementsResaving

--- src/Element/Events/ElementsResaving.php
+++ src/Element/Events/ElementsResaving.php
@@ -6,7 +6,7 @@ namespace CraftCms\Cms\Element\Events;
 
 use CraftCms\Cms\Element\Queries\Contracts\ElementQueryInterface;
 
-class BeforeResaveElements
+class ElementsResaving
 {
     public function __construct(
         public ElementQueryInterface $query,
