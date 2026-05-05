6d1b9c180960b45f2d29dd70b26b973b21c95661
Rias <hey@rias.be>
2026-05-05 16:10:31 +0200

    Alias User element

---
+++ src/User/Concerns/LegacyConstants.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CraftCms\Cms\User\Concerns;
+
+if (! class_exists(LegacyConstants::class)) {
+    /**
+     * @internal
+     */
+    #[\Deprecated(message: 'This is a hook for the yii2-adapter to define its constants.', since: '6.0.0')]
+    trait LegacyConstants {}
+}
