8cb8c018523995857b4ddb2f44385f4633c2c162
Rias <hey@rias.be>
2026-05-05 10:21:43 +0200

    AfterResaveElements -> ElementsResaved

--- src/Element/Events/ElementsResaved.php
+++ src/Element/Events/ElementsResaved.php
@@ -6,7 +6,7 @@ namespace CraftCms\Cms\Element\Events;
 
 use CraftCms\Cms\Element\Queries\Contracts\ElementQueryInterface;
 
-class AfterResaveElements
+class ElementsResaved
 {
     public function __construct(
         public ElementQueryInterface $query,
