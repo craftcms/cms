15ec296d44095637061e17f33fa96285240924ae
Brian Hanson <brian@brianhanson.net>
2026-05-07 10:53:47 -0500

    Move logout route back

--- routes/cp.php
+++ routes/cp.php
@@ -63,7 +63,6 @@ Route::get('install', [InstallController::class, 'index']);
 Route::middleware('craft.web')->group(function () {
     Route::get(CpAuthPath::Login->value, [LoginController::class, 'showLogin']);
     Route::post(CpAuthPath::Login->value, [LoginController::class, 'attemptLogin']);
-    Route::get(CpAuthPath::Logout->value, [LoginController::class, 'logout']);
     Route::get(CpAuthPath::TwoFactorChallenge->value, [TwoFactorAuthenticationController::class, 'showForm']);
     Route::get(CpAuthPath::SetPassword->value, [SetPasswordController::class, 'show']);
     Route::post(CpAuthPath::SetPassword->value, [SetPasswordController::class, 'store']);
@@ -77,6 +76,8 @@ Route::middleware('craft.web')->group(function () {
 Route::middleware(['auth:craft', 'can:accessCp'])->group(function () {
     Route::get('/', [DashboardController::class, 'redirect']);
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::get(CpAuthPath::Logout->value, [LoginController::class, 'logout']);
 
     Route::get('utilities', [UtilitiesController::class, 'index']);
 
