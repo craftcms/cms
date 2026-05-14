6203db89de5765315967a9c9c123a08624535098
Brian Hanson <brian@brianhanson.net>
2026-05-14 15:49:35 -0500

    A few more

--- src/Asset/Elements/Asset.php
+++ src/Asset/Elements/Asset.php
@@ -2013,7 +2013,7 @@ JS, [
     protected function thumbSvg(): string
     {
         if ($this->isFolder) {
-            return file_get_contents(Aliases::get('@resources/images/thumbs/folder.svg'));
+            return file_get_contents(Aliases::get('@resources/public/images/thumbs/folder.svg'));
         }
 
         return AssetsHelper::iconSvg($this->getExtension());
