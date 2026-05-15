2c9545637575dc81c38cd49c441c2b8ac8f1268b
Brian Hanson <brian@brianhanson.net>
2026-05-15 15:26:14 -0500

    Uncommit vite change

--- src/Providers/AppServiceProvider.php
+++ src/Providers/AppServiceProvider.php
@@ -24,7 +24,6 @@ use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Console\AboutCommand;
 use Illuminate\Foundation\Events\LocaleUpdated;
-use Illuminate\Foundation\Vite;
 use Illuminate\Http\Client\Factory;
 use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Http\Exceptions\ThrottleRequestsException;
@@ -83,10 +82,6 @@ class AppServiceProvider extends ServiceProvider
 
         $this->setNamespace();
         $this->bootAliases();
-
-        app(Vite::class)
-            ->useHotFile("{$this->root}/resources/hot")
-            ->useBuildDirectory('vendor/craft/build');
 
         $this->app->booted(function () {
             /**
