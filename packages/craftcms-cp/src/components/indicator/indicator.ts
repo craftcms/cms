33537005202aceb8b6c6d0b65b2f285aadf26270
Guadalupe Camacho <lupe@pixelandtonic.com>
2026-05-15 11:48:55 -0700

    Add static color variants and incorporate into indicator colors

--- packages/craftcms-cp/src/components/indicator/indicator.ts
+++ packages/craftcms-cp/src/components/indicator/indicator.ts
@@ -2,50 +2,30 @@ import {css, html, LitElement} from 'lit';
 import {property} from 'lit/decorators.js';
 import {Variant, type VariantKey} from '@src/types';
 import {classMap} from 'lit/directives/class-map.js';
-import variantsStyles from '@src/styles/variants.styles';
+import staticVariantsStyles from '@src/styles/static-variants.styles';
 
 export default class CraftIndicator extends LitElement {
   static override styles = [
-    variantsStyles,
+    staticVariantsStyles,
     css`
       .indicator {
         display: inline-flex;
         aspect-ratio: 1;
         width: var(--c-indicator-size, 0.5em);
         border-radius: var(--c-radius-full);
-        color: var(--c-color-on-loud);
-        background-color: var(--indicator-background-color);
-        border: 1px solid var(--indicator-border-color);
+        color: var(--c-color-static-on);
+        background-color: var(--c-color-static-fill);
+        border: 1px solid var(--c-color-static-border);
       }
 
       .indicator--empty {
-        --indicator-background-color: none;
-        --indicator-border-color: var(--c-text-default);
-      }
-
-      .indicator--success {
-        --indicator-background-color: var(--c-color-success-static);
-        --indicator-border-color: var(--c-success-border-color);
-      }
-
-      .indicator--danger {
-        --indicator-background-color: var(--c-danger-static-color);
-        --indicator-border-color: var(--c-danger-border-color);
-      }
-
-      .indicator--warning {
-        --indicator-background-color: var(--c-color-warning-static);
-        --indicator-border-color: var(--c-warning-border-color);
-      }
-
-      .indicator--info {
-        --indicator-background-color: var(--c-info-static-color);
-        --indicator-border-color: var(--c-info-border-color);
+        background-color: var(--c-color-neutral-fill-quiet);
+        border: 1px solid var(--c-color-neutral-border-normal);
       }
     `,
   ];
 
-  @property()
+  @property({reflect: true})
   variant: VariantKey | 'empty' = Variant.Default;
 
   @property()
