d23d7df423859bb027a2f1d485f401dfa557698d
Rias <hey@rias.be>
2026-05-19 20:59:28 +0200

    Just add a phpstan-ignore

--- yii2-adapter/legacy/mail/Mailer.php
+++ yii2-adapter/legacy/mail/Mailer.php
@@ -179,12 +179,7 @@ class Mailer extends \yii\symfonymailer\Mailer
 
                 $message->language = $rendered->language;
                 $message->setSubject($rendered->subject);
-                $textView = resource_path('views/mail/system-message-text.blade.php');
-                if (!file_exists($textView)) {
-                    throw new InvalidConfigException("The `$textView` view file does not exist.");
-                }
-
-                $message->setTextBody(view()->file($textView, $formatted->viewData)->render());
+                $message->setTextBody(view('mail.system-message-text', $formatted->viewData)->render()); // @phpstan-ignore argument.type
                 $message->setHtmlBody($formatted->htmlBody);
             }
 
