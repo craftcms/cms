df44ac3dd2b143576c2e2223b34842b758952264
Rias <hey@rias.be>
2026-05-21 13:50:21 +0200

    Fix phpstan

--- src/Cp/JsonResource.php
+++ src/Cp/JsonResource.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace CraftCms\Cms\Cp;
 
+use Override;
+
 class JsonResource extends \Illuminate\Http\Resources\Json\JsonResource
 {
-    #[\Override]
-    public static $wrap = false;
+    #[Override]
+    public static $wrap;
 }
