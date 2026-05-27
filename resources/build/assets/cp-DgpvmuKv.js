af3f6a7bc079f37a73f221863c7aa7a508efd909
Brian Hanson <brian@brianhanson.net>
2026-05-27 16:44:47 -0500

    Build

--- resources/build/assets/cp-DgpvmuKv.js
+++
@@ -1,1 +0,0 @@
-import"./cp-DpSwX5ei.js";import{t as e}from"./cp-tZjS4JPN.js";import"./CpGlobalSidebar-D3tqrxar.js";import"./CpQueueIndicator-Dee2JP4f.js";window.Cp={...window.Cp||{},...e},console.log(`window.Cp defined`,window.Cp);
