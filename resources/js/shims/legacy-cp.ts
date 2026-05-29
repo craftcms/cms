28e6f30623897c6aed19885da3bfc91e76c89f03
Brian Hanson <brian@brianhanson.net>
2026-05-29 15:04:20 -0500

    Remove comments

--- resources/js/shims/legacy-cp.ts
+++ resources/js/shims/legacy-cp.ts
@@ -7,20 +7,8 @@ import '../../legacy/animationblocker/dist/AnimationBlocker';
 // Axios (should be removed)
 import '../../legacy/axios/dist/axios';
 
-// D3 (hopefully remove)
-// import '../../legacy/d3/dist/'
-
-// Velocity
-// import '../../legacy/velocity/dist/velocity';
-
-// XRegex
-// import '../../legacy/xregexp/dist/xregexp-all.js';
-
 // IFrame resizer
 import '../../legacy/iframeresizer/dist/iframeResizer';
-
-// Fabric
-// import '../../legacy/fabric/dist/fabric';
 
 // JQuery (+ Garnish and plugins)
 import './legacy-jquery.js';
