0a2596c3a0aa66af46ed3d26e29e4e16649d3cc4
brandonkelly <brandon@pixelandtonic.com>
2026-06-01 15:52:00 -0700

    Fixed tests

--- tests/unit/helpers/dbhelper/MysqlDbHelperTest.php
+++ tests/unit/helpers/dbhelper/MysqlDbHelperTest.php
@@ -131,10 +131,22 @@ class MysqlDbHelperTest extends TestCase
                 ['not', ['or', ['foo' => null], ['foo' => '']]], 'foo', ':NOTEMPTY:',
             ],
             [
-                ['not', ['or', ['foo' => null], ['foo' => '']]], 'foo', 'not :empty:',
+                [
+                    'or',
+                    ['foo' => null],
+                    ['not', ['or', ['foo' => null], ['foo' => '']]],
+                ],
+                'foo',
+                'not :empty:',
             ],
             [
-                ['not', ['or', ['foo' => null], ['foo' => '']]], 'foo', 'NOT :EMPTY:',
+                [
+                    'or',
+                    ['foo' => null],
+                    ['not', ['or', ['foo' => null], ['foo' => '']]],
+                ],
+                'foo',
+                'NOT :EMPTY:',
             ],
         ];
     }
