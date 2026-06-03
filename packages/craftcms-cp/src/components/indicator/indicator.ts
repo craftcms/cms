3de00ff6fd97bfb5dc77c52870f4eb24b5b41270
Brian Hanson <brian@brianhanson.net>
2026-06-03 12:05:10 -0500

    Expand indicator component

--- packages/craftcms-cp/src/components/indicator/indicator.ts
+++ packages/craftcms-cp/src/components/indicator/indicator.ts
@@ -4,46 +4,100 @@ import {Variant, type VariantKey} from '@src/types';
 import {classMap} from 'lit/directives/class-map.js';
 import variantsStyles from '@src/styles/variants.styles';
 
+/**
+ * @summary Indicators are used to visually represent the status of an object.
+ * Most of the time, you won't want to use the component directly but instead
+ * should use one of the status components.
+ *
+ * @since 1.0
+ */
 export default class CraftIndicator extends LitElement {
   static override styles = [
     variantsStyles,
     css`
       .indicator {
+        --_fill: var(--fill, var(--c-color-fill-loud));
+        --_size: var(--size, 0.5em);
         display: inline-flex;
         aspect-ratio: 1;
-        width: var(--c-indicator-size, 0.5em);
+        width: var(--_size);
         border-radius: var(--c-radius-full);
-        color: var(--c-color-on-loud);
-        background-color: var(--c-color-fill-loud);
-        border: 1px solid var(--c-color-border-loud);
+        background: var(--_fill);
+        border: 1px solid var(--_fill);
       }
 
-      .indicator--empty {
-        background-color: var(--c-color-neutral-fill-quiet);
-        border: 1px solid var(--c-color-neutral-border-normal);
+      /* Appearances */
+      :host([appearance~='filled-outlined']) .indicator {
+        background: var(--_fill);
+        border: 1px solid rgba(0, 0, 0, 0.5);
+      }
+
+      :host([appearance~='filled']) .indicator {
+        background: var(--_fill);
+        border-color: transparent;
+      }
+
+      :host([appearance~='outlined']) .indicator {
+        background: transparent;
+        border: 2px solid var(--_fill);
       }
     `,
   ];
 
-  @property({reflect: true})
-  variant: VariantKey | 'empty' = Variant.Default;
+  @property()
+  size: 'md' | 'lg' = 'md';
+
+  /** @phpType {Color|string} */
+  @property()
+  fill: string = 'var(--c-color-fill-loud)';
 
   @property()
   label: string | null = null;
 
+  @property({reflect: true})
+  appearance: 'filled' | 'outlined' | 'filled-outlined' = 'filled-outlined';
+
+  getFill() {
+    switch (this.fill) {
+      case 'live':
+      case Variant.Success:
+        return 'var(--c-color-success-fill-loud)';
+      case Variant.Warning:
+        return 'var(--c-color-warning-fill-loud)';
+      case Variant.Danger:
+        return 'var(--c-color-danger-fill-loud)';
+      case Variant.Info:
+        return 'var(--c-color-info-fill-loud)';
+      case 'draft':
+      case 'default':
+        return 'var(--c-color-neutral-fill-loud)';
+
+      default:
+        return this.fill;
+    }
+  }
+
+  getSize() {
+    switch (this.size) {
+      case 'md':
+        return '0.6em';
+      case 'lg':
+        return '1em';
+      default:
+        return this.size;
+    }
+  }
+
   protected override render(): unknown {
     return html`<span
+      style="--fill: ${this.getFill()}; --size: ${this.getSize()}"
+      aria-label="${this.label}"
+      role="img"
       class="${classMap({
         indicator: true,
-        'indicator--success': this.variant === Variant.Success,
-        'indicator--danger': this.variant === Variant.Danger,
-        'indicator--warning': this.variant === Variant.Warning,
-        'indicator--info': this.variant === Variant.Info,
-        'indicator--empty': this.variant === 'empty',
+        'indicator--outlined': this.appearance === 'outlined',
       })}"
-    >
-      <slot></slot>
-    </span>`;
+    ></span>`;
   }
 }
 
