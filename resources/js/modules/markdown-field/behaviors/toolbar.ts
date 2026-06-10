54efbc90950f8ef7dbff7ab3d89244f8566049e3
Rias <hey@rias.be>
2026-06-10 20:33:36 +0200

    Consistent labels

--- resources/js/modules/markdown-field/behaviors/toolbar.ts
+++ resources/js/modules/markdown-field/behaviors/toolbar.ts
@@ -167,9 +167,24 @@ function toolbarButtonGroups(
       customizeToolbarButton(toolbarButtons.code, t('Code'), 'code'),
     ],
     [
-      customizeToolbarButton(toolbarButtons.h1, t('H1'), 'h1', 'heading-1'),
-      customizeToolbarButton(toolbarButtons.h2, t('H2'), 'h2', 'heading-2'),
-      customizeToolbarButton(toolbarButtons.h3, t('H3'), 'h3', 'heading-3'),
+      customizeToolbarButton(
+        toolbarButtons.h1,
+        t('Heading 1'),
+        'h1',
+        'heading-1'
+      ),
+      customizeToolbarButton(
+        toolbarButtons.h2,
+        t('Heading 2'),
+        'h2',
+        'heading-2'
+      ),
+      customizeToolbarButton(
+        toolbarButtons.h3,
+        t('Heading 3'),
+        'h3',
+        'heading-3'
+      ),
       headingButton(4, t('Heading 4')),
       headingButton(5, t('Heading 5')),
       headingButton(6, t('Heading 6')),
