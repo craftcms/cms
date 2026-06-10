7c7f7f55edc5b56e5db6155dffee1d69600ad7c1
i-just <iwona@pixelandtonic.com>
2026-06-10 13:50:13 +0200

    phpstan

--- src/Database/Migrations/2026_06_10_112441_drop_assets_alt_column.php
+++ src/Database/Migrations/2026_06_10_112441_drop_assets_alt_column.php
@@ -1,7 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
+use CraftCms\Cms\Database\Migration;
 use CraftCms\Cms\Database\Table;
-use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
