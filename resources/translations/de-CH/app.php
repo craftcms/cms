15664f70f75c7c2d25406d0147d61c0394d25387
shinybrad <83238715+shinybrad@users.noreply.github.com>
2026-06-21 06:55:29 -0700

    New translations app.php (German, Switzerland)
    [ci skip]
    [ci skip]

--- resources/translations/de-CH/app.php
+++ resources/translations/de-CH/app.php
@@ -132,7 +132,7 @@ return [
     'Announcements' => 'Ankündigungen',
     'Another page already has that name.' => 'Eine andere Seite hat diesen Namen bereits.',
     'Any changes will be lost if you leave this page.' => 'Änderungen werden beim Verlassen dieser Seite nicht gespeichert.',
-    'Anything cached with {method}' => 'Anything cached with {method}',
+    'Anything cached with {method}' => 'Alles, was mit {method} gecacht ist',
     'Application Info' => 'Anwendungsinformationen',
     'Applied new migrations successfully.' => 'Neue Migrationen wurden erfolgreich angewendet.',
     'Applied “{name}”' => 'Verwende «{name}»',
