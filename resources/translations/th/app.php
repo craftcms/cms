dac961125bee0a552908b12279326217acb7aaab
shinybrad <83238715+shinybrad@users.noreply.github.com>
2026-06-21 06:21:19 -0700

    New translations app.php (Thai)
    [ci skip]
    [ci skip]

--- resources/translations/th/app.php
+++ resources/translations/th/app.php
@@ -132,7 +132,7 @@ return [
     'Announcements' => 'ประกาศ',
     'Another page already has that name.' => 'มีหน้าอื่นใช้ชื่อนี้แล้ว',
     'Any changes will be lost if you leave this page.' => 'การเปลี่ยนแปลงใดๆ จะสูญหายไป หากคุณออกจากหน้านี้',
-    'Anything cached with {method}' => 'ทั้งหมดที่แคชด้วย {method}',
+    'Anything cached with {method}' => 'Anything cached with {method}',
     'Application Info' => 'ข้อมูลแอปพลิเคชัน',
     'Applied new migrations successfully.' => 'ใช้การโอนย้ายข้อมูลใหม่เรียบร้อยแล้ว',
     'Applied “{name}”' => 'ใช้ “{name}” แล้ว',
