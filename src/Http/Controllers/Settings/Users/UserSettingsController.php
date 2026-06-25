a68ab5052e01199911b3c1ee30f81fa99792aa6c
Rias <hey@rias.be>
2026-06-25 17:19:22 +0200

    Fix missing closing bracket

--- src/Http/Controllers/Settings/Users/UserSettingsController.php
+++ src/Http/Controllers/Settings/Users/UserSettingsController.php
@@ -88,6 +88,7 @@ class UserSettingsController extends BaseUserSettingsController
             ])
             ->sortBy('label')
             ->values();
+    }
 
     private function userGroupOptions(): Collection
     {
