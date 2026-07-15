fb560d68a1d4f3a4e02b360c5bcacde27780fa05
Rias <hey@rias.be>
2026-07-15 22:10:25 +0200

    Encrypt recovery codes

--- src/Cms.php
+++ src/Cms.php
@@ -30,7 +30,7 @@ readonly class Cms
 
     public const string VERSION = '6.0.0-alpha.11';
 
-    public const string SCHEMA_VERSION = '6.0.0.3';
+    public const string SCHEMA_VERSION = '6.0.0.4';
 
     public const string MIN_VERSION_REQUIRED = '5.9.0';
 
