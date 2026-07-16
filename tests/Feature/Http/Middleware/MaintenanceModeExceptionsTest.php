efd5efed23b5c2abb8e0d35d7ece11f23de46733
Rias <hey@rias.be>
2026-07-16 20:57:18 +0200

    Fix 503 on updater when URL contains a query string

--- tests/Feature/Http/Middleware/MaintenanceModeExceptionsTest.php
+++ tests/Feature/Http/Middleware/MaintenanceModeExceptionsTest.php
@@ -3,7 +3,9 @@
 declare(strict_types=1);
 
 use CraftCms\Cms\Cms;
+use CraftCms\Cms\Support\Json;
 use CraftCms\Cms\User\Elements\User;
+use Illuminate\Support\Facades\Crypt;
 
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\post;
@@ -31,6 +33,20 @@ test('migrate action route is accessible during maintenance mode', function () {
     $actionTrigger = Cms::config()->actionTrigger;
 
     $response = post("/{$actionTrigger}/migrate");
+
+    expect($response->status())->not->toBe(503);
+});
+
+test('updater action routes with query strings are accessible during maintenance mode', function () {
+    $cpTrigger = Cms::config()->cpTrigger;
+    $actionTrigger = Cms::config()->actionTrigger;
+    $data = Crypt::encrypt(Json::encode([
+        'postPrecheckState' => [],
+    ]));
+
+    $response = postJson("/{$cpTrigger}/{$actionTrigger}/updater/precheck?site=default", [
+        'data' => $data,
+    ]);
 
     expect($response->status())->not->toBe(503);
 });
