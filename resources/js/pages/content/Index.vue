953a91dbf16fdda730fa4d76d417e940b22564a0
Guadalupe Camacho <lupe@pixelandtonic.com>
2026-07-28 13:10:17 -0700

    Add translation for secondary navigation, use as nav label for consistency with skip link label

--- resources/js/pages/content/Index.vue
+++ resources/js/pages/content/Index.vue
@@ -219,10 +219,7 @@
   </LayoutSlot>
 
   <LayoutSlot name="sidebar">
-    <nav aria-labelledby="source-heading">
-      <h2 id="source-heading" class="sr-only">
-        {{ t('Sources') }}
-      </h2>
+    <nav :aria-label="t('Secondary')">
       <ElementSources
         :sources="sources"
         :route="route"
