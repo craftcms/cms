38c95edfbccd85f8b2a6b846f3cd169c9229494b
Brian Hanson <brian@brianhanson.net>
2026-07-31 12:07:48 -0500

    Initial port of users index

--- workbench/app/Providers/TypeScriptTransformerServiceProvider.php
+++ workbench/app/Providers/TypeScriptTransformerServiceProvider.php
@@ -15,6 +15,7 @@ use CraftCms\Cms\Http\ViewModels\EntryIndexViewModel;
 use CraftCms\Cms\Http\ViewModels\FieldEditViewModel;
 use CraftCms\Cms\Http\ViewModels\FilesystemsEditViewModel;
 use CraftCms\Cms\Http\ViewModels\UserAddressesViewModel;
+use CraftCms\Cms\Http\ViewModels\UserIndexViewModel;
 use CraftCms\Cms\Http\ViewModels\UserPermissionsViewModel;
 use CraftCms\Cms\Http\ViewModels\UserPreferencesViewModel;
 use CraftCms\Cms\Http\ViewModels\UserProfileViewModel;
@@ -64,6 +65,7 @@ class TypeScriptTransformerServiceProvider extends TypeScriptTransformerApplicat
                     EntryIndexViewModel::class,
                     FieldEditViewModel::class,
                     UserAddressesViewModel::class,
+                    UserIndexViewModel::class,
                     UserPermissionsViewModel::class,
                     UserPreferencesViewModel::class,
                     UserProfileViewModel::class,
