991b950670fa2bbae428aff856803f318bcb21fe
brandonkelly <brandon@pixelandtonic.com>
2026-08-05 08:14:16 -0700

    Simplify the migration
    Only create missing assets_sites rows for assets that have globally-defined alt text, and null/missing translated alt text

--- src/migrations/m260610_065719_translatable_alt_text_content.php
+++ src/migrations/m260610_065719_translatable_alt_text_content.php
@@ -6,7 +6,6 @@ use craft\db\Migration;
 use craft\db\Query;
 use craft\db\Table;
 use craft\helpers\Db;
-use craft\records\Asset;
 
 /**
  * m260610_065719_translatable_alt_text_content migration.
@@ -18,49 +17,39 @@ class m260610_065719_translatable_alt_text_content extends Migration
      */
     public function safeUp(): bool
     {
-        // get all asset records - we only need the id, alt and siteId
-        $query = Asset::find()
-            ->leftJoin('{{%elements_sites}}', '[[elements_sites.elementId]] = [[assets.id]]')
-            ->select(['assets.id', 'elements_sites.siteId', 'assets.alt']);
+        // Find assets that have globally-defined alt text and null/missing translated alt text,
+        // and fill in their translated values.
 
-        $assetsRecords = $query->createCommand()->queryAll();
+        $query = (new Query())
+            // fetch assets_sites.assetId so we know whether the row already exists
+            ->select(['assets.id', 'elements_sites.siteId', 'assets.alt', 'assets_sites.assetId'])
+            ->from(['assets' => Table::ASSETS])
+            ->innerJoin(['elements_sites' => Table::ELEMENTS_SITES], '[[elements_sites.elementId]] = [[assets.id]]')
+            ->leftJoin(['assets_sites' => Table::ASSETS_SITES], [
+                'and',
+                '[[assets_sites.assetId]] = [[assets.id]]',
+                '[[assets_sites.siteId]] = [[elements_sites.siteId]]',
+            ])
+            ->where(['not', ['assets.alt' => null]])
+            ->andWhere(['not', ['assets.alt' => '']])
+            ->andWhere(['assets_sites.alt' => null]);
 
-        // for each of those records,
-        foreach ($assetsRecords as $assetRecord) {
-            $exists = (new Query())
-                ->from(Table::ASSETS_SITES)
-                ->where([
-                        'assetId' => $assetRecord['id'],
-                        'siteId' => $assetRecord['siteId'], ]
-                )
-                ->exists();
-
-            if ($exists) {
-                // if the assets_sites row exists and the alt column is null, copy over the value from the assets.alt
-                Db::update(
-                    Table::ASSETS_SITES,
-                    [
-                        'alt' => $assetRecord['alt'],
-                    ],
-                    [
-                        'assetId' => $assetRecord['id'],
-                        'siteId' => $assetRecord['siteId'],
-                        'alt' => null,
-                    ]
-                );
+        foreach (Db::each($query) as $row) {
+            // If the assets_sites.assetId value came back, the row already exists
+            if (isset($row['assetId'])) {
+                Db::update(Table::ASSETS_SITES, [
+                    'alt' => $row['alt'],
+                ], [
+                    'assetId' => $row['id'],
+                    'siteId' => $row['siteId'],
+                ]);
             } else {
-                // if the assets_sites row doesn't exist, insert one with the assets.alt value
-                Db::insert(
-                    Table::ASSETS_SITES,
-                    [
-                        'assetId' => $assetRecord['id'],
-                        'siteId' => $assetRecord['siteId'],
-                        'alt' => $assetRecord['alt'],
-                    ]
-                );
+                Db::insert(Table::ASSETS_SITES, [
+                    'assetId' => $row['id'],
+                    'siteId' => $row['siteId'],
+                    'alt' => $row['alt'],
+                ]);
             }
-            // the above ensures we're not changing the alt text developers might expect during the update,
-            // but at the same time we're all set to no longer use the assets.alt column
         }
 
         return true;
