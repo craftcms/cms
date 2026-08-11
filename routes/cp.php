7224ce199777fdee8b7a44e43c3503ae301a7e37
Rias <hey@rias.be>
2026-08-11 20:46:40 +0200

    Port section editing to Form

--- routes/cp.php
+++ routes/cp.php
@@ -384,7 +384,10 @@ Route::middleware(['auth', 'can:accessCp'])->group(function () {
         // Sections
         Route::get('settings/sections', [SectionsController::class, 'index'])
             ->name('settings.sections.index');
-        Route::middleware(RequireAdminChanges::class)->get('settings/sections/new', [SectionsController::class, 'create']);
+        Route::middleware(RequireAdminChanges::class)->group(function () {
+            Route::get('settings/sections/new', [SectionsController::class, 'create']);
+            Route::post('settings/sections/render-form', [SectionsController::class, 'renderForm']);
+        });
         Route::get('settings/sections/{section}', [SectionsController::class, 'edit']);
         Route::middleware(RequireAdminChanges::class)->delete('settings/sections/{section}', [SectionsController::class, 'destroy']);
         Route::middleware(RequireAdminChanges::class)->post('sections/sections', [SectionsController::class, 'store']);
