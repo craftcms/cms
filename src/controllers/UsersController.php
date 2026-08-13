f6f0d2b0b1e5280e2b4ab20acea3d64116668861
brandonkelly <brandon@pixelandtonic.com>
2026-08-12 17:54:03 -0700

    Fixed GHSA-w5rh-mhxj-wr8r

--- src/controllers/UsersController.php
+++ src/controllers/UsersController.php
@@ -2557,6 +2557,14 @@ JS);
         }
 
         $returnUrl = $this->request->getQueryParam('returnUrl');
+
+        if ($returnUrl) {
+            $returnUrl = preg_replace('/[\t\r\n]/', '', trim($returnUrl));
+            if (StringHelper::startsWith($returnUrl, 'javascript:', false)) {
+                $returnUrl = null;
+            }
+        }
+
         if (!$returnUrl) {
             if ($this->request->getIsCpRequest()) {
                 // explicitly set the default return URL here, since checkPermission('accessCp') will be false
