5fa311ae2f2afb50ce77740a50fcc077c4787c8f
Rias <hey@rias.be>
2026-08-22 16:40:47 +0200

    Support uploader event callbacks

--- resources/js/common/types/globals.d.ts
+++ resources/js/common/types/globals.d.ts
@@ -14,7 +14,7 @@ type LegacySettingValue =
   | JQuery
   | LegacyWidgetSettings
   | LegacySettingValue[]
-  | (() => void);
+  | ((...args: any[]) => void);
 
 interface LegacyWidgetSettings {
   [key: string]: LegacySettingValue;
