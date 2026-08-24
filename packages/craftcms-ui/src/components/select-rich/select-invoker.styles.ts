7bc6b152dab37c4f5bc28e8c590ba2742e9b10e7
Guadalupe Camacho <lupe@pixelandtonic.com>
2026-08-24 11:43:22 -0700

    Update select styles

--- packages/craftcms-ui/src/components/select-rich/select-invoker.styles.ts
+++ packages/craftcms-ui/src/components/select-rich/select-invoker.styles.ts
@@ -1,19 +1,18 @@
 import {css} from 'lit';
+import {baseComboboxStyles} from '@src/styles/form.styles';
 
 export default css`
   :host {
+    ${baseComboboxStyles}
     display: flex;
     align-items: center;
     justify-content: space-between;
     gap: var(--c-spacing-sm);
-    width: 100%;
     cursor: pointer;
     padding-block: 0;
     padding-inline: var(--c-input-spacing-inline);
-    min-height: calc(var(--c-input-height, var(--c-size-control-md)) - 2px);
     font: inherit;
     overflow: clip;
-    border-radius: var(--c-input-radius, var(--c-radius-sm));
   }
 
   :host([disabled]) {
