9c0f601caeb6c6951d243a7fba67f8f9f002397d
i-just <iwona@pixelandtonic.com>
2026-09-01 09:06:55 +0200

    make positive lookahead non-optional

--- tests/unit/helpers/AppHelperTest.php
+++ tests/unit/helpers/AppHelperTest.php
@@ -135,6 +135,13 @@ class AppHelperTest extends TestCase
         self::assertNull(App::parseEnv('$TEST_MISSING'));
         self::assertNull(App::parseEnv(null));
 
+        // https://github.com/craftcms/cms/issues/19522
+        self::assertSame('$58 million', App::parseEnv('$58 million'));
+        self::assertSame('the $58 million', App::parseEnv('the $58 million'));
+        self::assertSame('test/', App::parseEnv('test/$58'));
+        self::assertSame('/test', App::parseEnv('$58/test'));
+        self::assertSame('test//test', App::parseEnv('test/$58/test'));
+
         foreach (array_keys($variables) as $name) {
             putenv($name);
         }
