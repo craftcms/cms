e7275335829bf114a478d4b3df4fd374ec3bba93
Rias <hey@rias.be>
2026-09-01 14:11:54 +0200

    Remove skip

--- tests/Arch/ArchTest.php
+++ tests/Arch/ArchTest.php
@@ -11,18 +11,15 @@ use Illuminate\Support\Env;
 use Illuminate\Support\Str;
 
 arch('No debug functions')
-    ->skip(fn () => getenv('SKIP_ARCH'))
     ->expect('src')
     ->not->toUse(['die', 'dd', 'dump', 'env']);
 
 arch('Don\'t use legacy logging')
-    ->skip(fn () => getenv('SKIP_ARCH'))
     ->expect(['Craft::info', 'Craft::warning', 'Craft::debug', 'Craft::error', 'Craft::trace'])
     ->not()
     ->toBeUsed();
 
 arch('Don\'t use legacy translations')
-    ->skip(fn () => getenv('SKIP_ARCH'))
     ->expect('Craft::t')
     ->not()
     ->toBeUsed();
@@ -31,35 +28,30 @@ arch('Don\'t use legacy translations')
  * We only want our own Env helpers to be used.
  */
 arch()
-    ->skip(fn () => getenv('SKIP_ARCH'))
     ->expect(Env::class)
     ->not
     ->toBeUsed()
     ->ignoring(CraftCms\Cms\Support\Env::class);
 
 arch()
-    ->skip(fn () => getenv('SKIP_ARCH'))
     ->expect(Arr::class)
     ->not
     ->toBeUsedIn('src')
     ->ignoring(CraftCms\Cms\Support\Arr::class);
 
 arch()
-    ->skip(fn () => getenv('SKIP_ARCH'))
     ->expect(Illuminate\Support\Facades\File::class)
     ->not
     ->toBeUsedIn('src')
     ->ignoring(File::class);
 
 arch()
-    ->skip(fn () => getenv('SKIP_ARCH'))
     ->expect(Str::class)
     ->not
     ->toBeUsedIn('src')
     ->ignoring(CraftCms\Cms\Support\Str::class);
 
 arch('Only use JSON helper')
-    ->skip(fn () => getenv('SKIP_ARCH'))
     ->expect(['json_encode', 'json_decode'])
     ->not
     ->toBeUsed()
@@ -69,14 +61,12 @@ arch('Only use JSON helper')
     ]);
 
 arch('Don\'t use default migrator')
-    ->skip(fn () => getenv('SKIP_ARCH'))
     ->expect(Illuminate\Database\Migrations\Migrator::class)
     ->not
     ->toBeUsed()
     ->ignoring(Migrator::class);
 
 arch('Don\'t use legacy aliases')
-    ->skip(fn () => getenv('SKIP_ARCH'))
     ->expect(['Craft::getAlias', 'Craft::setAlias', 'Craft::getRootAlias'])
     ->not()
     ->toBeUsed();
