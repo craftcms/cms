a46d4e593e7ab7261f8b9a4ea8c2cc5ee93675c1
i-just <iwona@pixelandtonic.com>
2026-09-01 09:12:07 +0200

    more tests

--- tests/unit/helpers/AppHelperTest.php
+++ tests/unit/helpers/AppHelperTest.php
@@ -138,9 +138,10 @@ class AppHelperTest extends TestCase
         // https://github.com/craftcms/cms/issues/19522
         self::assertSame('$58 million', App::parseEnv('$58 million'));
         self::assertSame('the $58 million', App::parseEnv('the $58 million'));
-        self::assertSame('test/', App::parseEnv('test/$58'));
+        self::assertSame('the $58', App::parseEnv('the $58'));
         self::assertSame('/test', App::parseEnv('$58/test'));
         self::assertSame('test//test', App::parseEnv('test/$58/test'));
+        self::assertSame('test/', App::parseEnv('test/$58'));
 
         foreach (array_keys($variables) as $name) {
             putenv($name);
