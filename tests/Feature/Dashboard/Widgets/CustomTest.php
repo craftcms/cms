a67760aa0506751913a70f45bb22b2346b0daeb9
Rias <hey@rias.be>
2026-09-05 13:30:09 +0200

    Isolate custom widget fixtures for parallel tests

--- tests/Feature/Dashboard/Widgets/CustomTest.php
+++ tests/Feature/Dashboard/Widgets/CustomTest.php
@@ -18,13 +18,17 @@ use function Pest\Laravel\get;
 use function Pest\Laravel\postJson;
 
 beforeEach(function () {
+    $this->originalBasePath = app()->basePath();
+    $this->fixturePath = sys_get_temp_dir().'/craft-custom-widgets-'.bin2hex(random_bytes(8));
+    app()->setBasePath($this->fixturePath);
+
     $this->widgetsPath = resource_path('widgets');
     File::ensureDirectoryExists($this->widgetsPath);
-    File::cleanDirectory($this->widgetsPath);
 });
 
 afterEach(function () {
-    File::deleteDirectory($this->widgetsPath);
+    app()->setBasePath($this->originalBasePath);
+    File::deleteDirectory($this->fixturePath);
 });
 
 it('discovers top-level Markdown files', function () {
