d1a876a8fad40a3a210f8cd1f257129d2d34fa4c
Rias <hey@rias.be>
2026-09-06 21:45:05 +0200

    Fix nested activity normalization and retry test

--- src/Activity/EntryActivity.php
+++ src/Activity/EntryActivity.php
@@ -242,6 +242,6 @@ class EntryActivity
             return false;
         }
 
-        return array_all($value, fn ($item) => self::normalizeSafeValue($item));
+        return array_all(array_keys($value), fn ($key) => self::normalizeSafeValue($value[$key]));
     }
 }
