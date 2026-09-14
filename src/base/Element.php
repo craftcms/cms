acc86892d8f1411cb8cef8dd2e1f521cd2e8b960
brandonkelly <brandon@pixelandtonic.com>
2026-09-14 11:39:14 -0700

    Make Content Block sub-fields bulk editable
    Resolves #19602

--- src/base/Element.php
+++ src/base/Element.php
@@ -6237,6 +6237,15 @@ JS,
 
     private function contentBlockAttributeHtml(string $attribute): string
     {
+        [$block, , $nestedAttribute] = $this->contentBlockFromAttribute($attribute);
+        return $block?->getAttributeHtml($nestedAttribute) ?? '';
+    }
+
+    /**
+     * @return array{0:ContentBlock|null,1:ContentBlockField|null,2:string|null}
+     */
+    private function contentBlockFromAttribute(string $attribute): array
+    {
         $parts = explode('.', $attribute);
         $uid = StringHelper::removeLeft(array_shift($parts), 'contentBlock:');
 
@@ -6254,11 +6263,13 @@ JS,
         }
 
         if (!$field instanceof ContentBlockField) {
-            return '';
+            return [null, null, null];
         }
 
         $block = $this->getFieldValue($field->handle);
-        return $block?->getAttributeHtml(implode('.', $parts)) ?? '';
+        $nestedAttribute = implode('.', $parts);
+
+        return [$block, $field, $nestedAttribute];
     }
 
     /**
@@ -6390,6 +6401,12 @@ JS,
             }
 
             $field ??= $this->_getFieldFromAlternativeLayouts($instanceUid) ?? null;
+        } elseif (str_starts_with($attribute, 'contentBlock:')) {
+            [$block, $field, $nestedAttribute] = $this->contentBlockFromAttribute($attribute);
+            if (!$block) {
+                return '';
+            }
+            return Craft::$app->getView()->namespaceInputs(fn() => $block->getInlineAttributeInputHtml($nestedAttribute), "$field->handle[fields]");
         }
 
         if ($field !== null) {
