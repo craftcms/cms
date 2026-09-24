34b8ece54cf4c279f98dd5abce0834a5ba8973d8
brandonkelly <brandon@pixelandtonic.com>
2026-09-24 10:49:11 -0700

    Missing `.`

--- resources/js/modules/elements/components/BaseElementIndex.vue
+++ resources/js/modules/elements/components/BaseElementIndex.vue
@@ -230,7 +230,7 @@
   }
 
   .element-index__header,
-  element-index__navbar,
+  .element-index__navbar,
   .element-index__body,
   .element-index__footer {
     padding-inline: var(--cp-container-padding);
