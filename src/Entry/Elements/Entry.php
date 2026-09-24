8b946790068c17ca7863ebca2bfc0d11c512f2c2
Rias <hey@rias.be>
2026-09-24 18:20:31 +0200

    Use slug control for inline entry editing

--- src/Entry/Elements/Entry.php
+++ src/Entry/Elements/Entry.php
@@ -61,7 +61,6 @@ use CraftCms\Cms\Form\Controls\Choice;
 use CraftCms\Cms\Form\Controls\DateTime;
 use CraftCms\Cms\Form\Controls\ElementSelect;
 use CraftCms\Cms\Form\Controls\Slug;
-use CraftCms\Cms\Form\Controls\Text;
 use CraftCms\Cms\Form\Enums\ControlMode;
 use CraftCms\Cms\Form\Form;
 use CraftCms\Cms\Form\Nodes\Field;
@@ -2100,7 +2099,7 @@ JS, [
                 ->value(self::dateTimeControlValue($this->$attribute === null
                     ? null
                     : Date::instance($this->$attribute)->setTimezone(Cms::timezone()))),
-            'slug' => Text::make('slug')->value($this->slug),
+            'slug' => Slug::make('slug')->value($this->slug),
             default => null,
         };
 
